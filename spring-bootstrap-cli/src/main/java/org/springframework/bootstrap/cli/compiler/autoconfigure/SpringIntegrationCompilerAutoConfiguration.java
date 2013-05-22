0a780c5c26d31f851a0fe90c8d2b455bc38d0518
Greg Turnquist <gturnquist@gopivotal.com>
2013-05-22 16:33:34 -0500

    Fixed name of annotation and a typo in the code.

--- spring-bootstrap-cli/src/main/java/org/springframework/bootstrap/cli/compiler/autoconfigure/SpringIntegrationCompilerAutoConfiguration.java
+++ spring-bootstrap-cli/src/main/java/org/springframework/bootstrap/cli/compiler/autoconfigure/SpringIntegrationCompilerAutoConfiguration.java
@@ -29,7 +29,7 @@ import org.springframework.bootstrap.cli.compiler.CompilerAutoConfiguration;
 import org.springframework.bootstrap.cli.compiler.DependencyCustomizer;
 
 /**
- * {@link CompilerAutoConfiguration} for Spring Batch.
+ * {@link CompilerAutoConfiguration} for Spring Integration.
  * 
  * @author Dave Syer
  */
@@ -39,7 +39,7 @@ public class SpringIntegrationCompilerAutoConfiguration extends CompilerAutoConf
 	public boolean matches(ClassNode classNode) {
 		// Slightly weird detection algorithm because there is no @Enable annotation for
 		// Integration
-		return AstUtils.hasLeastOneAnnotation(classNode, "MessageEndpoint",
+		return AstUtils.hasAtLeastOneAnnotation(classNode, "MessageEndpoint",
 				"EnableIntegrationPatterns");
 	}
 
