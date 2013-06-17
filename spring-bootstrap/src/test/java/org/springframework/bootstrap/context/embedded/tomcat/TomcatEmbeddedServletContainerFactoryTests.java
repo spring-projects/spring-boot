f97f5544aab54cdefc819c492e4599793a0e13a1
Greg Turnquist <gturnquist@gopivotal.com>
2013-06-17 09:36:37 -0400

    [BS-157] Fix tests that used the name as well
    Overlooked three test classes that invoked the misnamed method.

--- spring-bootstrap/src/test/java/org/springframework/bootstrap/context/embedded/tomcat/TomcatEmbeddedServletContainerFactoryTests.java
+++ spring-bootstrap/src/test/java/org/springframework/bootstrap/context/embedded/tomcat/TomcatEmbeddedServletContainerFactoryTests.java
@@ -48,7 +48,7 @@ public class TomcatEmbeddedServletContainerFactoryTests extends
 		}
 		factory.setContextLifecycleListeners(Arrays.asList(listeners[0], listeners[1]));
 		factory.addContextLifecycleListeners(listeners[2], listeners[3]);
-		this.container = factory.getEmbdeddedServletContainer();
+		this.container = factory.getEmbeddedServletContainer();
 		InOrder ordered = inOrder((Object[]) listeners);
 		for (LifecycleListener listener : listeners) {
 			ordered.verify(listener).lifecycleEvent((LifecycleEvent) anyObject());
