17cbe9267282d8e48e0689a2da74a54f3a817ab5
Phillip Webb <pwebb@vmware.com>
2013-07-30 16:20:36 -0700

    Renamed spring-boot-ups -> spring-boot-starter

--- spring-boot-cli/samples/ops.groovy
+++ spring-boot-cli/samples/ops.groovy
@@ -1,6 +1,6 @@
 package org.test
 
-@Grab("org.springframework.boot:spring-boot-up-ops:0.5.0.BUILD-SNAPSHOT")
+@Grab("org.springframework.boot:spring-boot-starter-ops:0.5.0.BUILD-SNAPSHOT")
 
 @Controller
 class SampleController {
--- spring-boot-samples/spring-boot-sample-ops/start.groovy
+++ spring-boot-samples/spring-boot-sample-ops/start.groovy
@@ -1,5 +1,5 @@
-@Grab("org.springframework.boot:spring-boot-up-ops:0.5.0.BUILD-SNAPSHOT")
-@Grab("org.springframework.boot:spring-boot-up-web:0.5.0.BUILD-SNAPSHOT")
+@Grab("org.springframework.boot:spring-boot-starter-ops:0.5.0.BUILD-SNAPSHOT")
+@Grab("org.springframework.boot:spring-boot-starter-web:0.5.0.BUILD-SNAPSHOT")
 @ComponentScan("org.springframework.boot.sample.service")
 class Start {
 }
