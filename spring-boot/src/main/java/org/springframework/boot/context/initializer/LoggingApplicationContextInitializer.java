169373c0c20313c566371bd7faea6366955d050f
Phillip Webb <pwebb@vmware.com>
2013-08-14 14:45:47 -0700

    Support --debug and --trace increased logging
    Update LoggingApplicationContextInitializer to detect the presence of
    '--debug' and '--trace' arguments to increase log output.
    
    Using '--debug' will set 'org.springframework.boot' loggers to DEBUG.
    
    Using '--trace' will set 'org.springframework', 'org.apache.tomcat' and
    'org.eclipse.jetty' loggers to TRACE.
    
    Issue: #55202588

--- spring-boot/src/main/java/org/springframework/boot/context/initializer/LoggingApplicationContextInitializer.java
+++ spring-boot/src/main/java/org/springframework/boot/context/initializer/LoggingApplicationContextInitializer.java
@@ -18,18 +18,24 @@ package org.springframework.boot.context.initializer;
 
 import java.lang.management.ManagementFactory;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.SpringApplicationInitializer;
+import org.springframework.boot.logging.LogLevel;
 import org.springframework.boot.logging.LoggingSystem;
 import org.springframework.context.ApplicationContextInitializer;
 import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.core.Ordered;
 import org.springframework.core.env.ConfigurableEnvironment;
 import org.springframework.core.env.Environment;
+import org.springframework.core.env.SimpleCommandLinePropertySource;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
+import org.springframework.util.ObjectUtils;
 import org.springframework.util.ResourceUtils;
 
 /**
@@ -76,17 +82,42 @@ public class LoggingApplicationContextInitializer implements
 		ENVIRONMENT_SYSTEM_PROPERTY_MAPPING.put("PID", "PID");
 	}
 
+	private static MultiValueMap<LogLevel, String> LOG_LEVEL_LOGGERS;
+	static {
+		LOG_LEVEL_LOGGERS = new LinkedMultiValueMap<LogLevel, String>();
+		LOG_LEVEL_LOGGERS.add(LogLevel.DEBUG, "org.springframework.boot");
+		LOG_LEVEL_LOGGERS.add(LogLevel.TRACE, "org.springframework");
+		LOG_LEVEL_LOGGERS.add(LogLevel.TRACE, "org.apache.tomcat");
+		LOG_LEVEL_LOGGERS.add(LogLevel.TRACE, "org.eclipse.jetty");
+	}
+
 	private final Log logger = LogFactory.getLog(getClass());
 
 	private int order = Integer.MIN_VALUE + 11;
+
+	private boolean parseArgs = true;
+
+	private LogLevel springBootLogging = null;
 
 	@Override
 	public void initialize(SpringApplication springApplication, String[] args) {
 		if (System.getProperty("PID") == null) {
 			System.setProperty("PID", getPid());
 		}
-		LoggingSystem.get(springApplication.getClass().getClassLoader())
-				.beforeInitialize();
+		LoggingSystem loggingSystem = LoggingSystem.get(springApplication.getClass()
+				.getClassLoader());
+		loggingSystem.beforeInitialize();
+		if (this.parseArgs && this.springBootLogging == null
+				&& !ObjectUtils.isEmpty(args)) {
+			SimpleCommandLinePropertySource parsedArgs = new SimpleCommandLinePropertySource(
+					args);
+			if (parsedArgs.containsProperty("debug")) {
+				this.springBootLogging = LogLevel.DEBUG;
+			}
+			if (parsedArgs.containsProperty("trace")) {
+				this.springBootLogging = LogLevel.TRACE;
+			}
+		}
 	}
 
 	/**
@@ -124,6 +155,18 @@ public class LoggingApplicationContextInitializer implements
 		}
 
 		system.initialize();
+		if (this.springBootLogging != null) {
+			initializeLogLeve(system, this.springBootLogging);
+		}
+	}
+
+	protected void initializeLogLeve(LoggingSystem system, LogLevel level) {
+		List<String> loggers = LOG_LEVEL_LOGGERS.get(level);
+		if (loggers != null) {
+			for (String logger : loggers) {
+				system.setLogLevel(logger, level);
+			}
+		}
 	}
 
 	private String getPid() {
@@ -143,4 +186,20 @@ public class LoggingApplicationContextInitializer implements
 		return this.order;
 	}
 
+	/**
+	 * Sets a custom logging level to be used for Spring Boot and related libraries.
+	 * @param springBootLogging the logging level
+	 */
+	public void setSpringBootLogging(LogLevel springBootLogging) {
+		this.springBootLogging = springBootLogging;
+	}
+
+	/**
+	 * Sets if initialization arguments should be parsed for {@literal --debug} and
+	 * {@literal --trace} options. Defaults to {@code true}.
+	 * @param parseArgs if arguments should be parsed
+	 */
+	public void setParseArgs(boolean parseArgs) {
+		this.parseArgs = parseArgs;
+	}
 }
