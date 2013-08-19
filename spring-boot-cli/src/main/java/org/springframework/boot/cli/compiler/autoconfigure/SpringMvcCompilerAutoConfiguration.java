d9f01dd85aa8a2237e169ab4872beb9ac7f8dc2d
Dave Syer <dsyer@gopivotal.com>
2013-08-19 16:00:50 +0100

    Restore Tomcat in CLI
    Seems to work. I think the problem was the race conditions
    that we hopefully already eliminated. I got an out of memory
    error running the samples tests in Eclipse, but then it went
    away again (something to look out for).
    
    [Fixes #54925992] [bs-280]

--- spring-boot-cli/src/main/java/org/springframework/boot/cli/compiler/autoconfigure/SpringMvcCompilerAutoConfiguration.java
+++ spring-boot-cli/src/main/java/org/springframework/boot/cli/compiler/autoconfigure/SpringMvcCompilerAutoConfiguration.java
@@ -43,9 +43,12 @@ public class SpringMvcCompilerAutoConfiguration extends CompilerAutoConfiguratio
 				.add("org.springframework", "spring-webmvc",
 						dependencies.getProperty("spring.version"));
 
-		dependencies.ifAnyMissingClasses("org.eclipse.jetty.server.Server").add(
-				"org.eclipse.jetty", "jetty-webapp",
-				dependencies.getProperty("jetty.version"));
+		dependencies
+				.ifAnyMissingClasses("org.apache.catalina.startup.Tomcat")
+				.add("org.apache.tomcat.embed", "tomcat-embed-core",
+						dependencies.getProperty("tomcat.version"))
+				.add("org.apache.tomcat.embed", "tomcat-embed-logging-juli",
+						dependencies.getProperty("tomcat.version"));
 
 		dependencies.add("org.codehaus.groovy", "groovy-templates",
 				dependencies.getProperty("groovy.version"));
