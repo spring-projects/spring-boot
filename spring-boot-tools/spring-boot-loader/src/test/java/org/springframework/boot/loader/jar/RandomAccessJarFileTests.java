5b7d56895b993437ef39a73734b271fcaf8ae513
Phillip Webb <pwebb@gopivotal.com>
2013-08-27 01:05:43 -0700

    Provide accurate InputStream.available() results
    Provide accurate InputStream.available() results by using the size
    attribute of the ZipEntry. This helps improve performance with
    CGLib and also fixes issues where libraries expect that a non-zero
    result from available() indicates that read() will not return -1.

--- spring-boot-tools/spring-boot-loader/src/test/java/org/springframework/boot/loader/jar/RandomAccessJarFileTests.java
+++ spring-boot-tools/spring-boot-loader/src/test/java/org/springframework/boot/loader/jar/RandomAccessJarFileTests.java
@@ -115,7 +115,9 @@ public class RandomAccessJarFileTests {
 	public void getInputStream() throws Exception {
 		InputStream inputStream = this.jarFile.getInputStream(this.jarFile
 				.getEntry("1.dat"));
+		assertThat(inputStream.available(), equalTo(1));
 		assertThat(inputStream.read(), equalTo(1));
+		assertThat(inputStream.available(), equalTo(0));
 		assertThat(inputStream.read(), equalTo(-1));
 	}
 
