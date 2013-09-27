f90c96b30f3bed39dbe920850f3778d6e8fc18e3
Dave Syer <dsyer@gopivotal.com>
2013-09-27 09:34:10 -0400

    Add spring.view.{prefix,suffix} properties
    Fixes #62

--- spring-boot-samples/spring-boot-sample-web-jsp/src/main/java/org/springframework/boot/sample/jsp/SampleWebJspApplication.java
+++ spring-boot-samples/spring-boot-sample-web-jsp/src/main/java/org/springframework/boot/sample/jsp/SampleWebJspApplication.java
@@ -18,24 +18,14 @@ package org.springframework.boot.sample.jsp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.view.InternalResourceViewResolver;
 
 @Configuration
 @EnableAutoConfiguration
 @ComponentScan
 public class SampleWebJspApplication {
-
-	@Bean
-	public InternalResourceViewResolver defaultViewResolver() {
-		InternalResourceViewResolver resolver = new InternalResourceViewResolver();
-		resolver.setPrefix("/WEB-INF/jsp/");
-		resolver.setSuffix(".jsp");
-		return resolver;
-	}
-
+	
 	public static void main(String[] args) throws Exception {
 		SpringApplication.run(SampleWebJspApplication.class, args);
 	}
