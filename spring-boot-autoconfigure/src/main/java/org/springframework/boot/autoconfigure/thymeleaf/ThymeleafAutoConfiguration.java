9afbd60d3e210ce1db94953216246c534231cdae
yanhua365 <yanhua365@gmail.com>
2013-10-09 21:43:07 +0800

    Make Thymeleaf TemplateResolver's characterEncoding can be configured.

--- spring-boot-autoconfigure/src/main/java/org/springframework/boot/autoconfigure/thymeleaf/ThymeleafAutoConfiguration.java
+++ spring-boot-autoconfigure/src/main/java/org/springframework/boot/autoconfigure/thymeleaf/ThymeleafAutoConfiguration.java
@@ -102,7 +102,8 @@ public class ThymeleafAutoConfiguration {
 			resolver.setTemplateMode(this.environment.getProperty("mode", "HTML5"));
 			resolver.setCacheable(this.environment.getProperty("cache", Boolean.class,
 					true));
-			resolver.setCharacterEncoding(this.environment.getProperty("encoding",null));
+			resolver.setCharacterEncoding(this.environment.getProperty("encoding", String.class,
+			                null));
 			return resolver;
 		}
 
