963104b649d9609c0857e34e43366405e30ba66d
Christian Dupuis <cdupuis@gopivotal.com>
2013-11-04 17:43:06 +0100

    Fix test assertion

--- spring-boot-actuator/src/test/java/org/springframework/boot/actuate/properties/CrshPropertiesTests.java
+++ spring-boot-actuator/src/test/java/org/springframework/boot/actuate/properties/CrshPropertiesTests.java
@@ -92,7 +92,7 @@ public class CrshPropertiesTests {
 		binder.bind(new MutablePropertyValues(Collections.singletonMap("shell.config_path_patterns",
 				"pattern1, pattern2")));
 		assertFalse(binder.getBindingResult().hasErrors());
-		assertEquals(2, props.getConfigPathPatterns().length, 2);
+		assertEquals(2, props.getConfigPathPatterns().length);
 		Assert.assertArrayEquals(new String[] { "pattern1", "pattern2" }, props.getConfigPathPatterns());
 	}
 
@@ -104,7 +104,7 @@ public class CrshPropertiesTests {
 		binder.bind(new MutablePropertyValues(Collections.singletonMap("shell.disabled_plugins",
 				"pattern1, pattern2")));
 		assertFalse(binder.getBindingResult().hasErrors());
-		assertEquals(2, props.getDisabledPlugins().length, 2);
+		assertEquals(2, props.getDisabledPlugins().length);
 		assertArrayEquals(new String[] { "pattern1", "pattern2" }, props.getDisabledPlugins());
 	}
 
