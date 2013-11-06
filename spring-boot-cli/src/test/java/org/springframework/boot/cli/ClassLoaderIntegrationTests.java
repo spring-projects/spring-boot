dac470a8fcf2b120f4ee5281291556ddbf784086
Dave Syer <dsyer@gopivotal.com>
2013-11-06 09:42:16 +0000

    Add useful features to CliTester

--- spring-boot-cli/src/test/java/org/springframework/boot/cli/ClassLoaderIntegrationTests.java
+++ spring-boot-cli/src/test/java/org/springframework/boot/cli/ClassLoaderIntegrationTests.java
@@ -30,12 +30,12 @@ import static org.junit.Assert.assertThat;
 public class ClassLoaderIntegrationTests {
 
 	@Rule
-	public CliTester cli = new CliTester();
+	public CliTester cli = new CliTester("src/test/resources/");
 
 	@Test
 	public void runWithIsolatedClassLoader() throws Exception {
 		// CLI classes or dependencies should not be exposed to the app
-		String output = this.cli.run("src/test/resources/classloader-test-app.groovy",
+		String output = this.cli.run("classloader-test-app.groovy",
 				SpringCli.class.getName());
 		assertThat(output, containsString("HasClasses-false-true-false"));
 	}
