44de687361d2163c0106266df1e0180ed72c7a8c
Phillip Webb <pwebb@gopivotal.com>
2013-11-15 23:21:00 -0800

    Add homebrew formula as generated artifact

---
+++ spring-boot-cli/src/main/homebrew/springboot.rb
@@ -0,0 +1,15 @@
+require 'formula'
+
+class Springboot < Formula
+	homepage 'http://projects.spring.io/spring-boot/'
+	url 'https://repo.spring.io/${repo}/org/springframework/boot/spring-boot-cli/${version}/spring-boot-cli-${version}-bin.tar.gz'
+	version '${version}'
+	sha1 '${checksum}'
+
+	def install
+		bin.install 'bin/spring'
+		lib.install 'lib/spring-boot-cli-${version}.jar'
+		bash_completion.install 'shell-completion.d/bash/spring'
+		zsh_completion.install 'shell-completion.d/zsh/_spring'
+	end
+end
