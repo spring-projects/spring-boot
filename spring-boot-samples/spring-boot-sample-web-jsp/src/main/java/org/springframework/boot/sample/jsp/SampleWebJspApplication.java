5d6b265794ebac73a6ad4580a42c7be5c33269f7
Dave Syer <dsyer@gopivotal.com>
2013-11-27 10:17:04 +0000

    Convert JSP sample to deployable (executable) WAR

--- spring-boot-samples/spring-boot-sample-web-jsp/src/main/java/org/springframework/boot/sample/jsp/SampleWebJspApplication.java
+++ spring-boot-samples/spring-boot-sample-web-jsp/src/main/java/org/springframework/boot/sample/jsp/SampleWebJspApplication.java
@@ -18,13 +18,20 @@ package org.springframework.boot.sample.jsp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.SpringBootServletInitializer;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 @EnableAutoConfiguration
 @ComponentScan
-public class SampleWebJspApplication {
+public class SampleWebJspApplication extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(SampleWebJspApplication.class);
+	}
 
 	public static void main(String[] args) throws Exception {
 		SpringApplication.run(SampleWebJspApplication.class, args);
