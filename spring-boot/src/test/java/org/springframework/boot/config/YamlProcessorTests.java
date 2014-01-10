cc996ce7f97fc5795fb728aa0f7e418c3fbe36f8
Dave Syer <dsyer@gopivotal.com>
2014-01-10 15:53:17 +0000

    Extend YAML map key special cases to nested maps
    Fixes gh-208

--- spring-boot/src/test/java/org/springframework/boot/config/YamlProcessorTests.java
+++ spring-boot/src/test/java/org/springframework/boot/config/YamlProcessorTests.java
@@ -76,4 +76,17 @@ public class YamlProcessorTests {
 		});
 	}
 
+	@Test
+	public void integerDeepKeyBehaves() {
+		this.processor.setResources(new Resource[] { new ByteArrayResource(
+				"foo:\n  1: bar".getBytes()) });
+		this.processor.process(new MatchCallback() {
+
+			@Override
+			public void process(Properties properties, Map<String, Object> map) {
+				assertEquals("bar", properties.get("foo[1]"));
+				assertEquals(1, properties.size());
+			}
+		});
+	}
 }
