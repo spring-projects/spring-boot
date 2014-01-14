3b259bc865df2f52864f9c91322bd1601dc86e1a
Phillip Webb <pwebb@gopivotal.com>
2014-01-14 10:21:36 -0800

    Polish

--- spring-boot-cli/src/test/java/org/springframework/boot/cli/compiler/grape/SettingsXmlRepositorySystemSessionAutoConfigurationTests.java
+++ spring-boot-cli/src/test/java/org/springframework/boot/cli/compiler/grape/SettingsXmlRepositorySystemSessionAutoConfigurationTests.java
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2012-2014 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package org.springframework.boot.cli.compiler.grape;
 
 import org.apache.maven.repository.internal.MavenRepositorySystemUtils;
@@ -11,10 +27,14 @@ import org.junit.rules.ExpectedException;
 import org.junit.runner.RunWith;
 import org.mockito.Mock;
 import org.mockito.runners.MockitoJUnitRunner;
-import org.springframework.boot.cli.compiler.grape.SettingsXmlRepositorySystemSessionAutoConfiguration;
 
 import static org.junit.Assert.assertNotNull;
 
+/**
+ * Tests for {@link SettingsXmlRepositorySystemSessionAutoConfiguration}.
+ * 
+ * @author Andy Wilkinson
+ */
 @RunWith(MockitoJUnitRunner.class)
 public class SettingsXmlRepositorySystemSessionAutoConfigurationTests {
 
