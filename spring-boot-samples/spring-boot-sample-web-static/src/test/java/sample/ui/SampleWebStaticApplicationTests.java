7b330897d9b82400fea157c5d0938189fe2ca72f
Dave Syer <dsyer@gopivotal.com>
2014-01-17 17:40:01 +0000

    Fix test

--- spring-boot-samples/spring-boot-sample-web-static/src/test/java/sample/ui/SampleWebStaticApplicationTests.java
+++ spring-boot-samples/spring-boot-sample-web-static/src/test/java/sample/ui/SampleWebStaticApplicationTests.java
@@ -63,7 +63,7 @@ public class SampleWebStaticApplicationTests {
 	@Test
 	public void testCss() throws Exception {
 		ResponseEntity<String> entity = getRestTemplate().getForEntity(
-				"http://localhost:8080/css/bootstrap.min.css", String.class);
+				"http://localhost:8080/webjars/bootstrap/3.0.3/css/bootstrap.min.css", String.class);
 		assertEquals(HttpStatus.OK, entity.getStatusCode());
 		assertTrue("Wrong body:\n" + entity.getBody(), entity.getBody().contains("body"));
 		assertEquals("Wrong content type:\n" + entity.getHeaders().getContentType(),
