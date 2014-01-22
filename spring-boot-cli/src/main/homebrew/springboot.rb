f500e48e2cf4dd4065082eea5865bcaa77a8d904
Phillip Webb <pwebb@gopivotal.com>
2014-01-21 21:05:23 -0800

    Fix version replacement in homebrew generation

--- spring-boot-cli/src/main/homebrew/springboot.rb
+++ spring-boot-cli/src/main/homebrew/springboot.rb
@@ -2,13 +2,13 @@ require 'formula'
 
 class Springboot < Formula
 	homepage 'http://projects.spring.io/spring-boot/'
-	url 'https://repo.spring.io/${repo}/org/springframework/boot/spring-boot-cli/${version}/spring-boot-cli-${version}-bin.tar.gz'
-	version '${version}'
+	url 'https://repo.spring.io/${repo}/org/springframework/boot/spring-boot-cli/${project.version}/spring-boot-cli-${project.version}-bin.tar.gz'
+	version '${project.version}'
 	sha1 '${checksum}'
 
 	def install
 		bin.install 'bin/spring'
-		lib.install 'lib/spring-boot-cli-${version}.jar'
+		lib.install 'lib/spring-boot-cli-${project.version}.jar'
 		bash_completion.install 'shell-completion/bash/spring'
 		zsh_completion.install 'shell-completion/zsh/_spring'
 	end
