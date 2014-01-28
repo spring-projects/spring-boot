073e6a0fc1330096cc3d9fb5a82ceef089f541dc
Christian Dupuis <cdupuis@gopivotal.com>
2014-01-28 17:02:56 +0100

    Beans annotated with @ConfigurationProperties can be their own validators when implementing Spring's Validator interface
    fixes #255

---
+++ spring-boot/src/test/java/org/springframework/boot/context/properties/ConfigurationPropertiesBindingPostProcessorTests.java
@@ -0,0 +1,151 @@
+/*
+ * Copyright 2012-2014 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.springframework.boot.context.properties;
+
+import javax.validation.constraints.NotNull;
+
+import org.junit.After;
+import org.junit.Test;
+import org.springframework.beans.factory.BeanCreationException;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.mock.env.MockEnvironment;
+import org.springframework.validation.BindException;
+import org.springframework.validation.Errors;
+import org.springframework.validation.ValidationUtils;
+import org.springframework.validation.Validator;
+import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
+
+import static org.junit.Assert.assertTrue;
+
+/**
+ * 
+ * Tests for {@link ConfigurationPropertiesBindingPostProcessor}.
+ * 
+ * @author Christian Dupuis
+ */
+public class ConfigurationPropertiesBindingPostProcessorTests {
+
+	private AnnotationConfigWebApplicationContext context;
+
+	@After
+	public void close() {
+		if (this.context != null) {
+			this.context.close();
+		}
+	}
+
+	@Test
+	public void testValidationWithoutJSR303() {
+		this.context = new AnnotationConfigWebApplicationContext();
+		this.context.register(TestConfigurationWithoutJSR303.class);
+		try {
+			this.context.refresh();
+		}
+		catch (BeanCreationException ex) {
+			BindException bex = (BindException) ex.getRootCause();
+			assertTrue(1 == bex.getErrorCount());
+		}
+	}
+
+	@Test
+	public void testValidationWithJSR303() {
+		this.context = new AnnotationConfigWebApplicationContext();
+		this.context.register(TestConfigurationWithJSR303.class);
+		try {
+			this.context.refresh();
+		}
+		catch (BeanCreationException ex) {
+			BindException bex = (BindException) ex.getRootCause();
+			assertTrue(2 == bex.getErrorCount());
+		}
+	}
+
+	@Test
+	public void testSuccessfulValidationWithJSR303() {
+		MockEnvironment env = new MockEnvironment();
+		env.setProperty("test.foo", "123456");
+		env.setProperty("test.bar", "654321");
+		this.context = new AnnotationConfigWebApplicationContext();
+		this.context.setEnvironment(env);
+		this.context.register(TestConfigurationWithJSR303.class);
+		this.context.refresh();
+	}
+
+	@Configuration
+	@EnableConfigurationProperties
+	public static class TestConfigurationWithoutJSR303 {
+
+		@Bean
+		public PropertyWithoutJSR303 testProperties() {
+			return new PropertyWithoutJSR303();
+		}
+
+	}
+
+	@ConfigurationProperties(name = "test")
+	public static class PropertyWithoutJSR303 implements Validator {
+
+		private String foo;
+
+		@Override
+		public boolean supports(Class<?> clazz) {
+			return clazz.isAssignableFrom(getClass());
+		}
+
+		@Override
+		public void validate(Object target, Errors errors) {
+			ValidationUtils.rejectIfEmpty(errors, "foo", "TEST1");
+		}
+
+		public String getFoo() {
+			return this.foo;
+		}
+
+		public void setFoo(String foo) {
+			this.foo = foo;
+		}
+
+	}
+
+	@Configuration
+	@EnableConfigurationProperties
+	public static class TestConfigurationWithJSR303 {
+
+		@Bean
+		public PropertyWithJSR303 testProperties() {
+			return new PropertyWithJSR303();
+		}
+
+	}
+
+	@ConfigurationProperties(name = "test")
+	public static class PropertyWithJSR303 extends PropertyWithoutJSR303 {
+
+		@NotNull
+		private String bar;
+
+		public void setBar(String bar) {
+			this.bar = bar;
+		}
+
+		public String getBar() {
+			return this.bar;
+		}
+	}
+
+}
