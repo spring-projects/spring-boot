7eb2da68c2ca1e28b3b8d8e06b079d72faf36a36
Phillip Webb <pwebb@gopivotal.com>
2014-02-03 10:29:17 -0800

    Fix package tangle
    Fix package tangle between the `org.springframework.boot` and the
    `event` sub-package. A new `SpringApplicationRunParticipant` abstraction
    has been introduced, with the `event` package solely responsible for
    handling event multi-casting.

---
+++ spring-boot/src/main/java/org/springframework/boot/event/EventPublishingRunParticipant.java
@@ -0,0 +1,95 @@
+/*
+ * Copyright 2012-2014 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.springframework.boot.event;
+
+import org.springframework.beans.factory.BeanFactoryAware;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.SpringApplicationRunParticipant;
+import org.springframework.context.ApplicationListener;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.event.ApplicationEventMulticaster;
+import org.springframework.context.event.SimpleApplicationEventMulticaster;
+import org.springframework.context.support.AbstractApplicationContext;
+import org.springframework.core.env.ConfigurableEnvironment;
+
+/**
+ * {@link SpringApplicationRunParticipant} to publish {@link SpringApplicationEvent}s.
+ * 
+ * @author Phillip Webb
+ */
+public class EventPublishingRunParticipant implements SpringApplicationRunParticipant {
+
+	private final ApplicationEventMulticaster multicaster;
+
+	private SpringApplication application;
+
+	private String[] args;
+
+	public EventPublishingRunParticipant(SpringApplication application, String[] args) {
+		this.application = application;
+		this.args = args;
+		this.multicaster = new SimpleApplicationEventMulticaster();
+		for (ApplicationListener<?> listener : application.getListeners()) {
+			this.multicaster.addApplicationListener(listener);
+		}
+	}
+
+	@Override
+	public void started() {
+		publishEvent(new ApplicationStartedEvent(this.application, this.args));
+	}
+
+	@Override
+	public void environmentPrepared(ConfigurableEnvironment environment) {
+		publishEvent(new ApplicationEnvironmentPreparedEvent(this.application, this.args,
+				environment));
+	}
+
+	@Override
+	public void contextPrepared(ConfigurableApplicationContext context) {
+		registerApplicationEventMulticaster(context);
+	}
+
+	private void registerApplicationEventMulticaster(
+			ConfigurableApplicationContext context) {
+		context.getBeanFactory().registerSingleton(
+				AbstractApplicationContext.APPLICATION_EVENT_MULTICASTER_BEAN_NAME,
+				this.multicaster);
+		if (this.multicaster instanceof BeanFactoryAware) {
+			((BeanFactoryAware) this.multicaster)
+					.setBeanFactory(context.getBeanFactory());
+		}
+	}
+
+	@Override
+	public void contextLoaded(ConfigurableApplicationContext context) {
+		publishEvent(new ApplicationPreparedEvent(this.application, this.args, context));
+	}
+
+	@Override
+	public void finished(ConfigurableApplicationContext context, Throwable exception) {
+		if (exception != null) {
+			publishEvent(new ApplicationFailedEvent(this.application, this.args, context,
+					exception));
+		}
+	}
+
+	private void publishEvent(SpringApplicationEvent event) {
+		this.multicaster.multicastEvent(event);
+	}
+
+}
