04c60365f7301101b2a813ba4001b691de49cbdb
Oliver Gierke <info@olivergierke.de>
2014-02-05 12:20:23 +0100

    A few improvements for the MongoDB auto-configuration.
    Allow to deactivate MongoDB repository auto-detection by using
    @EnableMongoRepositories. Adapted the test helper classes accordingly.
    
    Changed the property prefix and dependency management version property
    from …mongo to …mongodb to be consistent with the Spring Data module
    name.

--- spring-boot-autoconfigure/src/main/java/org/springframework/boot/autoconfigure/data/MongoRepositoriesAutoConfiguration.java
+++ spring-boot-autoconfigure/src/main/java/org/springframework/boot/autoconfigure/data/MongoRepositoriesAutoConfiguration.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2012-2013 the original author or authors.
+ * Copyright 2012-2014 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -43,6 +43,7 @@ import com.mongodb.MongoClientURI;
  * Repositories.
  * 
  * @author Dave Syer
+ * @author Oliver Gierke
  * @see EnableMongoRepositories
  */
 @Configuration
@@ -80,7 +81,7 @@ public class MongoRepositoriesAutoConfiguration {
 
 	}
 
-	@ConfigurationProperties(name = "spring.data.mongo")
+	@ConfigurationProperties(name = "spring.data.mongodb")
 	public static class MongoProperties {
 
 		private String host;
--- spring-boot-autoconfigure/src/test/java/org/springframework/boot/autoconfigure/data/MongoRepositoriesAutoConfigurationTests.java
+++ spring-boot-autoconfigure/src/test/java/org/springframework/boot/autoconfigure/data/MongoRepositoriesAutoConfigurationTests.java
@@ -19,19 +19,25 @@ package org.springframework.boot.autoconfigure.data;
 import org.junit.Test;
 import org.springframework.boot.autoconfigure.PropertyPlaceholderAutoConfiguration;
 import org.springframework.boot.autoconfigure.TestAutoConfigurationPackage;
+import org.springframework.boot.autoconfigure.data.alt.MongoDbCityRepository;
 import org.springframework.boot.autoconfigure.data.mongo.City;
 import org.springframework.boot.autoconfigure.data.mongo.CityRepository;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 
 import com.mongodb.Mongo;
+import com.mongodb.MongoClient;
 
+import static org.hamcrest.CoreMatchers.*;
+import static org.junit.Assert.assertThat;
 import static org.junit.Assert.assertNotNull;
 
 /**
  * Tests for {@link MongoRepositoriesAutoConfiguration}.
  * 
  * @author Dave Syer
+ * @author Oliver Gierke
  */
 public class MongoRepositoriesAutoConfigurationTests {
 
@@ -45,7 +51,9 @@ public class MongoRepositoriesAutoConfigurationTests {
 				PropertyPlaceholderAutoConfiguration.class);
 		this.context.refresh();
 		assertNotNull(this.context.getBean(CityRepository.class));
-		assertNotNull(this.context.getBean(Mongo.class));
+		
+		Mongo mongo = this.context.getBean(Mongo.class);
+		assertThat(mongo, is(instanceOf(MongoClient.class)));
 	}
 
 	@Test
@@ -55,7 +63,20 @@ public class MongoRepositoriesAutoConfigurationTests {
 				MongoRepositoriesAutoConfiguration.class,
 				PropertyPlaceholderAutoConfiguration.class);
 		this.context.refresh();
-		assertNotNull(this.context.getBean(Mongo.class));
+		
+		Mongo mongo = this.context.getBean(Mongo.class);
+		assertThat(mongo, is(instanceOf(MongoClient.class)));
+	}
+	
+	@Test
+	public void doesNotTriggerDefaultRepositoryDetectionIfCustomized() {
+		
+		this.context = new AnnotationConfigApplicationContext();
+		this.context.register(CustomizedConfiguration.class,
+				MongoRepositoriesAutoConfiguration.class,
+				PropertyPlaceholderAutoConfiguration.class);
+		this.context.refresh();
+		assertNotNull(this.context.getBean(MongoDbCityRepository.class));
 	}
 
 	@Configuration
@@ -70,4 +91,10 @@ public class MongoRepositoriesAutoConfigurationTests {
 
 	}
 
+	@Configuration
+	@TestAutoConfigurationPackage(MongoRepositoriesAutoConfigurationTests.class)
+	@EnableMongoRepositories(basePackageClasses = MongoDbCityRepository.class)
+	protected static class CustomizedConfiguration {
+		
+	}
 }
