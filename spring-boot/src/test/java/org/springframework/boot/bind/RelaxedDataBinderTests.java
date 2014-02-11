ca7201b4b2811a901c54b7bf1b7f8e313598572f
Andy Wilkinson <awilkinson@gopivotal.com>
2014-02-11 13:38:01 +0000

    Fix binding of overlapping nested maps
    When binding a nested map structure, RelaxedDataBinder pre-populates
    the target object with default empty maps. Previously, when these
    structures overlapped, each step in pre-population process could
    potentially overwrite what had come before it. This led to the output
    of the pre-population process being incomplete which would lead to a
    binding failure.
    
    This commit updates the pre-population process so that it checks to see
    if a property's value has already been set by an earlier step in the
    process. If it has been set, the existing value is now reused rather
    than being overwritten by a new empty map.
    
    Fixes #328

--- spring-boot/src/test/java/org/springframework/boot/bind/RelaxedDataBinderTests.java
+++ spring-boot/src/test/java/org/springframework/boot/bind/RelaxedDataBinderTests.java
@@ -380,6 +380,22 @@ public class RelaxedDataBinderTests {
 		assertEquals("123", map.get("value"));
 	}
 
+	@SuppressWarnings("unchecked")
+	@Test
+	public void testBindOverlappingNestedMaps() throws Exception {
+		Map<String, Object> target = new LinkedHashMap<String, Object>();
+		BindingResult result = bind(target, "a.b.c.d: abc\na.b.c1.d1: efg");
+		assertEquals(0, result.getErrorCount());
+
+		Map<String, Object> a = (Map<String, Object>) target.get("a");
+		Map<String, Object> b = (Map<String, Object>) a.get("b");
+		Map<String, Object> c = (Map<String, Object>) b.get("c");
+		assertEquals("abc", c.get("d"));
+
+		Map<String, Object> c1 = (Map<String, Object>) b.get("c1");
+		assertEquals("efg", c1.get("d1"));
+	}
+
 	private BindingResult bind(Object target, String values) throws Exception {
 		return bind(target, values, null);
 	}
