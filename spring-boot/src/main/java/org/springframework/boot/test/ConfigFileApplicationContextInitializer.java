d5c0ef6ca3dabc34a372d7567b31e1b2379a0682
Phillip Webb <pwebb@gopivotal.com>
2014-02-12 16:21:37 -0800

    Add ConfigFileApplicationContextInitializer
    Reintroduce ConfigFileApplicationContextInitializer for tests that
    wish to reuse 'application.properties' configuration.
    
    Fixes gh-344

---
+++ spring-boot/src/main/java/org/springframework/boot/test/ConfigFileApplicationContextInitializer.java
@@ -0,0 +1,45 @@
+/*
+ * Copyright 2012-2014 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.springframework.boot.test;
+
+import org.springframework.boot.context.config.ConfigFileApplicationListener;
+import org.springframework.context.ApplicationContextInitializer;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.test.context.ContextConfiguration;
+
+/**
+ * {@link ApplicationContextInitializer} that can be used with the
+ * {@link ContextConfiguration#initializers()} to trigger loading of
+ * {@literal application.properties}.
+ * 
+ * @author Phillip Webb
+ * @see ConfigFileApplicationListener
+ */
+public class ConfigFileApplicationContextInitializer implements
+		ApplicationContextInitializer<ConfigurableApplicationContext> {
+
+	@Override
+	public void initialize(final ConfigurableApplicationContext applicationContext) {
+		new ConfigFileApplicationListener() {
+			public void apply() {
+				addProperySources(applicationContext.getEnvironment());
+				addPostProcessors(applicationContext);
+			}
+		}.apply();
+	}
+
+}
