8cab9507ed232e8520c31bfe5ca2da5af6685fcd
Marten Deinum <mdeinum@gmail.com>
2014-02-28 11:50:45 +0100

    Spring Web Services Starter and Sample Project
    Fixes gh-412

---
+++ spring-boot-samples/spring-boot-sample-ws/src/main/java/sample/ws/service/HumanResourceService.java
@@ -0,0 +1,10 @@
+package sample.ws.service;
+
+import java.util.Date;
+
+/**
+ * Created by in329dei on 28-2-14.
+ */
+public interface HumanResourceService {
+    void bookHoliday(Date startDate, Date endDate, String name);
+}
