a79ff19b00a3b975ba8506624a4e9abd56c2b92a
Phillip Webb <pwebb@gopivotal.com>
2014-03-19 18:08:10 -0700

    Replace confusing '+' profile syntax.
    Replace the confusing `spring.profiles.active` `+` syntax with a new
    `spring.profiles.include` property.
    
    Fixes gh-483, Fixes gh-534

--- spring-boot/src/test/java/org/springframework/boot/context/config/ConfigFileApplicationListenerTests.java
+++ spring-boot/src/test/java/org/springframework/boot/context/config/ConfigFileApplicationListenerTests.java
@@ -433,8 +433,8 @@ public class ConfigFileApplicationListenerTests {
 		SpringApplication application = new SpringApplication(Config.class);
 		application.setWebEnvironment(false);
 		ConfigurableApplicationContext context = application
-				.run("--spring.profiles.active=activateprofile");
-		assertThat(context.getEnvironment(), acceptsProfiles("activateprofile"));
+				.run("--spring.profiles.active=includeprofile");
+		assertThat(context.getEnvironment(), acceptsProfiles("includeprofile"));
 		assertThat(context.getEnvironment(), acceptsProfiles("specific"));
 		assertThat(context.getEnvironment(), acceptsProfiles("morespecific"));
 		assertThat(context.getEnvironment(), acceptsProfiles("yetmorespecific"));
