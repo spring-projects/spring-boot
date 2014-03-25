c2b6862a49e00fe2a84c23bafccb6ab0b015cfac
Dave Syer <dsyer@gopivotal.com>
2014-03-25 17:13:55 +0000

    Ensure new VCAP_SERVICES format works

--- spring-boot/src/test/java/org/springframework/boot/cloudfoundry/VcapApplicationListenerTests.java
+++ spring-boot/src/test/java/org/springframework/boot/cloudfoundry/VcapApplicationListenerTests.java
@@ -75,4 +75,15 @@ public class VcapApplicationListenerTests {
 		assertEquals("mysql",
 				this.context.getEnvironment().getProperty("vcap.services.mysql.name"));
 	}
+
+	@Test
+	public void testServicePropertiesWithoutNA() {
+		EnvironmentTestUtils
+				.addEnvironment(
+						this.context,
+						"VCAP_SERVICES:{\"rds-mysql\":[{\"name\":\"mysql\",\"label\":\"rds-mysql\",\"plan\":\"10mb\",\"credentials\":{\"name\":\"d04fb13d27d964c62b267bbba1cffb9da\",\"hostname\":\"mysql-service-public.clqg2e2w3ecf.us-east-1.rds.amazonaws.com\",\"host\":\"mysql-service-public.clqg2e2w3ecf.us-east-1.rds.amazonaws.com\",\"port\":3306,\"user\":\"urpRuqTf8Cpe6\",\"username\":\"urpRuqTf8Cpe6\",\"password\":\"pxLsGVpsC9A5S\"}}]}");
+		this.initializer.onApplicationEvent(this.event);
+		assertEquals("mysql",
+				this.context.getEnvironment().getProperty("vcap.services.mysql.name"));
+	}
 }
