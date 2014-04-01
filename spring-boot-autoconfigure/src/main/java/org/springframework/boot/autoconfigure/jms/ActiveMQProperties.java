5241a0c0789f2149d73c84611204a021c07f2a38
Jean Detoeuf <detoeuf.opengroup@monecam.com>
2014-04-01 14:53:44 +0200

    Added ActiveMqCredentials (optional)
    Fixes gh-618

--- spring-boot-autoconfigure/src/main/java/org/springframework/boot/autoconfigure/jms/ActiveMQProperties.java
+++ spring-boot-autoconfigure/src/main/java/org/springframework/boot/autoconfigure/jms/ActiveMQProperties.java
@@ -32,6 +32,10 @@ public class ActiveMQProperties {
 
 	private boolean pooled = false;
 
+	private String user;
+
+	private String password;
+
 	// Will override brokerURL if inMemory is set to true
 	public String getBrokerUrl() {
 		if (this.inMemory) {
@@ -58,6 +62,22 @@ public class ActiveMQProperties {
 
 	public void setPooled(boolean pooled) {
 		this.pooled = pooled;
+	}
+
+	public String getUser() {
+		return this.user;
+	}
+
+	public void setUser(String user) {
+		this.user = user;
+	}
+
+	public String getPassword() {
+		return this.password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
 	}
 
 }
