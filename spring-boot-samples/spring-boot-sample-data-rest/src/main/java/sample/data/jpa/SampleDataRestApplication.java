ae7098ae1d8ed13bed52f67f30612e7ecf39d618
Rob Winch <rwinch@gopivotal.com>
2014-04-03 10:35:27 -0500

    Add RepositoryRestMvcAutoConfiguration
    Fixes gh-626

--- spring-boot-samples/spring-boot-sample-data-rest/src/main/java/sample/data/jpa/SampleDataRestApplication.java
+++ spring-boot-samples/spring-boot-sample-data-rest/src/main/java/sample/data/jpa/SampleDataRestApplication.java
@@ -20,13 +20,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Import;
-import org.springframework.data.rest.webmvc.config.RepositoryRestMvcConfiguration;
 
 @Configuration
 @ComponentScan
 @EnableAutoConfiguration
-@Import(RepositoryRestMvcConfiguration.class)
 public class SampleDataRestApplication {
 
 	public static void main(String[] args) throws Exception {
