d92d16d9351595b116d617df3a01527a525858c0
Dave Syer <dsyer@gopivotal.com>
2014-04-04 18:28:49 +0100

    Add test for groovy-xml parsing

--- spring-boot/src/test/java/org/springframework/boot/BeanDefinitionLoaderTests.java
+++ spring-boot/src/test/java/org/springframework/boot/BeanDefinitionLoaderTests.java
@@ -71,6 +71,17 @@ public class BeanDefinitionLoaderTests {
 	}
 
 	@Test
+	public void loadGroovyResourceWithNamespace() throws Exception {
+		ClassPathResource resource = new ClassPathResource("sample-namespace.groovy",
+				getClass());
+		BeanDefinitionLoader loader = new BeanDefinitionLoader(this.registry, resource);
+		int loaded = loader.load();
+		assertThat(loaded, equalTo(1));
+		assertTrue(this.registry.containsBean("myGroovyComponent"));
+
+	}
+
+	@Test
 	public void loadPackage() throws Exception {
 		BeanDefinitionLoader loader = new BeanDefinitionLoader(this.registry,
 				MyComponent.class.getPackage());
