6e5d4a68c8394aee04fe703348791511246ba334
Dave Syer <dsyer@gopivotal.com>
2014-04-11 16:46:09 +0100

    Add generic error handling to BasicErrorController
    Since Spring supports gobal error handling through
    @ControllerAdvice, it is quite easy to set up more meta-data
    about an exception for the BasicErrorController. You need
    to be careful not to swallow Security exceptions, and probably
    others (optionally) so this feature needs a bit more work.
    
    See gh-538

--- spring-boot-actuator/src/main/java/org/springframework/boot/actuate/web/BasicErrorController.java
+++ spring-boot-actuator/src/main/java/org/springframework/boot/actuate/web/BasicErrorController.java
@@ -24,19 +24,26 @@ import java.util.Map;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.context.embedded.AbstractEmbeddedServletContainerFactory;
+import org.springframework.core.annotation.Order;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindException;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.context.request.RequestAttributes;
 import org.springframework.web.context.request.ServletRequestAttributes;
 import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.support.DefaultHandlerExceptionResolver;
 
 /**
  * Basic global error {@link Controller}, rendering servlet container error codes and
@@ -48,11 +55,15 @@ import org.springframework.web.servlet.ModelAndView;
  * @author Dave Syer
  */
 @Controller
+@ControllerAdvice
+@Order(0)
 public class BasicErrorController implements ErrorController {
 
 	private static final String ERROR_KEY = "error";
 
 	private final Log logger = LogFactory.getLog(BasicErrorController.class);
+
+	private DefaultHandlerExceptionResolver resolver = new DefaultHandlerExceptionResolver();
 
 	@Value("${error.path:/error}")
 	private String errorPath;
@@ -60,6 +71,24 @@ public class BasicErrorController implements ErrorController {
 	@Override
 	public String getErrorPath() {
 		return this.errorPath;
+	}
+
+	@ExceptionHandler(Exception.class)
+	public void handle(HttpServletRequest request, HttpServletResponse response,
+			Exception e) throws Exception {
+		this.resolver.resolveException(request, response, null, e);
+		if (response.getStatus() == HttpServletResponse.SC_OK) {
+			response.sendError(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
+		}
+		// There's only one exception so it's easier for the error controller to identify
+		// it this way...
+		request.setAttribute(ErrorController.class.getName(), e);
+		if (e instanceof BindException) {
+			// ... but other error pages might be looking for it here as well
+			request.setAttribute(
+					BindingResult.MODEL_KEY_PREFIX + ((BindException) e).getObjectName(),
+					e);
+		}
 	}
 
 	@RequestMapping(value = "${error.path:/error}", produces = "text/html")
@@ -96,7 +125,7 @@ public class BasicErrorController implements ErrorController {
 		map.put("timestamp", new Date());
 		try {
 			Throwable error = (Throwable) attributes.getAttribute(
-					"javax.servlet.error.exception", RequestAttributes.SCOPE_REQUEST);
+					ErrorController.class.getName(), RequestAttributes.SCOPE_REQUEST);
 			Object obj = attributes.getAttribute("javax.servlet.error.status_code",
 					RequestAttributes.SCOPE_REQUEST);
 			int status = 999;
@@ -108,12 +137,16 @@ public class BasicErrorController implements ErrorController {
 				map.put(ERROR_KEY, "None");
 			}
 			map.put("status", status);
+			if (error == null) {
+				error = (Throwable) attributes.getAttribute(
+						"javax.servlet.error.exception", RequestAttributes.SCOPE_REQUEST);
+			}
 			if (error != null) {
 				while (error instanceof ServletException && error.getCause() != null) {
 					error = ((ServletException) error).getCause();
 				}
 				map.put("exception", error.getClass().getName());
-				map.put("message", error.getMessage());
+				addMessage(map, error);
 				if (trace) {
 					StringWriter stackTrace = new StringWriter();
 					error.printStackTrace(new PrintWriter(stackTrace));
@@ -129,6 +162,9 @@ public class BasicErrorController implements ErrorController {
 						RequestAttributes.SCOPE_REQUEST);
 				map.put("message", message == null ? "No message available" : message);
 			}
+			String path = (String) attributes.getAttribute(
+					"javax.servlet.error.request_uri", RequestAttributes.SCOPE_REQUEST);
+			map.put("path", path == null ? "No path available" : path);
 			return map;
 		}
 		catch (Exception ex) {
@@ -138,6 +174,24 @@ public class BasicErrorController implements ErrorController {
 				this.logger.error(ex);
 			}
 			return map;
+		}
+	}
+
+	protected void addMessage(Map<String, Object> map, Throwable error) {
+		if (error instanceof BindingResult) {
+			BindingResult result = (BindingResult) error;
+			if (result.getErrorCount() > 0) {
+				map.put("errors", result.getAllErrors());
+				map.put("message",
+						"Validation failed for object='" + result.getObjectName()
+								+ "'. Error count: " + result.getErrorCount());
+			}
+			else {
+				map.put("message", "No errors");
+			}
+		}
+		else {
+			map.put("message", error.getMessage());
 		}
 	}
 
