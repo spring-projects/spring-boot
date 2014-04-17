d53f5009b91c24fbb6a751af07bdb171d5d508bf
Marcel Overdijk <marcel@overdijk.me>
2014-04-17 23:59:36 +0200

    Added liquibase autoconfiguration for database migrations
    If Liquibase is on the classpath it will fire up on startup. Various
    config options are available (as well as the option to disable it).
    Liquibase uses a YAML format for changes (in classpath:db/changelog).

---
+++ spring-boot-autoconfigure/src/test/java/org/springframework/boot/autoconfigure/liquibase/LiquibaseAutoConfigurationTests.java
@@ -0,0 +1,135 @@
+/*
+ * Copyright 2012-2014 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.springframework.boot.autoconfigure.liquibase;
+
+import liquibase.integration.spring.SpringLiquibase;
+
+import org.junit.After;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.ExpectedException;
+import org.springframework.beans.factory.BeanCreationException;
+import org.springframework.boot.autoconfigure.PropertyPlaceholderAutoConfiguration;
+import org.springframework.boot.autoconfigure.jdbc.EmbeddedDataSourceConfiguration;
+import org.springframework.boot.test.EnvironmentTestUtils;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertNull;
+import static org.junit.Assert.assertTrue;
+
+/**
+ * Tests for {@link LiquibaseAutoConfiguration}.
+ * 
+ * @author Marcel Overdijk
+ */
+public class LiquibaseAutoConfigurationTests {
+
+	@Rule
+	public ExpectedException expected = ExpectedException.none();
+
+	private AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext();;
+
+	@After
+	public void close() {
+		if (this.context != null) {
+			this.context.close();
+		}
+	}
+
+	@Test
+	public void testNoDataSource() throws Exception {
+		this.context.register(LiquibaseAutoConfiguration.class,
+				PropertyPlaceholderAutoConfiguration.class);
+		this.expected.expect(BeanCreationException.class);
+		this.expected.expectMessage("No qualifying bean");
+		this.expected.expectMessage("DataSource");
+		this.context.refresh();
+	}
+
+	@Test
+	public void testDefaultSpringLiquibase() throws Exception {
+		this.context.register(EmbeddedDataSourceConfiguration.class,
+				LiquibaseAutoConfiguration.class,
+				PropertyPlaceholderAutoConfiguration.class);
+		this.context.refresh();
+		SpringLiquibase liquibase = this.context.getBean(SpringLiquibase.class);
+		assertEquals("classpath:/db/changelog/db.changelog-master.yaml", liquibase.getChangeLog());
+		assertNull(liquibase.getContexts());
+		assertNull(liquibase.getDefaultSchema());
+		assertFalse(liquibase.isDropFirst());
+	}
+
+	@Test
+	public void testOverrideChangeLog() throws Exception {
+		EnvironmentTestUtils.addEnvironment(this.context,
+				"spring.liquibase.change-log:classpath:/db/changelog/db.changelog-override.xml");
+		this.context.register(EmbeddedDataSourceConfiguration.class,
+				LiquibaseAutoConfiguration.class,
+				PropertyPlaceholderAutoConfiguration.class);
+		this.context.refresh();
+		SpringLiquibase liquibase = this.context.getBean(SpringLiquibase.class);
+		assertEquals("classpath:/db/changelog/db.changelog-override.xml", liquibase.getChangeLog());
+	}
+
+	@Test
+	public void testOverrideContexts() throws Exception {
+		EnvironmentTestUtils.addEnvironment(this.context,
+				"spring.liquibase.contexts:test, production");
+		this.context.register(EmbeddedDataSourceConfiguration.class,
+				LiquibaseAutoConfiguration.class,
+				PropertyPlaceholderAutoConfiguration.class);
+		this.context.refresh();
+		SpringLiquibase liquibase = this.context.getBean(SpringLiquibase.class);
+		assertEquals("test, production", liquibase.getContexts());
+	}
+	
+	@Test
+	public void testOverrideDefaultSchema() throws Exception {
+		EnvironmentTestUtils.addEnvironment(this.context,
+				"spring.liquibase.default-schema:public");
+		this.context.register(EmbeddedDataSourceConfiguration.class,
+				LiquibaseAutoConfiguration.class,
+				PropertyPlaceholderAutoConfiguration.class);
+		this.context.refresh();
+		SpringLiquibase liquibase = this.context.getBean(SpringLiquibase.class);
+		assertEquals("public", liquibase.getDefaultSchema());
+	}
+	
+	@Test
+	public void testOverrideDropFirst() throws Exception {
+		EnvironmentTestUtils.addEnvironment(this.context,
+				"spring.liquibase.drop-first:true");
+		this.context.register(EmbeddedDataSourceConfiguration.class,
+				LiquibaseAutoConfiguration.class,
+				PropertyPlaceholderAutoConfiguration.class);
+		this.context.refresh();
+		SpringLiquibase liquibase = this.context.getBean(SpringLiquibase.class);
+		assertTrue(liquibase.isDropFirst());
+	}
+	
+	@Test(expected = BeanCreationException.class)
+	public void testChangeLogDoesNotExist() throws Exception {
+		EnvironmentTestUtils.addEnvironment(this.context,
+				"spring.liquibase.change-log:classpath:/no-such-changelog.yaml");
+		this.context.register(EmbeddedDataSourceConfiguration.class,
+				LiquibaseAutoConfiguration.class,
+				PropertyPlaceholderAutoConfiguration.class);
+		this.context.refresh();
+	}
+}
