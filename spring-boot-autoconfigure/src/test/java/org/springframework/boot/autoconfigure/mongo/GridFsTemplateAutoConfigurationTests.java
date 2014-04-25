94dd510b52dcdc7779d1e55d03b45be903bd7145
Dave Syer <dsyer@gopivotal.com>
2014-04-25 13:41:09 +0100

    Merge GridFs with MongoTemplate configuration

--- spring-boot-autoconfigure/src/test/java/org/springframework/boot/autoconfigure/mongo/GridFsTemplateAutoConfigurationTests.java
+++ spring-boot-autoconfigure/src/test/java/org/springframework/boot/autoconfigure/mongo/GridFsTemplateAutoConfigurationTests.java
@@ -13,41 +13,39 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
- 
-package org.springframework.boot.autoconfigure.mongo;
 
+package org.springframework.boot.autoconfigure.mongo;
 
 import org.junit.After;
 import org.junit.Test;
 import org.springframework.boot.autoconfigure.PropertyPlaceholderAutoConfiguration;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
-import org.springframework.data.mongodb.core.MongoTemplate;
 import org.springframework.data.mongodb.gridfs.GridFsTemplate;
 
 import static org.junit.Assert.assertEquals;
 
-
 /**
- * Tests for {@link org.springframework.boot.autoconfigure.mongo.GridFsTemplateAutoConfiguration}.
+ * Tests for
+ * {@link org.springframework.boot.autoconfigure.mongo.MongoTemplateAutoConfiguration}.
  *
  * @author Josh Long
  */
 public class GridFsTemplateAutoConfigurationTests {
 
-    private AnnotationConfigApplicationContext context;
+	private AnnotationConfigApplicationContext context;
 
-    @After
-    public void close() {
-        if (this.context != null) {
-            this.context.close();
-        }
-    }
+	@After
+	public void close() {
+		if (this.context != null) {
+			this.context.close();
+		}
+	}
 
-    @Test
-    public void templateExists() {
-        this.context = new AnnotationConfigApplicationContext(
-                PropertyPlaceholderAutoConfiguration.class, MongoAutoConfiguration.class,
-                MongoTemplateAutoConfiguration.class , GridFsTemplateAutoConfiguration.class);
-        assertEquals(1, this.context.getBeanNamesForType(GridFsTemplate.class).length);
-    }
+	@Test
+	public void templateExists() {
+		this.context = new AnnotationConfigApplicationContext(
+				PropertyPlaceholderAutoConfiguration.class, MongoAutoConfiguration.class,
+				MongoTemplateAutoConfiguration.class);
+		assertEquals(1, this.context.getBeanNamesForType(GridFsTemplate.class).length);
+	}
 }
