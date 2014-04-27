a09722272e7462d62aec4b9f7aaee70694447e6a
Fermin Gallego <fegalodev@outlook.com>
2014-04-27 18:31:43 +0200

    Minor improvement in simple sample test

--- spring-boot-samples/spring-boot-sample-simple/src/test/java/sample/simple/SpringTestSampleSimpleApplicationTests.java
+++ spring-boot-samples/spring-boot-sample-simple/src/test/java/sample/simple/SpringTestSampleSimpleApplicationTests.java
@@ -16,9 +16,14 @@
 
 package sample.simple;
 
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
+
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.context.ApplicationContext;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 /**
@@ -30,8 +35,14 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 @SpringApplicationConfiguration(classes = SampleSimpleApplication.class)
 public class SpringTestSampleSimpleApplicationTests {
 
+	@Autowired
+	ApplicationContext ctx;
+
 	@Test
 	public void testContextLoads() throws Exception {
+		assertNotNull(ctx);
+		assertTrue(ctx.containsBean("helloWorldService"));
+		assertTrue(ctx.containsBean("sampleSimpleApplication"));
 	}
 
 }
