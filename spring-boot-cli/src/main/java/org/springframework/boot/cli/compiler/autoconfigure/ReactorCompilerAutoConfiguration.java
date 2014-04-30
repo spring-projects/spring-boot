f46d281b220793bd8f1fc337725a3da6ef18d215
Dave Syer <dsyer@gopivotal.com>
2014-04-30 11:26:57 +0100

    Upgrade to Reactor 1.1
    Fixes gh-706

--- spring-boot-cli/src/main/java/org/springframework/boot/cli/compiler/autoconfigure/ReactorCompilerAutoConfiguration.java
+++ spring-boot-cli/src/main/java/org/springframework/boot/cli/compiler/autoconfigure/ReactorCompilerAutoConfiguration.java
@@ -37,17 +37,22 @@ public class ReactorCompilerAutoConfiguration extends CompilerAutoConfiguration 
 	@Override
 	public void applyDependencies(DependencyCustomizer dependencies) {
 		dependencies.ifAnyMissingClasses("reactor.core.Reactor")
-				.add("reactor-spring", false).add("reactor-core");
+				.add("reactor-spring-context", false).add("reactor-spring-core", false)
+				.add("reactor-core");
 	}
 
 	@Override
 	public void applyImports(ImportCustomizer imports) {
 		imports.addImports("reactor.core.Reactor", "reactor.event.Event",
 				"reactor.function.Consumer", "reactor.function.Functions",
-				"reactor.event.selector.Selectors", "reactor.spring.annotation.Selector",
-				"reactor.spring.annotation.ReplyTo",
-				"reactor.spring.context.config.EnableReactor").addStarImports(
-				"reactor.event.Selectors");
+				"reactor.event.selector.Selectors",
+				"reactor.spring.context.annotation.Consumer",
+				"reactor.spring.context.annotation.Selector",
+				"reactor.spring.context.annotation.SelectorType",
+				"reactor.spring.context.annotation.ReplyTo",
+				"reactor.spring.context.config.EnableReactor")
+				.addStarImports("reactor.event.Selectors")
+				.addImport("ReactorEnvironment", "reactor.core.Environment");
 	}
 
 }
