34ef197b1446aaa819f0dfbad1e4be213ab5f30c
Artem Bilan <abilan@gopivotal.com>
2014-05-01 10:05:57 +0100

    Upgrade to Spring Integration 4.0
    Closes #751

--- spring-boot-cli/src/main/java/org/springframework/boot/cli/compiler/autoconfigure/SpringIntegrationCompilerAutoConfiguration.java
+++ spring-boot-cli/src/main/java/org/springframework/boot/cli/compiler/autoconfigure/SpringIntegrationCompilerAutoConfiguration.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2012-2013 the original author or authors.
+ * Copyright 2012-2014 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -21,43 +21,40 @@ import org.codehaus.groovy.control.customizers.ImportCustomizer;
 import org.springframework.boot.cli.compiler.AstUtils;
 import org.springframework.boot.cli.compiler.CompilerAutoConfiguration;
 import org.springframework.boot.cli.compiler.DependencyCustomizer;
-import org.springframework.boot.groovy.EnableIntegrationPatterns;
 
 /**
  * {@link CompilerAutoConfiguration} for Spring Integration.
- * 
+ *
  * @author Dave Syer
+ * @author Artem Bilan
  */
 public class SpringIntegrationCompilerAutoConfiguration extends CompilerAutoConfiguration {
 
 	@Override
 	public boolean matches(ClassNode classNode) {
-		// Slightly weird detection algorithm because there is no @Enable annotation for
-		// Integration
-		return AstUtils.hasAtLeastOneAnnotation(classNode, "MessageEndpoint",
-				"EnableIntegrationPatterns");
+		return AstUtils.hasAtLeastOneAnnotation(classNode, "EnableIntegration");
 	}
 
 	@Override
 	public void applyDependencies(DependencyCustomizer dependencies) {
-		dependencies.ifAnyMissingClasses("org.springframework.integration.Message").add(
+		dependencies.ifAnyMissingClasses(
+				"org.springframework.integration.config.EnableIntegration").add(
 				"spring-boot-starter-integration");
-		dependencies.ifAnyMissingClasses("groovy.util.XmlParser").add("groovy-xml");
 	}
 
 	@Override
 	public void applyImports(ImportCustomizer imports) {
-		imports.addImports("org.springframework.integration.Message",
+		imports.addImports("org.springframework.messaging.Message",
+				"org.springframework.messaging.MessageChannel",
+				"org.springframework.messaging.PollableChannel",
+				"org.springframework.messaging.SubscribableChannel",
+				"org.springframework.messaging.MessageHeaders",
 				"org.springframework.integration.support.MessageBuilder",
-				"org.springframework.integration.MessageChannel",
 				"org.springframework.integration.channel.DirectChannel",
 				"org.springframework.integration.channel.QueueChannel",
 				"org.springframework.integration.channel.ExecutorChannel",
-				"org.springframework.integration.MessageHeaders",
 				"org.springframework.integration.core.MessagingTemplate",
-				"org.springframework.integration.core.SubscribableChannel",
-				"org.springframework.integration.core.PollableChannel",
-				EnableIntegrationPatterns.class.getCanonicalName());
+				"org.springframework.integration.config.EnableIntegration");
 		imports.addStarImports("org.springframework.integration.annotation");
 	}
 }
