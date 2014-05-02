7e3614e6e08fb8096e36e5339eff4697fbcc0132
Dave Syer <dsyer@gopivotal.com>
2014-05-02 15:15:39 +0100

    Fix samples that need jdbc and don't import it

--- spring-boot-samples/spring-boot-sample-flyway/src/test/java/sample/flyway/SampleFlywayApplicationTests.java
+++ spring-boot-samples/spring-boot-sample-flyway/src/test/java/sample/flyway/SampleFlywayApplicationTests.java
@@ -27,7 +27,7 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @SpringApplicationConfiguration(classes=SampleFlywayApplication.class)
-public class SampleLiquibaseApplicationTests {
+public class SampleFlywayApplicationTests {
 	
 	@Autowired
 	private JdbcTemplate template;
