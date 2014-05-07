9ba88cf6b4c1fd6af730ffbd90ae8d359d220e54
Dave Syer <dsyer@gopivotal.com>
2014-05-06 18:03:06 -0700

    Add JPA to flyway sample

--- spring-boot-samples/spring-boot-sample-flyway/src/main/java/sample/flyway/SampleFlywayApplication.java
+++ spring-boot-samples/spring-boot-sample-flyway/src/main/java/sample/flyway/SampleFlywayApplication.java
@@ -16,17 +16,69 @@
 
 package sample.flyway;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
 @Configuration
 @ComponentScan
 @EnableAutoConfiguration
-public class SampleFlywayApplication {
+public class SampleFlywayApplication implements CommandLineRunner {
+
+	@Autowired
+	private PersonRepository repository;
+
+	@Override
+	public void run(String... args) throws Exception {
+		System.err.println(repository.findAll());
+	}
 
 	public static void main(String[] args) throws Exception {
 		SpringApplication.run(SampleFlywayApplication.class, args);
+	}
+}
+
+@Repository
+interface PersonRepository extends CrudRepository<Person, Long> {
+
+}
+
+@Entity
+class Person {
+	@Id
+	@GeneratedValue
+	private Long id;
+	private String firstName;
+	private String lastName;
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastname) {
+		this.lastName = lastname;
+	}
+
+	@Override
+	public String toString() {
+		return "Person [firstName=" + firstName + ", lastname=" + lastName
+				+ "]";
 	}
 }
