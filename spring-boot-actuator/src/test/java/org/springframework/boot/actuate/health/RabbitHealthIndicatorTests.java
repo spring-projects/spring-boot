b026b13c664d72566ddd5e08a647d2df2ed230f8
Christian Dupuis <cdupuis@gopivotal.com>
2014-05-10 11:10:29 +0200

    Add HealthIndicator for Rabbit

---
+++ spring-boot-actuator/src/test/java/org/springframework/boot/actuate/health/RabbitHealthIndicatorTests.java
@@ -0,0 +1,59 @@
+/*
+ * Copyright 2014 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.springframework.boot.actuate.health;
+
+import org.junit.After;
+import org.junit.Test;
+import org.springframework.amqp.rabbit.core.RabbitAdmin;
+import org.springframework.boot.actuate.autoconfigure.EndpointAutoConfiguration;
+import org.springframework.boot.actuate.autoconfigure.HealthIndicatorAutoConfiguration;
+import org.springframework.boot.autoconfigure.PropertyPlaceholderAutoConfiguration;
+import org.springframework.boot.autoconfigure.amqp.RabbitAutoConfiguration;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+
+/**
+ * Tests for {@link RabbitHealthIndicator}.
+ *
+ * @author Christian Dupuis
+ */
+public class RabbitHealthIndicatorTests {
+
+	private AnnotationConfigApplicationContext context;
+
+	@After
+	public void close() {
+		if (this.context != null) {
+			this.context.close();
+		}
+	}
+
+	@Test
+	public void indicatorExists() {
+		this.context = new AnnotationConfigApplicationContext(
+				PropertyPlaceholderAutoConfiguration.class,
+				RabbitAutoConfiguration.class, EndpointAutoConfiguration.class,
+				HealthIndicatorAutoConfiguration.class);
+		assertEquals(1, this.context.getBeanNamesForType(RabbitAdmin.class).length);
+		RabbitHealthIndicator healthIndicator = this.context
+				.getBean(RabbitHealthIndicator.class);
+		assertNotNull(healthIndicator);
+	}
+
+}
