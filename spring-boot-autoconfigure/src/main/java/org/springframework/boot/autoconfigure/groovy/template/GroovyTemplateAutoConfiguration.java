bb365432a3a324eb53ade4e11e090628e64f6deb
Dave Syer <dsyer@gopivotal.com>
2014-05-15 12:33:05 +0100

    Add Groovy template engine support for Spring MVC apps
    Default suffix .tpl. If groovy-templates is on the classpath user
    can now add templates and get them rendered and resolved in an MVC
    app.
    
    TODO: Macro helpers for message rendering etc.
    
    See gh-878

---
+++ spring-boot-autoconfigure/src/main/java/org/springframework/boot/autoconfigure/groovy/template/GroovyTemplateAutoConfiguration.java
@@ -0,0 +1,135 @@
+/*
+ * Copyright 2012-2013 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.springframework.boot.autoconfigure.groovy.template;
+
+import groovy.text.TemplateEngine;
+import groovy.text.markup.BaseTemplate;
+import groovy.text.markup.MarkupTemplateEngine;
+import groovy.text.markup.TemplateConfiguration;
+
+import java.net.URL;
+import java.net.URLClassLoader;
+
+import javax.annotation.PostConstruct;
+import javax.servlet.Servlet;
+
+import org.springframework.beans.factory.BeanClassLoaderAware;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.AutoConfigureAfter;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnWebApplication;
+import org.springframework.boot.autoconfigure.groovy.template.web.GroovyTemplateViewResolver;
+import org.springframework.boot.autoconfigure.groovy.template.web.LocaleAwareTemplate;
+import org.springframework.boot.autoconfigure.web.WebMvcAutoConfiguration;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.i18n.LocaleContextHolder;
+import org.springframework.core.Ordered;
+import org.springframework.core.io.DefaultResourceLoader;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.ResourceLoader;
+import org.springframework.util.Assert;
+
+/**
+ * Autoconfiguration support for Groovy templates in MVC. By default creates a
+ * {@link MarkupTemplateEngine} configured from {@link GroovyTemplateProperties}, but you
+ * can override that by providing a {@link TemplateEngine} of a different type.
+ * 
+ * @author Dave Syer
+ * @since 1.1.0
+ */
+@Configuration
+@ConditionalOnClass(TemplateEngine.class)
+@AutoConfigureAfter(WebMvcAutoConfiguration.class)
+@EnableConfigurationProperties(GroovyTemplateProperties.class)
+public class GroovyTemplateAutoConfiguration {
+
+	@Autowired
+	private final ResourceLoader resourceLoader = new DefaultResourceLoader();
+
+	@Autowired
+	private GroovyTemplateProperties properties;
+
+	@PostConstruct
+	public void checkTemplateLocationExists() {
+		if (this.properties.isCheckTemplateLocation()) {
+			Resource resource = this.resourceLoader.getResource(this.properties
+					.getPrefix());
+			Assert.state(resource.exists(), "Cannot find template location: " + resource
+					+ " (please add some templates "
+					+ "or check your FreeMarker configuration)");
+		}
+	}
+
+	@Configuration
+	@ConditionalOnClass({ Servlet.class, LocaleContextHolder.class })
+	@ConditionalOnWebApplication
+	public static class GroovyWebConfiguration implements BeanClassLoaderAware {
+
+		@Autowired
+		private final ResourceLoader resourceLoader = new DefaultResourceLoader();
+
+		@Autowired
+		private GroovyTemplateProperties properties;
+
+		private ClassLoader classLoader = GroovyWebConfiguration.class.getClassLoader();
+
+		@Override
+		public void setBeanClassLoader(ClassLoader classLoader) {
+			this.classLoader = classLoader;
+		}
+
+		@Bean
+		@ConditionalOnMissingBean(TemplateEngine.class)
+		public TemplateEngine groovyTemplateEngine() throws Exception {
+			TemplateConfiguration configuration = this.properties.getConfiguration();
+			if (configuration.getBaseTemplateClass() == BaseTemplate.class) {
+				// Enable locale-dependent includes
+				configuration.setBaseTemplateClass(LocaleAwareTemplate.class);
+			}
+			return new MarkupTemplateEngine(createParentLoaderForTemplates(),
+					configuration);
+		}
+
+		private ClassLoader createParentLoaderForTemplates() throws Exception {
+			return new URLClassLoader(new URL[] { this.resourceLoader.getResource(
+					this.properties.getPrefix()).getURL() }, this.classLoader);
+		}
+
+		@Bean
+		@ConditionalOnMissingBean(name = "groovyTemplateViewResolver")
+		public GroovyTemplateViewResolver groovyTemplateViewResolver(TemplateEngine engine) {
+			GroovyTemplateViewResolver resolver = new GroovyTemplateViewResolver();
+			resolver.setPrefix(this.properties.getPrefix());
+			resolver.setSuffix(this.properties.getSuffix());
+			resolver.setCache(this.properties.isCache());
+			resolver.setContentType(this.properties.getContentType());
+			resolver.setCharSet(this.properties.getCharSet());
+			resolver.setViewNames(this.properties.getViewNames());
+			resolver.setTemplateEngine(engine);
+
+			// This resolver acts as a fallback resolver (e.g. like a
+			// InternalResourceViewResolver) so it needs to have low precedence
+			resolver.setOrder(Ordered.LOWEST_PRECEDENCE - 6);
+
+			return resolver;
+		}
+	}
+
+}
---
+++ spring-boot-autoconfigure/src/main/java/org/springframework/boot/autoconfigure/groovy/template/GroovyTemplateProperties.java
@@ -0,0 +1,116 @@
+/*
+ * Copyright 2012-2013 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.springframework.boot.autoconfigure.groovy.template;
+
+import groovy.text.markup.TemplateConfiguration;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+/**
+ * @author Dave Syer
+ * @since 1.1.0
+ */
+@ConfigurationProperties(prefix = "spring.groovy.template")
+public class GroovyTemplateProperties {
+
+	public static final String DEFAULT_PREFIX = "classpath:/templates/";
+
+	public static final String DEFAULT_SUFFIX = ".tpl";
+
+	private String prefix = DEFAULT_PREFIX;
+
+	private String suffix = DEFAULT_SUFFIX;
+
+	private boolean cache;
+
+	private String contentType = "text/html";
+
+	private String charSet = "UTF-8";
+
+	private String[] viewNames;
+
+	private boolean checkTemplateLocation = true;
+
+	private TemplateConfiguration configuration = new TemplateConfiguration();
+
+	public void setCheckTemplateLocation(boolean checkTemplateLocation) {
+		this.checkTemplateLocation = checkTemplateLocation;
+	}
+
+	public boolean isCheckTemplateLocation() {
+		return this.checkTemplateLocation;
+	}
+
+	public String[] getViewNames() {
+		return this.viewNames;
+	}
+
+	public void setViewNames(String[] viewNames) {
+		this.viewNames = viewNames;
+	}
+
+	public boolean isCache() {
+		return this.cache;
+	}
+
+	public void setCache(boolean cache) {
+		this.cache = cache;
+	}
+
+	public String getContentType() {
+		return this.contentType
+				+ (this.contentType.contains(";charset=") ? "" : ";charset="
+						+ this.charSet);
+	}
+
+	public void setContentType(String contentType) {
+		this.contentType = contentType;
+	}
+
+	public String getCharSet() {
+		return this.charSet;
+	}
+
+	public void setCharSet(String charSet) {
+		this.charSet = charSet;
+	}
+
+	public String getPrefix() {
+		return this.prefix;
+	}
+
+	public void setPrefix(String prefix) {
+		this.prefix = prefix;
+	}
+
+	public String getSuffix() {
+		return this.suffix;
+	}
+
+	public void setSuffix(String suffix) {
+		this.suffix = suffix;
+	}
+
+	public void setConfiguration(TemplateConfiguration configuration) {
+		this.configuration = configuration;
+	}
+
+	public TemplateConfiguration getConfiguration() {
+		return this.configuration;
+	}
+
+}
