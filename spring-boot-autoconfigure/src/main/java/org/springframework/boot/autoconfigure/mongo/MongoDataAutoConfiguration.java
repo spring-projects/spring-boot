233a2e8fb5c57628d30ec7d73cee51eb861a0d94
Dave Syer <dsyer@gopivotal.com>
2014-05-20 17:17:08 +0100

    Fix ordering problem with Mongo autoconfig

--- spring-boot-autoconfigure/src/main/java/org/springframework/boot/autoconfigure/mongo/MongoDataAutoConfiguration.java
+++ spring-boot-autoconfigure/src/main/java/org/springframework/boot/autoconfigure/mongo/MongoDataAutoConfiguration.java
@@ -19,6 +19,7 @@ package org.springframework.boot.autoconfigure.mongo;
 import java.net.UnknownHostException;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.AutoConfigureAfter;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
@@ -47,6 +48,7 @@ import com.mongodb.Mongo;
  */
 @Configuration
 @ConditionalOnClass({ Mongo.class, MongoTemplate.class })
+@AutoConfigureAfter(MongoAutoConfiguration.class)
 public class MongoDataAutoConfiguration {
 
 	@Autowired
