665bd6e57ea965bd08b6fa931e34379e50cf5325
Dave Syer <dsyer@gopivotal.com>
2014-05-21 12:47:53 +0100

    Add conditional processing option for unhelpful FactoryBeans
    OnBeanCondition has some issues with FactoryBean object types where
    the FactoryBean is not generic (i.e. you have to instantiate it to
    get its object type). This is a known issue (see tests in
    ConditionalOnMissingBeanTests), but we can provide some help for
    library authors who know the type in advance. The approach we have
    taken here is to check the BeanDefinition for an attribute called
    "factoryBeanObjectType" (OnBeanCondition.FACTORY_BEAN_OBJECT_TYPE)
    which, if it exists, can be used as a tie-breaker. Its value should
    be a Class<?> instance.
    
    Fixes gh-921

--- spring-boot-autoconfigure/src/test/java/org/springframework/boot/autoconfigure/condition/ConditionalOnMissingBeanTests.java
+++ spring-boot-autoconfigure/src/test/java/org/springframework/boot/autoconfigure/condition/ConditionalOnMissingBeanTests.java
@@ -18,11 +18,16 @@ package org.springframework.boot.autoconfigure.condition;
 
 import org.junit.Test;
 import org.springframework.beans.factory.FactoryBean;
+import org.springframework.beans.factory.support.BeanDefinitionBuilder;
+import org.springframework.beans.factory.support.BeanDefinitionRegistry;
 import org.springframework.boot.autoconfigure.PropertyPlaceholderAutoConfiguration;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
+import org.springframework.context.annotation.ImportBeanDefinitionRegistrar;
 import org.springframework.context.annotation.ImportResource;
+import org.springframework.core.type.AnnotationMetadata;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.util.Assert;
 
@@ -142,6 +147,26 @@ public class ConditionalOnMissingBeanTests {
 	}
 
 	@Test
+	public void testOnMissingBeanConditionWithRegisteredFactoryBean() {
+		this.context.register(RegisteredFactoryBeanConfiguration.class,
+				ConditionalOnFactoryBean.class,
+				PropertyPlaceholderAutoConfiguration.class);
+		this.context.refresh();
+		assertThat(this.context.getBean(ExampleBean.class).toString(),
+				equalTo("fromFactory"));
+	}
+
+	@Test
+	public void testOnMissingBeanConditionWithNonspecificFactoryBean() {
+		this.context.register(NonspecificFactoryBeanConfiguration.class,
+				ConditionalOnFactoryBean.class,
+				PropertyPlaceholderAutoConfiguration.class);
+		this.context.refresh();
+		assertThat(this.context.getBean(ExampleBean.class).toString(),
+				equalTo("fromFactory"));
+	}
+
+	@Test
 	public void testOnMissingBeanConditionWithFactoryBeanInXml() {
 		this.context.register(FactoryBeanXmlConfiguration.class,
 				ConditionalOnFactoryBean.class,
@@ -183,6 +208,47 @@ public class ConditionalOnMissingBeanTests {
 		public FactoryBean exampleBeanFactoryBean() {
 			return new ExampleFactoryBean("foo");
 		}
+	}
+
+	@Configuration
+	@Import(NonspecificFactoryBeanRegistrar.class)
+	protected static class NonspecificFactoryBeanConfiguration {
+	}
+
+	protected static class NonspecificFactoryBeanRegistrar implements
+			ImportBeanDefinitionRegistrar {
+
+		@Override
+		public void registerBeanDefinitions(AnnotationMetadata meta,
+				BeanDefinitionRegistry registry) {
+			BeanDefinitionBuilder builder = BeanDefinitionBuilder
+					.genericBeanDefinition(NonspecificFactoryBean.class);
+			builder.addConstructorArgValue("foo");
+			builder.getBeanDefinition().setAttribute(
+					OnBeanCondition.FACTORY_BEAN_OBJECT_TYPE, ExampleBean.class);
+			registry.registerBeanDefinition("exampleBeanFactoryBean",
+					builder.getBeanDefinition());
+		}
+
+	}
+
+	@Configuration
+	@Import(FactoryBeanRegistrar.class)
+	protected static class RegisteredFactoryBeanConfiguration {
+	}
+
+	protected static class FactoryBeanRegistrar implements ImportBeanDefinitionRegistrar {
+
+		@Override
+		public void registerBeanDefinitions(AnnotationMetadata meta,
+				BeanDefinitionRegistry registry) {
+			BeanDefinitionBuilder builder = BeanDefinitionBuilder
+					.genericBeanDefinition(ExampleFactoryBean.class);
+			builder.addConstructorArgValue("foo");
+			registry.registerBeanDefinition("exampleBeanFactoryBean",
+					builder.getBeanDefinition());
+		}
+
 	}
 
 	@Configuration
@@ -272,6 +338,29 @@ public class ConditionalOnMissingBeanTests {
 	public static class ExampleFactoryBean implements FactoryBean<ExampleBean> {
 
 		public ExampleFactoryBean(String value) {
+			Assert.state(!value.contains("$"));
+		}
+
+		@Override
+		public ExampleBean getObject() throws Exception {
+			return new ExampleBean("fromFactory");
+		}
+
+		@Override
+		public Class<?> getObjectType() {
+			return ExampleBean.class;
+		}
+
+		@Override
+		public boolean isSingleton() {
+			return false;
+		}
+
+	}
+
+	public static class NonspecificFactoryBean implements FactoryBean<Object> {
+
+		public NonspecificFactoryBean(String value) {
 			Assert.state(!value.contains("$"));
 		}
 
