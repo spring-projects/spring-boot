377c6d361032ceaec3220130cf61e29c51e19ca0
Andy Wilkinson <awilkinson@gopivotal.com>
2014-05-30 18:14:27 +0100

    Enhance DependencyCustomer: allow type and classifier to be specified
    Closes #1002

--- spring-boot-cli/src/main/java/org/springframework/boot/cli/compiler/dependencies/ArtifactCoordinatesResolver.java
+++ spring-boot-cli/src/main/java/org/springframework/boot/cli/compiler/dependencies/ArtifactCoordinatesResolver.java
@@ -17,9 +17,13 @@
 package org.springframework.boot.cli.compiler.dependencies;
 
 /**
- * A resolver for artifacts' Maven coordinates, allowing a group id or version to be
- * obtained from an artifact ID.
- * 
+ * A resolver for artifacts' Maven coordinates, allowing group id, artifact id, or version
+ * to be obtained from a module identifier. A module identifier may be in the form
+ * {@code groupId:artifactId:version}, in which case coordinate resolution simply extracts
+ * the relevant piece from the identifier. Alternatively the identifier may be in the form
+ * {@code artifactId}, in which case coordinate resolution uses implementation-specific
+ * metadata to resolve the groupId and version.
+ *
  * @author Andy Wilkinson
  */
 public interface ArtifactCoordinatesResolver {
@@ -36,7 +40,7 @@ public interface ArtifactCoordinatesResolver {
 	 * Gets the artifact id of the artifact identified by the given {@code module}.
 	 * Returns {@code null} if the artifact is unknown to the resolver.
 	 * @param module The id of the module
-	 * @return The group id of the module
+	 * @return The artifact id of the module
 	 */
 	String getArtifactId(String module);
 
