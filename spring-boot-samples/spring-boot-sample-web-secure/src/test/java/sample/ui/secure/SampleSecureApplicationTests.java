b7d94d1364ebfaca47cbcd9cdc3b4e54510c6068
Javier Gayoso <jhavi85@gmail.com>
2014-06-05 16:11:03 +0200

    Optimized login form - delegated CSRF token creation to thymeleaf
    Also added additional test to verify behaviour.
    
    Fixes gh-1039

--- spring-boot-samples/spring-boot-sample-web-secure/src/test/java/sample/ui/secure/SampleSecureApplicationTests.java
+++ spring-boot-samples/spring-boot-sample-web-secure/src/test/java/sample/ui/secure/SampleSecureApplicationTests.java
@@ -70,6 +70,18 @@ public class SampleSecureApplicationTests {
 	}
 
 	@Test
+	public void testLoginPage() throws Exception {
+		HttpHeaders headers = new HttpHeaders();
+		headers.setAccept(Arrays.asList(MediaType.TEXT_HTML));
+		ResponseEntity<String> entity = new TestRestTemplate().exchange(
+				"http://localhost:" + this.port + "/login", HttpMethod.GET, new HttpEntity<Void>(
+						headers), String.class);
+		assertEquals(HttpStatus.OK, entity.getStatusCode());
+		assertTrue("Wrong content:\n" + entity.getBody(),
+				entity.getBody().contains("_csrf"));
+	}
+
+	@Test
 	public void testLogin() throws Exception {
 		HttpHeaders headers = getHeaders();
 		headers.setAccept(Arrays.asList(MediaType.TEXT_HTML));
