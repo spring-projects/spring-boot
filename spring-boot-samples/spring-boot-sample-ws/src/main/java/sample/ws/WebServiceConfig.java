b585afe537afd4b0f9a46d7497018c9b730051fe
Andy Wilkinson <awilkinson@gopivotal.com>
2014-06-09 18:03:22 +0100

    Polishing

--- spring-boot-samples/spring-boot-sample-ws/src/main/java/sample/ws/WebServiceConfig.java
+++ spring-boot-samples/spring-boot-sample-ws/src/main/java/sample/ws/WebServiceConfig.java
@@ -1,3 +1,18 @@
+/*
+ * Copyright 2012-2014 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package sample.ws;
 
 import org.springframework.boot.context.embedded.ServletRegistrationBean;
@@ -12,19 +27,14 @@ import org.springframework.ws.wsdl.wsdl11.DefaultWsdl11Definition;
 import org.springframework.xml.xsd.SimpleXsdSchema;
 import org.springframework.xml.xsd.XsdSchema;
 
-/**
- * Configures Spring Web Service components
- *
- * @author Maciej Walkowiak
- */
 @EnableWs
 @Configuration
 public class WebServiceConfig extends WsConfigurerAdapter {
+
 	@Bean
 	public ServletRegistrationBean dispatcherServlet(ApplicationContext applicationContext) {
 		MessageDispatcherServlet servlet = new MessageDispatcherServlet();
 		servlet.setApplicationContext(applicationContext);
-
 		return new ServletRegistrationBean(servlet, "/services/*");
 	}
 
--- spring-boot-samples/spring-boot-sample-ws/src/test/java/sample/ws/SampleWsApplicationTests.java
+++ spring-boot-samples/spring-boot-sample-ws/src/test/java/sample/ws/SampleWsApplicationTests.java
@@ -1,4 +1,24 @@
+/*
+ * Copyright 2012-2014 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package sample.ws;
+
+import java.io.StringReader;
+
+import javax.xml.transform.stream.StreamResult;
+import javax.xml.transform.stream.StreamSource;
 
 import org.junit.Before;
 import org.junit.Test;
@@ -10,20 +30,12 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.ws.client.core.WebServiceTemplate;
 
-import javax.xml.transform.stream.StreamResult;
-import javax.xml.transform.stream.StreamSource;
-import java.io.StringReader;
-
-/**
- * Tests handling SOAP message
- *
- * @author Maciej Walkowiak
- */
 @RunWith(SpringJUnit4ClassRunner.class)
 @SpringApplicationConfiguration(classes = SampleWsApplication.class)
 @WebAppConfiguration
 @IntegrationTest
 public class SampleWsApplicationTests {
+
 	private WebServiceTemplate webServiceTemplate = new WebServiceTemplate();
 
 	@Value("${local.server.port}")
@@ -31,7 +43,8 @@ public class SampleWsApplicationTests {
 
 	@Before
 	public void setUp() {
-		webServiceTemplate.setDefaultUri("http://localhost:" + serverPort + "/services/");
+		this.webServiceTemplate.setDefaultUri("http://localhost:" + this.serverPort
+				+ "/services/");
 	}
 
 	@Test
@@ -51,6 +64,6 @@ public class SampleWsApplicationTests {
 		StreamSource source = new StreamSource(new StringReader(request));
 		StreamResult result = new StreamResult(System.out);
 
-		webServiceTemplate.sendSourceAndReceiveToResult(source, result);
+		this.webServiceTemplate.sendSourceAndReceiveToResult(source, result);
 	}
 }
