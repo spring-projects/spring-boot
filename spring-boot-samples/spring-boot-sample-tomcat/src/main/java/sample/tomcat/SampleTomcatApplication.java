a2efe17fc9b62d1460891127e663095a2741a2b8
Phillip Webb <pwebb@gopivotal.com>
2014-06-18 16:30:22 -0700

    Polish

--- spring-boot-samples/spring-boot-sample-tomcat/src/main/java/sample/tomcat/SampleTomcatApplication.java
+++ spring-boot-samples/spring-boot-sample-tomcat/src/main/java/sample/tomcat/SampleTomcatApplication.java
@@ -34,13 +34,10 @@ public class SampleTomcatApplication {
 
 	private static Log logger = LogFactory.getLog(SampleTomcatApplication.class);
 
-	public static void main(String[] args) throws Exception {
-		SpringApplication.run(SampleTomcatApplication.class, args);
-	}
-
 	@Bean
 	protected ServletContextListener listener() {
 		return new ServletContextListener() {
+
 			@Override
 			public void contextInitialized(ServletContextEvent sce) {
 				logger.info("ServletContext initialized");
@@ -50,7 +47,12 @@ public class SampleTomcatApplication {
 			public void contextDestroyed(ServletContextEvent sce) {
 				logger.info("ServletContext destroyed");
 			}
+
 		};
+	}
+
+	public static void main(String[] args) throws Exception {
+		SpringApplication.run(SampleTomcatApplication.class, args);
 	}
 
 }
