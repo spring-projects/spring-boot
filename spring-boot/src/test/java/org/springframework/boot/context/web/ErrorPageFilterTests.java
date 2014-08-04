9372d15433b30e213e073022eeb5e642d5c9c8b8
Dave Syer <dsyer@pivotal.io>
2014-08-04 14:55:02 -0700

    Explicitly set status on raw unwrapped response
    The ErrorPageFilter wasn't setting the response status in the case that
    there was an error page mapped to the current request (i.e. for all
    autoconfigured apps). N.B. this only affects non-embedded apps.
    
    Fixes gh-1320

--- spring-boot/src/test/java/org/springframework/boot/context/web/ErrorPageFilterTests.java
+++ spring-boot/src/test/java/org/springframework/boot/context/web/ErrorPageFilterTests.java
@@ -64,6 +64,28 @@ public class ErrorPageFilterTests {
 	}
 
 	@Test
+	public void unauthorizedWithErrorPath() throws Exception {
+		this.filter.addErrorPages(new ErrorPage("/error"));
+		this.chain = new MockFilterChain() {
+			@Override
+			public void doFilter(ServletRequest request, ServletResponse response)
+					throws IOException, ServletException {
+				((HttpServletResponse) response).sendError(401, "UNAUTHORIZED");
+				super.doFilter(request, response);
+			}
+		};
+		this.filter.doFilter(this.request, this.response, this.chain);
+		assertThat(this.chain.getRequest(), equalTo((ServletRequest) this.request));
+		HttpServletResponseWrapper wrapper = (HttpServletResponseWrapper) this.chain
+				.getResponse();
+		assertThat(wrapper.getResponse(), equalTo((ServletResponse) this.response));
+		assertTrue(this.response.isCommitted());
+		assertThat(wrapper.getStatus(), equalTo(401));
+		// The real response has to be 401 as well...
+		assertThat(this.response.getStatus(), equalTo(401));
+	}
+
+	@Test
 	public void responseCommitted() throws Exception {
 		this.filter.addErrorPages(new ErrorPage("/error"));
 		this.response.setCommitted(true);
