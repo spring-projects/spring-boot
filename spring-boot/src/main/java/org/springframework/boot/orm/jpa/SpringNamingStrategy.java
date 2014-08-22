508bd14ad9b2f0bf0006cccfc224d1dab4b955bf
Josh Long <jlong@pivotal.io>
2014-08-22 11:55:51 -0700

    Add SpringJtaPlatform for Hibernate
    Add SpringJtaPlatform Hibernate AbstractJtaPlatform implementation. Also
    relocated SpringNamingStrategy to the `hibernate` sub-package.
    
    See gh-947

--- spring-boot/src/main/java/org/springframework/boot/orm/jpa/SpringNamingStrategy.java
+++ spring-boot/src/main/java/org/springframework/boot/orm/jpa/SpringNamingStrategy.java
@@ -16,32 +16,17 @@
 
 package org.springframework.boot.orm.jpa;
 
-import org.hibernate.cfg.ImprovedNamingStrategy;
 import org.hibernate.cfg.NamingStrategy;
-import org.hibernate.internal.util.StringHelper;
-import org.springframework.util.Assert;
-import org.springframework.util.StringUtils;
 
 /**
  * Hibernate {@link NamingStrategy} that follows Spring recommended naming conventions.
- * Naming conventions implemented here are identical to {@link ImprovedNamingStrategy}
- * with the exception that foreign key columns include the referenced column name.
  *
  * @author Phillip Webb
- * @see "http://stackoverflow.com/questions/7689206/ejb3namingstrategy-vs-improvednamingstrategy-foreign-key-naming"
+ * @deprecated Since 1.2.0 in favor of
+ * {@link org.springframework.boot.orm.jpa.hibernate.SpringNamingStrategy}
  */
-public class SpringNamingStrategy extends ImprovedNamingStrategy {
-
-	@Override
-	public String foreignKeyColumnName(String propertyName, String propertyEntityName,
-			String propertyTableName, String referencedColumnName) {
-		String name = propertyTableName;
-		if (propertyName != null) {
-			name = StringHelper.unqualify(propertyName);
-		}
-		Assert.state(StringUtils.hasLength(name),
-				"Unable to generate foreignKeyColumnName");
-		return columnName(name) + "_" + referencedColumnName;
-	}
+@Deprecated
+public class SpringNamingStrategy extends
+		org.springframework.boot.orm.jpa.hibernate.SpringNamingStrategy {
 
 }
---
+++ spring-boot/src/main/java/org/springframework/boot/orm/jpa/hibernate/SpringNamingStrategy.java
@@ -0,0 +1,48 @@
+/*
+ * Copyright 2012-2014 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.springframework.boot.orm.jpa.hibernate;
+
+import org.hibernate.cfg.ImprovedNamingStrategy;
+import org.hibernate.cfg.NamingStrategy;
+import org.hibernate.internal.util.StringHelper;
+import org.springframework.util.Assert;
+import org.springframework.util.StringUtils;
+
+/**
+ * Hibernate {@link NamingStrategy} that follows Spring recommended naming conventions.
+ * Naming conventions implemented here are identical to {@link ImprovedNamingStrategy}
+ * with the exception that foreign key columns include the referenced column name.
+ *
+ * @author Phillip Webb
+ * @see "http://stackoverflow.com/questions/7689206/ejb3namingstrategy-vs-improvednamingstrategy-foreign-key-naming"
+ * @since 1.2.0
+ */
+public class SpringNamingStrategy extends ImprovedNamingStrategy {
+
+	@Override
+	public String foreignKeyColumnName(String propertyName, String propertyEntityName,
+			String propertyTableName, String referencedColumnName) {
+		String name = propertyTableName;
+		if (propertyName != null) {
+			name = StringHelper.unqualify(propertyName);
+		}
+		Assert.state(StringUtils.hasLength(name),
+				"Unable to generate foreignKeyColumnName");
+		return columnName(name) + "_" + referencedColumnName;
+	}
+
+}
