8e9e502b6a07421c1f7cc081d7a78c8f8b45d14b
David Liu <dliu@pivotal.io>
2014-09-01 16:09:46 +0800

    Add support for auto-configuration of Commons DBCP2
    Closes gh-1292
    Closes gh-1477

--- spring-boot-autoconfigure/src/main/java/org/springframework/boot/autoconfigure/jdbc/DataSourceBuilder.java
+++ spring-boot-autoconfigure/src/main/java/org/springframework/boot/autoconfigure/jdbc/DataSourceBuilder.java
@@ -43,7 +43,8 @@ public class DataSourceBuilder {
 	private static final String[] DATA_SOURCE_TYPE_NAMES = new String[] {
 			"org.apache.tomcat.jdbc.pool.DataSource",
 			"com.zaxxer.hikari.HikariDataSource",
-			"org.apache.commons.dbcp.BasicDataSource" };
+			"org.apache.commons.dbcp.BasicDataSource",
+			"org.apache.commons.dbcp2.BasicDataSource" };
 
 	private Class<? extends DataSource> type;
 
