b948e6146521cd476865b47b3f6fd32bf143cab8
David Liu <dliu@pivotal.io>
2014-09-04 07:42:17 +0800

    Add CAMELCASE_TO_HYPHEN in RelaxedNames
    Fixes gh-1499

--- spring-boot/src/test/java/org/springframework/boot/bind/RelaxedNamesTests.java
+++ spring-boot/src/test/java/org/springframework/boot/bind/RelaxedNamesTests.java
@@ -72,9 +72,24 @@ public class RelaxedNamesTests {
 		Iterator<String> iterator = new RelaxedNames("caMel").iterator();
 		assertThat(iterator.next(), equalTo("caMel"));
 		assertThat(iterator.next(), equalTo("ca_mel"));
+		assertThat(iterator.next(), equalTo("ca-mel"));
 		assertThat(iterator.next(), equalTo("camel"));
 		assertThat(iterator.next(), equalTo("CAMEL"));
 		assertThat(iterator.next(), equalTo("CA_MEL"));
+		assertThat(iterator.next(), equalTo("CA-MEL"));
+		assertThat(iterator.hasNext(), equalTo(false));
+	}
+
+	@Test
+	public void fromCompoundCamelCase() throws Exception {
+		Iterator<String> iterator = new RelaxedNames("caMelCase").iterator();
+		assertThat(iterator.next(), equalTo("caMelCase"));
+		assertThat(iterator.next(), equalTo("ca_mel_case"));
+		assertThat(iterator.next(), equalTo("ca-mel-case"));
+		assertThat(iterator.next(), equalTo("camelcase"));
+		assertThat(iterator.next(), equalTo("CAMELCASE"));
+		assertThat(iterator.next(), equalTo("CA_MEL_CASE"));
+		assertThat(iterator.next(), equalTo("CA-MEL-CASE"));
 		assertThat(iterator.hasNext(), equalTo(false));
 	}
 
