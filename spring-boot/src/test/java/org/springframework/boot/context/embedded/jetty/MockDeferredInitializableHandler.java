3c4f102352fe81e5601523a85c68428500757606
bhandy <brad.handy@gmail.com>
2014-09-10 21:34:13 -0400

    Add javadoc class comment.

--- spring-boot/src/test/java/org/springframework/boot/context/embedded/jetty/MockDeferredInitializableHandler.java
+++ spring-boot/src/test/java/org/springframework/boot/context/embedded/jetty/MockDeferredInitializableHandler.java
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2012-2014 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package org.springframework.boot.context.embedded.jetty;
 
 import org.eclipse.jetty.server.Handler;
@@ -9,6 +25,11 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
+/**
+ * Base NoOp implementation of DeferredInitializable and Handler for unit tests.
+ *
+ * @author Bradley M Handy
+ */
 public class MockDeferredInitializableHandler implements DeferredInitializable, Handler {
 
     private Server server;
