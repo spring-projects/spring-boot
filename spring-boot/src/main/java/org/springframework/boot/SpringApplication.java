1e53e759b385a9c64d112f5a172da801d32cfd2e
Henryk Konsek <hekonsek@gmail.com>
2014-09-12 22:51:55 +0200

    Added "spring.main.sources" constant.

--- spring-boot/src/main/java/org/springframework/boot/SpringApplication.java
+++ spring-boot/src/main/java/org/springframework/boot/SpringApplication.java
@@ -146,6 +146,8 @@ import org.springframework.web.context.support.StandardServletEnvironment;
  */
 public class SpringApplication {
 
+    public static final String MAIN_SOURCES = "spring.main.sources";
+
 	private static final String DEFAULT_CONTEXT_CLASS = "org.springframework.context."
 			+ "annotation.AnnotationConfigApplicationContext";
 
