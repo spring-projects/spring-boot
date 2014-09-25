abed1f4d4cb7b35a9117e5aa77cec8ce011f74e7
Phillip Webb <pwebb@pivotal.io>
2014-09-25 10:41:12 -0700

    Restore ThymeleafAutoConfiguration test
    Restore the test removed during the merge.

--- spring-boot-autoconfigure/src/test/java/org/springframework/boot/autoconfigure/thymeleaf/ThymeleafAutoConfigurationTests.java
+++ spring-boot-autoconfigure/src/test/java/org/springframework/boot/autoconfigure/thymeleaf/ThymeleafAutoConfigurationTests.java
@@ -141,6 +141,17 @@ public class ThymeleafAutoConfigurationTests {
 	}
 
 	@Test
+	public void useDataDialect() throws Exception {
+		this.context.register(ThymeleafAutoConfiguration.class,
+				PropertyPlaceholderAutoConfiguration.class);
+		this.context.refresh();
+		TemplateEngine engine = this.context.getBean(TemplateEngine.class);
+		Context attrs = new Context(Locale.UK, Collections.singletonMap("foo", "bar"));
+		String result = engine.process("data-dialect", attrs);
+		assertEquals("<html><body data-foo=\"bar\"></body></html>", result);
+	}
+
+	@Test
 	public void renderTemplate() throws Exception {
 		this.context.register(ThymeleafAutoConfiguration.class,
 				PropertyPlaceholderAutoConfiguration.class);
