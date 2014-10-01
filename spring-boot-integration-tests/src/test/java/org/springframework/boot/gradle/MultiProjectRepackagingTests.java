bcd4c8eee20edaf4f9e71b3199dd36f5c0121101
Andy Wilkinson <awilkinson@pivotal.io>
2014-10-01 19:20:04 +0100

    Produce a single library for multiple file dependencies on the same file
    Previously, the Gradle plugin’s ProjectLibraries produced a new library
    for every file dependency, even if the dependencies where on the same
    file. This would lead to a repackaging failure due to multiple
    libraries having the same name.
    
    This commit updates ProjectLibraries to treat file dependencies on the
    same file as a single library, thereby resolving the name clash.
    
    Fixes gh-1646

--- spring-boot-integration-tests/src/test/java/org/springframework/boot/gradle/MultiProjectRepackagingTests.java
+++ spring-boot-integration-tests/src/test/java/org/springframework/boot/gradle/MultiProjectRepackagingTests.java
@@ -17,11 +17,9 @@
 package org.springframework.boot.gradle;
 
 import java.io.File;
-import java.io.IOException;
 import java.util.jar.JarFile;
 
 import org.gradle.tooling.ProjectConnection;
-import org.junit.BeforeClass;
 import org.junit.Test;
 import org.springframework.boot.dependency.tools.ManagedDependencies;
 
@@ -38,20 +36,30 @@ public class MultiProjectRepackagingTests {
 	private static final String BOOT_VERSION = ManagedDependencies.get()
 			.find("spring-boot").getVersion();
 
-	private static ProjectConnection project;
-
-	@BeforeClass
-	public static void createProject() throws IOException {
-		project = new ProjectCreator().createProject("multi-project-repackage");
+	@Test
+	public void repackageWithTransitiveFileDependency() throws Exception {
+		ProjectConnection project = new ProjectCreator()
+				.createProject("multi-project-transitive-file-dependency");
+		project.newBuild().forTasks("clean", "build")
+				.withArguments("-PbootVersion=" + BOOT_VERSION).run();
+		File buildLibs = new File(
+				"target/multi-project-transitive-file-dependency/main/build/libs");
+		JarFile jarFile = new JarFile(new File(buildLibs, "main.jar"));
+		assertThat(jarFile.getEntry("lib/commons-logging-1.1.3.jar"), notNullValue());
+		assertThat(jarFile.getEntry("lib/foo.jar"), notNullValue());
+		jarFile.close();
 	}
 
 	@Test
-	public void repackageWithTransitiveFileDependency() throws Exception {
+	public void repackageWithCommonFileDependency() throws Exception {
+		ProjectConnection project = new ProjectCreator()
+				.createProject("multi-project-common-file-dependency");
 		project.newBuild().forTasks("clean", "build")
-				.withArguments("-PbootVersion=" + BOOT_VERSION, "-Prepackage=true").run();
-		File buildLibs = new File("target/multi-project-repackage/main/build/libs");
-		JarFile jarFile = new JarFile(new File(buildLibs, "main.jar"));
-		assertThat(jarFile.getEntry("lib/commons-logging-1.1.3.jar"), notNullValue());
+				.withArguments("-PbootVersion=" + BOOT_VERSION).run();
+		File buildLibs = new File(
+				"target/multi-project-common-file-dependency/build/libs");
+		JarFile jarFile = new JarFile(new File(buildLibs,
+				"multi-project-common-file-dependency.jar"));
 		assertThat(jarFile.getEntry("lib/foo.jar"), notNullValue());
 		jarFile.close();
 	}
