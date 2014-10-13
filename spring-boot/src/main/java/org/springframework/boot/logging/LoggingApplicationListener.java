89197eb03b92bbcbed9ebc6293d57beab17bbcd1
Dave Syer <dsyer@pivotal.io>
2014-10-13 17:52:05 +0100

    Add org.hibernate.SQL to the --debug logger levels
    I think this fixes gh-446 (it seems unreasonable to want to log the DDL
    even if it is not being executed)

--- spring-boot/src/main/java/org/springframework/boot/logging/LoggingApplicationListener.java
+++ spring-boot/src/main/java/org/springframework/boot/logging/LoggingApplicationListener.java
@@ -91,6 +91,7 @@ public class LoggingApplicationListener implements SmartApplicationListener {
 		LOG_LEVEL_LOGGERS.add(LogLevel.TRACE, "org.apache.catalina");
 		LOG_LEVEL_LOGGERS.add(LogLevel.TRACE, "org.eclipse.jetty");
 		LOG_LEVEL_LOGGERS.add(LogLevel.TRACE, "org.hibernate.tool.hbm2ddl");
+		LOG_LEVEL_LOGGERS.add(LogLevel.DEBUG, "org.hibernate.SQL");
 	}
 
 	private static Class<?>[] EVENT_TYPES = { ApplicationStartedEvent.class,
