af2a867d1423d3e7cf48813a85e0b87ce2da6778
Dave Syer <dsyer@pivotal.io>
2014-10-28 16:14:31 +0000

    Remove missing module from pom (thanks Eclipse)

--- spring-boot/src/test/java/org/springframework/boot/builder/SpringApplicationBuilderTests.java
+++ spring-boot/src/test/java/org/springframework/boot/builder/SpringApplicationBuilderTests.java
@@ -184,7 +184,7 @@ public class SpringApplicationBuilderTests {
 				ExampleConfig.class).profiles("node").properties("transport=redis")
 				.child(ChildConfig.class).profiles("admin").web(false);
 		this.context = application.run();
-		assertThat(this.context.getEnvironment().acceptsProfiles("node"), is(true));
+		assertThat(this.context.getEnvironment().acceptsProfiles("node", "admin"), is(true));
 		assertThat(this.context.getParent().getEnvironment().acceptsProfiles("admin"),
 				is(false));
 	}
