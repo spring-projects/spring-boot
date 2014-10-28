e2f2099197b27d5135129807b3c1b8ad6e8f5463
Dave Syer <dsyer@pivotal.io>
2014-10-28 16:12:14 +0000

    Add test for gh-1776

--- spring-boot/src/test/java/org/springframework/boot/builder/SpringApplicationBuilderTests.java
+++ spring-boot/src/test/java/org/springframework/boot/builder/SpringApplicationBuilderTests.java
@@ -179,6 +179,17 @@ public class SpringApplicationBuilderTests {
 	}
 
 	@Test
+	public void parentFirstWithDifferentProfile() throws Exception {
+		SpringApplicationBuilder application = new SpringApplicationBuilder(
+				ExampleConfig.class).profiles("node").properties("transport=redis")
+				.child(ChildConfig.class).profiles("admin").web(false);
+		this.context = application.run();
+		assertThat(this.context.getEnvironment().acceptsProfiles("node"), is(true));
+		assertThat(this.context.getParent().getEnvironment().acceptsProfiles("admin"),
+				is(false));
+	}
+
+	@Test
 	public void parentContextIdentical() throws Exception {
 		SpringApplicationBuilder application = new SpringApplicationBuilder(
 				ExampleConfig.class);
