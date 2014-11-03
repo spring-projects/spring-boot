a85e02fbb99e17c30b65219f5a7b43b8241f2a32
Andy Wilkinson <awilkinson@pivotal.io>
2014-11-03 11:31:15 +0000

    Update to Spring Data Evans SR1
    Closes gh-1709

--- spring-boot-samples/spring-boot-sample-data-rest/src/test/java/sample/data/jpa/SampleDataRestApplicationTests.java
+++ spring-boot-samples/spring-boot-sample-data-rest/src/test/java/sample/data/jpa/SampleDataRestApplicationTests.java
@@ -61,7 +61,7 @@ public class SampleDataRestApplicationTests {
 	@Test
 	public void testHome() throws Exception {
 
-		this.mvc.perform(get("/")).andExpect(status().isOk())
+		this.mvc.perform(get("/api")).andExpect(status().isOk())
 				.andExpect(content().string(containsString("hotels")));
 	}
 
