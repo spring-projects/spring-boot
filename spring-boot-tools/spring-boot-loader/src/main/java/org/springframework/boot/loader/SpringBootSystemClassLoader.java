2032b378f44cd76e5397b09329b3d2f4f39d964e
Alex Antonov <alex@antonov.ws>
2014-11-05 12:56:39 -0800

    Enhanced the SpringBootClassLoader to support parameter configuration for spring.boot.classloader.delegate=[first|last] and spring.boot.classloader.exclude=<regex>

--- spring-boot-tools/spring-boot-loader/src/main/java/org/springframework/boot/loader/SpringBootSystemClassLoader.java
+++ spring-boot-tools/spring-boot-loader/src/main/java/org/springframework/boot/loader/SpringBootSystemClassLoader.java
@@ -1,7 +1,10 @@
 package org.springframework.boot.loader;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
+import java.util.regex.Pattern;
 
 /**
  * This class provides ability to specify a custom JVM System Class Loader to enable loading classes provided inside
@@ -17,13 +20,18 @@ import java.util.List;
  * @author Alex Antonov
  */
 public class SpringBootSystemClassLoader extends ClassLoader {
-    private ClassLoader systemParentClassLoader;
-    private ClassLoader springClassLoader;
+    public static final String ORG_SPRINGFRAMEWORK_BOOT_LOADER_PREFIX = "org.springframework.boot.loader";
+    public static final String NULL_PREFIX = "null.";
+    public static final String DELEGATE_ORDER_PROPERTY = "spring.boot.classloader.delegate";
+    public static final String CLASS_EXCLUSION_REGEX_PROPERTY = "spring.boot.classloader.exclude";
+
+    private final ClassLoader systemParentClassLoader;
+    private final ClassLoader springClassLoader;
 
     public SpringBootSystemClassLoader(ClassLoader systemParentClassLoader) {
         super(systemParentClassLoader);
         this.systemParentClassLoader = systemParentClassLoader;
-        init();
+        this.springClassLoader = initSpringClassLoader();
     }
 
     @Override
@@ -31,39 +39,74 @@ public class SpringBootSystemClassLoader extends ClassLoader {
         if (name == null) {
             throw new ClassNotFoundException("Class name can not be NULL!!!");
         }
+        if ("first".equalsIgnoreCase(System.getProperty(DELEGATE_ORDER_PROPERTY, "first"))) {
+            return loadClassFromSystemFirst(name);
+        } else {
+            return loadClassFromSystemLast(name);
+        }
+    }
+
+    private Class<?> loadClassFromSystemFirst(String name) throws ClassNotFoundException {
         try {
             return super.loadClass(name);
         } catch (Exception e) {
             // ClassNotFoundException thrown if class not found
             // from the non-null system parent class loader
         }
+        return loadClassFromSpring(name);
+    }
+
+    private Class<?> loadClassFromSystemLast(String name) throws ClassNotFoundException {
+        try {
+            return loadClassFromSpring(name);
+        } catch (Exception e) {
+            // ClassNotFoundException thrown if class not found
+            // from the spring class loader
+        }
+        return super.loadClass(name);
+    }
+
+    private Class<?> loadClassFromSpring(String name) throws ClassNotFoundException {
         if (!isSpecialCase(name) && this.springClassLoader != null) {
-            return springClassLoader.loadClass(name);
+            return this.springClassLoader.loadClass(name);
         }
         throw new ClassNotFoundException(name);
     }
 
     // Special cases include calls from URL class to retrieve protocol handlers (null.*), as well as anything in the
     //  org.springframework.boot.loader.* package, as those should always be attempted to load from real System CL
-    private static final List<String> specialCaseNames = Arrays.asList("null.file.Handler", "null.jar.Handler");
+    private static final List<Pattern> specialCaseNames = new ArrayList<Pattern>();
     private boolean isSpecialCase(String name) {
-        return specialCaseNames.contains(name)
-                || name.startsWith("org.springframework.boot.loader")
-                || name.startsWith("null.");
+        for (Pattern p : specialCaseNames) {
+            if (p.matcher(name).matches()) {
+                return true;
+            }
+        }
+        return name.startsWith(ORG_SPRINGFRAMEWORK_BOOT_LOADER_PREFIX)
+                || name.startsWith(NULL_PREFIX);
     }
 
-    private void init() {
+    private ClassLoader initSpringClassLoader() {
+        initExclusionRules();
         if (this.systemParentClassLoader != null) {
             JarLauncher jarLauncher = new JarLauncher();
             try {
-                springClassLoader = jarLauncher.createClassLoader(jarLauncher.getClassPathArchives());
+                return jarLauncher.createClassLoader(jarLauncher.getClassPathArchives());
             } catch (Exception e) {
-                e.printStackTrace();
+                throw new RuntimeException("Unable to create Spring Boot Class Loader", e);
             }
         }
+        return null;
     }
 
     protected ClassLoader getSpringClassLoader() {
         return this.springClassLoader;
+    }
+
+    private void initExclusionRules() {
+        String exclusionRulesRegex = System.getProperty(CLASS_EXCLUSION_REGEX_PROPERTY);
+        if (exclusionRulesRegex != null) {
+            specialCaseNames.add(Pattern.compile(exclusionRulesRegex));
+        }
     }
 }
