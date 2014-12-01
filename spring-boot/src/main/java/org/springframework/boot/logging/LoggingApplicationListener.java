ffe53480837f6a6526cae8acd1b77d297e6a4e2f
Phillip Webb <pwebb@pivotal.io>
2014-12-01 13:46:06 -0800

    Polish formatting

--- spring-boot/src/main/java/org/springframework/boot/logging/LoggingApplicationListener.java
+++ spring-boot/src/main/java/org/springframework/boot/logging/LoggingApplicationListener.java
@@ -168,13 +168,13 @@ public class LoggingApplicationListener implements SmartApplicationListener {
 		// Logback won't read backslashes so add a clean path for it to use
 		if (!StringUtils.hasLength(System.getProperty("LOG_TEMP"))) {
 			String path = System.getProperty("java.io.tmpdir");
-            if (path!=null) {
+			if (path != null) {
 				path = StringUtils.cleanPath(path);
 				if (path.endsWith("/")) {
 					path = path.substring(0, path.length() - 1);
 				}
 				System.setProperty("LOG_TEMP", path);
-            }
+			}
 		}
 	}
 
