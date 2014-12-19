1da918f388cf0613ec6a84cb3b90ad4e16852915
Phillip Webb <pwebb@pivotal.io>
2014-12-19 14:01:19 -0800

    Fix TypeNotPresentExceptionProxy error with Mongo
    Update MongoAutoConfiguration so that a TypeNotPresentExceptionProxy
    error is not thrown when Spring Data Mongo is not used.
    
    Fixes gh-2196

--- spring-boot-autoconfigure/src/main/java/org/springframework/boot/autoconfigure/mongo/MongoAutoConfiguration.java
+++ spring-boot-autoconfigure/src/main/java/org/springframework/boot/autoconfigure/mongo/MongoAutoConfiguration.java
@@ -27,7 +27,6 @@ import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.data.mongodb.MongoDbFactory;
 
 import com.mongodb.Mongo;
 import com.mongodb.MongoClientOptions;
@@ -42,7 +41,7 @@ import com.mongodb.MongoClientOptions;
 @Configuration
 @ConditionalOnClass(Mongo.class)
 @EnableConfigurationProperties(MongoProperties.class)
-@ConditionalOnMissingBean(MongoDbFactory.class)
+@ConditionalOnMissingBean(type = "org.springframework.data.mongodb.MongoDbFactory")
 public class MongoAutoConfiguration {
 
 	@Autowired
