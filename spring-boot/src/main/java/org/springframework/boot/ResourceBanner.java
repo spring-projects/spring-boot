8b04973244e14dae61178986d0ae10c8c379bc7c
drumonii <drumonii@users.noreply.github.com>
2015-01-06 22:08:15 -0500

    Add a SpringBootVersion class like SpringVersion
    Provide a static utility method to get the Spring Boot version based on
    the implementation version of SpringApplication's package. Similar to
    Spring Frameworks' SpringVersion class.
    
    Fixes gh-2300

--- spring-boot/src/main/java/org/springframework/boot/ResourceBanner.java
+++ spring-boot/src/main/java/org/springframework/boot/ResourceBanner.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2012-2014 the original author or authors.
+ * Copyright 2012-2015 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -106,7 +106,7 @@ public class ResourceBanner implements Banner {
 	}
 
 	protected String getBootVersion() {
-		return Banner.class.getPackage().getImplementationVersion();
+		return SpringBootVersion.getVersion();
 	}
 
 	private String getVersionString(String version, boolean format) {
--- spring-boot/src/main/java/org/springframework/boot/SpringBootBanner.java
+++ spring-boot/src/main/java/org/springframework/boot/SpringBootBanner.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2012-2013 the original author or authors.
+ * Copyright 2012-2015 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -50,7 +50,7 @@ class SpringBootBanner implements Banner {
 		for (String line : BANNER) {
 			printStream.println(line);
 		}
-		String version = Banner.class.getPackage().getImplementationVersion();
+		String version = SpringBootVersion.getVersion();
 		version = (version == null ? "" : " (v" + version + ")");
 		String padding = "";
 		while (padding.length() < STRAP_LINE_SIZE
