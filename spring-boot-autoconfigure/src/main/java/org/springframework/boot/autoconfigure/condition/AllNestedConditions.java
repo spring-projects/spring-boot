27224f109e23af491951a4dc21872cf48b7c0b58
Eric Fenderbosch <eric.fenderbosch@segmint.com>
2015-01-22 14:00:32 -0500

    extracted AbstractNestedCondition from AnyNestedCondition added AllNestedConditions and NoneOfNestedConditions

---
+++ spring-boot-autoconfigure/src/main/java/org/springframework/boot/autoconfigure/condition/AllNestedConditions.java
@@ -0,0 +1,27 @@
+package org.springframework.boot.autoconfigure.condition;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public abstract class AllNestedConditions extends AbstractNestedCondition {
+
+	public AllNestedConditions(ConfigurationPhase configurationPhase) {
+		super(configurationPhase);
+	}
+
+	@Override
+	protected ConditionOutcome buildConditionOutcome(List<ConditionOutcome> outcomes) {
+		List<ConditionOutcome> match = new ArrayList<ConditionOutcome>();
+		List<ConditionOutcome> nonMatch = new ArrayList<ConditionOutcome>();
+		for (ConditionOutcome outcome : outcomes) {
+			if (outcome.isMatch()) {
+				match.add(outcome);
+			} else {
+				nonMatch.add(outcome);
+			}
+		}
+		return new ConditionOutcome(match.size() == outcomes.size(), "all match resulted in " + match + " matches and "
+				+ nonMatch + " non matches");
+	}
+
+}
