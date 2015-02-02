dfdb8015332767b0678bff667e07b82a084a86fd
Stephane Nicoll <snicoll@pivotal.io>
2015-02-02 11:34:21 +0100

    Fix use of deprecated API.

--- spring-boot-samples/spring-boot-sample-web-ui/src/test/java/sample/ui/MessageControllerWebTests.java
+++ spring-boot-samples/spring-boot-sample-web-ui/src/test/java/sample/ui/MessageControllerWebTests.java
@@ -68,7 +68,7 @@ public class MessageControllerWebTests {
 	@Test
 	public void testCreate() throws Exception {
 		this.mockMvc.perform(post("/").param("text", "FOO text").param("summary", "FOO"))
-				.andExpect(status().isMovedTemporarily())
+				.andExpect(status().isFound())
 				.andExpect(header().string("location", RegexMatcher.matches("/[0-9]+")));
 	}
 
