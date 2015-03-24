147755f396cad20c393e6b7a491416b2fac8fbb3
Ben Hale <bhale@pivotal.io>
2015-03-24 15:55:25 +0000

    Support VcapApplicationListener Number Credentials
    Update VcapApplicationListener to support Number based credentials.
    Previously, any service credential value that wasn't a String would
    be discarded. This was particularly a problem for services that exposed
    a port as a JSON Number.  This change takes numbers in the credential
    payload into account, converting them to Strings so that they will pass
    through the properties system properly. There's no real downside to this
    as Spring will coerce them back into Numbers if needed by an application.
    
    Fixes gh-2707

--- spring-boot/src/test/java/org/springframework/boot/cloudfoundry/VcapApplicationListenerTests.java
+++ spring-boot/src/test/java/org/springframework/boot/cloudfoundry/VcapApplicationListenerTests.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2012-2014 the original author or authors.
+ * Copyright 2012-2015 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -75,6 +75,10 @@ public class VcapApplicationListenerTests {
 		this.initializer.onApplicationEvent(this.event);
 		assertEquals("mysql",
 				this.context.getEnvironment().getProperty("vcap.services.mysql.name"));
+		assertEquals(
+				"3306",
+				this.context.getEnvironment().getProperty(
+						"vcap.services.mysql.credentials.port"));
 	}
 
 	@Test
@@ -86,5 +90,9 @@ public class VcapApplicationListenerTests {
 		this.initializer.onApplicationEvent(this.event);
 		assertEquals("mysql",
 				this.context.getEnvironment().getProperty("vcap.services.mysql.name"));
+		assertEquals(
+				"3306",
+				this.context.getEnvironment().getProperty(
+						"vcap.services.mysql.credentials.port"));
 	}
 }
