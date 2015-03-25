6f8cc1d07dea6d22735df617919377c4593c6248
Etienne Neveu <etienne@instaply.com>
2015-03-25 21:02:59 +0100

    Configure QueryDsl.

--- spring-boot-samples/spring-boot-sample-data-jpa/src/main/java/sample/data/jpa/SampleDataJpaApplication.java
+++ spring-boot-samples/spring-boot-sample-data-jpa/src/main/java/sample/data/jpa/SampleDataJpaApplication.java
@@ -16,8 +16,16 @@
 
 package sample.data.jpa;
 
+import com.mysema.query.jpa.JPQLQueryFactory;
+import com.mysema.query.jpa.impl.JPAProvider;
+import com.mysema.query.jpa.impl.JPAQueryFactory;
+
+import javax.inject.Provider;
+import javax.persistence.EntityManager;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class SampleDataJpaApplication {
@@ -26,4 +34,8 @@ public class SampleDataJpaApplication {
     SpringApplication.run(SampleDataJpaApplication.class, args);
   }
 
+  @Bean
+  public JPQLQueryFactory queryFactoryForQueryDsl(Provider<EntityManager> entityManager) {
+    return new JPAQueryFactory(JPAProvider.getTemplates(entityManager.get()), entityManager);
+  }
 }
