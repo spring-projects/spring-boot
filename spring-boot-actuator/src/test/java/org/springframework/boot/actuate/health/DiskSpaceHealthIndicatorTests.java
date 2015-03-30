ee3521b6a268ef11420277656ee9eafd70e0c0cb
Andy Wilkinson <awilkinson@pivotal.io>
2015-03-30 13:40:16 +0100

    Test that total space is included in disk health information
    Closes gh-2705

--- spring-boot-actuator/src/test/java/org/springframework/boot/actuate/health/DiskSpaceHealthIndicatorTests.java
+++ spring-boot-actuator/src/test/java/org/springframework/boot/actuate/health/DiskSpaceHealthIndicatorTests.java
@@ -58,19 +58,23 @@ public class DiskSpaceHealthIndicatorTests {
 	@Test
 	public void diskSpaceIsUp() throws Exception {
 		given(this.fileMock.getFreeSpace()).willReturn(THRESHOLD_BYTES + 10);
+		given(this.fileMock.getTotalSpace()).willReturn(THRESHOLD_BYTES * 10);
 		Health health = this.healthIndicator.health();
 		assertEquals(Status.UP, health.getStatus());
 		assertEquals(THRESHOLD_BYTES, health.getDetails().get("threshold"));
 		assertEquals(THRESHOLD_BYTES + 10, health.getDetails().get("free"));
+		assertEquals(THRESHOLD_BYTES * 10, health.getDetails().get("total"));
 	}
 
 	@Test
 	public void diskSpaceIsDown() throws Exception {
 		given(this.fileMock.getFreeSpace()).willReturn(THRESHOLD_BYTES - 10);
+		given(this.fileMock.getTotalSpace()).willReturn(THRESHOLD_BYTES * 10);
 		Health health = this.healthIndicator.health();
 		assertEquals(Status.DOWN, health.getStatus());
 		assertEquals(THRESHOLD_BYTES, health.getDetails().get("threshold"));
 		assertEquals(THRESHOLD_BYTES - 10, health.getDetails().get("free"));
+		assertEquals(THRESHOLD_BYTES * 10, health.getDetails().get("total"));
 	}
 
 	private DiskSpaceHealthIndicatorProperties createProperties(File path, long threshold) {
