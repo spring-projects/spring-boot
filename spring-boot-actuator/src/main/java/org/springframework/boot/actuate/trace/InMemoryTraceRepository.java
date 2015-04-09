e94de70358b179126f9dc25d8dc15b92e390f1ff
Phillip Webb <pwebb@pivotal.io>
2015-04-09 10:42:53 -0700

    Polish

--- spring-boot-actuator/src/main/java/org/springframework/boot/actuate/trace/InMemoryTraceRepository.java
+++ spring-boot-actuator/src/main/java/org/springframework/boot/actuate/trace/InMemoryTraceRepository.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2012-2013 the original author or authors.
+ * Copyright 2012-2015 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -37,7 +37,6 @@ public class InMemoryTraceRepository implements TraceRepository {
 
 	/**
 	 * Flag to say that the repository lists traces in reverse order.
-	 *
 	 * @param reverse flag value (default true)
 	 */
 	public void setReverse(boolean reverse) {
