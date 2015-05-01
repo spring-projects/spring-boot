58ba302bbcf178fa63e646a47613ad350b667012
Dave Syer <dsyer@pivotal.io>
2015-05-01 22:04:10 +0100

    Add sample for Redis metric exporter

---
+++ spring-boot-samples/spring-boot-sample-metrics-redis/src/main/java/sample/metrics/redis/SampleRedisExportApplication.java
@@ -0,0 +1,45 @@
+/*
+ * Copyright 2012-2015 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package sample.metrics.redis;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.actuate.metrics.repository.redis.RedisMetricRepository;
+import org.springframework.boot.actuate.metrics.writer.MetricWriter;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+
+@SpringBootApplication
+@EnableConfigurationProperties(ExportProperties.class)
+public class SampleRedisExportApplication {
+
+	@Autowired
+	private ExportProperties export;
+
+	public static void main(String[] args) throws Exception {
+		SpringApplication.run(SampleRedisExportApplication.class, args);
+	}
+
+	@Bean
+	public MetricWriter redisMetricWriter(RedisConnectionFactory connectionFactory) {
+		return new RedisMetricRepository(connectionFactory, this.export.getPrefix(),
+				this.export.getKey());
+	}
+
+}
