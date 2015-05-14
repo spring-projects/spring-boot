b45a2bcfc0d19b7d70f73e1bdefca11f659ef7b9
Andy Wilkinson <awilkinson@pivotal.io>
2015-05-14 19:43:57 +0100

    Update javadoc following removal of deprecated APIs
    See gh-2697

--- spring-boot/src/main/java/org/springframework/boot/json/JsonParserFactory.java
+++ spring-boot/src/main/java/org/springframework/boot/json/JsonParserFactory.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2012-2014 the original author or authors.
+ * Copyright 2012-2015 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -33,7 +33,7 @@ public abstract class JsonParserFactory {
 	/**
 	 * Static factory for the "best" JSON parser available on the classpath. Tries Jackson
 	 * 2, then JSON (from eclipse), Simple JSON, Gson, Snake YAML, and then falls back to
-	 * the {@link SimpleJsonParser}.
+	 * the {@link BasicJsonParser}.
 	 *
 	 * @return a {@link JsonParser}
 	 */
