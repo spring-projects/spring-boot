5ab4b49534b57e8b57569cf225293a1c3854dd93
Phillip Webb <pwebb@pivotal.io>
2015-06-04 01:39:21 -0700

    Fix broken Javadoc

--- spring-boot-autoconfigure/src/main/java/org/springframework/boot/autoconfigure/security/Http401AuthenticationEntryPoint.java
+++ spring-boot-autoconfigure/src/main/java/org/springframework/boot/autoconfigure/security/Http401AuthenticationEntryPoint.java
@@ -27,9 +27,9 @@ import org.springframework.security.web.AuthenticationEntryPoint;
 import org.springframework.security.web.authentication.www.BasicAuthenticationEntryPoint;
 
 /**
- * AuthenticationEntryPoint that sends a 401 and Parameterized by the value of the {@coe
- * WWW-Authenticate} header. Like the {@link BasicAuthenticationEntryPoint} but more
- * flexible.
+ * AuthenticationEntryPoint that sends a 401 and Parameterized by the value of the
+ * {@code WWW-Authenticate} header. Like the {@link BasicAuthenticationEntryPoint} but
+ * more flexible.
  *
  * @author Dave Syer
  * @since 1.3.0
