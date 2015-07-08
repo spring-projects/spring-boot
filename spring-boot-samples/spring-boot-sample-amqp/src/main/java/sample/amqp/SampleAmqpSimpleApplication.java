34f69b238fae7afdf2727ba86844c167df61b134
Lucas Saldanha <lucascrsaldanha@gmail.com>
2015-07-08 19:49:44 -0300

    Update spring-boot-sample-amqp to use @EnableRabbit and @RabbitListener
    Recently updated the documentation to include details about using AMQP with
    Spring Boot. I have updated the spring-boot-sample-amqp to match the docs.
    
    Fixes #3451

--- spring-boot-samples/spring-boot-sample-amqp/src/main/java/sample/amqp/SampleAmqpSimpleApplication.java
+++ spring-boot-samples/spring-boot-sample-amqp/src/main/java/sample/amqp/SampleAmqpSimpleApplication.java
@@ -16,21 +16,20 @@
 
 package sample.amqp;
 
-import java.util.Date;
-
-import org.springframework.amqp.core.Queue;
-import org.springframework.amqp.rabbit.annotation.RabbitHandler;
-import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.amqp.rabbit.annotation.EnableRabbit;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
-import org.springframework.messaging.handler.annotation.Payload;
-import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.annotation.ScheduledAnnotationBeanPostProcessor;
 
 @SpringBootApplication
-@RabbitListener(queues = "foo")
-@EnableScheduling
+@EnableRabbit
 public class SampleAmqpSimpleApplication {
+
+	@Bean
+	public ScheduledAnnotationBeanPostProcessor scheduledAnnotationBeanPostProcessor() {
+		return new ScheduledAnnotationBeanPostProcessor();
+	}
 
 	@Bean
 	public Sender mySender() {
@@ -38,13 +37,8 @@ public class SampleAmqpSimpleApplication {
 	}
 
 	@Bean
-	public Queue fooQueue() {
-		return new Queue("foo");
-	}
-
-	@RabbitHandler
-	public void process(@Payload String foo) {
-		System.out.println(new Date() + ": " + foo);
+	public Listener myListener() {
+		return new Listener();
 	}
 
 	public static void main(String[] args) throws Exception {
