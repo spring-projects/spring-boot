spring-projects/spring-boot

commit 3de25164527f5011df4ea2c889794a7c72bcf5ce

Tweak LoggingApplicationListener order
Provide additional space between the ConfigFileApplicationListener order
and the LoggingApplicationListener order.

Fixes gh-2543

spring-boot/src/main/java/org/springframework/boot/logging/LoggingApplicationListener.java
@@ -109,7 +109,7 @@ public class LoggingApplicationListener implements GenericApplicationListener {
 
 	private LoggingSystem loggingSystem;
 
-	private int order = Ordered.HIGHEST_PRECEDENCE + 11;
+	private int order = Ordered.HIGHEST_PRECEDENCE + 20;
 
 	private boolean parseArgs = true;
 

commit 8280004f800d2d3386bd8859a13dd6bc8d0c1c12

Fix ClasspathLoggingApplicationListener order
Commit 3de25164 inadvertently caused early debug logging from the
ClasspathLoggingApplicationListener. We now set its order relative
to the LoggingApplicationListener.

See gh-2543

spring-boot/src/main/java/org/springframework/boot/logging/ClasspathLoggingApplicationListener.java
@@ -26,7 +26,6 @@ import org.springframework.boot.context.event.ApplicationStartedEvent;
 import org.springframework.context.ApplicationEvent;
 import org.springframework.context.event.GenericApplicationListener;
 import org.springframework.context.event.SmartApplicationListener;
-import org.springframework.core.Ordered;
 import org.springframework.core.ResolvableType;
 
 /**
@@ -40,7 +39,7 @@ import org.springframework.core.ResolvableType;
 public final class ClasspathLoggingApplicationListener implements
 		GenericApplicationListener {
 
-	private static final int ORDER = Ordered.HIGHEST_PRECEDENCE + 12;
+	private static final int ORDER = LoggingApplicationListener.DEFAULT_ORDER + 1;
 
 	private final Log logger = LogFactory.getLog(getClass());
 

spring-boot/src/main/java/org/springframework/boot/logging/LoggingApplicationListener.java
@@ -65,6 +65,11 @@ import org.springframework.util.StringUtils;
 public class LoggingApplicationListener implements GenericApplicationListener {
 
 	/**
+	 * The default order for the LoggingApplicationListener.
+	 */
+	public static final int DEFAULT_ORDER = Ordered.HIGHEST_PRECEDENCE + 20;
+
+	/**
 	 * The name of the Spring property that contains a reference to the logging
 	 * configuration to load.
 	 */
@@ -109,7 +114,7 @@ public class LoggingApplicationListener implements GenericApplicationListener {
 
 	private LoggingSystem loggingSystem;
 
-	private int order = Ordered.HIGHEST_PRECEDENCE + 20;
+	private int order = DEFAULT_ORDER;
 
 	private boolean parseArgs = true;
 