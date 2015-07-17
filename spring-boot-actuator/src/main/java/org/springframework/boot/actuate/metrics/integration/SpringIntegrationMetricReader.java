0f528f072bc3def7f55142021f4b903fe0e6949d
izeye <izeye@naver.com>
2015-07-17 10:29:33 +0900

    Make metric type more sensible.

--- spring-boot-actuator/src/main/java/org/springframework/boot/actuate/metrics/integration/SpringIntegrationMetricReader.java
+++ spring-boot-actuator/src/main/java/org/springframework/boot/actuate/metrics/integration/SpringIntegrationMetricReader.java
@@ -55,8 +55,8 @@ public class SpringIntegrationMetricReader implements MetricReader {
 			String prefix = "integration.channel." + name;
 			metrics.addAll(getStatistics(prefix + ".errorRate",
 					exporter.getChannelErrorRate(name)));
-			metrics.add(new Metric<Integer>(prefix + ".sendCount", exporter
-					.getChannelSendCount(name)));
+			metrics.add(new Metric<Long>(prefix + ".sendCount", exporter
+					.getChannelSendCountLong(name)));
 			metrics.addAll(getStatistics(prefix + ".sendRate",
 					exporter.getChannelSendRate(name)));
 			metrics.add(new Metric<Long>(prefix + ".receiveCount", exporter
@@ -66,8 +66,8 @@ public class SpringIntegrationMetricReader implements MetricReader {
 			metrics.addAll(getStatistics("integration.handler." + name + ".duration",
 					exporter.getHandlerDuration(name)));
 		}
-		metrics.add(new Metric<Long>("integration.activeHandlerCount", exporter
-				.getActiveHandlerCountLong()));
+		metrics.add(new Metric<Integer>("integration.activeHandlerCount", exporter
+				.getActiveHandlerCount()));
 		metrics.add(new Metric<Integer>("integration.handlerCount", exporter
 				.getHandlerCount()));
 		metrics.add(new Metric<Integer>("integration.channelCount", exporter
