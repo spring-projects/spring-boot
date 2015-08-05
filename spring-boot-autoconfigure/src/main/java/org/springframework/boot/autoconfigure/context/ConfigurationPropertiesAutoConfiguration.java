2b15ea65e2c5e82027de7f56d57eff14cb870b7d
Eric Bottard <eric.bottard+github@gmail.com>
2015-08-05 13:54:31 +0200

    Polish Javadoc
    Closes gh-3667

--- spring-boot-autoconfigure/src/main/java/org/springframework/boot/autoconfigure/context/ConfigurationPropertiesAutoConfiguration.java
+++ spring-boot-autoconfigure/src/main/java/org/springframework/boot/autoconfigure/context/ConfigurationPropertiesAutoConfiguration.java
@@ -23,7 +23,7 @@ import org.springframework.context.annotation.Configuration;
 
 /**
  * {@link EnableAutoConfiguration Auto-configuration} for {@link ConfigurationProperties}
- * beans. Automatically any bean annotated with {@code @ConfigurationProperties}.
+ * beans. Automatically binds and validates any bean annotated with {@code @ConfigurationProperties}.
  *
  * @author Stephane Nicoll
  * @since 1.3.0
