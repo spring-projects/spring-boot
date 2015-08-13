53b6420e8be074aedce71b320b9b45a6c435f10c
alexVengrovsk <ovengrovsk@i.ua>
2015-08-13 15:53:20 +0300

    Update SampleController.java
    I think, it will be good decision to change @Controller annotation to @RestController annotation. It will make possible to delete @ResponseBody annotation.

--- spring-boot-samples/spring-boot-sample-data-jpa/src/main/java/sample/data/jpa/web/SampleController.java
+++ spring-boot-samples/spring-boot-sample-data-jpa/src/main/java/sample/data/jpa/web/SampleController.java
@@ -17,21 +17,20 @@
 package sample.data.jpa.web;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import sample.data.jpa.service.CityService;
 
-@Controller
+@RestController 
 public class SampleController {
 
 	@Autowired
 	private CityService cityService;
 
 	@RequestMapping("/")
-	@ResponseBody
 	@Transactional(readOnly = true)
 	public String helloWorld() {
 		return this.cityService.getCity("Bath", "UK").getName();
