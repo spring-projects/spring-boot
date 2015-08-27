bb92387928afbab325abd5e71e2ae5e2c6723643
Andy Wilkinson <awilkinson@pivotal.io>
2015-08-27 17:42:02 +0100

    Polishing

--- spring-boot-autoconfigure/src/main/java/org/springframework/boot/autoconfigure/security/SecurityDataConfiguration.java
+++ spring-boot-autoconfigure/src/main/java/org/springframework/boot/autoconfigure/security/SecurityDataConfiguration.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2002-2015 the original author or authors.
+ * Copyright 2012-2015 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -29,7 +29,8 @@ import org.springframework.security.data.repository.query.SecurityEvaluationCont
  * @since 1.3
  */
 @Configuration
-@ConditionalOnClass({SecurityEvaluationContextExtension.class, EvaluationContextExtensionSupport.class})
+@ConditionalOnClass({ SecurityEvaluationContextExtension.class,
+		EvaluationContextExtensionSupport.class })
 public class SecurityDataConfiguration {
 
 	@ConditionalOnMissingBean(SecurityEvaluationContextExtension.class)
@@ -37,4 +38,5 @@ public class SecurityDataConfiguration {
 	public SecurityEvaluationContextExtension securityEvaluationContextExtension() {
 		return new SecurityEvaluationContextExtension();
 	}
+
 }
