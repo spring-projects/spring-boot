4ee3c8d196f9f363998e84712b2f24299e7de943
mrumpf <michael@rumpfonline.de>
2015-08-29 12:39:35 +0200

    Revert "Added some Javadoc according to the contribution guidelines"
    This reverts commit 83eae9a17892602e6c6e66f1a69fc96b7a950bdb.

--- spring-boot-tools/spring-boot-loader/src/main/java/org/springframework/boot/loader/Debug.java
+++ spring-boot-tools/spring-boot-loader/src/main/java/org/springframework/boot/loader/Debug.java
@@ -1,21 +1,10 @@
 package org.springframework.boot.loader;
 
-/**
- * This is a helper class for determining whether the debug mode is enabled or not.
- *
- * @author Michael Rumpf
- *
- */
 public final class Debug {
 	private Debug() {
 		// do not allow instances
 	}
 
-	/**
-	 * Returns true if debug mode is enabled and false otherwise.
-	 * 
-	 * @return true or false
-	 */
 	public static boolean isEnabled() {
 		String debug = System.getProperty("debug");
 		if (debug != null && !"false".equals(debug)) {
