bd942a35fbf90739b19b5d1e292bed63cfc0a865
Stephane Nicoll <snicoll@pivotal.io>
2015-09-01 15:11:01 +0200

    Fix upgrade to Spring Data Gosling RELEASE
    Closes gh-3860

--- spring-boot-autoconfigure/src/main/java/org/springframework/boot/autoconfigure/hateoas/HypermediaAutoConfiguration.java
+++ spring-boot-autoconfigure/src/main/java/org/springframework/boot/autoconfigure/hateoas/HypermediaAutoConfiguration.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2012-2014 the original author or authors.
+ * Copyright 2012-2015 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -101,7 +101,7 @@ public class HypermediaAutoConfiguration {
 				objectMapper.registerModule(new Jackson2HalModule());
 				Jackson2HalModule.HalHandlerInstantiator instantiator = new Jackson2HalModule.HalHandlerInstantiator(
 						HalObjectMapperConfiguration.this.relProvider,
-						HalObjectMapperConfiguration.this.curieProvider);
+						HalObjectMapperConfiguration.this.curieProvider, null);
 				objectMapper.setHandlerInstantiator(instantiator);
 			}
 
