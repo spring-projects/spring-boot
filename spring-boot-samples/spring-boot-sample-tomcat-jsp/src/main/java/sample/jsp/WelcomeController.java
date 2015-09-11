5f250ebbc16ba32d026060565860cc1c7bf25820
Phillip Webb <pwebb@pivotal.io>
2015-09-11 12:25:48 -0700

    Add exception endpoints to tomcat-jsp sample
    Update `spring-boot-sample-tomcat-jsp` to include endpoints that trigger
    exceptions. Primarily to aid testing of the ErrorPageFilter.
    
    See gh-2745

--- spring-boot-samples/spring-boot-sample-tomcat-jsp/src/main/java/sample/jsp/WelcomeController.java
+++ spring-boot-samples/spring-boot-sample-tomcat-jsp/src/main/java/sample/jsp/WelcomeController.java
@@ -20,8 +20,12 @@ import java.util.Date;
 import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
 
 @Controller
 public class WelcomeController {
@@ -34,6 +38,22 @@ public class WelcomeController {
 		model.put("time", new Date());
 		model.put("message", this.message);
 		return "welcome";
+	}
+
+	@RequestMapping("/fail")
+	public String fail() {
+		throw new MyException("Oh dear!");
+	}
+
+	@RequestMapping("/fail2")
+	public String fail2() {
+		throw new IllegalStateException();
+	}
+
+	@ExceptionHandler(MyException.class)
+	@ResponseStatus(HttpStatus.BAD_REQUEST)
+	public @ResponseBody MyRestResponse handleMyRuntimeException(MyException exception) {
+		return new MyRestResponse("Some data I want to send back to the client.");
 	}
 
 }
