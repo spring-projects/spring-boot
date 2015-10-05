ddbbd377181affe9bd7f5410f15b9fe9d804364d
Dave Syer <dsyer@pivotal.io>
2015-10-05 09:54:58 +0100

    Change order of bom transformation to allow others earlier

--- spring-boot-cli/src/main/java/org/springframework/boot/cli/compiler/DependencyManagementBomTransformation.java
+++ spring-boot-cli/src/main/java/org/springframework/boot/cli/compiler/DependencyManagementBomTransformation.java
@@ -66,7 +66,7 @@ public class DependencyManagementBomTransformation extends AnnotatedNodeASTTrans
 	/**
 	 * The order of the transformation.
 	 */
-	public static final int ORDER = Ordered.HIGHEST_PRECEDENCE;
+	public static final int ORDER = Ordered.HIGHEST_PRECEDENCE + 100;
 
 	private static final Set<String> DEPENDENCY_MANAGEMENT_BOM_ANNOTATION_NAMES = Collections
 			.unmodifiableSet(new HashSet<String>(Arrays.asList(
