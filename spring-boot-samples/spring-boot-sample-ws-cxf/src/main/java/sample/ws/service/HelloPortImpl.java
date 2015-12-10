fa92582c5c6a300d65bc1817f85cf35c3ee75f7a
rajakolli <rajadilipkolli@gmail.com>
2015-12-10 13:18:49 +0530

    Initial Project SetUp

---
+++ spring-boot-samples/spring-boot-sample-ws-cxf/src/main/java/sample/ws/service/HelloPortImpl.java
@@ -0,0 +1,46 @@
+/*
+ * Copyright 2012-2015 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package sample.ws.service;
+
+import java.util.logging.Logger;
+
+import javax.jws.WebService;
+
+/**
+ * CXF JAX webservice implemntation example.
+ *
+ * @author Elan Thangamani
+ */
+
+@WebService(serviceName = "HelloService", portName = "HelloPort", targetNamespace = "http://service.ws.sample/", endpointInterface = "sample.ws.service.Hello")
+public class HelloPortImpl implements Hello {
+
+	private static final Logger LOG = Logger.getLogger(HelloPortImpl.class.getName());
+
+	public String sayHello(java.lang.String myname) {
+		LOG.info("Executing operation sayHello" + myname);
+		try {
+			return "Welcome to CXF Spring boot " + myname + "!!!";
+
+		}
+		catch (Exception ex) {
+			ex.printStackTrace();
+			throw new RuntimeException(ex);
+		}
+	}
+
+}
