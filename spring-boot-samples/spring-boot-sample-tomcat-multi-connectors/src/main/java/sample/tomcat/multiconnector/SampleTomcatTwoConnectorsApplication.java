288aa962c4c182ada49cd4a3a88fed8397a56c43
David Morley <tdxm9@unigroupinc.com>
2015-12-13 09:04:50 -0600

    Rename method to clarify which connector is being created
    See gh-4770

--- spring-boot-samples/spring-boot-sample-tomcat-multi-connectors/src/main/java/sample/tomcat/multiconnector/SampleTomcatTwoConnectorsApplication.java
+++ spring-boot-samples/spring-boot-sample-tomcat-multi-connectors/src/main/java/sample/tomcat/multiconnector/SampleTomcatTwoConnectorsApplication.java
@@ -42,11 +42,11 @@ public class SampleTomcatTwoConnectorsApplication {
 	@Bean
 	public EmbeddedServletContainerFactory servletContainer() {
 		TomcatEmbeddedServletContainerFactory tomcat = new TomcatEmbeddedServletContainerFactory();
-		tomcat.addAdditionalTomcatConnectors(createSslConnector());
+		tomcat.addAdditionalTomcatConnectors(createNonSslConnector());
 		return tomcat;
 	}
 
-	private Connector createSslConnector() {
+	private Connector createNonSslConnector() {
 		Connector connector = new Connector("org.apache.coyote.http11.Http11NioProtocol");
 		connector.setPort(port());
 		return connector;
