221cb4fd360c8d6682e8c5e8a1b3f2b944b23280
Eddú Meléndez <eddu.melendez@gmail.com>
2016-01-08 21:01:34 -0500

    Remove code deprecated
    See gh-4905

--- spring-boot/src/main/java/org/springframework/boot/SpringApplication.java
+++ spring-boot/src/main/java/org/springframework/boot/SpringApplication.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2012-2015 the original author or authors.
+ * Copyright 2012-2016 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -771,7 +771,6 @@ public class SpringApplication {
 	 */
 	protected void afterRefresh(ConfigurableApplicationContext context,
 			ApplicationArguments args) {
-		afterRefresh(context, args.getSourceArgs());
 		callRunners(context, args);
 	}
 
@@ -806,17 +805,6 @@ public class SpringApplication {
 		catch (Exception ex) {
 			throw new IllegalStateException("Failed to execute CommandLineRunner", ex);
 		}
-	}
-
-	/**
-	 * Called after the context has been refreshed.
-	 * @param context the application context
-	 * @param args the application arguments
-	 * @deprecated in 1.3 in favor of
-	 * {@link #afterRefresh(ConfigurableApplicationContext, ApplicationArguments)}
-	 */
-	@Deprecated
-	protected void afterRefresh(ConfigurableApplicationContext context, String[] args) {
 	}
 
 	private void handleRunFailure(ConfigurableApplicationContext context,
