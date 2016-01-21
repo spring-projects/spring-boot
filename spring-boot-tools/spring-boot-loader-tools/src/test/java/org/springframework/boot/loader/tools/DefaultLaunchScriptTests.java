spring-projects/spring-boot

commit 81a47639407ce5cdf2798c34f7122690c9739b38

Address problems in launch.script reported by Shellcheck 0.4.1
Closes gh-4653

spring-boot-tools/spring-boot-loader-tools/src/main/resources/org/springframework/boot/loader/tools/launch.script
@@ -24,24 +24,26 @@
 
 # Initialize variables that cannot be provided by a .conf file
 WORKING_DIR="$(pwd)"
+# shellcheck disable=SC2153
 [[ -n "$JARFILE" ]] && jarfile="$JARFILE"
 [[ -n "$APP_NAME" ]] && identity="$APP_NAME"
 
 # Follow symlinks to find the real jar and detect init.d script
-cd $(dirname "$0")
+cd "$(dirname "$0")" || exit
 [[ -z "$jarfile" ]] && jarfile=$(pwd)/$(basename "$0")
 while [[ -L "$jarfile" ]]; do
-  [[ "$jarfile" =~ "init.d" ]] && init_script=$(basename "$jarfile")
+  [[ "$jarfile" =~ init\.d ]] && init_script=$(basename "$jarfile")
   jarfile=$(readlink "$jarfile")
-  cd $(dirname "$jarfile")
+  cd "$(dirname "$jarfile")" || exit
   jarfile=$(pwd)/$(basename "$jarfile")
 done
-jarfolder=$(dirname "$jarfile")
-cd "$WORKING_DIR"
+jarfolder="$(dirname "$jarfile")"
+cd "$WORKING_DIR" || exit
 
 # Source any config file
-configfile=$(basename "${jarfile%.*}.conf")
-[[ -r ${jarfolder}/${configfile} ]] && source ${jarfolder}/${configfile}
+configfile="$(basename "${jarfile%.*}.conf")"
+# shellcheck source=/dev/null
+[[ -r "${jarfolder}/${configfile}" ]] && source "${jarfolder}/${configfile}"
 
 # Initialize PID/LOG locations if they weren't provided by the config file
 [[ -z "$PID_FOLDER" ]] && PID_FOLDER="/var/run"
@@ -66,9 +68,9 @@ fi
 
 
 # ANSI Colors
-echoRed() { echo $'\e[0;31m'$1$'\e[0m'; }
-echoGreen() { echo $'\e[0;32m'$1$'\e[0m'; }
-echoYellow() { echo $'\e[0;33m'$1$'\e[0m'; }
+echoRed() { echo $'\e[0;31m'"$1"$'\e[0m'; }
+echoGreen() { echo $'\e[0;32m'"$1"$'\e[0m'; }
+echoYellow() { echo $'\e[0;33m'"$1"$'\e[0m'; }
 
 # Utility functions
 checkPermissions() {
@@ -77,7 +79,7 @@ checkPermissions() {
 }
 
 isRunning() {
-  ps -p $1 &> /dev/null
+  ps -p "$1" &> /dev/null
 }
 
 await_file() {
@@ -108,12 +110,13 @@ pid_file="$PID_FOLDER/${identity}.pid"
 log_file="$LOG_FOLDER/$LOG_FILENAME"
 
 # Determine the user to run as if we are root
+# shellcheck disable=SC2012
 [[ $(id -u) == "0" ]] && run_user=$(ls -ld "$jarfile" | awk '{print $3}')
 
 # Find Java
 if [[ -n "$JAVA_HOME" ]] && [[ -x "$JAVA_HOME/bin/java" ]]; then
     javaexe="$JAVA_HOME/bin/java"
-elif type -p java 2>&1> /dev/null; then
+elif type -p java > /dev/null 2>&1; then
     javaexe=$(type -p java)
 elif [[ -x "/usr/bin/java" ]];  then
     javaexe="/usr/bin/java"
@@ -123,15 +126,15 @@ else
 fi
 
 # Build actual command to execute
-command="$javaexe -Dsun.misc.URLClassPath.disableJarChecking=true $JAVA_OPTS -jar $jarfile $RUN_ARGS $@"
+command="$javaexe -Dsun.misc.URLClassPath.disableJarChecking=true $JAVA_OPTS -jar $jarfile $RUN_ARGS $*"
 
 # Action functions
 start() {
   if [[ -f "$pid_file" ]]; then
     pid=$(cat "$pid_file")
-    isRunning $pid && { echoYellow "Already running [$pid]"; return 0; }
+    isRunning "$pid" && { echoYellow "Already running [$pid]"; return 0; }
   fi
-  do_start
+  do_start "$@"
 }
 
 do_start() {
@@ -143,21 +146,21 @@ do_start() {
     chown "$run_user" "$PID_FOLDER"
     chown "$run_user" "$pid_file"
     chown "$run_user" "$log_file"
-    if [ {{useStartStopDaemon:true}} = true ] && which start-stop-daemon >/dev/null; then
+    if [ "${useStartStopDaemon:-true}" = true ] && which start-stop-daemon >/dev/null; then
       start-stop-daemon --start --quiet \
-        --chuid $run_user \
-        --name $identity \
-        --make-pidfile --pidfile $pid_file \
+        --chuid "$run_user" \
+        --name "$identity" \
+        --make-pidfile --pidfile "$pid_file" \
         --background --no-close \
-        --startas $javaexe \
+        --startas "$javaexe" \
         --chdir "$working_dir" \
         -- \
-        -Dsun.misc.URLClassPath.disableJarChecking=true $JAVA_OPTS \
-        -jar $jarfile $RUN_ARGS "$@" \
-        > $log_file 2>&1
-      await_file $pid_file
+        -Dsun.misc.URLClassPath.disableJarChecking=true "${JAVA_OPTS[@]}" \
+        -jar "$jarfile" "${RUN_ARGS[@]}" "$@" \
+        > "$log_file" 2>&1
+      await_file "$pid_file"
     else
-      su -s /bin/sh -c "$command &> \"$log_file\" & echo \$!" $run_user > "$pid_file"
+      su -s /bin/sh -c "$command > \"$log_file\" 2>&1 & echo \$!" "$run_user" > "$pid_file"
     fi
     pid=$(cat "$pid_file")
   else
@@ -174,14 +177,14 @@ do_start() {
 stop() {
   [[ -f $pid_file ]] || { echoYellow "Not running (pidfile not found)"; return 0; }
   pid=$(cat "$pid_file")
-  isRunning $pid || { echoYellow "Not running (process ${pid}). Removing stale pid file."; rm -f "$pid_file"; return 0; }
-  do_stop $pid $pid_file
+  isRunning "$pid" || { echoYellow "Not running (process ${pid}). Removing stale pid file."; rm -f "$pid_file"; return 0; }
+  do_stop "$pid" "$pid_file"
 }
 
 do_stop() {
-  kill $1 &> /dev/null || { echoRed "Unable to kill process $1"; return 1; }
-  for i in $(seq 1 60); do
-    isRunning $1 || { echoGreen "Stopped [$1]"; rm -f $2; return 0; }
+  kill "$1" &> /dev/null || { echoRed "Unable to kill process $1"; return 1; }
+  for _ in $(seq 1 60); do
+    isRunning "$1" || { echoGreen "Stopped [$1]"; rm -f "$2"; return 0; }
     sleep 1
   done
   echoRed "Unable to kill process $1";
@@ -196,25 +199,25 @@ force_reload() {
   [[ -f $pid_file ]] || { echoRed "Not running (pidfile not found)"; return 7; }
   pid=$(cat "$pid_file")
   rm -f "$pid_file"
-  isRunning $pid || { echoRed "Not running (process ${pid} not found)"; return 7; }
-  do_stop $pid $pid_file
+  isRunning "$pid" || { echoRed "Not running (process ${pid} not found)"; return 7; }
+  do_stop "$pid" "$pid_file"
   do_start
 }
 
 status() {
-  [[ -f $pid_file ]] || { echoRed "Not running"; return 3; }
+  [[ -f "$pid_file" ]] || { echoRed "Not running"; return 3; }
   pid=$(cat "$pid_file")
-  isRunning $pid || { echoRed "Not running (process ${pid} not found)"; return 1; }
+  isRunning "$pid" || { echoRed "Not running (process ${pid} not found)"; return 1; }
   echoGreen "Running [$pid]"
   return 0
 }
 
 run() {
-  pushd $(dirname "$jarfile") > /dev/null
-  exec $command
+  pushd "$(dirname "$jarfile")" > /dev/null
+  $command
   result=$?
   popd
-  return $result
+  return "$result"
 }
 
 # Call the appropriate action function

commit 854cacdb4fb5f1d4d53bfac22848960ab75e2af3

Fix useStartStopDaemon in launch script and allow config via conf file
Commit 5a1ee6eb added support for disabling use of start-stop-daemon
via a placeholder in the default launch script. Unfortunately, that
placeholder was subsequently broken in 81a47639.

This commit reinstates the placeholder and adds tests to verify that all
of the placeholders in the launch script can be replaced and that they
have the required default values. Furthermore, it also allows the use of
start-stop-daemon to be configured via USE_START_STOP_DAEMON in an
app’s .conf file. This allows the configuration to be changed after the
app has been built.

Closes gh-4985

spring-boot-tools/spring-boot-loader-tools/src/main/resources/org/springframework/boot/loader/tools/launch.script
@@ -51,8 +51,9 @@ configfile="$(basename "${jarfile%.*}.conf")"
 ! [[ -x "$PID_FOLDER" ]] && PID_FOLDER="/tmp"
 ! [[ -x "$LOG_FOLDER" ]] && LOG_FOLDER="/tmp"
 
-# Setup defaults
+# Set up defaults
 [[ -z "$MODE" ]] && MODE="{{mode:auto}}" # modes are "auto", "service" or "run"
+[[ -z "$USE_START_STOP_DAEMON" ]] && USE_START_STOP_DAEMON="{{useStartStopDaemon:true}}"
 
 # Create an identity for log/pid files
 if [[ -z "$identity" ]]; then
@@ -146,7 +147,7 @@ do_start() {
     chown "$run_user" "$PID_FOLDER"
     chown "$run_user" "$pid_file"
     chown "$run_user" "$log_file"
-    if [ "${useStartStopDaemon:-true}" = true ] && type start-stop-daemon > /dev/null 2>&1; then
+    if [ $USE_START_STOP_DAEMON = true ] && type start-stop-daemon > /dev/null 2>&1; then
       arguments=(-Dsun.misc.URLClassPath.disableJarChecking=true $JAVA_OPTS -jar $jarfile $RUN_ARGS "$@")
       start-stop-daemon --start --quiet \
         --chuid "$run_user" \

spring-boot-integration-tests/spring-boot-launch-script-tests/src/test/java/org/springframework/boot/launchscript/SysVinitLaunchScriptIT.java
@@ -50,7 +50,10 @@ import org.junit.runners.Parameterized.Parameters;
 import org.springframework.boot.ansi.AnsiColor;
 
 import static org.hamcrest.Matchers.containsString;
+import static org.hamcrest.Matchers.is;
+import static org.hamcrest.Matchers.not;
 import static org.junit.Assert.assertThat;
+import static org.junit.Assume.assumeThat;
 
 /**
  * Integration tests for Spring Boot's launch script on OSs that use SysVinit.
@@ -186,6 +189,13 @@ public class SysVinitLaunchScriptIT {
 	@Test
 	public void launchWithMultipleJavaOpts() throws Exception {
 		doLaunch("launch-with-multiple-java-opts.sh");
+	}
+
+	@Test
+	public void launchWithUseOfStartStopDaemonDisabled() throws Exception {
+		// CentOS doesn't have start-stop-daemon
+		assumeThat(this.os, is(not("CentOS")));
+		doLaunch("launch-with-use-of-start-stop-daemon-disabled.sh");
 	}
 
 	private void doLaunch(String script) throws Exception {

spring-boot-integration-tests/spring-boot-launch-script-tests/src/test/resources/scripts/launch-with-use-of-start-stop-daemon-disabled.sh
@@ -1,0 +1,7 @@
+source ./test-functions.sh
+chmod -x $(type -p start-stop-daemon)
+echo 'USE_START_STOP_DAEMON=false' > /spring-boot-app.conf
+install_service
+start_service
+await_app
+curl -s http://127.0.0.1:8080/

spring-boot-tools/spring-boot-loader-tools/src/test/java/org/springframework/boot/loader/tools/DefaultLaunchScriptTests.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2012-2015 the original author or authors.
+ * Copyright 2012-2016 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -17,7 +17,8 @@
 package org.springframework.boot.loader.tools;
 
 import java.io.File;
-import java.util.Properties;
+import java.util.HashMap;
+import java.util.Map;
 
 import org.junit.Rule;
 import org.junit.Test;
@@ -33,6 +34,7 @@ import static org.junit.Assert.assertThat;
  * Tests for {@link DefaultLaunchScript}.
  *
  * @author Phillip Webb
+ * @author Andy Wilkinson
  */
 public class DefaultLaunchScriptTests {
 
@@ -44,6 +46,49 @@ public class DefaultLaunchScriptTests {
 		DefaultLaunchScript script = new DefaultLaunchScript(null, null);
 		String content = new String(script.toByteArray());
 		assertThat(content, containsString("Spring Boot Startup Script"));
+	}
+
+	@Test
+	public void initInfoProvidesCanBeReplaced() throws Exception {
+		assertThatPlaceholderCanBeReplaced("initInfoProvides");
+	}
+
+	@Test
+	public void initInfoShortDescriptionCanBeReplaced() throws Exception {
+		assertThatPlaceholderCanBeReplaced("initInfoShortDescription");
+	}
+
+	@Test
+	public void initInfoDescriptionCanBeReplaced() throws Exception {
+		assertThatPlaceholderCanBeReplaced("initInfoDescription");
+	}
+
+	@Test
+	public void initInfoChkconfigCanBeReplaced() throws Exception {
+		assertThatPlaceholderCanBeReplaced("initInfoChkconfig");
+	}
+
+	@Test
+	public void modeCanBeReplaced() throws Exception {
+		assertThatPlaceholderCanBeReplaced("mode");
+	}
+
+	@Test
+	public void useStartStopDaemonCanBeReplaced() throws Exception {
+		assertThatPlaceholderCanBeReplaced("useStartStopDaemon");
+	}
+
+	@Test
+	public void defaultForUseStartStopDaemonIsTrue() throws Exception {
+		DefaultLaunchScript script = new DefaultLaunchScript(null, null);
+		String content = new String(script.toByteArray());
+		assertThat(content, containsString("USE_START_STOP_DAEMON=\"true\""));
+	}
+
+	@Test
+	public void defaultForModeIsAuto() throws Exception {
+		DefaultLaunchScript script = new DefaultLaunchScript(null, null);
+		String content = new String(script.toByteArray());
 		assertThat(content, containsString("MODE=\"auto\""));
 	}
 
@@ -60,10 +105,8 @@ public class DefaultLaunchScriptTests {
 	public void expandVariables() throws Exception {
 		File file = this.temporaryFolder.newFile();
 		FileCopyUtils.copy("h{{a}}ll{{b}}".getBytes(), file);
-		Properties properties = new Properties();
-		properties.put("a", "e");
-		properties.put("b", "o");
-		DefaultLaunchScript script = new DefaultLaunchScript(file, properties);
+		DefaultLaunchScript script = new DefaultLaunchScript(file,
+				createProperties("a:e", "b:o"));
 		String content = new String(script.toByteArray());
 		assertThat(content, equalTo("hello"));
 	}
@@ -72,10 +115,8 @@ public class DefaultLaunchScriptTests {
 	public void expandVariablesMultiLine() throws Exception {
 		File file = this.temporaryFolder.newFile();
 		FileCopyUtils.copy("h{{a}}l\nl{{b}}".getBytes(), file);
-		Properties properties = new Properties();
-		properties.put("a", "e");
-		properties.put("b", "o");
-		DefaultLaunchScript script = new DefaultLaunchScript(file, properties);
+		DefaultLaunchScript script = new DefaultLaunchScript(file,
+				createProperties("a:e", "b:o"));
 		String content = new String(script.toByteArray());
 		assertThat(content, equalTo("hel\nlo"));
 	}
@@ -93,9 +134,8 @@ public class DefaultLaunchScriptTests {
 	public void expandVariablesWithDefaultsOverride() throws Exception {
 		File file = this.temporaryFolder.newFile();
 		FileCopyUtils.copy("h{{a:e}}ll{{b:o}}".getBytes(), file);
-		Properties properties = new Properties();
-		properties.put("a", "a");
-		DefaultLaunchScript script = new DefaultLaunchScript(file, properties);
+		DefaultLaunchScript script = new DefaultLaunchScript(file,
+				createProperties("a:a"));
 		String content = new String(script.toByteArray());
 		assertThat(content, equalTo("hallo"));
 	}
@@ -107,6 +147,22 @@ public class DefaultLaunchScriptTests {
 		DefaultLaunchScript script = new DefaultLaunchScript(file, null);
 		String content = new String(script.toByteArray());
 		assertThat(content, equalTo("h{{a}}ll{{b}}"));
+	}
+
+	private void assertThatPlaceholderCanBeReplaced(String placeholder) throws Exception {
+		DefaultLaunchScript script = new DefaultLaunchScript(null,
+				createProperties(placeholder + ":__test__"));
+		String content = new String(script.toByteArray());
+		assertThat(content, containsString("__test__"));
+	}
+
+	private Map<?, ?> createProperties(String... pairs) {
+		Map<Object, Object> properties = new HashMap<Object, Object>();
+		for (String pair : pairs) {
+			String[] keyValue = pair.split(":");
+			properties.put(keyValue[0], keyValue[1]);
+		}
+		return properties;
 	}
 
 }