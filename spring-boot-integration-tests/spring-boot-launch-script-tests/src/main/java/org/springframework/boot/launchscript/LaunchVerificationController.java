64a883fa291836a432dfbc126b6e2209bc69b8b1
Johnny Lim <izeye@naver.com>
2016-01-23 09:26:50 +0900

    Polish
    Closes gh-5013

--- spring-boot-integration-tests/spring-boot-launch-script-tests/src/main/java/org/springframework/boot/launchscript/LaunchVerificationController.java
+++ spring-boot-integration-tests/spring-boot-launch-script-tests/src/main/java/org/springframework/boot/launchscript/LaunchVerificationController.java
@@ -20,7 +20,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-public class LaunchVerficationController {
+public class LaunchVerificationController {
 
 	@RequestMapping("/")
 	public String verifyLaunch() {
