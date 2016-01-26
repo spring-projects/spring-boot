a7b0ea6767e8150e52c8bb0440689685ec646677
Stephane Nicoll <snicoll@pivotal.io>
2016-01-26 14:59:38 +0100

    Polish contribution
    Closes gh-4590

--- spring-boot-autoconfigure/src/main/java/org/springframework/boot/autoconfigure/thymeleaf/ThymeleafAutoConfiguration.java
+++ spring-boot-autoconfigure/src/main/java/org/springframework/boot/autoconfigure/thymeleaf/ThymeleafAutoConfiguration.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2012-2015 the original author or authors.
+ * Copyright 2012-2016 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -195,7 +195,7 @@ public class ThymeleafAutoConfiguration {
 
 	@Configuration
 	@ConditionalOnJava(ConditionalOnJava.JavaVersion.EIGHT)
-	@ConditionalOnClass({ Java8TimeDialect.class })
+	@ConditionalOnClass(Java8TimeDialect.class)
 	protected static class ThymeleafJava8TimeDialect {
 
 		@Bean
