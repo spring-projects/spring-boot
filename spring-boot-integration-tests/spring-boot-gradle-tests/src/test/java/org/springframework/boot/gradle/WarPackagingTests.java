df64a74e607cf5387b1ef63e7c3026f4212e3ce8
Stephane Nicoll <snicoll@pivotal.io>
2016-01-28 09:38:39 +0100

    Fix broken build
    jetty-jndi is no longer shipped by default as of 2b2e21d but one test
    checking for this wasn't updated properly.

--- spring-boot-integration-tests/spring-boot-gradle-tests/src/test/java/org/springframework/boot/gradle/WarPackagingTests.java
+++ spring-boot-integration-tests/spring-boot-gradle-tests/src/test/java/org/springframework/boot/gradle/WarPackagingTests.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2012-2015 the original author or authors.
+ * Copyright 2012-2016 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -59,7 +59,7 @@ public class WarPackagingTests {
 					"asm-", "javax.websocket-api-", "asm-tree-", "asm-commons-",
 					"websocket-common-", "jetty-annotations-",
 					"javax-websocket-client-impl-", "websocket-client-",
-					"websocket-server-", "jetty-jndi-", "jetty-xml-",
+					"websocket-server-", "jetty-xml-",
 					"websocket-servlet-"));
 
 	private static final String BOOT_VERSION = Versions.getBootVersion();
