06fb2d6d3e74a1304429f533c1f46bf3f174d641
Andy Wilkinson <awilkinson@pivotal.io>
2016-02-25 20:17:32 +0000

    Upgrade to Log4j2 2.5
    Closes gh-5242

--- spring-boot/src/test/java/org/springframework/boot/logging/log4j2/Log4J2LoggingSystemTests.java
+++ spring-boot/src/test/java/org/springframework/boot/logging/log4j2/Log4J2LoggingSystemTests.java
@@ -26,7 +26,6 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.apache.logging.log4j.core.config.Configuration;
-import org.apache.logging.log4j.core.config.FileConfigurationMonitor;
 import org.hamcrest.Matcher;
 import org.hamcrest.Matchers;
 import org.junit.Before;
@@ -104,10 +103,7 @@ public class Log4J2LoggingSystemTests extends AbstractLoggingSystemTests {
 		assertThat(new File(tmpDir() + "/tmp.log").exists()).isFalse();
 		assertThat(configuration.getConfigurationSource().getFile().getAbsolutePath())
 				.contains("log4j2-nondefault.xml");
-		// we assume that "log4j2-nondefault.xml" contains the 'monitorInterval'
-		// attribute, so we check that a monitor is created
-		assertThat(configuration.getConfigurationMonitor())
-				.isInstanceOf(FileConfigurationMonitor.class);
+		assertThat(configuration.getWatchManager().getIntervalSeconds()).isEqualTo(30);
 	}
 
 	@Test(expected = IllegalStateException.class)
