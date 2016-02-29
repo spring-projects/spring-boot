a0f76eab470aeedbc447a432ad9d746d35fc1307
Vladimir Tsanev <tsachev@gmail.com>
2016-02-29 18:41:27 +0200

    Update JarURLConnection to support jar:file://… URLs
    Previously, JarURLConnection would fail when created with a URL that
    began with jar:file:// as the double-slash is not included in jarFile.getUrl().getFile().
    
    This commit updates JarURLConnection to normalise the value return from
    url.getFile() to remove a double-slash when present.
    
    Fixes gh-5287
    Closes gh-5289

--- spring-boot-tools/spring-boot-loader/src/test/java/org/springframework/boot/loader/jar/JarFileTests.java
+++ spring-boot-tools/spring-boot-loader/src/test/java/org/springframework/boot/loader/jar/JarFileTests.java
@@ -335,6 +335,22 @@ public class JarFileTests {
 	}
 
 	@Test
+	public void createNonNestedUrlFromStringCreatedUsingPath() throws Exception {
+		// gh-5287
+		JarFile.registerUrlProtocolHandler();
+		String spec = "jar:" + this.rootJarFile.toPath().toUri() + "!/2.dat";
+		URL url = new URL(spec);
+		assertThat(url.toString(), equalTo(spec));
+		InputStream inputStream = url.openStream();
+		assertThat(inputStream, notNullValue());
+		assertThat(inputStream.read(), equalTo(2));
+		JarURLConnection connection = (JarURLConnection) url.openConnection();
+		assertThat(connection.getURL().toString(), equalTo(spec));
+		assertThat(connection.getJarFileURL().toURI(), equalTo(this.rootJarFile.toURI()));
+		assertThat(connection.getEntryName(), equalTo("2.dat"));
+	}
+
+	@Test
 	public void getDirectoryInputStream() throws Exception {
 		InputStream inputStream = this.jarFile
 				.getInputStream(this.jarFile.getEntry("d/"));
