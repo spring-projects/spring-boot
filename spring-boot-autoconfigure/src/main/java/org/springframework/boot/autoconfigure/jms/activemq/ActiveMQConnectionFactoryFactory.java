2861c7651d491fb5c8e8ff802297510bb0bd2753
Phillip Webb <pwebb@pivotal.io>
2016-04-10 07:55:59 -0700

    Polish

--- spring-boot-autoconfigure/src/main/java/org/springframework/boot/autoconfigure/jms/activemq/ActiveMQConnectionFactoryFactory.java
+++ spring-boot-autoconfigure/src/main/java/org/springframework/boot/autoconfigure/jms/activemq/ActiveMQConnectionFactoryFactory.java
@@ -16,6 +16,8 @@
 
 package org.springframework.boot.autoconfigure.jms.activemq;
 
+import java.lang.reflect.InvocationTargetException;
+
 import org.apache.activemq.ActiveMQConnectionFactory;
 
 import org.springframework.boot.autoconfigure.jms.activemq.ActiveMQProperties.Packages;
@@ -56,27 +58,28 @@ class ActiveMQConnectionFactoryFactory {
 
 	private <T extends ActiveMQConnectionFactory> T doCreateConnectionFactory(
 			Class<T> factoryClass) throws Exception {
+		T factory = createConnectionFactoryInstance(factoryClass);
+		Packages packages = this.properties.getPackages();
+		if (packages.getTrustAll() != null) {
+			factory.setTrustAllPackages(packages.getTrustAll());
+		}
+		if (!packages.getTrusted().isEmpty()) {
+			factory.setTrustedPackages(packages.getTrusted());
+		}
+		return factory;
+	}
+
+	private <T extends ActiveMQConnectionFactory> T createConnectionFactoryInstance(
+			Class<T> factoryClass) throws InstantiationException, IllegalAccessException,
+					InvocationTargetException, NoSuchMethodException {
 		String brokerUrl = determineBrokerUrl();
 		String user = this.properties.getUser();
 		String password = this.properties.getPassword();
-		T activeMqConnectionFactory;
 		if (StringUtils.hasLength(user) && StringUtils.hasLength(password)) {
-			activeMqConnectionFactory = factoryClass
-					.getConstructor(String.class, String.class, String.class)
+			return factoryClass.getConstructor(String.class, String.class, String.class)
 					.newInstance(user, password, brokerUrl);
 		}
-		else {
-			activeMqConnectionFactory = factoryClass.getConstructor(String.class)
-					.newInstance(brokerUrl);
-		}
-		Packages packages = this.properties.getPackages();
-		if (packages.getTrustAll() != null) {
-			activeMqConnectionFactory.setTrustAllPackages(packages.getTrustAll());
-		}
-		if (!packages.getTrusted().isEmpty()) {
-			activeMqConnectionFactory.setTrustedPackages(packages.getTrusted());
-		}
-		return activeMqConnectionFactory;
+		return factoryClass.getConstructor(String.class).newInstance(brokerUrl);
 	}
 
 	String determineBrokerUrl() {
