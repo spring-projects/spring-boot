9210029109cf982b499e2d03523326982a816b5f
Andy Wilkinson <awilkinson@pivotal.io>
2016-04-18 16:10:26 +0100

    Record trace with response status of 500 following unhandled exception
    Previously, if the filter chain threw an unhandled exception,
    WebRequestTraceFilter would record a trace with a response status of
    200. This occurred because response.getStatus() would return 200 as
    the container had not yet caught the exception and mapped it to an
    error response.
    
    This commit updates WebRequestTraceFilter to align its behaviour with
    MetricsFilter. It now assumes that the response status will be a 500
    and only updates that to the status of the response if the call to the
    filter chain returns successfully.
    
    To avoid making a breaking change to the signature of the protected
    enhanceTrace method, an HttpServletResponseWrapper is used to include
    the correct status in the trace.
    
    Closes gh-5331

--- spring-boot-actuator/src/test/java/org/springframework/boot/actuate/trace/WebRequestTraceFilterTests.java
+++ spring-boot-actuator/src/test/java/org/springframework/boot/actuate/trace/WebRequestTraceFilterTests.java
@@ -37,8 +37,12 @@ import org.springframework.boot.autoconfigure.web.DefaultErrorAttributes;
 import org.springframework.mock.web.MockHttpServletRequest;
 import org.springframework.mock.web.MockHttpServletResponse;
 
+import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.is;
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertThat;
 import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.fail;
 import static org.mockito.Mockito.spy;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
@@ -181,6 +185,34 @@ public class WebRequestTraceFilterTests {
 		Map<String, Object> map = (Map<String, Object>) trace.get("error");
 		System.err.println(map);
 		assertEquals("Foo", map.get("message").toString());
+	}
+
+	@Test
+	@SuppressWarnings("unchecked")
+	public void filterHas500ResponseStatusWhenExceptionIsThrown()
+			throws ServletException, IOException {
+		MockHttpServletRequest request = new MockHttpServletRequest("GET", "/foo");
+		MockHttpServletResponse response = new MockHttpServletResponse();
+
+		try {
+			this.filter.doFilterInternal(request, response, new FilterChain() {
+
+				@Override
+				public void doFilter(ServletRequest request, ServletResponse response)
+						throws IOException, ServletException {
+					throw new RuntimeException();
+				}
+
+			});
+			fail("Exception was swallowed");
+		}
+		catch (RuntimeException ex) {
+			Map<String, Object> headers = (Map<String, Object>) this.repository.findAll()
+					.iterator().next().getInfo().get("headers");
+			Map<String, Object> responseHeaders = (Map<String, Object>) headers
+					.get("response");
+			assertThat((String) responseHeaders.get("status"), is(equalTo("500")));
+		}
 	}
 
 }
