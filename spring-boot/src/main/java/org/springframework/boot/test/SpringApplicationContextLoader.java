7709543e67748759a31d82c41a884d87811bab99
Andy Wilkinson <awilkinson@pivotal.io>
2016-04-20 10:39:42 +0100

    Set web application context on ServletContext in plain web app tests
    Previously, the web application context was only set on the
    ServletContext in a full-blown integration test or when MockMvc was
    used.
    
    This commit updates SpringApplicationContextLoader so that the context
    is also set on the context in a plain web application test (one where
    MockMvc is not being used). The change is a partial backport of commit
    7dffb702.
    
    Closes gh-4370

--- spring-boot/src/main/java/org/springframework/boot/test/SpringApplicationContextLoader.java
+++ spring-boot/src/main/java/org/springframework/boot/test/SpringApplicationContextLoader.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2012-2015 the original author or authors.
+ * Copyright 2012-2016 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -241,8 +241,8 @@ public class SpringApplicationContextLoader extends AbstractContextLoader {
 				WebMergedContextConfiguration webConfiguration) {
 			SpringBootMockServletContext servletContext = new SpringBootMockServletContext(
 					webConfiguration.getResourceBasePath());
-			initializers.add(0,
-					new ServletContextApplicationContextInitializer(servletContext));
+			initializers.add(0, new ServletContextApplicationContextInitializer(
+					servletContext, true));
 		}
 
 	}
--- spring-boot/src/test/java/org/springframework/boot/test/SpringApplicationMockServletTests.java
+++ spring-boot/src/test/java/org/springframework/boot/test/SpringApplicationMockServletTests.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2012-2016 the original author or authors.
+ * Copyright 2012-2015 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -18,37 +18,28 @@ package org.springframework.boot.test;
 
 import javax.servlet.ServletContext;
 
-import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.SpringApplicationMockMvcTests.Config;
+import org.springframework.boot.test.SpringApplicationMockServletTests.Config;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
-import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.setup.MockMvcBuilders;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.context.WebApplicationContext;
 import org.springframework.web.context.support.WebApplicationContextUtils;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 import static org.junit.Assert.assertSame;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 /**
- * Tests for {@link WebAppConfiguration} with {@link MockMvc}.
+ * Tests for {@link WebAppConfiguration} with a plain mock Servlet environment.
  *
- * @author Stephane Nicoll
+ * @author Andy Wilkinson
  */
 @RunWith(SpringJUnit4ClassRunner.class)
 @SpringApplicationConfiguration(classes = Config.class)
 @WebAppConfiguration
-public class SpringApplicationMockMvcTests {
+public class SpringApplicationMockServletTests {
 
 	@Autowired
 	private WebApplicationContext context;
@@ -56,34 +47,14 @@ public class SpringApplicationMockMvcTests {
 	@Autowired
 	private ServletContext servletContext;
 
-	private MockMvc mvc;
-
-	@Before
-	public void setUp() {
-		this.mvc = MockMvcBuilders.webAppContextSetup(this.context).build();
-	}
-
 	@Test
-	public void testMockHttpEndpoint() throws Exception {
-		this.mvc.perform(get("/")).andExpect(status().isOk())
-				.andExpect(content().string("Hello World"));
-	}
-
-	@Test
-	public void validateWebApplicationContextIsSet() {
+	public void webApplicationContextIsSetOnServletContext() {
 		assertSame(this.context,
 				WebApplicationContextUtils.getWebApplicationContext(this.servletContext));
 	}
 
 	@Configuration
-	@EnableWebMvc
-	@RestController
 	protected static class Config {
-
-		@RequestMapping("/")
-		public String home() {
-			return "Hello World";
-		}
 
 	}
 
