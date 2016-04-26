ac8e87d79f329fe2ca834198854a648c49ee52f8
Stephane Nicoll <snicoll@pivotal.io>
2016-04-26 09:24:37 +0200

    Polish contribution
    Closes gh-5782

--- spring-boot-autoconfigure/src/main/java/org/springframework/boot/autoconfigure/web/ResourceProperties.java
+++ spring-boot-autoconfigure/src/main/java/org/springframework/boot/autoconfigure/web/ResourceProperties.java
@@ -118,9 +118,9 @@ public class ResourceProperties implements ResourceLoaderAware {
 	}
 
 	List<Resource> getFaviconLocations() {
-		List<Resource> locations = new ArrayList<Resource>(staticLocations.length + 1);
+		List<Resource> locations = new ArrayList<Resource>(this.staticLocations.length + 1);
 		if (this.resourceLoader != null) {
-			for (String location : staticLocations) {
+			for (String location : this.staticLocations) {
 				locations.add(this.resourceLoader.getResource(location));
 			}
 		}
--- spring-boot-autoconfigure/src/test/java/org/springframework/boot/autoconfigure/web/WebMvcAutoConfigurationTests.java
+++ spring-boot-autoconfigure/src/test/java/org/springframework/boot/autoconfigure/web/WebMvcAutoConfigurationTests.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2012-2015 the original author or authors.
+ * Copyright 2012-2016 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -413,6 +413,13 @@ public class WebMvcAutoConfigurationTests {
 				.get("faviconHandlerMapping"), is(notNullValue()));
 		Map<String, List<Resource>> mappingLocations = getFaviconMappingLocations();
 		assertThat(mappingLocations.get("/**/favicon.ico").size(), equalTo(6));
+	}
+
+	@Test
+	public void faviconMappingUsesStaticLocations() throws IllegalAccessException {
+		load("spring.resources.static-locations=classpath:/static");
+		Map<String, List<Resource>> mappingLocations = getFaviconMappingLocations();
+		assertThat(mappingLocations.get("/**/favicon.ico").size(), equalTo(2));
 	}
 
 	@Test
