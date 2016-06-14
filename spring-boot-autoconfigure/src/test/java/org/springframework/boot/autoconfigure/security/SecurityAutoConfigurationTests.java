a3b29b0e4224dbf423b50fbaad5e7ad793be6612
Dave Syer <dsyer@pivotal.io>
2016-06-14 10:31:09 +0100

    Add /webjars/** to default list of unsecured resources
    Fixes gh-2460

--- spring-boot-autoconfigure/src/test/java/org/springframework/boot/autoconfigure/security/SecurityAutoConfigurationTests.java
+++ spring-boot-autoconfigure/src/test/java/org/springframework/boot/autoconfigure/security/SecurityAutoConfigurationTests.java
@@ -90,9 +90,9 @@ public class SecurityAutoConfigurationTests {
 				PropertyPlaceholderAutoConfiguration.class);
 		this.context.refresh();
 		assertThat(this.context.getBean(AuthenticationManagerBuilder.class)).isNotNull();
-		// 4 for static resources and one for the rest
+		// 5 for static resources and one for the rest
 		assertThat(this.context.getBean(FilterChainProxy.class).getFilterChains())
-				.hasSize(5);
+				.hasSize(6);
 	}
 
 	@Test
