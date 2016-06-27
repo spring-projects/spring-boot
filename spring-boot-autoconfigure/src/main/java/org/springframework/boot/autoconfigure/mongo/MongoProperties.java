0de5988c0e2cb7d8521fc7fa5dec4f1b6886c9d5
Stephane Nicoll <snicoll@pivotal.io>
2016-06-27 15:28:08 +0200

    Use Mongo 3.0 specific API
    See gh-6176

--- spring-boot-autoconfigure/src/main/java/org/springframework/boot/autoconfigure/mongo/MongoProperties.java
+++ spring-boot-autoconfigure/src/main/java/org/springframework/boot/autoconfigure/mongo/MongoProperties.java
@@ -249,35 +249,10 @@ public class MongoProperties {
 	}
 
 	private Builder builder(MongoClientOptions options) {
-		Builder builder = MongoClientOptions.builder();
 		if (options != null) {
-			builder.alwaysUseMBeans(options.isAlwaysUseMBeans());
-			builder.connectionsPerHost(options.getConnectionsPerHost());
-			builder.connectTimeout(options.getConnectTimeout());
-			builder.cursorFinalizerEnabled(options.isCursorFinalizerEnabled());
-			builder.dbDecoderFactory(options.getDbDecoderFactory());
-			builder.dbEncoderFactory(options.getDbEncoderFactory());
-			builder.description(options.getDescription());
-			builder.heartbeatConnectTimeout(options.getHeartbeatConnectTimeout());
-			builder.heartbeatFrequency(options.getHeartbeatFrequency());
-			builder.heartbeatSocketTimeout(options.getHeartbeatSocketTimeout());
-			builder.localThreshold(options.getLocalThreshold());
-			builder.minConnectionsPerHost(options.getMinConnectionsPerHost());
-			builder.minHeartbeatFrequency(options.getMinHeartbeatFrequency());
-			builder.maxConnectionIdleTime(options.getMaxConnectionIdleTime());
-			builder.maxConnectionLifeTime(options.getMaxConnectionLifeTime());
-			builder.maxWaitTime(options.getMaxWaitTime());
-			builder.readPreference(options.getReadPreference());
-			builder.requiredReplicaSetName(options.getRequiredReplicaSetName());
-			builder.sslEnabled(options.isSslEnabled());
-			builder.socketFactory(options.getSocketFactory());
-			builder.socketKeepAlive(options.isSocketKeepAlive());
-			builder.socketTimeout(options.getSocketTimeout());
-			builder.threadsAllowedToBlockForConnectionMultiplier(
-					options.getThreadsAllowedToBlockForConnectionMultiplier());
-			builder.writeConcern(options.getWriteConcern());
+			return MongoClientOptions.builder(options);
 		}
-		return builder;
+		return MongoClientOptions.builder();
 	}
 
 }
