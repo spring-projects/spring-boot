4e07003e1d5b33dc0e83db8c6047399c9b13b6e2
Stephane Nicoll <snicoll@pivotal.io>
2016-06-27 15:14:24 +0200

    Polish "Use missing MongoClientOptions in MongoProperties"
    Closes gh-6176

--- spring-boot-autoconfigure/src/main/java/org/springframework/boot/autoconfigure/mongo/MongoProperties.java
+++ spring-boot-autoconfigure/src/main/java/org/springframework/boot/autoconfigure/mongo/MongoProperties.java
@@ -271,23 +271,23 @@ public class MongoProperties {
 			builder.dbDecoderFactory(options.getDbDecoderFactory());
 			builder.dbEncoderFactory(options.getDbEncoderFactory());
 			builder.description(options.getDescription());
+			builder.heartbeatConnectTimeout(options.getHeartbeatConnectTimeout());
+			builder.heartbeatFrequency(options.getHeartbeatFrequency());
+			builder.heartbeatSocketTimeout(options.getHeartbeatSocketTimeout());
+			builder.localThreshold(options.getLocalThreshold());
+			builder.minConnectionsPerHost(options.getMinConnectionsPerHost());
+			builder.minHeartbeatFrequency(options.getMinHeartbeatFrequency());
+			builder.maxConnectionIdleTime(options.getMaxConnectionIdleTime());
+			builder.maxConnectionLifeTime(options.getMaxConnectionLifeTime());
 			builder.maxWaitTime(options.getMaxWaitTime());
 			builder.readPreference(options.getReadPreference());
+			builder.requiredReplicaSetName(options.getRequiredReplicaSetName());
 			builder.socketFactory(options.getSocketFactory());
 			builder.socketKeepAlive(options.isSocketKeepAlive());
 			builder.socketTimeout(options.getSocketTimeout());
 			builder.threadsAllowedToBlockForConnectionMultiplier(
 					options.getThreadsAllowedToBlockForConnectionMultiplier());
 			builder.writeConcern(options.getWriteConcern());
-			builder.minConnectionsPerHost(options.getMinConnectionsPerHost());
-			builder.maxConnectionIdleTime(options.getMaxConnectionIdleTime());
-			builder.maxConnectionLifeTime(options.getMaxConnectionLifeTime());
-			builder.heartbeatFrequency(options.getHeartbeatFrequency());
-			builder.minHeartbeatFrequency(options.getMinHeartbeatFrequency());
-			builder.heartbeatConnectTimeout(options.getHeartbeatConnectTimeout());
-			builder.heartbeatSocketTimeout(options.getHeartbeatSocketTimeout());
-			builder.localThreshold(options.getLocalThreshold());
-			builder.requiredReplicaSetName(options.getRequiredReplicaSetName());
 		}
 		return builder;
 	}
