13a9bc0537f3a6e911b2ddb1ca9430b325710cf7
Andy Wilkinson <awilkinson@pivotal.io>
2016-08-16 13:25:16 +0100

    Polish “Allow injection of StatsDClient into StatsdMetricWriter”
    Closes gh-6588

--- spring-boot-actuator/src/main/java/org/springframework/boot/actuate/metrics/statsd/StatsdMetricWriter.java
+++ spring-boot-actuator/src/main/java/org/springframework/boot/actuate/metrics/statsd/StatsdMetricWriter.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2012-2015 the original author or authors.
+ * Copyright 2012-2016 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -64,7 +64,7 @@ public class StatsdMetricWriter implements MetricWriter, Closeable {
 	 */
 	public StatsdMetricWriter(String prefix, String host, int port) {
 		this(new NonBlockingStatsDClient(trimPrefix(prefix), host, port,
-			new LoggingStatsdErrorHandler()));
+				new LoggingStatsdErrorHandler()));
 	}
 
 	/**
