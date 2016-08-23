5a1741e2e83516efc18aea79dc1f517863871cf6
Andy Wilkinson <awilkinson@pivotal.io>
2016-08-23 15:48:40 +0100

    Improve documentation of when TestRestTemplate is auto-configured
    Closes gh-6729

--- spring-boot-test/src/main/java/org/springframework/boot/test/context/SpringBootTest.java
+++ spring-boot-test/src/main/java/org/springframework/boot/test/context/SpringBootTest.java
@@ -55,7 +55,8 @@ import org.springframework.web.context.WebApplicationContext;
  * {@link WebEnvironment#DEFINED_PORT defined} or {@link WebEnvironment#RANDOM_PORT
  * random} port.</li>
  * <li>Registers a {@link org.springframework.boot.test.web.client.TestRestTemplate
- * TestRestTemplate} bean for use in web tests.</li>
+ * TestRestTemplate} bean for use in web tests that are using a fully running container
+ * </li>
  * </ul>
  *
  * @author Phillip Webb
