25c4e261e92ec9df13d0f7532954edf3b5e2e1c8
Phillip Webb <pwebb@pivotal.io>
2016-09-15 17:16:26 -0700

    Fix failing tests
    See gh-6897

--- spring-boot-test-autoconfigure/src/test/java/org/springframework/boot/test/autoconfigure/orm/jpa/AutoConfigureTestDatabaseWithNoDatabaseIntegrationTests.java
+++ spring-boot-test-autoconfigure/src/test/java/org/springframework/boot/test/autoconfigure/orm/jpa/AutoConfigureTestDatabaseWithNoDatabaseIntegrationTests.java
@@ -20,7 +20,7 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.SpringBootConfiguration;
+import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.context.ApplicationContext;
 import org.springframework.test.context.junit4.SpringRunner;
 
@@ -44,7 +44,7 @@ public class AutoConfigureTestDatabaseWithNoDatabaseIntegrationTests {
 		assertThat(this.context).isNotNull();
 	}
 
-	@SpringBootConfiguration
+	@TestConfiguration
 	static class Config {
 
 	}
