ac2609b5857727724709e7da793d82264bd6726d
Phillip Webb <pwebb@pivotal.io>
2016-09-17 22:36:58 -0700

    Fix WebDriver lifecycle issues
    Update WebDriver support to ensure that the `.quit()` method is called
    after each test method runs and that a new WebDriver instance is
    injected each time.
    
    Support is provided by introducing a new `Scope` which is applied by
    a ContextCustomizerFactory and reset by a TestExecutionListener.
    
    Fixes gh-6641

--- spring-boot-test-autoconfigure/src/test/java/org/springframework/boot/test/autoconfigure/web/servlet/WebMvcTestWebDriverIntegrationTests.java
+++ spring-boot-test-autoconfigure/src/test/java/org/springframework/boot/test/autoconfigure/web/servlet/WebMvcTestWebDriverIntegrationTests.java
@@ -16,16 +16,21 @@
 
 package org.springframework.boot.test.autoconfigure.web.servlet;
 
+import org.junit.FixMethodOrder;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.junit.runners.MethodSorters;
 import org.openqa.selenium.By;
+import org.openqa.selenium.NoSuchWindowException;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.util.ReflectionTestUtils;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.Assert.fail;
 
 /**
  * Tests for {@link WebMvcTest} with {@link WebDriver}.
@@ -34,7 +39,10 @@ import static org.assertj.core.api.Assertions.assertThat;
  */
 @RunWith(SpringRunner.class)
 @WebMvcTest(secure = false)
+@FixMethodOrder(MethodSorters.NAME_ASCENDING)
 public class WebMvcTestWebDriverIntegrationTests {
+
+	private static WebDriver previousWebDriver;
 
 	@Autowired
 	private WebDriver webDriver;
@@ -44,6 +52,22 @@ public class WebMvcTestWebDriverIntegrationTests {
 		this.webDriver.get("/html");
 		WebElement element = this.webDriver.findElement(By.tagName("body"));
 		assertThat(element.getText()).isEqualTo("Hello");
+		WebMvcTestWebDriverIntegrationTests.previousWebDriver = this.webDriver;
+	}
+
+	@Test
+	public void shouldBeADifferentWebClient() throws Exception {
+		this.webDriver.get("/html");
+		WebElement element = this.webDriver.findElement(By.tagName("body"));
+		assertThat(element.getText()).isEqualTo("Hello");
+		try {
+			ReflectionTestUtils.invokeMethod(previousWebDriver, "getCurrentWindow");
+			fail("Did not call quit()");
+		}
+		catch (NoSuchWindowException ex) {
+			// Expected
+		}
+		assertThat(previousWebDriver).isNotNull().isNotSameAs(this.webDriver);
 	}
 
 }
