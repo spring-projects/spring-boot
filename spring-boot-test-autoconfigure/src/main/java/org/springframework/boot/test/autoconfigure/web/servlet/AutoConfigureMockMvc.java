e8b0a648722d8cf45745cbcb0c28c6648882ce6a
sebastiankirsch <Sebastian.Kirsch@gmail.com>
2016-11-05 13:27:26 +0100

    Add @Inherited to all AutoConfigure* classes
    See gh-7326

--- spring-boot-test-autoconfigure/src/main/java/org/springframework/boot/test/autoconfigure/web/servlet/AutoConfigureMockMvc.java
+++ spring-boot-test-autoconfigure/src/main/java/org/springframework/boot/test/autoconfigure/web/servlet/AutoConfigureMockMvc.java
@@ -18,6 +18,7 @@ package org.springframework.boot.test.autoconfigure.web.servlet;
 
 import java.lang.annotation.Documented;
 import java.lang.annotation.ElementType;
+import java.lang.annotation.Inherited;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
@@ -41,6 +42,7 @@ import org.springframework.test.web.servlet.MvcResult;
  * @see SpringBootMockMvcBuilderCustomizer
  */
 @Documented
+@Inherited
 @Retention(RetentionPolicy.RUNTIME)
 @Target({ ElementType.TYPE, ElementType.METHOD })
 @ImportAutoConfiguration
