4728aacf6531cc99234e71f446a016bb676dbe90
Hiram Chirino <hiram@hiramchirino.com>
2016-11-17 17:37:09 -0500

    Add graceful shutdown support to undertow

--- spring-boot-autoconfigure/src/main/java/org/springframework/boot/autoconfigure/web/ServerProperties.java
+++ spring-boot-autoconfigure/src/main/java/org/springframework/boot/autoconfigure/web/ServerProperties.java
@@ -1188,6 +1188,13 @@ public class ServerProperties
 		 */
 		private Boolean directBuffers;
 
+		/**
+		 * Time in milliseconds that a shutdown will wait for in progress HTTP request to complete before
+		 * closing all the connections. When not set it defaults to 30000 ms.  Use a value of -1 to indicate no
+		 * (i.e. infinite) timeout.
+		 */
+		private Long gracefulShutdownTimeout;
+
 		private final Accesslog accesslog = new Accesslog();
 
 		public Integer getBufferSize() {
@@ -1265,6 +1272,9 @@ public class ServerProperties
 			}
 			if (this.accesslog.enabled != null) {
 				factory.setAccessLogEnabled(this.accesslog.enabled);
+			}
+			if (this.gracefulShutdownTimeout != null) {
+				factory.setGracefulShutdownTimeout(this.gracefulShutdownTimeout);
 			}
 			factory.setUseForwardHeaders(serverProperties.getOrDeduceUseForwardHeaders());
 			if (serverProperties.getMaxHttpHeaderSize() > 0) {
