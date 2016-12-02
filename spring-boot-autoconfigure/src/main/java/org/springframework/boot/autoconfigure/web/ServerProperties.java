b60150b05ea19f2db52773fa809e9900b28a2df0
Andy Wilkinson <awilkinson@pivotal.io>
2016-12-02 13:58:26 +0000

    Make Tomcat Access Log's buffering configurable via the environment
    Closes gh-7456

--- spring-boot-autoconfigure/src/main/java/org/springframework/boot/autoconfigure/web/ServerProperties.java
+++ spring-boot-autoconfigure/src/main/java/org/springframework/boot/autoconfigure/web/ServerProperties.java
@@ -1005,6 +1005,7 @@ public class ServerProperties
 			valve.setRequestAttributesEnabled(
 					this.accesslog.isRequestAttributesEnabled());
 			valve.setRotatable(this.accesslog.isRotate());
+			valve.setBuffered(this.accesslog.isBuffered());
 			factory.addEngineValves(valve);
 		}
 
@@ -1064,6 +1065,11 @@ public class ServerProperties
 			 * the request.
 			 */
 			private boolean requestAttributesEnabled;
+
+			/**
+			 * Buffer output such that it is only flushed periodically.
+			 */
+			private boolean buffered = true;
 
 			public boolean isEnabled() {
 				return this.enabled;
@@ -1127,6 +1133,14 @@ public class ServerProperties
 
 			public void setRequestAttributesEnabled(boolean requestAttributesEnabled) {
 				this.requestAttributesEnabled = requestAttributesEnabled;
+			}
+
+			public boolean isBuffered() {
+				return this.buffered;
+			}
+
+			public void setBuffered(boolean buffered) {
+				this.buffered = buffered;
 			}
 
 		}
