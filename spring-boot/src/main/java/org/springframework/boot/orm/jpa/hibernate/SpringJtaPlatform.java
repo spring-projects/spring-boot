ef3f32f4931c6c54fc6615a9fb2089a517bed699
Stephane Nicoll <snicoll@pivotal.io>
2016-12-16 14:58:22 +0100

    Remove unused "hasTransactionManager" method
    Closes gh-7592

--- spring-boot/src/main/java/org/springframework/boot/orm/jpa/hibernate/SpringJtaPlatform.java
+++ spring-boot/src/main/java/org/springframework/boot/orm/jpa/hibernate/SpringJtaPlatform.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2012-2014 the original author or authors.
+ * Copyright 2012-2016 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -42,10 +42,6 @@ public class SpringJtaPlatform extends AbstractJtaPlatform {
 	public SpringJtaPlatform(JtaTransactionManager transactionManager) {
 		Assert.notNull(transactionManager, "TransactionManager must not be null");
 		this.transactionManager = transactionManager;
-	}
-
-	protected boolean hasTransactionManager() {
-		return true;
 	}
 
 	@Override
