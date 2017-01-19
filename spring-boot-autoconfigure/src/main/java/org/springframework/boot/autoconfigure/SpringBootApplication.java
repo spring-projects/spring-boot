f93d4a0cbf3c361b31afe1c0d96b6419420e5296
Andy Wilkinson <awilkinson@pivotal.io>
2017-01-19 12:21:27 +0000

    Add explicit aliases for exclude and excludeName on SpringBootApplication
    Previously, SpringBootApplication relied on implicity aliasing of
    exclude and excludeName that worked because the two attributes have
    the same names as the equivalent attributes on
    EnableAutoConfiguration.
    
    This commit updates SpringBootApplication to make the aliases explicit
    and also adds tests to EnableAutoConfigurationImportSelectorTests to
    verify that the aliasing is working as intended.
    
    Closes gh-7951

--- spring-boot-autoconfigure/src/main/java/org/springframework/boot/autoconfigure/SpringBootApplication.java
+++ spring-boot-autoconfigure/src/main/java/org/springframework/boot/autoconfigure/SpringBootApplication.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2012-2016 the original author or authors.
+ * Copyright 2012-2017 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -58,6 +58,7 @@ public @interface SpringBootApplication {
 	 * Exclude specific auto-configuration classes such that they will never be applied.
 	 * @return the classes to exclude
 	 */
+	@AliasFor(annotation = EnableAutoConfiguration.class, attribute = "exclude")
 	Class<?>[] exclude() default {};
 
 	/**
@@ -66,6 +67,7 @@ public @interface SpringBootApplication {
 	 * @return the class names to exclude
 	 * @since 1.3.0
 	 */
+	@AliasFor(annotation = EnableAutoConfiguration.class, attribute = "excludeName")
 	String[] excludeName() default {};
 
 	/**
