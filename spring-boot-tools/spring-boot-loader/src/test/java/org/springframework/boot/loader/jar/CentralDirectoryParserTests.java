2b7557068017e2b50b88dbb98efc8d26ce36123b
Andy Wilkinson <awilkinson@pivotal.io>
2017-01-24 11:07:43 +0000

    Fix dead code warning in CentralDirectoryParserTests

--- spring-boot-tools/spring-boot-loader/src/test/java/org/springframework/boot/loader/jar/CentralDirectoryParserTests.java
+++ spring-boot-tools/spring-boot-loader/src/test/java/org/springframework/boot/loader/jar/CentralDirectoryParserTests.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2012-2016 the original author or authors.
+ * Copyright 2012-2017 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -59,7 +59,7 @@ public class CentralDirectoryParserTests {
 		CentralDirectoryParser parser = new CentralDirectoryParser();
 		parser.addVisitor(visitor);
 		parser.parse(this.jarData, false);
-		List<String> invocations = visitor.invocations;
+		List<String> invocations = visitor.getInvocations();
 		assertThat(invocations).startsWith("visitStart").endsWith("visitEnd")
 				.contains("visitFileHeader");
 	}
