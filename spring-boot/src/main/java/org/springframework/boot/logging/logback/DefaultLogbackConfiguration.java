e8f8556df39322812bcdecccf4d1af6e6c0217da
Andy Wilkinson <awilkinson@pivotal.io>
2017-01-25 14:08:02 +0000

    Remove remapping of Thymeleaf's logging as it is no longer necessary
    Thymeleaf 3.0 no longer logs its initialization information at INFO
    level so we no longer need to remap its INFO output to DEBUG.
    
    Closes gh-7657

--- spring-boot/src/main/java/org/springframework/boot/logging/logback/DefaultLogbackConfiguration.java
+++ spring-boot/src/main/java/org/springframework/boot/logging/logback/DefaultLogbackConfiguration.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2012-2016 the original author or authors.
+ * Copyright 2012-2017 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -108,7 +108,6 @@ class DefaultLogbackConfiguration {
 		config.logger("org.hibernate.validator.internal.util.Version", Level.WARN);
 		config.logger("org.springframework.boot.actuate.endpoint.jmx", null, false,
 				debugRemapAppender);
-		config.logger("org.thymeleaf", null, false, debugRemapAppender);
 	}
 
 	private Appender<ILoggingEvent> consoleAppender(LogbackConfigurator config) {
