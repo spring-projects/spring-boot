30d1046f0e265b9acab4ca311f046ad8ab73be3f
Bruce Brouwer <bruce.brouwer@gfs.com>
2017-01-31 17:34:04 -0500

    Adding root view auto-config support for thymeleaf

---
+++ spring-boot-autoconfigure/src/test/java/org/springframework/boot/autoconfigure/thymeleaf/ThymeleafAutoConfigIntegrationTestBase.java
@@ -0,0 +1,60 @@
+/*
+ * Copyright 2012-2017 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.springframework.boot.autoconfigure.thymeleaf;
+
+import org.junit.Before;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.context.PropertyPlaceholderAutoConfiguration;
+import org.springframework.boot.autoconfigure.web.DispatcherServletAutoConfiguration;
+import org.springframework.boot.autoconfigure.web.EmbeddedServletContainerAutoConfiguration;
+import org.springframework.boot.autoconfigure.web.HttpMessageConvertersAutoConfiguration;
+import org.springframework.boot.autoconfigure.web.ServerPropertiesAutoConfiguration;
+import org.springframework.boot.autoconfigure.web.WebMvcAutoConfiguration;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+
+/**
+ * Base for integration tests for {@link ThymeleafAutoConfiguration}.
+ *
+ * @author Bruce Brouwer
+ */
+public class ThymeleafAutoConfigIntegrationTestBase {
+
+	@Autowired
+	protected WebApplicationContext wac;
+
+	protected MockMvc mockMvc;
+
+	@Before
+	public void setup() {
+		this.mockMvc = MockMvcBuilders.webAppContextSetup(this.wac).build();
+	}
+
+    @Configuration
+    @Import({ EmbeddedServletContainerAutoConfiguration.class,
+    	ServerPropertiesAutoConfiguration.class,
+    	DispatcherServletAutoConfiguration.class, WebMvcAutoConfiguration.class,
+    	HttpMessageConvertersAutoConfiguration.class, ThymeleafAutoConfiguration.class,
+    	PropertyPlaceholderAutoConfiguration.class })
+    static class TestConfig {
+        // simplest configuration possible
+    }
+}
