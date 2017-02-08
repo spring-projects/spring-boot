8a59e88a9db0f80713553823bbe554c79f920557
Stephane Nicoll <snicoll@pivotal.io>
2017-02-08 14:29:33 +0100

    Fix MongoDB CustomConversions bean name
    This commit qualifies the `CustomConversions` bean name that the Mongo
    auto-configuration might create. `CustomConversions` is a common pattern
    in Spring Data and other stores are using the same name.
    
    See https://jira.spring.io/browse/DATASOLR-362
    
    Closes gh-8225

--- spring-boot-autoconfigure/src/main/java/org/springframework/boot/autoconfigure/data/mongo/MongoDataAutoConfiguration.java
+++ spring-boot-autoconfigure/src/main/java/org/springframework/boot/autoconfigure/data/mongo/MongoDataAutoConfiguration.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2012-2016 the original author or authors.
+ * Copyright 2012-2017 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -68,6 +68,7 @@ import org.springframework.util.StringUtils;
  * @author Josh Long
  * @author Phillip Webb
  * @author Eddú Meléndez
+ * @author Stephane Nicoll
  * @since 1.1.0
  */
 @Configuration
@@ -139,7 +140,7 @@ public class MongoDataAutoConfiguration {
 
 	@Bean
 	@ConditionalOnMissingBean
-	public CustomConversions customConversions() {
+	public CustomConversions mongoCustomConversions() {
 		return new CustomConversions(Collections.emptyList());
 	}
 
