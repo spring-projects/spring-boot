spring-projects/spring-boot

commit 0af53b361f29a4e357ebe33edbcea5ea255c1d05

Ensure embedded start can be called multiple times
Update all `EmbeddedServletContainer` implementations to ensure that
the `start()` method can be called multiple times, with the second call
being ignored.

Fixes gh-8036

spring-boot/src/main/java/org/springframework/boot/context/embedded/jetty/JettyEmbeddedServletContainer.java
@@ -59,6 +59,8 @@ public class JettyEmbeddedServletContainer implements EmbeddedServletContainer {
 
 	private Connector[] connectors;
 
+	private volatile boolean started;
+
 	/**
 	 * Create a new {@link JettyEmbeddedServletContainer} instance.
 	 * @param server the underlying Jetty server
@@ -111,37 +113,43 @@ public class JettyEmbeddedServletContainer implements EmbeddedServletContainer {
 
 	@Override
 	public void start() throws EmbeddedServletContainerException {
-		this.server.setConnectors(this.connectors);
-		if (!this.autoStart) {
-			return;
-		}
-		try {
-			this.server.start();
-			for (Handler handler : this.server.getHandlers()) {
-				handleDeferredInitialize(handler);
+		synchronized (this.monitor) {
+			if (this.started) {
+				return;
 			}
-			Connector[] connectors = this.server.getConnectors();
-			for (Connector connector : connectors) {
-				try {
-					connector.start();
+			this.server.setConnectors(this.connectors);
+			if (!this.autoStart) {
+				return;
+			}
+			try {
+				this.server.start();
+				for (Handler handler : this.server.getHandlers()) {
+					handleDeferredInitialize(handler);
 				}
-				catch (BindException ex) {
-					if (connector instanceof NetworkConnector) {
-						throw new PortInUseException(
-								((NetworkConnector) connector).getPort());
+				Connector[] connectors = this.server.getConnectors();
+				for (Connector connector : connectors) {
+					try {
+						connector.start();
 					}
-					throw ex;
+					catch (BindException ex) {
+						if (connector instanceof NetworkConnector) {
+							throw new PortInUseException(
+									((NetworkConnector) connector).getPort());
+						}
+						throw ex;
+					}
 				}
+				this.started = true;
+				JettyEmbeddedServletContainer.logger
+						.info("Jetty started on port(s) " + getActualPortsDescription());
 			}
-			JettyEmbeddedServletContainer.logger
-					.info("Jetty started on port(s) " + getActualPortsDescription());
-		}
-		catch (EmbeddedServletContainerException ex) {
-			throw ex;
-		}
-		catch (Exception ex) {
-			throw new EmbeddedServletContainerException(
-					"Unable to start embedded Jetty servlet container", ex);
+			catch (EmbeddedServletContainerException ex) {
+				throw ex;
+			}
+			catch (Exception ex) {
+				throw new EmbeddedServletContainerException(
+						"Unable to start embedded Jetty servlet container", ex);
+			}
 		}
 	}
 
@@ -197,6 +205,10 @@ public class JettyEmbeddedServletContainer implements EmbeddedServletContainer {
 	@Override
 	public void stop() {
 		synchronized (this.monitor) {
+			if (!this.started) {
+				return;
+			}
+			this.started = false;
 			try {
 				this.server.stop();
 			}

spring-boot/src/main/java/org/springframework/boot/context/embedded/tomcat/TomcatEmbeddedServletContainer.java
@@ -62,6 +62,8 @@ public class TomcatEmbeddedServletContainer implements EmbeddedServletContainer 
 
 	private final boolean autoStart;
 
+	private volatile boolean started;
+
 	/**
 	 * Create a new {@link TomcatEmbeddedServletContainer} instance.
 	 * @param tomcat the underlying Tomcat server
@@ -174,28 +176,34 @@ public class TomcatEmbeddedServletContainer implements EmbeddedServletContainer 
 
 	@Override
 	public void start() throws EmbeddedServletContainerException {
-		try {
-			addPreviouslyRemovedConnectors();
-			Connector connector = this.tomcat.getConnector();
-			if (connector != null && this.autoStart) {
-				startConnector(connector);
+		synchronized (this.monitor) {
+			if (this.started) {
+				return;
 			}
-			checkThatConnectorsHaveStarted();
-			TomcatEmbeddedServletContainer.logger
-					.info("Tomcat started on port(s): " + getPortsDescription(true));
-		}
-		catch (ConnectorStartFailedException ex) {
-			stopSilently();
-			throw ex;
-		}
-		catch (Exception ex) {
-			throw new EmbeddedServletContainerException(
-					"Unable to start embedded Tomcat servlet container", ex);
-		}
-		finally {
-			Context context = findContext();
-			ContextBindings.unbindClassLoader(context, getNamingToken(context),
-					getClass().getClassLoader());
+			try {
+				addPreviouslyRemovedConnectors();
+				Connector connector = this.tomcat.getConnector();
+				if (connector != null && this.autoStart) {
+					startConnector(connector);
+				}
+				checkThatConnectorsHaveStarted();
+				this.started = true;
+				TomcatEmbeddedServletContainer.logger
+						.info("Tomcat started on port(s): " + getPortsDescription(true));
+			}
+			catch (ConnectorStartFailedException ex) {
+				stopSilently();
+				throw ex;
+			}
+			catch (Exception ex) {
+				throw new EmbeddedServletContainerException(
+						"Unable to start embedded Tomcat servlet container", ex);
+			}
+			finally {
+				Context context = findContext();
+				ContextBindings.unbindClassLoader(context, getNamingToken(context),
+						getClass().getClassLoader());
+			}
 		}
 	}
 
@@ -271,7 +279,11 @@ public class TomcatEmbeddedServletContainer implements EmbeddedServletContainer 
 	@Override
 	public void stop() throws EmbeddedServletContainerException {
 		synchronized (this.monitor) {
+			if (!this.started) {
+				return;
+			}
 			try {
+				this.started = false;
 				try {
 					stopTomcat();
 					this.tomcat.destroy();

spring-boot/src/main/java/org/springframework/boot/context/embedded/undertow/UndertowEmbeddedServletContainer.java
@@ -88,7 +88,7 @@ public class UndertowEmbeddedServletContainer implements EmbeddedServletContaine
 
 	private Undertow undertow;
 
-	private boolean started = false;
+	private volatile boolean started = false;
 
 	/**
 	 * Create a new {@link UndertowEmbeddedServletContainer} instance.
@@ -201,6 +201,9 @@ public class UndertowEmbeddedServletContainer implements EmbeddedServletContaine
 	@Override
 	public void start() throws EmbeddedServletContainerException {
 		synchronized (this.monitor) {
+			if (this.started) {
+				return;
+			}
 			try {
 				if (!this.autoStart) {
 					return;
@@ -362,16 +365,17 @@ public class UndertowEmbeddedServletContainer implements EmbeddedServletContaine
 	@Override
 	public void stop() throws EmbeddedServletContainerException {
 		synchronized (this.monitor) {
-			if (this.started) {
-				try {
-					this.started = false;
-					this.manager.stop();
-					this.undertow.stop();
-				}
-				catch (Exception ex) {
-					throw new EmbeddedServletContainerException("Unable to stop undertow",
-							ex);
-				}
+			if (!this.started) {
+				return;
+			}
+			this.started = false;
+			try {
+				this.manager.stop();
+				this.undertow.stop();
+			}
+			catch (Exception ex) {
+				throw new EmbeddedServletContainerException("Unable to stop undertow",
+						ex);
 			}
 		}
 	}

spring-boot/src/test/java/org/springframework/boot/context/embedded/AbstractEmbeddedServletContainerFactoryTests.java
@@ -79,6 +79,7 @@ import org.mockito.InOrder;
 import org.springframework.boot.ApplicationHome;
 import org.springframework.boot.ApplicationTemp;
 import org.springframework.boot.context.embedded.Ssl.ClientAuth;
+import org.springframework.boot.testutil.InternalOutputCapture;
 import org.springframework.boot.web.servlet.ErrorPage;
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.boot.web.servlet.ServletContextInitializer;
@@ -120,6 +121,9 @@ public abstract class AbstractEmbeddedServletContainerFactoryTests {
 	@Rule
 	public TemporaryFolder temporaryFolder = new TemporaryFolder();
 
+	@Rule
+	public InternalOutputCapture output = new InternalOutputCapture();
+
 	protected EmbeddedServletContainer container;
 
 	private final HttpClientContext httpClientContext = HttpClientContext.create();
@@ -151,6 +155,19 @@ public abstract class AbstractEmbeddedServletContainerFactoryTests {
 				.getEmbeddedServletContainer(exampleServletRegistration());
 		this.container.start();
 		assertThat(getResponse(getLocalUrl("/hello"))).isEqualTo("Hello World");
+	}
+
+	@Test
+	public void startCalledTwice() throws Exception {
+		AbstractEmbeddedServletContainerFactory factory = getFactory();
+		this.container = factory
+				.getEmbeddedServletContainer(exampleServletRegistration());
+		this.container.start();
+		int port = this.container.getPort();
+		this.container.start();
+		assertThat(this.container.getPort()).isEqualTo(port);
+		assertThat(getResponse(getLocalUrl("/hello"))).isEqualTo("Hello World");
+		assertThat(this.output.toString()).containsOnlyOnce("started on port");
 	}
 
 	@Test

commit 757aa647cfa44196c93f5730d0f7b33032b5981f

Ensure web containers are stopped after close
Update `EmbeddedServletContainer` implementations to ensure that stop
can be called even if start has not. This allows servers that are
partially started during `initialize()` to still be shut down.

This commit fixes a regression caused by commit 0af53b361f.

See gh-8036
Fixes gh-8224
Closes gh-8227

spring-boot/src/main/java/org/springframework/boot/context/embedded/jetty/JettyEmbeddedServletContainer.java
@@ -205,9 +205,6 @@ public class JettyEmbeddedServletContainer implements EmbeddedServletContainer {
 	@Override
 	public void stop() {
 		synchronized (this.monitor) {
-			if (!this.started) {
-				return;
-			}
 			this.started = false;
 			try {
 				this.server.stop();

spring-boot/src/main/java/org/springframework/boot/context/embedded/tomcat/TomcatEmbeddedServletContainer.java
@@ -279,9 +279,6 @@ public class TomcatEmbeddedServletContainer implements EmbeddedServletContainer 
 	@Override
 	public void stop() throws EmbeddedServletContainerException {
 		synchronized (this.monitor) {
-			if (!this.started) {
-				return;
-			}
 			try {
 				this.started = false;
 				try {

spring-boot/src/test/java/org/springframework/boot/context/embedded/jetty/JettyEmbeddedServletContainerFactoryTests.java
@@ -143,6 +143,16 @@ public class JettyEmbeddedServletContainerFactoryTests
 				.isEmpty();
 	}
 
+	@Test
+	public void stopNoStart() throws Exception {
+		JettyEmbeddedServletContainerFactory factory = getFactory();
+		this.container = factory
+				.getEmbeddedServletContainer(exampleServletRegistration());
+		this.container.stop();
+		Server server = ((JettyEmbeddedServletContainer) this.container).getServer();
+		assertThat(server.isStopped()).isTrue();
+	}
+
 	@Override
 	protected void addConnector(final int port,
 			AbstractEmbeddedServletContainerFactory factory) {

spring-boot/src/test/java/org/springframework/boot/context/embedded/tomcat/TomcatEmbeddedServletContainerFactoryTests.java
@@ -352,6 +352,16 @@ public class TomcatEmbeddedServletContainerFactoryTests
 				.doesNotContain("appears to have started a thread named [main]");
 	}
 
+	@Test
+	public void stopNoStart() throws Exception {
+		TomcatEmbeddedServletContainerFactory factory = getFactory();
+		this.container = factory
+				.getEmbeddedServletContainer(exampleServletRegistration());
+		this.container.stop();
+		Tomcat tomcat = ((TomcatEmbeddedServletContainer) this.container).getTomcat();
+		assertThat(tomcat.getServer().getState()).isSameAs(LifecycleState.DESTROYED);
+	}
+
 	@Override
 	protected void addConnector(int port,
 			AbstractEmbeddedServletContainerFactory factory) {