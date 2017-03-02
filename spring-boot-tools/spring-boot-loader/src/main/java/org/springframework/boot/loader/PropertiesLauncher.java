cb17f98ba40c78043667a77bb67198fb3c5e8c25
Dave Syer <dsyer@pivotal.io>
2017-03-02 08:50:07 +0000

    Allow loader.path to refer to nested jars (not the root)
    The fix is pretty easy, and cuts out some dead code by the looks of it.
    There's an integration test to verify that it works.
    
    Example:
    
    $ java -jar -Dloader.path='jar:file:./lib.jar/!BOOT-INF/lib' my.jar
    
    Fixes gh-8334

--- spring-boot-tools/spring-boot-loader/src/main/java/org/springframework/boot/loader/PropertiesLauncher.java
+++ spring-boot-tools/spring-boot-loader/src/main/java/org/springframework/boot/loader/PropertiesLauncher.java
@@ -21,12 +21,10 @@ import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.net.HttpURLConnection;
-import java.net.MalformedURLException;
 import java.net.URL;
 import java.net.URLConnection;
 import java.util.ArrayList;
 import java.util.Collections;
-import java.util.Iterator;
 import java.util.List;
 import java.util.Properties;
 import java.util.jar.Manifest;
@@ -497,14 +495,18 @@ public class PropertiesLauncher extends Launcher {
 			// If home dir is same as parent archive, no need to add it twice.
 			return list;
 		}
-		EntryFilter filter = new PrefixMatchingArchiveFilter(root);
-		if (this.parent.getNestedArchives(filter).isEmpty()) {
-			return list;
+		Archive parent = this.parent;
+		if (root.startsWith("jar:file:") && root.contains("!")) {
+			int index = root.indexOf("!");
+			String file = root.substring("jar:file:".length(), index);
+			parent = new JarFileArchive(new File(file));
+			root = root.substring(index + 1, root.length());
+			while (root.startsWith("/")) {
+				root = root.substring(1);
+			}
 		}
-		// If there are more archives nested in this subdirectory (root) then create a new
-		// virtual archive for them, and have it added to the classpath
-		list.add(new FilteredArchive(this.parent, filter));
-		return list;
+		EntryFilter filter = new PrefixMatchingArchiveFilter(root);
+		return parent.getNestedArchives(filter);
 	}
 
 	private void addNestedEntries(List<Archive> lib) {
@@ -622,49 +624,6 @@ public class PropertiesLauncher extends Launcher {
 		@Override
 		public boolean matches(Entry entry) {
 			return entry.getName().endsWith(DOT_JAR) || entry.getName().endsWith(DOT_ZIP);
-		}
-
-	}
-
-	/**
-	 * Decorator to apply an {@link Archive.EntryFilter} to an existing {@link Archive}.
-	 */
-	private static class FilteredArchive implements Archive {
-
-		private final Archive parent;
-
-		private final EntryFilter filter;
-
-		FilteredArchive(Archive parent, EntryFilter filter) {
-			this.parent = parent;
-			this.filter = filter;
-		}
-
-		@Override
-		public URL getUrl() throws MalformedURLException {
-			return this.parent.getUrl();
-		}
-
-		@Override
-		public Manifest getManifest() throws IOException {
-			return this.parent.getManifest();
-		}
-
-		@Override
-		public Iterator<Entry> iterator() {
-			throw new UnsupportedOperationException();
-		}
-
-		@Override
-		public List<Archive> getNestedArchives(final EntryFilter filter)
-				throws IOException {
-			return this.parent.getNestedArchives(new EntryFilter() {
-				@Override
-				public boolean matches(Entry entry) {
-					return FilteredArchive.this.filter.matches(entry)
-							&& filter.matches(entry);
-				}
-			});
 		}
 
 	}
