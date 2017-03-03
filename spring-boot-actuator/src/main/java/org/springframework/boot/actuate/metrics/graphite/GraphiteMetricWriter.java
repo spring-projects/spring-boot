4ce92adbb852b9e90f5dda8904a13c713424483d
Mark Sailes <mark.sailes@skybettingandgaming.com>
2017-03-03 09:29:50 +0000

    Formatting changes made to follow guidelines.

--- spring-boot-actuator/src/main/java/org/springframework/boot/actuate/metrics/graphite/GraphiteMetricWriter.java
+++ spring-boot-actuator/src/main/java/org/springframework/boot/actuate/metrics/graphite/GraphiteMetricWriter.java
@@ -16,16 +16,17 @@
 
 package org.springframework.boot.actuate.metrics.graphite;
 
+import java.io.IOException;
+import java.io.OutputStream;
+import java.net.Socket;
+
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
+
 import org.springframework.boot.actuate.metrics.Metric;
 import org.springframework.boot.actuate.metrics.writer.Delta;
 import org.springframework.boot.actuate.metrics.writer.MetricWriter;
 import org.springframework.util.StringUtils;
-
-import java.io.IOException;
-import java.io.OutputStream;
-import java.net.Socket;
 
 /**
  * A {@link MetricWriter} that pushes data to Graphite.
@@ -34,59 +35,61 @@ import java.net.Socket;
  */
 public class GraphiteMetricWriter implements MetricWriter {
 
-    private static final Log logger = LogFactory.getLog(GraphiteMetricWriter.class);
+	private static final Log logger = LogFactory.getLog(GraphiteMetricWriter.class);
 
-    private final String prefix;
-    private final String host;
-    private final int port;
+	private final String prefix;
+	private final String host;
+	private final int port;
 
-    public GraphiteMetricWriter(String host, int port) {
-        this(null, host, port);
-    }
+	public GraphiteMetricWriter(String host, int port) {
+		this(null, host, port);
+	}
 
-    public GraphiteMetricWriter(String prefix, String host, int port) {
-        this.prefix = removeTrailingDots(prefix);
-        this.host = host;
-        this.port = port;
-    }
+	public GraphiteMetricWriter(String prefix, String host, int port) {
+		this.prefix = removeTrailingDots(prefix);
+		this.host = host;
+		this.port = port;
+	}
 
-    private static String removeTrailingDots(String prefix) {
-        String trimmedPrefix = StringUtils.hasText(prefix) ? prefix : null;
-        while (trimmedPrefix != null && trimmedPrefix.endsWith(".")) {
-            trimmedPrefix = trimmedPrefix.substring(0, trimmedPrefix.length() - 1);
-        }
+	private static String removeTrailingDots(String prefix) {
+		String trimmedPrefix = StringUtils.hasText(prefix) ? prefix : null;
+		while (trimmedPrefix != null && trimmedPrefix.endsWith(".")) {
+			trimmedPrefix = trimmedPrefix.substring(0, trimmedPrefix.length() - 1);
+		}
 
-        return trimmedPrefix;
-    }
+		return trimmedPrefix;
+	}
 
-    @Override
-    public void increment(Delta<?> delta) {
-        sendMetric(delta);
-    }
+	@Override
+	public void increment(Delta<?> delta) {
+		sendMetric(delta);
+	}
 
-    @Override
-    public void reset(String s) {
-        // Not implemented
-    }
+	@Override
+	public void reset(String s) {
+		// Not implemented
+	}
 
-    @Override
-    public void set(Metric<?> metric) {
-        sendMetric(metric);
-    }
+	@Override
+	public void set(Metric<?> metric) {
+		sendMetric(metric);
+	}
 
-    private void sendMetric(Metric<?> metric) {
-        String fullMetric = metric.getName();
+	private void sendMetric(Metric<?> metric) {
+		String fullMetric = metric.getName();
 
-        if (this.prefix != null) {
-            fullMetric = this.prefix + "." + metric.getName();
-        }
+		if (this.prefix != null) {
+			fullMetric = this.prefix + "." + metric.getName();
+		}
 
-        try (Socket socket = new Socket(this.host, this.port);
-             OutputStream stream = socket.getOutputStream()) {
-            String payload = String.format("%s %d %d%n", fullMetric, metric.getValue().intValue(), metric.getTimestamp().getTime() / 1000);
-            stream.write(payload.getBytes());
-        } catch (IOException e) {
-            logger.error(e.getMessage(), e);
-        }
-    }
+		try (Socket socket = new Socket(this.host, this.port);
+				OutputStream stream = socket.getOutputStream()) {
+			String payload = String.format("%s %d %d%n", fullMetric,
+					metric.getValue().intValue(), metric.getTimestamp().getTime() / 1000);
+			stream.write(payload.getBytes());
+		}
+		catch (IOException e) {
+			logger.error(e.getMessage(), e);
+		}
+	}
 }
--- spring-boot-actuator/src/test/java/org/springframework/boot/actuate/metrics/graphite/GraphiteMetricWriterTest.java
+++ spring-boot-actuator/src/test/java/org/springframework/boot/actuate/metrics/graphite/GraphiteMetricWriterTest.java
@@ -16,12 +16,7 @@
 
 package org.springframework.boot.actuate.metrics.graphite;
 
-import org.junit.After;
-import org.junit.Ignore;
-import org.junit.Test;
-import org.springframework.boot.actuate.metrics.Metric;
-import org.springframework.boot.actuate.metrics.writer.Delta;
-import org.springframework.util.SocketUtils;
+import static org.assertj.core.api.Assertions.assertThat;
 
 import java.io.BufferedReader;
 import java.io.IOException;
@@ -32,7 +27,13 @@ import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 
-import static org.assertj.core.api.Assertions.assertThat;
+import org.junit.After;
+import org.junit.Ignore;
+import org.junit.Test;
+
+import org.springframework.boot.actuate.metrics.Metric;
+import org.springframework.boot.actuate.metrics.writer.Delta;
+import org.springframework.util.SocketUtils;
 
 /**
  * Tests for {@link GraphiteMetricWriter}.
@@ -41,115 +42,125 @@ import static org.assertj.core.api.Assertions.assertThat;
  */
 public class GraphiteMetricWriterTest {
 
-    private int port = SocketUtils.findAvailableTcpPort();
+	private int port = SocketUtils.findAvailableTcpPort();
 
-    private DummyGraphiteServer server = new DummyGraphiteServer(this.port);
+	private DummyGraphiteServer server = new DummyGraphiteServer(this.port);
 
-    private GraphiteMetricWriter writer = new GraphiteMetricWriter("me", "localhost", this.port);
+	private GraphiteMetricWriter writer = new GraphiteMetricWriter("me", "localhost",
+			this.port);
 
-    @After
-    public void close() {
-        this.server.stop();
-    }
+	@After
+	public void close() {
+		this.server.stop();
+	}
 
-    @Test
-    public void increment() {
-        this.writer.increment(new Delta<Long>("counter.foo", 3L, new Date(1488405805)));
-        this.server.waitForMessage();
-        assertThat(this.server.messagesReceived().get(0)).isEqualTo("me.counter.foo 3 1488405");
-    }
+	@Test
+	public void increment() {
+		this.writer.increment(new Delta<Long>("counter.foo", 3L, new Date(1488405805)));
+		this.server.waitForMessage();
+		assertThat(this.server.messagesReceived().get(0))
+				.isEqualTo("me.counter.foo 3 1488405");
+	}
 
-    @Test
-    public void setLongMetric() throws Exception {
-        this.writer.set(new Metric<Long>("gauge.foo", 3L, new Date(1488405805)));
-        this.server.waitForMessage();
-        assertThat(this.server.messagesReceived().get(0)).isEqualTo("me.gauge.foo 3 1488405");
-    }
+	@Test
+	public void setLongMetric() throws Exception {
+		this.writer.set(new Metric<Long>("gauge.foo", 3L, new Date(1488405805)));
+		this.server.waitForMessage();
+		assertThat(this.server.messagesReceived().get(0))
+				.isEqualTo("me.gauge.foo 3 1488405");
+	}
 
-    @Ignore
-    @Test
-    public void setDoubleMetric() throws Exception {
-        this.writer.set(new Metric<Double>("gauge.foo", 3.7, new Date(1488405805)));
-        this.server.waitForMessage();
-        // Doubles are truncated
-        assertThat(this.server.messagesReceived().get(0)).isEqualTo("me.gauge.foo 3.7 1488405");
-    }
+	@Ignore
+	@Test
+	public void setDoubleMetric() throws Exception {
+		this.writer.set(new Metric<Double>("gauge.foo", 3.7, new Date(1488405805)));
+		this.server.waitForMessage();
+		// Doubles are truncated
+		assertThat(this.server.messagesReceived().get(0))
+				.isEqualTo("me.gauge.foo 3.7 1488405");
+	}
 
-    @Test
-    public void setTimerMetric() throws Exception {
-        this.writer.set(new Metric<Long>("timer.foo", 37L, new Date(1488405805)));
-        this.server.waitForMessage();
-        assertThat(this.server.messagesReceived().get(0)).isEqualTo("me.timer.foo 37 1488405");
-    }
+	@Test
+	public void setTimerMetric() throws Exception {
+		this.writer.set(new Metric<Long>("timer.foo", 37L, new Date(1488405805)));
+		this.server.waitForMessage();
+		assertThat(this.server.messagesReceived().get(0))
+				.isEqualTo("me.timer.foo 37 1488405");
+	}
 
-    @Test
-    public void nullPrefix() throws Exception {
-        this.writer = new GraphiteMetricWriter("localhost", this.port);
-        this.writer.set(new Metric<Long>("gauge.foo", 3L, new Date(1488405805)));
-        this.server.waitForMessage();
-        assertThat(this.server.messagesReceived().get(0)).isEqualTo("gauge.foo 3 1488405");
-    }
+	@Test
+	public void nullPrefix() throws Exception {
+		this.writer = new GraphiteMetricWriter("localhost", this.port);
+		this.writer.set(new Metric<Long>("gauge.foo", 3L, new Date(1488405805)));
+		this.server.waitForMessage();
+		assertThat(this.server.messagesReceived().get(0))
+				.isEqualTo("gauge.foo 3 1488405");
+	}
 
-    @Test
-    public void periodPrefix() throws Exception {
-        this.writer = new GraphiteMetricWriter("my.", "localhost", this.port);
-        this.writer.set(new Metric<Long>("gauge.foo", 3L, new Date(1488405805)));
-        this.server.waitForMessage();
-        assertThat(this.server.messagesReceived().get(0)).isEqualTo("my.gauge.foo 3 1488405");
-    }
+	@Test
+	public void periodPrefix() throws Exception {
+		this.writer = new GraphiteMetricWriter("my.", "localhost", this.port);
+		this.writer.set(new Metric<Long>("gauge.foo", 3L, new Date(1488405805)));
+		this.server.waitForMessage();
+		assertThat(this.server.messagesReceived().get(0))
+				.isEqualTo("my.gauge.foo 3 1488405");
+	}
 
-    private static final class DummyGraphiteServer implements Runnable {
+	private static final class DummyGraphiteServer implements Runnable {
 
-        private final List<String> messagesReceived = new ArrayList<>();
+		private final List<String> messagesReceived = new ArrayList<>();
 
-        private final ServerSocket server;
+		private final ServerSocket server;
 
-        DummyGraphiteServer(int port) {
-            try {
-                this.server = new ServerSocket(port);
-            } catch (IOException e) {
-                throw new IllegalStateException(e);
-            }
-            new Thread(this).start();
-        }
+		DummyGraphiteServer(int port) {
+			try {
+				this.server = new ServerSocket(port);
+			}
+			catch (IOException e) {
+				throw new IllegalStateException(e);
+			}
+			new Thread(this).start();
+		}
 
-        void stop() {
-            try {
-                this.server.close();
-            } catch (IOException e) {
-                throw new IllegalStateException(e);
-            }
-        }
+		void stop() {
+			try {
+				this.server.close();
+			}
+			catch (IOException e) {
+				throw new IllegalStateException(e);
+			}
+		}
 
-        @Override
-        public void run() {
-            try (Socket socket = this.server.accept();
-                 BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()))) {
+		@Override
+		public void run() {
+			try (Socket socket = this.server.accept();
+					BufferedReader in = new BufferedReader(
+							new InputStreamReader(socket.getInputStream()))) {
 
-                String serverResponse;
-                while ((serverResponse = in.readLine()) != null) {
-                    this.messagesReceived.add(serverResponse);
-                }
-            }
-            catch (IOException e) {
-                e.printStackTrace();
-            }
-        }
+				String serverResponse;
+				while ((serverResponse = in.readLine()) != null) {
+					this.messagesReceived.add(serverResponse);
+				}
+			}
+			catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
 
-        void waitForMessage() {
-            while (this.messagesReceived.isEmpty()) {
-                try {
-                    Thread.sleep(50L);
-                }
-                catch (InterruptedException e) {
-                    Thread.currentThread().interrupt();
-                }
-            }
-        }
+		void waitForMessage() {
+			while (this.messagesReceived.isEmpty()) {
+				try {
+					Thread.sleep(50L);
+				}
+				catch (InterruptedException e) {
+					Thread.currentThread().interrupt();
+				}
+			}
+		}
 
-        List<String> messagesReceived() {
-            return new ArrayList<>(this.messagesReceived);
-        }
+		List<String> messagesReceived() {
+			return new ArrayList<>(this.messagesReceived);
+		}
 
-    }
+	}
 }
