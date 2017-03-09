e7922a574a39fc2222ad51b5aa97b516796ea942
Stephane Nicoll <snicoll@pivotal.io>
2017-03-09 18:33:13 +0100

    Polish

--- spring-boot-autoconfigure/src/test/java/org/springframework/boot/autoconfigure/cache/CacheAutoConfigurationTests.java
+++ spring-boot-autoconfigure/src/test/java/org/springframework/boot/autoconfigure/cache/CacheAutoConfigurationTests.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2012-2016 the original author or authors.
+ * Copyright 2012-2017 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -667,7 +667,7 @@ public class CacheAutoConfigurationTests {
 	}
 
 	@Test
-	public void jCacheCacheWithCachesAndCustomizer() { // this one
+	public void jCacheCacheWithCachesAndCustomizer() {
 		String cachingProviderFqn = HazelcastCachingProvider.class.getName();
 		try {
 			load(JCacheWithCustomizerConfiguration.class, "spring.cache.type=jcache",
