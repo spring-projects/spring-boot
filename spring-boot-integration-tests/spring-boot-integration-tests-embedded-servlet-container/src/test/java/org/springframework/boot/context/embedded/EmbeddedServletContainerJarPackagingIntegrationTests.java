2a875aca8c75798eafa24b5c6108ccd7393ef355
Johnny Lim <izeye@naver.com>
2017-03-09 22:07:27 +0900

    Remove redundant tests

--- spring-boot-integration-tests/spring-boot-integration-tests-embedded-servlet-container/src/test/java/org/springframework/boot/context/embedded/EmbeddedServletContainerJarPackagingIntegrationTests.java
+++ spring-boot-integration-tests/spring-boot-integration-tests-embedded-servlet-container/src/test/java/org/springframework/boot/context/embedded/EmbeddedServletContainerJarPackagingIntegrationTests.java
@@ -54,13 +54,6 @@ public class EmbeddedServletContainerJarPackagingIntegrationTests
 	}
 
 	@Test
-	public void nestedMetaInfResourceIsAvailableViaServletContext() throws Exception {
-		ResponseEntity<String> entity = this.rest
-				.getForEntity("/nested-meta-inf-resource.txt", String.class);
-		assertThat(entity.getStatusCode()).isEqualTo(HttpStatus.OK);
-	}
-
-	@Test
 	public void nestedJarIsNotAvailableViaHttp() throws Exception {
 		ResponseEntity<String> entity = this.rest
 				.getForEntity("/BOOT-INF/lib/resources-1.0.jar", String.class);
