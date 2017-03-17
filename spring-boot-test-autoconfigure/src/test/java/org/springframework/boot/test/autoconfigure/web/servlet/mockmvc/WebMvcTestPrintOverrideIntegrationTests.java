356b78b77f14a59b39eef3c484702346c805c93c
Madhura Bhave <mbhave@pivotal.io>
2017-03-17 10:29:35 -0700

    Stop autoconfiguration from being scanned in tests These tests used to pass even without explicitly adding the autoconfigure annotation. Moving them to a subpackage prevents scanning of the autoconfiguration by component scanning.
    Closes gh-8645

---
+++ spring-boot-test-autoconfigure/src/test/java/org/springframework/boot/test/autoconfigure/web/servlet/mockmvc/WebMvcTestPrintOverrideIntegrationTests.java
@@ -0,0 +1,59 @@
+/*
+ * Copyright 2012-2017 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.springframework.boot.test.autoconfigure.web.servlet.mockmvc;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.autoconfigure.web.servlet.MockMvcPrint;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.rule.OutputCapture;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+/**
+ * Tests for {@link WebMvcTest} when a specific print option is defined.
+ *
+ * @author Phillip Webb
+ */
+@RunWith(SpringRunner.class)
+@WebMvcTest
+@AutoConfigureMockMvc(secure = false, print = MockMvcPrint.NONE)
+public class WebMvcTestPrintOverrideIntegrationTests {
+
+	@Rule
+	public OutputCapture output = new OutputCapture();
+
+	@Autowired
+	private MockMvc mvc;
+
+	@Test
+	public void shouldNotPrint() throws Exception {
+		this.mvc.perform(get("/one")).andExpect(content().string("one"))
+				.andExpect(status().isOk());
+		assertThat(this.output.toString()).doesNotContain("Request URI = /one");
+	}
+
+}
