83555eeba66137582665a0607fd0a4fa1ec7c9fa
igreenfi <igreenfi@cisco.com>
2017-04-19 10:45:36 +0300

    [JUL] Add delete logger configuration. Add test.

--- spring-boot/src/test/java/org/springframework/boot/logging/java/JavaLoggingSystemTests.java
+++ spring-boot/src/test/java/org/springframework/boot/logging/java/JavaLoggingSystemTests.java
@@ -169,6 +169,19 @@ public class JavaLoggingSystemTests extends AbstractLoggingSystemTests {
 	}
 
 	@Test
+	public void deleteLevel() throws Exception {
+		this.loggingSystem.beforeInitialize();
+		this.loggingSystem.initialize(null, null, null);
+		this.logger.debug("Hello");
+		this.loggingSystem.setLogLevel("org.springframework.boot", LogLevel.DEBUG);
+		this.logger.debug("Hello");
+		this.loggingSystem.deleteLoggerConfiguration("org.springframework.boot");
+		this.logger.debug("Hello");
+		assertThat(StringUtils.countOccurrencesOf(this.output.toString(), "Hello"))
+				.isEqualTo(1);
+	}
+
+	@Test
 	public void getLoggingConfigurations() throws Exception {
 		this.loggingSystem.beforeInitialize();
 		this.loggingSystem.initialize(null, null, null);
