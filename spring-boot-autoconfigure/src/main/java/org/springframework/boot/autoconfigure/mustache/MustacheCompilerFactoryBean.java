ea33cc2f7d33f806459b44377a2684286c5bf676
Stephane Nicoll <snicoll@pivotal.io>
2017-05-03 13:09:52 +0200

    Deprecated MustacheCompilerFactoryBean
    Closes gh-8999

--- spring-boot-autoconfigure/src/main/java/org/springframework/boot/autoconfigure/mustache/MustacheCompilerFactoryBean.java
+++ spring-boot-autoconfigure/src/main/java/org/springframework/boot/autoconfigure/mustache/MustacheCompilerFactoryBean.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2012-2015 the original author or authors.
+ * Copyright 2012-2017 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -33,7 +33,9 @@ import org.springframework.beans.factory.FactoryBean;
  * @author Dave Syer
  * @since 1.2.2
  * @see MustacheResourceTemplateLoader
+ * @deprecated  as of 1.5
  */
+@Deprecated
 public class MustacheCompilerFactoryBean implements FactoryBean<Mustache.Compiler> {
 
 	private String delims;
