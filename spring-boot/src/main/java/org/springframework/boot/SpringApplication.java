3e5caebae599b3f0963bcea2aa92dfe743fbbfa3
Suhan <suhankoh@gmail.com>
2017-05-06 12:45:12 -0700

    Formatting into single line.

--- spring-boot/src/main/java/org/springframework/boot/SpringApplication.java
+++ spring-boot/src/main/java/org/springframework/boot/SpringApplication.java
@@ -545,8 +545,7 @@ public class SpringApplication {
 			if (sources.contains(name)) {
 				PropertySource<?> source = sources.get(name);
 				CompositePropertySource composite = new CompositePropertySource(name);
-				composite.addPropertySource(new SimpleCommandLinePropertySource(
-						name, args));
+				composite.addPropertySource(new SimpleCommandLinePropertySource(name, args));
 				composite.addPropertySource(source);
 				sources.replace(name, composite);
 			}
