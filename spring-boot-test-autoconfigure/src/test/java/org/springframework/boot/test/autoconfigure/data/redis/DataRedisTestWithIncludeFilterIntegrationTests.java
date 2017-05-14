ddb951809758646e5d61ca6bbabea3ea73bc7aa6
Jayaram Pradhan <jayaramimca@gmail.com>
2017-05-14 07:51:20 +0530

    Add Slice test annotation for Redis
    This commit adds new annotation `@DataRedisTest` which provides test
    infrastructure for redis.

---
+++ spring-boot-test-autoconfigure/src/test/java/org/springframework/boot/test/autoconfigure/data/redis/DataRedisTestWithIncludeFilterIntegrationTests.java
@@ -0,0 +1,70 @@
+/*
+ * Copyright 2012-2017 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.springframework.boot.test.autoconfigure.data.redis;
+
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import redis.embedded.RedisServer;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.ComponentScan.Filter;
+import org.springframework.stereotype.Service;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+/**
+ * Integration test with custom include filter for {@link DataRedisTest}.
+ *
+ * @author Jayaram Pradhan
+ */
+@RunWith(SpringRunner.class)
+@DataRedisTest(includeFilters = @Filter(Service.class))
+public class DataRedisTestWithIncludeFilterIntegrationTests {
+
+	@Autowired
+	private ExampleRepository exampleRepository;
+
+	@Autowired
+	private ExampleService service;
+
+	private static RedisServer redisServer;
+
+	@BeforeClass
+	public static void setUp() throws Exception {
+		redisServer = new RedisServer(6379);
+		redisServer.start();
+	}
+
+	@AfterClass
+	public static void tearDown() throws Exception {
+		redisServer.stop();
+	}
+
+	@Test
+	public void testService() {
+		PersonHash personHash = new PersonHash();
+		personHash.setDescription("Look, new @DataRedisTest!");
+		assertThat(personHash.getId()).isNull();
+		PersonHash savedEntity = this.exampleRepository.save(personHash);
+		assertThat(this.service.hasRecord(savedEntity)).isTrue();
+		this.exampleRepository.deleteAll();
+	}
+
+}
