b2e3c5dd617b0c2fd0480f7489afc24daf3118f1
Stephane Nicoll <snicoll@pivotal.io>
2017-05-14 17:48:07 +0200

    Fix order of connection pools in DataSourceBuilder
    Closes gh-6013

--- spring-boot-autoconfigure/src/test/java/org/springframework/boot/autoconfigure/jdbc/DataSourceInitializerTests.java
+++ spring-boot-autoconfigure/src/test/java/org/springframework/boot/autoconfigure/jdbc/DataSourceInitializerTests.java
@@ -95,9 +95,7 @@ public class DataSourceInitializerTests {
 	public void testTwoDataSources() throws Exception {
 		EnvironmentTestUtils.addEnvironment(this.context,
 				"datasource.one.url=jdbc:hsqldb:mem:/one",
-				"datasource.one.driverClassName=org.hsqldb.Driver",
-				"datasource.two.url=jdbc:hsqldb:mem:/two",
-				"datasource.two.driverClassName=org.hsqldb.Driver");
+				"datasource.two.url=jdbc:hsqldb:mem:/two");
 		this.context.register(TwoDataSources.class, DataSourceInitializer.class,
 				PropertyPlaceholderAutoConfiguration.class, DataSourceProperties.class);
 		this.context.refresh();
