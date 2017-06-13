8eb4eeee54b9b223b0c3f2b04a4f4ee798e9c5e0
孙健 <jian.sun.arch@dianrong.com>
2017-06-13 10:42:11 +0800

    should check the added one, and not the metric var, it will throw exception under concurrent

--- spring-boot-actuator/src/main/java/org/springframework/boot/actuate/metrics/dropwizard/DropwizardMetricServices.java
+++ spring-boot-actuator/src/main/java/org/springframework/boot/actuate/metrics/dropwizard/DropwizardMetricServices.java
@@ -148,7 +148,7 @@ public class DropwizardMetricServices implements CounterService, GaugeService {
 		}
 		catch (IllegalArgumentException ex) {
 			Metric added = this.registry.getMetrics().get(name);
-			registrar.checkExisting(metric);
+			registrar.checkExisting(added);
 			return (T) added;
 		}
 	}
