166545ae14342061a3bd9678c81336fa1ce94b86
Eddú Meléndez <eddu.melendez@gmail.com>
2017-06-20 20:40:17 -0500

    Add support to RestDocs to work with ResAssured
    See gh-7228

--- spring-boot-test-autoconfigure/src/test/java/org/springframework/boot/test/autoconfigure/restdocs/RestDocsTestApplication.java
+++ spring-boot-test-autoconfigure/src/test/java/org/springframework/boot/test/autoconfigure/restdocs/RestDocsTestApplication.java
@@ -17,13 +17,14 @@
 package org.springframework.boot.test.autoconfigure.restdocs;
 
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.SecurityAutoConfiguration;
 
 /**
  * Test application used with {@link AutoConfigureRestDocs} tests.
  *
  * @author Andy Wilkinson
  */
-@SpringBootApplication
+@SpringBootApplication(exclude = SecurityAutoConfiguration.class)
 public class RestDocsTestApplication {
 
 }
