f2e77e46cd12a9eb5a7d701cb4612bdd41c613b0
Brian Clozel <bclozel@pivotal.io>
2017-06-23 12:05:13 +0200

    Auto-configure codecs in WebTestClient
    This commit applies what's been done in gh-9166 for WebFlux client and
    server, but for the `WebTestClient` auto-configuration.
    
    `WebTestClient` can be configured for mock or integration tests and this
    change applies `CodecCustomizer` beans to the client being built.
    
    Closes gh-9577

--- spring-boot-test-autoconfigure/src/main/java/org/springframework/boot/test/autoconfigure/web/reactive/WebTestClientAutoConfiguration.java
+++ spring-boot-test-autoconfigure/src/main/java/org/springframework/boot/test/autoconfigure/web/reactive/WebTestClientAutoConfiguration.java
@@ -16,12 +16,19 @@
 
 package org.springframework.boot.test.autoconfigure.web.reactive;
 
+import java.util.Collection;
+
+import org.springframework.boot.autoconfigure.AutoConfigureAfter;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
+import org.springframework.boot.autoconfigure.http.codec.CodecsAutoConfiguration;
+import org.springframework.boot.web.codec.CodecCustomizer;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.test.web.reactive.server.WebTestClient;
+import org.springframework.util.CollectionUtils;
+import org.springframework.web.reactive.function.client.ExchangeStrategies;
 import org.springframework.web.reactive.function.client.WebClient;
 
 /**
@@ -31,13 +38,32 @@ import org.springframework.web.reactive.function.client.WebClient;
  * @since 2.0.0
  */
 @Configuration
-@ConditionalOnClass({ WebClient.class, WebTestClient.class })
+@ConditionalOnClass({WebClient.class, WebTestClient.class})
+@AutoConfigureAfter(CodecsAutoConfiguration.class)
 public class WebTestClientAutoConfiguration {
 
 	@Bean
 	@ConditionalOnMissingBean
 	public WebTestClient webTestClient(ApplicationContext applicationContext) {
-		return WebTestClient.bindToApplicationContext(applicationContext).build();
+
+		WebTestClient.Builder clientBuilder = WebTestClient
+				.bindToApplicationContext(applicationContext).configureClient();
+		customizeWebTestClientCodecs(clientBuilder, applicationContext);
+		return clientBuilder.build();
+	}
+
+	private void customizeWebTestClientCodecs(WebTestClient.Builder clientBuilder,
+			ApplicationContext applicationContext) {
+
+		Collection<CodecCustomizer> codecCustomizers = applicationContext
+				.getBeansOfType(CodecCustomizer.class).values();
+		if (!CollectionUtils.isEmpty(codecCustomizers)) {
+			clientBuilder.exchangeStrategies(ExchangeStrategies.builder()
+					.codecs(codecs -> {
+						codecCustomizers.forEach(codecCustomizer -> codecCustomizer.customize(codecs));
+					})
+					.build());
+		}
 	}
 
 }
