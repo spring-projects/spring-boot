51c2b049c21582fcf9db82244063e67196cabad5
Phillip Webb <pwebb@pivotal.io>
2017-07-13 13:43:50 -0700

    Fix CodecsAutoConfiguration following API change
    Update CodecsAutoConfiguration to align with the latest Spring Framework
    changes.

--- spring-boot-autoconfigure/src/main/java/org/springframework/boot/autoconfigure/http/codec/CodecsAutoConfiguration.java
+++ spring-boot-autoconfigure/src/main/java/org/springframework/boot/autoconfigure/http/codec/CodecsAutoConfiguration.java
@@ -55,9 +55,9 @@ public class CodecsAutoConfiguration {
 		public CodecCustomizer jacksonCodecCustomizer(ObjectMapper objectMapper) {
 			return (configurer) -> {
 				CodecConfigurer.DefaultCodecs defaults = configurer.defaultCodecs();
-				defaults.jackson2Decoder(
+				defaults.jackson2JsonDecoder(
 						new Jackson2JsonDecoder(objectMapper, EMPTY_MIME_TYPES));
-				defaults.jackson2Encoder(
+				defaults.jackson2JsonEncoder(
 						new Jackson2JsonEncoder(objectMapper, EMPTY_MIME_TYPES));
 			};
 		}
