4990b52b721913d9566e7cafd3bf7a6a196e23f1
Madhura Bhave <mbhave@pivotal.io>
2017-08-27 21:27:46 -0700

    Reinstate CloudFoundry support for actuators
    Closes gh-9996

--- spring-boot/src/main/java/org/springframework/boot/endpoint/web/mvc/WebEndpointServletHandlerMapping.java
+++ spring-boot/src/main/java/org/springframework/boot/endpoint/web/mvc/WebEndpointServletHandlerMapping.java
@@ -21,38 +21,25 @@ import java.util.Arrays;
 import java.util.Collection;
 import java.util.Collections;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 
 import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
 
-import org.springframework.beans.factory.InitializingBean;
 import org.springframework.boot.endpoint.EndpointInfo;
 import org.springframework.boot.endpoint.OperationInvoker;
 import org.springframework.boot.endpoint.ParameterMappingException;
 import org.springframework.boot.endpoint.web.EndpointLinksResolver;
 import org.springframework.boot.endpoint.web.Link;
-import org.springframework.boot.endpoint.web.OperationRequestPredicate;
 import org.springframework.boot.endpoint.web.WebEndpointOperation;
 import org.springframework.boot.endpoint.web.WebEndpointResponse;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.util.ReflectionUtils;
-import org.springframework.util.StringUtils;
-import org.springframework.web.accept.PathExtensionContentNegotiationStrategy;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.servlet.HandlerMapping;
-import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
-import org.springframework.web.servlet.mvc.condition.ConsumesRequestCondition;
-import org.springframework.web.servlet.mvc.condition.PatternsRequestCondition;
-import org.springframework.web.servlet.mvc.condition.ProducesRequestCondition;
-import org.springframework.web.servlet.mvc.condition.RequestMethodsRequestCondition;
-import org.springframework.web.servlet.mvc.method.RequestMappingInfo;
 import org.springframework.web.servlet.mvc.method.RequestMappingInfoHandlerMapping;
 
 /**
@@ -62,8 +49,7 @@ import org.springframework.web.servlet.mvc.method.RequestMappingInfoHandlerMappi
  * @author Andy Wilkinson
  * @since 2.0.0
  */
-public class WebEndpointServletHandlerMapping extends RequestMappingInfoHandlerMapping
-		implements InitializingBean {
+public class WebEndpointServletHandlerMapping extends AbstractWebEndpointServletHandlerMapping {
 
 	private final Method handle = ReflectionUtils.findMethod(OperationHandler.class,
 			"handle", HttpServletRequest.class, Map.class);
@@ -72,12 +58,6 @@ public class WebEndpointServletHandlerMapping extends RequestMappingInfoHandlerM
 			WebEndpointServletHandlerMapping.class, "links", HttpServletRequest.class);
 
 	private final EndpointLinksResolver endpointLinksResolver = new EndpointLinksResolver();
-
-	private final String endpointPath;
-
-	private final Collection<EndpointInfo<WebEndpointOperation>> webEndpoints;
-
-	private final CorsConfiguration corsConfiguration;
 
 	/**
 	 * Creates a new {@code WebEndpointHandlerMapping} that provides mappings for the
@@ -100,87 +80,23 @@ public class WebEndpointServletHandlerMapping extends RequestMappingInfoHandlerM
 	public WebEndpointServletHandlerMapping(String endpointPath,
 			Collection<EndpointInfo<WebEndpointOperation>> webEndpoints,
 			CorsConfiguration corsConfiguration) {
-		this.endpointPath = (endpointPath.startsWith("/") ? "" : "/") + endpointPath;
-		this.webEndpoints = webEndpoints;
-		this.corsConfiguration = corsConfiguration;
+		super(endpointPath, webEndpoints, corsConfiguration);
 		setOrder(-100);
 	}
 
-	public Collection<EndpointInfo<WebEndpointOperation>> getEndpoints() {
-		return this.webEndpoints;
-	}
-
-	public String getEndpointPath() {
-		return this.endpointPath;
-	}
-
-	@Override
-	protected void initHandlerMethods() {
-		this.webEndpoints.stream()
-				.flatMap((webEndpoint) -> webEndpoint.getOperations().stream())
-				.forEach(this::registerMappingForOperation);
-		registerMapping(new RequestMappingInfo(patternsRequestConditionForPattern(""),
-				new RequestMethodsRequestCondition(RequestMethod.GET), null, null, null,
-				null, null), this, this.links);
-	}
-
-	@Override
-	protected CorsConfiguration initCorsConfiguration(Object handler, Method method,
-			RequestMappingInfo mapping) {
-		return this.corsConfiguration;
-	}
-
-	private void registerMappingForOperation(WebEndpointOperation operation) {
+	protected void registerMappingForOperation(WebEndpointOperation operation) {
 		registerMapping(createRequestMappingInfo(operation),
 				new OperationHandler(operation.getOperationInvoker()), this.handle);
 	}
 
-	private RequestMappingInfo createRequestMappingInfo(
-			WebEndpointOperation operationInfo) {
-		OperationRequestPredicate requestPredicate = operationInfo.getRequestPredicate();
-		return new RequestMappingInfo(null,
-				patternsRequestConditionForPattern(requestPredicate.getPath()),
-				new RequestMethodsRequestCondition(
-						RequestMethod.valueOf(requestPredicate.getHttpMethod().name())),
-				null, null,
-				new ConsumesRequestCondition(
-						toStringArray(requestPredicate.getConsumes())),
-				new ProducesRequestCondition(
-						toStringArray(requestPredicate.getProduces())),
-				null);
-	}
-
-	private PatternsRequestCondition patternsRequestConditionForPattern(String path) {
-		return new PatternsRequestCondition(
-				new String[] { this.endpointPath
-						+ (StringUtils.hasText(path) ? "/" + path : "") },
-				null, null, false, false);
-	}
-
-	private String[] toStringArray(Collection<String> collection) {
-		return collection.toArray(new String[collection.size()]);
-	}
-
-	@Override
-	protected boolean isHandler(Class<?> beanType) {
-		return false;
-	}
-
-	@Override
-	protected RequestMappingInfo getMappingForMethod(Method method,
-			Class<?> handlerType) {
-		return null;
-	}
-
-	@Override
-	protected void extendInterceptors(List<Object> interceptors) {
-		interceptors.add(new SkipPathExtensionContentNegotiation());
+	protected Method getLinks() {
+		return this.links;
 	}
 
 	@ResponseBody
 	private Map<String, Map<String, Link>> links(HttpServletRequest request) {
 		return Collections.singletonMap("_links", this.endpointLinksResolver
-				.resolveLinks(this.webEndpoints, request.getRequestURL().toString()));
+				.resolveLinks(getEndpoints(), request.getRequestURL().toString()));
 	}
 
 	/**
@@ -225,25 +141,6 @@ public class WebEndpointServletHandlerMapping extends RequestMappingInfoHandlerM
 			WebEndpointResponse<?> response = (WebEndpointResponse<?>) result;
 			return new ResponseEntity<Object>(response.getBody(),
 					HttpStatus.valueOf(response.getStatus()));
-		}
-
-	}
-
-	/**
-	 * {@link HandlerInterceptorAdapter} to ensure that
-	 * {@link PathExtensionContentNegotiationStrategy} is skipped for web endpoints.
-	 */
-	private static final class SkipPathExtensionContentNegotiation
-			extends HandlerInterceptorAdapter {
-
-		private static final String SKIP_ATTRIBUTE = PathExtensionContentNegotiationStrategy.class
-				.getName() + ".SKIP";
-
-		@Override
-		public boolean preHandle(HttpServletRequest request, HttpServletResponse response,
-				Object handler) throws Exception {
-			request.setAttribute(SKIP_ATTRIBUTE, Boolean.TRUE);
-			return true;
 		}
 
 	}
