7388b4b76881e97b9e80396c847aae4f6f72c4f3
Stephane Nicoll <snicoll@pivotal.io>
2017-09-06 11:42:42 +0200

    Fix build failure

--- spring-boot-actuator/src/main/java/org/springframework/boot/actuate/health/CompositeReactiveHealthIndicator.java
+++ spring-boot-actuator/src/main/java/org/springframework/boot/actuate/health/CompositeReactiveHealthIndicator.java
@@ -94,7 +94,7 @@ public class CompositeReactiveHealthIndicator implements ReactiveHealthIndicator
 	public Mono<Health> health() {
 		return Flux.fromIterable(this.indicators.entrySet())
 				.flatMap((entry) -> Mono.just(entry.getKey())
-						.and(entry.getValue().health().compose(this.timeoutCompose)))
+						.zipWith(entry.getValue().health().compose(this.timeoutCompose)))
 				.collectMap(Tuple2::getT1, Tuple2::getT2)
 				.map(this.healthAggregator::aggregate);
 	}
