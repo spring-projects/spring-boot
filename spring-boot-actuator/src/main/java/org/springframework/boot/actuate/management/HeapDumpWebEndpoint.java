6c6ce7221a076e49b5e1647bc896ade45bf1883e
Vedran Pavic <vedran.pavic@gmail.com>
2017-09-19 19:49:50 +0200

    Remove usage of `HttpStatus` in Web Endpoints
    See gh-10350

--- spring-boot-actuator/src/main/java/org/springframework/boot/actuate/management/HeapDumpWebEndpoint.java
+++ spring-boot-actuator/src/main/java/org/springframework/boot/actuate/management/HeapDumpWebEndpoint.java
@@ -42,7 +42,6 @@ import org.springframework.boot.actuate.endpoint.annotation.ReadOperation;
 import org.springframework.boot.actuate.endpoint.web.WebEndpointResponse;
 import org.springframework.core.io.FileSystemResource;
 import org.springframework.core.io.Resource;
-import org.springframework.http.HttpStatus;
 import org.springframework.util.ClassUtils;
 import org.springframework.util.ReflectionUtils;
 
@@ -89,12 +88,14 @@ public class HeapDumpWebEndpoint {
 			Thread.currentThread().interrupt();
 		}
 		catch (IOException ex) {
-			return new WebEndpointResponse<>(HttpStatus.INTERNAL_SERVER_ERROR.value());
+			return new WebEndpointResponse<>(
+					WebEndpointResponse.INTERNAL_SERVER_ERROR_STATUS);
 		}
 		catch (HeapDumperUnavailableException ex) {
-			return new WebEndpointResponse<>(HttpStatus.SERVICE_UNAVAILABLE.value());
+			return new WebEndpointResponse<>(
+					WebEndpointResponse.SERVICE_UNAVAILABLE_STATUS);
 		}
-		return new WebEndpointResponse<>(HttpStatus.TOO_MANY_REQUESTS.value());
+		return new WebEndpointResponse<>(WebEndpointResponse.TOO_MANY_REQUESTS_STATUS);
 	}
 
 	private Resource dumpHeap(boolean live) throws IOException, InterruptedException {
