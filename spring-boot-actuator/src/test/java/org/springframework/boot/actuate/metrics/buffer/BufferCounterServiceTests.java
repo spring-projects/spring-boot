aca30950cf468dfabfac28594a5508aa297c934f
Andy Wilkinson <awilkinson@pivotal.io>
2017-09-20 06:51:22 +0100

    Polish "Align prefix match in BufferCounterService with DefaultCounterService"
    See gh-10278

--- spring-boot-actuator/src/test/java/org/springframework/boot/actuate/metrics/buffer/BufferCounterServiceTests.java
+++ spring-boot-actuator/src/test/java/org/springframework/boot/actuate/metrics/buffer/BufferCounterServiceTests.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2017 the original author or authors.
+ * Copyright 2012-2017 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -21,12 +21,10 @@ import org.junit.Test;
 import org.springframework.boot.actuate.metrics.CounterService;
 import org.springframework.boot.actuate.metrics.Metric;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertNotNull;
-import static org.junit.Assert.assertNull;
+import static org.assertj.core.api.Assertions.assertThat;
 
 /**
- * Standard tests for {@link BufferCounterService}.
+ * Tests for {@link BufferCounterService}.
  *
  * @author Venil Noronha
  */
@@ -36,56 +34,52 @@ public class BufferCounterServiceTests {
 
 	private CounterService service = new BufferCounterService(this.counters);
 
-	private BufferMetricReader reader = new BufferMetricReader(this.counters, new GaugeBuffers());
+	private BufferMetricReader reader = new BufferMetricReader(this.counters,
+			new GaugeBuffers());
 
 	@Test
 	public void matchExtendedPrefix() {
 		this.service.increment("foo");
-		Metric<?> fooMetric = this.reader.findOne("foo");
+		assertThat(this.reader.findOne("foo")).isNull();
 		Metric<?> counterFooMetric = this.reader.findOne("counter.foo");
-		assertNull(fooMetric);
-		assertNotNull(counterFooMetric);
-		assertEquals(1L, counterFooMetric.getValue());
+		assertThat(counterFooMetric).isNotNull();
+		assertThat(counterFooMetric.getValue()).isEqualTo(1L);
 	}
 
 	@Test
 	public void matchCounterPrefix() {
 		this.service.increment("counterfoo");
-		Metric<?> counterfooMetric = this.reader.findOne("counterfoo");
+		assertThat(this.reader.findOne("counterfoo")).isNull();
 		Metric<?> counterCounterfooMetric = this.reader.findOne("counter.counterfoo");
-		assertNull(counterfooMetric);
-		assertNotNull(counterCounterfooMetric);
-		assertEquals(1L, counterCounterfooMetric.getValue());
+		assertThat(counterCounterfooMetric).isNotNull();
+		assertThat(counterCounterfooMetric.getValue()).isEqualTo(1L);
 	}
 
 	@Test
 	public void matchCounterDotPrefix() {
 		this.service.increment("counter.foo");
+		assertThat(this.reader.findOne("counter.counter.foo")).isNull();
 		Metric<?> counterFooMetric = this.reader.findOne("counter.foo");
-		Metric<?> counterCounterFooMetric = this.reader.findOne("counter.counter.foo");
-		assertNull(counterCounterFooMetric);
-		assertNotNull(counterFooMetric);
-		assertEquals(1L, counterFooMetric.getValue());
+		assertThat(counterFooMetric).isNotNull();
+		assertThat(counterFooMetric.getValue()).isEqualTo(1L);
 	}
 
 	@Test
 	public void matchMeterPrefix() {
 		this.service.increment("meterfoo");
-		Metric<?> meterfooMetric = this.reader.findOne("meterfoo");
+		assertThat(this.reader.findOne("meterfoo")).isNull();
 		Metric<?> counterMeterfooMetric = this.reader.findOne("counter.meterfoo");
-		assertNull(meterfooMetric);
-		assertNotNull(counterMeterfooMetric);
-		assertEquals(1L, counterMeterfooMetric.getValue());
+		assertThat(counterMeterfooMetric).isNotNull();
+		assertThat(counterMeterfooMetric.getValue()).isEqualTo(1L);
 	}
 
 	@Test
 	public void matchMeterDotPrefix() {
 		this.service.increment("meter.foo");
+		assertThat(this.reader.findOne("counter.meter.foo")).isNull();
 		Metric<?> meterFooMetric = this.reader.findOne("meter.foo");
-		Metric<?> counterMeterFooMetric = this.reader.findOne("counter.meter.foo");
-		assertNull(counterMeterFooMetric);
-		assertNotNull(meterFooMetric);
-		assertEquals(1L, meterFooMetric.getValue());
+		assertThat(meterFooMetric).isNotNull();
+		assertThat(meterFooMetric.getValue()).isEqualTo(1L);
 	}
 
 }
