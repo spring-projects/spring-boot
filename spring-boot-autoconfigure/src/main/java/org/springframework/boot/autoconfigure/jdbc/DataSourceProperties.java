d490b9947346fd673137dee40bed23ff4c640306
Stephane Nicoll <snicoll@pivotal.io>
2017-09-25 10:04:37 +0200

    Clarify scope of spring.datasource.platform
    Closes gh-10416

--- spring-boot-autoconfigure/src/main/java/org/springframework/boot/autoconfigure/jdbc/DataSourceProperties.java
+++ spring-boot-autoconfigure/src/main/java/org/springframework/boot/autoconfigure/jdbc/DataSourceProperties.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2012-2016 the original author or authors.
+ * Copyright 2012-2017 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -102,7 +102,8 @@ public class DataSourceProperties
 	private boolean initialize = true;
 
 	/**
-	 * Platform to use in the schema resource (schema-${platform}.sql).
+	 * Platform to use in the DDL or DML scripts (e.g. schema-${platform}.sql or
+	 * data-${platform}.sql).
 	 */
 	private String platform = "all";
 
