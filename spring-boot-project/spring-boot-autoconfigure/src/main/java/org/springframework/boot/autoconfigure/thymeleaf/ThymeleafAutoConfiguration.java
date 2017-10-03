f70d85cd5fcd8db17859b0b8479da32d16dd2bd8
Daniel Fernández <daniel.fernandez@11thlabs.org>
2017-10-04 01:48:32 +0200

    Added configuration parameters at ThymeleafProperties for "fullModeViewNames" and "chunkedModeViewNames"

--- spring-boot-project/spring-boot-autoconfigure/src/main/java/org/springframework/boot/autoconfigure/thymeleaf/ThymeleafAutoConfiguration.java
+++ spring-boot-project/spring-boot-autoconfigure/src/main/java/org/springframework/boot/autoconfigure/thymeleaf/ThymeleafAutoConfiguration.java
@@ -264,6 +264,8 @@ public class ThymeleafAutoConfiguration {
 				resolver.setResponseMaxChunkSizeBytes(
 						this.properties.getReactive().getMaxChunkSize());
 			}
+			resolver.setFullModeViewNames(this.properties.getReactive().getFullModeViewNames());
+			resolver.setChunkedModeViewNames(this.properties.getReactive().getChunkedModeViewNames());
 			// This resolver acts as a fallback resolver (e.g. like a
 			// InternalResourceViewResolver) so it needs to have low precedence
 			resolver.setOrder(Ordered.LOWEST_PRECEDENCE - 5);
