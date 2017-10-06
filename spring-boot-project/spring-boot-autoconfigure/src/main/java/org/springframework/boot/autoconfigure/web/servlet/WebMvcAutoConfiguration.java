bb13749aa65df4ad224abf9d0b1fd09f9a958952
Bruce Brouwer <bruce.brouwer@gfs.com>
2017-10-06 15:17:26 -0400

    Auto configure templated welcome page

--- spring-boot-project/spring-boot-autoconfigure/src/main/java/org/springframework/boot/autoconfigure/web/servlet/WebMvcAutoConfiguration.java
+++ spring-boot-project/spring-boot-autoconfigure/src/main/java/org/springframework/boot/autoconfigure/web/servlet/WebMvcAutoConfiguration.java
@@ -48,6 +48,7 @@ import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnWebApplication;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnWebApplication.Type;
 import org.springframework.boot.autoconfigure.http.HttpMessageConverters;
+import org.springframework.boot.autoconfigure.template.TemplateAvailabilityProviders;
 import org.springframework.boot.autoconfigure.validation.ValidationAutoConfiguration;
 import org.springframework.boot.autoconfigure.validation.ValidatorAdapter;
 import org.springframework.boot.autoconfigure.web.ConditionalOnEnabledResourceChain;
@@ -57,6 +58,7 @@ import org.springframework.boot.context.properties.EnableConfigurationProperties
 import org.springframework.boot.web.servlet.filter.OrderedHiddenHttpMethodFilter;
 import org.springframework.boot.web.servlet.filter.OrderedHttpPutFormContentFilter;
 import org.springframework.boot.web.servlet.filter.OrderedRequestContextFilter;
+import org.springframework.context.ApplicationContext;
 import org.springframework.context.ResourceLoaderAware;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -132,6 +134,7 @@ import org.springframework.web.servlet.view.InternalResourceViewResolver;
  * @author Sébastien Deleuze
  * @author Eddú Meléndez
  * @author Stephane Nicoll
+ * @author Bruce Brouwer
  */
 @Configuration
 @ConditionalOnWebApplication(type = Type.SERVLET)
@@ -324,8 +327,9 @@ public class WebMvcAutoConfiguration {
 		}
 
 		@Bean
-		public WelcomePageHandlerMapping welcomePageHandlerMapping() {
-			return new WelcomePageHandlerMapping(getWelcomePage(),
+		public WelcomePageHandlerMapping welcomePageHandlerMapping(
+				ApplicationContext applicationContext) {
+			return new WelcomePageHandlerMapping(applicationContext, getWelcomePage(),
 					this.mvcProperties.getStaticPathPattern());
 		}
 
@@ -605,15 +609,30 @@ public class WebMvcAutoConfiguration {
 		private static final Log logger = LogFactory
 				.getLog(WelcomePageHandlerMapping.class);
 
-		private WelcomePageHandlerMapping(Optional<Resource> welcomePage,
-				String staticPathPattern) {
-			if (welcomePage.isPresent() && "/**".equals(staticPathPattern)) {
-				logger.info("Adding welcome page: " + welcomePage);
-				ParameterizableViewController controller = new ParameterizableViewController();
-				controller.setViewName("forward:index.html");
-				setRootHandler(controller);
-				setOrder(0);
+		private WelcomePageHandlerMapping(ApplicationContext applicationContext,
+				Optional<Resource> welcomePage, String staticPathPattern) {
+			if (welcomeTemplateExists(applicationContext)) {
+				logger.info("Adding welcome page template: index");
+				setRootViewName("index");
 			}
+			else if (welcomePage.isPresent() && "/**".equals(staticPathPattern)) {
+				logger.info("Adding welcome page: " + welcomePage);
+				setRootViewName("forward:index.html");
+			}
+		}
+
+		private boolean welcomeTemplateExists(
+				final ApplicationContext applicationContext) {
+			final TemplateAvailabilityProviders providers = new TemplateAvailabilityProviders(
+					applicationContext);
+			return providers.getProvider("index", applicationContext) != null;
+		}
+
+		private void setRootViewName(final String viewName) {
+			final ParameterizableViewController controller = new ParameterizableViewController();
+			controller.setViewName(viewName);
+			setRootHandler(controller);
+			setOrder(0);
 		}
 
 		@Override
