0cc33d5c7ccaa04c6d41f6193da029ed4d9d4ee1
Phillip Webb <pwebb@pivotal.io>
2017-10-08 17:09:12 -0700

    Revert class name change
    This reverts commit de7b624643d5606491d9a66e1ac2b84b8c4c23f6.

--- spring-boot-project/spring-boot-test/src/main/java/org/springframework/boot/test/web/reactive/WebTestClientContextCustomizerFactory.java
+++ spring-boot-project/spring-boot-test/src/main/java/org/springframework/boot/test/web/reactive/WebTestClientContextCustomizerFactory.java
@@ -32,7 +32,7 @@ import org.springframework.util.ClassUtils;
  */
 public class WebTestClientContextCustomizerFactory implements ContextCustomizerFactory {
 
-	private static final String WEB_TEST_CLIENT_CLASS = "org.springframework.web.reactive.function.client.WebTestClient";
+	private static final String WEB_TEST_CLIENT_CLASS = "org.springframework.web.reactive.function.client.WebClient";
 
 	@Override
 	public ContextCustomizer createContextCustomizer(Class<?> testClass,
