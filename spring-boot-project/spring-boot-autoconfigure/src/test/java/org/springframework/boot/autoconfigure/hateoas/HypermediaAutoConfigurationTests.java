f59fbe6f81996df3541a69a1827c397bbb099886
Andy Wilkinson <awilkinson@pivotal.io>
2017-10-30 17:00:47 +0000

    Update test to align with media type change in HATEOAS 0.24
    See gh-10828

--- spring-boot-project/spring-boot-autoconfigure/src/test/java/org/springframework/boot/autoconfigure/hateoas/HypermediaAutoConfigurationTests.java
+++ spring-boot-project/spring-boot-autoconfigure/src/test/java/org/springframework/boot/autoconfigure/hateoas/HypermediaAutoConfigurationTests.java
@@ -140,7 +140,8 @@ public class HypermediaAutoConfigurationTests {
 		for (HttpMessageConverter<?> converter : handlerAdapter.getMessageConverters()) {
 			if (converter instanceof TypeConstrainedMappingJackson2HttpMessageConverter) {
 				assertThat(converter.getSupportedMediaTypes())
-						.containsExactly(MediaTypes.HAL_JSON);
+						.containsExactly(MediaTypes.HAL_JSON, MediaType.parseMediaType(
+								MediaTypes.HAL_JSON_VALUE + ";charset=UTF-8"));
 			}
 		}
 	}
