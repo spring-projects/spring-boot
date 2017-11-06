283c9de28fd6cbde5e7e7e83631f1e4327bd4453
Ryon <rday@underarmour.com>
2017-11-06 13:51:03 -0600

    Improve spring-boot-autoconfigure for Redis caches
    Expose key prefix, TTL and null value settings for spring-data-redis'
    RedisCacheConfiguration in Spring .properties/yml configuration files.
    
    Example:
    
    spring.cache.redis.ttl=PT15M
    spring.cache.redis.keyPrefix=foo
    spring.cache.redis.useKeyPrefix=false
    spring.cache.redis.cacheNullValues=false
    
    Fixes gs-10795

--- spring-boot-project/spring-boot-autoconfigure/src/main/java/org/springframework/boot/autoconfigure/cache/CacheProperties.java
+++ spring-boot-project/spring-boot-autoconfigure/src/main/java/org/springframework/boot/autoconfigure/cache/CacheProperties.java
@@ -16,8 +16,10 @@
 
 package org.springframework.boot.autoconfigure.cache;
 
+import java.time.Duration;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 import java.util.concurrent.TimeUnit;
 
 import org.springframework.boot.context.properties.ConfigurationProperties;
@@ -29,6 +31,7 @@ import org.springframework.util.Assert;
  *
  * @author Stephane Nicoll
  * @author Eddú Meléndez
+ * @author Ryon Day
  * @since 1.3.0
  */
 @ConfigurationProperties(prefix = "spring.cache")
@@ -54,6 +57,8 @@ public class CacheProperties {
 	private final Infinispan infinispan = new Infinispan();
 
 	private final JCache jcache = new JCache();
+
+	private final Redis redis = new Redis();
 
 	public CacheType getType() {
 		return this.type;
@@ -89,6 +94,10 @@ public class CacheProperties {
 
 	public JCache getJcache() {
 		return this.jcache;
+	}
+
+	public Redis getRedis() {
+		return this.redis;
 	}
 
 	/**
@@ -231,6 +240,74 @@ public class CacheProperties {
 			this.config = config;
 		}
 
+	}
+
+	/**
+	 * Redis-specific cache properties. Properties set will be used as the defaults for
+	 * all Redis caches.
+	 *
+	 * @see org.springframework.data.redis.cache.RedisCacheConfiguration
+	 */
+	public static class Redis {
+
+		/**
+		 * The TTL for entries. By default, entries do not expire, a value of zero
+		 * indicates an eternal entry, and the given value will be converted to seconds..
+		 *
+		 * @see Duration#parse(CharSequence)
+		 */
+		private Duration ttl = Duration.ZERO;
+
+		/**
+		 * Whether to allow caching of {@literal null} values.
+		 */
+		private boolean cacheNullValues = true;
+
+		/**
+		 * The non-null Redis key prefix to use instead of the default one.
+		 */
+		private String keyPrefix;
+
+		/**
+		 * Whether to use the key prefix when writing to Redis.
+		 */
+		private boolean useKeyPrefix = true;
+
+		public Duration getTtl() {
+			return this.ttl;
+		}
+
+		public Redis setTtl(Duration ttl) {
+			this.ttl = ttl;
+			return this;
+		}
+
+		public boolean isCacheNullValues() {
+			return this.cacheNullValues;
+		}
+
+		public Redis setCacheNullValues(boolean cacheNullValues) {
+			this.cacheNullValues = cacheNullValues;
+			return this;
+		}
+
+		public Optional<String> getKeyPrefix() {
+			return Optional.ofNullable(this.keyPrefix);
+		}
+
+		public Redis setKeyPrefix(String keyPrefix) {
+			this.keyPrefix = keyPrefix;
+			return this;
+		}
+
+		public boolean isUseKeyPrefix() {
+			return this.useKeyPrefix;
+		}
+
+		public Redis setUseKeyPrefix(boolean useKeyPrefix) {
+			this.useKeyPrefix = useKeyPrefix;
+			return this;
+		}
 	}
 
 }
