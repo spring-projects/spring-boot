2963eb783dc9593512f391e70a8cd13883149e04
Roman Zaynetdinov <roman@zaynetro.com>
2017-11-12 12:29:37 +0200

    Add default web test client builder customizer

--- spring-boot-project/spring-boot-test-autoconfigure/src/main/java/org/springframework/boot/test/autoconfigure/web/reactive/WebTestClientAutoConfiguration.java
+++ spring-boot-project/spring-boot-test-autoconfigure/src/main/java/org/springframework/boot/test/autoconfigure/web/reactive/WebTestClientAutoConfiguration.java
@@ -16,25 +16,16 @@
 
 package org.springframework.boot.test.autoconfigure.web.reactive;
 
-import java.time.Duration;
-import java.util.Collection;
 import java.util.List;
-import java.util.function.Consumer;
 
 import org.springframework.boot.autoconfigure.AutoConfigureAfter;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.boot.autoconfigure.http.codec.CodecsAutoConfiguration;
-import org.springframework.boot.context.properties.bind.Binder;
-import org.springframework.boot.web.codec.CodecCustomizer;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.codec.ClientCodecConfigurer;
 import org.springframework.test.web.reactive.server.WebTestClient;
-import org.springframework.test.web.reactive.server.WebTestClient.Builder;
-import org.springframework.util.CollectionUtils;
-import org.springframework.web.reactive.function.client.ExchangeStrategies;
 import org.springframework.web.reactive.function.client.WebClient;
 
 /**
@@ -48,41 +39,27 @@ import org.springframework.web.reactive.function.client.WebClient;
 @AutoConfigureAfter(CodecsAutoConfiguration.class)
 public class WebTestClientAutoConfiguration {
 
+	private final ApplicationContext context;
+
+	WebTestClientAutoConfiguration(ApplicationContext context) {
+		this.context = context;
+	}
+
 	@Bean
 	@ConditionalOnMissingBean
 	public WebTestClient webTestClient(ApplicationContext applicationContext,
 		List<WebTestClientBuilderCustomizer> customizers) {
 		WebTestClient.Builder builder = WebTestClient
 				.bindToApplicationContext(applicationContext).configureClient();
-		customizeWebTestClient(builder, applicationContext);
-		customizeWebTestClientCodecs(builder, applicationContext);
 		for (WebTestClientBuilderCustomizer customizer : customizers) {
 			customizer.customize(builder);
 		}
 		return builder.build();
 	}
 
-	private void customizeWebTestClient(Builder builder,
-			ApplicationContext applicationContext) {
-		Binder.get(applicationContext.getEnvironment())
-				.bind("spring.test.webtestclient.timeout", Duration.class)
-				.ifBound(builder::responseTimeout);
-	}
-
-	private void customizeWebTestClientCodecs(WebTestClient.Builder builder,
-			ApplicationContext applicationContext) {
-		Collection<CodecCustomizer> customizers = applicationContext
-				.getBeansOfType(CodecCustomizer.class).values();
-		if (!CollectionUtils.isEmpty(customizers)) {
-			builder.exchangeStrategies(ExchangeStrategies.builder()
-					.codecs(applyCustomizers(customizers)).build());
-		}
-	}
-
-	private Consumer<ClientCodecConfigurer> applyCustomizers(
-			Collection<CodecCustomizer> customizers) {
-		return (codecs) -> customizers
-				.forEach((customizer) -> customizer.customize(codecs));
+	@Bean
+	public SpringBootWebTestClientBuilderCustomizer springBootWebTestClientBuilderCustomizer() {
+		return new SpringBootWebTestClientBuilderCustomizer(this.context);
 	}
 
 }
