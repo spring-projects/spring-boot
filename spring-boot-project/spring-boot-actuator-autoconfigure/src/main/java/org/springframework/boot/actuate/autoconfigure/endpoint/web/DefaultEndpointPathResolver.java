01a48412d2653f1d670501eb438111d9ca3c63a4
Johnny Lim <izeye@naver.com>
2017-11-23 23:04:34 +0900

    Polish
    Closes gh-11126

--- spring-boot-project/spring-boot-actuator-autoconfigure/src/main/java/org/springframework/boot/actuate/autoconfigure/endpoint/web/DefaultEndpointPathResolver.java
+++ spring-boot-project/spring-boot-actuator-autoconfigure/src/main/java/org/springframework/boot/actuate/autoconfigure/endpoint/web/DefaultEndpointPathResolver.java
@@ -22,8 +22,8 @@ import org.springframework.boot.actuate.endpoint.web.EndpointPathResolver;
 import org.springframework.core.env.Environment;
 
 /**
- * Default {@link EndpointPathResolver} implementation that use the {@link Environment} to
- * determine if an endpoint has a custom path.
+ * Default {@link EndpointPathResolver} implementation that uses the {@link Environment}
+ * to determine if an endpoint has a custom path.
  *
  * @author Stephane Nicoll
  */
