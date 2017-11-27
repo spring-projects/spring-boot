9543327406d20d45edbb13549b1ca8264f70cc35
Stephane Nicoll <snicoll@pivotal.io>
2017-11-27 10:32:28 +0100

    Polish "Use BeanIds.SPRING_SECURITY_FILTER_CHAIN"
    Closes gh-11162

--- spring-boot-project/spring-boot-autoconfigure/src/main/java/org/springframework/boot/autoconfigure/security/WebSecurityEnablerConfiguration.java
+++ spring-boot-project/spring-boot-autoconfigure/src/main/java/org/springframework/boot/autoconfigure/security/WebSecurityEnablerConfiguration.java
@@ -28,8 +28,8 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
  * {@link EnableWebSecurity} annotation. This will make sure that the annotation is
  * present with default security auto-configuration and also if the user adds custom
  * security and forgets to add the annotation. If {@link EnableWebSecurity} has already
- * been added or if a bean with name {@value BeanIds#SPRING_SECURITY_FILTER_CHAIN} has been configured by the
- * user, this will back-off.
+ * been added or if a bean with name {@value BeanIds#SPRING_SECURITY_FILTER_CHAIN} has
+ * been configured by the user, this will back-off.
  *
  * @author Madhura Bhave
  * @since 2.0.0
