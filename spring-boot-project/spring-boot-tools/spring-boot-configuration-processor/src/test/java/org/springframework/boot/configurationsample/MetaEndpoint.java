a626f5462d835f619d2a93d627be5f43e1c1383a
Stephane Nicoll <snicoll@pivotal.io>
2017-12-13 14:42:16 +0100

    Consistent use of @AliasFor
    Closes gh-10836

--- spring-boot-project/spring-boot-tools/spring-boot-configuration-processor/src/test/java/org/springframework/boot/configurationsample/MetaEndpoint.java
+++ spring-boot-project/spring-boot-tools/spring-boot-configuration-processor/src/test/java/org/springframework/boot/configurationsample/MetaEndpoint.java
@@ -22,14 +22,18 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
+import org.springframework.core.annotation.AliasFor;
+
 @Target(ElementType.TYPE)
 @Retention(RetentionPolicy.RUNTIME)
 @Documented
 @Endpoint
 public @interface MetaEndpoint {
 
+	@AliasFor(annotation = Endpoint.class)
 	String id();
 
+	@AliasFor(annotation = Endpoint.class)
 	boolean enableByDefault() default true;
 
 }
