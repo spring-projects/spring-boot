397d3e85e81e1a3dcc98d4cb1b4313b4e35238f7
Stephane Nicoll <snicoll@pivotal.io>
2018-01-02 15:45:43 +0100

    Fix Windows build
    Closes gh-11459

--- spring-boot-tools/spring-boot-loader/src/test/java/org/springframework/boot/loader/data/RandomAccessDataFileTests.java
+++ spring-boot-tools/spring-boot-loader/src/test/java/org/springframework/boot/loader/data/RandomAccessDataFileTests.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2012-2017 the original author or authors.
+ * Copyright 2012-2018 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -106,7 +106,8 @@ public class RandomAccessDataFileTests {
 	@Test
 	public void fileExists() throws Exception {
 		this.thrown.expect(IllegalArgumentException.class);
-		this.thrown.expectMessage("File /does/not/exist must exist");
+		this.thrown.expectMessage(String.format("File %s must exist",
+				new File("/does/not/exist").getAbsolutePath()));
 		new RandomAccessDataFile(new File("/does/not/exist"));
 	}
 
@@ -120,7 +121,8 @@ public class RandomAccessDataFileTests {
 	@Test
 	public void fileExistsWithConcurrentReads() throws Exception {
 		this.thrown.expect(IllegalArgumentException.class);
-		this.thrown.expectMessage("File /does/not/exist must exist");
+		this.thrown.expectMessage(String.format("File %s must exist",
+				new File("/does/not/exist").getAbsolutePath()));
 		new RandomAccessDataFile(new File("/does/not/exist"), 1);
 	}
 
