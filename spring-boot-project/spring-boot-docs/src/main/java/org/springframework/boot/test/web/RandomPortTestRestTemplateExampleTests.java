027e6baba53a83e385bc307925c654c55da346a8
Stephane Nicoll <snicoll@pivotal.io>
2018-01-10 16:15:15 +0100

    Improve documentation for WebTestClient
    Closes gh-11203

--- spring-boot-project/spring-boot-docs/src/main/java/org/springframework/boot/test/web/RandomPortTestRestTemplateExampleTests.java
+++ spring-boot-project/spring-boot-docs/src/main/java/org/springframework/boot/test/web/RandomPortTestRestTemplateExampleTests.java
@@ -30,7 +30,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
-public class RandomPortExampleTests {
+public class RandomPortTestRestTemplateExampleTests {
 
 	@Autowired
 	private TestRestTemplate restTemplate;
