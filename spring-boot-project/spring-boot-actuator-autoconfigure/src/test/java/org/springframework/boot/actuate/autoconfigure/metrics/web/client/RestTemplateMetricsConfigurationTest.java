12190a3ce0e7cd9b108e12694ce7df987aa14161
Jon Schneider <jkschneider@gmail.com>
2018-01-10 21:32:05 -0600

    Micrometer #300

---
+++ spring-boot-project/spring-boot-actuator-autoconfigure/src/test/java/org/springframework/boot/actuate/autoconfigure/metrics/web/client/RestTemplateMetricsConfigurationTest.java
@@ -0,0 +1,66 @@
+/*
+ * Copyright 2012-2017 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.springframework.boot.actuate.autoconfigure.metrics.web.client;
+
+import io.micrometer.core.instrument.MeterRegistry;
+import io.micrometer.core.instrument.simple.SimpleMeterRegistry;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.web.client.RestTemplate;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes = ClientApp.class, webEnvironment = SpringBootTest.WebEnvironment.NONE)
+public class RestTemplateMetricsConfigurationTest {
+	@Autowired
+	private RestTemplate client;
+
+	@Autowired
+	private MeterRegistry registry;
+
+	@Test
+	public void restTemplatesCreatedWithBuilderAreInstrumented() {
+		try {
+			this.client.getForObject("http://google.com", String.class);
+		}
+		catch (Throwable ignored) {
+			// doesn't matter whether the request succeeded or not
+		}
+		assertThat(this.registry.find("http.client.requests").meter()).isPresent();
+	}
+}
+
+@SpringBootApplication
+class ClientApp {
+	@Bean
+	public MeterRegistry registry() {
+		return new SimpleMeterRegistry();
+	}
+
+	@Bean
+	public RestTemplate restTemplate(RestTemplateBuilder builder) {
+		return builder.build();
+	}
+}
