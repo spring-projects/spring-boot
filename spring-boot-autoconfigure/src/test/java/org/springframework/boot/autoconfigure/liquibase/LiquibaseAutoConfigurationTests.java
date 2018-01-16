6c191e149d42931d288160cc98fadd2f74bc54e2
Stephane Nicoll <snicoll@pivotal.io>
2018-01-16 11:43:36 +0100

    Fix broken usage of LiquibaseServiceLocatorApplicationListener

--- spring-boot-autoconfigure/src/test/java/org/springframework/boot/autoconfigure/liquibase/LiquibaseAutoConfigurationTests.java
+++ spring-boot-autoconfigure/src/test/java/org/springframework/boot/autoconfigure/liquibase/LiquibaseAutoConfigurationTests.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2012-2017 the original author or authors.
+ * Copyright 2012-2018 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -30,9 +30,11 @@ import org.junit.rules.ExpectedException;
 import org.junit.rules.TemporaryFolder;
 
 import org.springframework.beans.factory.BeanCreationException;
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.context.PropertyPlaceholderAutoConfiguration;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceBuilder;
 import org.springframework.boot.autoconfigure.jdbc.EmbeddedDataSourceConfiguration;
+import org.springframework.boot.context.event.ApplicationStartingEvent;
 import org.springframework.boot.liquibase.CommonsLoggingLiquibaseLogger;
 import org.springframework.boot.liquibase.LiquibaseServiceLocatorApplicationListener;
 import org.springframework.boot.test.rule.OutputCapture;
@@ -70,7 +72,9 @@ public class LiquibaseAutoConfigurationTests {
 	public void init() {
 		EnvironmentTestUtils.addEnvironment(this.context,
 				"spring.datasource.name:liquibasetest");
-		new LiquibaseServiceLocatorApplicationListener().onApplicationEvent(null);
+		new LiquibaseServiceLocatorApplicationListener().onApplicationEvent(
+				new ApplicationStartingEvent(new SpringApplication(Object.class),
+						new String[0]));
 	}
 
 	@After
