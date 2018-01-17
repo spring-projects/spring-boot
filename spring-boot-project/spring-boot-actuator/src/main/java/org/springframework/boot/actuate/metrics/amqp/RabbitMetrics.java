9249b2355d96044c1c91af17bd8c66b151e5ce5f
Arnaud Cogoluègnes <acogoluegnes@gmail.com>
2018-01-17 14:19:03 +0100

    Use RabbitMQ Client native Micrometer support
    Spring Boot metrics support now uses the RabbitMQ Java Client 5.1.2
    Micrometer metrics collector.

--- spring-boot-project/spring-boot-actuator/src/main/java/org/springframework/boot/actuate/metrics/amqp/RabbitMetrics.java
+++ spring-boot-project/spring-boot-actuator/src/main/java/org/springframework/boot/actuate/metrics/amqp/RabbitMetrics.java
@@ -17,14 +17,9 @@
 package org.springframework.boot.actuate.metrics.amqp;
 
 import java.util.Collections;
-import java.util.concurrent.atomic.AtomicLong;
-import java.util.function.Function;
 
-import com.rabbitmq.client.Channel;
-import com.rabbitmq.client.Connection;
 import com.rabbitmq.client.ConnectionFactory;
-import com.rabbitmq.client.impl.AbstractMetricsCollector;
-import io.micrometer.core.instrument.Counter;
+import com.rabbitmq.client.impl.MicrometerMetricsCollector;
 import io.micrometer.core.instrument.MeterRegistry;
 import io.micrometer.core.instrument.Tag;
 import io.micrometer.core.instrument.binder.MeterBinder;
@@ -53,150 +48,6 @@ public class RabbitMetrics implements MeterBinder {
 	@Override
 	public void bindTo(MeterRegistry registry) {
 		this.connectionFactory.setMetricsCollector(new MicrometerMetricsCollector(registry, "rabbitmq", this.tags));
-	}
-
-	/**
-	 * Micrometer implementation of {@link com.rabbitmq.client.MetricsCollector}.
-	 */
-	public static class MicrometerMetricsCollector extends AbstractMetricsCollector {
-
-		private final AtomicLong connections;
-
-		private final AtomicLong channels;
-
-		private final Counter publishedMessages;
-
-		private final Counter consumedMessages;
-
-		private final Counter acknowledgedMessages;
-
-		private final Counter rejectedMessages;
-
-		public MicrometerMetricsCollector(MeterRegistry registry) {
-			this(registry, "rabbitmq", Collections.emptyList());
-		}
-
-		public MicrometerMetricsCollector(final MeterRegistry registry, final String prefix, Iterable<Tag> tags) {
-			this(metric -> metric.create(registry, prefix, tags));
-		}
-
-		public MicrometerMetricsCollector(Function<Metrics, Object> metricsCreator) {
-			this.connections = (AtomicLong) metricsCreator.apply(
-					org.springframework.boot.actuate.metrics.amqp.RabbitMetrics.MicrometerMetricsCollector.Metrics.CONNECTIONS);
-			this.channels = (AtomicLong) metricsCreator.apply(
-					org.springframework.boot.actuate.metrics.amqp.RabbitMetrics.MicrometerMetricsCollector.Metrics.CHANNELS);
-			this.publishedMessages = (Counter) metricsCreator.apply(
-					org.springframework.boot.actuate.metrics.amqp.RabbitMetrics.MicrometerMetricsCollector.Metrics.PUBLISHED_MESSAGES);
-			this.consumedMessages = (Counter) metricsCreator.apply(
-					org.springframework.boot.actuate.metrics.amqp.RabbitMetrics.MicrometerMetricsCollector.Metrics.CONSUMED_MESSAGES);
-			this.acknowledgedMessages = (Counter) metricsCreator.apply(
-					org.springframework.boot.actuate.metrics.amqp.RabbitMetrics.MicrometerMetricsCollector.Metrics.ACKNOWLEDGED_MESSAGES);
-			this.rejectedMessages = (Counter) metricsCreator.apply(
-					org.springframework.boot.actuate.metrics.amqp.RabbitMetrics.MicrometerMetricsCollector.Metrics.REJECTED_MESSAGES);
-		}
-
-		@Override
-		protected void incrementConnectionCount(Connection connection) {
-			this.connections.incrementAndGet();
-		}
-
-		@Override
-		protected void decrementConnectionCount(Connection connection) {
-			this.connections.decrementAndGet();
-		}
-
-		@Override
-		protected void incrementChannelCount(Channel channel) {
-			this.channels.incrementAndGet();
-		}
-
-		@Override
-		protected void decrementChannelCount(Channel channel) {
-			this.channels.decrementAndGet();
-		}
-
-		@Override
-		protected void markPublishedMessage() {
-			this.publishedMessages.increment();
-		}
-
-		@Override
-		protected void markConsumedMessage() {
-			this.consumedMessages.increment();
-		}
-
-		@Override
-		protected void markAcknowledgedMessage() {
-			this.acknowledgedMessages.increment();
-		}
-
-		@Override
-		protected void markRejectedMessage() {
-			this.rejectedMessages.increment();
-		}
-
-		/**
-		 * RabbitMQ client metrics types.
-		 */
-		public enum Metrics {
-			/**
-			 * Number of connections metrics.
-			 */
-			CONNECTIONS {
-				@Override
-				Object create(MeterRegistry registry, String prefix, Iterable<Tag> tags) {
-					return registry.gauge(prefix + ".connections", tags, new AtomicLong(0));
-				}
-			},
-			/**
-			 * Number of channels metrics.
-			 */
-			CHANNELS {
-				@Override
-				Object create(MeterRegistry registry, String prefix, Iterable<Tag> tags) {
-					return registry.gauge(prefix + ".channels", tags, new AtomicLong(0));
-				}
-			},
-			/**
-			 * Published messages metrics.
-			 */
-			PUBLISHED_MESSAGES {
-				@Override
-				Object create(MeterRegistry registry, String prefix, Iterable<Tag> tags) {
-					return registry.counter(prefix + "published", tags);
-				}
-			},
-			/**
-			 * Consumed messages metrics.
-			 */
-			CONSUMED_MESSAGES {
-				@Override
-				Object create(MeterRegistry registry, String prefix, Iterable<Tag> tags) {
-					return registry.counter(prefix + ".consumed", tags);
-				}
-			},
-			/**
-			 * Acknowledged messages metrics.
-			 */
-			ACKNOWLEDGED_MESSAGES {
-				@Override
-				Object create(MeterRegistry registry, String prefix, Iterable<Tag> tags) {
-					return registry.counter(prefix + ".acknowledged", tags);
-				}
-			},
-			/**
-			 * Rejected messages metrics.
-			 */
-			REJECTED_MESSAGES {
-				@Override
-				Object create(MeterRegistry registry, String prefix, Iterable<Tag> tags) {
-					return registry.counter(prefix + ".rejected", tags);
-				}
-			};
-
-			abstract Object create(MeterRegistry registry, String prefix, Iterable<Tag> tags);
-		}
-
 	}
 
 }
