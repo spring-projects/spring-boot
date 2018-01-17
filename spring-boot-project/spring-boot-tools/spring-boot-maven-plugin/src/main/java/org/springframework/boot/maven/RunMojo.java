d43346d6c2fdf4b69abb90946259d781ff8e4bb4
Stephane Nicoll <snicoll@pivotal.io>
2018-01-17 13:56:53 +0100

    Polish "Fix potential resource leaks"
    Closes gh-11624

--- spring-boot-project/spring-boot-tools/spring-boot-maven-plugin/src/main/java/org/springframework/boot/maven/RunMojo.java
+++ spring-boot-project/spring-boot-tools/spring-boot-maven-plugin/src/main/java/org/springframework/boot/maven/RunMojo.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2012-2017 the original author or authors.
+ * Copyright 2012-2018 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -123,7 +123,6 @@ public class RunMojo extends AbstractRunMojo {
 		return this.hasDevtools;
 	}
 
-	@SuppressWarnings("resource")
 	private boolean checkForDevtools() {
 		try {
 			URL[] urls = getClassPathUrls();
