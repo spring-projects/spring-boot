aa748cb0efca3b34673be6cf9c6b7f5e5625986d
Stephane Nicoll <snicoll@pivotal.io>
2018-01-18 16:45:29 +0100

    Polish "Use System.lineSeparator()""
    Closes gh-11665

--- spring-boot-project/spring-boot/src/test/java/org/springframework/boot/logging/log4j2/ExtendedWhitespaceThrowablePatternConverterTests.java
+++ spring-boot-project/spring-boot/src/test/java/org/springframework/boot/logging/log4j2/ExtendedWhitespaceThrowablePatternConverterTests.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2012-2017 the original author or authors.
+ * Copyright 2012-2018 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -48,7 +48,8 @@ public class ExtendedWhitespaceThrowablePatternConverterTests {
 		LogEvent event = Log4jLogEvent.newBuilder().setThrown(new Exception()).build();
 		StringBuilder builder = new StringBuilder();
 		this.converter.format(event, builder);
-		assertThat(builder).startsWith(System.lineSeparator()).endsWith(System.lineSeparator());
+		assertThat(builder).startsWith(System.lineSeparator())
+				.endsWith(System.lineSeparator());
 	}
 
 }
