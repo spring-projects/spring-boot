77596fa8e6c4de66661fb547f69ed2927067bec8
Andy Wilkinson <awilkinson@pivotal.io>
2018-01-22 21:15:48 +0000

    Polish

--- spring-boot-project/spring-boot/src/test/java/org/springframework/boot/web/server/CompressionTests.java
+++ spring-boot-project/spring-boot/src/test/java/org/springframework/boot/web/server/CompressionTests.java
@@ -20,8 +20,8 @@ import org.apache.coyote.http11.Http11NioProtocol;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
-import org.springframework.boot.junit.runner.classpath.ClassPathOverrides;
-import org.springframework.boot.junit.runner.classpath.ModifiedClassPathRunner;
+import org.springframework.boot.testsupport.runner.classpath.ClassPathOverrides;
+import org.springframework.boot.testsupport.runner.classpath.ModifiedClassPathRunner;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
