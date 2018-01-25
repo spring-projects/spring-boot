56c59e1aef8cda880bddf4e22b0df3bd536b3c51
Yulin Qin <yulin.qin.ext@gmail.com>
2018-01-25 21:50:57 +0800

    naming change for test target object

--- spring-boot-project/spring-boot-autoconfigure/src/test/java/org/springframework/boot/autoconfigure/web/embedded/undertow/UndertowCustomizerTest.java
+++ spring-boot-project/spring-boot-autoconfigure/src/test/java/org/springframework/boot/autoconfigure/web/embedded/undertow/UndertowCustomizerTest.java
@@ -49,7 +49,7 @@ public class UndertowCustomizerTest {
 	private Environment environment;
 
 	@Mock
-	private ConfigurableUndertowWebServerFactory configurableUndertowWebServerFactory;
+	private ConfigurableUndertowWebServerFactory factory;
 
 	@Mock
 	private org.springframework.boot.autoconfigure.web.ServerProperties.Undertow undertowProperties;
@@ -67,183 +67,183 @@ public class UndertowCustomizerTest {
 	@Test
 	public void shouldNotSetBufferSizeWhileItIsNull() throws Exception {
 		given(this.undertowProperties.getBufferSize()).willReturn(null);
-		UndertowCustomizer.customizeUndertow(this.serverProperties, this.environment, this.configurableUndertowWebServerFactory);
-		verify(this.configurableUndertowWebServerFactory, times(0)).setBufferSize(anyInt());
+		UndertowCustomizer.customizeUndertow(this.serverProperties, this.environment, this.factory);
+		verify(this.factory, times(0)).setBufferSize(anyInt());
 	}
 
 	@Test
 	public void shouldSetBufferSizeWhileItIsNotNull() throws Exception {
 		given(this.undertowProperties.getBufferSize()).willReturn(1);
-		UndertowCustomizer.customizeUndertow(this.serverProperties, this.environment, this.configurableUndertowWebServerFactory);
-		verify(this.configurableUndertowWebServerFactory, times(1)).setBufferSize(anyInt());
+		UndertowCustomizer.customizeUndertow(this.serverProperties, this.environment, this.factory);
+		verify(this.factory, times(1)).setBufferSize(anyInt());
 	}
 
 	@Test
 	public void shouldNotSetIoThreadWhileItIsNull() throws Exception {
 		given(this.undertowProperties.getIoThreads()).willReturn(null);
-		UndertowCustomizer.customizeUndertow(this.serverProperties, this.environment, this.configurableUndertowWebServerFactory);
-		verify(this.configurableUndertowWebServerFactory, times(0)).setIoThreads(any());
+		UndertowCustomizer.customizeUndertow(this.serverProperties, this.environment, this.factory);
+		verify(this.factory, times(0)).setIoThreads(any());
 	}
 
 	@Test
 	public void shouldSetIoThreadWhileItIsNotNull() throws Exception {
 		given(this.undertowProperties.getIoThreads()).willReturn(1);
-		UndertowCustomizer.customizeUndertow(this.serverProperties, this.environment, this.configurableUndertowWebServerFactory);
-		verify(this.configurableUndertowWebServerFactory, times(1)).setIoThreads(any());
+		UndertowCustomizer.customizeUndertow(this.serverProperties, this.environment, this.factory);
+		verify(this.factory, times(1)).setIoThreads(any());
 	}
 
 	@Test
 	public void shouldNotSetWorkerThreadsWhileItIsNull() throws Exception {
 		given(this.undertowProperties.getWorkerThreads()).willReturn(null);
-		UndertowCustomizer.customizeUndertow(this.serverProperties, this.environment, this.configurableUndertowWebServerFactory);
-		verify(this.configurableUndertowWebServerFactory, times(0)).setWorkerThreads(any());
+		UndertowCustomizer.customizeUndertow(this.serverProperties, this.environment, this.factory);
+		verify(this.factory, times(0)).setWorkerThreads(any());
 	}
 
 	@Test
 	public void shouldSetWorkerThreadsWhileItIsNotNull() throws Exception {
 		given(this.undertowProperties.getWorkerThreads()).willReturn(1);
-		UndertowCustomizer.customizeUndertow(this.serverProperties, this.environment, this.configurableUndertowWebServerFactory);
-		verify(this.configurableUndertowWebServerFactory, times(1)).setWorkerThreads(any());
+		UndertowCustomizer.customizeUndertow(this.serverProperties, this.environment, this.factory);
+		verify(this.factory, times(1)).setWorkerThreads(any());
 	}
 
 	@Test
 	public void shouldNotSetDirectBuffersWhileItIsNull() throws Exception {
 		given(this.undertowProperties.getDirectBuffers()).willReturn(null);
-		UndertowCustomizer.customizeUndertow(this.serverProperties, this.environment, this.configurableUndertowWebServerFactory);
-		verify(this.configurableUndertowWebServerFactory, times(0)).setUseDirectBuffers(anyBoolean());
+		UndertowCustomizer.customizeUndertow(this.serverProperties, this.environment, this.factory);
+		verify(this.factory, times(0)).setUseDirectBuffers(anyBoolean());
 	}
 
 	@Test
 	public void shouldSetDirectBuffersWhileItIsNotNull() throws Exception {
 		given(this.undertowProperties.getDirectBuffers()).willReturn(Boolean.TRUE);
-		UndertowCustomizer.customizeUndertow(this.serverProperties, this.environment, this.configurableUndertowWebServerFactory);
-		verify(this.configurableUndertowWebServerFactory, times(1)).setUseDirectBuffers(anyBoolean());
+		UndertowCustomizer.customizeUndertow(this.serverProperties, this.environment, this.factory);
+		verify(this.factory, times(1)).setUseDirectBuffers(anyBoolean());
 	}
 
 	@Test
 	public void shouldNotSetAccesslogWhileItIsNull() throws Exception {
 		given(this.accesslogProperties.getEnabled()).willReturn(null);
-		UndertowCustomizer.customizeUndertow(this.serverProperties, this.environment, this.configurableUndertowWebServerFactory);
-		verify(this.configurableUndertowWebServerFactory, times(0)).setAccessLogEnabled(any());
+		UndertowCustomizer.customizeUndertow(this.serverProperties, this.environment, this.factory);
+		verify(this.factory, times(0)).setAccessLogEnabled(any());
 	}
 
 	@Test
 	public void shouldSetAccesslogWhileItIsNotNull() throws Exception {
 		given(this.accesslogProperties.getEnabled()).willReturn(Boolean.FALSE);
-		UndertowCustomizer.customizeUndertow(this.serverProperties, this.environment, this.configurableUndertowWebServerFactory);
-		verify(this.configurableUndertowWebServerFactory, times(1)).setAccessLogEnabled(any());
+		UndertowCustomizer.customizeUndertow(this.serverProperties, this.environment, this.factory);
+		verify(this.factory, times(1)).setAccessLogEnabled(any());
 	}
 
 	@Test
 	public void shouldAlwaysSetAccessLogDirectory() throws Exception {
 		given(this.accesslogProperties.getDir()).willReturn(null);
-		UndertowCustomizer.customizeUndertow(this.serverProperties, this.environment, this.configurableUndertowWebServerFactory);
-		verify(this.configurableUndertowWebServerFactory, times(1)).setAccessLogDirectory(any());
+		UndertowCustomizer.customizeUndertow(this.serverProperties, this.environment, this.factory);
+		verify(this.factory, times(1)).setAccessLogDirectory(any());
 	}
 
 	@Test
 	public void shouldAlwaysSetAccessLogPattern() throws Exception {
 		given(this.accesslogProperties.getPattern()).willReturn(null);
-		UndertowCustomizer.customizeUndertow(this.serverProperties, this.environment, this.configurableUndertowWebServerFactory);
-		verify(this.configurableUndertowWebServerFactory, times(1)).setAccessLogPattern(any());
+		UndertowCustomizer.customizeUndertow(this.serverProperties, this.environment, this.factory);
+		verify(this.factory, times(1)).setAccessLogPattern(any());
 	}
 
 	@Test
 	public void shouldAlwaysSetAccessLogPrefix() throws Exception {
 		given(this.accesslogProperties.getPrefix()).willReturn(null);
-		UndertowCustomizer.customizeUndertow(this.serverProperties, this.environment, this.configurableUndertowWebServerFactory);
-		verify(this.configurableUndertowWebServerFactory, times(1)).setAccessLogPrefix(any());
+		UndertowCustomizer.customizeUndertow(this.serverProperties, this.environment, this.factory);
+		verify(this.factory, times(1)).setAccessLogPrefix(any());
 	}
 
 	@Test
 	public void shouldAlwaysSetAccessLogSuffix() throws Exception {
 		given(this.accesslogProperties.getSuffix()).willReturn(null);
-		UndertowCustomizer.customizeUndertow(this.serverProperties, this.environment, this.configurableUndertowWebServerFactory);
-		verify(this.configurableUndertowWebServerFactory, times(1)).setAccessLogSuffix(any());
+		UndertowCustomizer.customizeUndertow(this.serverProperties, this.environment, this.factory);
+		verify(this.factory, times(1)).setAccessLogSuffix(any());
 	}
 
 	@Test
 	public void shouldAlwaysSetAccessLogRotate() throws Exception {
 		given(this.accesslogProperties.isRotate()).willReturn(false);
-		UndertowCustomizer.customizeUndertow(this.serverProperties, this.environment, this.configurableUndertowWebServerFactory);
-		verify(this.configurableUndertowWebServerFactory, times(1)).setAccessLogRotate(any());
+		UndertowCustomizer.customizeUndertow(this.serverProperties, this.environment, this.factory);
+		verify(this.factory, times(1)).setAccessLogRotate(any());
 	}
 
 	@Test
 	public void shouldSetUserForwardHeaderWhenItIsTrue() throws Exception {
 		given(this.serverProperties.isUseForwardHeaders()).willReturn(true);
-		UndertowCustomizer.customizeUndertow(this.serverProperties, this.environment, this.configurableUndertowWebServerFactory);
-		verify(this.configurableUndertowWebServerFactory, times(1)).setUseForwardHeaders(Boolean.TRUE);
+		UndertowCustomizer.customizeUndertow(this.serverProperties, this.environment, this.factory);
+		verify(this.factory, times(1)).setUseForwardHeaders(Boolean.TRUE);
 	}
 
 	@Test
 	public void shouldSetUserForwardHeaderWhenItIsNUll() throws Exception {
 		given(this.serverProperties.isUseForwardHeaders()).willReturn(null);
-		UndertowCustomizer.customizeUndertow(this.serverProperties, this.environment, this.configurableUndertowWebServerFactory);
-		verify(this.configurableUndertowWebServerFactory, times(1)).setUseForwardHeaders(any());
+		UndertowCustomizer.customizeUndertow(this.serverProperties, this.environment, this.factory);
+		verify(this.factory, times(1)).setUseForwardHeaders(any());
 	}
 
 	@Test
 	public void shouldCustomizeMaxHttpHeaderSizeWhenItIsGreaterThanZero() throws Exception {
 		given(this.serverProperties.getMaxHttpHeaderSize()).willReturn(1);
-		UndertowCustomizer.customizeUndertow(this.serverProperties, this.environment, this.configurableUndertowWebServerFactory);
-		verify(this.configurableUndertowWebServerFactory, times(1)).addBuilderCustomizers(any());
+		UndertowCustomizer.customizeUndertow(this.serverProperties, this.environment, this.factory);
+		verify(this.factory, times(1)).addBuilderCustomizers(any());
 	}
 
 	@Test
 	public void shouldNotCustomizeMaxHttpHeaderSizeWhenItIsEqualZero() throws Exception {
 		given(this.serverProperties.getMaxHttpHeaderSize()).willReturn(0);
-		UndertowCustomizer.customizeUndertow(this.serverProperties, this.environment, this.configurableUndertowWebServerFactory);
-		verify(this.configurableUndertowWebServerFactory, times(0)).addBuilderCustomizers(any());
+		UndertowCustomizer.customizeUndertow(this.serverProperties, this.environment, this.factory);
+		verify(this.factory, times(0)).addBuilderCustomizers(any());
 	}
 
 	@Test
 	public void shouldNotCustomizeMaxHttpHeaderSizeWhenItIsLessThanZero() throws Exception {
 		given(this.serverProperties.getMaxHttpHeaderSize()).willReturn(-1);
-		UndertowCustomizer.customizeUndertow(this.serverProperties, this.environment, this.configurableUndertowWebServerFactory);
-		verify(this.configurableUndertowWebServerFactory, times(0)).addBuilderCustomizers(any());
+		UndertowCustomizer.customizeUndertow(this.serverProperties, this.environment, this.factory);
+		verify(this.factory, times(0)).addBuilderCustomizers(any());
 	}
 
 	@Test
 	public void shouldCustomizeMaxHttpPostSizeWhenItIsGreaterThanZero() throws Exception {
 		given(this.undertowProperties.getMaxHttpPostSize()).willReturn(Long.valueOf(1));
-		UndertowCustomizer.customizeUndertow(this.serverProperties, this.environment, this.configurableUndertowWebServerFactory);
-		verify(this.configurableUndertowWebServerFactory, times(1)).addBuilderCustomizers(any());
+		UndertowCustomizer.customizeUndertow(this.serverProperties, this.environment, this.factory);
+		verify(this.factory, times(1)).addBuilderCustomizers(any());
 	}
 
 	@Test
 	public void shouldNotCustomizeMaxHttpPostSizeWhenItIsEqualZero() throws Exception {
 		given(this.undertowProperties.getMaxHttpPostSize()).willReturn(Long.valueOf(0));
-		UndertowCustomizer.customizeUndertow(this.serverProperties, this.environment, this.configurableUndertowWebServerFactory);
-		verify(this.configurableUndertowWebServerFactory, times(0)).addBuilderCustomizers(any());
+		UndertowCustomizer.customizeUndertow(this.serverProperties, this.environment, this.factory);
+		verify(this.factory, times(0)).addBuilderCustomizers(any());
 	}
 
 	@Test
 	public void shouldNotCustomizeMaxHttpPostSizeWhenItIsLessThanZero() throws Exception {
 		given(this.undertowProperties.getMaxHttpPostSize()).willReturn(Long.valueOf(-1));
-		UndertowCustomizer.customizeUndertow(this.serverProperties, this.environment, this.configurableUndertowWebServerFactory);
-		verify(this.configurableUndertowWebServerFactory, times(0)).addBuilderCustomizers(any());
+		UndertowCustomizer.customizeUndertow(this.serverProperties, this.environment, this.factory);
+		verify(this.factory, times(0)).addBuilderCustomizers(any());
 	}
 
 	@Test
 	public void shouldCustomizeConnectionTimeoutWhenItIsNotNull() throws Exception {
 		given(this.serverProperties.getConnectionTimeout()).willReturn(Duration.ZERO);
-		UndertowCustomizer.customizeUndertow(this.serverProperties, this.environment, this.configurableUndertowWebServerFactory);
-		verify(this.configurableUndertowWebServerFactory, times(1)).addBuilderCustomizers(any());
+		UndertowCustomizer.customizeUndertow(this.serverProperties, this.environment, this.factory);
+		verify(this.factory, times(1)).addBuilderCustomizers(any());
 	}
 
 	@Test
 	public void shouldNotCustomizeConnectionTimeoutWhenItIsNull() throws Exception {
 		given(this.serverProperties.getConnectionTimeout()).willReturn(null);
-		UndertowCustomizer.customizeUndertow(this.serverProperties, this.environment, this.configurableUndertowWebServerFactory);
-		verify(this.configurableUndertowWebServerFactory, times(0)).addBuilderCustomizers(any());
+		UndertowCustomizer.customizeUndertow(this.serverProperties, this.environment, this.factory);
+		verify(this.factory, times(0)).addBuilderCustomizers(any());
 	}
 
 	@Test
 	public void shouldAlwaysCallAddDeploymentInfoCustomizers() throws Exception {
 		given(this.undertowProperties.isEagerFilterInit()).willReturn(null);
-		UndertowCustomizer.customizeUndertow(this.serverProperties, this.environment, this.configurableUndertowWebServerFactory);
-		verify(this.configurableUndertowWebServerFactory, times(1)).addDeploymentInfoCustomizers(any());
+		UndertowCustomizer.customizeUndertow(this.serverProperties, this.environment, this.factory);
+		verify(this.factory, times(1)).addDeploymentInfoCustomizers(any());
 	}
 
 }
