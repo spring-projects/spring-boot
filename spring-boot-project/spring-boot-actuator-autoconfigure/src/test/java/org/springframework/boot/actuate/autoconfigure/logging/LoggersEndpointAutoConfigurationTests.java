eba476ba6ec5c9eb4ce80d016ded3f214fa04d89
Stephane Nicoll <snicoll@pivotal.io>
2018-01-29 16:37:28 +0100

    Disable LoggersEndpoint if the logging system is disabled
    Closes gh-11793

--- spring-boot-project/spring-boot-actuator-autoconfigure/src/test/java/org/springframework/boot/actuate/autoconfigure/logging/LoggersEndpointAutoConfigurationTests.java
+++ spring-boot-project/spring-boot-actuator-autoconfigure/src/test/java/org/springframework/boot/actuate/autoconfigure/logging/LoggersEndpointAutoConfigurationTests.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2012-2017 the original author or authors.
+ * Copyright 2012-2018 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -51,6 +51,17 @@ public class LoggersEndpointAutoConfigurationTests {
 		this.contextRunner.withPropertyValues("management.endpoint.loggers.enabled:false")
 				.run((context) -> assertThat(context)
 						.doesNotHaveBean(LoggersEndpoint.class));
+	}
+
+	@Test
+	public void runWithNoneLoggerEndpointShouldNotHaveEndpointBean() {
+		this.contextRunner
+				.withSystemProperties("org.springframework.boot.logging.LoggingSystem=none")
+				.run((context) -> {
+					System.out.println(context.getBean(LoggingSystem.class));
+					assertThat(context)
+							.doesNotHaveBean(LoggersEndpoint.class);
+				});
 	}
 
 	@Configuration
