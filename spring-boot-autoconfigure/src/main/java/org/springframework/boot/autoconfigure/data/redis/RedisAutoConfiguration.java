11f700aa9d00f3d7552c5eab7ce2666131fa29f7
Stephane Nicoll <snicoll@pivotal.io>
2018-01-30 10:27:26 +0100

    Fix handling of redis password containing a colon
    Closes gh-11371

--- spring-boot-autoconfigure/src/main/java/org/springframework/boot/autoconfigure/data/redis/RedisAutoConfiguration.java
+++ spring-boot-autoconfigure/src/main/java/org/springframework/boot/autoconfigure/data/redis/RedisAutoConfiguration.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2012-2016 the original author or authors.
+ * Copyright 2012-2018 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -129,7 +129,7 @@ public class RedisAutoConfiguration {
 				factory.setPort(uri.getPort());
 				if (uri.getUserInfo() != null) {
 					String password = uri.getUserInfo();
-					int index = password.lastIndexOf(":");
+					int index = password.indexOf(":");
 					if (index >= 0) {
 						password = password.substring(index + 1);
 					}
--- spring-boot-autoconfigure/src/test/java/org/springframework/boot/autoconfigure/data/redis/RedisAutoConfigurationTests.java
+++ spring-boot-autoconfigure/src/test/java/org/springframework/boot/autoconfigure/data/redis/RedisAutoConfigurationTests.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2012-2017 the original author or authors.
+ * Copyright 2012-2018 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -89,6 +89,28 @@ public class RedisAutoConfigurationTests {
 				.isEqualTo("password");
 		assertThat(this.context.getBean(JedisConnectionFactory.class).isUseSsl())
 				.isEqualTo(true);
+	}
+
+	@Test
+	public void testPasswordInUrlWithColon() {
+		load("spring.redis.url:redis://:pass:word@example:33");
+		assertThat(this.context.getBean(JedisConnectionFactory.class).getHostName())
+				.isEqualTo("example");
+		assertThat(this.context.getBean(JedisConnectionFactory.class).getPort())
+				.isEqualTo(33);
+		assertThat(this.context.getBean(JedisConnectionFactory.class).getPassword())
+				.isEqualTo("pass:word");
+	}
+
+	@Test
+	public void testPasswordInUrlStartsWithColon() {
+		load("spring.redis.url:redis://user::pass:word@example:33");
+		assertThat(this.context.getBean(JedisConnectionFactory.class).getHostName())
+				.isEqualTo("example");
+		assertThat(this.context.getBean(JedisConnectionFactory.class).getPort())
+				.isEqualTo(33);
+		assertThat(this.context.getBean(JedisConnectionFactory.class).getPassword())
+				.isEqualTo(":pass:word");
 	}
 
 	@Test
