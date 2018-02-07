a8e07312f04daf412f1345f207fe227e152387a7
Patryk Kostrzewa <kostrzewapatryk7@gmail.com>
2018-02-07 19:06:27 +0100

    Restored accidentaly removed message to description

--- spring-boot-project/spring-boot-autoconfigure/src/main/java/org/springframework/boot/autoconfigure/jdbc/DataSourceBeanCreationFailureAnalyzer.java
+++ spring-boot-project/spring-boot-autoconfigure/src/main/java/org/springframework/boot/autoconfigure/jdbc/DataSourceBeanCreationFailureAnalyzer.java
@@ -44,8 +44,8 @@ class DataSourceBeanCreationFailureAnalyzer
 	private String createDescription(DataSourceBeanCreationException cause) {
 
 		StringBuilder message = new StringBuilder();
-		message.append(cause.getMessage());
-		message.append(" Property spring.datasource.url was not specified. ");
+		message.append("Cannot auto-configure DataSource. ");
+		message.append("Property spring.datasource.url was not specified. ");
 		message.append("Cannot auto-configure embedded database as well. ");
 		message.append("Cannot determine embedded database ");
 		message.append(cause.getProperty());
