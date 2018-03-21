f84149d699301083467d3e3fd0390625bdbcb4c1
Johnny Lim <izeye@naver.com>
2018-03-22 03:23:04 +0900

    Polish

--- spring-boot-project/spring-boot-actuator-autoconfigure/src/main/java/org/springframework/boot/actuate/autoconfigure/metrics/web/tomcat/TomcatMetricsAutoConfiguration.java
+++ spring-boot-project/spring-boot-actuator-autoconfigure/src/main/java/org/springframework/boot/actuate/autoconfigure/metrics/web/tomcat/TomcatMetricsAutoConfiguration.java
@@ -30,7 +30,6 @@ import org.springframework.boot.autoconfigure.condition.ConditionalOnWebApplicat
 import org.springframework.boot.web.embedded.tomcat.TomcatReactiveWebServerFactory;
 import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
 import org.springframework.boot.web.server.WebServerFactoryCustomizer;
-import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
 
 /**
@@ -47,7 +46,7 @@ public class TomcatMetricsAutoConfiguration {
 
 	@Bean
 	@ConditionalOnMissingBean(TomcatMetrics.class)
-	public TomcatMetrics tomcatMetrics(ApplicationContext applicationContext) {
+	public TomcatMetrics tomcatMetrics() {
 		return new TomcatMetrics(this.context == null ? null : this.context.getManager(),
 				Collections.emptyList());
 	}
--- spring-boot-project/spring-boot/src/test/java/org/springframework/boot/ReproTests.java
+++ spring-boot-project/spring-boot/src/test/java/org/springframework/boot/ReproTests.java
@@ -170,7 +170,6 @@ public class ReproTests {
 				.isEqualTo(expectedActiveProfiles);
 		assertThat(context.getEnvironment().getProperty("version")).as("version mismatch")
 				.isEqualTo(expectedVersion);
-		context.close();
 	}
 
 	@Configuration
