69ab956e8b75ed1f6bee77b7c7aa9c2eb82223bd
Stephane Nicoll <snicoll@pivotal.io>
2018-05-03 11:46:27 +0200

    Stop assuming datasource creation failure originates from auto-config
    There are documented way to reuse bits of the infrastructure in user
    config to offer similar datasource configuration. If that fails, the
    regular failure there will kick in.
    
    This commit improves `DataSourceBeanCreationFailureAnalyzer` to not
    misguide users that the auto-configuration has failed. Rather, it
    describes what has failed in a more generic way.
    
    Closes gh-12947

--- spring-boot-project/spring-boot-autoconfigure/src/main/java/org/springframework/boot/autoconfigure/jdbc/DataSourceProperties.java
+++ spring-boot-project/spring-boot-autoconfigure/src/main/java/org/springframework/boot/autoconfigure/jdbc/DataSourceProperties.java
@@ -235,7 +235,7 @@ public class DataSourceProperties implements BeanClassLoaderAware, InitializingB
 		if (!StringUtils.hasText(driverClassName)) {
 			throw new DataSourceBeanCreationException(
 					"Failed to determine a suitable driver class",
-					this.embeddedDatabaseConnection);
+					this, this.embeddedDatabaseConnection);
 		}
 		return driverClassName;
 	}
@@ -282,7 +282,7 @@ public class DataSourceProperties implements BeanClassLoaderAware, InitializingB
 		if (!StringUtils.hasText(url)) {
 			throw new DataSourceBeanCreationException(
 					"Failed to determine suitable jdbc url",
-					this.embeddedDatabaseConnection);
+					this, this.embeddedDatabaseConnection);
 		}
 		return url;
 	}
@@ -513,12 +513,19 @@ public class DataSourceProperties implements BeanClassLoaderAware, InitializingB
 
 	static class DataSourceBeanCreationException extends BeanCreationException {
 
+		private final DataSourceProperties properties;
+
 		private final EmbeddedDatabaseConnection connection;
 
-		DataSourceBeanCreationException(String message,
+		DataSourceBeanCreationException(String message, DataSourceProperties properties,
 				EmbeddedDatabaseConnection connection) {
 			super(message);
+			this.properties = properties;
 			this.connection = connection;
+		}
+
+		public DataSourceProperties getProperties() {
+			return this.properties;
 		}
 
 		public EmbeddedDatabaseConnection getConnection() {
