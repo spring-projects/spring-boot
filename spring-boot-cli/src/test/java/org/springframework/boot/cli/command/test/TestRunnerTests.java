83a6f02ec483f5f1f371a471466f3e40c47b4e0c
Andy Wilkinson <awilkinson@pivotal.io>
2018-05-12 09:53:33 +0100

    Polish

--- spring-boot-cli/src/test/java/org/springframework/boot/cli/command/test/TestRunnerTests.java
+++ spring-boot-cli/src/test/java/org/springframework/boot/cli/command/test/TestRunnerTests.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2012-2015 the original author or authors.
+ * Copyright 2012-2018 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -40,8 +40,7 @@ public class TestRunnerTests {
 		given(configuration.getClasspath()).willReturn(new String[0]);
 		this.thrown.expect(RuntimeException.class);
 		this.thrown.expectMessage(equalTo("No classes found in '[foo, bar]'"));
-		new TestRunner(configuration, new String[] { "foo", "bar" }, new String[0])
-				.compileAndRunTests();
+		new TestRunner(configuration, new String[] { "foo", "bar" }).compileAndRunTests();
 	}
 
 }
