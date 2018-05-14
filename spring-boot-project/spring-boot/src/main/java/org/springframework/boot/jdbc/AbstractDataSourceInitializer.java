8f53c2e2bb247976e135edd12ba7a009d25d45c0
Stephane Nicoll <snicoll@pivotal.io>
2018-05-14 09:32:17 +0200

    Support for custom comment prefix
    Some Quartz initialization scripts have comments in a different format.
    
    This commit introduces a `comment-prefix` property that should be set by
    the user if their target database has a script that contains those
    unusual comments.
    
    Closes gh-13041

--- spring-boot-project/spring-boot/src/main/java/org/springframework/boot/jdbc/AbstractDataSourceInitializer.java
+++ spring-boot-project/spring-boot/src/main/java/org/springframework/boot/jdbc/AbstractDataSourceInitializer.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2012-2017 the original author or authors.
+ * Copyright 2012-2018 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -62,6 +62,7 @@ public abstract class AbstractDataSourceInitializer {
 		}
 		populator.addScript(this.resourceLoader.getResource(schemaLocation));
 		populator.setContinueOnError(true);
+		customize(populator);
 		DatabasePopulatorUtils.execute(populator, this.dataSource);
 	}
 
@@ -74,6 +75,13 @@ public abstract class AbstractDataSourceInitializer {
 			return false;
 		}
 		return true;
+	}
+
+	/**
+	 * Customize the {@link ResourceDatabasePopulator}.
+	 * @param populator the configured database populator
+	 */
+	protected void customize(ResourceDatabasePopulator populator) {
 	}
 
 	protected abstract DataSourceInitializationMode getMode();
