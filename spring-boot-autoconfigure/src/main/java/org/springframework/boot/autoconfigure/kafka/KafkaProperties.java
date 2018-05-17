d9029ef69e87cbd5d31fd8617d4ed0c9e8b9ccae
Stephane Nicoll <snicoll@pivotal.io>
2018-05-17 16:08:38 +0200

    Fix documentation of spring.kafka.producer.batch-size
    Closes gh-13127

--- spring-boot-autoconfigure/src/main/java/org/springframework/boot/autoconfigure/kafka/KafkaProperties.java
+++ spring-boot-autoconfigure/src/main/java/org/springframework/boot/autoconfigure/kafka/KafkaProperties.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2012-2016 the original author or authors.
+ * Copyright 2012-2018 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -444,7 +444,8 @@ public class KafkaProperties {
 		private String acks;
 
 		/**
-		 * Number of records to batch before sending.
+		 * Default batch size in bytes. A small batch size will make batching less common
+		 * and may reduce throughput (a batch size of zero disables batching entirely).
 		 */
 		private Integer batchSize;
 
