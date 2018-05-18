b3c021a2423de019cce7350029d4120ba2627879
Mihhail Lapushkin <mihhail.lapushkin@gmail.com>
2018-05-18 11:31:51 +0300

    Allow LocalHostUriTemplateHandler to be created with a custom underlying UriTemplateHandler

--- spring-boot-project/spring-boot-test/src/main/java/org/springframework/boot/test/web/client/LocalHostUriTemplateHandler.java
+++ spring-boot-project/spring-boot-test/src/main/java/org/springframework/boot/test/web/client/LocalHostUriTemplateHandler.java
@@ -58,9 +58,23 @@ public class LocalHostUriTemplateHandler extends RootUriTemplateHandler {
 	 * @since 1.4.1
 	 */
 	public LocalHostUriTemplateHandler(Environment environment, String scheme) {
-		super(new DefaultUriBuilderFactory());
+		this(environment, scheme, new DefaultUriBuilderFactory());
+	}
+
+	/**
+	 * Create a new {@code LocalHostUriTemplateHandler} that will generate URIs with the
+	 * given {@code scheme}, use the given {@code environment} to determine the
+	 * context-path and port and delegate to the given template handler.
+	 * @param environment the environment used to determine the port
+	 * @param scheme the scheme of the root uri
+	 * @param delegateHandler the delegate handler
+	 * @since 2.0.X
+	 */
+	public LocalHostUriTemplateHandler(Environment environment, String scheme, UriTemplateHandler delegateHandler) {
+		super(delegateHandler);
 		Assert.notNull(environment, "Environment must not be null");
 		Assert.notNull(scheme, "Scheme must not be null");
+		Assert.notNull(delegateHandler, "DelegateHandler must not be null");
 		this.environment = environment;
 		this.scheme = scheme;
 	}
