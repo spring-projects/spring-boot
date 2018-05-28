065eebfb4bd68bc63aed9c297acf98ea658e1fe2
Eddú Meléndez <eddu.melendez@gmail.com>
2018-05-27 23:42:47 -0500

    Fix javadoc
    Closes gh-13274

--- spring-boot-project/spring-boot-test/src/test/java/org/springframework/boot/test/context/SpringBootTestCustomConfigNameTests.java
+++ spring-boot-project/spring-boot-test/src/test/java/org/springframework/boot/test/context/SpringBootTestCustomConfigNameTests.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2012-2017 the original author or authors.
+ * Copyright 2012-2018 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -27,7 +27,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 import static org.assertj.core.api.Assertions.assertThat;
 
 /**
- * Tests for {@SpringBootTest} with a custom config name
+ * Tests for {@link SpringBootTest} with a custom config name
  *
  * @author Andy Wilkinson
  */
--- spring-boot-project/spring-boot/src/main/java/org/springframework/boot/web/embedded/undertow/CompositeResourceManager.java
+++ spring-boot-project/spring-boot/src/main/java/org/springframework/boot/web/embedded/undertow/CompositeResourceManager.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2012-2017 the original author or authors.
+ * Copyright 2012-2018 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -26,7 +26,7 @@ import io.undertow.server.handlers.resource.ResourceChangeListener;
 import io.undertow.server.handlers.resource.ResourceManager;
 
 /**
- * A {@ResourceManager} that delegates to multiple {@code ResourceManager} instances.
+ * A {@link ResourceManager} that delegates to multiple {@code ResourceManager} instances.
  *
  * @author Andy Wilkinson
  */
