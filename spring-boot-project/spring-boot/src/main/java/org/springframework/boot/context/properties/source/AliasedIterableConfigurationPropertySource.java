125069880a432aa01431f3a239ffd51776244fda
dreis2211 <christoph.dreis@freenet.de>
2018-06-03 14:14:56 +0200

    Polish contribution

--- spring-boot-project/spring-boot/src/main/java/org/springframework/boot/context/properties/source/AliasedIterableConfigurationPropertySource.java
+++ spring-boot-project/spring-boot/src/main/java/org/springframework/boot/context/properties/source/AliasedIterableConfigurationPropertySource.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2012-2017 the original author or authors.
+ * Copyright 2012-2018 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -38,8 +38,7 @@ class AliasedIterableConfigurationPropertySource
 
 	@Override
 	public Stream<ConfigurationPropertyName> stream() {
-		return getSource().stream()
-				.flatMap(this::addAliases);
+		return getSource().stream().flatMap(this::addAliases);
 	}
 
 	private Stream<ConfigurationPropertyName> addAliases(ConfigurationPropertyName name) {
