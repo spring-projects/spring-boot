601f41788b731cc82b8fa01032c8eee70be0a0cd
Keqi Huang <keqih@smu.edu>
2018-06-21 14:09:48 +0800

    Create WelcomeController.java

---
+++ spring-boot-samples/spring-boot-sample-web-thymeleaf/src/main/java/sample/thymeleaf/WelcomeController.java
@@ -0,0 +1,35 @@
+/*
+ * Copyright 2012-2018 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+ 
+package sample.thymeleaf;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class WelcomeController {
+    @Value("${username}")
+    private String username;
+
+    @RequestMapping("")
+    public ModelAndView welcome(ModelAndView mav) {
+        mav.setViewName("welcome");
+        mav.addObject("username", username);
+        return mav;
+    }
+}
