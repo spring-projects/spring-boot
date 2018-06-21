17d69b8585d0fca2540797f3f1c16a32f04ad49f
Keqi Huang <keqih@smu.edu>
2018-06-21 14:22:59 +0800

    Update SampleWebThymeleafApplicationTests.java

--- spring-boot-samples/spring-boot-sample-web-thymeleaf/src/test/java/sample/thymeleaf/SampleWebThymeleafApplicationTests.java
+++ spring-boot-samples/spring-boot-sample-web-thymeleaf/src/test/java/sample/thymeleaf/SampleWebThymeleafApplicationTests.java
@@ -14,7 +14,7 @@
  * limitations under the License.
  */
 
-package sample.freemarker;
+package sample.thymeleaf;
 
 import java.util.Arrays;
 
@@ -35,29 +35,23 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-/**
- * Basic integration tests for FreeMarker application.
- *
- * @author Phillip Webb
- * @author Andy Wilkinson
- */
 @RunWith(SpringRunner.class)
 @SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
-public class SampleWebFreeMarkerApplicationTests {
+public class SampleWebThymeleafApplicationTests {
 
 	@Autowired
 	private TestRestTemplate testRestTemplate;
 
 	@Test
-	public void testFreeMarkerTemplate() {
-		ResponseEntity<String> entity = this.testRestTemplate.getForEntity("/",
+	public void testThymeleafTemplate() {
+		ResponseEntity<String> entity = this.testRestTemplate.getForEntity("",
 				String.class);
 		assertThat(entity.getStatusCode()).isEqualTo(HttpStatus.OK);
-		assertThat(entity.getBody()).contains("Hello, Andy");
+		assertThat(entity.getBody()).contains("Hello, keqi");
 	}
 
 	@Test
-	public void testFreeMarkerErrorTemplate() {
+	public void testThymeleafErrorTemplate() {
 		HttpHeaders headers = new HttpHeaders();
 		headers.setAccept(Arrays.asList(MediaType.TEXT_HTML));
 		HttpEntity<String> requestEntity = new HttpEntity<>(headers);
