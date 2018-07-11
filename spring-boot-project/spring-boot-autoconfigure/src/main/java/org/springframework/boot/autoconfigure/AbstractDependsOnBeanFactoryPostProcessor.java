48819253eb5171b93ede2b630fdb8e64ca845e62
Stephane Nicoll <snicoll@pivotal.io>
2018-07-11 11:23:44 +0200

    Fix dependency order between JdbcTemplate and database migration tools
    This commit makes sure that Flyway/Liquibase migrates the schema if
    necessary before a `JdbcTemplate` is made available as an injection
    point.
    
    This commit also adds a test that validates simple datasource
    initialization (spring.datasource.*) happens before a `JdbcTemplate`
    bean can be used.
    
    Closes gh-13155

--- spring-boot-project/spring-boot-autoconfigure/src/main/java/org/springframework/boot/autoconfigure/AbstractDependsOnBeanFactoryPostProcessor.java
+++ spring-boot-project/spring-boot-autoconfigure/src/main/java/org/springframework/boot/autoconfigure/AbstractDependsOnBeanFactoryPostProcessor.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2012-2017 the original author or authors.
+ * Copyright 2012-2018 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -58,6 +58,11 @@ public abstract class AbstractDependsOnBeanFactoryPostProcessor
 		this.dependsOn = dependsOn;
 	}
 
+	protected AbstractDependsOnBeanFactoryPostProcessor(Class<?> beanClass,
+			String... dependsOn) {
+		this(beanClass, null, dependsOn);
+	}
+
 	@Override
 	public void postProcessBeanFactory(ConfigurableListableBeanFactory beanFactory) {
 		for (String beanName : getBeanNames(beanFactory)) {
@@ -74,9 +79,12 @@ public abstract class AbstractDependsOnBeanFactoryPostProcessor
 		Set<String> names = new HashSet<>();
 		names.addAll(Arrays.asList(BeanFactoryUtils.beanNamesForTypeIncludingAncestors(
 				beanFactory, this.beanClass, true, false)));
-		for (String factoryBeanName : BeanFactoryUtils.beanNamesForTypeIncludingAncestors(
-				beanFactory, this.factoryBeanClass, true, false)) {
-			names.add(BeanFactoryUtils.transformedBeanName(factoryBeanName));
+		if (this.factoryBeanClass != null) {
+			for (String factoryBeanName : BeanFactoryUtils
+					.beanNamesForTypeIncludingAncestors(beanFactory,
+							this.factoryBeanClass, true, false)) {
+				names.add(BeanFactoryUtils.transformedBeanName(factoryBeanName));
+			}
 		}
 		return names;
 	}
