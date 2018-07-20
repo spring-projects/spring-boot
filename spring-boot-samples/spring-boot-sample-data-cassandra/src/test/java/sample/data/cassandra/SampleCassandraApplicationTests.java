d60d186c1c1549087bb8f0e2261237c4dca5eaa9
Dmytro Nosan <dimanosan@gmail.com>
2018-07-20 17:13:15 +0300

    cassandra sample refactoring

--- spring-boot-samples/spring-boot-sample-data-cassandra/src/test/java/sample/data/cassandra/SampleCassandraApplicationTests.java
+++ spring-boot-samples/spring-boot-sample-data-cassandra/src/test/java/sample/data/cassandra/SampleCassandraApplicationTests.java
@@ -16,21 +16,13 @@
 
 package sample.data.cassandra;
 
-import java.io.File;
-
-import org.cassandraunit.spring.CassandraDataSet;
-import org.cassandraunit.spring.EmbeddedCassandra;
+import com.github.nosan.embedded.cassandra.spring.EmbeddedCassandra;
 import org.junit.ClassRule;
 import org.junit.Test;
-import org.junit.rules.TestRule;
-import org.junit.runner.Description;
 import org.junit.runner.RunWith;
-import org.junit.runners.model.Statement;
 
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.rule.OutputCapture;
-import org.springframework.test.context.TestExecutionListeners;
-import org.springframework.test.context.TestExecutionListeners.MergeMode;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -39,45 +31,17 @@ import static org.assertj.core.api.Assertions.assertThat;
  * Tests for {@link SampleCassandraApplication}.
  */
 @RunWith(SpringRunner.class)
-@TestExecutionListeners(mergeMode = MergeMode.MERGE_WITH_DEFAULTS, listeners = {
-		OrderedCassandraTestExecutionListener.class })
 @SpringBootTest
-@CassandraDataSet(keyspace = "mykeyspace", value = "setup.cql")
-@EmbeddedCassandra(timeout = 60000)
+@EmbeddedCassandra("/setup.cql")
 public class SampleCassandraApplicationTests {
 
 	@ClassRule
 	public static OutputCapture outputCapture = new OutputCapture();
 
-	@ClassRule
-	public static SkipOnWindows skipOnWindows = new SkipOnWindows();
-
 	@Test
 	public void testDefaultSettings() {
-		String output = SampleCassandraApplicationTests.outputCapture.toString();
-		assertThat(output).contains("firstName='Alice', lastName='Smith'");
-	}
-
-	static class SkipOnWindows implements TestRule {
-
-		@Override
-		public Statement apply(Statement base, Description description) {
-			return new Statement() {
-
-				@Override
-				public void evaluate() throws Throwable {
-					if (!runningOnWindows()) {
-						base.evaluate();
-					}
-				}
-
-				private boolean runningOnWindows() {
-					return File.separatorChar == '\\';
-				}
-
-			};
-		}
-
+		assertThat(outputCapture.toString())
+				.contains("firstName='Alice', lastName='Smith'");
 	}
 
 }
