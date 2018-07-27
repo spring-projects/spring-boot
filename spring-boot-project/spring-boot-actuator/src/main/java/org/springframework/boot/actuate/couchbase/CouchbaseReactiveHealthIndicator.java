20ff0d97e488ab1d59d97e3ffffc17e3b7677014
Mikalai Lushchytski <mikalai.lushchytski@altoros.com>
2018-07-27 15:23:37 +0300

    Add reactive health indicator for Couchbase
    See gh-13926

---
+++ spring-boot-project/spring-boot-actuator/src/main/java/org/springframework/boot/actuate/couchbase/CouchbaseReactiveHealthIndicator.java
@@ -0,0 +1,73 @@
+/*
+ * Copyright 2012-2018 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.springframework.boot.actuate.couchbase;
+
+import com.couchbase.client.java.bucket.BucketInfo;
+import com.couchbase.client.java.cluster.ClusterInfo;
+import reactor.core.publisher.Mono;
+import rx.Observable;
+import rx.RxReactiveStreams;
+import rx.Single;
+
+import org.springframework.boot.actuate.health.AbstractReactiveHealthIndicator;
+import org.springframework.boot.actuate.health.Health;
+import org.springframework.data.couchbase.core.RxJavaCouchbaseOperations;
+import org.springframework.util.StringUtils;
+
+/**
+ * A {@link org.springframework.boot.actuate.health.ReactiveHealthIndicator} for
+ * Couchbase.
+ *
+ * @author Mikalai Lushchytski
+ * @since 2.1.0
+ */
+public class CouchbaseReactiveHealthIndicator extends AbstractReactiveHealthIndicator {
+
+	private final RxJavaCouchbaseOperations couchbaseOperations;
+
+	/**
+	 * Create a new {@link CouchbaseReactiveHealthIndicator} instance.
+	 * @param couchbaseOperations Reactive couchbase client.
+	 */
+	public CouchbaseReactiveHealthIndicator(
+			RxJavaCouchbaseOperations couchbaseOperations) {
+		this.couchbaseOperations = couchbaseOperations;
+	}
+
+	@Override
+	protected Mono<Health> doHealthCheck(Health.Builder builder) {
+		ClusterInfo cluster = this.couchbaseOperations.getCouchbaseClusterInfo();
+		String versions = StringUtils
+				.collectionToCommaDelimitedString(cluster.getAllVersions());
+		Observable<BucketInfo> bucket = this.couchbaseOperations.getCouchbaseBucket()
+				.bucketManager().async().info();
+		Single<Health> health = bucket.map(BucketInfo::nodeList)
+				.map(StringUtils::collectionToCommaDelimitedString)
+				.map((nodes) -> up(builder, versions, nodes))
+				.onErrorReturn((error) -> down(builder, error)).toSingle();
+		return Mono.from(RxReactiveStreams.toPublisher(health));
+	}
+
+	private Health up(Health.Builder builder, String versions, String nodes) {
+		return builder.up().withDetail("versions", versions).withDetail("nodes", nodes)
+				.build();
+	}
+
+	private Health down(Health.Builder builder, Throwable error) {
+		return builder.down(error).build();
+	}
+
+}
