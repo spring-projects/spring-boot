cb2618b79a7ffa208acfff343038c043e0ccf960
artsiom <a.yudovin6695@gmail.com>
2018-08-24 21:59:36 +0300

    adding custom condition for creating JwtDecoder by oidc issuer location

--- spring-boot-project/spring-boot-autoconfigure/src/main/java/org/springframework/boot/autoconfigure/security/oauth2/resource/servlet/OAuth2ResourceServerJwkConfiguration.java
+++ spring-boot-project/spring-boot-autoconfigure/src/main/java/org/springframework/boot/autoconfigure/security/oauth2/resource/servlet/OAuth2ResourceServerJwkConfiguration.java
@@ -18,7 +18,9 @@ package org.springframework.boot.autoconfigure.security.oauth2.resource.servlet;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.boot.autoconfigure.security.oauth2.resource.OAuth2ResourceServerProperties;
+import org.springframework.boot.autoconfigure.security.oauth2.resource.OidcIssuerLocationCondition;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Conditional;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.oauth2.jwt.JwtDecoder;
 import org.springframework.security.oauth2.jwt.JwtDecoders;
@@ -48,7 +50,7 @@ class OAuth2ResourceServerJwkConfiguration {
 	}
 
 	@Bean
-	@ConditionalOnProperty(name = "spring.security.oauth2.resourceserver.jwt.oidc-issuer-location")
+	@Conditional(OidcIssuerLocationCondition.class)
 	@ConditionalOnMissingBean
 	public JwtDecoder jwtDecoderByOidcIssuerLocation() {
 		return JwtDecoders
