fe3fe0fa63df25699d23a9ee02c37aae3c17a38a
Johnny Lim <izeye@naver.com>
2018-08-26 05:03:02 +0900

    Polish
    Closes gh-14202

--- spring-boot-project/spring-boot/src/test/java/org/springframework/boot/context/properties/source/AbstractPropertyMapperTests.java
+++ spring-boot-project/spring-boot/src/test/java/org/springframework/boot/context/properties/source/AbstractPropertyMapperTests.java
@@ -31,20 +31,12 @@ public abstract class AbstractPropertyMapperTests {
 	protected abstract PropertyMapper getMapper();
 
 	protected final Iterator<String> namesFromString(String name) {
-		return namesFromString(name, "value");
-	}
-
-	protected final Iterator<String> namesFromString(String name, Object value) {
 		return Arrays.stream(getMapper().map(name))
 				.map((mapping) -> mapping.getConfigurationPropertyName().toString())
 				.iterator();
 	}
 
 	protected final Iterator<String> namesFromConfiguration(String name) {
-		return namesFromConfiguration(name, "value");
-	}
-
-	protected final Iterator<String> namesFromConfiguration(String name, String value) {
 		return Arrays.stream(getMapper().map(ConfigurationPropertyName.of(name)))
 				.map(PropertyMapping::getPropertySourceName).iterator();
 	}
