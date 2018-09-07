e41394233ba73fb309cb569443de9b801f048427
Madhura Bhave <mbhave@pivotal.io>
2018-09-07 11:38:09 -0700

    Harmonize ReactiveSecurityAutoConfiguration
    Fixes gh-14263

--- spring-boot-project/spring-boot-autoconfigure/src/main/java/org/springframework/boot/autoconfigure/security/reactive/ReactiveSecurityAutoConfiguration.java
+++ spring-boot-project/spring-boot-autoconfigure/src/main/java/org/springframework/boot/autoconfigure/security/reactive/ReactiveSecurityAutoConfiguration.java
@@ -16,22 +16,40 @@
 
 package org.springframework.boot.autoconfigure.security.reactive;
 
+import reactor.core.publisher.Flux;
+
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnWebApplication;
 import org.springframework.boot.autoconfigure.security.SecurityProperties;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Import;
+import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
+import org.springframework.security.web.server.WebFilterChainProxy;
 
 /**
  * {@link EnableAutoConfiguration Auto-configuration} for Spring Security in a reactive
- * application.
+ * application. Switches on {@link EnableWebFluxSecurity} for a reactive web application
+ * if this annotation has not been added by the user. It delegates to Spring Security's
+ * content-negotiation mechanism for authentication. This configuration also backs off if
+ * a bean of type {@link WebFilterChainProxy} has been configured in any other way.
  *
  * @author Madhura Bhave
  * @since 2.0.0
  */
 @Configuration
 @EnableConfigurationProperties(SecurityProperties.class)
-@Import(WebFluxSecurityConfiguration.class)
+@ConditionalOnClass({ Flux.class, EnableWebFluxSecurity.class,
+		WebFilterChainProxy.class })
 public class ReactiveSecurityAutoConfiguration {
+
+	@Configuration
+	@ConditionalOnMissingBean(WebFilterChainProxy.class)
+	@ConditionalOnWebApplication(type = ConditionalOnWebApplication.Type.REACTIVE)
+	@EnableWebFluxSecurity
+	static class EnableWebFluxSecurityConfiguration {
+
+	}
 
 }
