cb412dd68d40482450b5e778cf4b12d5751b2e97
jnizet <jb@ninja-squad.com>
2018-09-24 18:54:40 +0200

    avoid using deprecated configurations in gradle plugin tests

--- spring-boot-project/spring-boot-tools/spring-boot-gradle-plugin/src/test/java/org/springframework/boot/gradle/plugin/KotlinPluginActionIntegrationTests.java
+++ spring-boot-project/spring-boot-tools/spring-boot-gradle-plugin/src/test/java/org/springframework/boot/gradle/plugin/KotlinPluginActionIntegrationTests.java
@@ -44,9 +44,8 @@ public class KotlinPluginActionIntegrationTests {
 
 	@Test
 	public void kotlinVersionMatchesKotlinPluginVersion() {
-		String output = this.gradleBuild
-				.build("kotlinVersion", "dependencies", "--configuration", "compile")
-				.getOutput();
+		String output = this.gradleBuild.build("kotlinVersion", "dependencies",
+				"--configuration", "compileClasspath").getOutput();
 		assertThat(output).contains("Kotlin version: 1.2.10");
 		assertThat(output)
 				.containsPattern("org.jetbrains.kotlin:kotlin-stdlib-jdk8:* -> 1.2.10");
