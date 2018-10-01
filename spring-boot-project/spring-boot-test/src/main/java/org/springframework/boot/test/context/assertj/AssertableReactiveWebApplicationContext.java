94b366bc16edd7ad0f0534b1ede24a0df6686c2f
Phillip Webb <pwebb@pivotal.io>
2018-10-01 10:22:43 -0700

    Make assertable context interfaces configurable
    Update `Assertable*ApplicationContext` interfaces so that they also
    extend the appropriate `Configurable*ApplicationContext` interface.
    
    Closes gh-14650

--- spring-boot-project/spring-boot-test/src/main/java/org/springframework/boot/test/context/assertj/AssertableReactiveWebApplicationContext.java
+++ spring-boot-project/spring-boot-test/src/main/java/org/springframework/boot/test/context/assertj/AssertableReactiveWebApplicationContext.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2012-2017 the original author or authors.
+ * Copyright 2012-2018 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -35,7 +35,7 @@ import org.springframework.boot.web.reactive.context.ReactiveWebApplicationConte
  */
 public interface AssertableReactiveWebApplicationContext extends
 		ApplicationContextAssertProvider<ConfigurableReactiveWebApplicationContext>,
-		ReactiveWebApplicationContext {
+		ConfigurableReactiveWebApplicationContext {
 
 	/**
 	 * Factory method to create a new {@link AssertableReactiveWebApplicationContext}
