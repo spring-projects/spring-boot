ff35d14170ea8f1654707c636169f6e002e7afef
Madhura Bhave <mbhave@pivotal.io>
2018-10-11 13:40:57 -0700

    Change order of loggingCodecCustomizer
    It is set to 0 so that users' customizers can be positioned before and
    after it.
    
    Fixes gh-14723

--- spring-boot-project/spring-boot-autoconfigure/src/test/java/org/springframework/boot/autoconfigure/http/codec/CodecsAutoConfigurationTests.java
+++ spring-boot-project/spring-boot-autoconfigure/src/test/java/org/springframework/boot/autoconfigure/http/codec/CodecsAutoConfigurationTests.java
@@ -15,14 +15,18 @@
  */
 package org.springframework.boot.autoconfigure.http.codec;
 
+import java.lang.reflect.Method;
+
 import org.junit.Test;
 
 import org.springframework.boot.autoconfigure.AutoConfigurations;
+import org.springframework.boot.autoconfigure.http.HttpProperties;
 import org.springframework.boot.test.context.runner.WebApplicationContextRunner;
 import org.springframework.boot.web.codec.CodecCustomizer;
+import org.springframework.core.annotation.AnnotationAwareOrderComparator;
 import org.springframework.http.codec.CodecConfigurer;
 import org.springframework.http.codec.support.DefaultClientCodecConfigurer;
-import org.springframework.test.util.ReflectionTestUtils;
+import org.springframework.util.ReflectionUtils;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -38,23 +42,48 @@ public class CodecsAutoConfigurationTests {
 
 	@Test
 	public void autoConfigShouldProvideALoggingRequestDetailsCustomizer() {
-		this.contextRunner.withPropertyValues("spring.http.log-request-details=false")
-				.run((context) -> {
-					CodecCustomizer customizer = context.getBean(CodecCustomizer.class);
-					CodecConfigurer configurer = new DefaultClientCodecConfigurer();
-					customizer.customize(configurer);
-					boolean enableLoggingRequestDetails = (boolean) ReflectionTestUtils
-							.getField(configurer.defaultCodecs(),
-									"enableLoggingRequestDetails");
-					assertThat(enableLoggingRequestDetails).isFalse();
-				});
+		this.contextRunner.run((context) -> {
+			CodecCustomizer customizer = context.getBean(CodecCustomizer.class);
+			CodecConfigurer configurer = new DefaultClientCodecConfigurer();
+			customizer.customize(configurer);
+			assertThat(configurer.defaultCodecs())
+					.hasFieldOrPropertyWithValue("enableLoggingRequestDetails", false);
+		});
 
 	}
 
 	@Test
-	public void autoConfigWhenLoggingRequestDetailsPropertyNotSetShouldNotProvideCustomizer() {
-		this.contextRunner.run(
-				(context) -> assertThat(context).doesNotHaveBean(CodecCustomizer.class));
+	public void loggingRequestDetailsCustomizerShouldUseHttpProperties() {
+		this.contextRunner.withPropertyValues("spring.http.log-request-details=true")
+				.run((context) -> {
+					CodecCustomizer customizer = context.getBean(CodecCustomizer.class);
+					CodecConfigurer configurer = new DefaultClientCodecConfigurer();
+					customizer.customize(configurer);
+					assertThat(configurer.defaultCodecs()).hasFieldOrPropertyWithValue(
+							"enableLoggingRequestDetails", true);
+				});
+	}
+
+	@Test
+	public void loggingRequestDetailsBeanShouldHaveOrderZero() {
+		this.contextRunner.run((context) -> {
+			Method customizerMethod = ReflectionUtils.findMethod(
+					CodecsAutoConfiguration.LoggingCodecConfiguration.class,
+					"loggingCodecCustomizer", HttpProperties.class);
+			Integer order = new TestAnnotationAwareOrderComparator()
+					.findOrder(customizerMethod);
+			assertThat(order).isEqualTo(0);
+		});
+	}
+
+	static class TestAnnotationAwareOrderComparator
+			extends AnnotationAwareOrderComparator {
+
+		@Override
+		public Integer findOrder(Object obj) {
+			return super.findOrder(obj);
+		}
+
 	}
 
 }
