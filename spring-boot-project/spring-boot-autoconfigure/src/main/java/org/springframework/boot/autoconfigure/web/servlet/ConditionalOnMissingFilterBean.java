44a46f1514cb22b0b9e9b5b066ae63e4acf472fa
Phillip Webb <pwebb@pivotal.io>
2018-10-23 17:49:14 -0700

    Add @ConditionalOnMissingFilterBean
    Add a new condition that can be used to check for servlet `Filter`
    beans that are either registered directly, or via a
    `FilterRegistrationBean`.
    
    Closes gh-14940

---
+++ spring-boot-project/spring-boot-autoconfigure/src/main/java/org/springframework/boot/autoconfigure/web/servlet/ConditionalOnMissingFilterBean.java
@@ -0,0 +1,58 @@
+/*
+ * Copyright 2012-2018 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.springframework.boot.autoconfigure.web.servlet;
+
+import java.lang.annotation.Documented;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+import javax.servlet.Filter;
+
+import org.springframework.beans.factory.BeanFactory;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Conditional;
+import org.springframework.core.annotation.AliasFor;
+
+/**
+ * {@link Conditional} that only matches when no {@link Filter} beans of the specified
+ * type are contained in the {@link BeanFactory}. This condition will detect both directly
+ * register {@link Filter} beans as well as those registered via a
+ * {@link FilterRegistrationBean}.
+ * <p>
+ * When placed on a {@code @Bean} method, the bean class defaults to the return type of
+ * the factory method:
+ *
+ * @author Phillip Webb
+ * @since 2.1.0
+ */
+@Target({ ElementType.TYPE, ElementType.METHOD })
+@Retention(RetentionPolicy.RUNTIME)
+@Documented
+@ConditionalOnMissingBean(parameterizedContainer = FilterRegistrationBean.class)
+public @interface ConditionalOnMissingFilterBean {
+
+	/**
+	 * The filter bean type that must not be present.
+	 * @return the bean type
+	 */
+	@AliasFor(annotation = ConditionalOnMissingBean.class)
+	Class<? extends Filter>[] value() default {};
+
+}
