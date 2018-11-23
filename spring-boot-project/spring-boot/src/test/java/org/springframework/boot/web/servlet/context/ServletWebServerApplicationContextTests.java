548cc05bb631efd7788a9461c44c77d279e6ffb0
Andy Wilkinson <awilkinson@pivotal.io>
2018-11-23 19:33:33 +0000

    Polish

--- spring-boot-project/spring-boot/src/test/java/org/springframework/boot/web/servlet/context/ServletWebServerApplicationContextTests.java
+++ spring-boot-project/spring-boot/src/test/java/org/springframework/boot/web/servlet/context/ServletWebServerApplicationContextTests.java
@@ -31,6 +31,7 @@ import javax.servlet.ServletResponse;
 
 import org.junit.After;
 import org.junit.Before;
+import org.junit.Rule;
 import org.junit.Test;
 import org.mockito.ArgumentCaptor;
 import org.mockito.Captor;
