510a8e2ec16d93de743d2d475929ca3e66c3ec2a
ayudovin <a.yudovin6695@gmail.com>
2018-12-25 16:46:27 +0300

    Disable Hibernate entity scanning for default JPA setup
    See gh-15565

--- spring-boot-project/spring-boot-autoconfigure/src/main/java/org/springframework/boot/autoconfigure/orm/jpa/HibernateProperties.java
+++ spring-boot-project/spring-boot-autoconfigure/src/main/java/org/springframework/boot/autoconfigure/orm/jpa/HibernateProperties.java
@@ -34,6 +34,7 @@ import org.springframework.util.StringUtils;
  * Configuration properties for Hibernate.
  *
  * @author Stephane Nicoll
+ * @author Artsiom Yudovin
  * @since 2.1.0
  * @see JpaProperties
  */
@@ -95,6 +96,7 @@ public class HibernateProperties {
 			HibernateSettings settings) {
 		Map<String, Object> result = new HashMap<>(existing);
 		applyNewIdGeneratorMappings(result);
+		applyArchiveScanner(result);
 		getNaming().applyNamingStrategies(result);
 		String ddlAuto = determineDdlAuto(existing, settings::getDdlAuto);
 		if (StringUtils.hasText(ddlAuto) && !"none".equals(ddlAuto)) {
@@ -118,6 +120,13 @@ public class HibernateProperties {
 		}
 		else if (!result.containsKey(AvailableSettings.USE_NEW_ID_GENERATOR_MAPPINGS)) {
 			result.put(AvailableSettings.USE_NEW_ID_GENERATOR_MAPPINGS, "true");
+		}
+	}
+
+	private void applyArchiveScanner(Map<String, Object> result) {
+		if (!result.containsKey(AvailableSettings.SCANNER)) {
+			result.put(AvailableSettings.SCANNER,
+					"org.hibernate.boot.archive.scan.internal.DisabledScanner");
 		}
 	}
 
