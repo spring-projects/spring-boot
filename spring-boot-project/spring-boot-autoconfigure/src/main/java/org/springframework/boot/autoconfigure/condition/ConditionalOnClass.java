4172d999ff401a387c35d7f939fcd4ebf7907d05
Chintan Pandya <38644871+cpandya231@users.noreply.github.com>
2018-12-29 22:47:30 +0530

    Updated ConditionalOnClass javadoc

--- spring-boot-project/spring-boot-autoconfigure/src/main/java/org/springframework/boot/autoconfigure/condition/ConditionalOnClass.java
+++ spring-boot-project/spring-boot-autoconfigure/src/main/java/org/springframework/boot/autoconfigure/condition/ConditionalOnClass.java
@@ -26,7 +26,7 @@ import org.springframework.context.annotation.Conditional;
 
 /**
  * {@link Conditional} that only matches when the specified classes are on the classpath.
- *
+ * @ConditionalOnClass should not be used directly on methods.
  * @author Phillip Webb
  */
 @Target({ ElementType.TYPE, ElementType.METHOD })
