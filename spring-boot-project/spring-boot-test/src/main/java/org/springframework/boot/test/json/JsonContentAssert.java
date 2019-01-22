b0dcc7643b23bd16d807971c7bc16a896affb9b8
Johnny Lim <izeye@naver.com>
2019-01-22 09:02:49 +0900

    Fix typo

--- spring-boot-project/spring-boot-test/src/main/java/org/springframework/boot/test/json/JsonContentAssert.java
+++ spring-boot-project/spring-boot-test/src/main/java/org/springframework/boot/test/json/JsonContentAssert.java
@@ -97,7 +97,7 @@ public class JsonContentAssert extends AbstractAssert<JsonContentAssert, CharSeq
 		if (expected instanceof Resource) {
 			return isEqualToJson((Resource) expected);
 		}
-		failWithMessage("Unsupport type for JSON assert {}", expected.getClass());
+		failWithMessage("Unsupported type for JSON assert {}", expected.getClass());
 		return null;
 	}
 
@@ -434,7 +434,7 @@ public class JsonContentAssert extends AbstractAssert<JsonContentAssert, CharSeq
 		if (expected instanceof Resource) {
 			return isNotEqualToJson((Resource) expected);
 		}
-		failWithMessage("Unsupported type for JSON assert {]", expected.getClass());
+		failWithMessage("Unsupported type for JSON assert {}", expected.getClass());
 		return null;
 	}
 
