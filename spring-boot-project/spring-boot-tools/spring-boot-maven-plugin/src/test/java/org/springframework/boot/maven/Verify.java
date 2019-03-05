d6a869fa98eaeb3c180a8a3d94dcce8e3f51e204
Andy Wilkinson <awilkinson@pivotal.io>
2019-03-05 20:20:52 +0000

    Switch to Jakarta EE API dependencies where possible
    Closes gh-16113
    Closes gh-16112
    Closes gh-16111
    Closes gh-15916
    Closes gh-15689

--- spring-boot-project/spring-boot-tools/spring-boot-maven-plugin/src/test/java/org/springframework/boot/maven/Verify.java
+++ spring-boot-project/spring-boot-tools/spring-boot-maven-plugin/src/test/java/org/springframework/boot/maven/Verify.java
@@ -230,7 +230,7 @@ public final class Verify {
 			super.verifyZipEntries(verifier);
 			verifier.assertHasEntryNameStartingWith("BOOT-INF/lib/spring-context");
 			verifier.assertHasEntryNameStartingWith("BOOT-INF/lib/spring-core");
-			verifier.assertHasEntryNameStartingWith("BOOT-INF/lib/javax.servlet-api-4");
+			verifier.assertHasEntryNameStartingWith("BOOT-INF/lib/jakarta.servlet-api-4");
 			assertThat(verifier
 					.hasEntry("org/springframework/boot/loader/JarLauncher.class"))
 							.as("Unpacked launcher classes").isTrue();
@@ -263,7 +263,7 @@ public final class Verify {
 			verifier.assertHasEntryNameStartingWith("WEB-INF/lib/spring-context");
 			verifier.assertHasEntryNameStartingWith("WEB-INF/lib/spring-core");
 			verifier.assertHasEntryNameStartingWith(
-					"WEB-INF/lib-provided/javax.servlet-api-4");
+					"WEB-INF/lib-provided/jakarta.servlet-api-4");
 			assertThat(verifier
 					.hasEntry("org/" + "springframework/boot/loader/JarLauncher.class"))
 							.as("Unpacked launcher classes").isTrue();
@@ -314,7 +314,7 @@ public final class Verify {
 			super.verifyZipEntries(verifier);
 			verifier.assertHasEntryNameStartingWith("lib/spring-context");
 			verifier.assertHasEntryNameStartingWith("lib/spring-core");
-			verifier.assertHasNoEntryNameStartingWith("lib/javax.servlet-api");
+			verifier.assertHasNoEntryNameStartingWith("lib/jakarta.servlet-api");
 			assertThat(verifier
 					.hasEntry("org/" + "springframework/boot/loader/JarLauncher.class"))
 							.as("Unpacked launcher classes").isFalse();
