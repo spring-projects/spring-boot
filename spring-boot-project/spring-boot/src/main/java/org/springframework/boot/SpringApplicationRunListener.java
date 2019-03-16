d6250250512008505982b6ff409fe283e20f5834
Stephane Nicoll <snicoll@pivotal.io>
2019-03-16 08:02:13 +0100

    Polish "Add default no-op method to SpringApplicationRunListener"
    Closes gh-16104

--- spring-boot-project/spring-boot/src/main/java/org/springframework/boot/SpringApplicationRunListener.java
+++ spring-boot-project/spring-boot/src/main/java/org/springframework/boot/SpringApplicationRunListener.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2012-2018 the original author or authors.
+ * Copyright 2012-2019 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -31,7 +31,6 @@ import org.springframework.core.io.support.SpringFactoriesLoader;
  * @author Phillip Webb
  * @author Dave Syer
  * @author Andy Wilkinson
- * @author Artsiom Yudovin
  */
 public interface SpringApplicationRunListener {
 
