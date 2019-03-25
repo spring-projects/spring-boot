567a82f4bad28ba9beb76aeebae5a74c02a39dee
Stephane Nicoll <snicoll@pivotal.io>
2019-03-25 16:13:24 +0100

    Update multi release test jar to include Java 12
    See gh-16212

--- spring-boot-project/spring-boot-tools/spring-boot-loader/src/test/java/org/springframework/boot/loader/TestJarCreator.java
+++ spring-boot-project/spring-boot-tools/spring-boot-loader/src/test/java/org/springframework/boot/loader/TestJarCreator.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2012-2018 the original author or authors.
+ * Copyright 2012-2019 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -95,6 +95,7 @@ public abstract class TestJarCreator {
 			writeEntry(jarOutputStream, "META-INF/versions/9/multi-release.dat", 9);
 			writeEntry(jarOutputStream, "META-INF/versions/10/multi-release.dat", 10);
 			writeEntry(jarOutputStream, "META-INF/versions/11/multi-release.dat", 11);
+			writeEntry(jarOutputStream, "META-INF/versions/12/multi-release.dat", 12);
 		}
 		else {
 			writeEntry(jarOutputStream, "3.dat", 3);
