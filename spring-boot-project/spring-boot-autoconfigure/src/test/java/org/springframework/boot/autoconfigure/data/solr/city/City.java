03a5bb785729573b3f25d81cd3ae4e7a64e664ce
dreis2211 <christoph.dreis@freenet.de>
2019-04-02 20:13:57 +0200

    Avoid using deprecated @SolrDocument#solrCoreName
    Closes gh-16419

--- spring-boot-project/spring-boot-autoconfigure/src/test/java/org/springframework/boot/autoconfigure/data/solr/city/City.java
+++ spring-boot-project/spring-boot-autoconfigure/src/test/java/org/springframework/boot/autoconfigure/data/solr/city/City.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2012-2017 the original author or authors.
+ * Copyright 2012-2019 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -23,7 +23,7 @@ import org.springframework.data.solr.core.mapping.SolrDocument;
 /**
  * @author Christoph Strobl
  */
-@SolrDocument(solrCoreName = "collection1")
+@SolrDocument(collection = "collection1")
 public class City {
 
 	@Id
