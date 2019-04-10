c7a0ef7ac9af3ce1cac435fdcd7f131bba7fa7a0
maly7 <paul.aly@outlook.com>
2019-04-09 20:18:54 -0400

    add two tests for key serialization and deserialization

---
+++ spring-boot-project/spring-boot/src/test/java/org/springframework/boot/jackson/NameAndAgeJsonKeyComponent.java
@@ -0,0 +1,28 @@
+package org.springframework.boot.jackson;
+
+import com.fasterxml.jackson.core.JsonGenerator;
+import com.fasterxml.jackson.databind.DeserializationContext;
+import com.fasterxml.jackson.databind.JsonSerializer;
+import com.fasterxml.jackson.databind.KeyDeserializer;
+import com.fasterxml.jackson.databind.SerializerProvider;
+
+import java.io.IOException;
+
+@JsonComponent(handle = JsonComponent.Handle.KEYS)
+public class NameAndAgeJsonKeyComponent {
+
+	public static class Serializer extends JsonSerializer<NameAndAge> {
+
+		@Override
+		public void serialize(NameAndAge value, JsonGenerator jgen, SerializerProvider serializers) throws IOException {
+			jgen.writeString(value.asKey());
+		}
+	}
+
+	public static class Deserializer extends KeyDeserializer {
+		@Override
+		public NameAndAge deserializeKey(String key, DeserializationContext ctxt) throws IOException {
+			return new NameAndAge(key);
+		}
+	}
+}
