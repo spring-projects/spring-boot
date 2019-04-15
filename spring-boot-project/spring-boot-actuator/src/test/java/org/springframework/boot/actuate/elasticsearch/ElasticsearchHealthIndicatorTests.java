2e107929a02dad25a8769b765a6733ff317d426a
Brian Clozel <bclozel@pivotal.io>
2019-04-16 00:21:17 +0200

    Upgrade to Elasticsearch 6.7.1
    Closes gh-16569

--- spring-boot-project/spring-boot-actuator/src/test/java/org/springframework/boot/actuate/elasticsearch/ElasticsearchHealthIndicatorTests.java
+++ spring-boot-project/spring-boot-actuator/src/test/java/org/springframework/boot/actuate/elasticsearch/ElasticsearchHealthIndicatorTests.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2012-2018 the original author or authors.
+ * Copyright 2012-2019 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -176,7 +176,7 @@ public class ElasticsearchHealthIndicatorTests {
 			super("test-cluster", new String[0],
 					new ClusterState(null, 0, null, null, RoutingTable.builder().build(),
 							DiscoveryNodes.builder().build(),
-							ClusterBlocks.builder().build(), null, false));
+							ClusterBlocks.builder().build(), null, 1, false));
 			this.status = status;
 		}
 
