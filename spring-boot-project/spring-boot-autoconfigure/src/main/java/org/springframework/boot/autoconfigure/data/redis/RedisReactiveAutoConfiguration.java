5a6607a66fac330cd65b5cb790d57331ab0b8e37
Qiang Lee <18018968632@163.com>
2019-04-19 17:47:02 +0800

    Instantiate default ReactiveStringRedisTemplate bean in RedisReactiveAutoConfiguration
    Like the @Bean-annotated method {stringRedisTemplate()} in @Configuration-annotated class RedisAutoConfiguration, add a @Bean-annotated method {reactiveRedisTemplate()} in class RedisReactiveAutoConfiguration, which avoids unnecessary repeated configuration when used in different applications.

--- spring-boot-project/spring-boot-autoconfigure/src/main/java/org/springframework/boot/autoconfigure/data/redis/RedisReactiveAutoConfiguration.java
+++ spring-boot-project/spring-boot-autoconfigure/src/main/java/org/springframework/boot/autoconfigure/data/redis/RedisReactiveAutoConfiguration.java
@@ -28,6 +28,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.core.io.ResourceLoader;
 import org.springframework.data.redis.connection.ReactiveRedisConnectionFactory;
 import org.springframework.data.redis.core.ReactiveRedisTemplate;
+import org.springframework.data.redis.core.ReactiveStringRedisTemplate;
 import org.springframework.data.redis.serializer.JdkSerializationRedisSerializer;
 import org.springframework.data.redis.serializer.RedisSerializationContext;
 
@@ -60,4 +61,9 @@ public class RedisReactiveAutoConfiguration {
 				serializationContext);
 	}
 
+	@Bean
+    @ConditionalOnMissingBean
+    public ReactiveStringRedisTemplate reactiveStringRedisTemplate(ReactiveRedisConnectionFactory factory) {
+        return new ReactiveStringRedisTemplate(factory);
+    }
 }
