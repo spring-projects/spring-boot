52c2c534dba865234ea090df107264cf41a6e5e1
daonan.zhan <daonan.zhan@gmail.com>
2019-04-22 10:07:31 +0800

    Polish Maven Plugin's tests
    See gh-16618

--- spring-boot-project/spring-boot-tools/spring-boot-maven-plugin/src/it/jar-exclude-entry/verify.groovy
+++ spring-boot-project/spring-boot-tools/spring-boot-maven-plugin/src/it/jar-exclude-entry/verify.groovy
@@ -22,6 +22,6 @@ new Verify.JarArchiveVerification(f, Verify.SAMPLE_APP) {
 	@Override
 	protected void verifyZipEntries(Verify.ArchiveVerifier verifier) throws Exception {
 		super.verifyZipEntries(verifier)
-		verifier.assertHasNoEntryNameStartingWith("lib/servlet-api-2.5.jar")
+		verifier.assertHasNoEntryNameStartingWith("BOOT-INF/lib/servlet-api-2.5.jar")
 	}
 }.verify()
