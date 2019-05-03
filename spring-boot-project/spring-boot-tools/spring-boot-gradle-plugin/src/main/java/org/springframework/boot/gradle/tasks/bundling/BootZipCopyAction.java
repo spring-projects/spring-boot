f35b91a17260413b4c6f14e98ef2042cea501cf5
Andy Wilkinson <awilkinson@pivotal.io>
2019-05-03 13:07:15 +0100

    Raise minimum supported version of Gradle to 4.10 and encourage use of 5.x
    Closes gh-16681

--- spring-boot-project/spring-boot-tools/spring-boot-gradle-plugin/src/main/java/org/springframework/boot/gradle/tasks/bundling/BootZipCopyAction.java
+++ spring-boot-project/spring-boot-tools/spring-boot-gradle-plugin/src/main/java/org/springframework/boot/gradle/tasks/bundling/BootZipCopyAction.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2012-2018 the original author or authors.
+ * Copyright 2012-2019 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -119,7 +119,6 @@ class BootZipCopyAction implements CopyAction {
 		return () -> true;
 	}
 
-	@SuppressWarnings("unchecked")
 	private Spec<FileTreeElement> createExclusionSpec(
 			Spec<FileTreeElement> loaderEntries) {
 		return Specs.union(loaderEntries, this.exclusions);
