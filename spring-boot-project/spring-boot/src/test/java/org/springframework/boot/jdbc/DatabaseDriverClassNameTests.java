f8eb230dea9e9738d3f974d19ca884dd2689e0ad
Andy Wilkinson <awilkinson@pivotal.io>
2019-05-20 17:17:44 +0100

    Ignore Redshift in DatabaseDriverClassNameTests
    Closes gh-16831

--- spring-boot-project/spring-boot/src/test/java/org/springframework/boot/jdbc/DatabaseDriverClassNameTests.java
+++ spring-boot-project/spring-boot/src/test/java/org/springframework/boot/jdbc/DatabaseDriverClassNameTests.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2012-2018 the original author or authors.
+ * Copyright 2012-2019 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -46,7 +46,8 @@ public class DatabaseDriverClassNameTests {
 	private static final Set<DatabaseDriver> EXCLUDED_DRIVERS = Collections
 			.unmodifiableSet(EnumSet.of(DatabaseDriver.UNKNOWN, DatabaseDriver.ORACLE,
 					DatabaseDriver.DB2, DatabaseDriver.DB2_AS400, DatabaseDriver.INFORMIX,
-					DatabaseDriver.HANA, DatabaseDriver.TERADATA));
+					DatabaseDriver.HANA, DatabaseDriver.TERADATA,
+					DatabaseDriver.REDSHIFT));
 
 	private final String className;
 
