275d7945535a3a13fefc061615e4210ce687a75e
Stephane Nicoll <snicoll@pivotal.io>
2019-05-20 10:52:00 +0200

    Make sure JdbcTemplateConfiguration is imported once
    Closes gh-13806

--- spring-boot-project/spring-boot-autoconfigure/src/main/java/org/springframework/boot/autoconfigure/jdbc/NamedParameterJdbcTemplateConfiguration.java
+++ spring-boot-project/spring-boot-autoconfigure/src/main/java/org/springframework/boot/autoconfigure/jdbc/NamedParameterJdbcTemplateConfiguration.java
@@ -16,35 +16,30 @@
 
 package org.springframework.boot.autoconfigure.jdbc;
 
-import javax.sql.DataSource;
-
-import org.springframework.boot.autoconfigure.AutoConfigureAfter;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnSingleCandidate;
-import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Import;
+import org.springframework.context.annotation.Primary;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcOperations;
 import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
 
 /**
- * {@link EnableAutoConfiguration Auto-configuration} for {@link JdbcTemplate} and
- * {@link NamedParameterJdbcTemplate}.
+ * Configuration for {@link NamedParameterJdbcTemplate}.
  *
- * @author Dave Syer
- * @author Phillip Webb
  * @author Stephane Nicoll
- * @author Kazuki Shimizu
- * @since 1.4.0
  */
 @Configuration(proxyBeanMethods = false)
-@ConditionalOnClass({ DataSource.class, JdbcTemplate.class })
-@ConditionalOnSingleCandidate(DataSource.class)
-@AutoConfigureAfter(DataSourceAutoConfiguration.class)
-@EnableConfigurationProperties(JdbcProperties.class)
-@Import({ JdbcTemplateConfiguration.class,
-		NamedParameterJdbcTemplateConfiguration.class })
-public class JdbcTemplateAutoConfiguration {
+@ConditionalOnSingleCandidate(JdbcTemplate.class)
+@ConditionalOnMissingBean(NamedParameterJdbcOperations.class)
+class NamedParameterJdbcTemplateConfiguration {
+
+	@Bean
+	@Primary
+	public NamedParameterJdbcTemplate namedParameterJdbcTemplate(
+			JdbcTemplate jdbcTemplate) {
+		return new NamedParameterJdbcTemplate(jdbcTemplate);
+	}
 
 }
