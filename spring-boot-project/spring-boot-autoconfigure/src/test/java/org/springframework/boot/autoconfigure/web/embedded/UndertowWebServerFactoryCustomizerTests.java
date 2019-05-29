38c21f58b85237c776e3195d11004c213aa4cfa5
dreis2211 <christoph.dreis@freenet.de>
2019-05-29 19:09:59 +0200

    Add missing @Test annotation
    See gh-17012

--- spring-boot-project/spring-boot-autoconfigure/src/test/java/org/springframework/boot/autoconfigure/web/embedded/UndertowWebServerFactoryCustomizerTests.java
+++ spring-boot-project/spring-boot-autoconfigure/src/test/java/org/springframework/boot/autoconfigure/web/embedded/UndertowWebServerFactoryCustomizerTests.java
@@ -100,6 +100,7 @@ public class UndertowWebServerFactoryCustomizerTests {
 		assertThat(boundServerOption(UndertowOptions.MAX_HEADER_SIZE)).isNull();
 	}
 
+	@Test
 	public void customMaxHttpHeaderSizeIgnoredIfZero() {
 		bind("server.max-http-header-size=0");
 		assertThat(boundServerOption(UndertowOptions.MAX_HEADER_SIZE)).isNull();
