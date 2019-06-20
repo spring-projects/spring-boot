98c546d663ff2934d5d6c34d8c57910844494115
Johnny Lim <izeye@naver.com>
2019-06-20 14:08:28 +0900

    Use "JUnit" instead of "Junit"

--- spring-boot-samples/spring-boot-sample-junit-vintage/src/main/java/sample/SampleJUnitVintageApplication.java
+++ spring-boot-samples/spring-boot-sample-junit-vintage/src/main/java/sample/SampleJUnitVintageApplication.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2012-2017 the original author or authors.
+ * Copyright 2012-2019 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -20,10 +20,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SampleJunitJupiterApplication {
+public class SampleJUnitVintageApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SampleJunitJupiterApplication.class, args);
+		SpringApplication.run(SampleJUnitVintageApplication.class, args);
 	}
 
 }
