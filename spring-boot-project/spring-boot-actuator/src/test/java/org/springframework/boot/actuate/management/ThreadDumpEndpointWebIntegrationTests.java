c5cae282613d4229a8e4e9b0c3de5a1924d177a9
Andy Wilkinson <awilkinson@pivotal.io>
2019-07-03 21:44:06 +0100

    Add support for plain text thread dumps to the threaddump endpoint
    When a request that accepts text/plain is received, the threaddump
    endpoint will now return a thread dump in plain text. The format of
    this text is modelled after the output produced by JVisualVM when
    connecting to a remote process over JMX. Note that this output does
    not include all of the information in, for example, JStack's output
    as it is not available via Java 8's ThreadInfo API.
    
    Rather than the custom formatting logic, using ThreadInfo's toString()
    method was considered but its output is documented as being undefined
    and implementation specific. The implementation used while developing
    this feature produced output that did not match that of JStack or
    JVisualVM and truncated stack traces quite considerably.
    
    At the time of writing the format produced by the endpoint could be
    consumed by both Thread Dump Analyzer [1] and https://fastthread.io.
    
    Closes gh-2339
    
    [1] https://github.com/irockel/tda

---
+++ spring-boot-project/spring-boot-actuator/src/test/java/org/springframework/boot/actuate/management/ThreadDumpEndpointWebIntegrationTests.java
@@ -0,0 +1,59 @@
+/*
+ * Copyright 2012-2019 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.springframework.boot.actuate.management;
+
+import org.springframework.boot.actuate.endpoint.web.test.WebEndpointTest;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.reactive.server.WebTestClient;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+/**
+ * Integration tests for {@link ThreadDumpEndpoint} exposed by Jersey, Spring MVC, and
+ * WebFlux.
+ *
+ * @author Andy Wilkinson
+ */
+class ThreadDumpEndpointWebIntegrationTests {
+
+	@WebEndpointTest
+	void getRequestWithJsonAcceptHeaderShouldProduceJsonThreadDumpResponse(WebTestClient client) throws Exception {
+		client.get().uri("/actuator/threaddump").accept(MediaType.APPLICATION_JSON).exchange().expectStatus().isOk()
+				.expectHeader().contentType(MediaType.APPLICATION_JSON);
+	}
+
+	@WebEndpointTest
+	void getRequestWithTextPlainAcceptHeaderShouldProduceTextPlainResponse(WebTestClient client) throws Exception {
+		String response = client.get().uri("/actuator/threaddump").accept(MediaType.TEXT_PLAIN).exchange()
+				.expectStatus().isOk().expectHeader().contentType("text/plain;charset=UTF-8").expectBody(String.class)
+				.returnResult().getResponseBody();
+		assertThat(response).contains("Full thread dump");
+	}
+
+	@Configuration(proxyBeanMethods = false)
+	public static class TestConfiguration {
+
+		@Bean
+		public ThreadDumpEndpoint endpoint() {
+			return new ThreadDumpEndpoint();
+		}
+
+	}
+
+}
