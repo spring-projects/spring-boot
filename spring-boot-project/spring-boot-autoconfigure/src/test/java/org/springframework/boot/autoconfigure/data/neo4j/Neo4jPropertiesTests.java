c5241f0e82f481fa0aa8df64a0cb7b813b875eff
Andy Wilkinson <awilkinson@pivotal.io>
2019-07-09 14:01:57 +0100

    Remove accidental usage of com.hazelcast.util.Base64
    Closes gh-17461

--- spring-boot-project/spring-boot-autoconfigure/src/test/java/org/springframework/boot/autoconfigure/data/neo4j/Neo4jPropertiesTests.java
+++ spring-boot-project/spring-boot-autoconfigure/src/test/java/org/springframework/boot/autoconfigure/data/neo4j/Neo4jPropertiesTests.java
@@ -16,7 +16,8 @@
 
 package org.springframework.boot.autoconfigure.data.neo4j;
 
-import com.hazelcast.util.Base64;
+import java.util.Base64;
+
 import org.junit.After;
 import org.junit.Test;
 import org.neo4j.ogm.config.AutoIndexMode;
@@ -149,10 +150,8 @@ public class Neo4jPropertiesTests {
 			assertThat(credentials).isNotNull();
 			Object content = credentials.credentials();
 			assertThat(content).isInstanceOf(String.class);
-			String[] auth = new String(Base64.decode(((String) content).getBytes())).split(":");
-			assertThat(auth[0]).isEqualTo(username);
-			assertThat(auth[1]).isEqualTo(password);
-			assertThat(auth).hasSize(2);
+			String[] auth = new String(Base64.getDecoder().decode((String) content)).split(":");
+			assertThat(auth).containsExactly(username, password);
 		}
 	}
 
