3c28622ebc81444974859685b14a663697612e23
Brian Clozel <bclozel@pivotal.io>
2019-07-10 12:05:40 +0200

    Add support for reactor-tools debug agent
    The `reactor-tools` dependency now brings a new Reactor Debug Agent
    which instruments loaded classes for better Reactor stacktraces.
    This commit removes the `spring.reactor.stacktrace-mode.enabled`
    configuration property since the related Reactor Hook is about to be
    removed.
    As a replacement, we're introducing `spring.reactor.debug-agent.enabled`
    which tells whether the Reactor Debug Agent should be loaded, given that
    the `reactor-tools` dependency is available. This option is enabled by
    default, since adding the dependency on classpath is a strong signal
    already.
    
    Fixes gh-17128

--- spring-boot-project/spring-boot/src/test/java/org/springframework/boot/reactor/InstrumentedFluxProvider.java
+++ spring-boot-project/spring-boot/src/test/java/org/springframework/boot/reactor/InstrumentedFluxProvider.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2012-2018 the original author or authors.
+ * Copyright 2012-2019 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -14,7 +14,20 @@
  * limitations under the License.
  */
 
+package org.springframework.boot.reactor;
+
+import reactor.core.publisher.Flux;
+
 /**
- * Auto-configuration for Reactor Core.
+ * Utility class that should be instrumented by the reactor debug agent.
+ *
+ * @author Brian Clozel
+ * @see DebugAgentEnvironmentPostProcessorTests
  */
-package org.springframework.boot.autoconfigure.reactor.core;
+class InstrumentedFluxProvider {
+
+	Flux<Integer> newFluxJust() {
+		return Flux.just(1);
+	}
+
+}
