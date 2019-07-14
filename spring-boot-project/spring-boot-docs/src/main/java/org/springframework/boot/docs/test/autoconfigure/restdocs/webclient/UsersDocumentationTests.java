93bcc3dcf5d87d3d6314afb235925012af1f9be7
wonwoo <aoruqjfu@gmail.com>
2019-07-14 20:51:59 +0900

    Update documentation to use JUnit Jupiter
    See gh-17507

--- spring-boot-project/spring-boot-docs/src/main/java/org/springframework/boot/docs/test/autoconfigure/restdocs/webclient/UsersDocumentationTests.java
+++ spring-boot-project/spring-boot-docs/src/main/java/org/springframework/boot/docs/test/autoconfigure/restdocs/webclient/UsersDocumentationTests.java
@@ -18,20 +18,17 @@ package org.springframework.boot.docs.test.autoconfigure.restdocs.webclient;
 
 // tag::source[]
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.restdocs.AutoConfigureRestDocs;
 import org.springframework.boot.test.autoconfigure.web.reactive.WebFluxTest;
-import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.reactive.server.WebTestClient;
 
 import static org.springframework.restdocs.webtestclient.WebTestClientRestDocumentation.document;
 
-@ExtendWith(SpringExtension.class)
 @WebFluxTest
 @AutoConfigureRestDocs
-public class UsersDocumentationTests {
+class UsersDocumentationTests {
 
 	@Autowired
 	private WebTestClient webTestClient;
