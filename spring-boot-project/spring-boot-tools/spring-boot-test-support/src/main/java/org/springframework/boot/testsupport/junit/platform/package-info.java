b3f70b4832c8580093e723142213d5ef589988ed
dreis2211 <christoph.dreis@freenet.de>
2019-07-15 20:41:34 +0200

    Polish testsupport.junit.platform package

--- spring-boot-project/spring-boot-tools/spring-boot-test-support/src/main/java/org/springframework/boot/testsupport/junit/platform/package-info.java
+++ spring-boot-project/spring-boot-tools/spring-boot-test-support/src/main/java/org/springframework/boot/testsupport/junit/platform/package-info.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2012-2018 the original author or authors.
+ * Copyright 2012-2019 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -15,7 +15,7 @@
  */
 
 /**
- * Reflective mirror of JUnit 5 classes required to workaround shurefire bug
+ * Reflective mirror of JUnit 5 classes required to workaround surefire bug
  * {@code SUREFIRE-1679}.
  */
 package org.springframework.boot.testsupport.junit.platform;
