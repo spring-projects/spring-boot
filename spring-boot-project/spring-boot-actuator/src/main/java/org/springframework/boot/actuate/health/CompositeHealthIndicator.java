cdb275caaa674346d9df9c1e1c7eabe482531818
Dmytro Nosan <dimanosan@gmail.com>
2019-07-24 15:36:41 +0300

    Update CompositeHealthIndicator to be able to run HealthIndicators concurrently.

--- spring-boot-project/spring-boot-actuator/src/main/java/org/springframework/boot/actuate/health/CompositeHealthIndicator.java
+++ spring-boot-project/spring-boot-actuator/src/main/java/org/springframework/boot/actuate/health/CompositeHealthIndicator.java
@@ -18,6 +18,14 @@ package org.springframework.boot.actuate.health;
 
 import java.util.LinkedHashMap;
 import java.util.Map;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.Executor;
+import java.util.concurrent.Future;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.TimeoutException;
+
+import org.springframework.util.Assert;
 
 /**
  * {@link HealthIndicator} that returns health indications from all registered delegates.
@@ -32,6 +40,8 @@ public class CompositeHealthIndicator implements HealthIndicator {
 	private final HealthIndicatorRegistry registry;
 
 	private final HealthAggregator aggregator;
+
+	private final HealthStrategy strategy;
 
 	/**
 	 * Create a new {@link CompositeHealthIndicator} from the specified indicators.
@@ -50,8 +60,14 @@ public class CompositeHealthIndicator implements HealthIndicator {
 	 * @param registry the registry of {@link HealthIndicator HealthIndicators}.
 	 */
 	public CompositeHealthIndicator(HealthAggregator healthAggregator, HealthIndicatorRegistry registry) {
+		this(healthAggregator, registry, new SequentialStrategy());
+	}
+
+	private CompositeHealthIndicator(HealthAggregator healthAggregator, HealthIndicatorRegistry registry,
+			HealthStrategy strategy) {
 		this.aggregator = healthAggregator;
 		this.registry = registry;
+		this.strategy = strategy;
 	}
 
 	/**
@@ -63,13 +79,117 @@ public class CompositeHealthIndicator implements HealthIndicator {
 		return this.registry;
 	}
 
+	/**
+	 * Returns a new {@link CompositeHealthIndicator} with a parallel strategy that
+	 * returns health indications from all registered delegates concurrently.
+	 * @param timeout number of milliseconds to wait before using the
+	 * {@code timeoutHealth}
+	 * @param timeoutHealth the {@link Health} to use if an health indicator reached the
+	 * {@code timeout}. Defaults to {@code unknown} status.
+	 * @param executor the executor to submit {@link HealthIndicator HealthIndicators} on.
+	 * @return new instance with a parallel strategy
+	 * @since 2.2.0
+	 */
+	public CompositeHealthIndicator parallel(Executor executor, long timeout, Health timeoutHealth) {
+		Assert.notNull(executor, "Executor must not be null");
+		ParallelStrategy strategy = new ParallelStrategy(executor, timeout, timeoutHealth);
+		return new CompositeHealthIndicator(this.aggregator, this.registry, strategy);
+	}
+
+	/**
+	 * Returns a new {@link CompositeHealthIndicator} with a parallel strategy that
+	 * returns health indications from all registered delegates concurrently.
+	 * @param executor the executor to submit {@link HealthIndicator HealthIndicators} on.
+	 * @return new instance with a parallel strategy
+	 * @since 2.2.0
+	 */
+	public CompositeHealthIndicator parallel(Executor executor) {
+		Assert.notNull(executor, "Executor must not be null");
+		ParallelStrategy strategy = new ParallelStrategy(executor, null, null);
+		return new CompositeHealthIndicator(this.aggregator, this.registry, strategy);
+	}
+
+	/**
+	 * Returns a new {@link CompositeHealthIndicator} with a sequential strategy that
+	 * returns health indications from all registered delegates sequentially.
+	 * @return new instance with a sequential strategy
+	 * @since 2.2.0
+	 */
+	public CompositeHealthIndicator sequential() {
+		return new CompositeHealthIndicator(this.aggregator, this.registry, new SequentialStrategy());
+	}
+
 	@Override
 	public Health health() {
-		Map<String, Health> healths = new LinkedHashMap<>();
-		for (Map.Entry<String, HealthIndicator> entry : this.registry.getAll().entrySet()) {
-			healths.put(entry.getKey(), entry.getValue().health());
-		}
+		Map<String, Health> healths = this.strategy.doHealth(this.registry.getAll());
 		return this.aggregator.aggregate(healths);
+	}
+
+	@FunctionalInterface
+	private interface HealthStrategy {
+
+		Map<String, Health> doHealth(Map<String, HealthIndicator> healthIndicators);
+
+	}
+
+	private static final class SequentialStrategy implements HealthStrategy {
+
+		@Override
+		public Map<String, Health> doHealth(Map<String, HealthIndicator> healthIndicators) {
+			Map<String, Health> healths = new LinkedHashMap<>();
+			for (Map.Entry<String, HealthIndicator> entry : healthIndicators.entrySet()) {
+				healths.put(entry.getKey(), entry.getValue().health());
+			}
+			return healths;
+		}
+
+	}
+
+	private static final class ParallelStrategy implements HealthStrategy {
+
+		private final Executor executor;
+
+		private final Long timeout;
+
+		private final Health timeoutHealth;
+
+		private ParallelStrategy(Executor executor, Long timeout, Health timeoutHealth) {
+			this.executor = executor;
+			this.timeout = timeout;
+			this.timeoutHealth = (timeoutHealth != null) ? timeoutHealth : Health.unknown().build();
+		}
+
+		@Override
+		public Map<String, Health> doHealth(Map<String, HealthIndicator> healthIndicators) {
+			Map<String, Future<Health>> healthsFutures = new LinkedHashMap<>();
+			for (Map.Entry<String, HealthIndicator> entry : healthIndicators.entrySet()) {
+				healthsFutures.put(entry.getKey(),
+						CompletableFuture.supplyAsync(entry.getValue()::health, this.executor));
+			}
+			Map<String, Health> healths = new LinkedHashMap<>();
+			for (Map.Entry<String, Future<Health>> entry : healthsFutures.entrySet()) {
+				healths.put(entry.getKey(), getHealth(entry.getValue(), this.timeout, this.timeoutHealth));
+			}
+			return healths;
+		}
+
+		private static Health getHealth(Future<Health> healthFuture, Long timeout, Health timeoutHealth) {
+			try {
+				return (timeout != null) ? healthFuture.get(timeout, TimeUnit.MILLISECONDS) : healthFuture.get();
+			}
+			catch (InterruptedException ex) {
+				Thread.currentThread().interrupt();
+				return Health.unknown().withException(ex).build();
+			}
+			catch (TimeoutException ex) {
+				return timeoutHealth;
+			}
+			catch (ExecutionException ex) {
+				Throwable cause = ex.getCause();
+				return Health.down((cause instanceof Exception) ? ((Exception) cause) : ex).build();
+			}
+		}
+
 	}
 
 }
