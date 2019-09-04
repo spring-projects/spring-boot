0f26f4d6e2b06135a8389c3240127f469411990b
Tyler Van Gorder <tyler.vangorder@build.com>
2019-09-03 18:12:04 -0700

    Support programmatic lazy-int exclusion
    Allow the `LazyInitializationBeanFactoryPostProcessor` to skip setting
    lazy-init based on a programmatic callback. This feature allows
    downstream projects to deal with edge-cases in which it is not easy to
    support lazy-loading (such as in DSLs that dynamically create additional
    beans).
    
    See gh-16615

--- spring-boot-project/spring-boot/src/main/java/org/springframework/boot/LazyInitializationBeanFactoryPostProcessor.java
+++ spring-boot-project/spring-boot/src/main/java/org/springframework/boot/LazyInitializationBeanFactoryPostProcessor.java
@@ -16,6 +16,10 @@
 
 package org.springframework.boot;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
 import org.springframework.beans.BeansException;
 import org.springframework.beans.factory.config.BeanDefinition;
 import org.springframework.beans.factory.config.BeanFactoryPostProcessor;
@@ -26,16 +30,42 @@ import org.springframework.core.Ordered;
 /**
  * {@link BeanFactoryPostProcessor} to set the lazy attribute on bean definition.
  *
+ * <P>
+ * This processor will not touch a bean definition that has already had its "lazy" flag
+ * explicitly set to "false".
+ *
+ * <P>
+ * There are edge cases in which it is not easy to explicitly set the "lazy" flag to
+ * "false" (such as in DSLs that dynamically create additional beans) and therefore this
+ * class uses a customizer strategy that allows downstream projects to contribute
+ * predicates which impact if a class is considered for lazy-loading.
+ *
+ * <P>
+ * Because this is a BeanFactoryPostProcessor, this class does not use dependency
+ * injection to collect the customizers. The post processor actually makes two passes
+ * through the bean definitions; the first is used to find and instantiate any
+ * {@link org.springframework.boot.EagerLoadingBeanDefinitionPredicate} and the second
+ * pass is where bean definitions are marked as lazy.
+ *
  * @author Andy Wilkinson
  * @author Madhura Bhave
+ * @author Tyler Van Gorder
  * @since 2.2.0
  */
 public final class LazyInitializationBeanFactoryPostProcessor implements BeanFactoryPostProcessor, Ordered {
 
 	@Override
 	public void postProcessBeanFactory(ConfigurableListableBeanFactory beanFactory) throws BeansException {
-		for (String name : beanFactory.getBeanDefinitionNames()) {
-			BeanDefinition beanDefinition = beanFactory.getBeanDefinition(name);
+
+		List<EagerLoadingBeanDefinitionPredicate> eagerPredicateList = getEagerLoadingPredicatesFromContext(
+				beanFactory);
+
+		for (String beanName : beanFactory.getBeanDefinitionNames()) {
+			BeanDefinition beanDefinition = beanFactory.getBeanDefinition(beanName);
+			if (eagerPredicateList.stream()
+					.anyMatch((predicate) -> predicate.test(beanFactory.getType(beanName, false)))) {
+				continue;
+			}
 			if (beanDefinition instanceof AbstractBeanDefinition) {
 				Boolean lazyInit = ((AbstractBeanDefinition) beanDefinition).getLazyInit();
 				if (lazyInit != null && !lazyInit) {
@@ -44,6 +74,25 @@ public final class LazyInitializationBeanFactoryPostProcessor implements BeanFac
 			}
 			beanDefinition.setLazyInit(true);
 		}
+	}
+
+	/**
+	 * This method extracts the list of
+	 * {@link org.springframework.boot.EagerLoadingBeanDefinitionPredicate} beans from the
+	 * bean factory. Because this method is called early in the factory life cycle, we
+	 * take care not to force the eager initialization of factory beans.
+	 * @param beanFactory bean factory passed into the post-processor.
+	 * @return a list of {@link EagerLoadingBeanDefinitionPredicate} that can be used to
+	 * customize the behavior of this processor.
+	 */
+	private List<EagerLoadingBeanDefinitionPredicate> getEagerLoadingPredicatesFromContext(
+			ConfigurableListableBeanFactory beanFactory) {
+
+		Map<String, EagerLoadingBeanDefinitionPredicate> eagerPredicates = beanFactory
+				.getBeansOfType(EagerLoadingBeanDefinitionPredicate.class, false, false);
+
+		return new ArrayList<>(eagerPredicates.values());
+
 	}
 
 	@Override
