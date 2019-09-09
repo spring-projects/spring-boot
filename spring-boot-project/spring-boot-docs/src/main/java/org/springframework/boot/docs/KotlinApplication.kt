ecb3c1f2a06c0aaa794345fd789548d34b424556
Ibanga Enoobong Ime <ibangaenoobong@yahoo.com>
2019-09-09 09:29:24 +0100

    add runnable code sample for Applicaton class

---
+++ spring-boot-project/spring-boot-docs/src/main/java/org/springframework/boot/docs/KotlinApplication.kt
@@ -0,0 +1,20 @@
+package org.springframework.boot.docs
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+/**
+ * Example showing Application Class with [org.springframework.boot.autoconfigure.SpringBootApplication]
+ *
+ * @author Ibanga Enoobong Ime
+ */
+
+// tag::customizer[]
+@SpringBootApplication
+class KotlinApplication
+
+fun main(args: Array<String>) {
+	runApplication<Application>(*args)
+}
+
+// end::customizer[]
