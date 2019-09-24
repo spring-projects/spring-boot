c3c5939d13fe10316fe158f98bd4003fa60ad0dc
Yuyan <845058547@qq.com>
2019-09-24 22:07:27 +0800

    Some code that can be simplified 1.```'for'```loop can replaceable with ```'foreach'```. 2.Anonymous type can be replaced with lambda.

--- spring-boot-project/spring-boot-tools/spring-boot-configuration-processor/src/json-shade/java/org/springframework/boot/configurationprocessor/json/JSONArray.java
+++ spring-boot-project/spring-boot-tools/spring-boot-configuration-processor/src/json-shade/java/org/springframework/boot/configurationprocessor/json/JSONArray.java
@@ -65,8 +65,8 @@ public class JSONArray {
 	public JSONArray(Collection copyFrom) {
 		this();
 		if (copyFrom != null) {
-			for (Iterator it = copyFrom.iterator(); it.hasNext();) {
-				put(JSONObject.wrap(it.next()));
+			for (Object o : copyFrom) {
+				put(JSONObject.wrap(o));
 			}
 		}
 	}
