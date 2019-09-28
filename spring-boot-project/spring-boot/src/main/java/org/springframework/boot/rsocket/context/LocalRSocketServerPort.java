4b0fb8ff242f928cceac25d5cbcee80380bad68b
Phillip Webb <pwebb@pivotal.io>
2019-09-27 21:02:57 -0700

    Polish 'Add @LocalRSocketServerPort support'
    See gh-18287

--- spring-boot-project/spring-boot/src/main/java/org/springframework/boot/rsocket/context/LocalRSocketServerPort.java
+++ spring-boot-project/spring-boot/src/main/java/org/springframework/boot/rsocket/context/LocalRSocketServerPort.java
@@ -14,7 +14,7 @@
  * limitations under the License.
  */
 
-package org.springframework.boot.web.server;
+package org.springframework.boot.rsocket.context;
 
 import java.lang.annotation.Documented;
 import java.lang.annotation.ElementType;
