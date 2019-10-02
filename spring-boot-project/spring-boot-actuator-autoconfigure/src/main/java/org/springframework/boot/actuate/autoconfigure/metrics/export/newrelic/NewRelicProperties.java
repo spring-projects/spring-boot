e73ae62621a0d2e097a7c01c30b90c425a354558
Neil Powell <52715665+neiljpowell@users.noreply.github.com>
2019-10-01 22:21:56 -0500

    Support 'New Relic' eventType properties
    Update `NewRelicProperties` so that the event type sent with each
    metric can be configured. An additional `boolean` property has
    also been added if the previous behavior using the "meter-name"
    is required.
    
    NewRelic's own agents publish metrics to eventTypes aligned with broader
    categories. To align with their recommendation the default behavior is
    to publish metrics under a "SpringBootSample" category. When doing so,
    additional context is provided by including "metricName" and
    "metricType" attributes.
    
    See gh-18472

--- spring-boot-project/spring-boot-actuator-autoconfigure/src/main/java/org/springframework/boot/actuate/autoconfigure/metrics/export/newrelic/NewRelicProperties.java
+++ spring-boot-project/spring-boot-actuator-autoconfigure/src/main/java/org/springframework/boot/actuate/autoconfigure/metrics/export/newrelic/NewRelicProperties.java
@@ -26,10 +26,25 @@ import org.springframework.boot.context.properties.ConfigurationProperties;
  * @author Jon Schneider
  * @author Andy Wilkinson
  * @author Stephane Nicoll
+ * @author Neil Powell
  * @since 2.0.0
  */
 @ConfigurationProperties(prefix = "management.metrics.export.newrelic")
 public class NewRelicProperties extends StepRegistryProperties {
+
+	/**
+	 * When this is {@code false}, the New Relic eventType value will be set to
+	 * {@link #eventType()}. Otherwise, the meter name will be used. Defaults to
+	 * {@code false}.
+	 */
+	private boolean meterNameEventTypeEnabled;
+
+	/**
+	 * This configuration property will only be used if
+	 * {@link #meterNameEventTypeEnabled()} is {@code false}. Default value is
+	 * {@code SpringBootSample}.
+	 */
+	private String eventType = "SpringBootSample";
 
 	/**
 	 * New Relic API key.
@@ -45,6 +60,22 @@ public class NewRelicProperties extends StepRegistryProperties {
 	 * URI to ship metrics to.
 	 */
 	private String uri = "https://insights-collector.newrelic.com";
+
+	public boolean isMeterNameEventTypeEnabled() {
+		return this.meterNameEventTypeEnabled;
+	}
+
+	public void setMeterNameEventTypeEnabled(boolean meterNameEventTypeEnabled) {
+		this.meterNameEventTypeEnabled = meterNameEventTypeEnabled;
+	}
+
+	public String getEventType() {
+		return this.eventType;
+	}
+
+	public void setEventType(String eventType) {
+		this.eventType = eventType;
+	}
 
 	public String getApiKey() {
 		return this.apiKey;
