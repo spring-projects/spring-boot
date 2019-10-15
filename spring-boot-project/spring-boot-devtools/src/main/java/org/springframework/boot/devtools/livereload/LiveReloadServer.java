681a94b0d583f783495ea1c40c28a69b52b58fc7
wycm <wangyang1@u51.com>
2019-10-15 20:37:02 +0800

    Optimize debug level logs
    See gh-18604

--- spring-boot-project/spring-boot-devtools/src/main/java/org/springframework/boot/devtools/livereload/LiveReloadServer.java
+++ spring-boot-project/spring-boot-devtools/src/main/java/org/springframework/boot/devtools/livereload/LiveReloadServer.java
@@ -109,7 +109,9 @@ public class LiveReloadServer {
 	public int start() throws IOException {
 		synchronized (this.monitor) {
 			Assert.state(!isStarted(), "Server already started");
-			logger.debug("Starting live reload server on port " + this.port);
+			if (logger.isDebugEnabled()) {
+				logger.debug("Starting live reload server on port " + this.port);
+			}
 			this.serverSocket = new ServerSocket(this.port);
 			int localPort = this.serverSocket.getLocalPort();
 			this.listenThread = this.threadFactory.newThread(this::acceptConnections);
