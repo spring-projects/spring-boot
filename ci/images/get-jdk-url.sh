0bb36ce529588657e0cbf7b137c68a719921e16f
dreis2211 <christoph.dreis@freenet.de>
2019-11-04 07:35:32 +0100

    Drop JDK 12-based CI
    Closes gh-18843

--- ci/images/get-jdk-url.sh
+++ ci/images/get-jdk-url.sh
@@ -8,9 +8,6 @@ case "$1" in
 	java11)
 		 echo "https://github.com/AdoptOpenJDK/openjdk11-binaries/releases/download/jdk-11.0.5%2B10/OpenJDK11U-jdk_x64_linux_hotspot_11.0.5_10.tar.gz"
 	;;
-	java12)
-		 echo "https://github.com/AdoptOpenJDK/openjdk12-binaries/releases/download/jdk-12.0.2%2B10/OpenJDK12U-jdk_x64_linux_hotspot_12.0.2_10.tar.gz"
-	;;
 	*)
 		echo $"Unknown java version"
 		exit 1
