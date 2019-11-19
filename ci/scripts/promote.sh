656f73f0c52ffcf3232c12bf44251abd88759c53
Madhura Bhave <mbhave@pivotal.io>
2019-11-19 12:19:55 -0800

    Parse version from build.json for release notes generation
    Fixes gh-18908

--- ci/scripts/promote.sh
+++ ci/scripts/promote.sh
@@ -2,6 +2,7 @@
 
 source $(dirname $0)/common.sh
 
+version=$( cat artifactory-repo/build-info.json | jq -r '.buildInfo.modules[0].id' | sed 's/.*:.*:\(.*\)/\1/' )
 export BUILD_INFO_LOCATION=$(pwd)/artifactory-repo/build-info.json
 
 java -jar /spring-boot-release-scripts.jar promote $RELEASE_TYPE $BUILD_INFO_LOCATION > /dev/null || { exit 1; }
