6220f11e21fb1cbf2920d8113a21285bb0849e8e
Andreas Grub <andreas.grub@qaware.de>
2019-11-21 11:13:22 +0100

    Only enable ReactorDebugAgent if property is actually true
    This makes it run in case the EnvironmentPostProcessor gets called
    too early before properties from the other sources are read in
    
    Note that the Reactor Debug Agent can only be enabled
    when running inside a JDK, but not within a JRE

--- spring-boot-project/spring-boot/src/main/java/org/springframework/boot/reactor/DebugAgentEnvironmentPostProcessor.java
+++ spring-boot-project/spring-boot/src/main/java/org/springframework/boot/reactor/DebugAgentEnvironmentPostProcessor.java
@@ -43,7 +43,7 @@ public class DebugAgentEnvironmentPostProcessor implements EnvironmentPostProces
 	public void postProcessEnvironment(ConfigurableEnvironment environment, SpringApplication application) {
 		if (ClassUtils.isPresent(REACTOR_DEBUGAGENT_CLASS, null)) {
 			Boolean agentEnabled = environment.getProperty(DEBUGAGENT_ENABLED_CONFIG_KEY, Boolean.class);
-			if (agentEnabled != Boolean.FALSE) {
+			if (Boolean.TRUE.equals(agentEnabled)) {
 				try {
 					Class<?> debugAgent = Class.forName(REACTOR_DEBUGAGENT_CLASS);
 					debugAgent.getMethod("init").invoke(null);
