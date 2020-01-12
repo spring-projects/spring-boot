bda3cedf101158d140d211c1f08255da83a7b91f
Andy Wilkinson <awilkinson@pivotal.io>
2020-01-12 11:30:35 +0000

    Suppress Gradle's welcome message in CI builds
    Closes gh-19630

--- ci/scripts/build-project.sh
+++ ci/scripts/build-project.sh
@@ -5,5 +5,5 @@ source $(dirname $0)/common.sh
 repository=$(pwd)/distribution-repository
 
 pushd git-repo > /dev/null
-./gradlew --no-daemon --max-workers=4 -PdeploymentRepository=${repository} build publishAllPublicationsToDeploymentRepository
+./gradlew --no-daemon --max-workers=4 --quiet -PdeploymentRepository=${repository} build publishAllPublicationsToDeploymentRepository
 popd > /dev/null
