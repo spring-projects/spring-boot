4c8626ea56be87932656a5a46b6cd6218521607f
Andy Wilkinson <awilkinson@pivotal.io>
2020-01-16 09:33:22 +0000

    Allow Boot's Jetty error handler to be overridden
    Fixes gh-19520

--- spring-boot-project/spring-boot/src/main/java/org/springframework/boot/web/embedded/jetty/JettyServletWebServerFactory.java
+++ spring-boot-project/spring-boot/src/main/java/org/springframework/boot/web/embedded/jetty/JettyServletWebServerFactory.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2012-2019 the original author or authors.
+ * Copyright 2012-2020 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -327,9 +327,9 @@ public class JettyServletWebServerFactory extends AbstractServletWebServerFactor
 			ServletContextInitializer... initializers) {
 		List<Configuration> configurations = new ArrayList<>();
 		configurations.add(getServletContextInitializerConfiguration(webAppContext, initializers));
-		configurations.addAll(getConfigurations());
 		configurations.add(getErrorPageConfiguration());
 		configurations.add(getMimeTypeConfiguration());
+		configurations.addAll(getConfigurations());
 		return configurations.toArray(new Configuration[0]);
 	}
 
