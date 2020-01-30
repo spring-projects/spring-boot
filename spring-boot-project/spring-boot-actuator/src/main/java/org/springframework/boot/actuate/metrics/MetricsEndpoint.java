6db5ca97d344ecc73a090f901bb167eb3b6ac554
Andy Wilkinson <awilkinson@pivotal.io>
2020-01-30 14:05:53 +0000

    Sort names alphabetically in metrics list response
    Closes gh-19934

--- spring-boot-project/spring-boot-actuator/src/main/java/org/springframework/boot/actuate/metrics/MetricsEndpoint.java
+++ spring-boot-project/spring-boot-actuator/src/main/java/org/springframework/boot/actuate/metrics/MetricsEndpoint.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2012-2019 the original author or authors.
+ * Copyright 2012-2020 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -21,10 +21,10 @@ import java.util.Collections;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.LinkedHashMap;
-import java.util.LinkedHashSet;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
+import java.util.TreeSet;
 import java.util.function.BiFunction;
 import java.util.stream.Collectors;
 
@@ -58,7 +58,7 @@ public class MetricsEndpoint {
 
 	@ReadOperation
 	public ListNamesResponse listNames() {
-		Set<String> names = new LinkedHashSet<>();
+		Set<String> names = new TreeSet<>();
 		collectNames(names, this.registry);
 		return new ListNamesResponse(names);
 	}
