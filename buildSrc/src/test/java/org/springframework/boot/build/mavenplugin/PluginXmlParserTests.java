020ae2c7baaed85920336fe67db3b9cccfbfe3d9
Mike Smithson <mikesmithso@gmail.com>
2020-02-14 18:59:12 -0500

    Revisit PluginXmlParserTests
    See gh-20190

--- buildSrc/src/test/java/org/springframework/boot/build/mavenplugin/PluginXmlParserTests.java
+++ buildSrc/src/test/java/org/springframework/boot/build/mavenplugin/PluginXmlParserTests.java
@@ -16,25 +16,42 @@
 
 package org.springframework.boot.build.mavenplugin;
 
-import java.io.File;
-
 import org.junit.jupiter.api.Test;
-
 import org.springframework.boot.build.mavenplugin.PluginXmlParser.Plugin;
+
+import java.io.File;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 /**
  * Tests for {@link PluginXmlParser}.
  *
  * @author Andy Wilkinson
+ * @author Mike Smithson
  */
 public class PluginXmlParserTests {
 
 	private final PluginXmlParser parser = new PluginXmlParser();
 
 	@Test
-	void dunno() {
+	void parsingAValidPluginXMLFileReturnsTheGAVForThePlugin() {
 		Plugin plugin = this.parser.parse(new File("src/test/resources/plugin.xml"));
-		System.out.println(plugin);
+		assertThat(plugin.getGroupId()).isEqualTo("org.springframework.boot");
+		assertThat(plugin.getArtifactId()).isEqualTo("spring-boot-maven-plugin");
+		assertThat(plugin.getVersion()).isEqualTo("2.2.0.GRADLE-SNAPSHOT");
+		assertThat(plugin.getGoalPrefix()).isEqualTo("spring-boot");
+		assertThat(plugin.getMojos().stream().map(PluginXmlParser.Mojo::getGoal).collect(Collectors.toList()))
+				.isEqualTo(Arrays.<String>asList("build-info", "help", "repackage", "run", "start", "stop"));
+	}
+
+	@Test
+	void aNonExistentPluginFileThrowsARuntimeException() {
+		assertThrows(RuntimeException.class, () ->
+				this.parser.parse(new File("src/test/resources/nonexistent.xml")));
 	}
 
 }
