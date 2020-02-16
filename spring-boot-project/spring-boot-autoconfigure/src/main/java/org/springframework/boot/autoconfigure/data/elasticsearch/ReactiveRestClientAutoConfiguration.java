a7e57e090930aefd7a45dec682087ca32c0b6488
wonwoo <aoruqjfu@gmail.com>
2020-02-16 20:09:45 +0900

    Configure codec buffer size in ES Reactive Rest client
    This commit adds a new configuration property
    `"spring.data.elasticsearch.client.reactive.max-in-memory-size"`
    which configures the maximum amount of memory buffered by the
    `WebClient` used by the Reactive ElasticSearch client.
    
    See gh-20205

--- spring-boot-project/spring-boot-autoconfigure/src/main/java/org/springframework/boot/autoconfigure/data/elasticsearch/ReactiveRestClientAutoConfiguration.java
+++ spring-boot-project/spring-boot-autoconfigure/src/main/java/org/springframework/boot/autoconfigure/data/elasticsearch/ReactiveRestClientAutoConfiguration.java
@@ -29,6 +29,8 @@ import org.springframework.data.elasticsearch.client.ClientConfiguration;
 import org.springframework.data.elasticsearch.client.reactive.ReactiveElasticsearchClient;
 import org.springframework.data.elasticsearch.client.reactive.ReactiveRestClients;
 import org.springframework.http.HttpHeaders;
+import org.springframework.util.unit.DataSize;
+import org.springframework.web.reactive.function.client.ExchangeStrategies;
 import org.springframework.web.reactive.function.client.WebClient;
 
 /**
@@ -52,6 +54,7 @@ public class ReactiveRestClientAutoConfiguration {
 			builder.usingSsl();
 		}
 		configureTimeouts(builder, properties);
+		configureWebClient(builder, properties);
 		return builder.build();
 	}
 
@@ -64,6 +67,19 @@ public class ReactiveRestClientAutoConfiguration {
 			HttpHeaders headers = new HttpHeaders();
 			headers.setBasicAuth(username, properties.getPassword());
 			builder.withDefaultHeaders(headers);
+		});
+	}
+
+	private void configureWebClient(ClientConfiguration.TerminalClientConfigurationBuilder builder,
+			ReactiveRestClientProperties properties) {
+		PropertyMapper map = PropertyMapper.get();
+		builder.withWebClientConfigurer((webClient) -> {
+			ExchangeStrategies exchangeStrategies = ExchangeStrategies.builder()
+					.codecs((configurer) -> map.from(properties.getMaxInMemorySize()).whenNonNull()
+							.asInt(DataSize::toBytes)
+							.to((maxInMemorySize) -> configurer.defaultCodecs().maxInMemorySize(maxInMemorySize)))
+					.build();
+			return webClient.mutate().exchangeStrategies(exchangeStrategies).build();
 		});
 	}
 
