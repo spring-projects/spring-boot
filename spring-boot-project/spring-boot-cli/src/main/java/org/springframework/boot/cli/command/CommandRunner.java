86c5ee7230b58750cc03b1a1b900230ab2d08c98
wycm <wangyang1@u51.com>
2020-03-08 16:18:22 +0800

    Polish

--- spring-boot-project/spring-boot-cli/src/main/java/org/springframework/boot/cli/command/CommandRunner.java
+++ spring-boot-project/spring-boot-cli/src/main/java/org/springframework/boot/cli/command/CommandRunner.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2012-2019 the original author or authors.
+ * Copyright 2012-2020 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -171,7 +171,7 @@ public class CommandRunner implements Iterable<Command> {
 			ExitStatus result = run(argsWithoutDebugFlags);
 			// The caller will hang up if it gets a non-zero status
 			if (result != null && result.isHangup()) {
-				return (result.getCode() > 0) ? result.getCode() : 0;
+				return Math.max(result.getCode(), 0);
 			}
 			return 0;
 		}
