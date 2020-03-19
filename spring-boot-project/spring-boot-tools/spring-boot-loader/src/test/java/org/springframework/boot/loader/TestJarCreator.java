579a6ac254995742ff2395b9a7c4dcc75b15774e
Stephane Nicoll <snicoll@pivotal.io>
2020-03-19 14:14:10 +0100

    Adapt tests for support for Java 14
    See gh-20576

--- spring-boot-project/spring-boot-tools/spring-boot-loader/src/test/java/org/springframework/boot/loader/TestJarCreator.java
+++ spring-boot-project/spring-boot-tools/spring-boot-loader/src/test/java/org/springframework/boot/loader/TestJarCreator.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2012-2019 the original author or authors.
+ * Copyright 2012-2020 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -98,6 +98,7 @@ public abstract class TestJarCreator {
 			writeEntry(jarOutputStream, "META-INF/versions/11/multi-release.dat", 11);
 			writeEntry(jarOutputStream, "META-INF/versions/12/multi-release.dat", 12);
 			writeEntry(jarOutputStream, "META-INF/versions/13/multi-release.dat", 13);
+			writeEntry(jarOutputStream, "META-INF/versions/14/multi-release.dat", 14);
 		}
 		else {
 			writeEntry(jarOutputStream, "3.dat", 3);
