9eed719c44352b5f3d2fe10d6c9456408b91c880
Brian Clozel <bclozel@pivotal.io>
2020-03-24 23:11:18 +0100

    Do not register shutdownHook for WAR deployments
    The application context shutdownHook is not needed for WAR deployments,
    and we should let the Servlet container handle the application lifecycle
    here.
    
    Closes gh-19398

--- spring-boot-project/spring-boot/src/main/java/org/springframework/boot/web/servlet/support/SpringBootServletInitializer.java
+++ spring-boot-project/spring-boot/src/main/java/org/springframework/boot/web/servlet/support/SpringBootServletInitializer.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2012-2019 the original author or authors.
+ * Copyright 2012-2020 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -131,6 +131,7 @@ public abstract class SpringBootServletInitializer implements WebApplicationInit
 		if (this.registerErrorPageFilter) {
 			application.addPrimarySources(Collections.singleton(ErrorPageFilterConfiguration.class));
 		}
+		application.setRegisterShutdownHook(false);
 		return run(application);
 	}
 
