63be1678dec87541441492a59062b186e679403d
Stephane Nicoll <snicoll@pivotal.io>
2020-03-28 16:02:04 +0100

    Polish "Use LOCAL_ONE when querying system.local"
    See gh-20709

--- spring-boot-project/spring-boot-actuator/src/main/java/org/springframework/boot/actuate/cassandra/CassandraHealthIndicator.java
+++ spring-boot-project/spring-boot-actuator/src/main/java/org/springframework/boot/actuate/cassandra/CassandraHealthIndicator.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2012-2019 the original author or authors.
+ * Copyright 2012-2020 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -32,6 +32,7 @@ import org.springframework.util.Assert;
  * Cassandra data stores.
  *
  * @author Julien Dubois
+ * @author Alexandre Dutra
  * @since 2.0.0
  */
 public class CassandraHealthIndicator extends AbstractHealthIndicator {
