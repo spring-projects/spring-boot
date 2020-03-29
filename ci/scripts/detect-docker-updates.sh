1b5c12c0b9184b69cce19157223701993cf7bd7e
dreis2211 <christoph.dreis@freenet.de>
2020-03-29 13:47:33 +0200

    Implement review changes

--- ci/scripts/detect-docker-updates.sh
+++ ci/scripts/detect-docker-updates.sh
@@ -15,11 +15,8 @@ if [[ $current = $latest ]]; then
 	exit 0;
 fi
 
-
-issue_title="Upgrade to Docker $latest_version in CI"
-
 existing_tasks=$( curl -s https://api.github.com/repos/${GITHUB_ORGANIZATION}/${GITHUB_REPO}/pulls\?labels\=type:%20task\&state\=open\&creator\=spring-buildmaster )
-existing_upgrade_issues=$( echo "$existing_tasks" | jq -c --arg TITLE "$issue_title" '.[] | select(.title==$TITLE)' )
+existing_upgrade_issues=$( echo "$existing_tasks" | jq -c --arg TITLE "$ISSUE_TITLE" '.[] | select(.title==$TITLE)' )
 
 if [[ ${existing_upgrade_issues} = "" ]]; then
 	# Create changes in dedicated branch
@@ -27,20 +24,10 @@ if [[ ${existing_upgrade_issues} = "" ]]; then
 	git config user.name "Spring Buildmaster" > /dev/null
 	git config user.email "buildmaster@springframework.org" > /dev/null
 	git checkout -b "$branch" origin/master > /dev/null
-	sed -i "s/version=.*/version=\"$latest_version\"/" git-repo/ci/images/get-docker-url.sh
-	git add git-repo/ci/images/get-docker-url.sh > /dev/null
-	git commit -m "$issue_title" > /dev/null
-	git push origin "$branch" > /dev/null
-
-	# Create pull request
-	curl \
-	-s \
-	-u ${GITHUB_USERNAME}:${GITHUB_PASSWORD} \
-	-H "Content-type:application/json" \
-	-d "{\"head\":\"${branch}\",\"base\":\"master\",\"title\":\"${ISSUE_TITLE}\",\"body\": \"Upgrade to Docker ${latest_version}\",\"labels\":[\"status: waiting-for-triage\",\"type: task\"]}"  \
-	-f \
-	-X \
-	POST "https://api.github.com/repos/${GITHUB_ORGANIZATION}/${GITHUB_REPO}/pulls" > /dev/null || { echo "Failed to create pull request" >&2; exit 1; }
+	sed -i "s/version=.*/version=\"$latest_version\"/" ci/images/get-docker-url.sh
+	git add ci/images/get-docker-url.sh > /dev/null
+	commit_message="Upgrade to Docker $latest_version in CI"
+	git commit -m "$commit_message" > /dev/null
 else
 	echo "Pull request already exists."
 fi
