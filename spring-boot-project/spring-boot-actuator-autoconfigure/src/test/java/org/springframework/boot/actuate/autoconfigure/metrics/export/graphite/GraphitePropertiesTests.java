4813606b70139885e3799d8e39cda4270dad5077
Stephane Nicoll <snicoll@pivotal.io>
2020-04-04 15:14:35 +0200

    Expose Graphite's graphiteTagsEnabled property
    This commit exposes an additional property for Graphite that allows to
    restore the previous default behaviour with regards to tags, i.e.
    prefixing the ones defined by the "tagsAsPrefix" property.
    
    Close gh-20834

--- spring-boot-project/spring-boot-actuator-autoconfigure/src/test/java/org/springframework/boot/actuate/autoconfigure/metrics/export/graphite/GraphitePropertiesTests.java
+++ spring-boot-project/spring-boot-actuator-autoconfigure/src/test/java/org/springframework/boot/actuate/autoconfigure/metrics/export/graphite/GraphitePropertiesTests.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2012-2019 the original author or authors.
+ * Copyright 2012-2020 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -39,6 +39,7 @@ class GraphitePropertiesTests {
 		assertThat(properties.getHost()).isEqualTo(config.host());
 		assertThat(properties.getPort()).isEqualTo(config.port());
 		assertThat(properties.getProtocol()).isEqualTo(config.protocol());
+		assertThat(properties.isGraphiteTagsEnabled()).isEqualTo(config.graphiteTagsEnabled());
 		assertThat(properties.getTagsAsPrefix()).isEqualTo(config.tagsAsPrefix());
 	}
 
