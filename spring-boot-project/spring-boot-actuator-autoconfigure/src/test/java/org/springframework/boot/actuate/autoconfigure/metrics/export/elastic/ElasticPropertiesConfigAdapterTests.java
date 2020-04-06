f64f5a0f557d47df93ef6aae7457ec2a69e9e977
Stephane Nicoll <snicoll@pivotal.io>
2020-04-06 17:01:16 +0200

    Expose Elastic's pipeline and indexDateSeparator properties
    Closes gh-20852

--- spring-boot-project/spring-boot-actuator-autoconfigure/src/test/java/org/springframework/boot/actuate/autoconfigure/metrics/export/elastic/ElasticPropertiesConfigAdapterTests.java
+++ spring-boot-project/spring-boot-actuator-autoconfigure/src/test/java/org/springframework/boot/actuate/autoconfigure/metrics/export/elastic/ElasticPropertiesConfigAdapterTests.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2012-2019 the original author or authors.
+ * Copyright 2012-2020 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -49,6 +49,13 @@ class ElasticPropertiesConfigAdapterTests {
 	}
 
 	@Test
+	void whenPropertiesIndexDateSeparatorIsSetAdapterIndexDateFormatReturnsIt() {
+		ElasticProperties properties = new ElasticProperties();
+		properties.setIndexDateSeparator("*");
+		assertThat(new ElasticPropertiesConfigAdapter(properties).indexDateSeparator()).isEqualTo("*");
+	}
+
+	@Test
 	void whenPropertiesTimestampFieldNameIsSetAdapterTimestampFieldNameReturnsIt() {
 		ElasticProperties properties = new ElasticProperties();
 		properties.setTimestampFieldName("@test");
@@ -74,6 +81,13 @@ class ElasticPropertiesConfigAdapterTests {
 		ElasticProperties properties = new ElasticProperties();
 		properties.setPassword("secret");
 		assertThat(new ElasticPropertiesConfigAdapter(properties).password()).isEqualTo("secret");
+	}
+
+	@Test
+	void whenPropertiesPipelineIsSetAdapterPasswordReturnsIt() {
+		ElasticProperties properties = new ElasticProperties();
+		properties.setPipeline("testPipeline");
+		assertThat(new ElasticPropertiesConfigAdapter(properties).pipeline()).isEqualTo("testPipeline");
 	}
 
 }
