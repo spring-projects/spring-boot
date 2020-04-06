f293f6ad9b66347f0b2eec552f601109406feb33
Stephane Nicoll <snicoll@pivotal.io>
2020-04-06 15:27:57 +0200

    Expose AppOptics's floorTimes property
    Closes gh-20850

--- spring-boot-project/spring-boot-actuator-autoconfigure/src/test/java/org/springframework/boot/actuate/autoconfigure/metrics/export/appoptics/AppOpticsPropertiesConfigAdapterTests.java
+++ spring-boot-project/spring-boot-actuator-autoconfigure/src/test/java/org/springframework/boot/actuate/autoconfigure/metrics/export/appoptics/AppOpticsPropertiesConfigAdapterTests.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2012-2019 the original author or authors.
+ * Copyright 2012-2020 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -59,6 +59,13 @@ class AppOpticsPropertiesConfigAdapterTests
 		AppOpticsProperties properties = createProperties();
 		properties.setHostTag("node");
 		assertThat(createConfigAdapter(properties).hostTag()).isEqualTo("node");
+	}
+
+	@Test
+	void whenPropertiesFloorTimesIsSetAdapterHostTagReturnsIt() {
+		AppOpticsProperties properties = createProperties();
+		properties.setFloorTimes(true);
+		assertThat(createConfigAdapter(properties).floorTimes()).isTrue();
 	}
 
 }
