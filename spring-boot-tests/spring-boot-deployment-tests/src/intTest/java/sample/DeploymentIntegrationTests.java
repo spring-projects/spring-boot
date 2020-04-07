257703f54614db91f3c901b004f35d9bd5c18d4d
Scott Frederick <sfrederick@pivotal.io>
2020-04-07 17:08:51 -0500

    Polish integration test

--- spring-boot-tests/spring-boot-deployment-tests/src/intTest/java/sample/DeploymentIntegrationTests.java
+++ spring-boot-tests/spring-boot-deployment-tests/src/intTest/java/sample/DeploymentIntegrationTests.java
@@ -48,7 +48,7 @@ class DeploymentIntegrationTests {
 
 	@ParameterizedTest
 	@MethodSource("deployedApplications")
-	void home(DeployedApplication app) throws Exception {
+	void home(DeployedApplication app) {
 		app.test((rest) -> {
 			ResponseEntity<String> response = rest.getForEntity("/", String.class);
 			assertThat(response.getStatusCode()).isEqualTo(HttpStatus.OK);
@@ -58,7 +58,7 @@ class DeploymentIntegrationTests {
 
 	@ParameterizedTest
 	@MethodSource("deployedApplications")
-	void health(DeployedApplication application) throws Exception {
+	void health(DeployedApplication application) {
 		application.test((rest) -> {
 			ResponseEntity<String> response = rest.getForEntity("/actuator/health", String.class);
 			assertThat(response.getStatusCode()).isEqualTo(HttpStatus.OK);
@@ -70,7 +70,7 @@ class DeploymentIntegrationTests {
 		return Arrays.asList(new DeployedApplication("open-liberty:19.0.0.9-webProfile8", "/config/dropins", 9080),
 				new DeployedApplication("tomcat:9.0.29-jdk8-openjdk", "/usr/local/tomcat/webapps", 8080),
 				new DeployedApplication("tomee:11-jre-8.0.0-M3-webprofile", "/usr/local/tomee/webapps", 8080),
-				new DeployedApplication("jboss/wildfly:18.0.1.Final", "/opt/jboss/wildfly/standalone/deployments/",
+				new DeployedApplication("jboss/wildfly:18.0.1.Final", "/opt/jboss/wildfly/standalone/deployments",
 						8080));
 	}
 
