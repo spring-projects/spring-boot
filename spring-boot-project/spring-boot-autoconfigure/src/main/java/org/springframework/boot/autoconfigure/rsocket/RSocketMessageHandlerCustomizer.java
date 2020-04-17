456d6e78fe07968a8157e639a3ab5fff70487148
Aarti Gupta <aartiguptaa@gmail.com>
2020-04-17 16:03:28 -0700

    Add support for customizing RSocketMessageHandler
    See gh-21081

---
+++ spring-boot-project/spring-boot-autoconfigure/src/main/java/org/springframework/boot/autoconfigure/rsocket/RSocketMessageHandlerCustomizer.java
@@ -0,0 +1,18 @@
+package org.springframework.boot.autoconfigure.rsocket;
+
+import org.apache.catalina.connector.Connector;
+import org.springframework.boot.web.embedded.tomcat.ConfigurableTomcatWebServerFactory;
+import org.springframework.messaging.rsocket.RSocketStrategies;
+import org.springframework.messaging.rsocket.annotation.support.RSocketMessageHandler;
+import org.springframework.util.RouteMatcher;
+
+/**
+ * @author Aarti Gupta
+ * Callback interface that can be used to customize a RSocketMessageHandler {@link Connector}.
+ */
+@FunctionalInterface
+public interface RSocketMessageHandlerCustomizer {
+
+	RSocketMessageHandler setRouteMatcher(RouteMatcher handler);
+
+}
