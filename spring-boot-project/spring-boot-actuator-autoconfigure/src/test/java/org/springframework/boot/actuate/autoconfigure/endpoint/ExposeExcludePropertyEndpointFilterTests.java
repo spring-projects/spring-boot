6a4d98a8653d21acec1ea517229c6d73ac6da359
Phillip Webb <pwebb@pivotal.io>
2020-04-19 15:55:22 -0700

    Allow exposure patterns to match dashed IDs
    Update `ExposeExcludePropertyEndpointFilter` so that patterns will
    also match endpoint IDs that contain a dash.
    
    Closes gh-20997

--- spring-boot-project/spring-boot-actuator-autoconfigure/src/test/java/org/springframework/boot/actuate/autoconfigure/endpoint/ExposeExcludePropertyEndpointFilterTests.java
+++ spring-boot-project/spring-boot-actuator-autoconfigure/src/test/java/org/springframework/boot/actuate/autoconfigure/endpoint/ExposeExcludePropertyEndpointFilterTests.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2012-2019 the original author or authors.
+ * Copyright 2012-2020 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -145,6 +145,12 @@ public class ExposeExcludePropertyEndpointFilterTests {
 	public void matchWhenMixedCaseShouldMatch() {
 		setupFilter("foo-bar", "");
 		assertThat(match(EndpointId.of("fooBar"))).isTrue();
+	}
+
+	@Test // gh-20997
+	public void matchWhenDashInName() throws Exception {
+		setupFilter("bus-refresh", "");
+		assertThat(match(EndpointId.of("bus-refresh"))).isTrue();
 	}
 
 	private void setupFilter(String include, String exclude) {
