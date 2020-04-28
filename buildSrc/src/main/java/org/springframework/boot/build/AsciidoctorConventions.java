4c9c9ccd9184fe5e54dc8440b6e8a281308cc4d3
Brian Clozel <bclozel@pivotal.io>
2020-04-28 11:41:44 +0200

    Upgrade to Spring Doc Resources 0.2.2.RELEASE
    Closes gh-21057

--- buildSrc/src/main/java/org/springframework/boot/build/AsciidoctorConventions.java
+++ buildSrc/src/main/java/org/springframework/boot/build/AsciidoctorConventions.java
@@ -113,7 +113,7 @@ class AsciidoctorConventions {
 	private UnzipDocumentationResources createUnzipDocumentationResourcesTask(Project project) {
 		Configuration documentationResources = project.getConfigurations().maybeCreate("documentationResources");
 		documentationResources.getDependencies()
-				.add(project.getDependencies().create("io.spring.docresources:spring-doc-resources:0.2.1.RELEASE"));
+				.add(project.getDependencies().create("io.spring.docresources:spring-doc-resources:0.2.2.RELEASE"));
 		UnzipDocumentationResources unzipResources = project.getTasks().create("unzipDocumentationResources",
 				UnzipDocumentationResources.class);
 		unzipResources.setResources(documentationResources);
