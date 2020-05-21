cf9222caa35d5337876b3e6c90e4aa19b104cc67
Radek Koubsky <radekkoubsky@gmail.com>
2020-05-21 18:38:03 +0200

    Support for webAdminPassword property of H2 Console

--- spring-boot-project/spring-boot-autoconfigure/src/test/java/org/springframework/boot/autoconfigure/h2/H2ConsoleAutoConfigurationTests.java
+++ spring-boot-project/spring-boot-autoconfigure/src/test/java/org/springframework/boot/autoconfigure/h2/H2ConsoleAutoConfigurationTests.java
@@ -79,6 +79,7 @@ class H2ConsoleAutoConfigurationTests {
 		assertThat(registrationBean.getUrlMappings()).contains("/h2-console/*");
 		assertThat(registrationBean.getInitParameters()).doesNotContainKey("trace");
 		assertThat(registrationBean.getInitParameters()).doesNotContainKey("webAllowOthers");
+		assertThat(registrationBean.getInitParameters()).doesNotContainKey("webAdminPassword");
 	}
 
 	@Test
@@ -114,13 +115,15 @@ class H2ConsoleAutoConfigurationTests {
 	void customInitParameters() {
 		this.context.register(H2ConsoleAutoConfiguration.class);
 		TestPropertyValues.of("spring.h2.console.enabled:true", "spring.h2.console.settings.trace=true",
-				"spring.h2.console.settings.webAllowOthers=true").applyTo(this.context);
+				"spring.h2.console.settings.webAllowOthers=true", "spring.h2.console.settings.webAdminPassword=abcd")
+				.applyTo(this.context);
 		this.context.refresh();
 		assertThat(this.context.getBeansOfType(ServletRegistrationBean.class)).hasSize(1);
 		ServletRegistrationBean<?> registrationBean = this.context.getBean(ServletRegistrationBean.class);
 		assertThat(registrationBean.getUrlMappings()).contains("/h2-console/*");
 		assertThat(registrationBean.getInitParameters()).containsEntry("trace", "");
 		assertThat(registrationBean.getInitParameters()).containsEntry("webAllowOthers", "");
+		assertThat(registrationBean.getInitParameters()).containsEntry("webAdminPassword", "abcd");
 	}
 
 	@Test
