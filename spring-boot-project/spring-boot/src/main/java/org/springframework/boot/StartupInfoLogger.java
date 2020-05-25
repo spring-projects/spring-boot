767078fed9701b04bba8ba88fbd8fad7d01a98a5
dreis2211 <christoph.dreis@freenet.de>
2020-05-25 12:35:49 +0200

    Print the java version being used on startup
    Closes gh-21553

--- spring-boot-project/spring-boot/src/main/java/org/springframework/boot/StartupInfoLogger.java
+++ spring-boot-project/spring-boot/src/main/java/org/springframework/boot/StartupInfoLogger.java
@@ -70,6 +70,7 @@ class StartupInfoLogger {
 		appendOn(message);
 		appendPid(message);
 		appendContext(message);
+		appendJavaVersion(message);
 		return message;
 	}
 
@@ -145,6 +146,10 @@ class StartupInfoLogger {
 			message.append(context);
 			message.append(")");
 		}
+	}
+
+	private void appendJavaVersion(StringBuilder message) {
+		append(message, "on Java ", () -> System.getProperty("java.version"));
 	}
 
 	private void append(StringBuilder message, String prefix, Callable<Object> call) {
