48acaa4bd92cdcc7f21dc106cc62f44f42da22cd
Andy Wilkinson <awilkinson@pivotal.io>
2020-06-10 09:42:43 +0100

    Make AutoConfigureMockMvc use SecurityProperties' filter ordering
    Previously, AutoConfigureMockMvc used Spring Security's default filter
    ordering, ignoring the value configured by SecurityProperties that is
    used at runtime. This resulted in different ordering at runtime and in
    tests.
    
    This commit updates the configuration for AutoConfigureMockMvc to
    import the Spring Security filter auto-configuration, thereby ensuring
    that the ordering configured via SecurityProperties is applied.
    
    Fixes gh-21801

--- spring-boot-project/spring-boot-test-autoconfigure/src/test/java/org/springframework/boot/test/autoconfigure/web/servlet/mockmvc/AfterSecurityFilter.java
+++ spring-boot-project/spring-boot-test-autoconfigure/src/test/java/org/springframework/boot/test/autoconfigure/web/servlet/mockmvc/AfterSecurityFilter.java
@@ -17,46 +17,39 @@
 package org.springframework.boot.test.autoconfigure.web.servlet.mockmvc;
 
 import java.io.IOException;
+import java.security.Principal;
 
 import javax.servlet.Filter;
 import javax.servlet.FilterChain;
-import javax.servlet.FilterConfig;
 import javax.servlet.ServletException;
 import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
-import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.boot.autoconfigure.security.SecurityProperties;
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.core.Ordered;
-import org.springframework.stereotype.Component;
 
 /**
- * Example filter used with {@link WebMvcTest @WebMvcTest} tests.
+ * {@link Filter} that is ordered to run after Spring Security's filter.
  *
- * @author Phillip Webb
+ * @author Andy Wilkinson
  */
-@Component
-public class ExampleFilter implements Filter, Ordered {
+public class AfterSecurityFilter implements Filter, Ordered {
 
 	@Override
-	public void init(FilterConfig filterConfig) throws ServletException {
+	public int getOrder() {
+		return SecurityProperties.DEFAULT_FILTER_ORDER + 1;
 	}
 
 	@Override
 	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
 			throws IOException, ServletException {
-		chain.doFilter(request, response);
-		((HttpServletResponse) response).addHeader("x-test", "abc");
-	}
-
-	@Override
-	public void destroy() {
-	}
-
-	@Override
-	public int getOrder() {
-		return SecurityProperties.DEFAULT_FILTER_ORDER - 1;
+		Principal principal = ((HttpServletRequest) request).getUserPrincipal();
+		if (principal == null) {
+			throw new ServletException("No user principal");
+		}
+		response.getWriter().write(principal.getName());
+		response.getWriter().flush();
 	}
 
 }
