603a1b8be81154f66c8652bb4fdd2abb22dab6ff
dreis2211 <christoph.dreis@freenet.de>
2020-06-11 09:12:43 +0200

    Only fallback to Class.getName() in HandlerFunctionDescription
    See gh-21604

--- spring-boot-project/spring-boot-actuator/src/main/java/org/springframework/boot/actuate/web/mappings/reactive/HandlerFunctionDescription.java
+++ spring-boot-project/spring-boot-actuator/src/main/java/org/springframework/boot/actuate/web/mappings/reactive/HandlerFunctionDescription.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2012-2019 the original author or authors.
+ * Copyright 2012-2020 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -29,11 +29,17 @@ public class HandlerFunctionDescription {
 	private final String className;
 
 	HandlerFunctionDescription(HandlerFunction<?> handlerFunction) {
-		this.className = handlerFunction.getClass().getName();
+		this.className = getHandlerFunctionClassName(handlerFunction);
 	}
 
 	public String getClassName() {
 		return this.className;
+	}
+
+	private String getHandlerFunctionClassName(HandlerFunction<?> handlerFunction) {
+		Class<?> functionClass = handlerFunction.getClass();
+		String canonicalName = functionClass.getCanonicalName();
+		return canonicalName != null ? canonicalName : functionClass.getName();
 	}
 
 }
