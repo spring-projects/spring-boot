bf96b0c57b7c6f9a6a5ab1904c8ccc1c1d912964
Stephane Nicoll <snicoll@pivotal.io>
2020-06-15 16:19:56 +0200

    Polish "Add support for webAdminPassword property of H2 Console"
    See gh-21533

--- spring-boot-project/spring-boot-autoconfigure/src/main/java/org/springframework/boot/autoconfigure/h2/H2ConsoleProperties.java
+++ spring-boot-project/spring-boot-autoconfigure/src/main/java/org/springframework/boot/autoconfigure/h2/H2ConsoleProperties.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2012-2019 the original author or authors.
+ * Copyright 2012-2020 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -77,6 +77,9 @@ public class H2ConsoleProperties {
 		 */
 		private boolean webAllowOthers = false;
 
+		/**
+		 * Password to access preferences and tools of H2 Console.
+		 */
 		private String webAdminPassword;
 
 		public boolean isTrace() {
