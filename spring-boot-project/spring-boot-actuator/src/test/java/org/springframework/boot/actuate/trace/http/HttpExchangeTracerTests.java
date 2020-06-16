855d596c2a46990c0f842b257dba4bdcfe1b16a3
dreis2211 <christoph.dreis@freenet.de>
2020-06-16 19:50:03 +0200

    Use lazy lambda instead of explicit argument
    See gh-21986

--- spring-boot-project/spring-boot-actuator/src/test/java/org/springframework/boot/actuate/trace/http/HttpExchangeTracerTests.java
+++ spring-boot-project/spring-boot-actuator/src/test/java/org/springframework/boot/actuate/trace/http/HttpExchangeTracerTests.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2012-2019 the original author or authors.
+ * Copyright 2012-2020 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -318,7 +318,7 @@ class HttpExchangeTracerTests {
 		@Override
 		protected void postProcessRequestHeaders(Map<String, List<String>> headers) {
 			headers.remove("to-remove");
-			headers.putIfAbsent("to-add", Collections.singletonList("42"));
+			headers.computeIfAbsent("to-add", (key) -> Collections.singletonList("42"));
 		}
 
 	}
