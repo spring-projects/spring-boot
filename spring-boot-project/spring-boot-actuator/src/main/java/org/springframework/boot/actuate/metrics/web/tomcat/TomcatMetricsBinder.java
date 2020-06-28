b34c268547cd7920273eed4d4620fd62a8af45a9
im47cn <67424112+im47cn@users.noreply.github.com>
2020-06-28 10:41:10 +0800

    Avoid NPE when binder is closed before started event
    Previously, if TomcatMetricsBinder destroy() was called before it had
    received an ApplicationStartedEvent an NPE would be thrown due to
    TomcatMetrics being null. This NPE was then caught and logged at
    warning level by the disposable bean adapter.
    
    This prevents the NPE by checking that the TomcatMetrics instance is
    null before calling close() on it.
    
    See gh-22141

--- spring-boot-project/spring-boot-actuator/src/main/java/org/springframework/boot/actuate/metrics/web/tomcat/TomcatMetricsBinder.java
+++ spring-boot-project/spring-boot-actuator/src/main/java/org/springframework/boot/actuate/metrics/web/tomcat/TomcatMetricsBinder.java
@@ -86,7 +86,9 @@ public class TomcatMetricsBinder implements ApplicationListener<ApplicationStart
 
 	@Override
 	public void destroy() {
-		this.tomcatMetrics.close();
+		if (null != this.tomcatMetrics) {
+			this.tomcatMetrics.close();
+		}
 	}
 
 }
