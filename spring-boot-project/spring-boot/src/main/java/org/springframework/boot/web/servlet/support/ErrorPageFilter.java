26f591264c5e15231e24940f81402d753f341eb5
Andy Wilkinson <awilkinson@pivotal.io>
2020-07-07 11:34:25 +0100

    Fix handling of NestedServletException with no root cause
    Fixes gh-22169

--- spring-boot-project/spring-boot/src/main/java/org/springframework/boot/web/servlet/support/ErrorPageFilter.java
+++ spring-boot-project/spring-boot/src/main/java/org/springframework/boot/web/servlet/support/ErrorPageFilter.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2012-2019 the original author or authors.
+ * Copyright 2012-2020 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -137,7 +137,10 @@ public class ErrorPageFilter implements Filter, ErrorPageRegistry {
 		catch (Throwable ex) {
 			Throwable exceptionToHandle = ex;
 			if (ex instanceof NestedServletException) {
-				exceptionToHandle = ((NestedServletException) ex).getRootCause();
+				Throwable rootCause = ((NestedServletException) ex).getRootCause();
+				if (rootCause != null) {
+					exceptionToHandle = rootCause;
+				}
 			}
 			handleException(request, response, wrapped, exceptionToHandle);
 			response.flushBuffer();
