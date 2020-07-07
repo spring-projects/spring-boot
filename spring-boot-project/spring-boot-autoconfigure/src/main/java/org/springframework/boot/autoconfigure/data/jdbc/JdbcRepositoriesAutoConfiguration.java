4e79bb1cff2a938cea99323c0d7122ec9edfcfd7
Andy Wilkinson <awilkinson@pivotal.io>
2020-07-07 11:51:07 +0100

    Disable bean method proxying in SpringBootJdbcConfiguration
    Closes gh-22231

--- spring-boot-project/spring-boot-autoconfigure/src/main/java/org/springframework/boot/autoconfigure/data/jdbc/JdbcRepositoriesAutoConfiguration.java
+++ spring-boot-project/spring-boot-autoconfigure/src/main/java/org/springframework/boot/autoconfigure/data/jdbc/JdbcRepositoriesAutoConfiguration.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2012-2019 the original author or authors.
+ * Copyright 2012-2020 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -61,7 +61,7 @@ public class JdbcRepositoriesAutoConfiguration {
 
 	}
 
-	@Configuration
+	@Configuration(proxyBeanMethods = false)
 	@ConditionalOnMissingBean({ AbstractJdbcConfiguration.class, JdbcConfiguration.class })
 	static class SpringBootJdbcConfiguration extends AbstractJdbcConfiguration {
 
