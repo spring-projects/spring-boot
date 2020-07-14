9558779dd411f02a471a9a8b61f5da5960b90a02
Andy Wilkinson <awilkinson@pivotal.io>
2020-07-14 10:58:09 +0100

    Replace ConfigurationPropertiesBeanDefinitionValidator with a FailureAnalyzer
    Previously, regular bean definitions for configuration properties classes
    that attempt to use constructor binding were detected in a bean factory
    post-processor, ConfigurationPropertiesBeanDefinitionValidator. This
    validation examined every standard bean definition and failed if it
    encountered one for a class that should have used constructor binding.
    There were two downsides to this approach:
    
    1. Reflection used to identify if the bean should be using constructor
       binding triggered class loading that could prevent subsequent
       instrumentation by a load-time weaver.
    2. The cost of the validation was incurred when there was no
       misconfiguration to report.
    
    This commit replaces ConfigurationPropertiesBeanDefinitionValidator
    with a failure analyzer. This failure analyzer only runs once a failure
    has occurred and the application context is not going to complete
    refresh. This avoids causing problems with subsequent instrumentation
    and also avoids the cost of validation and error reporting unless there
    is an error.
    
    Fixes gh-20798

--- spring-boot-project/spring-boot/src/main/java/org/springframework/boot/context/properties/EnableConfigurationPropertiesRegistrar.java
+++ spring-boot-project/spring-boot/src/main/java/org/springframework/boot/context/properties/EnableConfigurationPropertiesRegistrar.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2012-2019 the original author or authors.
+ * Copyright 2012-2020 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -49,7 +49,6 @@ class EnableConfigurationPropertiesRegistrar implements ImportBeanDefinitionRegi
 	@SuppressWarnings("deprecation")
 	static void registerInfrastructureBeans(BeanDefinitionRegistry registry) {
 		ConfigurationPropertiesBindingPostProcessor.register(registry);
-		ConfigurationPropertiesBeanDefinitionValidator.register(registry);
 		ConfigurationBeanFactoryMetadata.register(registry);
 	}
 
--- spring-boot-project/spring-boot/src/test/java/org/springframework/boot/context/properties/ConfigurationPropertiesTests.java
+++ spring-boot-project/spring-boot/src/test/java/org/springframework/boot/context/properties/ConfigurationPropertiesTests.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2012-2019 the original author or authors.
+ * Copyright 2012-2020 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -58,7 +58,6 @@ import org.springframework.boot.testsupport.system.OutputCaptureExtension;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Import;
 import org.springframework.context.annotation.ImportResource;
 import org.springframework.context.annotation.Scope;
 import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
@@ -760,13 +759,6 @@ class ConfigurationPropertiesTests {
 		ConstructorParameterProperties bean = this.context.getBean(ConstructorParameterProperties.class);
 		assertThat(bean.getFoo()).isEqualTo("baz");
 		assertThat(bean.getBar()).isEqualTo(5);
-	}
-
-	@Test // gh-17831
-	void loadWhenBindingConstructorParametersViaImportShouldThrowException() {
-		assertThatExceptionOfType(BeanCreationException.class)
-				.isThrownBy(() -> load(ImportConstructorParameterPropertiesConfiguration.class))
-				.withMessageContaining("@EnableConfigurationProperties or @ConfigurationPropertiesScan must be used");
 	}
 
 	@Test
@@ -1922,13 +1914,6 @@ class ConfigurationPropertiesTests {
 		int getBar() {
 			return this.bar;
 		}
-
-	}
-
-	@Configuration(proxyBeanMethods = false)
-	@EnableConfigurationProperties
-	@Import(ConstructorParameterProperties.class)
-	static class ImportConstructorParameterPropertiesConfiguration {
 
 	}
 
