e31f871b96256918c565ed633ae7da6c9429d5ef
Andy Wilkinson <awilkinson@pivotal.io>
2020-07-16 10:50:21 +0100

    Update JDK 15 CI to use 2020-07-15-08-59
    Closes gh-22346

--- ci/images/get-jdk-url.sh
+++ ci/images/get-jdk-url.sh
@@ -12,7 +12,7 @@ case "$1" in
 		 echo "https://github.com/AdoptOpenJDK/openjdk14-binaries/releases/download/jdk-14.0.1%2B7/OpenJDK14U-jdk_x64_linux_hotspot_14.0.1_7.tar.gz"
 	;;
 	java15)
-		 echo "https://github.com/AdoptOpenJDK/openjdk15-binaries/releases/download/jdk15-2020-07-08-05-49/OpenJDK15-jdk_x64_linux_hotspot_2020-07-08-05-49.tar.gz"
+		 echo "https://github.com/AdoptOpenJDK/openjdk15-binaries/releases/download/jdk15-2020-07-15-08-59/OpenJDK15-jdk_x64_linux_hotspot_2020-07-15-08-59.tar.gz"
 	;;
   *)
 		echo $"Unknown java version"
