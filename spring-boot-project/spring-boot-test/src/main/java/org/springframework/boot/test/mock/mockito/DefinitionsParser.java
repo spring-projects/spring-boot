41954533b28769ae82664d80e4f58b3a210dcb64
Andy Wilkinson <awilkinson@pivotal.io>
2020-07-27 12:52:42 +0100

    Fix Mock|SpyBean context caching
    The fix for gh-20916 updated DefinitionsParser so that the
    ResolvableType for each MockBean or SpyBean field included the
    implementation class from which the field was found. Where the field
    was declared with a variable generic signature that was made constant
    by its implementation class, this allowed the correct concrete type to
    be determined. It also had the unintended side-effect of preventing two
    test classes with identical `@MockBean` and `@SpyBean` configuration
    from sharing a context as the resolvable types for their mock and spy
    bean fields would now be different.
    
    This commit updates DefinitionsParser to only include the
    implementation class in the ResolvableType if the field's generic type
    is variable. For cases where it is not variable, this restores the
    behaviour prior to the fix for gh-20916.
    
    Fixes gh-22566

--- spring-boot-project/spring-boot-test/src/main/java/org/springframework/boot/test/mock/mockito/DefinitionsParser.java
+++ spring-boot-project/spring-boot-test/src/main/java/org/springframework/boot/test/mock/mockito/DefinitionsParser.java
@@ -18,6 +18,7 @@ package org.springframework.boot.test.mock.mockito;
 
 import java.lang.reflect.AnnotatedElement;
 import java.lang.reflect.Field;
+import java.lang.reflect.TypeVariable;
 import java.util.Collection;
 import java.util.Collections;
 import java.util.LinkedHashMap;
@@ -114,7 +115,13 @@ class DefinitionsParser {
 			types.add(ResolvableType.forClass(clazz));
 		}
 		if (types.isEmpty() && element instanceof Field) {
-			types.add(ResolvableType.forField((Field) element, source));
+			Field field = (Field) element;
+			if (field.getGenericType() instanceof TypeVariable) {
+				types.add(ResolvableType.forField(field, source));
+			}
+			else {
+				types.add(ResolvableType.forField(field));
+			}
 		}
 		return types;
 	}
