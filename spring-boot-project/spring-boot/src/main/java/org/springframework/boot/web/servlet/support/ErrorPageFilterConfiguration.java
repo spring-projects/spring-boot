49f8943a5f053d0728eccfad9265d9c5980f4ba7
Andy Wilkinson <awilkinson@pivotal.io>
2020-07-30 13:38:02 +0100

    Register ErrorPageFilter for async dispatch
    Fixes gh-19471

--- spring-boot-project/spring-boot/src/main/java/org/springframework/boot/web/servlet/support/ErrorPageFilterConfiguration.java
+++ spring-boot-project/spring-boot/src/main/java/org/springframework/boot/web/servlet/support/ErrorPageFilterConfiguration.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2012-2019 the original author or authors.
+ * Copyright 2012-2020 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -16,6 +16,9 @@
 
 package org.springframework.boot.web.servlet.support;
 
+import javax.servlet.DispatcherType;
+
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -30,6 +33,13 @@ class ErrorPageFilterConfiguration {
 	@Bean
 	ErrorPageFilter errorPageFilter() {
 		return new ErrorPageFilter();
+	}
+
+	@Bean
+	FilterRegistrationBean<ErrorPageFilter> errorPageFilterRegistration(ErrorPageFilter filter) {
+		FilterRegistrationBean<ErrorPageFilter> registration = new FilterRegistrationBean<>(filter);
+		registration.setDispatcherTypes(DispatcherType.REQUEST, DispatcherType.ASYNC);
+		return registration;
 	}
 
 }
--- spring-boot-project/spring-boot/src/test/java/org/springframework/boot/web/servlet/support/SpringBootServletInitializerTests.java
+++ spring-boot-project/spring-boot/src/test/java/org/springframework/boot/web/servlet/support/SpringBootServletInitializerTests.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2012-2019 the original author or authors.
+ * Copyright 2012-2020 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -17,7 +17,10 @@
 package org.springframework.boot.web.servlet.support;
 
 import java.util.Collections;
+import java.util.EnumSet;
+import java.util.Map;
 
+import javax.servlet.DispatcherType;
 import javax.servlet.ServletContext;
 
 import org.junit.jupiter.api.AfterEach;
@@ -31,6 +34,7 @@ import org.springframework.boot.testsupport.system.CapturedOutput;
 import org.springframework.boot.testsupport.system.OutputCaptureExtension;
 import org.springframework.boot.web.embedded.undertow.UndertowServletWebServerFactory;
 import org.springframework.boot.web.server.WebServer;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
 import org.springframework.context.ApplicationListener;
 import org.springframework.context.ConfigurableApplicationContext;
@@ -106,6 +110,28 @@ class SpringBootServletInitializerTests {
 			try (AbstractApplicationContext context = (AbstractApplicationContext) new WithErrorPageFilterNotRegistered()
 					.createRootApplicationContext(servletContext)) {
 				assertThat(context.getBeansOfType(ErrorPageFilter.class)).hasSize(0);
+			}
+		});
+		try {
+			webServer.start();
+		}
+		finally {
+			webServer.stop();
+		}
+	}
+
+	@Test
+	@SuppressWarnings("rawtypes")
+	void errorPageFilterIsRegisteredForRequestAndAsyncDispatch() {
+		WebServer webServer = new UndertowServletWebServerFactory(0).getWebServer((servletContext) -> {
+			try (AbstractApplicationContext context = (AbstractApplicationContext) new WithErrorPageFilter()
+					.createRootApplicationContext(servletContext)) {
+				Map<String, FilterRegistrationBean> registrations = context
+						.getBeansOfType(FilterRegistrationBean.class);
+				assertThat(registrations).hasSize(1);
+				FilterRegistrationBean errorPageFilterRegistration = registrations.get("errorPageFilterRegistration");
+				assertThat(errorPageFilterRegistration).hasFieldOrPropertyWithValue("dispatcherTypes",
+						EnumSet.of(DispatcherType.ASYNC, DispatcherType.REQUEST));
 			}
 		});
 		try {
@@ -196,6 +222,11 @@ class SpringBootServletInitializerTests {
 		WithErrorPageFilterNotRegistered() {
 			setRegisterErrorPageFilter(false);
 		}
+
+	}
+
+	@Configuration(proxyBeanMethods = false)
+	static class WithErrorPageFilter extends SpringBootServletInitializer {
 
 	}
 
