dd9c9fd171faf89c9ca3e19a93bbe0ac72f4dec5
Stephane Nicoll <snicoll@pivotal.io>
2020-08-10 14:53:18 +0200

    Polish "Migrate legacy endpoint that have a dash in their id"
    See gh-21615

--- spring-boot-tests/spring-boot-smoke-tests/spring-boot-smoke-test-actuator/src/main/java/smoketest/actuator/SampleLegacyEndpointWithDot.java
+++ spring-boot-tests/spring-boot-smoke-tests/spring-boot-smoke-test-actuator/src/main/java/smoketest/actuator/SampleLegacyEndpointWithDot.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2012-2019 the original author or authors.
+ * Copyright 2012-2020 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -25,7 +25,7 @@ import org.springframework.stereotype.Component;
 
 @Component
 @Endpoint(id = "lega.cy")
-public class SampleLegacyEndpoint {
+public class SampleLegacyEndpointWithDot {
 
 	@ReadOperation
 	public Map<String, String> example() {
