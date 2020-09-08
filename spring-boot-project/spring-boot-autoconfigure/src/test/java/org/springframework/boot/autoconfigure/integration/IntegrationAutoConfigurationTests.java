016c46c6b3e1521696488195c1364618075d3767
Andy Wilkinson <awilkinson@pivotal.io>
2020-09-08 09:00:19 +0100

    Temporarily disable Spring Integration RSocket test
    See gh-23214
    See gh-23010

--- spring-boot-project/spring-boot-autoconfigure/src/test/java/org/springframework/boot/autoconfigure/integration/IntegrationAutoConfigurationTests.java
+++ spring-boot-project/spring-boot-autoconfigure/src/test/java/org/springframework/boot/autoconfigure/integration/IntegrationAutoConfigurationTests.java
@@ -20,6 +20,7 @@ import javax.management.MBeanServer;
 
 import io.rsocket.transport.ClientTransport;
 import io.rsocket.transport.netty.client.TcpClientTransport;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import reactor.core.publisher.Mono;
 
@@ -195,6 +196,7 @@ class IntegrationAutoConfigurationTests {
 	}
 
 	@Test
+	@Disabled("SI is incompatible with the latest RSocket snapshots")
 	void rsocketSupportEnabled() {
 		this.contextRunner.withUserConfiguration(RSocketServerConfiguration.class)
 				.withConfiguration(AutoConfigurations.of(RSocketServerAutoConfiguration.class,
