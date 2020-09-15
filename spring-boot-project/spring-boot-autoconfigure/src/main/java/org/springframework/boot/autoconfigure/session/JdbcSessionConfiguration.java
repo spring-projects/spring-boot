5a266155b09f6ce65ae8dc3c4658c3a302f78037
Andy Wilkinson <awilkinson@pivotal.io>
2020-09-15 16:32:15 +0100

    Disable proxyBeanMethods on Spring Session config sub-classes
    Closes gh-23280

--- spring-boot-project/spring-boot-autoconfigure/src/main/java/org/springframework/boot/autoconfigure/session/JdbcSessionConfiguration.java
+++ spring-boot-project/spring-boot-autoconfigure/src/main/java/org/springframework/boot/autoconfigure/session/JdbcSessionConfiguration.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2012-2019 the original author or authors.
+ * Copyright 2012-2020 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -56,7 +56,7 @@ class JdbcSessionConfiguration {
 		return new JdbcSessionDataSourceInitializer(dataSource, resourceLoader, properties);
 	}
 
-	@Configuration
+	@Configuration(proxyBeanMethods = false)
 	static class SpringBootJdbcHttpSessionConfiguration extends JdbcHttpSessionConfiguration {
 
 		@Autowired
