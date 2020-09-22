15f2186aafbf5f4740860d9bba5677874283ab8e
Phillip Webb <pwebb@vmware.com>
2020-09-21 17:46:20 -0700

    Add Java 15 to download script
    See gh-23433

--- ci/images/get-jdk-url.sh
+++ ci/images/get-jdk-url.sh
@@ -11,6 +11,9 @@ case "$1" in
 	java14)
 		 echo "https://github.com/AdoptOpenJDK/openjdk14-binaries/releases/download/jdk-14.0.2%2B12/OpenJDK14U-jdk_x64_linux_hotspot_14.0.2_12.tar.gz"
 	;;
+	java15)
+		 echo "https://github.com/AdoptOpenJDK/openjdk15-binaries/releases/download/jdk-15%2B36/OpenJDK15U-jdk_x64_linux_hotspot_15_36.tar.gz"
+	;;
   *)
 		echo $"Unknown java version"
 		exit 1
