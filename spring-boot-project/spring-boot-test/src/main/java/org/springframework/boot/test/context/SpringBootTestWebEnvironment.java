bf9d23e55a64f78537181788baa695f607ea2ecb
Andy Wilkinson <awilkinson@pivotal.io>
2020-09-24 14:11:43 +0100

    Consider SpringBootTest's web environment in context cache key
    Previously, the web environment configured on `@SpringBootTest` was not
    part of the context cache key. As a result, two test classes that has
    identical configuration other than one using a MOCK web environment and
    the other using a DEFINED_PORT web environment would share a context
    when they should not do so. Classes that use MOCK and RANDOM_PORT were
    not affected as the use of RANDOM_PORT results in a property for the
    port being added to the environment.
    
    This commit adds a new ContextCustomizer, SpringBootTestWebEnvironment,
    that is used to capture the `webEnvironment` from `@SpringBootTest`
    and use it in its hashCode and equals implementations. This fixes the
    problem as all context customizers are evaluated when determing the
    equality of two context cache keys.
    
    Fixes gh-23085

---
+++ spring-boot-project/spring-boot-test/src/main/java/org/springframework/boot/test/context/SpringBootTestWebEnvironment.java
@@ -0,0 +1,57 @@
+/*
+ * Copyright 2012-2020 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.springframework.boot.test.context;
+
+import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.core.annotation.MergedAnnotations;
+import org.springframework.test.context.ContextCustomizer;
+import org.springframework.test.context.MergedContextConfiguration;
+
+/**
+ * {@link ContextCustomizer} to track the web environment that is used in a
+ * {@link SpringBootTest}. The web environment is taken into account when evaluating a
+ * {@link MergedContextConfiguration} to determine if a context can be shared between
+ * tests.
+ *
+ * @author Andy Wilkinson
+ */
+class SpringBootTestWebEnvironment implements ContextCustomizer {
+
+	private final WebEnvironment webEnvironment;
+
+	SpringBootTestWebEnvironment(Class<?> testClass) {
+		this.webEnvironment = MergedAnnotations.from(testClass, MergedAnnotations.SearchStrategy.TYPE_HIERARCHY)
+				.get(SpringBootTest.class).getValue("webEnvironment", WebEnvironment.class).orElse(null);
+	}
+
+	@Override
+	public void customizeContext(ConfigurableApplicationContext context, MergedContextConfiguration mergedConfig) {
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		return (obj != null) && (getClass() == obj.getClass())
+				&& this.webEnvironment == ((SpringBootTestWebEnvironment) obj).webEnvironment;
+	}
+
+	@Override
+	public int hashCode() {
+		return (this.webEnvironment != null) ? this.webEnvironment.hashCode() : 0;
+	}
+
+}
