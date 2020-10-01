baa9d129f296e8a2e1f8f5a88c8337fce582f093
Jasmine Howard <jkhoward@email.wm.edu>
2020-10-01 17:52:21 -0400

    Add configuration option for channelRpcTimeout
    This commit adds a configuration option to configure
    RabbitConnectionFactory's channelRpcTimeout property.
    
    See gh-23564

--- spring-boot-project/spring-boot-autoconfigure/src/test/java/org/springframework/boot/autoconfigure/amqp/RabbitAutoConfigurationTests.java
+++ spring-boot-project/spring-boot-autoconfigure/src/test/java/org/springframework/boot/autoconfigure/amqp/RabbitAutoConfigurationTests.java
@@ -140,7 +140,7 @@ class RabbitAutoConfigurationTests {
 				.withPropertyValues("spring.rabbitmq.host:remote-server", "spring.rabbitmq.port:9000",
 						"spring.rabbitmq.address-shuffle-mode=random", "spring.rabbitmq.username:alice",
 						"spring.rabbitmq.password:secret", "spring.rabbitmq.virtual_host:/vhost",
-						"spring.rabbitmq.connection-timeout:123")
+						"spring.rabbitmq.connection-timeout:123", "spring.rabbitmq.channel-rpc-timeout:140")
 				.run((context) -> {
 					CachingConnectionFactory connectionFactory = context.getBean(CachingConnectionFactory.class);
 					assertThat(connectionFactory.getHost()).isEqualTo("remote-server");
@@ -150,6 +150,7 @@ class RabbitAutoConfigurationTests {
 					assertThat(connectionFactory.getVirtualHost()).isEqualTo("/vhost");
 					com.rabbitmq.client.ConnectionFactory rcf = connectionFactory.getRabbitConnectionFactory();
 					assertThat(rcf.getConnectionTimeout()).isEqualTo(123);
+					assertThat(rcf.getChannelRpcTimeout()).isEqualTo(140);
 					assertThat((List<Address>) ReflectionTestUtils.getField(connectionFactory, "addresses")).hasSize(1);
 				});
 	}
