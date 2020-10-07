e4547107c7a1b71af149dc40526584e0a12651e6
o.pakers <o.pakers@intrum.com>
2020-10-07 23:40:37 +0300

    Support for svg favicon

--- spring-boot-project/spring-boot-autoconfigure/src/main/java/org/springframework/boot/autoconfigure/security/StaticResourceLocation.java
+++ spring-boot-project/spring-boot-autoconfigure/src/main/java/org/springframework/boot/autoconfigure/security/StaticResourceLocation.java
@@ -50,7 +50,7 @@ public enum StaticResourceLocation {
 	/**
 	 * The {@code "favicon.ico"} resource.
 	 */
-	FAVICON("/**/favicon.ico");
+	FAVICON("/**/favicon.ico", "/**/favicon.svg");
 
 	private final String[] patterns;
 
