ba88c9dc3e307f1f58902d7a0358a653b1e5f3a3
Stephane Nicoll <snicoll@pivotal.io>
2020-10-26 13:06:02 +0100

    Fix configuration of Webflux Spring Session smoke test
    See gh-23752

--- spring-boot-tests/spring-boot-smoke-tests/spring-boot-smoke-test-session-webflux/src/test/java/smoketest/session/SampleSessionWebFluxApplicationTests.java
+++ spring-boot-tests/spring-boot-smoke-tests/spring-boot-smoke-test-session-webflux/src/test/java/smoketest/session/SampleSessionWebFluxApplicationTests.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2012-2019 the original author or authors.
+ * Copyright 2012-2020 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -35,8 +35,7 @@ import static org.assertj.core.api.Assertions.assertThat;
  *
  * @author Vedran Pavic
  */
-@SpringBootTest(properties = "server.servlet.session.timeout:2",
-		webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@SpringBootTest(properties = "spring.session.timeout:2", webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class SampleSessionWebFluxApplicationTests {
 
 	@LocalServerPort
