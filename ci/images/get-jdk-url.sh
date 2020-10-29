88a105591621e6c69704997fa6f2568f8b38b34c
Phillip Webb <pwebb@vmware.com>
2020-10-28 19:25:05 -0700

    Upgrade CI to Java 11.0.9_11
    Closes gh-23947

--- ci/images/get-jdk-url.sh
+++ ci/images/get-jdk-url.sh
@@ -6,7 +6,7 @@ case "$1" in
 		 echo "https://github.com/AdoptOpenJDK/openjdk8-binaries/releases/download/jdk8u272-b10/OpenJDK8U-jdk_x64_linux_hotspot_8u272b10.tar.gz"
 	;;
 	java11)
-		 echo "https://github.com/AdoptOpenJDK/openjdk11-binaries/releases/download/jdk-11.0.8%2B10/OpenJDK11U-jdk_x64_linux_hotspot_11.0.8_10.tar.gz"
+		 echo "https://github.com/AdoptOpenJDK/openjdk11-binaries/releases/download/jdk-11.0.9%2B11.1/OpenJDK11U-jdk_x64_linux_hotspot_11.0.9_11.tar.gz"
 	;;
 	*)
 		echo $"Unknown java version"
