e7eb7739dcbcc31dcf0ab2461f42b023efca0b01
Andy Wilkinson <wilkinsona@vmware.com>
2020-10-29 14:35:38 +0000

    Use overriding rather than overridden getter methods in config prop AP
    Fixes gh-23966

--- spring-boot-project/spring-boot-tools/spring-boot-configuration-processor/src/main/java/org/springframework/boot/configurationprocessor/TypeElementMembers.java
+++ spring-boot-project/spring-boot-tools/spring-boot-configuration-processor/src/main/java/org/springframework/boot/configurationprocessor/TypeElementMembers.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2012-2019 the original author or authors.
+ * Copyright 2012-2020 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -74,7 +74,7 @@ class TypeElementMembers {
 	private void processMethod(ExecutableElement method) {
 		if (isPublic(method)) {
 			String name = method.getSimpleName().toString();
-			if (isGetter(method) && !this.publicGetters.containsKey(name)) {
+			if (isGetter(method) && !this.publicGetters.containsKey(getAccessorName(name))) {
 				this.publicGetters.put(getAccessorName(name), method);
 			}
 			else if (isSetter(method)) {
