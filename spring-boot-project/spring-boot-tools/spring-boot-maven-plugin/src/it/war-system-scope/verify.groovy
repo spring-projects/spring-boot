31042bfc4980610b2b6bc3af4f66b7a2625a330d
Andy Wilkinson <wilkinsona@vmware.com>
2020-11-10 13:59:23 +0000

    Test inclusion of system scope dependencies in repackaged war
    Closes gh-22503

---
+++ spring-boot-project/spring-boot-tools/spring-boot-maven-plugin/src/it/war-system-scope/verify.groovy
@@ -0,0 +1,27 @@
+/*
+ * Copyright 2012-2020 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import java.io.*;
+import org.springframework.boot.maven.*;
+
+File f = new File(basedir, "target/war-system-scope-0.0.1.BUILD-SNAPSHOT.war")
+new Verify.WarArchiveVerification(f) {
+	@Override
+	protected void verifyZipEntries(Verify.ArchiveVerifier verifier) throws Exception {
+		super.verifyZipEntries(verifier)
+		verifier.assertHasEntryNameStartingWith("WEB-INF/lib-provided/sample-1.0.0.jar")
+	}
+}.verify()
