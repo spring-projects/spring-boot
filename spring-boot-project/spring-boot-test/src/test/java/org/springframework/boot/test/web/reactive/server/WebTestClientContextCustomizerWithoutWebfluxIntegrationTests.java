f248cfe79c8cd82ce7914dca6d6a3cae11d7ccf0
Stephane Nicoll <snicoll@vmware.com>
2020-11-25 14:52:47 +0100

    Polish "Check that WebClient is available"
    See gh-24152

--- spring-boot-project/spring-boot-test/src/test/java/org/springframework/boot/test/web/reactive/server/WebTestClientContextCustomizerWithoutWebfluxIntegrationTests.java
+++ spring-boot-project/spring-boot-test/src/test/java/org/springframework/boot/test/web/reactive/server/WebTestClientContextCustomizerWithoutWebfluxIntegrationTests.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2012-2019 the original author or authors.
+ * Copyright 2012-2020 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -17,38 +17,34 @@ package org.springframework.boot.test.web.reactive.server;
 
 import java.util.Collections;
 
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.testsupport.classpath.ClassPathExclusions;
 import org.springframework.test.context.ContextCustomizer;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 /**
- * Tests for {@link WebTestClientContextCustomizerFactory} when spring webflux is on the
- * classpath.
+ * Tests for {@link WebTestClientContextCustomizerFactory} when spring webflux is not on
+ * the classpath.
  *
  * @author Tobias Gesellchen
+ * @author Stephane Nicoll
  */
-class WebTestClientContextCustomizerFactoryWithWebfluxTest {
-
-	WebTestClientContextCustomizerFactory contextCustomizerFactory;
-
-	@BeforeEach
-	void setup() {
-		this.contextCustomizerFactory = new WebTestClientContextCustomizerFactory();
-	}
+@ClassPathExclusions("spring-webflux*.jar")
+public class WebTestClientContextCustomizerWithoutWebfluxIntegrationTests {
 
 	@Test
-	void createContextCustomizer() {
-		ContextCustomizer contextCustomizer = this.contextCustomizerFactory.createContextCustomizer(TestClass.class,
+	void customizerIsNotCreatedWithoutWebClient() {
+		WebTestClientContextCustomizerFactory contextCustomizerFactory = new WebTestClientContextCustomizerFactory();
+		ContextCustomizer contextCustomizer = contextCustomizerFactory.createContextCustomizer(TestClass.class,
 				Collections.emptyList());
-		assertThat(contextCustomizer).isNotNull();
+		assertThat(contextCustomizer).isNull();
 	}
 
 	@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
-	static class TestClass {
+	private static class TestClass {
 
 	}
 
