08b692581a963848012af373f751c7d34da3b012
Stephane Nicoll <snicoll@vmware.com>
2020-12-14 14:22:07 +0100

    Prepare 2.4.x branch

--- buildSrc/src/main/java/org/springframework/boot/build/AsciidoctorConventions.java
+++ buildSrc/src/main/java/org/springframework/boot/build/AsciidoctorConventions.java
@@ -166,7 +166,7 @@ class AsciidoctorConventions {
 
 	private String determineGitHubTag(Project project) {
 		String version = "v" + project.getVersion();
-		return (version.endsWith("-SNAPSHOT")) ? "master" : version;
+		return (version.endsWith("-SNAPSHOT")) ? "2.4.x" : version;
 	}
 
 	/**
