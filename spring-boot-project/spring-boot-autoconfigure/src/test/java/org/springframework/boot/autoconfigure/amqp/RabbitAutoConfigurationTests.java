59910338c97253cf2b0b08fa493d044a7766d7f1
Stephane Nicoll <snicoll@vmware.com>
2020-12-15 10:59:39 +0100

    Polish "Add configuration for Rabbit's key store and trust store algorithm"
    See gh-24076

--- spring-boot-project/spring-boot-autoconfigure/src/test/java/org/springframework/boot/autoconfigure/amqp/RabbitAutoConfigurationTests.java
+++ spring-boot-project/spring-boot-autoconfigure/src/test/java/org/springframework/boot/autoconfigure/amqp/RabbitAutoConfigurationTests.java
@@ -739,7 +739,7 @@ class RabbitAutoConfigurationTests {
 	}
 
 	@Test
-	void enableSslWithValidStoreAlgorithmShouldWork() throws Exception {
+	void enableSslWithValidStoreAlgorithmShouldWork() {
 		this.contextRunner.withUserConfiguration(TestConfiguration.class)
 				.withPropertyValues("spring.rabbitmq.ssl.enabled:true",
 						"spring.rabbitmq.ssl.keyStore=/org/springframework/boot/autoconfigure/amqp/test.jks",
@@ -752,18 +752,29 @@ class RabbitAutoConfigurationTests {
 	}
 
 	@Test
-	void enableSslWithInvalidStoreAlgorithmShouldFail() throws Exception {
+	void enableSslWithInvalidKeyStoreAlgorithmShouldFail() {
 		this.contextRunner.withUserConfiguration(TestConfiguration.class)
 				.withPropertyValues("spring.rabbitmq.ssl.enabled:true",
 						"spring.rabbitmq.ssl.keyStore=/org/springframework/boot/autoconfigure/amqp/test.jks",
 						"spring.rabbitmq.ssl.keyStoreType=jks", "spring.rabbitmq.ssl.keyStorePassword=secret",
-						"spring.rabbitmq.ssl.keyStoreAlgorithm=foo",
-						"spring.rabbitmq.ssl.trustStore=/org/springframework/boot/autoconfigure/amqp/test.jks",
-						"spring.rabbitmq.ssl.trustStoreType=jks", "spring.rabbitmq.ssl.trustStorePassword=secret",
-						"spring.rabbitmq.ssl.trustStoreAlgorithm=foo")
+						"spring.rabbitmq.ssl.keyStoreAlgorithm=test-invalid-algo")
 				.run((context) -> {
 					assertThat(context).hasFailed();
-					assertThat(context).getFailure().hasMessageContaining("foo");
+					assertThat(context).getFailure().hasMessageContaining("test-invalid-algo");
+					assertThat(context).getFailure().hasRootCauseInstanceOf(NoSuchAlgorithmException.class);
+				});
+	}
+
+	@Test
+	void enableSslWithInvalidTrustStoreAlgorithmShouldFail() {
+		this.contextRunner.withUserConfiguration(TestConfiguration.class)
+				.withPropertyValues("spring.rabbitmq.ssl.enabled:true",
+						"spring.rabbitmq.ssl.trustStore=/org/springframework/boot/autoconfigure/amqp/test.jks",
+						"spring.rabbitmq.ssl.trustStoreType=jks", "spring.rabbitmq.ssl.trustStorePassword=secret",
+						"spring.rabbitmq.ssl.trustStoreAlgorithm=test-invalid-algo")
+				.run((context) -> {
+					assertThat(context).hasFailed();
+					assertThat(context).getFailure().hasMessageContaining("test-invalid-algo");
 					assertThat(context).getFailure().hasRootCauseInstanceOf(NoSuchAlgorithmException.class);
 				});
 	}
