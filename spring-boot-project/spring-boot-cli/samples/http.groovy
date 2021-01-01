097104584cb29185b953767b4541111b8ab62763
Andrey Kolchanov <andreyka4623@gmail.com>
2021-01-01 17:15:38 +0300

    Remove unnecessary semicolon
    See gh-24628

--- spring-boot-project/spring-boot-cli/samples/http.groovy
+++ spring-boot-project/spring-boot-cli/samples/http.groovy
@@ -7,7 +7,7 @@ import groovyx.net.http.*
 class Example implements CommandLineRunner {
 
 	@Autowired
-	ApplicationContext context;
+	ApplicationContext context
 
 	@RequestMapping("/")
 	@ResponseBody
@@ -16,7 +16,7 @@ class Example implements CommandLineRunner {
 	}
 
 	void run(String... args) {
-		def port = context.webServer.port;
+		def port = context.webServer.port
 		def world = new RESTClient("http://localhost:" + port).get(path:"/").data.text
 		print "Hello " + world
 	}
