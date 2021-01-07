7cbd57b6359b2ca7f53f7e5e5eee8fe834fc19bd
Phillip Webb <pwebb@vmware.com>
2021-01-06 17:26:49 -0800

    Log FailureAnalyzer errors at trace level
    Update `FailureAnalyzers` to log errors at `trace` rather than `debug`.
    This should reduce noise when an analyzer unexpectedly fails and the
    user is running with `--debug`.
    
    Fixes gh-24630

--- spring-boot-project/spring-boot/src/main/java/org/springframework/boot/diagnostics/FailureAnalyzers.java
+++ spring-boot-project/spring-boot/src/main/java/org/springframework/boot/diagnostics/FailureAnalyzers.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2012-2019 the original author or authors.
+ * Copyright 2012-2021 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -114,7 +114,7 @@ final class FailureAnalyzers implements SpringBootExceptionReporter {
 				}
 			}
 			catch (Throwable ex) {
-				logger.debug(LogMessage.format("FailureAnalyzer %s failed", analyzer), ex);
+				logger.trace(LogMessage.format("FailureAnalyzer %s failed", analyzer), ex);
 			}
 		}
 		return null;
