604e6a3e4e4e90949fb8d7a67902de3cfe61852a
a1015032205 <1015032205@qq.com>
2021-01-24 21:52:26 +0800

    init

---
+++ spring-boot-cfx01/src/main/java/com/md/cfx/CfxApplication.java
@@ -0,0 +1,18 @@
+package com.md.cfx;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @Author 秒度
+ * @Email fangxin.md@Gmail.com
+ * @Date 2021/1/24 16:00
+ * @Description
+ */
+@SpringBootApplication
+public class CfxApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(CfxApplication.class);
+
+	}
+}
