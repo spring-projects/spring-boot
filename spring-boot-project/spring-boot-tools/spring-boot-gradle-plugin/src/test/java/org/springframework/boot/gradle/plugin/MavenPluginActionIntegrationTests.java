d30251092b3f4805586f67e5cf2084ee80d890df
Andy Wilkinson <wilkinsona@vmware.com>
2021-02-04 10:16:48 +0000

    Test the Gradle Plugin against 7.0-milestone-1
    Closes gh-25103

--- spring-boot-project/spring-boot-tools/spring-boot-gradle-plugin/src/test/java/org/springframework/boot/gradle/plugin/MavenPluginActionIntegrationTests.java
+++ spring-boot-project/spring-boot-tools/spring-boot-gradle-plugin/src/test/java/org/springframework/boot/gradle/plugin/MavenPluginActionIntegrationTests.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2012-2020 the original author or authors.
+ * Copyright 2012-2021 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -28,7 +28,7 @@ import static org.assertj.core.api.Assertions.assertThat;
  *
  * @author Andy Wilkinson
  */
-@GradleCompatibility
+@GradleCompatibility(versionsLessThan = "7.0-milestone-1")
 class MavenPluginActionIntegrationTests {
 
 	GradleBuild gradleBuild;
