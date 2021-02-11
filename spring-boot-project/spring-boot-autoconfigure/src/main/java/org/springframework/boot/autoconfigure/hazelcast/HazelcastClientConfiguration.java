0bc03c7141cb055add162e5e33873e482fb1c058
Stephane Nicoll <snicoll@vmware.com>
2021-02-11 15:13:10 +0100

    Associate application classloader to auto-configured Hazelcast instance
    Closes gh-24836

--- spring-boot-project/spring-boot-autoconfigure/src/main/java/org/springframework/boot/autoconfigure/hazelcast/HazelcastClientConfiguration.java
+++ spring-boot-project/spring-boot-autoconfigure/src/main/java/org/springframework/boot/autoconfigure/hazelcast/HazelcastClientConfiguration.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2012-2020 the original author or authors.
+ * Copyright 2012-2021 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -17,9 +17,12 @@
 package org.springframework.boot.autoconfigure.hazelcast;
 
 import java.io.IOException;
+import java.net.URL;
 
 import com.hazelcast.client.HazelcastClient;
 import com.hazelcast.client.config.ClientConfig;
+import com.hazelcast.client.config.XmlClientConfigBuilder;
+import com.hazelcast.client.config.YamlClientConfigBuilder;
 import com.hazelcast.core.HazelcastInstance;
 
 import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
@@ -29,6 +32,8 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Conditional;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.io.Resource;
+import org.springframework.core.io.ResourceLoader;
+import org.springframework.util.StringUtils;
 
 /**
  * Configuration for Hazelcast client.
@@ -43,18 +48,34 @@ class HazelcastClientConfiguration {
 
 	static final String CONFIG_SYSTEM_PROPERTY = "hazelcast.client.config";
 
+	private static HazelcastInstance getHazelcastInstance(ClientConfig config) {
+		if (StringUtils.hasText(config.getInstanceName())) {
+			return HazelcastClient.getOrCreateHazelcastClient(config);
+		}
+		return HazelcastClient.newHazelcastClient(config);
+	}
+
 	@Configuration(proxyBeanMethods = false)
 	@ConditionalOnMissingBean(ClientConfig.class)
 	@Conditional(HazelcastClientConfigAvailableCondition.class)
 	static class HazelcastClientConfigFileConfiguration {
 
 		@Bean
-		HazelcastInstance hazelcastInstance(HazelcastProperties properties) throws IOException {
-			Resource config = properties.resolveConfigLocation();
-			if (config != null) {
-				return new HazelcastClientFactory(config).getHazelcastInstance();
+		HazelcastInstance hazelcastInstance(HazelcastProperties properties, ResourceLoader resourceLoader)
+				throws IOException {
+			Resource configLocation = properties.resolveConfigLocation();
+			ClientConfig config = (configLocation != null) ? loadClientConfig(configLocation) : ClientConfig.load();
+			config.setClassLoader(resourceLoader.getClassLoader());
+			return getHazelcastInstance(config);
+		}
+
+		private ClientConfig loadClientConfig(Resource configLocation) throws IOException {
+			URL configUrl = configLocation.getURL();
+			String configFileName = configUrl.getPath();
+			if (configFileName.endsWith(".yaml")) {
+				return new YamlClientConfigBuilder(configUrl).build();
 			}
-			return HazelcastClient.newHazelcastClient();
+			return new XmlClientConfigBuilder(configUrl).build();
 		}
 
 	}
@@ -65,7 +86,7 @@ class HazelcastClientConfiguration {
 
 		@Bean
 		HazelcastInstance hazelcastInstance(ClientConfig config) {
-			return new HazelcastClientFactory(config).getHazelcastInstance();
+			return getHazelcastInstance(config);
 		}
 
 	}
