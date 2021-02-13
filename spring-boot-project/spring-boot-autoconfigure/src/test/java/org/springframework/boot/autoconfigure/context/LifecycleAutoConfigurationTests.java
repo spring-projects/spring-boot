4f4c9ddd58ea866223155aece5caf36bb0be81c6
izeye <izeye@naver.com>
2021-02-13 23:18:27 +0900

    Polish

--- spring-boot-project/spring-boot-autoconfigure/src/test/java/org/springframework/boot/autoconfigure/context/LifecycleAutoConfigurationTests.java
+++ spring-boot-project/spring-boot-autoconfigure/src/test/java/org/springframework/boot/autoconfigure/context/LifecycleAutoConfigurationTests.java
@@ -47,7 +47,7 @@ public class LifecycleAutoConfigurationTests {
 	}
 
 	@Test
-	void lifecycleProcessorIsConfiguredWithCustomDefaultTimeout() {
+	void lifecycleProcessorIsConfiguredWithCustomTimeout() {
 		this.contextRunner.withPropertyValues("spring.lifecycle.timeout-per-shutdown-phase=15s").run((context) -> {
 			assertThat(context).hasBean(AbstractApplicationContext.LIFECYCLE_PROCESSOR_BEAN_NAME);
 			Object processor = context.getBean(AbstractApplicationContext.LIFECYCLE_PROCESSOR_BEAN_NAME);
@@ -56,7 +56,7 @@ public class LifecycleAutoConfigurationTests {
 	}
 
 	@Test
-	void lifecycleProcessorIsConfiguredWithCustomDefaultTimeoutInAChildContext() {
+	void lifecycleProcessorIsConfiguredWithCustomTimeoutInAChildContext() {
 		new ApplicationContextRunner().run((parent) -> {
 			this.contextRunner.withParent(parent).withPropertyValues("spring.lifecycle.timeout-per-shutdown-phase=15s")
 					.run((child) -> {
