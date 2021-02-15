3f9f9a463726cb871cd46f490395eba09b67e007
Stephane Nicoll <snicoll@vmware.com>
2021-02-15 13:46:39 +0100

    Polish "Start building against Spring Data Neumann-SR7 snapshots"
    See gh-25282

--- spring-boot-project/spring-boot-autoconfigure/src/test/java/org/springframework/boot/autoconfigure/data/neo4j/Neo4jDataAutoConfigurationTests.java
+++ spring-boot-project/spring-boot-autoconfigure/src/test/java/org/springframework/boot/autoconfigure/data/neo4j/Neo4jDataAutoConfigurationTests.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2012-2020 the original author or authors.
+ * Copyright 2012-2021 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -215,7 +215,9 @@ class Neo4jDataAutoConfigurationTests {
 	void doesNotProvideABookmarkManagerIfNotPossible() {
 		this.contextRunner.withClassLoader(new FilteredClassLoader(Caffeine.class, EmbeddedDriver.class))
 				.withUserConfiguration(BookmarkManagementEnabledConfiguration.class)
-				.run((context) -> assertThat(context).doesNotHaveBean(BookmarkManager.class));
+				.run((context) -> assertThat(context).hasFailed().getFailure()
+						.hasMessageContaining("Bookmark management has been enabled")
+						.hasMessageContaining("Please provide a bookmark manager"));
 	}
 
 	private static void assertDomainTypesDiscovered(Neo4jMappingContext mappingContext, Class<?>... types) {
