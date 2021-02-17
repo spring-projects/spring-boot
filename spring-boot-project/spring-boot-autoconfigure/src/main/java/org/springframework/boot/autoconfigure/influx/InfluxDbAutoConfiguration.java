5718a7dd609d1e7492627d98a035926ab3d8408a
Eddú Meléndez <eddu.melendez@gmail.com>
2021-02-16 18:01:52 -0600

    Add configuration properties to InfluxDb auto-configuration
    More configuration properties are added in order to allow
    customization for `influxdb` bean. Also, in order to
    customize `BatchOptions`, `InfluxDbBatchOptionsCustomizer` is added.
    
    See gh-20867

--- spring-boot-project/spring-boot-autoconfigure/src/main/java/org/springframework/boot/autoconfigure/influx/InfluxDbAutoConfiguration.java
+++ spring-boot-project/spring-boot-autoconfigure/src/main/java/org/springframework/boot/autoconfigure/influx/InfluxDbAutoConfiguration.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2012-2019 the original author or authors.
+ * Copyright 2012-2021 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -17,6 +17,7 @@
 package org.springframework.boot.autoconfigure.influx;
 
 import okhttp3.OkHttpClient;
+import org.influxdb.BatchOptions;
 import org.influxdb.InfluxDB;
 import org.influxdb.impl.InfluxDBImpl;
 
@@ -26,6 +27,7 @@ import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.boot.context.properties.PropertyMapper;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -45,10 +47,34 @@ public class InfluxDbAutoConfiguration {
 	@Bean
 	@ConditionalOnMissingBean
 	@ConditionalOnProperty("spring.influx.url")
-	public InfluxDB influxDb(InfluxDbProperties properties,
-			ObjectProvider<InfluxDbOkHttpClientBuilderProvider> builder) {
-		return new InfluxDBImpl(properties.getUrl(), properties.getUser(), properties.getPassword(),
+	public InfluxDB influxDb(InfluxDbProperties properties, ObjectProvider<InfluxDbOkHttpClientBuilderProvider> builder,
+			ObjectProvider<InfluxDbBatchOptionsCustomizer> customizers) {
+		InfluxDB influxDb = new InfluxDBImpl(properties.getUrl(), properties.getUser(), properties.getPassword(),
 				determineBuilder(builder.getIfAvailable()));
+		PropertyMapper map = PropertyMapper.get().alwaysApplyingWhenNonNull();
+		map.from(properties::getConsistency).to(influxDb::setConsistency);
+		map.from(properties::getDatabase).to(influxDb::setDatabase);
+		map.from(properties::getLog).to(influxDb::setLogLevel);
+		map.from(properties::getRetentionPolicy).to(influxDb::setRetentionPolicy);
+		if (properties.isGzipEnabled()) {
+			influxDb.enableGzip();
+		}
+		if (properties.getBatch().isEnabled()) {
+			BatchOptions batchOptions = mapBatchOptions(properties);
+			InfluxDbBatchOptionsCustomizer influxDbBatchOptionsCustomizer = customizers.orderedStream()
+					.reduce((after, before) -> (options) -> after.customize(before.customize(options)))
+					.orElse((options) -> options);
+			influxDb.enableBatch(influxDbBatchOptionsCustomizer.customize(batchOptions));
+		}
+		return influxDb;
+	}
+
+	private BatchOptions mapBatchOptions(InfluxDbProperties properties) {
+		InfluxDbProperties.Batch batch = properties.getBatch();
+		return BatchOptions.DEFAULTS.actions(batch.getActions()).flushDuration(batch.getFlushDuration())
+				.jitterDuration(batch.getJitterDuration()).bufferLimit(batch.getBufferLimit())
+				.consistency(batch.getConsistency()).precision(batch.getPrecision())
+				.dropActionsOnQueueExhaustion(batch.isDropActionsOnQueueExhaustion());
 	}
 
 	private static OkHttpClient.Builder determineBuilder(InfluxDbOkHttpClientBuilderProvider builder) {
