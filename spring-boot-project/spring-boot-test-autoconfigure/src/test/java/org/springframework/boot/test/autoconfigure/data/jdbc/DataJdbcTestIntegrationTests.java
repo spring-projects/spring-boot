cb1de4791f9c5dbba8795714483c8475d5294169
Zhengxi Li <lzx404243@hotmail.com>
2021-02-25 02:13:11 -0600

    Clear all entry from the repository in DataJdbcTestIntegrationTests

--- spring-boot-project/spring-boot-test-autoconfigure/src/test/java/org/springframework/boot/test/autoconfigure/data/jdbc/DataJdbcTestIntegrationTests.java
+++ spring-boot-project/spring-boot-test-autoconfigure/src/test/java/org/springframework/boot/test/autoconfigure/data/jdbc/DataJdbcTestIntegrationTests.java
@@ -59,6 +59,7 @@ class DataJdbcTestIntegrationTests {
 		this.jdbcTemplate.update("INSERT INTO EXAMPLE_ENTITY (id, name, reference) VALUES (1, 'a', 'alpha')");
 		this.jdbcTemplate.update("INSERT INTO EXAMPLE_ENTITY (id, name, reference) VALUES (2, 'b', 'bravo')");
 		assertThat(this.repository.findAll()).hasSize(2);
+		this.repository.deleteAll();
 	}
 
 	@Test
