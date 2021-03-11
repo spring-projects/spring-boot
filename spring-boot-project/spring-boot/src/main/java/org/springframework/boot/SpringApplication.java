3d0e319442d01bbfd198e5beed73f17eff664d35
dududu <dududuburn@git.dev.tencent.com>
2021-03-11 16:54:40 +0800

    Quickly to find the main

--- spring-boot-project/spring-boot/src/main/java/org/springframework/boot/SpringApplication.java
+++ spring-boot-project/spring-boot/src/main/java/org/springframework/boot/SpringApplication.java
@@ -291,7 +291,11 @@ public class SpringApplication {
 	private Class<?> deduceMainApplicationClass() {
 		try {
 			StackTraceElement[] stackTrace = new RuntimeException().getStackTrace();
-			for (StackTraceElement stackTraceElement : stackTrace) {
+			StackTraceElement[] var2 = stackTrace;
+			int var3 = stackTrace.length;
+
+			for(int var4 = var3 - 1; var4 >= 0; --var4) {
+				StackTraceElement stackTraceElement = var2[var4];
 				if ("main".equals(stackTraceElement.getMethodName())) {
 					return Class.forName(stackTraceElement.getClassName());
 				}
