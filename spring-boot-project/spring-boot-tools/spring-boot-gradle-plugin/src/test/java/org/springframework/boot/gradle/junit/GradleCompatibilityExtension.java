83342c7aa7119da8dccf98e364c4d7872d5016c2
Andy Wilkinson <wilkinsona@vmware.com>
2021-03-19 17:01:38 +0000

    Test the Gradle plugin against 7.0-milestone-3
    Closes gh-25747

--- spring-boot-project/spring-boot-tools/spring-boot-gradle-plugin/src/test/java/org/springframework/boot/gradle/junit/GradleCompatibilityExtension.java
+++ spring-boot-project/spring-boot-tools/spring-boot-gradle-plugin/src/test/java/org/springframework/boot/gradle/junit/GradleCompatibilityExtension.java
@@ -49,11 +49,11 @@ final class GradleCompatibilityExtension implements TestTemplateInvocationContex
 		JavaVersion javaVersion = JavaVersion.current();
 		if (javaVersion.isCompatibleWith(JavaVersion.VERSION_14)
 				|| javaVersion.isCompatibleWith(JavaVersion.VERSION_13)) {
-			GRADLE_VERSIONS = Arrays.asList("6.3", "6.4.1", "6.5.1", "6.6.1", "6.7.1", "current", "7.0-milestone-2");
+			GRADLE_VERSIONS = Arrays.asList("6.3", "6.4.1", "6.5.1", "6.6.1", "6.7.1", "current", "7.0-milestone-3");
 		}
 		else {
 			GRADLE_VERSIONS = Arrays.asList("5.6.4", "6.3", "6.4.1", "6.5.1", "6.6.1", "6.7.1", "current",
-					"7.0-milestone-2");
+					"7.0-milestone-3");
 		}
 	}
 
