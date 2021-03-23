b8873578ca545e2b6309c64a8ccbedc1803e48e1
Phillip Webb <pwebb@vmware.com>
2021-03-23 16:16:49 -0700

    Add missing @Deprecated annotations

--- spring-boot-project/spring-boot-tools/spring-boot-loader/src/main/java/org/springframework/boot/loader/archive/ExplodedArchive.java
+++ spring-boot-project/spring-boot-tools/spring-boot-loader/src/main/java/org/springframework/boot/loader/archive/ExplodedArchive.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2012-2020 the original author or authors.
+ * Copyright 2012-2021 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -103,6 +103,7 @@ public class ExplodedArchive implements Archive {
 	}
 
 	@Override
+	@Deprecated
 	public Iterator<Entry> iterator() {
 		return new EntryIterator(this.root, this.recursive, null, null);
 	}
@@ -321,6 +322,7 @@ public class ExplodedArchive implements Archive {
 		}
 
 		@Override
+		@Deprecated
 		public Iterator<Entry> iterator() {
 			return Collections.emptyIterator();
 		}
--- spring-boot-project/spring-boot-tools/spring-boot-loader/src/main/java/org/springframework/boot/loader/archive/JarFileArchive.java
+++ spring-boot-project/spring-boot-tools/spring-boot-loader/src/main/java/org/springframework/boot/loader/archive/JarFileArchive.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2012-2020 the original author or authors.
+ * Copyright 2012-2021 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -81,6 +81,7 @@ public class JarFileArchive implements Archive {
 	}
 
 	@Override
+	@Deprecated
 	public Iterator<Entry> iterator() {
 		return new EntryIterator(this.jarFile.iterator(), null, null);
 	}
