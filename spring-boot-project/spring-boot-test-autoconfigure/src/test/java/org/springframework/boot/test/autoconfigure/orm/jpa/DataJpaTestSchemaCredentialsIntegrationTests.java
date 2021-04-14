05adf02a4716043d3578d679c6c7f1b25710a974
Andy Wilkinson <wilkinsona@vmware.com>
2021-04-14 15:10:51 +0100

    Include SQL init auto-config in JDBC-based test slices
    Closes gh-25323

--- spring-boot-project/spring-boot-test-autoconfigure/src/test/java/org/springframework/boot/test/autoconfigure/orm/jpa/DataJpaTestSchemaCredentialsIntegrationTests.java
+++ spring-boot-project/spring-boot-test-autoconfigure/src/test/java/org/springframework/boot/test/autoconfigure/orm/jpa/DataJpaTestSchemaCredentialsIntegrationTests.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2012-2020 the original author or authors.
+ * Copyright 2012-2021 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -32,8 +32,7 @@ import static org.assertj.core.api.Assertions.assertThat;
  * @author Andy Wilkinson
  */
 @DataJpaTest
-@TestPropertySource(
-		properties = { "spring.datasource.schema-username=alice", "spring.datasource.schema-password=secret" })
+@TestPropertySource(properties = { "spring.sql.init.username=alice", "spring.sql.init.password=secret" })
 class DataJpaTestSchemaCredentialsIntegrationTests {
 
 	@Autowired
