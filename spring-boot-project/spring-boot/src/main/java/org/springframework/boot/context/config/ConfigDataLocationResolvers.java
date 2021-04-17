46a0e1ec3ff657e237301349afe021f233df30f6
Sriram <vsriram92@users.noreply.github.com>
2021-04-17 22:44:09 +0530

    Formatting fix

--- spring-boot-project/spring-boot/src/main/java/org/springframework/boot/context/config/ConfigDataLocationResolvers.java
+++ spring-boot-project/spring-boot/src/main/java/org/springframework/boot/context/config/ConfigDataLocationResolvers.java
@@ -54,7 +54,8 @@ class ConfigDataLocationResolvers {
 	ConfigDataLocationResolvers(DeferredLogFactory logFactory, ConfigurableBootstrapContext bootstrapContext,
 			Binder binder, ResourceLoader resourceLoader) {
 		this(logFactory, bootstrapContext, binder, resourceLoader,
-				SpringFactoriesLoader.loadFactoryNames(ConfigDataLocationResolver.class, ConfigDataLocationResolver.class.getClassLoader()));
+				SpringFactoriesLoader.loadFactoryNames(ConfigDataLocationResolver.class, 
+													   ConfigDataLocationResolver.class.getClassLoader()));
 	}
 
 	/**
