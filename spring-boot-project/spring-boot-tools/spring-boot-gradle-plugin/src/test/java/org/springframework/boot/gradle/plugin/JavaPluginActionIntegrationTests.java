b3275c5647d60bdbb7a1f1db702cf45ad15bf0bb
Andy Wilkinson <wilkinsona@vmware.com>
2021-05-05 08:27:35 +0100

    Align productionRuntimeClasspath with runtimeClasspath
    Fixes gh-25798

--- spring-boot-project/spring-boot-tools/spring-boot-gradle-plugin/src/test/java/org/springframework/boot/gradle/plugin/JavaPluginActionIntegrationTests.java
+++ spring-boot-project/spring-boot-tools/spring-boot-gradle-plugin/src/test/java/org/springframework/boot/gradle/plugin/JavaPluginActionIntegrationTests.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2012-2020 the original author or authors.
+ * Copyright 2012-2021 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -153,6 +153,18 @@ class JavaPluginActionIntegrationTests {
 				.getOutput()).contains("3 productionRuntimeClasspath attributes:")
 						.contains("org.gradle.usage: java-runtime").contains("org.gradle.libraryelements: jar")
 						.contains("org.gradle.dependency.bundling: external");
+	}
+
+	@TestTemplate
+	void productionRuntimeClasspathIsConfiguredWithResolvabilityAndConsumabilityThatMatchesRuntimeClasspath() {
+		String productionRuntime = this.gradleBuild.build("configurationResolvabilityAndConsumability",
+				"-PconfigurationName=runtimeClasspath", "-PapplyJavaPlugin").getOutput();
+		assertThat(productionRuntime).contains("canBeResolved: true");
+		assertThat(productionRuntime).contains("canBeConsumed: false");
+		String runtime = this.gradleBuild.build("configurationResolvabilityAndConsumability",
+				"-PconfigurationName=productionRuntimeClasspath", "-PapplyJavaPlugin").getOutput();
+		assertThat(runtime).contains("canBeResolved: true");
+		assertThat(runtime).contains("canBeConsumed: false");
 	}
 
 	private void createMinimalMainSource() throws IOException {
