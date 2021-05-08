507a8382d1a1adeebe70d396321f72503ba4b2cb
nguyensach <sachnbbkhn@gmail.com>
2021-05-09 01:19:57 +0900

    Use  try-with-resources statement

--- spring-boot-tests/spring-boot-smoke-tests/spring-boot-smoke-test-jta-atomikos/src/main/java/smoketest/atomikos/SampleAtomikosApplication.java
+++ spring-boot-tests/spring-boot-smoke-tests/spring-boot-smoke-test-jta-atomikos/src/main/java/smoketest/atomikos/SampleAtomikosApplication.java
@@ -16,30 +16,28 @@
 
 package smoketest.atomikos;
 
-import java.io.Closeable;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ApplicationContext;
+import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootApplication
 public class SampleAtomikosApplication {
 
 	public static void main(String[] args) throws Exception {
-		ApplicationContext context = SpringApplication.run(SampleAtomikosApplication.class, args);
-		AccountService service = context.getBean(AccountService.class);
-		AccountRepository repository = context.getBean(AccountRepository.class);
-		service.createAccountAndNotify("josh");
-		System.out.println("Count is " + repository.count());
-		try {
-			service.createAccountAndNotify("error");
+		try (ConfigurableApplicationContext context = SpringApplication.run(SampleAtomikosApplication.class, args)) {
+			AccountService service = context.getBean(AccountService.class);
+			AccountRepository repository = context.getBean(AccountRepository.class);
+			service.createAccountAndNotify("josh");
+			System.out.println("Count is " + repository.count());
+			try {
+				service.createAccountAndNotify("error");
+			}
+			catch (Exception ex) {
+				System.out.println(ex.getMessage());
+			}
+			System.out.println("Count is " + repository.count());
+			Thread.sleep(100);
 		}
-		catch (Exception ex) {
-			System.out.println(ex.getMessage());
-		}
-		System.out.println("Count is " + repository.count());
-		Thread.sleep(100);
-		((Closeable) context).close();
 	}
 
 }
