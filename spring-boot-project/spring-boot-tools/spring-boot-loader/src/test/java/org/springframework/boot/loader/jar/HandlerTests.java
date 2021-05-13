4546d1f33246fc888fd7e6bc516945c3b746e404
weixsun <weixsun@aliyun.com>
2021-05-13 17:06:49 +0800

    Use try-with-resources statement

--- spring-boot-project/spring-boot-tools/spring-boot-loader/src/test/java/org/springframework/boot/loader/jar/HandlerTests.java
+++ spring-boot-project/spring-boot-tools/spring-boot-loader/src/test/java/org/springframework/boot/loader/jar/HandlerTests.java
@@ -172,12 +172,8 @@ class HandlerTests {
 		TestJarCreator.createTestJar(testJar);
 		URL url = new URL(null, "jar:" + testJar.toURI().toURL() + "!/nested.jar!/3.dat", this.handler);
 		JarURLConnection connection = (JarURLConnection) url.openConnection();
-		JarFile jarFile = JarFileWrapper.unwrap(connection.getJarFile());
-		try {
+		try (JarFile jarFile = JarFileWrapper.unwrap(connection.getJarFile())) {
 			assertThat(jarFile.getRootJarFile().getFile()).isEqualTo(testJar);
-		}
-		finally {
-			jarFile.close();
 		}
 	}
 
@@ -187,12 +183,8 @@ class HandlerTests {
 		TestJarCreator.createTestJar(testJar);
 		URL url = new URL(null, "jar:" + testJar.toURI().toURL() + "!/nested.jar!/3.dat", this.handler);
 		JarURLConnection connection = (JarURLConnection) url.openConnection();
-		JarFile jarFile = JarFileWrapper.unwrap(connection.getJarFile());
-		try {
+		try (JarFile jarFile = JarFileWrapper.unwrap(connection.getJarFile())) {
 			assertThat(jarFile.getRootJarFile().getFile()).isEqualTo(testJar);
-		}
-		finally {
-			jarFile.close();
 		}
 	}
 
