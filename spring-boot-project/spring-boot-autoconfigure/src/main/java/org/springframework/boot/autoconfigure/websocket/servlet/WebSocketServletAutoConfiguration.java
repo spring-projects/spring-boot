bc7004d9c6646599cba16c6d3d53871dab65eda9
Andy Wilkinson <wilkinsona@vmware.com>
2021-06-15 16:59:33 +0100

    Fix WebSocket support with Jetty 10.0.x
    Fixes gh-26847

--- spring-boot-project/spring-boot-autoconfigure/src/main/java/org/springframework/boot/autoconfigure/websocket/servlet/WebSocketServletAutoConfiguration.java
+++ spring-boot-project/spring-boot-autoconfigure/src/main/java/org/springframework/boot/autoconfigure/websocket/servlet/WebSocketServletAutoConfiguration.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2012-2019 the original author or authors.
+ * Copyright 2012-2021 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -79,6 +79,19 @@ public class WebSocketServletAutoConfiguration {
 		@ConditionalOnMissingBean(name = "websocketServletWebServerCustomizer")
 		JettyWebSocketServletWebServerCustomizer websocketServletWebServerCustomizer() {
 			return new JettyWebSocketServletWebServerCustomizer();
+		}
+
+	}
+
+	@Configuration(proxyBeanMethods = false)
+	@ConditionalOnClass(name = { Jetty10WebSocketServletWebServerCustomizer.JAVAX_WEB_SOCKET_SERVER_CONTAINER,
+			Jetty10WebSocketServletWebServerCustomizer.JETTY_WEB_SOCKET_SERVER_CONTAINER })
+	static class Jetty10WebSocketConfiguration {
+
+		@Bean
+		@ConditionalOnMissingBean(name = "websocketServletWebServerCustomizer")
+		Jetty10WebSocketServletWebServerCustomizer websocketServletWebServerCustomizer() {
+			return new Jetty10WebSocketServletWebServerCustomizer();
 		}
 
 	}
