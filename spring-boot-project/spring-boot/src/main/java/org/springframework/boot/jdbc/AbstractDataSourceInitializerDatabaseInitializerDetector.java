94f94f30f531f3c8ceab6c9953b263cc9ced0c7c
Andy Wilkinson <wilkinsona@vmware.com>
2021-07-09 11:41:33 +0100

    Polish "Detect AbstractDataSourceInitializers as DB initializers"
    See gh-27215

--- spring-boot-project/spring-boot/src/main/java/org/springframework/boot/jdbc/AbstractDataSourceInitializerDatabaseInitializerDetector.java
+++ spring-boot-project/spring-boot/src/main/java/org/springframework/boot/jdbc/AbstractDataSourceInitializerDatabaseInitializerDetector.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2021 the original author or authors.
+ * Copyright 2012-2021 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -32,11 +32,6 @@ class AbstractDataSourceInitializerDatabaseInitializerDetector extends AbstractB
 	@Override
 	protected Set<Class<?>> getDatabaseInitializerBeanTypes() {
 		return Collections.singleton(AbstractDataSourceInitializer.class);
-	}
-
-	@Override
-	public int getOrder() {
-		return 1;
 	}
 
 }
