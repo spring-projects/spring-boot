4d1a53d9a89a9b077d313d2b061e9f16f7396b79
dreis2211 <christoph.dreis@freenet.de>
2021-08-01 16:55:24 +0200

    Switch to Adoptium JDK builds in CI
    See gh-27539

--- ci/scripts/detect-jdk-updates.sh
+++ ci/scripts/detect-jdk-updates.sh
@@ -9,11 +9,11 @@ trap 'report_error $? $LINENO' ERR
 
 case "$JDK_VERSION" in
 	java8)
-		 BASE_URL="https://api.adoptopenjdk.net/v3/assets/feature_releases/8/ga"
+		 BASE_URL="https://api.adoptium.net/v3/assets/feature_releases/8/ga"
 		 ISSUE_TITLE="Upgrade Java 8 version in CI image"
 	;;
 	java11)
-		 BASE_URL="https://api.adoptopenjdk.net/v3/assets/feature_releases/11/ga"
+		 BASE_URL="https://api.adoptium.net/v3/assets/feature_releases/11/ga"
 		 ISSUE_TITLE="Upgrade Java 11 version in CI image"
 	;;
 	java15)
@@ -25,7 +25,7 @@ case "$JDK_VERSION" in
 		exit 1;
 esac
 
-response=$( curl -s ${BASE_URL}\?architecture\=x64\&heap_size\=normal\&image_type\=jdk\&jvm_impl\=hotspot\&os\=linux\&sort_order\=DESC\&vendor\=adoptopenjdk )
+response=$( curl -s ${BASE_URL}\?architecture\=x64\&heap_size\=normal\&image_type\=jdk\&jvm_impl\=hotspot\&os\=linux\&sort_order\=DESC\&vendor\=adoptium )
 latest=$( jq -r '.[0].binaries[0].package.link' <<< "$response" )
 if [[ ${latest} = "null" || ${latest} = "" ]]; then
 	echo "Could not parse JDK response: $response"
