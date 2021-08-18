ffbd28b60a90e8a8108fa191d6ea0a006eeea3cb
Andy Wilkinson <wilkinsona@vmware.com>
2021-08-18 13:59:42 +0100

    Polish "Polish access modifiers for test classes"
    See gh-27736

--- spring-boot-project/spring-boot/src/test/java/org/springframework/boot/web/servlet/NoSpringWebFilterRegistrationBeanTests.java
+++ spring-boot-project/spring-boot/src/test/java/org/springframework/boot/web/servlet/NoSpringWebFilterRegistrationBeanTests.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2012-2020 the original author or authors.
+ * Copyright 2012-2021 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -30,7 +30,7 @@ import static org.mockito.ArgumentMatchers.eq;
  * @author Andy Wilkinson
  */
 @ClassPathExclusions("spring-web-*.jar")
-public class NoSpringWebFilterRegistrationBeanTests extends AbstractFilterRegistrationBeanTests {
+class NoSpringWebFilterRegistrationBeanTests extends AbstractFilterRegistrationBeanTests {
 
 	private final MockFilter filter = new MockFilter();
 
