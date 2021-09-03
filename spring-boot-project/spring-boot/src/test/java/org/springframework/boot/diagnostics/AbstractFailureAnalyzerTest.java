98c2d2122f775f503f840b2fbb3e952f0a46fe13
smoothbear <kjbin0420@naver.com>
2021-09-03 21:14:11 +0900

    Fix code lint

--- spring-boot-project/spring-boot/src/test/java/org/springframework/boot/diagnostics/AbstractFailureAnalyzerTest.java
+++ spring-boot-project/spring-boot/src/test/java/org/springframework/boot/diagnostics/AbstractFailureAnalyzerTest.java
@@ -1,9 +1,25 @@
+/*
+ * Copyright 2012-2020 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package org.springframework.boot.diagnostics;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.assertj.core.api.Assertions.assertThat;
 
 class AbstractFailureAnalyzerTest {
 
@@ -11,30 +27,30 @@ class AbstractFailureAnalyzerTest {
 
 	@BeforeEach
 	void configureFailureAnalyzer() {
-		failureAnalyzerConcrete = new FailureAnalyzerConcrete();
+		this.failureAnalyzerConcrete = new FailureAnalyzerConcrete();
 	}
 
 	@Test
 	void findCauseExtendsThrowable() {
 		ThrowableExtendsException ex = new ThrowableExtendsException();
-		assertNotNull(failureAnalyzerConcrete.findCause(ex, Throwable.class).getClass());
+		assertThat(this.failureAnalyzerConcrete.findCause(ex, Throwable.class).getClass()).isNotNull();
 	}
 
 	@Test
 	void findCauseExtendsOtherException() {
 		ExtendsThrowableExtendsException ex = new ExtendsThrowableExtendsException();
-		assertNotNull(failureAnalyzerConcrete.findCause(ex, ThrowableExtendsException.class).getClass());
+		assertThat(this.failureAnalyzerConcrete.findCause(ex, ThrowableExtendsException.class).getClass()).isNotNull();
 	}
 
 	@Test
 	void findCauseOtherException() {
 		ThrowableExtendsException ex = new ThrowableExtendsException();
-		assertNull(failureAnalyzerConcrete.findCause(ex, OtherException.class));
+		assertThat(this.failureAnalyzerConcrete.findCause(ex, OtherException.class)).isNull();
 	}
 
 	@Test
 	void findCauseNullObject() {
-		assertNull(failureAnalyzerConcrete.findCause(null, Throwable.class));
+		assertThat(this.failureAnalyzerConcrete.findCause(null, Throwable.class)).isNull();
 	}
 
 	static class FailureAnalyzerConcrete extends AbstractFailureAnalyzer<Throwable> {
