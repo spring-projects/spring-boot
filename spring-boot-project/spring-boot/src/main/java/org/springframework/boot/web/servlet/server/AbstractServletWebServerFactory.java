26eb577af781725e2c0db46777a13003e10dc003
leeseojune <sung07288346@gmail.com>
2021-09-04 01:50:03 +0900

    Fix abstract class's constructor access modifier

--- spring-boot-project/spring-boot/src/main/java/org/springframework/boot/web/servlet/server/AbstractServletWebServerFactory.java
+++ spring-boot-project/spring-boot/src/main/java/org/springframework/boot/web/servlet/server/AbstractServletWebServerFactory.java
@@ -55,6 +55,7 @@ import org.springframework.util.ClassUtils;
  * @author Ivan Sopov
  * @author Eddú Meléndez
  * @author Brian Clozel
+ * @author Lee SeoJune
  * @since 2.0.0
  */
 public abstract class AbstractServletWebServerFactory extends AbstractConfigurableWebServerFactory
@@ -89,7 +90,7 @@ public abstract class AbstractServletWebServerFactory extends AbstractConfigurab
 	/**
 	 * Create a new {@link AbstractServletWebServerFactory} instance.
 	 */
-	public AbstractServletWebServerFactory() {
+	protected AbstractServletWebServerFactory() {
 	}
 
 	/**
@@ -97,7 +98,7 @@ public abstract class AbstractServletWebServerFactory extends AbstractConfigurab
 	 * port.
 	 * @param port the port number for the web server
 	 */
-	public AbstractServletWebServerFactory(int port) {
+	protected AbstractServletWebServerFactory(int port) {
 		super(port);
 	}
 
@@ -107,7 +108,7 @@ public abstract class AbstractServletWebServerFactory extends AbstractConfigurab
 	 * @param contextPath the context path for the web server
 	 * @param port the port number for the web server
 	 */
-	public AbstractServletWebServerFactory(String contextPath, int port) {
+	protected AbstractServletWebServerFactory(String contextPath, int port) {
 		super(port);
 		checkContextPath(contextPath);
 		this.contextPath = contextPath;
