78d6c94a7e4fa0fe021d7240061ac0a6d4a07cc8
Stephane Nicoll <snicoll@vmware.com>
2021-09-10 08:20:10 +0200

    Polish "Auto-Configure Kafka CommonErrorHandler"
    See gh-27927

--- spring-boot-project/spring-boot-autoconfigure/src/main/java/org/springframework/boot/autoconfigure/kafka/ConcurrentKafkaListenerContainerFactoryConfigurer.java
+++ spring-boot-project/spring-boot-autoconfigure/src/main/java/org/springframework/boot/autoconfigure/kafka/ConcurrentKafkaListenerContainerFactoryConfigurer.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2012-2020 the original author or authors.
+ * Copyright 2012-2021 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -133,7 +133,7 @@ public class ConcurrentKafkaListenerContainerFactoryConfigurer {
 	/**
 	 * Set the {@link CommonErrorHandler} to use.
 	 * @param commonErrorHandler the error handler.
-	 * @since 2.6
+	 * @since 2.6.0
 	 */
 	public void setCommonErrorHandler(CommonErrorHandler commonErrorHandler) {
 		this.commonErrorHandler = commonErrorHandler;
