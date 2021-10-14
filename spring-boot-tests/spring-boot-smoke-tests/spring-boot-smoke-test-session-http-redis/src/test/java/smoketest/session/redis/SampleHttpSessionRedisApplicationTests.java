0bfc2d577c3bf7085b35a0c3d25c3af13154444e
Angel L. Villalain Garcia <avilla@localhost.localdomain>
2021-10-13 23:58:52 -0500

    Add smoke tests for Spring Session Redis/Mongo
    Add smoke tests that verify the correct behavior of the sessions
    endpoint when using Spring Session with MongoDB and Redis.
    
    See gh-28291

---
+++ spring-boot-tests/spring-boot-smoke-tests/spring-boot-smoke-test-session-http-redis/src/test/java/smoketest/session/redis/SampleHttpSessionRedisApplicationTests.java
@@ -0,0 +1,78 @@
+/*
+ * Copyright 2012-2019 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package smoketest.session.redis;
+
+import java.net.URI;
+import java.util.List;
+import java.util.Map;
+
+import org.junit.jupiter.api.Test;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.RequestEntity;
+import org.springframework.http.ResponseEntity;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT,
+		properties = { "spring.security.user.name=" + SampleHttpSessionRedisApplicationTests.USERNAME,
+				"spring.security.user.password=" + SampleHttpSessionRedisApplicationTests.PASSWORD,
+				"spring.redis.host=localhost", "spring.redis.port=6379" },
+		classes = TestRedisConfiguration.class)
+public class SampleHttpSessionRedisApplicationTests {
+
+	static final String USERNAME = "user";
+	static final String PASSWORD = "password";
+
+	@Autowired
+	private TestRestTemplate template;
+
+	@LocalServerPort
+	private int port;
+
+	@Test
+	@SuppressWarnings("unchecked")
+	void sessionsEndpointShouldReturnUserSessions() {
+		createSession();
+		ResponseEntity<Map<String, Object>> response = this.getSessions();
+		assertThat(response).isNotNull();
+		assertThat(response.getStatusCode()).isEqualTo(HttpStatus.OK);
+		List<Map<String, Object>> sessions = (List<Map<String, Object>>) response.getBody().get("sessions");
+		assertThat(sessions.size()).isEqualTo(1);
+	}
+
+	private void createSession() {
+		URI uri = URI.create("http://localhost:" + this.port + "/");
+		HttpHeaders headers = new HttpHeaders();
+		headers.setBasicAuth(USERNAME, PASSWORD);
+		RequestEntity<Object> request = new RequestEntity<>(headers, HttpMethod.GET, uri);
+		this.template.exchange(request, String.class);
+	}
+
+	@SuppressWarnings("unchecked")
+	private ResponseEntity<Map<String, Object>> getSessions() {
+		return (ResponseEntity<Map<String, Object>>) (ResponseEntity) this.template.withBasicAuth(USERNAME, PASSWORD)
+				.getForEntity("/actuator/sessions?username=" + USERNAME, Map.class);
+	}
+
+}
---
+++ spring-boot-tests/spring-boot-smoke-tests/spring-boot-smoke-test-session-http-redis/src/test/java/smoketest/session/redis/TestRedisConfiguration.java
@@ -0,0 +1,46 @@
+/*
+ * Copyright 2012-2019 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package smoketest.session.redis;
+
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+
+import redis.embedded.RedisServer;
+
+import org.springframework.boot.autoconfigure.data.redis.RedisProperties;
+import org.springframework.boot.test.context.TestConfiguration;
+
+@TestConfiguration
+public class TestRedisConfiguration {
+
+	private final RedisServer server;
+
+	public TestRedisConfiguration(final RedisProperties properties) {
+		this.server = new RedisServer(properties.getPort());
+	}
+
+	@PostConstruct
+	public void start() {
+		this.server.start();
+	}
+
+	@PreDestroy
+	public void stop() {
+		this.server.stop();
+	}
+
+}
