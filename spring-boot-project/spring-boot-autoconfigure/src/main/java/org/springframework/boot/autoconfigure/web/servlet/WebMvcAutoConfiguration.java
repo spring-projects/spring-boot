9d6a0cfd24b137ce18ad3f89be31f87eba2c6003
Andy Wilkinson <wilkinsona@vmware.com>
2021-10-19 18:43:53 +0100

    Starting building against Spring Framework 5.3.12 snapshots
    This reverts commit 3969e63ced8e349474c9de5ca2907583c1516d95, reversing
    changes made to f01d086a8d1ac5bee92d04c21b3c6c9785006aa9.
    
    Closes gh-28385
    See gh-28252

--- spring-boot-project/spring-boot-autoconfigure/src/main/java/org/springframework/boot/autoconfigure/web/servlet/WebMvcAutoConfiguration.java
+++ spring-boot-project/spring-boot-autoconfigure/src/main/java/org/springframework/boot/autoconfigure/web/servlet/WebMvcAutoConfiguration.java
@@ -204,15 +204,13 @@ public class WebMvcAutoConfiguration {
 
 		private final ResourceHandlerRegistrationCustomizer resourceHandlerRegistrationCustomizer;
 
-		private final ResourceLoader resourceLoader;
-
 		private ServletContext servletContext;
 
 		public WebMvcAutoConfigurationAdapter(WebProperties webProperties, WebMvcProperties mvcProperties,
 				ListableBeanFactory beanFactory, ObjectProvider<HttpMessageConverters> messageConvertersProvider,
 				ObjectProvider<ResourceHandlerRegistrationCustomizer> resourceHandlerRegistrationCustomizerProvider,
 				ObjectProvider<DispatcherServletPath> dispatcherServletPath,
-				ObjectProvider<ServletRegistrationBean<?>> servletRegistrations, ResourceLoader resourceLoader) {
+				ObjectProvider<ServletRegistrationBean<?>> servletRegistrations) {
 			this.resourceProperties = webProperties.getResources();
 			this.mvcProperties = mvcProperties;
 			this.beanFactory = beanFactory;
@@ -220,7 +218,6 @@ public class WebMvcAutoConfiguration {
 			this.resourceHandlerRegistrationCustomizer = resourceHandlerRegistrationCustomizerProvider.getIfAvailable();
 			this.dispatcherServletPath = dispatcherServletPath;
 			this.servletRegistrations = servletRegistrations;
-			this.resourceLoader = resourceLoader;
 			this.mvcProperties.checkConfiguration();
 		}
 
@@ -337,11 +334,7 @@ public class WebMvcAutoConfiguration {
 				logger.debug("Default resource handling disabled");
 				return;
 			}
-			Resource webjarsLocationResource = this.resourceLoader
-					.getResource("classpath:/META-INF/resources/webjars/");
-			if (webjarsLocationResource.exists()) {
-				addResourceHandler(registry, "/webjars/**", webjarsLocationResource);
-			}
+			addResourceHandler(registry, "/webjars/**", "classpath:/META-INF/resources/webjars/");
 			addResourceHandler(registry, this.mvcProperties.getStaticPathPattern(), (registration) -> {
 				registration.addResourceLocations(this.resourceProperties.getStaticLocations());
 				if (this.servletContext != null) {
@@ -351,7 +344,7 @@ public class WebMvcAutoConfiguration {
 			});
 		}
 
-		private void addResourceHandler(ResourceHandlerRegistry registry, String pattern, Resource... locations) {
+		private void addResourceHandler(ResourceHandlerRegistry registry, String pattern, String... locations) {
 			addResourceHandler(registry, pattern, (registration) -> registration.addResourceLocations(locations));
 		}
 
