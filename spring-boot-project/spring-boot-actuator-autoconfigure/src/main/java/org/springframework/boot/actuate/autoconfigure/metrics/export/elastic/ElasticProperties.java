537cba3ef4153c29a467a1d6919ffb4aba35c246
Jonatan Ivanov <jonatan.ivanov@gmail.com>
2021-10-20 12:47:36 -0700

    Expose Elastic's apiKeyCredentials property

--- spring-boot-project/spring-boot-actuator-autoconfigure/src/main/java/org/springframework/boot/actuate/autoconfigure/metrics/export/elastic/ElasticProperties.java
+++ spring-boot-project/spring-boot-actuator-autoconfigure/src/main/java/org/springframework/boot/actuate/autoconfigure/metrics/export/elastic/ElasticProperties.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2012-2020 the original author or authors.
+ * Copyright 2012-2021 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -60,12 +60,14 @@ public class ElasticProperties extends StepRegistryProperties {
 	private boolean autoCreateIndex = true;
 
 	/**
-	 * Login user of the Elastic server.
+	 * Login user of the Elastic server. If API key is configured, it will be used for
+	 * authentication instead of username/password.
 	 */
 	private String userName;
 
 	/**
-	 * Login password of the Elastic server.
+	 * Login password of the Elastic server. If API key is configured, it will be used for
+	 * authentication instead of username/password.
 	 */
 	private String password;
 
@@ -73,6 +75,12 @@ public class ElasticProperties extends StepRegistryProperties {
 	 * Ingest pipeline name. By default, events are not pre-processed.
 	 */
 	private String pipeline;
+
+	/**
+	 * Base64-encoded credentials string. If configured, it will be used for
+	 * authentication instead of username/password.
+	 */
+	private String apiKeyCredentials;
 
 	public String getHost() {
 		return this.host;
@@ -144,6 +152,14 @@ public class ElasticProperties extends StepRegistryProperties {
 
 	public void setPipeline(String pipeline) {
 		this.pipeline = pipeline;
+	}
+
+	public String getApiKeyCredentials() {
+		return this.apiKeyCredentials;
+	}
+
+	public void setApiKeyCredentials(String apiKeyCredentials) {
+		this.apiKeyCredentials = apiKeyCredentials;
 	}
 
 }
