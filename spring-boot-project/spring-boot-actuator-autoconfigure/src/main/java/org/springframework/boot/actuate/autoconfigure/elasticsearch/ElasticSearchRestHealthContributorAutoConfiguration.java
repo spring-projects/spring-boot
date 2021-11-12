5db6e6f821b58a2be0465b17aa72295ef4a9e016
Filip Hrisafov <filip.hrisafov@gmail.com>
2021-11-12 15:26:30 +0100

    Expose RestClientBuilder when RestHighLevelClient is not available
    This commits exposes the RestClientBuilder as a bean even when the RestHighLevelClient is not available
    It allows users to create their own RestClient beans using the Spring Boot configured RestClientBuilder
    when they are not using the RestHighLevelClient.
    
    In addition to that a new ElasticsearchRestClientHealthIndicator decoupled from the RestHighLevelClient has been added
    and is used as an elasticsearch health contributor when there are no RestHighLevelClient beans.

--- spring-boot-project/spring-boot-actuator-autoconfigure/src/main/java/org/springframework/boot/actuate/autoconfigure/elasticsearch/ElasticSearchRestHealthContributorAutoConfiguration.java
+++ spring-boot-project/spring-boot-actuator-autoconfigure/src/main/java/org/springframework/boot/actuate/autoconfigure/elasticsearch/ElasticSearchRestHealthContributorAutoConfiguration.java
@@ -16,43 +16,31 @@
 
 package org.springframework.boot.actuate.autoconfigure.elasticsearch;
 
-import java.util.Map;
-
 import org.elasticsearch.client.RestClient;
-import org.elasticsearch.client.RestHighLevelClient;
 
-import org.springframework.boot.actuate.autoconfigure.health.CompositeHealthContributorConfiguration;
+import org.springframework.boot.actuate.autoconfigure.elasticsearch.ElasticSearchRestHealthContributorConfigurations.RestClientHealthContributorConfiguration;
 import org.springframework.boot.actuate.autoconfigure.health.ConditionalOnEnabledHealthIndicator;
-import org.springframework.boot.actuate.elasticsearch.ElasticsearchRestHealthIndicator;
-import org.springframework.boot.actuate.health.HealthContributor;
 import org.springframework.boot.autoconfigure.AutoConfigureAfter;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-import org.springframework.boot.autoconfigure.condition.ConditionalOnBean;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
-import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.boot.autoconfigure.elasticsearch.ElasticsearchRestClientAutoConfiguration;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
 
 /**
- * {@link EnableAutoConfiguration Auto-configuration} for
- * {@link ElasticsearchRestHealthIndicator} using the {@link RestClient}.
+ * {@link EnableAutoConfiguration Auto-configuration} for for Elasticsearch health
+ * incidator using REST clients.
  *
  * @author Artsiom Yudovin
  * @since 2.1.1
  */
+@SuppressWarnings("deprecation")
 @Configuration(proxyBeanMethods = false)
-@ConditionalOnClass(RestHighLevelClient.class)
-@ConditionalOnBean(RestHighLevelClient.class)
+@ConditionalOnClass(RestClient.class)
 @ConditionalOnEnabledHealthIndicator("elasticsearch")
 @AutoConfigureAfter(ElasticsearchRestClientAutoConfiguration.class)
-public class ElasticSearchRestHealthContributorAutoConfiguration
-		extends CompositeHealthContributorConfiguration<ElasticsearchRestHealthIndicator, RestHighLevelClient> {
-
-	@Bean
-	@ConditionalOnMissingBean(name = { "elasticsearchHealthIndicator", "elasticsearchHealthContributor" })
-	public HealthContributor elasticsearchHealthContributor(Map<String, RestHighLevelClient> clients) {
-		return createContributor(clients);
-	}
+@Import({ ElasticSearchRestHealthContributorConfigurations.RestHighLevelClientHealthContributorConfiguration.class,
+		RestClientHealthContributorConfiguration.class })
+public class ElasticSearchRestHealthContributorAutoConfiguration {
 
 }
--- spring-boot-project/spring-boot-actuator/src/main/java/org/springframework/boot/actuate/elasticsearch/ElasticsearchRestHealthIndicator.java
+++ spring-boot-project/spring-boot-actuator/src/main/java/org/springframework/boot/actuate/elasticsearch/ElasticsearchRestHealthIndicator.java
@@ -16,75 +16,31 @@
 
 package org.springframework.boot.actuate.elasticsearch;
 
-import java.io.InputStream;
-import java.nio.charset.StandardCharsets;
-import java.util.Map;
-
-import org.apache.http.HttpStatus;
-import org.apache.http.StatusLine;
-import org.elasticsearch.client.Request;
-import org.elasticsearch.client.Response;
 import org.elasticsearch.client.RestClient;
 import org.elasticsearch.client.RestHighLevelClient;
 
-import org.springframework.boot.actuate.health.AbstractHealthIndicator;
-import org.springframework.boot.actuate.health.Health;
 import org.springframework.boot.actuate.health.HealthIndicator;
-import org.springframework.boot.json.JsonParser;
-import org.springframework.boot.json.JsonParserFactory;
-import org.springframework.util.StreamUtils;
 
 /**
- * {@link HealthIndicator} for an Elasticsearch cluster using a {@link RestClient}.
+ * {@link HealthIndicator} for an Elasticsearch cluster using a {@link RestClient} or a
+ * {@link RestHighLevelClient}.
  *
  * @author Artsiom Yudovin
  * @author Brian Clozel
  * @author Filip Hrisafov
  * @since 2.1.1
+ * @deprecated since 2.6.0 for removal in 2.8.0 in favor of
+ * {@link ElasticsearchRestClientHealthIndicator}
  */
-public class ElasticsearchRestHealthIndicator extends AbstractHealthIndicator {
-
-	private static final String RED_STATUS = "red";
-
-	private final RestClient client;
-
-	private final JsonParser jsonParser;
+@Deprecated
+public class ElasticsearchRestHealthIndicator extends ElasticsearchRestClientHealthIndicator {
 
 	public ElasticsearchRestHealthIndicator(RestHighLevelClient client) {
 		this(client.getLowLevelClient());
 	}
 
 	public ElasticsearchRestHealthIndicator(RestClient client) {
-		super("Elasticsearch health check failed");
-		this.client = client;
-		this.jsonParser = JsonParserFactory.getJsonParser();
-	}
-
-	@Override
-	protected void doHealthCheck(Health.Builder builder) throws Exception {
-		Response response = this.client.performRequest(new Request("GET", "/_cluster/health/"));
-		StatusLine statusLine = response.getStatusLine();
-		if (statusLine.getStatusCode() != HttpStatus.SC_OK) {
-			builder.down();
-			builder.withDetail("statusCode", statusLine.getStatusCode());
-			builder.withDetail("reasonPhrase", statusLine.getReasonPhrase());
-			return;
-		}
-		try (InputStream inputStream = response.getEntity().getContent()) {
-			doHealthCheck(builder, StreamUtils.copyToString(inputStream, StandardCharsets.UTF_8));
-		}
-	}
-
-	private void doHealthCheck(Health.Builder builder, String json) {
-		Map<String, Object> response = this.jsonParser.parseMap(json);
-		String status = (String) response.get("status");
-		if (RED_STATUS.equals(status)) {
-			builder.outOfService();
-		}
-		else {
-			builder.up();
-		}
-		builder.withDetails(response);
+		super(client);
 	}
 
 }
