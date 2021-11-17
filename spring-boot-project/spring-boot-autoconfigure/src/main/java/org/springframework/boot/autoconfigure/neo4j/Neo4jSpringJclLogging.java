7fbb9d471b51ea2027d67436f9e313e582c3ba5e
Stephane Nicoll <snicoll@vmware.com>
2021-11-17 16:14:25 +0100

    Polish "Upgrade to Neo4j Java Driver 4.2.8"
    See gh-28717

--- spring-boot-project/spring-boot-autoconfigure/src/main/java/org/springframework/boot/autoconfigure/neo4j/Neo4jSpringJclLogging.java
+++ spring-boot-project/spring-boot-autoconfigure/src/main/java/org/springframework/boot/autoconfigure/neo4j/Neo4jSpringJclLogging.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2012-2020 the original author or authors.
+ * Copyright 2012-2021 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -77,6 +77,13 @@ class Neo4jSpringJclLogging implements Logging {
 		public void debug(String format, Object... params) {
 			if (isDebugEnabled()) {
 				this.delegate.debug(String.format(format, params));
+			}
+		}
+
+		@Override
+		public void debug(String message, Throwable throwable) {
+			if (isDebugEnabled()) {
+				this.delegate.debug(message, throwable);
 			}
 		}
 
