f2b959b0a5fce7b904ac7d39f640d0ed5a01e3be
Andy Wilkinson <wilkinsona@vmware.com>
2021-11-18 18:29:27 +0000

    Add <type>zip</type> to Elasticsearch dist in dependencies bom
    Fixes gh-28725

--- buildSrc/src/main/java/org/springframework/boot/build/bom/BomExtension.java
+++ buildSrc/src/main/java/org/springframework/boot/build/bom/BomExtension.java
@@ -301,20 +301,26 @@ public class BomExtension {
 				if (args instanceof Object[] && ((Object[]) args).length == 1) {
 					Object arg = ((Object[]) args)[0];
 					if (arg instanceof Closure) {
-						ExclusionHandler exclusionHandler = new ExclusionHandler();
-						ConfigureUtil.configure((Closure<?>) arg, exclusionHandler);
-						return new Module(name, exclusionHandler.exclusions);
+						ModuleHandler moduleHandler = new ModuleHandler();
+						ConfigureUtil.configure((Closure<?>) arg, moduleHandler);
+						return new Module(name, moduleHandler.type, moduleHandler.exclusions);
 					}
 				}
-				throw new InvalidUserDataException("Invalid exclusion configuration for module '" + name + "'");
+				throw new InvalidUserDataException("Invalid configuration for module '" + name + "'");
 			}
 
-			public class ExclusionHandler {
+			public class ModuleHandler {
 
 				private final List<Exclusion> exclusions = new ArrayList<>();
 
+				private String type;
+
 				public void exclude(Map<String, String> exclusion) {
 					this.exclusions.add(new Exclusion(exclusion.get("group"), exclusion.get("module")));
+				}
+
+				public void setType(String type) {
+					this.type = type;
 				}
 
 			}
