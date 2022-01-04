d24720d9581fc69dad74e82c3f2ff0afce5a328d
Stephane Nicoll <snicoll@vmware.com>
2022-01-04 14:06:36 +0100

    Polish "Expose OS information as an InfoContributor"
    See gh-28907

--- spring-boot-project/spring-boot-actuator/src/test/java/org/springframework/boot/actuate/info/OsInfoContributorTests.java
+++ spring-boot-project/spring-boot-actuator/src/test/java/org/springframework/boot/actuate/info/OsInfoContributorTests.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2012-2021 the original author or authors.
+ * Copyright 2012-2022 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -18,7 +18,6 @@ package org.springframework.boot.actuate.info;
 
 import org.junit.jupiter.api.Test;
 
-import org.springframework.boot.info.JavaInfo;
 import org.springframework.boot.info.OsInfo;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -28,7 +27,7 @@ import static org.assertj.core.api.Assertions.assertThat;
  *
  * @author Jonatan Ivanov
  */
-public class OsInfoContributorTests {
+class OsInfoContributorTests {
 
 	@Test
 	void osInfoShouldBeAdded() {
