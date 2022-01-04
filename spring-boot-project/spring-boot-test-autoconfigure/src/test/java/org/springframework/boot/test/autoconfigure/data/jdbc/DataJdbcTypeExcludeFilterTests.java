9b34c31916ab3d63ad581b44723054b7e12f54f3
Stephane Nicoll <snicoll@vmware.com>
2022-01-04 15:32:23 +0100

    Polish "Include AbstractJdbcConfiguration beans in @DataJdbcTest"
    See gh-29003

--- spring-boot-project/spring-boot-test-autoconfigure/src/test/java/org/springframework/boot/test/autoconfigure/data/jdbc/DataJdbcTypeExcludeFilterTests.java
+++ spring-boot-project/spring-boot-test-autoconfigure/src/test/java/org/springframework/boot/test/autoconfigure/data/jdbc/DataJdbcTypeExcludeFilterTests.java
@@ -16,13 +16,14 @@
 
 package org.springframework.boot.test.autoconfigure.data.jdbc;
 
+import java.io.IOException;
+
 import org.junit.jupiter.api.Test;
+
 import org.springframework.core.type.classreading.MetadataReader;
 import org.springframework.core.type.classreading.MetadataReaderFactory;
 import org.springframework.core.type.classreading.SimpleMetadataReaderFactory;
 import org.springframework.data.jdbc.repository.config.AbstractJdbcConfiguration;
-
-import java.io.IOException;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -30,21 +31,22 @@ import static org.assertj.core.api.Assertions.assertThat;
  * Tests for {@link DataJdbcTypeExcludeFilter}.
  *
  * @author Ravi Undupitiya
+ * @author Stephane Nicoll
  */
-public class DataJdbcTypeExcludeFilterTests {
+class DataJdbcTypeExcludeFilterTests {
 
-	private MetadataReaderFactory metadataReaderFactory = new SimpleMetadataReaderFactory();
-
-	@Test
-	void matchNotUsingDefaultFilters() throws Exception {
-		DataJdbcTypeExcludeFilter filter = new DataJdbcTypeExcludeFilter(NotUsingDefaultFilters.class);
-		assertThat(excludes(filter, AbstractJdbcConfiguration.class)).isTrue();
-	}
+	private final MetadataReaderFactory metadataReaderFactory = new SimpleMetadataReaderFactory();
 
 	@Test
 	void matchUsingDefaultFilters() throws Exception {
 		DataJdbcTypeExcludeFilter filter = new DataJdbcTypeExcludeFilter(UsingDefaultFilters.class);
-		assertThat(excludes(filter, AbstractJdbcConfiguration.class)).isFalse();
+		assertThat(excludes(filter, TestJdbcConfiguration.class)).isFalse();
+	}
+
+	@Test
+	void matchNotUsingDefaultFilters() throws Exception {
+		DataJdbcTypeExcludeFilter filter = new DataJdbcTypeExcludeFilter(NotUsingDefaultFilters.class);
+		assertThat(excludes(filter, TestJdbcConfiguration.class)).isTrue();
 	}
 
 	private boolean excludes(DataJdbcTypeExcludeFilter filter, Class<?> type) throws IOException {
@@ -59,6 +61,10 @@ public class DataJdbcTypeExcludeFilterTests {
 
 	@DataJdbcTest(useDefaultFilters = false)
 	static class NotUsingDefaultFilters {
+
+	}
+
+	static class TestJdbcConfiguration extends AbstractJdbcConfiguration {
 
 	}
 
