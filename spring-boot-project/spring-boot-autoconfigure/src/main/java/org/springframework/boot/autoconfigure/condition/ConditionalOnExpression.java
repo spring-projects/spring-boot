2e075b4f8eb0d8fbdeeef70cc8f2915cc2303f7e
Mihail Cornescu <cornescu_mihail@yahoo.com>
2022-01-05 18:03:29 +0000

    Warning added in @ConditionalOnExpression value property javadoc

--- spring-boot-project/spring-boot-autoconfigure/src/main/java/org/springframework/boot/autoconfigure/condition/ConditionalOnExpression.java
+++ spring-boot-project/spring-boot-autoconfigure/src/main/java/org/springframework/boot/autoconfigure/condition/ConditionalOnExpression.java
@@ -40,6 +40,9 @@ public @interface ConditionalOnExpression {
 	/**
 	 * The SpEL expression to evaluate. Expression should return {@code true} if the
 	 * condition passes or {@code false} if it fails.
+	 * WARNING: Referencing a bean will cause the bean to be initialized very early in
+	 *          context refresh processing. As a result, this will cause the bean
+	 *          properties not to be loaded accordingly.
 	 * @return the SpEL expression
 	 */
 	String value() default "true";
