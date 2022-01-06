69d1c3674aa32056dacdbc70dccc99b6364f3e73
Stephane Nicoll <snicoll@vmware.com>
2022-01-06 17:40:13 +0100

    Upgrade to latest Hazelcast 3.x in integration tests

--- spring-boot-project/spring-boot-autoconfigure/src/test/java/org/springframework/boot/autoconfigure/session/SessionAutoConfigurationHazelcast3Tests.java
+++ spring-boot-project/spring-boot-autoconfigure/src/test/java/org/springframework/boot/autoconfigure/session/SessionAutoConfigurationHazelcast3Tests.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2012-2020 the original author or authors.
+ * Copyright 2012-2022 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -40,7 +40,7 @@ import static org.assertj.core.api.Assertions.assertThat;
  * @author Vedran Pavic
  */
 @ClassPathExclusions("hazelcast*.jar")
-@ClassPathOverrides("com.hazelcast:hazelcast:3.12.8")
+@ClassPathOverrides("com.hazelcast:hazelcast:3.12.12")
 class SessionAutoConfigurationHazelcast3Tests extends AbstractSessionAutoConfigurationTests {
 
 	private final WebApplicationContextRunner contextRunner = new WebApplicationContextRunner()
