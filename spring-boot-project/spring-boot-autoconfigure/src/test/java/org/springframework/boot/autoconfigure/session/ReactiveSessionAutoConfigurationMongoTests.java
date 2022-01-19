d211b0f3b3a98c58e947982cc253a91c981849e9
Phillip Webb <pwebb@vmware.com>
2022-01-19 15:53:07 -0800

    Disable test is Docker is not available
    See gh-28843

--- spring-boot-project/spring-boot-autoconfigure/src/test/java/org/springframework/boot/autoconfigure/session/ReactiveSessionAutoConfigurationMongoTests.java
+++ spring-boot-project/spring-boot-autoconfigure/src/test/java/org/springframework/boot/autoconfigure/session/ReactiveSessionAutoConfigurationMongoTests.java
@@ -43,7 +43,7 @@ import static org.assertj.core.api.Assertions.assertThat;
  *
  * @author Andy Wilkinson
  */
-@Testcontainers
+@Testcontainers(disabledWithoutDocker = true)
 class ReactiveSessionAutoConfigurationMongoTests extends AbstractSessionAutoConfigurationTests {
 
 	@Container
