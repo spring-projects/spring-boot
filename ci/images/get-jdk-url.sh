faf47942226031475e2b2d4216d391ff4eb7a61b
Stephane Nicoll <snicoll@vmware.com>
2022-01-27 09:49:41 +0100

    Upgrade Java 8 version in CI image
    Closes gh-29578

--- ci/images/get-jdk-url.sh
+++ ci/images/get-jdk-url.sh
@@ -3,7 +3,7 @@ set -e
 
 case "$1" in
 	java8)
-		 echo "https://github.com/adoptium/temurin8-binaries/releases/download/jdk8u312-b07/OpenJDK8U-jdk_x64_linux_hotspot_8u312b07.tar.gz"
+		 echo "https://github.com/adoptium/temurin8-binaries/releases/download/jdk8u322-b06/OpenJDK8U-jdk_x64_linux_hotspot_8u322b06.tar.gz"
 	;;
 	java11)
 		 echo "https://github.com/adoptium/temurin11-binaries/releases/download/jdk-11.0.13%2B8/OpenJDK11U-jdk_x64_linux_hotspot_11.0.13_8.tar.gz"
