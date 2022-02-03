ddc481136ba440d86277d0d80e342f4f1123cd64
Stephane Nicoll <snicoll@vmware.com>
2022-02-03 08:41:02 +0100

    Upgrade Java 17 version in CI image
    Closes gh-29631

--- ci/images/get-jdk-url.sh
+++ ci/images/get-jdk-url.sh
@@ -3,7 +3,7 @@ set -e
 
 case "$1" in
 	java17)
-		 echo "https://github.com/adoptium/temurin17-binaries/releases/download/jdk-17.0.1%2B12/OpenJDK17U-jdk_x64_linux_hotspot_17.0.1_12.tar.gz"
+		 echo "https://github.com/adoptium/temurin17-binaries/releases/download/jdk-17.0.2%2B8/OpenJDK17U-jdk_x64_linux_hotspot_17.0.2_8.tar.gz"
 	;;
   *)
 		echo $"Unknown java version"
