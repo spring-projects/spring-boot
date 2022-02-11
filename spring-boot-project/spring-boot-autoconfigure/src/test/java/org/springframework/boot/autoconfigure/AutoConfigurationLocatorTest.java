74af49fc13b259fd9457e90d0c0ba57cadc55bb0
Moritz Halbritter <mhalbritter@vmware.com>
2022-02-11 10:52:13 +0100

    Fix checkstyle issues

--- spring-boot-project/spring-boot-autoconfigure/src/test/java/org/springframework/boot/autoconfigure/AutoConfigurationLocatorTest.java
+++ spring-boot-project/spring-boot-autoconfigure/src/test/java/org/springframework/boot/autoconfigure/AutoConfigurationLocatorTest.java
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2012-2022 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package org.springframework.boot.autoconfigure;
 
 import java.lang.annotation.ElementType;
@@ -17,12 +33,12 @@ class AutoConfigurationLocatorTest {
 
 	@BeforeEach
 	void setUp() {
-		sut = new AutoConfigurationLocator();
+		this.sut = new AutoConfigurationLocator();
 	}
 
 	@Test
 	void locate() {
-		List<String> classNames = sut.locate(TestAutoConfiguration.class, null);
+		List<String> classNames = this.sut.locate(TestAutoConfiguration.class, null);
 
 		assertThat(classNames).containsExactly("class1", "class2", "class3");
 	}
