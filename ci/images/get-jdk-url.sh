cca97ff4b7647b513f203c74f46cc9e2b5716737
Stephane Nicoll <snicoll@vmware.com>
2022-02-17 08:40:46 +0100

    Upgrade Java 11 version in CI image
    Closes gh-29889

--- ci/images/get-jdk-url.sh
+++ ci/images/get-jdk-url.sh
@@ -6,7 +6,7 @@ case "$1" in
 		 echo "https://github.com/adoptium/temurin8-binaries/releases/download/jdk8u322-b06/OpenJDK8U-jdk_x64_linux_hotspot_8u322b06.tar.gz"
 	;;
 	java11)
-		 echo "https://github.com/adoptium/temurin11-binaries/releases/download/jdk-11.0.14%2B9/OpenJDK11U-jdk_x64_linux_hotspot_11.0.14_9.tar.gz"
+		 echo "https://github.com/adoptium/temurin11-binaries/releases/download/jdk-11.0.14.1%2B1/OpenJDK11U-jdk_x64_linux_hotspot_11.0.14.1_1.tar.gz"
 	;;
 	java17)
 		 echo "https://github.com/adoptium/temurin17-binaries/releases/download/jdk-17.0.2%2B8/OpenJDK17U-jdk_x64_linux_hotspot_17.0.2_8.tar.gz"
