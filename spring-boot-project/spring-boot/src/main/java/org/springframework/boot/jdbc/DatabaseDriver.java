631b87616521705a42e36d4188cc050c779f6ff1
Andy Wilkinson <wilkinsona@vmware.com>
2022-04-12 11:02:23 +0100

    Deprecate DatabaseDriver.GAE
    Closes gh-29984

--- spring-boot-project/spring-boot/src/main/java/org/springframework/boot/jdbc/DatabaseDriver.java
+++ spring-boot-project/spring-boot/src/main/java/org/springframework/boot/jdbc/DatabaseDriver.java
@@ -78,7 +78,10 @@ public enum DatabaseDriver {
 
 	/**
 	 * Google App Engine.
+	 * @deprecated since 2.7.0 for removal in 2.9.0 without replacement following the
+	 * removal of AppEngineDriver from version 2.0 of the AppEngine API SDK.
 	 */
+	@Deprecated
 	GAE(null, "com.google.appengine.api.rdbms.AppEngineDriver"),
 
 	/**
