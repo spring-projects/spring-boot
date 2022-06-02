8e57ac82ca7288c976661c2f76575e7a25e9b8f1
saikiranbachu09 <saikiranbachu09@gmail.com>
2022-06-02 15:21:57 +0000

    Update NodeAssert.java

--- buildSrc/src/test/java/org/springframework/boot/build/assertj/NodeAssert.java
+++ buildSrc/src/test/java/org/springframework/boot/build/assertj/NodeAssert.java
@@ -1,6 +1,7 @@
 /*
  * Copyright 2012-2020 the original author or authors.
  *
+ *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
