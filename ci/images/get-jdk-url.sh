c4bbbcd3e142e94e1df7ed3dfa081674c0f8dce8
Stephane Nicoll <snicoll@vmware.com>
2022-06-09 08:04:49 +0200

    Upgrade Java 18 version in CI image
    Closes gh-31299

--- ci/images/get-jdk-url.sh
+++ ci/images/get-jdk-url.sh
@@ -6,7 +6,7 @@ case "$1" in
 		 echo "https://github.com/bell-sw/Liberica/releases/download/17.0.3.1+2/bellsoft-jdk17.0.3.1+2-linux-amd64.tar.gz"
 	;;
 	java18)
-		 echo "https://github.com/bell-sw/Liberica/releases/download/18.0.1+12/bellsoft-jdk18.0.1+12-linux-amd64.tar.gz"
+		 echo "https://github.com/bell-sw/Liberica/releases/download/18.0.1.1+2/bellsoft-jdk18.0.1.1+2-linux-amd64.tar.gz"
 	;;
   *)
 		echo $"Unknown java version"
