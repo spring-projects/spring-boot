522ea0a90e369549a75ff035771587c0a971bad6
Andy Wilkinson <wilkinsona@vmware.com>
2022-06-09 07:22:48 +0100

    Handle malformed JSON more consistently
    Closes gh-31301

--- spring-boot-project/spring-boot/src/main/java/org/springframework/boot/json/BasicJsonParser.java
+++ spring-boot-project/spring-boot/src/main/java/org/springframework/boot/json/BasicJsonParser.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2012-2019 the original author or authors.
+ * Copyright 2012-2022 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -39,12 +39,12 @@ public class BasicJsonParser extends AbstractJsonParser {
 
 	@Override
 	public Map<String, Object> parseMap(String json) {
-		return parseMap(json, this::parseMapInternal);
+		return tryParse(() -> parseMap(json, this::parseMapInternal), Exception.class);
 	}
 
 	@Override
 	public List<Object> parseList(String json) {
-		return parseList(json, this::parseListInternal);
+		return tryParse(() -> parseList(json, this::parseListInternal), Exception.class);
 	}
 
 	private List<Object> parseListInternal(String json) {
