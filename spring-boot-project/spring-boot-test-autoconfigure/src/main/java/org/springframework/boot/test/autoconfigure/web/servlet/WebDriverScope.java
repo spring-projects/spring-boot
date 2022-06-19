e746dfa677201c2ffa63e30a4dc84dfe4bd82232
Stephane Nicoll <snicoll@vmware.com>
2022-06-19 10:35:09 +0200

    Polish "Allow configuring WebDriver with TestNG"
    See gh-27947

--- spring-boot-project/spring-boot-test-autoconfigure/src/main/java/org/springframework/boot/test/autoconfigure/web/servlet/WebDriverScope.java
+++ spring-boot-project/spring-boot-test-autoconfigure/src/main/java/org/springframework/boot/test/autoconfigure/web/servlet/WebDriverScope.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2012-2019 the original author or authors.
+ * Copyright 2012-2022 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -36,7 +36,7 @@ import org.springframework.util.StringUtils;
  * {@link WebDriverTestExecutionListener}.
  *
  * @author Phillip Webb
- * @since 2.6.0
+ * @since 3.0.0
  * @see WebDriverContextCustomizerFactory
  * @see WebDriverTestExecutionListener
  */
