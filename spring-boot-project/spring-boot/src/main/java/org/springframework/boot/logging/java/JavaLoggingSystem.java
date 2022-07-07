dc5eee8f487c4f5fa3e3caf3aab96d9475dad750
Moritz Halbritter <mkammerer@vmware.com>
2022-07-07 14:20:13 +0200

    Polish JavaLoggingSystem

--- spring-boot-project/spring-boot/src/main/java/org/springframework/boot/logging/java/JavaLoggingSystem.java
+++ spring-boot-project/spring-boot/src/main/java/org/springframework/boot/logging/java/JavaLoggingSystem.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2012-2021 the original author or authors.
+ * Copyright 2012-2022 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -89,7 +89,7 @@ public class JavaLoggingSystem extends AbstractLoggingSystem {
 			loadConfiguration(getPackagedConfigFile("logging-file.properties"), logFile);
 		}
 		else {
-			loadConfiguration(getPackagedConfigFile("logging.properties"), logFile);
+			loadConfiguration(getPackagedConfigFile("logging.properties"), null);
 		}
 	}
 
