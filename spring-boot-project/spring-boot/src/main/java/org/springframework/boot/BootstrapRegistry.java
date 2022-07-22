de5834d20312d5a8bdf16de275de81cb486decab
dreis2211 <christoph.dreis@freenet.de>
2022-07-22 14:59:21 +0200

    Polish
    See gh-31845

--- spring-boot-project/spring-boot/src/main/java/org/springframework/boot/BootstrapRegistry.java
+++ spring-boot-project/spring-boot/src/main/java/org/springframework/boot/BootstrapRegistry.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2012-2021 the original author or authors.
+ * Copyright 2012-2022 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -163,7 +163,7 @@ public interface BootstrapRegistry {
 	}
 
 	/**
-	 * The scope of a instance.
+	 * The scope of an instance.
 	 * @since 2.4.2
 	 */
 	enum Scope {
