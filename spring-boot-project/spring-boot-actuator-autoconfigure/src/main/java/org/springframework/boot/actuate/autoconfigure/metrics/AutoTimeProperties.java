fa73b73898af093ba4e9fca6e90f13bf42d47b23
Andy Wilkinson <wilkinsona@vmware.com>
2022-07-29 12:35:54 +0100

    Fail build on missing configuration property descriptions
    Closes gh-31916

--- spring-boot-project/spring-boot-actuator-autoconfigure/src/main/java/org/springframework/boot/actuate/autoconfigure/metrics/AutoTimeProperties.java
+++ spring-boot-project/spring-boot-actuator-autoconfigure/src/main/java/org/springframework/boot/actuate/autoconfigure/metrics/AutoTimeProperties.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2012-2019 the original author or authors.
+ * Copyright 2012-2022 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -30,10 +30,19 @@ import org.springframework.boot.actuate.metrics.AutoTimer;
  */
 public final class AutoTimeProperties implements AutoTimer {
 
+	/**
+	 * Whether to enable auto-timing.
+	 */
 	private boolean enabled = true;
 
+	/**
+	 * Whether to publish percentile histrograms.
+	 */
 	private boolean percentilesHistogram;
 
+	/**
+	 * Percentiles for which additional time series should be published.
+	 */
 	private double[] percentiles;
 
 	/**
