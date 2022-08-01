f3d0da64aef4b97b345de6966015b839727e22c4
Stephane Nicoll <snicoll@vmware.com>
2022-08-01 09:50:41 +0200

    Upgrade Java 17 version in CI image
    Closes gh-31933

--- ci/images/get-jdk-url.sh
+++ ci/images/get-jdk-url.sh
@@ -9,7 +9,7 @@ case "$1" in
 		 echo "https://github.com/bell-sw/Liberica/releases/download/11.0.16+8/bellsoft-jdk11.0.16+8-linux-amd64.tar.gz"
 	;;
 	java17)
-		 echo "https://github.com/bell-sw/Liberica/releases/download/17.0.3.1+2/bellsoft-jdk17.0.3.1+2-linux-amd64.tar.gz"
+		 echo "https://github.com/bell-sw/Liberica/releases/download/17.0.4+8/bellsoft-jdk17.0.4+8-linux-amd64.tar.gz"
 	;;
 	java18)
 		 echo "https://github.com/bell-sw/Liberica/releases/download/18.0.1.1+2/bellsoft-jdk18.0.1.1+2-linux-amd64.tar.gz"
