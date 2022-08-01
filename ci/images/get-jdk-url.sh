4ffb8eeda1eb024302c4508b05732061d4839279
Stephane Nicoll <snicoll@vmware.com>
2022-08-01 09:50:15 +0200

    Upgrade Java 11 version in CI image
    Closes gh-31931

--- ci/images/get-jdk-url.sh
+++ ci/images/get-jdk-url.sh
@@ -6,7 +6,7 @@ case "$1" in
 		 echo "https://github.com/bell-sw/Liberica/releases/download/8u342+7/bellsoft-jdk8u342+7-linux-amd64.tar.gz"
 	;;
 	java11)
-		 echo "https://github.com/bell-sw/Liberica/releases/download/11.0.15.1+2/bellsoft-jdk11.0.15.1+2-linux-amd64.tar.gz"
+		 echo "https://github.com/bell-sw/Liberica/releases/download/11.0.16+8/bellsoft-jdk11.0.16+8-linux-amd64.tar.gz"
 	;;
 	java17)
 		 echo "https://github.com/bell-sw/Liberica/releases/download/17.0.3.1+2/bellsoft-jdk17.0.3.1+2-linux-amd64.tar.gz"
