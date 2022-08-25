a7e2c0c8af0159bcf69c26bc4a5278afad38a4de
Phillip Webb <pwebb@vmware.com>
2022-08-24 17:50:36 -0700

    Polish

--- ci/images/get-jdk-url.sh
+++ ci/images/get-jdk-url.sh
@@ -14,7 +14,7 @@ case "$1" in
 	java18)
 		 echo "https://github.com/bell-sw/Liberica/releases/download/18.0.2+10/bellsoft-jdk18.0.2+10-linux-amd64.tar.gz"
 	;;
-  *)
+	*)
 		echo $"Unknown java version"
 		exit 1
 esac
