7a6e6ee00745d5ad69ba3a7d7711d6fb11faa2d6
Andy Wilkinson <wilkinsona@vmware.com>
2022-09-01 17:37:05 +0100

    Upgrade Java 18 CI to 18.0.2.1+1
    Closes gh-32206

--- ci/images/get-jdk-url.sh
+++ ci/images/get-jdk-url.sh
@@ -6,7 +6,7 @@ case "$1" in
 		 echo "https://github.com/bell-sw/Liberica/releases/download/17.0.4.1+1/bellsoft-jdk17.0.4.1+1-linux-amd64.tar.gz"
 	;;
 	java18)
-		 echo "https://github.com/bell-sw/Liberica/releases/download/18.0.2+10/bellsoft-jdk18.0.2+10-linux-amd64.tar.gz"
+		 echo "https://github.com/bell-sw/Liberica/releases/download/18.0.2.1+1/bellsoft-jdk18.0.2.1+1-linux-amd64.tar.gz"
 	;;
 	*)
 		echo $"Unknown java version"
