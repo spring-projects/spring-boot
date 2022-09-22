spring-projects/spring-boot

commit 6175c4210dca82776ad26ab140a0ae19704c9fe6

Fix skipping of processAot when there is no main source
See gh-32424

spring-boot-project/spring-boot-tools/spring-boot-gradle-plugin/src/main/java/org/springframework/boot/gradle/tasks/aot/AbstractAot.java
@@ -92,7 +92,7 @@ public abstract class AbstractAot extends JavaExec {
 	@IgnoreEmptyDirectories
 	@PathSensitive(PathSensitivity.RELATIVE)
 	public final FileCollection getInputClasses() {
-		return this.inputClasses.getAsFileTree().matching((filter) -> filter.include((spec) -> !spec.isDirectory()));
+		return this.inputClasses.getAsFileTree();
 	}
 
 	public void setInputClasses(FileCollection inputClasses) {

spring-boot-project/spring-boot-tools/spring-boot-gradle-plugin/src/test/java/org/springframework/boot/gradle/plugin/SpringBootAotPluginIntegrationTests.java
@@ -102,9 +102,34 @@ class SpringBootAotPluginIntegrationTests {
 	}
 
 	@TestTemplate
+	void processAotRunsWhenProjectHasMainSource() throws IOException {
+		writeMainClass("org.springframework.boot", "AotProcessor");
+		writeMainClass("com.example", "Main");
+		assertThat(this.gradleBuild.build("processAot").task(":processAot").getOutcome())
+				.isEqualTo(TaskOutcome.SUCCESS);
+	}
+
+	@TestTemplate
 	void processTestAotIsSkippedWhenProjectHasNoTestSource() {
 		assertThat(this.gradleBuild.build("processTestAot").task(":processTestAot").getOutcome())
 				.isEqualTo(TaskOutcome.NO_SOURCE);
+	}
+
+	private void writeMainClass(String packageName, String className) throws IOException {
+		File java = new File(this.gradleBuild.getProjectDir(),
+				"src/main/java/" + packageName.replace(".", "/") + "/" + className + ".java");
+		java.getParentFile().mkdirs();
+		Files.writeString(java.toPath(), """
+				package %s;
+
+				public class %s {
+
+					public static void main(String[] args) {
+
+					}
+
+				}
+				""".formatted(packageName, className));
 	}
 
 }

spring-boot-project/spring-boot-tools/spring-boot-gradle-plugin/src/test/resources/org/springframework/boot/gradle/plugin/SpringBootAotPluginIntegrationTests-processAotRunsWhenProjectHasMainSource.gradle
@@ -1,0 +1,13 @@
+plugins {
+	id 'org.springframework.boot'
+	id 'org.springframework.boot.aot'
+	id 'java'
+}
+
+repositories {
+	mavenCentral()
+}
+
+springBoot {
+	mainClass = 'com.example.Main'
+}

commit 7c7c34cda16dc571b671777b3c2ac9f69af37c22

Fix classpath roots of AOT test processing
This was regressed in 6175c4210dca82776ad26ab140a0ae19704c9fe6 so that
each class on the classpath was passed in as a "root". This commit
corrects this so that only the roots are passed to the AOT test
processor.

See gh-32424

spring-boot-project/spring-boot-tools/spring-boot-gradle-plugin/src/main/java/org/springframework/boot/gradle/plugin/SpringBootAotPlugin.java
@@ -138,7 +138,7 @@ public class SpringBootAotPlugin implements Plugin<Project> {
 		task.getClassesOutput().set(generatedClasses);
 		task.getGroupId().set(project.provider(() -> String.valueOf(project.getGroup())));
 		task.getArtifactId().set(project.provider(() -> project.getName()));
-		task.setInputClasses(inputSourceSet.getOutput().getClassesDirs());
+		task.setClasspathRoots(inputSourceSet.getOutput().getClassesDirs());
 	}
 
 	private void configureDependsOn(Project project, SourceSet aotSourceSet,

spring-boot-project/spring-boot-tools/spring-boot-gradle-plugin/src/main/java/org/springframework/boot/gradle/tasks/aot/AbstractAot.java
@@ -21,6 +21,7 @@ import java.util.List;
 
 import org.gradle.api.file.DirectoryProperty;
 import org.gradle.api.file.FileCollection;
+import org.gradle.api.file.FileTree;
 import org.gradle.api.provider.Property;
 import org.gradle.api.tasks.IgnoreEmptyDirectories;
 import org.gradle.api.tasks.Input;
@@ -52,7 +53,7 @@ public abstract class AbstractAot extends JavaExec {
 
 	private final Property<String> artifactId;
 
-	private FileCollection inputClasses;
+	private FileCollection classpathRoots;
 
 	protected AbstractAot() {
 		this.sourcesDir = getProject().getObjects().directoryProperty();
@@ -88,15 +89,21 @@ public abstract class AbstractAot extends JavaExec {
 	}
 
 	@InputFiles
+	@PathSensitive(PathSensitivity.RELATIVE)
+	public final FileCollection getClasspathRoots() {
+		return this.classpathRoots;
+	}
+
+	@InputFiles
 	@SkipWhenEmpty
 	@IgnoreEmptyDirectories
 	@PathSensitive(PathSensitivity.RELATIVE)
-	public final FileCollection getInputClasses() {
-		return this.inputClasses.getAsFileTree();
+	final FileTree getInputClasses() {
+		return this.classpathRoots.getAsFileTree();
 	}
 
-	public void setInputClasses(FileCollection inputClasses) {
-		this.inputClasses = inputClasses;
+	public void setClasspathRoots(FileCollection classpathRoots) {
+		this.classpathRoots = classpathRoots;
 	}
 
 	List<String> processorArgs() {

spring-boot-project/spring-boot-tools/spring-boot-gradle-plugin/src/main/java/org/springframework/boot/gradle/tasks/aot/ProcessTestAot.java
@@ -69,7 +69,7 @@ public class ProcessTestAot extends AbstractAot {
 	@TaskAction
 	public void exec() {
 		List<String> args = new ArrayList<>();
-		args.add(this.getInputClasses().getFiles().stream().filter(File::exists).map(File::getAbsolutePath)
+		args.add(this.getClasspathRoots().getFiles().stream().filter(File::exists).map(File::getAbsolutePath)
 				.collect(Collectors.joining(File.pathSeparator)));
 		args.addAll(processorArgs());
 		this.setArgs(args);