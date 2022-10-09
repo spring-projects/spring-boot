c5a194426786973ce3e697163c6a6031b9c0b71a
Ada-Claire <acripps@gmail.com>
2022-10-08 20:58:04 -0700

    Harmonize ConfigurationProperties code samples
    Rename the SomeProperties class so that it is consistent with other
    examples given within the Type-safe Configuration Properties. This
    ensures that readers can intuitively draw the conclusion that the
    MyProperties class is annotated with ConfigurationProperties.
    
    See gh-32644

--- spring-boot-project/spring-boot-docs/src/main/java/org/springframework/boot/docs/features/externalconfig/typesafeconfigurationproperties/usingannotatedtypes/MyProperties.java
+++ spring-boot-project/spring-boot-docs/src/main/java/org/springframework/boot/docs/features/externalconfig/typesafeconfigurationproperties/usingannotatedtypes/MyProperties.java
@@ -16,7 +16,7 @@
 
 package org.springframework.boot.docs.features.externalconfig.typesafeconfigurationproperties.usingannotatedtypes;
 
-class SomeProperties {
+class MyProperties {
 
 	Object getRemoteAddress() {
 		return null;
