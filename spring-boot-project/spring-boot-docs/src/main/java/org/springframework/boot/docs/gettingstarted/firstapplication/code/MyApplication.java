2cdd071775f848df010f2b3a54768d613a773669
Phillip Webb <pwebb@vmware.com>
2022-10-18 20:44:28 -0700

    Update getting started documentation to use @SpringBootApplication
    Closes gh-32795

--- spring-boot-project/spring-boot-docs/src/main/java/org/springframework/boot/docs/gettingstarted/firstapplication/code/MyApplication.java
+++ spring-boot-project/spring-boot-docs/src/main/java/org/springframework/boot/docs/gettingstarted/firstapplication/code/MyApplication.java
@@ -17,12 +17,12 @@
 package org.springframework.boot.docs.gettingstarted.firstapplication.code;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@EnableAutoConfiguration
+@SpringBootApplication
 public class MyApplication {
 
 	@RequestMapping("/")
