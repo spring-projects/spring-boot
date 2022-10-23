7e424bdf0c63a219953c4b014f61b7c8785e8884
tumit <tumit@live.com>
2022-10-23 18:13:05 +0700

    Add tests for BindableRuntimeHintsRegistrar
    See gh-32851

---
+++ spring-boot-project/spring-boot/src/test/java/org/springframework/boot/context/properties/bind/BindableRuntimeHintsRegistrarTests.java
@@ -0,0 +1,101 @@
+/*
+ * Copyright 2012-2022 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.springframework.boot.context.properties.bind;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.aot.hint.RuntimeHints;
+import org.springframework.aot.hint.predicate.RuntimeHintsPredicates;
+import org.springframework.boot.context.properties.BoundConfigurationProperties;
+import org.springframework.boot.context.properties.ConfigurationPropertiesBean;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Set;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+/**
+ * Tests for {@link BindableRuntimeHintsRegistrar}.
+ *
+ * @author Tumit Watcharapol
+ */
+class BindableRuntimeHintsRegistrarTests {
+
+	@Test
+	void shouldRegisterHints() {
+
+		RuntimeHints runtimeHints = new RuntimeHints();
+
+		Class<?>[] types = { BoundConfigurationProperties.class, ConfigurationPropertiesBean.class };
+
+		BindableRuntimeHintsRegistrar registrar = new BindableRuntimeHintsRegistrar(types);
+
+		registrar.registerHints(runtimeHints, getClass().getClassLoader());
+
+		for (Class<?> type : types) {
+			assertThat(RuntimeHintsPredicates.reflection().onType(type)).accepts(runtimeHints);
+		}
+
+	}
+
+	@Test
+	void shouldNoRegisterHints() {
+
+		RuntimeHints runtimeHints = new RuntimeHints();
+		BindableRuntimeHintsRegistrar registrar = new BindableRuntimeHintsRegistrar();
+
+		registrar.registerHints(runtimeHints);
+
+		assertThat(RuntimeHintsPredicates.reflection().onType(BoundConfigurationProperties.class))
+				.rejects(runtimeHints);
+	}
+
+	@Test
+	void shouldRegisterHintsForTypes() {
+
+		RuntimeHints runtimeHints = new RuntimeHints();
+
+		Class<?>[] types = { BoundConfigurationProperties.class, ConfigurationPropertiesBean.class };
+
+		BindableRuntimeHintsRegistrar registrar = BindableRuntimeHintsRegistrar.forTypes(types);
+
+		registrar.registerHints(runtimeHints);
+
+		for (Class<?> type : types) {
+			assertThat(RuntimeHintsPredicates.reflection().onType(type)).accepts(runtimeHints);
+		}
+
+	}
+
+	@Test
+	void shouldRegisterHintsForTypesIterable() {
+
+		RuntimeHints runtimeHints = new RuntimeHints();
+
+		List<Class<?>> types = Arrays.asList(BoundConfigurationProperties.class, ConfigurationPropertiesBean.class);
+
+		BindableRuntimeHintsRegistrar registrar = BindableRuntimeHintsRegistrar.forTypes(types);
+
+		registrar.registerHints(runtimeHints);
+
+		for (Class<?> type : types) {
+			assertThat(RuntimeHintsPredicates.reflection().onType(type)).accepts(runtimeHints);
+		}
+
+	}
+
+}
