159910b0d92a632db9d6a1cb42181c949f1bbdbe
Phillip Webb <pwebb@vmware.com>
2022-10-25 14:49:04 -0700

    Polish

--- spring-boot-project/spring-boot/src/test/java/org/springframework/boot/web/client/ClientHttpRequestFactoriesRuntimeHintsTests.java
+++ spring-boot-project/spring-boot/src/test/java/org/springframework/boot/web/client/ClientHttpRequestFactoriesRuntimeHintsTests.java
@@ -34,7 +34,7 @@ import static org.assertj.core.api.Assertions.assertThat;
  *
  * @author Andy Wilkinson
  */
-public class ClientHttpRequestFactoriesRuntimeHintsTests {
+class ClientHttpRequestFactoriesRuntimeHintsTests {
 
 	@Test
 	void shouldRegisterHints() {
