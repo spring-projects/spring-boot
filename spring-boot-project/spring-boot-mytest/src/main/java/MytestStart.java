8497a66c6ea6ba3821f48d871d41cfbc79a91448
yuanfacheng <yuanfacheng70321@163.com>
2022-10-25 16:21:28 +0800

    添加测试类，调试springboot源码

---
+++ spring-boot-project/spring-boot-mytest/src/main/java/MytestStart.java
@@ -0,0 +1,18 @@
+
+//import org.springframework.boot.SpringApplication;
+//import org.springframework.boot.autoconfigure.SpringBootApplication;
+//@SpringBootApplication
+public class MytestStart {
+//		public static void main(String[] args) {
+//			System.out.println("Spring Boot 源码剖析之源码环境搭建验证");
+//			SpringApplication.run(DemoApplication.class, args);
+//			System.out.println("Spring Boot 源码剖析之源码环境搭建验证成功");
+//		}
+}
+
+
+
+
+
+
+
--- spring-boot-tests/spring-boot-smoke-tests/spring-boot-smoke-test-hibernate52/src/main/java/org/springframework/boot/tests/hibernate52/MytestStart.java
+++ spring-boot-tests/spring-boot-smoke-tests/spring-boot-smoke-test-hibernate52/src/main/java/org/springframework/boot/tests/hibernate52/MytestStart.java
@@ -1,20 +1,13 @@
 package org.springframework.boot.tests.hibernate52;
 
-import java.util.Arrays;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class MytestStart {
 	public static void main(String[] args) {
 		System.out.println("Spring Boot 源码剖析之源码环境搭建验证");
-		SpringApplication.run(MytestStart.class, args);;
-		System.out.println(1111);
+		SpringApplication.run(MytestStart.class, args);
 		System.out.println("Spring Boot 源码剖析之源码环境搭建验证成功");
-		Arrays.asList(1,2,3,45).stream().
-				filter(i->i%2==0||i%3==0).
-				map(i->i*i).
-				forEach(System.out::println);
 	}
 }
 
