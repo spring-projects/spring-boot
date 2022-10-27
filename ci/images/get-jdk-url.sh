f37219adc1368d1a8882790dfb808eae26e1b5e7
Moritz Halbritter <mkammerer@vmware.com>
2022-10-27 16:40:10 +0200

    Update JDK 19 build to use 19.0.1+11
    Closes gh-32892

--- ci/images/get-jdk-url.sh
+++ ci/images/get-jdk-url.sh
@@ -6,7 +6,7 @@ case "$1" in
 		 echo "https://github.com/bell-sw/Liberica/releases/download/17.0.5+8/bellsoft-jdk17.0.5+8-linux-amd64.tar.gz"
 	;;
 	java19)
-		 echo "https://github.com/bell-sw/Liberica/releases/download/19+37/bellsoft-jdk19+37-linux-amd64.tar.gz"
+		 echo "https://github.com/bell-sw/Liberica/releases/download/19.0.1+11/bellsoft-jdk19.0.1+11-linux-amd64.tar.gz"
 	;;
 	*)
 		echo $"Unknown java version"
