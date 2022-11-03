bb684cc0adb9addb42c40e95a9195dbc908a8325
Scott Frederick <sfrederick@vmware.com>
2022-11-03 11:29:02 -0500

    Add support for CNB platform API 0.10
    Fixes gh-32949

--- spring-boot-project/spring-boot-tools/spring-boot-buildpack-platform/src/main/java/org/springframework/boot/buildpack/platform/build/ApiVersions.java
+++ spring-boot-project/spring-boot-tools/spring-boot-buildpack-platform/src/main/java/org/springframework/boot/buildpack/platform/build/ApiVersions.java
@@ -31,7 +31,7 @@ final class ApiVersions {
 	/**
 	 * The platform API versions supported by this release.
 	 */
-	static final ApiVersions SUPPORTED_PLATFORMS = ApiVersions.of(0, IntStream.rangeClosed(3, 9));
+	static final ApiVersions SUPPORTED_PLATFORMS = ApiVersions.of(0, IntStream.rangeClosed(3, 10));
 
 	private final ApiVersion[] apiVersions;
 
