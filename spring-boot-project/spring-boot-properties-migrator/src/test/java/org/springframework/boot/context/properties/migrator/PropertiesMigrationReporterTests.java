7c65c3e260a7df15870b66b2352bdf81573b54e2
Andy Wilkinson <wilkinsona@vmware.com>
2022-11-17 20:50:26 +0000

    Handle invalid names gracefully in properties migrator
    Closes gh-32729

--- spring-boot-project/spring-boot-properties-migrator/src/test/java/org/springframework/boot/context/properties/migrator/PropertiesMigrationReporterTests.java
+++ spring-boot-project/spring-boot-properties-migrator/src/test/java/org/springframework/boot/context/properties/migrator/PropertiesMigrationReporterTests.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2012-2019 the original author or authors.
+ * Copyright 2012-2022 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -18,6 +18,7 @@ package org.springframework.boot.context.properties.migrator;
 
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
@@ -142,6 +143,15 @@ class PropertiesMigrationReporterTests {
 		assertThat(report).containsSubsequence("Property source 'first'", "deprecated.six.test", "Line: 1", "Reason",
 				"No metadata found for replacement key 'does.not.exist'");
 		assertThat(report).doesNotContain("null");
+	}
+
+	@Test
+	void invalidNameHandledGracefully() {
+		this.environment.getPropertySources()
+				.addFirst(new MapPropertySource("first", Collections.singletonMap("invalid.property-name", "value")));
+		String report = createWarningReport(loadRepository("metadata/sample-metadata-invalid-name.json"));
+		assertThat(report).isNotNull();
+		assertThat(report).contains("Key: invalid.PropertyName").contains("Replacement: valid.property-name");
 	}
 
 	private List<String> mapToNames(PropertySources sources) {
