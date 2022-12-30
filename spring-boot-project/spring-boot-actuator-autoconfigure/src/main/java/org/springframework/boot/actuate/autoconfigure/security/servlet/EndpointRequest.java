a03e3c08c18ee8577db4b87b59636f30a40767c6
rishal <devrishal@gmail.com>
2022-12-30 14:55:08 +0100

    Removed unnecessary brackets

--- spring-boot-project/spring-boot-actuator-autoconfigure/src/main/java/org/springframework/boot/actuate/autoconfigure/security/servlet/EndpointRequest.java
+++ spring-boot-project/spring-boot-actuator-autoconfigure/src/main/java/org/springframework/boot/actuate/autoconfigure/security/servlet/EndpointRequest.java
@@ -277,8 +277,8 @@ public final class EndpointRequest {
 		public String toString() {
 			StringBuilder sb = new StringBuilder();
 			sb.append("EndpointRequest [includes='").append(this.includes).append("'");
-			sb.append(", [Excludes='").append(this.excludes).append("'");
-			sb.append(", [IncludeLinks=]").append(this.includeLinks).append("'");
+			sb.append(", Excludes='").append(this.excludes).append("'");
+			sb.append(", IncludeLinks=").append(this.includeLinks).append("'");
 			sb.append("]");
 			return sb.toString();
 		}
