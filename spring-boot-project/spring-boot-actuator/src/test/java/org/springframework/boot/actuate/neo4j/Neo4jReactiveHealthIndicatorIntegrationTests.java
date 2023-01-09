232807d388df1c64657e74e5a11eb03dacb35798
Phillip Webb <pwebb@vmware.com>
2023-01-09 15:16:20 -0800

    Increase block timeout

--- spring-boot-project/spring-boot-actuator/src/test/java/org/springframework/boot/actuate/neo4j/Neo4jReactiveHealthIndicatorIntegrationTests.java
+++ spring-boot-project/spring-boot-actuator/src/test/java/org/springframework/boot/actuate/neo4j/Neo4jReactiveHealthIndicatorIntegrationTests.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2012-2022 the original author or authors.
+ * Copyright 2012-2023 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -64,7 +64,7 @@ public class Neo4jReactiveHealthIndicatorIntegrationTests {
 
 	@Test
 	void health() {
-		Health health = this.healthIndicator.getHealth(true).block(Duration.ofSeconds(5));
+		Health health = this.healthIndicator.getHealth(true).block(Duration.ofSeconds(20));
 		assertThat(health.getStatus()).isEqualTo(Status.UP);
 		assertThat(health.getDetails()).containsEntry("edition", "community");
 	}
