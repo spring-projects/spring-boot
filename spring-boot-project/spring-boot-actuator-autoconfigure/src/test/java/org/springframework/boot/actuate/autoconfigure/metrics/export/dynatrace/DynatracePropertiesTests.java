ff04f009c090e8712a1937ad19a9d6830bce5746
Mirko Sobeck <msobeck.dev@gmail.com>
2023-01-10 17:31:40 +0100

    Add missing Micrometer PropertiesConfigAdapterTests
    Closes gh-33743

--- spring-boot-project/spring-boot-actuator-autoconfigure/src/test/java/org/springframework/boot/actuate/autoconfigure/metrics/export/dynatrace/DynatracePropertiesTests.java
+++ spring-boot-project/spring-boot-actuator-autoconfigure/src/test/java/org/springframework/boot/actuate/autoconfigure/metrics/export/dynatrace/DynatracePropertiesTests.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2012-2021 the original author or authors.
+ * Copyright 2012-2023 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -38,6 +38,8 @@ class DynatracePropertiesTests extends StepRegistryPropertiesTests {
 		assertStepRegistryDefaultValues(properties, config);
 		assertThat(properties.getTechnologyType()).isEqualTo(config.technologyType());
 		assertThat(properties.getV1().getTechnologyType()).isEqualTo(config.technologyType());
+		assertThat(properties.getV2().isUseDynatraceSummaryInstruments())
+				.isEqualTo(config.useDynatraceSummaryInstruments());
 	}
 
 }
