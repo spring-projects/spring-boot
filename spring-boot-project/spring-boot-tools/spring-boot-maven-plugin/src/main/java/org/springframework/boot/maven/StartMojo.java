9f2ed201b29c84a92db32b7a74d55c996d48e389
Moritz Halbritter <mkammerer@vmware.com>
2023-01-16 15:58:03 +0100

    Improve error message when start goal can't contact running application
    Closes gh-24044

--- spring-boot-project/spring-boot-tools/spring-boot-maven-plugin/src/main/java/org/springframework/boot/maven/StartMojo.java
+++ spring-boot-project/spring-boot-tools/spring-boot-maven-plugin/src/main/java/org/springframework/boot/maven/StartMojo.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2012-2022 the original author or authors.
+ * Copyright 2012-2023 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -185,7 +185,8 @@ public class StartMojo extends AbstractRunMojo {
 			}
 		}
 		catch (IOException ex) {
-			throw new MojoFailureException("Could not contact Spring Boot application", ex);
+			throw new MojoFailureException("Could not contact Spring Boot application via JMX on port " + this.jmxPort
+					+ ". Please make sure that no other process is using that port", ex);
 		}
 		catch (Exception ex) {
 			throw new MojoExecutionException("Could not figure out if the application has started", ex);
