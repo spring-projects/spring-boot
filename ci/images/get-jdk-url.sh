8238fe1f4dbf99015a54afafe6961bf61e2543ce
Moritz Halbritter <mkammerer@vmware.com>
2023-01-19 11:52:59 +0100

    Update Java 17 in CI
    Closes gh-33881

--- ci/images/get-jdk-url.sh
+++ ci/images/get-jdk-url.sh
@@ -9,7 +9,7 @@ case "$1" in
 		 echo "https://github.com/bell-sw/Liberica/releases/download/11.0.17+7/bellsoft-jdk11.0.17+7-linux-amd64.tar.gz"
 	;;
 	java17)
-		 echo "https://github.com/bell-sw/Liberica/releases/download/17.0.5+8/bellsoft-jdk17.0.5+8-linux-amd64.tar.gz"
+		 echo "https://github.com/bell-sw/Liberica/releases/download/17.0.6+10/bellsoft-jdk17.0.6+10-linux-amd64.tar.gz"
 	;;
 	java19)
 		 echo "https://github.com/bell-sw/Liberica/releases/download/19.0.1+11/bellsoft-jdk19.0.1+11-linux-amd64.tar.gz"
