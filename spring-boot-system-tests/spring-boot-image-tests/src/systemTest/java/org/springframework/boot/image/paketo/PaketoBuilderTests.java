359076e2130ce6c47fdcb3110f2b963342968faa
Johnny Lim <izeye@naver.com>
2023-01-25 22:31:43 +0900

    Remove EnabledForJreRange.min in PaketoBuilderTests
    See gh-33976

--- spring-boot-system-tests/spring-boot-image-tests/src/systemTest/java/org/springframework/boot/image/paketo/PaketoBuilderTests.java
+++ spring-boot-system-tests/spring-boot-image-tests/src/systemTest/java/org/springframework/boot/image/paketo/PaketoBuilderTests.java
@@ -60,7 +60,7 @@ import static org.assertj.core.api.Assertions.entry;
  * @author Scott Frederick
  */
 @ExtendWith({ GradleBuildInjectionExtension.class, GradleBuildExtension.class })
-@EnabledForJreRange(min = JRE.JAVA_8, max = JRE.JAVA_18)
+@EnabledForJreRange(max = JRE.JAVA_18)
 class PaketoBuilderTests {
 
 	GradleBuild gradleBuild;
