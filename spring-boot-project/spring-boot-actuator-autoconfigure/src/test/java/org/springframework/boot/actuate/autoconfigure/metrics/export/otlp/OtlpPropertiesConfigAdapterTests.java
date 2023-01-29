92dd004375849af7b53344a7e7f7f81c3eb14963
Eddú Meléndez <eddu.melendez@gmail.com>
2023-01-28 20:18:50 -0500

    Add property `headers` in `OtlpProperties`
    In Micrometer 1.11, headers property has been introduced.
    
    See gh-34004

--- spring-boot-project/spring-boot-actuator-autoconfigure/src/test/java/org/springframework/boot/actuate/autoconfigure/metrics/export/otlp/OtlpPropertiesConfigAdapterTests.java
+++ spring-boot-project/spring-boot-actuator-autoconfigure/src/test/java/org/springframework/boot/actuate/autoconfigure/metrics/export/otlp/OtlpPropertiesConfigAdapterTests.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2012-2022 the original author or authors.
+ * Copyright 2012-2023 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -42,6 +42,13 @@ class OtlpPropertiesConfigAdapterTests {
 		properties.setResourceAttributes(Map.of("service.name", "boot-service"));
 		assertThat(new OtlpPropertiesConfigAdapter(properties).resourceAttributes()).containsEntry("service.name",
 				"boot-service");
+	}
+
+	@Test
+	void whenPropertiesHeadersIsSetAdapterHeadersReturnsIt() {
+		OtlpProperties properties = new OtlpProperties();
+		properties.setHeaders(Map.of("header", "value"));
+		assertThat(new OtlpPropertiesConfigAdapter(properties).headers()).containsEntry("header", "value");
 	}
 
 }
