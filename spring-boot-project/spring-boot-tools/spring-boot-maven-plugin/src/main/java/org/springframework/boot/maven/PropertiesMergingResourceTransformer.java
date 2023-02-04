b03220cbaf5494df2037e4b99401c8f3e1160368
Enimiste <enimiste@users.noreply.github.com>
2023-02-04 15:05:28 +0100

    Remove inputStream.close() line to conform to the interface
    The "ReproducibleResourceTransformer" interface says that "An input stream for the resource, the implementation should *not* close this stream".

--- spring-boot-project/spring-boot-tools/spring-boot-maven-plugin/src/main/java/org/springframework/boot/maven/PropertiesMergingResourceTransformer.java
+++ spring-boot-project/spring-boot-tools/spring-boot-maven-plugin/src/main/java/org/springframework/boot/maven/PropertiesMergingResourceTransformer.java
@@ -69,7 +69,6 @@ public class PropertiesMergingResourceTransformer implements ReproducibleResourc
 			throws IOException {
 		Properties properties = new Properties();
 		properties.load(inputStream);
-		inputStream.close();
 		properties.forEach((name, value) -> process((String) name, (String) value));
 		if (time > this.time) {
 			this.time = time;
