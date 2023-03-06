27ba20f310586b769b910d70fd9bc1d6b1e59ad3
Andy Wilkinson <wilkinsona@vmware.com>
2023-03-06 10:21:21 +0000

    Test Gradle plugin against Gradle 8.0.2
    Closes gh-34486

--- spring-boot-project/spring-boot-tools/spring-boot-gradle-test-support/src/main/java/org/springframework/boot/testsupport/gradle/testkit/GradleVersions.java
+++ spring-boot-project/spring-boot-tools/spring-boot-gradle-test-support/src/main/java/org/springframework/boot/testsupport/gradle/testkit/GradleVersions.java
@@ -34,15 +34,15 @@ public final class GradleVersions {
 
 	public static List<String> allCompatible() {
 		if (isJava18()) {
-			return Arrays.asList("7.3.3", GradleVersion.current().getVersion(), "8.0.1");
+			return Arrays.asList("7.3.3", GradleVersion.current().getVersion(), "8.0.2");
 		}
 		if (isJava17()) {
-			return Arrays.asList("7.2", GradleVersion.current().getVersion(), "8.0.1");
+			return Arrays.asList("7.2", GradleVersion.current().getVersion(), "8.0.2");
 		}
 		if (isJava16()) {
-			return Arrays.asList("7.0.2", GradleVersion.current().getVersion(), "8.0.1");
+			return Arrays.asList("7.0.2", GradleVersion.current().getVersion(), "8.0.2");
 		}
-		return Arrays.asList("6.8.3", "6.9.4", "7.0.2", GradleVersion.current().getVersion(), "8.0.1");
+		return Arrays.asList("6.8.3", "6.9.4", "7.0.2", GradleVersion.current().getVersion(), "8.0.2");
 	}
 
 	public static String minimumCompatible() {
