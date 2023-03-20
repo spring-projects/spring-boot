2cbaf645b1c15deead19dce7d1a830a941110838
Andy Wilkinson <wilkinsona@vmware.com>
2023-03-20 10:04:41 +0000

    Add CI with JDK 20
    Closes gh-34175

--- ci/images/get-jdk-url.sh
+++ ci/images/get-jdk-url.sh
@@ -14,6 +14,9 @@ case "$1" in
 	java19)
 		 echo "https://github.com/bell-sw/Liberica/releases/download/19.0.2+9/bellsoft-jdk19.0.2+9-linux-amd64.tar.gz"
 	;;
+	java20)
+		 echo "https://download.java.net/java/GA/jdk20/bdc68b4b9cbc4ebcb30745c85038d91d/36/GPL/openjdk-20_linux-x64_bin.tar.gz"
+	;;
 	*)
 		echo $"Unknown java version"
 		exit 1
