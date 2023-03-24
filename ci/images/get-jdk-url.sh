04e7e70ebd4a23e3aff94227d57b9275a402fdf9
Andy Wilkinson <wilkinsona@vmware.com>
2023-03-24 15:47:06 +0000

    Remove JDK 19 CI
    Closes gh-34639

--- ci/images/get-jdk-url.sh
+++ ci/images/get-jdk-url.sh
@@ -11,9 +11,6 @@ case "$1" in
 	java17)
 		 echo "https://github.com/bell-sw/Liberica/releases/download/17.0.6+10/bellsoft-jdk17.0.6+10-linux-amd64.tar.gz"
 	;;
-	java19)
-		 echo "https://github.com/bell-sw/Liberica/releases/download/19.0.2+9/bellsoft-jdk19.0.2+9-linux-amd64.tar.gz"
-	;;
 	java20)
 		 echo "https://download.java.net/java/GA/jdk20/bdc68b4b9cbc4ebcb30745c85038d91d/36/GPL/openjdk-20_linux-x64_bin.tar.gz"
 	;;
