17a4d303f852754c0d40df51e35f38cab72cd97a
Andy Wilkinson <wilkinsona@vmware.com>
2023-03-24 15:59:45 +0000

    Switch Java 20 CI to Bellsoft Liberica
    Closes gh-34752

--- ci/images/get-jdk-url.sh
+++ ci/images/get-jdk-url.sh
@@ -12,7 +12,7 @@ case "$1" in
 		 echo "https://github.com/bell-sw/Liberica/releases/download/17.0.6+10/bellsoft-jdk17.0.6+10-linux-amd64.tar.gz"
 	;;
 	java20)
-		 echo "https://download.java.net/java/GA/jdk20/bdc68b4b9cbc4ebcb30745c85038d91d/36/GPL/openjdk-20_linux-x64_bin.tar.gz"
+		 echo "https://github.com/bell-sw/Liberica/releases/download/20+37/bellsoft-jdk20+37-linux-amd64.tar.gz"
 	;;
 	*)
 		echo $"Unknown java version"
