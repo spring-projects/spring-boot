2508eb30b5e30470afc8a73c3e27ed43d1ff259d
Eddú Meléndez <eddu.melendez@gmail.com>
2023-03-27 10:11:17 -0600

    Add service connection from Testcontainers Redpanda
    [Redpanda](https://redpanda.com/) is Kafka compatible and Testcontainers
    provides a module. `@RedpandaServiceConnection` is added providing
    `KafkaConnectionDetails`.

---
+++ spring-boot-project/spring-boot-test-autoconfigure/src/test/java/org/springframework/boot/test/autoconfigure/redpanda/RedpandaServiceConnectionTests.java
@@ -0,0 +1,89 @@
+/*
+ * Copyright 2012-2023 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.springframework.boot.test.autoconfigure.redpanda;
+
+import org.awaitility.Awaitility;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.ImportAutoConfiguration;
+import org.springframework.boot.autoconfigure.kafka.KafkaAutoConfiguration;
+import org.springframework.boot.test.autoconfigure.redpanda.RedpandaServiceConnectionTests.TestConfiguration;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.testsupport.testcontainers.DockerImageNames;
+import org.springframework.context.annotation.Bean;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
+import org.testcontainers.redpanda.RedpandaContainer;
+
+import java.time.Duration;
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+/**
+ * Tests for {@link RedpandaServiceConnection}.
+ *
+ * @author Eddú Meléndez
+ */
+@SpringBootTest(classes = TestConfiguration.class,
+		properties = { "spring.kafka.consumer.group-id=test-group",
+				"spring.kafka.consumer.auto-offset-reset=earliest" })
+@Testcontainers(disabledWithoutDocker = true)
+class RedpandaServiceConnectionTests {
+
+	@Container
+	@RedpandaServiceConnection
+	static final RedpandaContainer redpanda = new RedpandaContainer(DockerImageNames.redpanda());
+
+	@Autowired
+	KafkaTemplate<String, String> kafkaTemplate;
+
+	@Autowired
+	TestListener listener;
+
+	@Test
+	void connectionCanBeMadeToRedpandaContainer() {
+		this.kafkaTemplate.send("test-topic", "test-data");
+		Awaitility.waitAtMost(Duration.ofSeconds(30))
+			.untilAsserted(() -> assertThat(this.listener.messages).containsExactly("test-data"));
+	}
+
+	@ImportAutoConfiguration(KafkaAutoConfiguration.class)
+	static class TestConfiguration {
+
+		@Bean
+		TestListener testListener() {
+			return new TestListener();
+		}
+
+	}
+
+	static class TestListener {
+
+		private final List<String> messages = new ArrayList<>();
+
+		@KafkaListener(topics = "test-topic")
+		void processMessage(String message) {
+			this.messages.add(message);
+		}
+
+	}
+
+}
