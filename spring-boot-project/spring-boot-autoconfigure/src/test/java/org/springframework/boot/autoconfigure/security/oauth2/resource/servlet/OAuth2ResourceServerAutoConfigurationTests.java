9a4a522004a6e93c473c06043b9e64b4fffb8875
Stephane Nicoll <snicoll@vmware.com>
2023-04-12 12:29:58 +0200

    Polish "Start building against Spring Security 6.0.3 snapshots"
    See gh-34952

--- spring-boot-project/spring-boot-autoconfigure/src/test/java/org/springframework/boot/autoconfigure/security/oauth2/resource/servlet/OAuth2ResourceServerAutoConfigurationTests.java
+++ spring-boot-project/spring-boot-autoconfigure/src/test/java/org/springframework/boot/autoconfigure/security/oauth2/resource/servlet/OAuth2ResourceServerAutoConfigurationTests.java
@@ -192,7 +192,7 @@ class OAuth2ResourceServerAutoConfigurationTests {
 				assertThat(context.containsBean("jwtDecoderByIssuerUri")).isTrue();
 				SupplierJwtDecoder supplierJwtDecoderBean = context.getBean(SupplierJwtDecoder.class);
 				Supplier<JwtDecoder> jwtDecoderSupplier = (Supplier<JwtDecoder>) ReflectionTestUtils
-					.getField(supplierJwtDecoderBean, "jwtDecoderSupplier");
+					.getField(supplierJwtDecoderBean, "delegate");
 				jwtDecoderSupplier.get();
 			});
 		// The last request is to the JWK Set endpoint to look up the algorithm
@@ -216,7 +216,7 @@ class OAuth2ResourceServerAutoConfigurationTests {
 				assertThat(context.containsBean("jwtDecoderByIssuerUri")).isTrue();
 				SupplierJwtDecoder supplierJwtDecoderBean = context.getBean(SupplierJwtDecoder.class);
 				Supplier<JwtDecoder> jwtDecoderSupplier = (Supplier<JwtDecoder>) ReflectionTestUtils
-					.getField(supplierJwtDecoderBean, "jwtDecoderSupplier");
+					.getField(supplierJwtDecoderBean, "delegate");
 				jwtDecoderSupplier.get();
 			});
 		// The last request is to the JWK Set endpoint to look up the algorithm
@@ -241,7 +241,7 @@ class OAuth2ResourceServerAutoConfigurationTests {
 				assertThat(context.containsBean("jwtDecoderByIssuerUri")).isTrue();
 				SupplierJwtDecoder supplierJwtDecoderBean = context.getBean(SupplierJwtDecoder.class);
 				Supplier<JwtDecoder> jwtDecoderSupplier = (Supplier<JwtDecoder>) ReflectionTestUtils
-					.getField(supplierJwtDecoderBean, "jwtDecoderSupplier");
+					.getField(supplierJwtDecoderBean, "delegate");
 				jwtDecoderSupplier.get();
 			});
 		// The last request is to the JWK Set endpoint to look up the algorithm
@@ -516,7 +516,7 @@ class OAuth2ResourceServerAutoConfigurationTests {
 			.run((context) -> {
 				SupplierJwtDecoder supplierJwtDecoderBean = context.getBean(SupplierJwtDecoder.class);
 				Supplier<JwtDecoder> jwtDecoderSupplier = (Supplier<JwtDecoder>) ReflectionTestUtils
-					.getField(supplierJwtDecoderBean, "jwtDecoderSupplier");
+					.getField(supplierJwtDecoderBean, "delegate");
 				JwtDecoder jwtDecoder = jwtDecoderSupplier.get();
 				validate(issuerUri, jwtDecoder);
 			});
