aea5b77d1eb87fafbc0c3e00a55253209c0b36c8
Eddú Meléndez <eddu.melendez@gmail.com>
2023-04-17 07:12:34 -0500

    Add missing containers configuration reference to code snippet
    See gh-35031

--- spring-boot-project/spring-boot-docs/src/main/java/org/springframework/boot/docs/features/testing/testcontainers/atdevelopmenttime/test/TestMyApplication.java
+++ spring-boot-project/spring-boot-docs/src/main/java/org/springframework/boot/docs/features/testing/testcontainers/atdevelopmenttime/test/TestMyApplication.java
@@ -21,7 +21,7 @@ import org.springframework.boot.SpringApplication;
 public class TestMyApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.from(MyApplication::main).run(args);
+		SpringApplication.from(MyApplication::main).with(MyContainersConfiguration.class).run(args);
 	}
 
 }
