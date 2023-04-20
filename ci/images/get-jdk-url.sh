bf630bd1661b9896328f054faeea2c13e389ddf2
Stephane Nicoll <snicoll@vmware.com>
2023-04-20 14:31:26 +0200

    Upgrade Java 20 version in CI image
    Closes gh-35072

--- ci/images/get-jdk-url.sh
+++ ci/images/get-jdk-url.sh
@@ -6,7 +6,7 @@ case "$1" in
 		 echo "https://github.com/bell-sw/Liberica/releases/download/17.0.7+7/bellsoft-jdk17.0.7+7-linux-amd64.tar.gz"
 	;;
 	java20)
-		 echo "https://github.com/bell-sw/Liberica/releases/download/20+37/bellsoft-jdk20+37-linux-amd64.tar.gz"
+		 echo "https://github.com/bell-sw/Liberica/releases/download/20.0.1+10/bellsoft-jdk20.0.1+10-linux-amd64.tar.gz"
 	;;
 	*)
 		echo $"Unknown java version"
