1e785e81f81599b5d15cabed18e60bf43933092b
Andy Wilkinson <wilkinsona@vmware.com>
2023-04-25 13:06:40 +0100

    Add Docker Compose support for Oracle Database using R2DBC
    Closes gh-35143

---
+++ spring-boot-project/spring-boot-docker-compose/src/main/java/org/springframework/boot/docker/compose/service/connection/oracle/OracleEnvironment.java
@@ -0,0 +1,68 @@
+/*
+ * Copyright 2012-2023 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.springframework.boot.docker.compose.service.connection.oracle;
+
+import java.util.Map;
+
+import org.springframework.util.Assert;
+import org.springframework.util.StringUtils;
+
+/**
+ * Oracle Database environment details.
+ *
+ * @author Andy Wilkinson
+ */
+class OracleEnvironment {
+
+	private final String username;
+
+	private final String password;
+
+	private final String database;
+
+	OracleEnvironment(Map<String, String> env) {
+		this.username = env.getOrDefault("APP_USER", "system");
+		this.password = extractPassword(env);
+		this.database = env.getOrDefault("ORACLE_DATABASE", "xepdb1");
+	}
+
+	private String extractPassword(Map<String, String> env) {
+		if (env.containsKey("APP_USER")) {
+			String password = env.get("APP_PASSWORD");
+			Assert.state(StringUtils.hasLength(password), "No Oracle app password found");
+			return password;
+		}
+		Assert.state(!env.containsKey("ORACLE_RANDOM_PASSWORD"),
+				"ORACLE_RANDOM_PASSWORD is not supported without APP_USER and APP_PASSWORD");
+		String password = env.get("ORACLE_PASSWORD");
+		Assert.state(StringUtils.hasLength(password), "No Oracle password found");
+		return password;
+	}
+
+	String getUsername() {
+		return this.username;
+	}
+
+	String getPassword() {
+		return this.password;
+	}
+
+	String getDatabase() {
+		return this.database;
+	}
+
+}
---
+++ spring-boot-project/spring-boot-docker-compose/src/test/java/org/springframework/boot/docker/compose/service/connection/oracle/OracleR2dbcDockerComposeConnectionDetailsFactoryIntegrationTests.java
@@ -0,0 +1,56 @@
+/*
+ * Copyright 2012-2023 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.springframework.boot.docker.compose.service.connection.oracle;
+
+import io.r2dbc.spi.ConnectionFactories;
+import io.r2dbc.spi.ConnectionFactoryOptions;
+import org.junit.jupiter.api.Test;
+
+import org.springframework.boot.autoconfigure.r2dbc.R2dbcConnectionDetails;
+import org.springframework.boot.docker.compose.service.connection.test.AbstractDockerComposeIntegrationTests;
+import org.springframework.boot.jdbc.DatabaseDriver;
+import org.springframework.r2dbc.core.DatabaseClient;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+/**
+ * Integration tests for {@link OracleR2dbcDockerComposeConnectionDetailsFactory}
+ *
+ * @author Andy Wilkinson
+ */
+class OracleR2dbcDockerComposeConnectionDetailsFactoryIntegrationTests extends AbstractDockerComposeIntegrationTests {
+
+	OracleR2dbcDockerComposeConnectionDetailsFactoryIntegrationTests() {
+		super("oracle-compose.yaml");
+	}
+
+	@Test
+	void runCreatesConnectionDetailsThatCanBeUsedToAccessDatabase() {
+		R2dbcConnectionDetails connectionDetails = run(R2dbcConnectionDetails.class);
+		ConnectionFactoryOptions connectionFactoryOptions = connectionDetails.getConnectionFactoryOptions();
+		assertThat(connectionFactoryOptions.toString()).contains("database=xepdb1", "driver=oracle",
+				"password=REDACTED", "user=system");
+		assertThat(connectionFactoryOptions.getRequiredValue(ConnectionFactoryOptions.PASSWORD)).isEqualTo("secret");
+		Object result = DatabaseClient.create(ConnectionFactories.get(connectionFactoryOptions))
+			.sql(DatabaseDriver.ORACLE.getValidationQuery())
+			.map((row, metadata) -> row.get(0))
+			.first()
+			.block();
+		assertThat(result).isEqualTo("Hello");
+	}
+
+}
