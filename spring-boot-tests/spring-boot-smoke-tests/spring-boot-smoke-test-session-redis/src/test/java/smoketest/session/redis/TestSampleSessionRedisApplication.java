eabce52b93037b72fade567475a316ee858f30e4
Phillip Webb <pwebb@vmware.com>
2023-05-01 22:06:43 -0700

    Fix TestSampleSessionRedisApplication
    Update the `@ServiceConnection` annotation to include the
    service connection name.
    
    See gh-35168

--- spring-boot-tests/spring-boot-smoke-tests/spring-boot-smoke-test-session-redis/src/test/java/smoketest/session/redis/TestSampleSessionRedisApplication.java
+++ spring-boot-tests/spring-boot-smoke-tests/spring-boot-smoke-test-session-redis/src/test/java/smoketest/session/redis/TestSampleSessionRedisApplication.java
@@ -26,7 +26,7 @@ import org.springframework.context.annotation.Bean;
 public class TestSampleSessionRedisApplication {
 
 	@Bean
-	@ServiceConnection
+	@ServiceConnection("redis")
 	RedisContainer redisContainer() {
 		return new RedisContainer();
 	}
