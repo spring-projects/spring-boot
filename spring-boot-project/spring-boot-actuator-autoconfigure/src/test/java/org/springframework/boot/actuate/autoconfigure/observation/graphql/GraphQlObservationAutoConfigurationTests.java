1b4f9a7e9c45323ec66de2a9bc7b2dabffe8b9d9
Stephane Nicoll <snicoll@vmware.com>
2023-05-05 12:07:12 +0200

    Remove GraphQL's tracing observation auto-configuration
    This commit removes the auto-configuration for
    PropagationWebGraphQlInterceptor as it has been deprecated with no
    replacement.
    
    See gh-35303

--- spring-boot-project/spring-boot-actuator-autoconfigure/src/test/java/org/springframework/boot/actuate/autoconfigure/observation/graphql/GraphQlObservationAutoConfigurationTests.java
+++ spring-boot-project/spring-boot-actuator-autoconfigure/src/test/java/org/springframework/boot/actuate/autoconfigure/observation/graphql/GraphQlObservationAutoConfigurationTests.java
@@ -18,7 +18,6 @@ package org.springframework.boot.actuate.autoconfigure.observation.graphql;
 
 import io.micrometer.observation.ObservationRegistry;
 import io.micrometer.observation.tck.TestObservationRegistry;
-import io.micrometer.tracing.propagation.Propagator;
 import org.junit.jupiter.api.Test;
 
 import org.springframework.boot.autoconfigure.AutoConfigurations;
@@ -28,7 +27,6 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.graphql.observation.DefaultDataFetcherObservationConvention;
 import org.springframework.graphql.observation.DefaultExecutionRequestObservationConvention;
 import org.springframework.graphql.observation.GraphQlObservationInstrumentation;
-import org.springframework.graphql.observation.PropagationWebGraphQlInterceptor;
 import org.springframework.graphql.server.WebGraphQlHandler;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -76,24 +74,6 @@ class GraphQlObservationAutoConfigurationTests {
 		});
 	}
 
-	@Test
-	void propagationInterceptorNotContributedWhenPropagatorIsMissing() {
-		this.contextRunner.withUserConfiguration(WebGraphQlConfiguration.class)
-			.run((context) -> assertThat(context).doesNotHaveBean(PropagationWebGraphQlInterceptor.class));
-	}
-
-	@Test
-	void propagationInterceptorNotContributedWhenNotWebApplication() {
-		this.contextRunner.withUserConfiguration(TracingConfiguration.class)
-			.run((context) -> assertThat(context).doesNotHaveBean(PropagationWebGraphQlInterceptor.class));
-	}
-
-	@Test
-	void propagationInterceptorContributed() {
-		this.contextRunner.withUserConfiguration(WebGraphQlConfiguration.class, TracingConfiguration.class)
-			.run((context) -> assertThat(context).hasSingleBean(PropagationWebGraphQlInterceptor.class));
-	}
-
 	@Configuration(proxyBeanMethods = false)
 	static class InstrumentationConfiguration {
 
@@ -133,16 +113,6 @@ class GraphQlObservationAutoConfigurationTests {
 		@Bean
 		WebGraphQlHandler webGraphQlHandler() {
 			return mock(WebGraphQlHandler.class);
-		}
-
-	}
-
-	@Configuration(proxyBeanMethods = false)
-	static class TracingConfiguration {
-
-		@Bean
-		Propagator propagator() {
-			return mock(Propagator.class);
 		}
 
 	}
