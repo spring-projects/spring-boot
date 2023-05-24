44c209009206d9d52b546e002d5b19afa4072d0f
Stephane Nicoll <snicoll@vmware.com>
2023-05-24 13:28:01 +0200

    Polish "Fix description of build-image[-no-fork] Maven plugin goals"
    See gh-35609

--- spring-boot-project/spring-boot-tools/spring-boot-maven-plugin/src/main/java/org/springframework/boot/maven/BuildImageNoForkMojo.java
+++ spring-boot-project/spring-boot-tools/spring-boot-maven-plugin/src/main/java/org/springframework/boot/maven/BuildImageNoForkMojo.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2012-2022 the original author or authors.
+ * Copyright 2012-2023 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -22,9 +22,8 @@ import org.apache.maven.plugins.annotations.ResolutionScope;
 
 /**
  * Package an application into an OCI image using a buildpack, but without forking the
- * lifecycle to make sure that {@code package} ran. This goal should be used when
- * configuring a goal {@code execution} in your build. To invoke the goal on the
- * command-line, use {@code build-image} instead.
+ * lifecycle. This goal should be used when configuring a goal {@code execution} in your
+ * build. To invoke the goal on the command-line, use {@code build-image} instead.
  *
  * @author Stephane Nicoll
  * @since 3.0.0
