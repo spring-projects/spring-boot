2d2f0502623232d994b7f0ed40d99032838fe745
Phillip Webb <pwebb@vmware.com>
2023-07-05 10:42:06 +0100

    Polish 'Add initial support for RestClient'
    See gh-36213

--- spring-boot-project/spring-boot-autoconfigure/src/main/java/org/springframework/boot/autoconfigure/web/client/RestClientAutoConfiguration.java
+++ spring-boot-project/spring-boot-autoconfigure/src/main/java/org/springframework/boot/autoconfigure/web/client/RestClientAutoConfiguration.java
@@ -21,9 +21,9 @@ import org.springframework.boot.autoconfigure.AutoConfiguration;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
-import org.springframework.boot.autoconfigure.condition.ConditionalOnWebApplication;
-import org.springframework.boot.autoconfigure.condition.NoneNestedConditions;
 import org.springframework.boot.autoconfigure.http.HttpMessageConvertersAutoConfiguration;
+import org.springframework.boot.web.client.ClientHttpRequestFactories;
+import org.springframework.boot.web.client.ClientHttpRequestFactorySettings;
 import org.springframework.boot.web.client.RestClientCustomizer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Conditional;
@@ -33,38 +33,26 @@ import org.springframework.web.client.RestClient;
 /**
  * {@link EnableAutoConfiguration Auto-configuration} for {@link RestClient}.
  * <p>
- * This will produce a {@link RestClient.Builder RestClient.Builder} bean with the
- * {@code prototype} scope, meaning each injection point will receive a newly cloned
- * instance of the builder.
+ * This will produce a {@link org.springframework.web.client.RestClient.Builder
+ * RestClient.Builder} bean with the {@code prototype} scope, meaning each injection point
+ * will receive a newly cloned instance of the builder.
  *
  * @author Arjen Poutsma
  * @since 3.2.0
  */
 @AutoConfiguration(after = HttpMessageConvertersAutoConfiguration.class)
 @ConditionalOnClass(RestClient.class)
-@Conditional(RestClientAutoConfiguration.NotReactiveWebApplicationCondition.class)
+@Conditional(NotReactiveWebApplicationCondition.class)
 public class RestClientAutoConfiguration {
 
 	@Bean
 	@Scope("prototype")
 	@ConditionalOnMissingBean
 	public RestClient.Builder webClientBuilder(ObjectProvider<RestClientCustomizer> customizerProvider) {
-		RestClient.Builder builder = RestClient.builder();
+		RestClient.Builder builder = RestClient.builder()
+			.requestFactory(ClientHttpRequestFactories.get(ClientHttpRequestFactorySettings.DEFAULTS));
 		customizerProvider.orderedStream().forEach((customizer) -> customizer.customize(builder));
 		return builder;
-	}
-
-	static class NotReactiveWebApplicationCondition extends NoneNestedConditions {
-
-		NotReactiveWebApplicationCondition() {
-			super(ConfigurationPhase.PARSE_CONFIGURATION);
-		}
-
-		@ConditionalOnWebApplication(type = ConditionalOnWebApplication.Type.REACTIVE)
-		private static class ReactiveWebApplication {
-
-		}
-
 	}
 
 }
