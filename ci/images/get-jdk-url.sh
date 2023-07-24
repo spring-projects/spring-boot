9112b581d6ad797d6485a2f3af940b89009b394c
Stephane Nicoll <snicoll@vmware.com>
2023-07-24 12:39:04 +0200

    Upgrade Java 8 version in CI image and .sdkmanrc
    Closes gh-36461

--- ci/images/get-jdk-url.sh
+++ ci/images/get-jdk-url.sh
@@ -3,7 +3,7 @@ set -e
 
 case "$1" in
 	java8)
-		 echo "https://github.com/bell-sw/Liberica/releases/download/8u372+7/bellsoft-jdk8u372+7-linux-amd64.tar.gz"
+		 echo "https://github.com/bell-sw/Liberica/releases/download/8u382+6/bellsoft-jdk8u382+6-linux-amd64.tar.gz"
 	;;
 	java11)
 		 echo "https://github.com/bell-sw/Liberica/releases/download/11.0.19+7/bellsoft-jdk11.0.19+7-linux-amd64.tar.gz"
