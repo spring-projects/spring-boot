eb467f64bb9ffac66d0a111fbcc809077a60b4f2
Stephane Nicoll <snicoll@vmware.com>
2023-07-24 12:42:55 +0200

    Upgrade Java 17 version in CI image and .sdkmanrc
    Closes gh-36457

--- ci/images/get-jdk-url.sh
+++ ci/images/get-jdk-url.sh
@@ -3,7 +3,7 @@ set -e
 
 case "$1" in
 	java17)
-		 echo "https://github.com/bell-sw/Liberica/releases/download/17.0.7+7/bellsoft-jdk17.0.7+7-linux-amd64.tar.gz"
+		 echo "https://github.com/bell-sw/Liberica/releases/download/17.0.8+7/bellsoft-jdk17.0.8+7-linux-amd64.tar.gz"
 	;;
 	java20)
 		 echo "https://github.com/bell-sw/Liberica/releases/download/20.0.1+10/bellsoft-jdk20.0.1+10-linux-amd64.tar.gz"
