726d2e66786ad8463cfc54b3782aa58992b35475
Moritz Halbritter <mkammerer@vmware.com>
2023-08-03 17:33:52 +0200

    Remove unnecessary synchronization
    - on AtomicBoolean in SpringApplicationBuilder
    - on SimpleFormatter
    - in a private method in FileSystemWatcher which is always called in a
      synchronized block
    - Replaced synchronized guarded HashMap with ConcurrentHashMap

--- spring-boot-project/spring-boot-devtools/src/main/java/org/springframework/boot/devtools/filewatch/FileSystemWatcher.java
+++ spring-boot-project/spring-boot-devtools/src/main/java/org/springframework/boot/devtools/filewatch/FileSystemWatcher.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2012-2022 the original author or authors.
+ * Copyright 2012-2023 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -158,9 +158,7 @@ public class FileSystemWatcher {
 	}
 
 	private void checkNotStarted() {
-		synchronized (this.monitor) {
-			Assert.state(this.watchThread == null, "FileSystemWatcher already started");
-		}
+		Assert.state(this.watchThread == null, "FileSystemWatcher already started");
 	}
 
 	/**
