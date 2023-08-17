0f6342a8821ee6c1ca06439bda8f433d8ee2e331
Moritz Halbritter <mkammerer@vmware.com>
2023-08-17 18:09:53 +0200

    Make JarLaunchScript and SysVinit integration tests ARM64 compatible
    Closes gh-36799

--- spring-boot-tests/spring-boot-integration-tests/spring-boot-launch-script-tests/src/intTest/java/org/springframework/boot/launchscript/AbstractLaunchScriptIntegrationTests.java
+++ spring-boot-tests/spring-boot-integration-tests/spring-boot-launch-script-tests/src/intTest/java/org/springframework/boot/launchscript/AbstractLaunchScriptIntegrationTests.java
@@ -17,9 +17,14 @@
 package org.springframework.boot.launchscript;
 
 import java.io.File;
+import java.net.URI;
+import java.nio.file.Paths;
 import java.time.Duration;
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.function.Predicate;
 
 import org.assertj.core.api.Condition;
@@ -43,8 +48,19 @@ import static org.hamcrest.Matchers.containsString;
  * @author Andy Wilkinson
  * @author Ali Shahbour
  * @author Alexey Vinogradov
+ * @author Moritz Halbritter
  */
 abstract class AbstractLaunchScriptIntegrationTests {
+
+	private static final Map<Architecture, URI> JAVA_DOWNLOAD_URLS;
+	static {
+		Map<Architecture, URI> urls = new HashMap<>();
+		urls.put(Architecture.AMD64,
+				URI.create("https://download.bell-sw.com/java/8u382+6/bellsoft-jdk8u382+6-linux-amd64.tar.gz"));
+		urls.put(Architecture.AARCH64,
+				URI.create("https://download.bell-sw.com/java/8u382+6/bellsoft-jdk8u382+6-linux-aarch64.tar.gz"));
+		JAVA_DOWNLOAD_URLS = Collections.unmodifiableMap(urls);
+	}
 
 	protected static final char ESC = 27;
 
@@ -100,8 +116,8 @@ abstract class AbstractLaunchScriptIntegrationTests {
 
 		private LaunchScriptTestContainer(String os, String version, String scriptsDir, String testScript) {
 			super(new ImageFromDockerfile("spring-boot-launch-script/" + os.toLowerCase() + "-" + version)
-				.withFileFromFile("Dockerfile",
-						new File("src/intTest/resources/conf/" + os + "/" + version + "/Dockerfile")));
+				.withDockerfile(Paths.get("src/intTest/resources/conf/" + os + "/" + version + "/Dockerfile"))
+				.withBuildArg("JAVA_DOWNLOAD_URL", getJavaDownloadUrl()));
 			withCopyFileToContainer(MountableFile.forHostPath(findApplication().getAbsolutePath()), "/app.jar");
 			withCopyFileToContainer(
 					MountableFile.forHostPath("src/intTest/resources/scripts/" + scriptsDir + "test-functions.sh"),
@@ -114,11 +130,46 @@ abstract class AbstractLaunchScriptIntegrationTests {
 			withStartupCheckStrategy(new OneShotStartupCheckStrategy().withTimeout(Duration.ofMinutes(5)));
 		}
 
+		private static String getJavaDownloadUrl() {
+			Architecture architecture = Architecture.current();
+			Assert.notNull(architecture,
+					() -> String.format("Failed to find current architecture. Value of os.arch is: '%s'",
+							System.getProperty("os.arch")));
+			URI uri = JAVA_DOWNLOAD_URLS.get(architecture);
+			Assert.notNull(uri, () -> String.format("No JDK download URL for architecture %s found", architecture));
+			return uri.toString();
+		}
+
 		private static File findApplication() {
 			String name = String.format("build/%1$s/build/libs/%1$s.jar", "spring-boot-launch-script-tests-app");
 			File jar = new File(name);
 			Assert.state(jar.isFile(), () -> "Could not find " + name + ". Have you built it?");
 			return jar;
+		}
+
+	}
+
+	private enum Architecture {
+
+		AMD64, AARCH64;
+
+		/**
+		 * Returns the current architecture.
+		 * @return the current architecture or {@code null}
+		 */
+		static Architecture current() {
+			String arch = System.getProperty("os.arch");
+			if (arch == null) {
+				return null;
+			}
+			switch (arch) {
+				case "amd64":
+					return AMD64;
+				case "aarch64":
+					return AARCH64;
+				default:
+					return null;
+			}
 		}
 
 	}
