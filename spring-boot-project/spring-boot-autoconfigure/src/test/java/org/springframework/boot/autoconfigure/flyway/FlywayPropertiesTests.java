19df3934c61d94f414aa40f380954e51f83bac7d
Stephane Nicoll <snicoll@vmware.com>
2023-08-23 10:19:47 +0200

    Upgrade to Flyway 9.21.2
    Closes gh-37076

--- spring-boot-project/spring-boot-autoconfigure/src/test/java/org/springframework/boot/autoconfigure/flyway/FlywayPropertiesTests.java
+++ spring-boot-project/spring-boot-autoconfigure/src/test/java/org/springframework/boot/autoconfigure/flyway/FlywayPropertiesTests.java
@@ -114,6 +114,8 @@ class FlywayPropertiesTests {
 				"oracleWalletLocation", "sqlServerKerberosLoginFile");
 		// Properties that are managed by specific extensions
 		ignoreProperties(properties, "oracle", "postgresql", "sqlserver");
+		// https://github.com/flyway/flyway/issues/3732
+		ignoreProperties(configuration, "environment");
 		// High level object we can't set with properties
 		ignoreProperties(configuration, "callbacks", "classLoader", "dataSource", "javaMigrations",
 				"javaMigrationClassProvider", "pluginRegister", "resourceProvider", "resolvers");
