e3e578f78a02a1d8f42a000515aab25465f44306
Andy Wilkinson <wilkinsona@vmware.com>
2023-08-31 10:31:25 +0100

    Upgrade Java 11 CI image to 11.0.20.1+1
    Closes gh-37150

--- ci/images/get-jdk-url.sh
+++ ci/images/get-jdk-url.sh
@@ -6,7 +6,7 @@ case "$1" in
 		 echo "https://github.com/bell-sw/Liberica/releases/download/8u382+6/bellsoft-jdk8u382+6-linux-amd64.tar.gz"
 	;;
 	java11)
-		 echo "https://github.com/bell-sw/Liberica/releases/download/11.0.20+8/bellsoft-jdk11.0.20+8-linux-amd64.tar.gz"
+		 echo "https://github.com/bell-sw/Liberica/releases/download/11.0.20.1+1/bellsoft-jdk11.0.20.1+1-linux-amd64.tar.gz"
 	;;
 	java17)
 		 echo "https://github.com/bell-sw/Liberica/releases/download/17.0.8+7/bellsoft-jdk17.0.8+7-linux-amd64.tar.gz"
