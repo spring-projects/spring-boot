a52d7e255b72360e4391dd3fe9e8f600adba9c42
Stephane Nicoll <snicoll@vmware.com>
2023-09-11 10:20:00 +0200

    Upgrade to Couchbase Client 3.4.10
    Closes gh-37289

--- spring-boot-project/spring-boot-autoconfigure/src/main/java/org/springframework/boot/autoconfigure/couchbase/CouchbaseProperties.java
+++ spring-boot-project/spring-boot-autoconfigure/src/main/java/org/springframework/boot/autoconfigure/couchbase/CouchbaseProperties.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2012-2020 the original author or authors.
+ * Copyright 2012-2023 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -117,7 +117,7 @@ public class CouchbaseProperties {
 		 * Length of time an HTTP connection may remain idle before it is closed and
 		 * removed from the pool.
 		 */
-		private Duration idleHttpConnectionTimeout = Duration.ofMillis(4500);
+		private Duration idleHttpConnectionTimeout = Duration.ofSeconds(1);
 
 		public int getMinEndpoints() {
 			return this.minEndpoints;
