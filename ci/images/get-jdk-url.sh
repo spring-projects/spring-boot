75233e9865e29c15ad4947fcec664d3bf003dbc3
Andy Wilkinson <wilkinsona@vmware.com>
2023-09-13 10:42:46 +0100

    Set up CI with JDK 21
    Closes gh-37359

--- ci/images/get-jdk-url.sh
+++ ci/images/get-jdk-url.sh
@@ -14,6 +14,9 @@ case "$1" in
 	java20)
 		 echo "https://github.com/bell-sw/Liberica/releases/download/20.0.2+10/bellsoft-jdk20.0.2+10-linux-amd64.tar.gz"
 	;;
+	java21)
+		 echo "https://download.java.net/java/early_access/jdk21/25/GPL/openjdk-21-ea+25_linux-x64_bin.tar.gz"
+	;;
 	*)
 		echo $"Unknown java version"
 		exit 1
