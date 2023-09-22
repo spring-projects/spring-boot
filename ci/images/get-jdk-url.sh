3f9a04331cc30d5c5ec917cef5cb5687824681e6
Moritz Halbritter <mkammerer@vmware.com>
2023-09-22 09:03:35 +0200

    Remove Java 20 from CI
    Closes gh-37527

--- ci/images/get-jdk-url.sh
+++ ci/images/get-jdk-url.sh
@@ -11,9 +11,6 @@ case "$1" in
 	java17)
 		 echo "https://github.com/bell-sw/Liberica/releases/download/17.0.8.1+1/bellsoft-jdk17.0.8.1+1-linux-amd64.tar.gz"
 	;;
-	java20)
-		 echo "https://github.com/bell-sw/Liberica/releases/download/20.0.2+10/bellsoft-jdk20.0.2+10-linux-amd64.tar.gz"
-	;;
 	java21)
 		 echo "https://github.com/bell-sw/Liberica/releases/download/21+37/bellsoft-jdk21+37-linux-amd64.tar.gz"
 	;;
