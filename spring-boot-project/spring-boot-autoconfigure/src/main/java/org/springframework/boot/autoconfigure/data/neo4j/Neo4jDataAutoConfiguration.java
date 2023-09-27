6b107530f521b61c8057a108c3ec6b2ff053ac37
Andy Wilkinson <wilkinsona@vmware.com>
2023-09-27 09:32:53 +0100

    Polish "Use Neo4jManagedTypes to populate the mapping context"
    See gh-37574

--- spring-boot-project/spring-boot-autoconfigure/src/main/java/org/springframework/boot/autoconfigure/data/neo4j/Neo4jDataAutoConfiguration.java
+++ spring-boot-project/spring-boot-autoconfigure/src/main/java/org/springframework/boot/autoconfigure/data/neo4j/Neo4jDataAutoConfiguration.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2012-2022 the original author or authors.
+ * Copyright 2012-2023 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -81,7 +81,6 @@ public class Neo4jDataAutoConfiguration {
 	@Bean
 	@ConditionalOnMissingBean
 	public Neo4jMappingContext neo4jMappingContext(Neo4jManagedTypes managedTypes, Neo4jConversions neo4jConversions) {
-
 		Neo4jMappingContext context = new Neo4jMappingContext(neo4jConversions);
 		context.setManagedTypes(managedTypes);
 		return context;
