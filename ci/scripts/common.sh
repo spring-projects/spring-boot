f04d354c291e9db25f969ddeffe4066ff58fea6d
Phillip Webb <pwebb@vmware.com>
2023-10-10 11:46:20 -0700

    Attempt to fix download cache on CI
    See gh-37450

--- ci/scripts/common.sh
+++ ci/scripts/common.sh
@@ -1,5 +1,9 @@
 source /opt/concourse-java.sh
 
+if [[ -d $PWD/gradle ]]; then
+	EXPORT GRADLE_USER_HOMR=`pwd`/gradle
+fi
+
 setup_symlinks
 if [[ -d $PWD/embedmongo && ! -d $HOME/.embedmongo ]]; then
 	ln -s "$PWD/embedmongo" "$HOME/.embedmongo"
