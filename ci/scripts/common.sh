fec9804dad83ad58440b0c9326631364a1c4a5f3
Phillip Webb <pwebb@vmware.com>
2023-10-10 12:08:44 -0700

    Don't create .gradle folder early
    See gh-37450

--- ci/scripts/common.sh
+++ ci/scripts/common.sh
@@ -1,12 +1,10 @@
 source /opt/concourse-java.sh
 
-if [[ -d $PWD/gradle ]]; then
-	export GRADLE_USER_HOME=`pwd`/gradle
-fi
-
 setup_symlinks
 if [[ -d $PWD/embedmongo && ! -d $HOME/.embedmongo ]]; then
 	ln -s "$PWD/embedmongo" "$HOME/.embedmongo"
 fi
 
 cleanup_maven_repo "org.springframework.boot"
+
+echo 'systemProp.user.name=concourse' > ~/.gradle/gradle.properties
