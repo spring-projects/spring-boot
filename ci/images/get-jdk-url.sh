b226ff40f09710d3eee87c4b9f099628c14e4512
Phillip Webb <pwebb@vmware.com>
2023-10-18 21:03:47 -0700

    Upgrade to Java 21.0.1+12
    Closes gh-37948

--- ci/images/get-jdk-url.sh
+++ ci/images/get-jdk-url.sh
@@ -12,7 +12,7 @@ case "$1" in
 		 echo "https://github.com/bell-sw/Liberica/releases/download/17.0.9+11/bellsoft-jdk17.0.9+11-linux-amd64.tar.gz"
 	;;
 	java21)
-		 echo "https://github.com/bell-sw/Liberica/releases/download/21+37/bellsoft-jdk21+37-linux-amd64.tar.gz"
+		 echo "https://github.com/bell-sw/Liberica/releases/download/21.0.1+12/bellsoft-jdk21.0.1+12-linux-amd64.tar.gz"
 	;;
 	*)
 		echo $"Unknown java version"
