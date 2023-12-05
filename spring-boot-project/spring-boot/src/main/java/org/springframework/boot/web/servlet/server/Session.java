b5de38787c6abd4fc6522aa006b3d460ae23ffc5
Phillip Webb <phil.webb@broadcom.com>
2023-12-05 14:57:33 -0800

    Restore `Session.Cookie` class for binary back-compatibility
    Fixes gh-38589

--- spring-boot-project/spring-boot/src/main/java/org/springframework/boot/web/servlet/server/Session.java
+++ spring-boot-project/spring-boot/src/main/java/org/springframework/boot/web/servlet/server/Session.java
@@ -23,7 +23,6 @@ import java.util.Set;
 
 import org.springframework.boot.context.properties.NestedConfigurationProperty;
 import org.springframework.boot.convert.DurationUnit;
-import org.springframework.boot.web.server.Cookie;
 
 /**
  * Session properties.
@@ -101,6 +100,15 @@ public class Session {
 
 	SessionStoreDirectory getSessionStoreDirectory() {
 		return this.sessionStoreDirectory;
+	}
+
+	/**
+	 * Session cookie properties. This class is provided only for back-compatibility
+	 * reasons, consider using {@link org.springframework.boot.web.server.Cookie} whever
+	 * possible.
+	 */
+	public static class Cookie extends org.springframework.boot.web.server.Cookie {
+
 	}
 
 	/**
