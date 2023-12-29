28f6fa832945f21c8df2f0c0c646f3c83dd7e8e0
Fabrice Bibonne <fabrice.bibonne@insee.fr>
2023-12-29 14:03:22 +0100

    fix(error mark position for PatternParseException)
    PatternParseException.toDetailedString() return a String
    with a mark to specify the error position in the pattern.
    The mark takes place in the second line in the String
    returned. Because PatternParseFailureAnalyzer.analyze
    appended "Invalid mapping pattern detected:" at the
    beginning of the returned String, the mark was not well
    positioned.
    
    Now, a "\n" is inserted after "Invalid mapping pattern detected:"
    and the mark is well positioned

--- spring-boot-project/spring-boot/src/test/java/org/springframework/boot/diagnostics/analyzer/PatternParseFailureAnalyzerTests.java
+++ spring-boot-project/spring-boot/src/test/java/org/springframework/boot/diagnostics/analyzer/PatternParseFailureAnalyzerTests.java
@@ -36,10 +36,14 @@ class PatternParseFailureAnalyzerTests {
 	@Test
 	void patternParseFailureQuotesPattern() {
 		FailureAnalysis failureAnalysis = performAnalysis("/spring/**/framework");
-		assertThat(failureAnalysis.getDescription()).contains("Invalid mapping pattern detected: /spring/**/framework");
+		assertThat(failureAnalysis.getDescription()).contains("""
+				Invalid mapping pattern detected:
+				/spring/**/framework
+				       ^
+				""");
 		assertThat(failureAnalysis.getAction())
-			.contains("Fix this pattern in your application or switch to the legacy parser"
-					+ " implementation with 'spring.mvc.pathmatch.matching-strategy=ant_path_matcher'.");
+				.contains("Fix this pattern in your application or switch to the legacy parser"
+						+ " implementation with 'spring.mvc.pathmatch.matching-strategy=ant_path_matcher'.");
 	}
 
 	private FailureAnalysis performAnalysis(String pattern) {
