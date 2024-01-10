47e8f6168f73688e2e62651e9bd401362b68bb81
Moritz Halbritter <moritz.halbritter@broadcom.com>
2024-01-10 09:21:03 +0100

    Polish "Fix error mark position for PatternParseException"
    See gh-38944

--- spring-boot-project/spring-boot/src/test/java/org/springframework/boot/diagnostics/analyzer/PatternParseFailureAnalyzerTests.java
+++ spring-boot-project/spring-boot/src/test/java/org/springframework/boot/diagnostics/analyzer/PatternParseFailureAnalyzerTests.java
@@ -36,14 +36,11 @@ class PatternParseFailureAnalyzerTests {
 	@Test
 	void patternParseFailureQuotesPattern() {
 		FailureAnalysis failureAnalysis = performAnalysis("/spring/**/framework");
-		assertThat(failureAnalysis.getDescription()).contains("""
-				Invalid mapping pattern detected:
-				/spring/**/framework
-				       ^
-				""");
+		assertThat(failureAnalysis.getDescription())
+			.contains("Invalid mapping pattern detected:\n" + "/spring/**/framework\n" + "       ^");
 		assertThat(failureAnalysis.getAction())
-				.contains("Fix this pattern in your application or switch to the legacy parser"
-						+ " implementation with 'spring.mvc.pathmatch.matching-strategy=ant_path_matcher'.");
+			.contains("Fix this pattern in your application or switch to the legacy parser"
+					+ " implementation with 'spring.mvc.pathmatch.matching-strategy=ant_path_matcher'.");
 	}
 
 	private FailureAnalysis performAnalysis(String pattern) {
