8da4351182a50d6c8a7f32f2f1fc71a45418fb85
Siddharth1410 <Siddharth1410@gmail.com>
2024-01-18 01:09:31 -0600

    Upgrade Java 17 version in CI image and .sdkmanrc

--- ci/images/get-jdk-url.sh
+++ ci/images/get-jdk-url.sh
@@ -3,7 +3,7 @@ set -e
 
 case "$1" in
 	java17)
-		 echo "https://github.com/bell-sw/Liberica/releases/download/17.0.9+11/bellsoft-jdk17.0.9+11-linux-amd64.tar.gz"
+		 echo "https://github.com/bell-sw/Liberica/releases/download/17.0.10+13/bellsoft-jdk17.0.10+13-linux-amd64.tar.gz"
 	;;
 	java21)
 	   echo "https://github.com/bell-sw/Liberica/releases/download/21.0.2+14/bellsoft-jdk21.0.2+14-linux-amd64.tar.gz"
