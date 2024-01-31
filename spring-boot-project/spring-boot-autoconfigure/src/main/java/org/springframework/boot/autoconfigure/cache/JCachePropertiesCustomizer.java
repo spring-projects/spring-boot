622c9ed8828ce26d166b0fb8f3c94c06bcb1d910
Andy Wilkinson <andy.wilkinson@broadcom.com>
2024-01-31 10:41:30 +0000

    Enable customization of properties used to create JCache CacheManager

--- spring-boot-project/spring-boot-autoconfigure/src/main/java/org/springframework/boot/autoconfigure/cache/JCachePropertiesCustomizer.java
+++ spring-boot-project/spring-boot-autoconfigure/src/main/java/org/springframework/boot/autoconfigure/cache/JCachePropertiesCustomizer.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2012-2019 the original author or authors.
+ * Copyright 2012-2024 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -25,16 +25,17 @@ import javax.cache.spi.CachingProvider;
  * Callback interface that can be implemented by beans wishing to customize the properties
  * used by the {@link CachingProvider} to create the {@link CacheManager}.
  *
+ * @see CachingProvider#getCacheManager(java.net.URI, ClassLoader, Properties)
  * @author Stephane Nicoll
+ * @since 3.3.0
  */
-interface JCachePropertiesCustomizer {
+public interface JCachePropertiesCustomizer {
 
 	/**
 	 * Customize the properties.
-	 * @param cacheProperties the cache properties
 	 * @param properties the current properties
-	 * @see CachingProvider#getCacheManager(java.net.URI, ClassLoader, Properties)
+	 *
 	 */
-	void customize(CacheProperties cacheProperties, Properties properties);
+	void customize(Properties properties);
 
 }
