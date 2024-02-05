8c21a40be7c443abcc822ac172d35cd39517b87c
archana <karchana@virtusa.com>
2024-02-05 13:30:44 +0530

    Trying to contribute

--- spring-boot-project/spring-boot-tools/spring-boot-gradle-plugin/src/docs/gradle/getting-started/snapshot-settings.gradle.kts
+++
@@ -1,7 +0,0 @@
-pluginManagement {
-	repositories {
-		maven { url = uri("https://repo.spring.io/milestone") }
-		maven { url = uri("https://repo.spring.io/snapshot") }
-		gradlePluginPortal()
-	}
-}
--- spring-boot-tests/spring-boot-integration-tests/spring-boot-launch-script-tests/src/intTest/resources/scripts/init.d/restart-when-stopped.sh
+++
@@ -1,5 +0,0 @@
-source ./test-functions.sh
-install_service
-restart_service
-echo "Status: $?"
-echo "PID: $(cat /var/run/spring-boot-app/spring-boot-app.pid)"
