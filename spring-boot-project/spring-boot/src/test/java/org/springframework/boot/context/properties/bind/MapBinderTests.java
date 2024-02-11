f90f29dd12020d9643da87587242e7086902cba4
Scott Frederick <scott.frederick@broadcom.com>
2024-02-11 15:05:48 -0600

    Polish "Resolve property placeholders when binding properties to a Map"
    See gh-39507

--- spring-boot-project/spring-boot/src/test/java/org/springframework/boot/context/properties/bind/MapBinderTests.java
+++ spring-boot-project/spring-boot/src/test/java/org/springframework/boot/context/properties/bind/MapBinderTests.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2012-2023 the original author or authors.
+ * Copyright 2012-2024 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -615,7 +615,8 @@ class MapBinderTests {
 		DefaultConversionService conversionService = new DefaultConversionService();
 		conversionService.addConverter(new MapConverter());
 		StandardEnvironment environment = new StandardEnvironment();
-		Binder binder = new Binder(this.sources, new PropertySourcesPlaceholdersResolver(environment), conversionService, null, null);
+		Binder binder = new Binder(this.sources, new PropertySourcesPlaceholdersResolver(environment),
+				conversionService, null, null);
 		TestPropertySourceUtils.addInlinedPropertiesToEnvironment(environment, "bar=bc");
 		this.sources.add(new MockConfigurationPropertySource("foo", "a${bar},${bar}d"));
 		Map<String, String> map = binder.bind("foo", STRING_STRING_MAP).get();
