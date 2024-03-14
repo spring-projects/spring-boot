b3ddb22f750143ecb9cbae29d0ae10453877a3cf
Jens Schauder <jens.schauder@broadcom.com>
2024-03-14 10:06:04 +0100

    Add property spring.data.jdbc.dialect
    The class valued property allows to configure a dialect, without relying
    on a database connection to determine it.
    
    See gh-39941

--- spring-boot-project/spring-boot-autoconfigure/src/main/java/org/springframework/boot/autoconfigure/data/jdbc/JdbcRepositoriesAutoConfiguration.java
+++ spring-boot-project/spring-boot-autoconfigure/src/main/java/org/springframework/boot/autoconfigure/data/jdbc/JdbcRepositoriesAutoConfiguration.java
@@ -16,9 +16,11 @@
 
 package org.springframework.boot.autoconfigure.data.jdbc;
 
+import java.lang.reflect.InvocationTargetException;
 import java.util.Optional;
 import java.util.Set;
 
+import org.springframework.beans.factory.BeanCreationException;
 import org.springframework.boot.autoconfigure.AutoConfiguration;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnBean;
@@ -28,6 +30,7 @@ import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.boot.autoconfigure.domain.EntityScanner;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceTransactionManagerAutoConfiguration;
 import org.springframework.boot.autoconfigure.jdbc.JdbcTemplateAutoConfiguration;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -59,6 +62,7 @@ import org.springframework.transaction.PlatformTransactionManager;
  * @author Andy Wilkinson
  * @author Stephane Nicoll
  * @author Mark Paluch
+ * @author Jens Schauder
  * @since 2.1.0
  * @see EnableJdbcRepositories
  */
@@ -67,6 +71,7 @@ import org.springframework.transaction.PlatformTransactionManager;
 @ConditionalOnClass({ NamedParameterJdbcOperations.class, AbstractJdbcConfiguration.class })
 @ConditionalOnProperty(prefix = "spring.data.jdbc.repositories", name = "enabled", havingValue = "true",
 		matchIfMissing = true)
+@EnableConfigurationProperties(JdbcDataProperties.class)
 public class JdbcRepositoriesAutoConfiguration {
 
 	@Configuration(proxyBeanMethods = false)
@@ -82,8 +87,11 @@ public class JdbcRepositoriesAutoConfiguration {
 
 		private final ApplicationContext applicationContext;
 
-		SpringBootJdbcConfiguration(ApplicationContext applicationContext) {
+		private final JdbcDataProperties properties;
+
+		SpringBootJdbcConfiguration(ApplicationContext applicationContext, JdbcDataProperties properties) {
 			this.applicationContext = applicationContext;
+			this.properties = properties;
 		}
 
 		@Override
@@ -141,6 +149,17 @@ public class JdbcRepositoriesAutoConfiguration {
 		@Bean
 		@ConditionalOnMissingBean
 		public Dialect jdbcDialect(NamedParameterJdbcOperations operations) {
+			if (this.properties.getDialect() != null
+			) {
+				Class<?> dialectType = this.properties.getDialect();
+				try {
+					return (Dialect)dialectType.getDeclaredConstructor().newInstance();
+				}
+				catch (InstantiationException | IllegalAccessException |
+					   InvocationTargetException | NoSuchMethodException e) {
+					throw new BeanCreationException("Couldn't create instance of type " + dialectType, e);
+				}
+			}
 			return super.jdbcDialect(operations);
 		}
 
