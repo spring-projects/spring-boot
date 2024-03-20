ecf89c3fe39e819c4c43a9ab0d501ff32e7820b7
Phillip Webb <phil.webb@broadcom.com>
2024-03-19 21:50:53 -0700

    Polish asciidoc generated by changelog generator
    Refine the generator so that an additional space is included after
    section titles.

--- spring-boot-project/spring-boot-tools/spring-boot-configuration-metadata-changelog-generator/src/main/java/org/springframework/boot/configurationmetadata/changelog/ChangelogWriter.java
+++ spring-boot-project/spring-boot-tools/spring-boot-configuration-metadata-changelog-generator/src/main/java/org/springframework/boot/configurationmetadata/changelog/ChangelogWriter.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2012-2023 the original author or authors.
+ * Copyright 2012-2024 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -68,11 +68,11 @@ class ChangelogWriter implements AutoCloseable {
 		String newVersionNumber = changelog.newVersionNumber();
 		Map<DifferenceType, List<Difference>> differencesByType = collateByType(changelog);
 		write("Configuration property changes between `%s` and `%s`%n", oldVersionNumber, newVersionNumber);
-		write("%n%n%n== Deprecated in %s%n", newVersionNumber);
+		write("%n%n%n== Deprecated in %s%n%n", newVersionNumber);
 		writeDeprecated(differencesByType.get(DifferenceType.DEPRECATED));
-		write("%n%n%n== Added in %s%n", newVersionNumber);
+		write("%n%n%n== Added in %s%n%n", newVersionNumber);
 		writeAdded(differencesByType.get(DifferenceType.ADDED));
-		write("%n%n%n== Removed in %s%n", newVersionNumber);
+		write("%n%n%n== Removed in %s%n%n", newVersionNumber);
 		writeRemoved(differencesByType.get(DifferenceType.DELETED), differencesByType.get(DifferenceType.DEPRECATED));
 	}
 
