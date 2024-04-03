fe4c34d2263f4f4905a70594720d4bc8cb221291
Stéphane Nicoll <stephane.nicoll@broadcom.com>
2024-04-03 10:16:43 +0200

    Adapt test to include MappingJackson2CborHttpMessageConverter
    See spring-projects/spring-framework#32428
    
    See gh-41177

--- spring-boot-project/spring-boot-autoconfigure/src/test/java/org/springframework/boot/autoconfigure/http/HttpMessageConvertersTests.java
+++ spring-boot-project/spring-boot-autoconfigure/src/test/java/org/springframework/boot/autoconfigure/http/HttpMessageConvertersTests.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2012-2023 the original author or authors.
+ * Copyright 2012-2024 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -146,7 +146,7 @@ class HttpMessageConvertersTests {
 		}
 		assertThat(converterClasses).containsExactly(ByteArrayHttpMessageConverter.class,
 				StringHttpMessageConverter.class, ResourceHttpMessageConverter.class,
-				MappingJackson2HttpMessageConverter.class);
+				MappingJackson2HttpMessageConverter.class, MappingJackson2CborHttpMessageConverter.class);
 	}
 
 	private List<HttpMessageConverter<?>> extractFormPartConverters(List<HttpMessageConverter<?>> converters) {
