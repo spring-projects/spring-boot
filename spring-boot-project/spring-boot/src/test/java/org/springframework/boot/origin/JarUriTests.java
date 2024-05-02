68a63a77972390c238028bafddd1e1187b272d39
Moritz Halbritter <moritz.halbritter@broadcom.com>
2024-05-02 14:47:53 +0200

    Polish

--- spring-boot-project/spring-boot/src/test/java/org/springframework/boot/origin/JarUriTests.java
+++ spring-boot-project/spring-boot/src/test/java/org/springframework/boot/origin/JarUriTests.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2012-2023 the original author or authors.
+ * Copyright 2012-2024 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -21,7 +21,9 @@ import org.junit.jupiter.api.Test;
 import static org.assertj.core.api.Assertions.assertThat;
 
 /**
- * @author pwebb
+ * Tests for {@link JarUri}.
+ *
+ * @author Phillip Webb
  */
 class JarUriTests {
 
