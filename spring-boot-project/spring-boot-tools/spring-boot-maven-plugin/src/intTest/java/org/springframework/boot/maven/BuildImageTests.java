70aceb21eb195e434449ba098c7525bf816cf142
Scott Frederick <scott.frederick@broadcom.com>
2024-05-15 18:08:18 -0500

    Relocate test CNB builder and buildpack
    The test CNB builder and buildpack used in integration tests have moved
    from a projects.registry.vmware.com registry to ghcr.io/spring-io. This
    commit modifies tests to use the new location.
    
    Closes gh-40800

--- spring-boot-project/spring-boot-tools/spring-boot-maven-plugin/src/intTest/java/org/springframework/boot/maven/BuildImageTests.java
+++ spring-boot-project/spring-boot-tools/spring-boot-maven-plugin/src/intTest/java/org/springframework/boot/maven/BuildImageTests.java
@@ -245,9 +245,8 @@ class BuildImageTests extends AbstractArchiveIntegrationTests {
 			.goals("package")
 			.systemProperty("spring-boot.build-image.pullPolicy", "IF_NOT_PRESENT")
 			.systemProperty("spring-boot.build-image.imageName", "example.com/test/cmd-property-name:v1")
-			.systemProperty("spring-boot.build-image.builder",
-					"projects.registry.vmware.com/springboot/spring-boot-cnb-builder:0.0.2")
-			.systemProperty("spring-boot.build-image.runImage", "projects.registry.vmware.com/springboot/run:tiny-cnb")
+			.systemProperty("spring-boot.build-image.builder", "ghcr.io/spring-io/spring-boot-cnb-test-builder:0.0.1")
+			.systemProperty("spring-boot.build-image.runImage", "paketobuildpacks/run-jammy-tiny")
 			.systemProperty("spring-boot.build-image.createdDate", "2020-07-01T12:34:56Z")
 			.systemProperty("spring-boot.build-image.applicationDirectory", "/application")
 			.execute((project) -> {
