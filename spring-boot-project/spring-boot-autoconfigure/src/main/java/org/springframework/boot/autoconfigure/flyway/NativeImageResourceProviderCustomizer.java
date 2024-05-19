04938477f3663012fa4f96f1e59877c2dc3d4f3b
Maziz <maziz.esa@cbtw.tech>
2024-05-19 13:05:55 +0800

    1. Implement scanner for flyway10 2. Add test for flyway10 case

--- spring-boot-project/spring-boot-autoconfigure/src/main/java/org/springframework/boot/autoconfigure/flyway/NativeImageResourceProviderCustomizer.java
+++ spring-boot-project/spring-boot-autoconfigure/src/main/java/org/springframework/boot/autoconfigure/flyway/NativeImageResourceProviderCustomizer.java
@@ -16,6 +16,8 @@
 
 package org.springframework.boot.autoconfigure.flyway;
 
+import java.lang.reflect.Constructor;
+import java.lang.reflect.InvocationTargetException;
 import java.util.Arrays;
 
 import org.flywaydb.core.api.configuration.FluentConfiguration;
@@ -23,6 +25,8 @@ import org.flywaydb.core.api.migration.JavaMigration;
 import org.flywaydb.core.internal.scanner.LocationScannerCache;
 import org.flywaydb.core.internal.scanner.ResourceNameCache;
 import org.flywaydb.core.internal.scanner.Scanner;
+
+import org.springframework.util.ClassUtils;
 
 /**
  * Registers {@link NativeImageResourceProvider} as a Flyway
@@ -32,18 +36,55 @@ import org.flywaydb.core.internal.scanner.Scanner;
  */
 class NativeImageResourceProviderCustomizer extends ResourceProviderCustomizer {
 
+	public static final int NUM_FLYWAY10_SCANNER_PARAMETERS = 5;
+
 	@Override
 	public void customize(FluentConfiguration configuration) {
 		if (configuration.getResourceProvider() == null) {
-			Scanner<JavaMigration> scanner = new Scanner<>(JavaMigration.class,
-					Arrays.asList(configuration.getLocations()), configuration.getClassLoader(),
-					configuration.getEncoding(), configuration.isDetectEncoding(), false, new ResourceNameCache(),
-					new LocationScannerCache(), configuration.isFailOnMissingLocations());
+			Constructor<?> scannerConstructor;
+			Scanner scanner;
+			try {
+				scannerConstructor = ClassUtils.forName("org.flywaydb.core.internal.scanner.Scanner", null)
+					.getDeclaredConstructors()[0];
+				if (scannerConstructor.getParameters().length > NUM_FLYWAY10_SCANNER_PARAMETERS) {
+					scanner = getFlyway9Scanner(configuration);
+				}
+				else {
+					scanner = getFlyway10Scanner(configuration, scannerConstructor);
+				}
+				NativeImageResourceProvider resourceProvider = new NativeImageResourceProvider(scanner,
+						configuration.getClassLoader(), Arrays.asList(configuration.getLocations()),
+						configuration.getEncoding(), configuration.isFailOnMissingLocations());
+				configuration.resourceProvider(resourceProvider);
+			}
+			catch (ClassNotFoundException ex) {
+				throw new IllegalStateException(ex);
+			}
+			catch (InvocationTargetException | InstantiationException | IllegalAccessException ex) {
+				throw new RuntimeException(ex);
+			}
+
 			NativeImageResourceProvider resourceProvider = new NativeImageResourceProvider(scanner,
 					configuration.getClassLoader(), Arrays.asList(configuration.getLocations()),
 					configuration.getEncoding(), configuration.isFailOnMissingLocations());
 			configuration.resourceProvider(resourceProvider);
 		}
+	}
+
+	private static Scanner getFlyway10Scanner(FluentConfiguration configuration, Constructor<?> scannerConstructor)
+			throws InstantiationException, IllegalAccessException, InvocationTargetException {
+		Scanner scanner;
+		scanner = (Scanner) scannerConstructor.newInstance(JavaMigration.class, false, new ResourceNameCache(),
+				new LocationScannerCache(), configuration);
+		return scanner;
+	}
+
+	private static Scanner getFlyway9Scanner(FluentConfiguration configuration) {
+		Scanner scanner;
+		scanner = new Scanner<>(JavaMigration.class, Arrays.asList(configuration.getLocations()),
+				configuration.getClassLoader(), configuration.getEncoding(), configuration.isDetectEncoding(), false,
+				new ResourceNameCache(), new LocationScannerCache(), configuration.isFailOnMissingLocations());
+		return scanner;
 	}
 
 }
---
+++ spring-boot-project/spring-boot-autoconfigure/src/test/java/org/springframework/boot/autoconfigure/flyway/Flyway10NativeImageResourceProviderCustomizerTests.java
@@ -0,0 +1,52 @@
+/*
+ * Copyright 2012-2023 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.springframework.boot.autoconfigure.flyway;
+
+import java.util.Collection;
+
+import org.flywaydb.core.api.ResourceProvider;
+import org.flywaydb.core.api.configuration.FluentConfiguration;
+import org.flywaydb.core.api.resource.LoadableResource;
+import org.junit.jupiter.api.Test;
+
+import org.springframework.boot.testsupport.classpath.ClassPathOverrides;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+/**
+ * Tests for {@link NativeImageResourceProviderCustomizer} with Flyway 10.
+ *
+ * @author Moritz Halbritter
+ * @author Andy Wilkinson
+ * @author Maziz
+ */
+@ClassPathOverrides("org.flywaydb:flyway-core:10.12.0")
+class Flyway10NativeImageResourceProviderCustomizerTests {
+
+	private final NativeImageResourceProviderCustomizer customizer = new NativeImageResourceProviderCustomizer();
+
+	@Test
+	void nativeImageResourceProviderShouldFindMigrations() {
+		FluentConfiguration configuration = new FluentConfiguration();
+		this.customizer.customize(configuration);
+		ResourceProvider resourceProvider = configuration.getResourceProvider();
+		Collection<LoadableResource> migrations = resourceProvider.getResources("V", new String[] { ".sql" });
+		LoadableResource migration = resourceProvider.getResource("V1__init.sql");
+		assertThat(migrations).containsExactly(migration);
+	}
+
+}
