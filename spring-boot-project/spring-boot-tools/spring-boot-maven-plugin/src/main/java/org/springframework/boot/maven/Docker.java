47465f6ed5d456d0f07e9520b0fe7a7149c3f5e3
Stéphane Nicoll <stephane.nicoll@broadcom.com>
2024-07-29 15:36:57 +0200

    Do not validate settings if publishing is disabled
    This commit improves the Maven Plugin to only validate the publishing
    settings if publishing is actually enabled.
    
    Closes gh-29756

--- spring-boot-project/spring-boot-tools/spring-boot-maven-plugin/src/main/java/org/springframework/boot/maven/Docker.java
+++ spring-boot-project/spring-boot-tools/spring-boot-maven-plugin/src/main/java/org/springframework/boot/maven/Docker.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2012-2023 the original author or authors.
+ * Copyright 2012-2024 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -140,14 +140,15 @@ public class Docker {
 	 * Returns this configuration as a {@link DockerConfiguration} instance. This method
 	 * should only be called when the configuration is complete and will no longer be
 	 * changed.
+	 * @param publish whether the image should be published
 	 * @return the Docker configuration
 	 */
-	DockerConfiguration asDockerConfiguration() {
+	DockerConfiguration asDockerConfiguration(boolean publish) {
 		DockerConfiguration dockerConfiguration = new DockerConfiguration();
 		dockerConfiguration = customizeHost(dockerConfiguration);
 		dockerConfiguration = dockerConfiguration.withBindHostToBuilder(this.bindHostToBuilder);
 		dockerConfiguration = customizeBuilderAuthentication(dockerConfiguration);
-		dockerConfiguration = customizePublishAuthentication(dockerConfiguration);
+		dockerConfiguration = customizePublishAuthentication(dockerConfiguration, publish);
 		return dockerConfiguration;
 	}
 
@@ -180,7 +181,11 @@ public class Docker {
 				"Invalid Docker builder registry configuration, either token or username/password must be provided");
 	}
 
-	private DockerConfiguration customizePublishAuthentication(DockerConfiguration dockerConfiguration) {
+	private DockerConfiguration customizePublishAuthentication(DockerConfiguration dockerConfiguration,
+			boolean publish) {
+		if (!publish) {
+			return dockerConfiguration;
+		}
 		if (this.publishRegistry == null || this.publishRegistry.isEmpty()) {
 			return dockerConfiguration.withEmptyPublishRegistryAuthentication();
 		}
