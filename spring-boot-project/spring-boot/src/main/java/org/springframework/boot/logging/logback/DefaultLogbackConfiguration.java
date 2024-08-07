33711828f984f23897775e4ec0441b5720be792f
Stéphane Nicoll <stephane.nicoll@broadcom.com>
2024-08-07 20:12:27 +0200

    Polish "Harmonize use of StructuredLogEncoder"
    See gh-41712

--- spring-boot-project/spring-boot/src/main/java/org/springframework/boot/logging/logback/DefaultLogbackConfiguration.java
+++ spring-boot-project/spring-boot/src/main/java/org/springframework/boot/logging/logback/DefaultLogbackConfiguration.java
@@ -161,7 +161,7 @@ class DefaultLogbackConfiguration {
 		Charset charset = resolveCharset(config, "${" + type + "_LOG_CHARSET}");
 		String structuredLogFormat = resolve(config, "${" + type + "_LOG_STRUCTURED_FORMAT}");
 		if (StringUtils.hasLength(structuredLogFormat)) {
-			StructuredLogEncoder encoder = createStructuredLoggingEncoder(config, structuredLogFormat);
+			StructuredLogEncoder encoder = createStructuredLogEncoder(config, structuredLogFormat);
 			encoder.setCharset(charset);
 			return encoder;
 		}
@@ -171,7 +171,7 @@ class DefaultLogbackConfiguration {
 		return encoder;
 	}
 
-	private StructuredLogEncoder createStructuredLoggingEncoder(LogbackConfigurator config, String format) {
+	private StructuredLogEncoder createStructuredLogEncoder(LogbackConfigurator config, String format) {
 		StructuredLogEncoder encoder = new StructuredLogEncoder();
 		encoder.setFormat(format);
 		return encoder;
