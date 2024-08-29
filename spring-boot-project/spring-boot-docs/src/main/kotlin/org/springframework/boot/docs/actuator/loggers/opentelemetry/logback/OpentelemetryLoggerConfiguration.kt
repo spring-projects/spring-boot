c3ebcb4418e061c41f891af8e6bc3f4d84cdb341
famaridon <famaridon@users.noreply.github.com>
2024-08-29 14:55:38 +0200

    add opentelemetry logger appeder link to Opentelemetry sdk sample

---
+++ spring-boot-project/spring-boot-docs/src/main/kotlin/org/springframework/boot/docs/actuator/loggers/opentelemetry/logback/OpentelemetryLoggerConfiguration.kt
@@ -0,0 +1,34 @@
+/*
+ * Copyright 2012-2022 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+ 
+package org.springframework.boot.docs.actuator.loggers.opentelemetry.logback
+
+import org.springframework.boot.context.event.ApplicationReadyEvent
+import org.springframework.context.ApplicationListener
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+
+import io.opentelemetry.api.OpenTelemetry
+import io.opentelemetry.instrumentation.logback.appender.v1_0.OpenTelemetryAppender
+
+@Configuration
+class OpentelemetryLoggerConfiguration {
+
+    @Bean
+    fun logbackOtelAppenderInitializer(openTelemetry: OpenTelemetry): ApplicationListener<ApplicationReadyEvent> {
+        return ApplicationListener { OpenTelemetryAppender.install(openTelemetry) }
+    }
+}
