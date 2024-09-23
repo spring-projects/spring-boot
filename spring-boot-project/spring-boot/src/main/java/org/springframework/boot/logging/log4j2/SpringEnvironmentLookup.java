9b49e93f6214ef800fef2d46d0c106967ac559f9
Phillip Webb <phil.webb@broadcom.com>
2024-09-22 20:33:41 -0700

    Polish 'Improve exception message to hint that you might need a '-spring' file'
    See gh-42405

--- spring-boot-project/spring-boot/src/main/java/org/springframework/boot/logging/log4j2/SpringEnvironmentLookup.java
+++ spring-boot-project/spring-boot/src/main/java/org/springframework/boot/logging/log4j2/SpringEnvironmentLookup.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2012-2022 the original author or authors.
+ * Copyright 2012-2024 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -30,6 +30,7 @@ import org.springframework.util.Assert;
  *
  * @author Ralph Goers
  * @author Phillip Webb
+ * @author Dmytro Nosan
  */
 @Plugin(name = "spring", category = StrLookup.CATEGORY)
 class SpringEnvironmentLookup implements LoggerContextAware, StrLookup {
