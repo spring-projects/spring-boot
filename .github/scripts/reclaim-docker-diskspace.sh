38b441cfe9a0b195867b697ffec4487aa4938746
Phillip Webb <phil.webb@broadcom.com>
2024-10-17 19:44:44 -0700

    Refine reclaim space script
    See gh-42776

--- .github/scripts/reclaim-docker-diskspace.sh
+++ .github/scripts/reclaim-docker-diskspace.sh
@@ -1,11 +1,16 @@
 echo "Reclaiming Docker Disk Space"
 echo
 
-docker image ls --format "{{.Size}} {{.ID}}" | LANG=en_US sort -rh | while read line; do
-	size=$( echo "$line" | cut -d' ' -f1 | sed -e 's/\.[0-9]*//' | sed -e 's/MB/000000/' | sed -e 's/MB/000000000/' )
-	if [ $size -gt 200000000 ]; then
-		image=$( echo "$line" | cut -d' ' -f2 )
-		docker image rm -f $image
+docker image ls --format "{{.Size}} {{.ID}} {{.Repository}} {{.Tag}}" | LANG=en_US sort -rh | while read line; do
+	size=$( echo "$line" | cut -d' ' -f1 | sed -e 's/\.[0-9]*//' | sed -e 's/MB/000000/' | sed -e 's/GB/000000000/' )
+	image=$( echo "$line" | cut -d' ' -f2 )
+	repository=$( echo "$line" | cut -d' ' -f3 )
+	tag=$( echo "$line" | cut -d' ' -f4 )
+	if [ "$tag" != "<none>" ]; then
+		if [ "$size" -gt 200000000 ]; then
+			echo "Cleaning $image $repository:$size"
+			docker image rm $image
+		fi
 	fi
 done
 
