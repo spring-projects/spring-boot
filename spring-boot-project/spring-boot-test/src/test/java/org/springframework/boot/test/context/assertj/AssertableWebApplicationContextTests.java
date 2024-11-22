bb3651b7d1c4334d5c350a9b127729aac2bfbf10
Andy Wilkinson <andy.wilkinson@broadcom.com>
2024-11-22 14:42:48 +0000

    Reduce warnings reported by Eclipse
    Closes gh-43269

--- spring-boot-project/spring-boot-test/src/test/java/org/springframework/boot/test/context/assertj/AssertableWebApplicationContextTests.java
+++ spring-boot-project/spring-boot-test/src/test/java/org/springframework/boot/test/context/assertj/AssertableWebApplicationContextTests.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2012-2023 the original author or authors.
+ * Copyright 2012-2024 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -32,6 +32,7 @@ import static org.mockito.Mockito.mock;
 class AssertableWebApplicationContextTests {
 
 	@Test
+	@SuppressWarnings("resource")
 	void getShouldReturnProxy() {
 		AssertableWebApplicationContext context = AssertableWebApplicationContext
 			.get(() -> mock(ConfigurableWebApplicationContext.class));
