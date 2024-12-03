7a4e071709a502fda9f23def27f26b0049200583
Phillip Webb <phil.webb@broadcom.com>
2024-12-03 14:45:25 -0800

    Polish

--- spring-boot-project/spring-boot-autoconfigure/src/test/java/org/springframework/boot/autoconfigure/ssl/SslAutoConfigurationTests.java
+++ spring-boot-project/spring-boot-autoconfigure/src/test/java/org/springframework/boot/autoconfigure/ssl/SslAutoConfigurationTests.java
@@ -108,14 +108,14 @@ class SslAutoConfigurationTests {
 			.run((context) -> {
 				assertThat(context).hasSingleBean(SslBundles.class);
 				SslBundles bundles = context.getBean(SslBundles.class);
-				SslBundle first = bundles.getBundle("custom");
-				assertThat(first).isNotNull();
-				assertThat(first.getStores()).isNotNull();
-				assertThat(first.getManagers()).isNotNull();
-				assertThat(first.getKey().getAlias()).isEqualTo("alias1");
-				assertThat(first.getKey().getPassword()).isEqualTo("secret1");
-				assertThat(first.getStores().getKeyStore().getType()).isEqualTo("PKCS12");
-				assertThat(first.getStores().getTrustStore().getType()).isEqualTo("PKCS12");
+				SslBundle bundle = bundles.getBundle("custom");
+				assertThat(bundle).isNotNull();
+				assertThat(bundle.getStores()).isNotNull();
+				assertThat(bundle.getManagers()).isNotNull();
+				assertThat(bundle.getKey().getAlias()).isEqualTo("alias1");
+				assertThat(bundle.getKey().getPassword()).isEqualTo("secret1");
+				assertThat(bundle.getStores().getKeyStore().getType()).isEqualTo("PKCS12");
+				assertThat(bundle.getStores().getTrustStore().getType()).isEqualTo("PKCS12");
 			});
 	}
 
