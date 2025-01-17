e43427bf7ea4062b628f7cc5563b67961aea64d8
Yanming Zhou <zhouyanming@gmail.com>
2025-01-17 11:25:09 +0800

    Fix the build
    ```
    Execution failed for task ':spring-boot-project:spring-boot-autoconfigure:compileJava'.
    > Compilation failed; see the compiler output below.
      error: warnings found and -Werror specified/home/runner/work/spring-boot/spring-boot/spring-boot-project/spring-boot-autoconfigure/src/main/java/org/springframework/boot/autoconfigure/security/oauth2/resource/reactive/ReactiveOAuth2ResourceServerOpaqueTokenConfiguration.java:49: warning: [removal] SpringReactiveOpaqueTokenIntrospector(String,String,String) in SpringReactiveOpaqueTokenIntrospector has been deprecated and marked for removal
      			return new SpringReactiveOpaqueTokenIntrospector(opaqueToken.getIntrospectionUri(),
      			       ^
      /home/runner/work/spring-boot/spring-boot/spring-boot-project/spring-boot-autoconfigure/src/main/java/org/springframework/boot/autoconfigure/security/oauth2/resource/servlet/OAuth2ResourceServerOpaqueTokenConfiguration.java:51: warning: [removal] SpringOpaqueTokenIntrospector(String,String,String) in SpringOpaqueTokenIntrospector has been deprecated and marked for removal
      			return new SpringOpaqueTokenIntrospector(opaqueToken.getIntrospectionUri(), opaqueToken.getClientId(),
      			       ^
      1 error
      2 warnings
    ```
    
    Signed-off-by: Yanming Zhou <zhouyanming@gmail.com>

--- spring-boot-project/spring-boot-autoconfigure/src/main/java/org/springframework/boot/autoconfigure/security/oauth2/resource/servlet/OAuth2ResourceServerOpaqueTokenConfiguration.java
+++ spring-boot-project/spring-boot-autoconfigure/src/main/java/org/springframework/boot/autoconfigure/security/oauth2/resource/servlet/OAuth2ResourceServerOpaqueTokenConfiguration.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2012-2023 the original author or authors.
+ * Copyright 2012-2025 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -44,6 +44,7 @@ class OAuth2ResourceServerOpaqueTokenConfiguration {
 	@ConditionalOnMissingBean(OpaqueTokenIntrospector.class)
 	static class OpaqueTokenIntrospectionClientConfiguration {
 
+		@SuppressWarnings("removal")
 		@Bean
 		@ConditionalOnProperty(name = "spring.security.oauth2.resourceserver.opaquetoken.introspection-uri")
 		SpringOpaqueTokenIntrospector opaqueTokenIntrospector(OAuth2ResourceServerProperties properties) {
