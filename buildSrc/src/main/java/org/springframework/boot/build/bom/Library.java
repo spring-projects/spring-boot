ba5704e94c5407234e190cd421d86424e13b6628
arefbehboudi <behboodiaref@gmail.com>
2025-01-21 18:54:39 +0330

    Resolve comments
    Signed-off-by: arefbehboudi <behboodiaref@gmail.com>

--- buildSrc/src/main/java/org/springframework/boot/build/bom/Library.java
+++ buildSrc/src/main/java/org/springframework/boot/build/bom/Library.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2012-2025 the original author or authors.
+ * Copyright 2012-2024 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -541,7 +541,7 @@ public class Library {
 
 	public record Link(String rootName, Function<LibraryVersion, String> factory, List<String> packages) {
 
-		private static final Pattern PACKAGE_EXPAND = Pattern.compile("^(.*)\\[(.*)]$");
+		private static final Pattern PACKAGE_EXPAND = Pattern.compile("^(.*)\\[(.*)\\]$");
 
 		public Link {
 			packages = (packages != null) ? List.copyOf(expandPackages(packages)) : Collections.emptyList();
