SKYTRAIN-IGOR/JavaSpring

Let `logging.structured.json.customizer` accept multiple Customizers also
See https://github.com/spring-projects/spring-boot/issues/43312#issuecomment-2509058761

spring-boot-project/spring-boot/src/main/java/org/springframework/boot/logging/structured/StructuredLoggingJsonProperties.java
@@ -16,6 +16,7 @@
 
 package org.springframework.boot.logging.structured;
 
+import java.util.List;
 import java.util.Map;
 import java.util.Set;
 
@@ -32,9 +33,10 @@ import org.springframework.core.env.Environment;
  * @param customizer the fully qualified name of a
  * {@link StructuredLoggingJsonMembersCustomizer}
  * @author Phillip Webb
+ * @author Yanming Zhou
  */
 record StructuredLoggingJsonProperties(Set<String> include, Set<String> exclude, Map<String, String> rename,
-		Map<String, String> add, Class<? extends StructuredLoggingJsonMembersCustomizer<?>> customizer) {
+		Map<String, String> add, List<Class<? extends StructuredLoggingJsonMembersCustomizer<?>>> customizer) {
 
 	static StructuredLoggingJsonProperties get(Environment environment) {
 		return Binder.get(environment)

spring-boot-project/spring-boot/src/main/java/org/springframework/boot/logging/structured/StructuredLoggingJsonPropertiesJsonMembersCustomizer.java
@@ -16,6 +16,7 @@
 
 package org.springframework.boot.logging.structured;
 
+import java.util.List;
 import java.util.Map;
 
 import org.springframework.boot.json.JsonWriter.MemberPath;
@@ -28,6 +29,7 @@ import org.springframework.util.CollectionUtils;
  * {@link StructuredLoggingJsonProperties}.
  *
  * @author Phillip Webb
+ * @author Yanming Zhou
  */
 class StructuredLoggingJsonPropertiesJsonMembersCustomizer implements StructuredLoggingJsonMembersCustomizer<Object> {
 
@@ -49,9 +51,9 @@ class StructuredLoggingJsonPropertiesJsonMembersCustomizer implements Structured
 		if (!CollectionUtils.isEmpty(add)) {
 			add.forEach(members::add);
 		}
-		Class<? extends StructuredLoggingJsonMembersCustomizer<?>> customizer = this.properties.customizer();
+		List<Class<? extends StructuredLoggingJsonMembersCustomizer<?>>> customizer = this.properties.customizer();
 		if (customizer != null) {
-			createAndApplyCustomizer(members, customizer);
+			customizer.forEach((c) -> createAndApplyCustomizer(members, c));
 		}
 	}
 

spring-boot-project/spring-boot/src/test/java/org/springframework/boot/logging/structured/StructuredLoggingJsonPropertiesJsonMembersCustomizerTests.java
@@ -17,6 +17,7 @@
 package org.springframework.boot.logging.structured;
 
 import java.util.Collections;
+import java.util.List;
 import java.util.Map;
 import java.util.Set;
 
@@ -37,6 +38,7 @@ import static org.mockito.BDDMockito.given;
  * Tests for {@link StructuredLoggingJsonPropertiesJsonMembersCustomizer}.
  *
  * @author Phillip Webb
+ * @author Yanming Zhou
  */
 @ExtendWith(MockitoExtension.class)
 class StructuredLoggingJsonPropertiesJsonMembersCustomizerTests {
@@ -102,10 +104,23 @@ class StructuredLoggingJsonPropertiesJsonMembersCustomizerTests {
 			.applyingNameProcessor(NameProcessor.of(String::toUpperCase));
 		given(((Instantiator) this.instantiator).instantiateType(TestCustomizer.class)).willReturn(uppercaseCustomizer);
 		StructuredLoggingJsonProperties properties = new StructuredLoggingJsonProperties(Collections.emptySet(),
-				Collections.emptySet(), Collections.emptyMap(), Collections.emptyMap(), TestCustomizer.class);
+				Collections.emptySet(), Collections.emptyMap(), Collections.emptyMap(), List.of(TestCustomizer.class));
 		StructuredLoggingJsonPropertiesJsonMembersCustomizer customizer = new StructuredLoggingJsonPropertiesJsonMembersCustomizer(
 				this.instantiator, properties);
 		assertThat(writeSampleJson(customizer)).contains("\"A\":\"a\"");
+	}
+
+	@Test
+	@SuppressWarnings({ "rawtypes", "unchecked" })
+	void multipleCustomizers() {
+		given(((Instantiator) this.instantiator).instantiateType(FooCustomizer.class)).willReturn(new FooCustomizer());
+		given(((Instantiator) this.instantiator).instantiateType(BarCustomizer.class)).willReturn(new BarCustomizer());
+		StructuredLoggingJsonProperties properties = new StructuredLoggingJsonProperties(Collections.emptySet(),
+				Collections.emptySet(), Collections.emptyMap(), Collections.emptyMap(),
+				List.of(FooCustomizer.class, BarCustomizer.class));
+		StructuredLoggingJsonPropertiesJsonMembersCustomizer customizer = new StructuredLoggingJsonPropertiesJsonMembersCustomizer(
+				this.instantiator, properties);
+		assertThat(writeSampleJson(customizer)).contains("\"foo\":\"foo\"").contains("\"bar\":\"bar\"");
 	}
 
 	@SuppressWarnings({ "rawtypes", "unchecked" })
@@ -122,6 +137,24 @@ class StructuredLoggingJsonPropertiesJsonMembersCustomizerTests {
 
 		@Override
 		public void customize(Members<String> members) {
+		}
+
+	}
+
+	static class FooCustomizer implements StructuredLoggingJsonMembersCustomizer<String> {
+
+		@Override
+		public void customize(Members<String> members) {
+			members.add("foo", "foo");
+		}
+
+	}
+
+	static class BarCustomizer implements StructuredLoggingJsonMembersCustomizer<String> {
+
+		@Override
+		public void customize(Members<String> members) {
+			members.add("bar", "bar");
 		}
 
 	}

spring-boot-project/spring-boot/src/test/java/org/springframework/boot/logging/structured/StructuredLoggingJsonPropertiesTests.java
@@ -16,6 +16,7 @@
 
 package org.springframework.boot.logging.structured;
 
+import java.util.List;
 import java.util.Map;
 import java.util.Set;
 
@@ -43,7 +44,7 @@ class StructuredLoggingJsonPropertiesTests {
 		environment.setProperty("logging.structured.json.customizer", TestCustomizer.class.getName());
 		StructuredLoggingJsonProperties properties = StructuredLoggingJsonProperties.get(environment);
 		assertThat(properties).isEqualTo(new StructuredLoggingJsonProperties(Set.of("a", "b"), Set.of("c", "d"),
-				Map.of("e", "f"), Map.of("g", "h"), TestCustomizer.class));
+				Map.of("e", "f"), Map.of("g", "h"), List.of(TestCustomizer.class)));
 	}
 
 	@Test

spring-projects/spring-boot

Signed-off-by: Yanming Zhou <zhouyanming@gmail.com>

spring-boot-project/spring-boot/src/main/java/org/springframework/boot/logging/structured/StructuredLoggingJsonProperties.java
@@ -33,9 +33,10 @@ import org.springframework.core.env.Environment;
  * @param customizer the fully qualified name of a
  * {@link StructuredLoggingJsonMembersCustomizer}
  * @author Phillip Webb
+ * @author Yanming Zhou
  */
 record StructuredLoggingJsonProperties(Set<String> include, Set<String> exclude, Map<String, String> rename,
-		Map<String, String> add, Class<? extends StructuredLoggingJsonMembersCustomizer<?>> customizer) {
+		Map<String, String> add, Set<Class<? extends StructuredLoggingJsonMembersCustomizer<?>>> customizer) {
 
 	static StructuredLoggingJsonProperties get(Environment environment) {
 		return Binder.get(environment)

spring-boot-project/spring-boot/src/main/java/org/springframework/boot/logging/structured/StructuredLoggingJsonPropertiesJsonMembersCustomizer.java
@@ -17,6 +17,7 @@
 package org.springframework.boot.logging.structured;
 
 import java.util.Map;
+import java.util.Set;
 
 import org.springframework.boot.json.JsonWriter.MemberPath;
 import org.springframework.boot.json.JsonWriter.Members;
@@ -28,6 +29,7 @@ import org.springframework.util.CollectionUtils;
  * {@link StructuredLoggingJsonProperties}.
  *
  * @author Phillip Webb
+ * @author Yanming Zhou
  */
 class StructuredLoggingJsonPropertiesJsonMembersCustomizer implements StructuredLoggingJsonMembersCustomizer<Object> {
 
@@ -49,9 +51,9 @@ class StructuredLoggingJsonPropertiesJsonMembersCustomizer implements Structured
 		if (!CollectionUtils.isEmpty(add)) {
 			add.forEach(members::add);
 		}
-		Class<? extends StructuredLoggingJsonMembersCustomizer<?>> customizer = this.properties.customizer();
+		Set<Class<? extends StructuredLoggingJsonMembersCustomizer<?>>> customizer = this.properties.customizer();
 		if (customizer != null) {
-			createAndApplyCustomizer(members, customizer);
+			customizer.forEach((c) -> createAndApplyCustomizer(members, c));
 		}
 	}
 

spring-boot-project/spring-boot/src/test/java/org/springframework/boot/logging/structured/StructuredLoggingJsonPropertiesJsonMembersCustomizerTests.java
@@ -37,6 +37,7 @@ import static org.mockito.BDDMockito.given;
  * Tests for {@link StructuredLoggingJsonPropertiesJsonMembersCustomizer}.
  *
  * @author Phillip Webb
+ * @author Yanming Zhou
  */
 @ExtendWith(MockitoExtension.class)
 class StructuredLoggingJsonPropertiesJsonMembersCustomizerTests {
@@ -102,10 +103,23 @@ class StructuredLoggingJsonPropertiesJsonMembersCustomizerTests {
 			.applyingNameProcessor(NameProcessor.of(String::toUpperCase));
 		given(((Instantiator) this.instantiator).instantiateType(TestCustomizer.class)).willReturn(uppercaseCustomizer);
 		StructuredLoggingJsonProperties properties = new StructuredLoggingJsonProperties(Collections.emptySet(),
-				Collections.emptySet(), Collections.emptyMap(), Collections.emptyMap(), TestCustomizer.class);
+				Collections.emptySet(), Collections.emptyMap(), Collections.emptyMap(), Set.of(TestCustomizer.class));
 		StructuredLoggingJsonPropertiesJsonMembersCustomizer customizer = new StructuredLoggingJsonPropertiesJsonMembersCustomizer(
 				this.instantiator, properties);
 		assertThat(writeSampleJson(customizer)).contains("\"A\":\"a\"");
+	}
+
+	@Test
+	@SuppressWarnings({ "rawtypes", "unchecked" })
+	void multipleCustomizers() {
+		given(((Instantiator) this.instantiator).instantiateType(FooCustomizer.class)).willReturn(new FooCustomizer());
+		given(((Instantiator) this.instantiator).instantiateType(BarCustomizer.class)).willReturn(new BarCustomizer());
+		StructuredLoggingJsonProperties properties = new StructuredLoggingJsonProperties(Collections.emptySet(),
+				Collections.emptySet(), Collections.emptyMap(), Collections.emptyMap(),
+				Set.of(FooCustomizer.class, BarCustomizer.class));
+		StructuredLoggingJsonPropertiesJsonMembersCustomizer customizer = new StructuredLoggingJsonPropertiesJsonMembersCustomizer(
+				this.instantiator, properties);
+		assertThat(writeSampleJson(customizer)).contains("\"foo\":\"foo\"").contains("\"bar\":\"bar\"");
 	}
 
 	@SuppressWarnings({ "rawtypes", "unchecked" })
@@ -122,6 +136,24 @@ class StructuredLoggingJsonPropertiesJsonMembersCustomizerTests {
 
 		@Override
 		public void customize(Members<String> members) {
+		}
+
+	}
+
+	static class FooCustomizer implements StructuredLoggingJsonMembersCustomizer<String> {
+
+		@Override
+		public void customize(Members<String> members) {
+			members.add("foo", "foo");
+		}
+
+	}
+
+	static class BarCustomizer implements StructuredLoggingJsonMembersCustomizer<String> {
+
+		@Override
+		public void customize(Members<String> members) {
+			members.add("bar", "bar");
 		}
 
 	}

spring-boot-project/spring-boot/src/test/java/org/springframework/boot/logging/structured/StructuredLoggingJsonPropertiesTests.java
@@ -48,7 +48,7 @@ class StructuredLoggingJsonPropertiesTests {
 		environment.setProperty("logging.structured.json.customizer", TestCustomizer.class.getName());
 		StructuredLoggingJsonProperties properties = StructuredLoggingJsonProperties.get(environment);
 		assertThat(properties).isEqualTo(new StructuredLoggingJsonProperties(Set.of("a", "b"), Set.of("c", "d"),
-				Map.of("e", "f"), Map.of("g", "h"), TestCustomizer.class));
+				Map.of("e", "f"), Map.of("g", "h"), Set.of(TestCustomizer.class)));
 	}
 
 	@Test
@@ -64,7 +64,7 @@ class StructuredLoggingJsonPropertiesTests {
 		assertThat(RuntimeHintsPredicates.reflection().onType(StructuredLoggingJsonProperties.class)).accepts(hints);
 		assertThat(RuntimeHintsPredicates.reflection()
 			.onConstructor(StructuredLoggingJsonProperties.class.getDeclaredConstructor(Set.class, Set.class, Map.class,
-					Map.class, Class.class))
+					Map.class, Set.class))
 			.invoke()).accepts(hints);
 	}
 