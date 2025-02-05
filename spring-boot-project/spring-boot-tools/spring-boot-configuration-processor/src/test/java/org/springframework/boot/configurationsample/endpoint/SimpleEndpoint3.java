e4202609b258e22ca5df7366a941b252448cdf87
Andy Wilkinson <andy.wilkinson@broadcom.com>
2025-02-05 11:34:28 +0000

    Compile with Java 23 and -release 17

--- spring-boot-project/spring-boot-tools/spring-boot-configuration-processor/src/test/java/org/springframework/boot/configurationsample/endpoint/SimpleEndpoint3.java
+++ spring-boot-project/spring-boot-tools/spring-boot-configuration-processor/src/test/java/org/springframework/boot/configurationsample/endpoint/SimpleEndpoint3.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2012-2024 the original author or authors.
+ * Copyright 2012-2025 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -25,6 +25,7 @@ import org.springframework.boot.configurationsample.ReadOperation;
  *
  * @author Moritz Halbritter
  */
+@SuppressWarnings({ "deprecation", "removal" })
 @Endpoint(id = "simple", enableByDefault = false)
 public class SimpleEndpoint3 {
 
