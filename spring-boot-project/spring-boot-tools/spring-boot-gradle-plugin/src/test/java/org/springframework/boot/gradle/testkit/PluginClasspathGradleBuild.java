4fbbc3eda174437e1a68578effb1f9fe96a32ef0
Moritz Halbritter <moritz.halbritter@broadcom.com>
2025-02-06 15:40:05 +0100

    Clean up duplicate JsonView entry

--- spring-boot-project/spring-boot-tools/spring-boot-gradle-plugin/src/test/java/org/springframework/boot/gradle/testkit/PluginClasspathGradleBuild.java
+++ spring-boot-project/spring-boot-tools/spring-boot-gradle-plugin/src/test/java/org/springframework/boot/gradle/testkit/PluginClasspathGradleBuild.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2012-2024 the original author or authors.
+ * Copyright 2012-2025 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -84,7 +84,7 @@ public class PluginClasspathGradleBuild extends GradleBuild {
 				new File(pathOfJarContaining(HttpClientConnectionManager.class)),
 				new File(pathOfJarContaining(HttpRequest.class)),
 				new File(pathOfJarContaining(HttpVersionPolicy.class)), new File(pathOfJarContaining(Module.class)),
-				new File(pathOfJarContaining(Versioned.class)), new File(pathOfJarContaining(JsonView.class)),
+				new File(pathOfJarContaining(Versioned.class)),
 				new File(pathOfJarContaining(ParameterNamesModule.class)),
 				new File(pathOfJarContaining(JsonView.class)), new File(pathOfJarContaining(Platform.class)),
 				new File(pathOfJarContaining(Toml.class)), new File(pathOfJarContaining(Lexer.class)),
