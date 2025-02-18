0a420826715d7f93d219e8abd55e10330b5dea2e
Andy Wilkinson <andy.wilkinson@broadcom.com>
2025-02-18 10:03:03 +0000

    Fail fast when trying to use SNI with reactive Jetty
    Previously only a servlet-based Jetty server would fail fast when
    trying to use SNI with Jetty. A reactive Jetty server just ignored
    the configuration. This commit aligns the behavior of the two by
    making the reactive server fail fast as well.
    
    Closes gh-44316

--- spring-boot-project/spring-boot/src/main/java/org/springframework/boot/web/embedded/jetty/JettyReactiveWebServerFactory.java
+++ spring-boot-project/spring-boot/src/main/java/org/springframework/boot/web/embedded/jetty/JettyReactiveWebServerFactory.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2012-2024 the original author or authors.
+ * Copyright 2012-2025 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -249,6 +249,7 @@ public class JettyReactiveWebServerFactory extends AbstractReactiveWebServerFact
 	}
 
 	private void customizeSsl(Server server, InetSocketAddress address) {
+		Assert.state(getSsl().getServerNameBundles().isEmpty(), "Server name SSL bundles are not supported with Jetty");
 		new SslServerCustomizer(getHttp2(), address, getSsl().getClientAuth(), getSslBundle()).customize(server);
 	}
 
