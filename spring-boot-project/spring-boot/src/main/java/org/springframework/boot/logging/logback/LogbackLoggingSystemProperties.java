98707abdb814a50a49bde92dbfe3cc384b5a5089
Dmytro Nosan <dimanosan@gmail.com>
2025-02-19 12:12:21 +0200

    Use Console charset for console logging when available
    Prior to this commit, LogbackLoggingSystemProperties
    doesn't respect Console.charset(). It used Charset.getDefaultCharset()
    for logback and  UTF-8 for log42j as defaults.
    
    This commit changes the behaviour of LogbackLoggingSystemProperties to use
     Console.charset() when available. If no console is present, the default
     charset is used instead. These changes bring consistency across
     logging implementations.
    
    See gh-43118
    
    Signed-off-by: Dmytro Nosan <dimanosan@gmail.com>

--- spring-boot-project/spring-boot/src/main/java/org/springframework/boot/logging/logback/LogbackLoggingSystemProperties.java
+++ spring-boot-project/spring-boot/src/main/java/org/springframework/boot/logging/logback/LogbackLoggingSystemProperties.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2012-2024 the original author or authors.
+ * Copyright 2012-2025 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -16,7 +16,7 @@
 
 package org.springframework.boot.logging.logback;
 
-import java.nio.charset.Charset;
+import java.io.Console;
 import java.util.function.BiConsumer;
 import java.util.function.Function;
 
@@ -71,8 +71,8 @@ public class LogbackLoggingSystemProperties extends LoggingSystemProperties {
 	}
 
 	@Override
-	protected Charset getDefaultCharset() {
-		return Charset.defaultCharset();
+	protected Console getConsole() {
+		return super.getConsole();
 	}
 
 	@Override
