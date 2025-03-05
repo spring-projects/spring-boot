5ccb11d7ace8afb61380bed3f70a53cff56da94d
Vanio Begic <vanio.begic123@gmail.com>
2025-03-05 21:47:09 +0100

    Adapt the deleted tests to apply to ZipkinHttpClientSender instead of the originally applied to UrlConnectionSender
    Signed-off-by: Vanio Begic <vanio.begic123@gmail.com>

--- spring-boot-project/spring-boot-actuator-autoconfigure/src/test/java/org/springframework/boot/actuate/autoconfigure/tracing/zipkin/ZipkinConfigurationsSenderConfigurationTests.java
+++ spring-boot-project/spring-boot-actuator-autoconfigure/src/test/java/org/springframework/boot/actuate/autoconfigure/tracing/zipkin/ZipkinConfigurationsSenderConfigurationTests.java
@@ -16,12 +16,15 @@
 
 package org.springframework.boot.actuate.autoconfigure.tracing.zipkin;
 
+import java.net.http.HttpClient;
+
 import org.junit.jupiter.api.Test;
 import zipkin2.reporter.BytesMessageSender;
 import zipkin2.reporter.HttpEndpointSupplier;
-
+import org.springframework.boot.actuate.autoconfigure.tracing.zipkin.ZipkinConfigurations.HttpClientSenderConfiguration;
 import org.springframework.boot.actuate.autoconfigure.tracing.zipkin.ZipkinConfigurations.SenderConfiguration;
 import org.springframework.boot.autoconfigure.AutoConfigurations;
+import org.springframework.boot.test.context.FilteredClassLoader;
 import org.springframework.boot.test.context.runner.ApplicationContextRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -39,7 +42,7 @@ import static org.mockito.Mockito.mock;
 class ZipkinConfigurationsSenderConfigurationTests {
 
 	private final ApplicationContextRunner contextRunner = new ApplicationContextRunner()
-		.withConfiguration(AutoConfigurations.of(DefaultEncodingConfiguration.class, SenderConfiguration.class));
+			.withConfiguration(AutoConfigurations.of(DefaultEncodingConfiguration.class, SenderConfiguration.class));
 
 	@Test
 	void shouldSupplyDefaultHttpClientSenderBean() {
@@ -50,11 +53,30 @@ class ZipkinConfigurationsSenderConfigurationTests {
 	}
 
 	@Test
+	void shouldNotProvideHttpClientSenderIfHttpClientIsNotAvailable() {
+		this.contextRunner.withUserConfiguration(HttpClientSenderConfiguration.class)
+				.withClassLoader(new FilteredClassLoader(HttpClient.class))
+				.run((context) -> {
+					assertThat(context).doesNotHaveBean(ZipkinHttpClientSender.class);
+				});
+	}
+
+	@Test
 	void shouldBackOffOnCustomBeans() {
 		this.contextRunner.withUserConfiguration(CustomConfiguration.class).run((context) -> {
 			assertThat(context).hasBean("customSender");
 			assertThat(context).hasSingleBean(BytesMessageSender.class);
 		});
+	}
+
+	@Test
+	void shouldUseCustomHttpEndpointSupplierFactory() {
+		this.contextRunner.withUserConfiguration(CustomHttpEndpointSupplierFactoryConfiguration.class)
+				.run((context) -> {
+					ZipkinHttpClientSender httpClientSender = context.getBean(ZipkinHttpClientSender.class);
+					assertThat(httpClientSender).extracting("endpointSupplier")
+							.isInstanceOf(CustomHttpEndpointSupplier.class);
+				});
 	}
 
 	@Configuration(proxyBeanMethods = false)
@@ -63,6 +85,25 @@ class ZipkinConfigurationsSenderConfigurationTests {
 		@Bean
 		BytesMessageSender customSender() {
 			return mock(BytesMessageSender.class);
+		}
+
+	}
+
+	@Configuration(proxyBeanMethods = false)
+	private static final class CustomHttpEndpointSupplierFactoryConfiguration {
+
+		@Bean
+		HttpEndpointSupplier.Factory httpEndpointSupplier() {
+			return new CustomHttpEndpointSupplierFactory();
+		}
+
+	}
+
+	private static final class CustomHttpEndpointSupplierFactory implements HttpEndpointSupplier.Factory {
+
+		@Override
+		public HttpEndpointSupplier create(String endpoint) {
+			return new CustomHttpEndpointSupplier(endpoint);
 		}
 
 	}
