51a0e89d759ae265ce97791f189218b9b3682c1e
Doğaç Eldenk <dogac@carbonhealth.com>
2025-03-07 21:27:37 -0600

    Fix jetty auto-start when port is -1 Signed-off-by: Doğaç Eldenk <dogac@carbonhealth.com>

--- spring-boot-project/spring-boot/src/main/java/org/springframework/boot/web/embedded/jetty/JettyWebServer.java
+++ spring-boot-project/spring-boot/src/main/java/org/springframework/boot/web/embedded/jetty/JettyWebServer.java
@@ -145,24 +145,23 @@ public class JettyWebServer implements WebServer {
 				return;
 			}
 			this.server.setConnectors(this.connectors);
-			if (!this.autoStart) {
-				return;
-			}
 			try {
 				this.server.start();
-				for (Handler handler : this.server.getHandlers()) {
-					handleDeferredInitialize(handler);
-				}
-				Connector[] connectors = this.server.getConnectors();
-				for (Connector connector : connectors) {
-					try {
-						connector.start();
+				if (this.autoStart) {
+					for (Handler handler : this.server.getHandlers()) {
+						handleDeferredInitialize(handler);
 					}
-					catch (IOException ex) {
-						if (connector instanceof NetworkConnector networkConnector) {
-							PortInUseException.throwIfPortBindingException(ex, networkConnector::getPort);
+					Connector[] connectors = this.server.getConnectors();
+					for (Connector connector : connectors) {
+						try {
+							connector.start();
 						}
-						throw ex;
+						catch (IOException ex) {
+							if (connector instanceof NetworkConnector networkConnector) {
+								PortInUseException.throwIfPortBindingException(ex, networkConnector::getPort);
+							}
+							throw ex;
+						}
 					}
 				}
 				this.started = true;
