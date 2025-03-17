186840ba3f1a4c605afd43ca0d9461edb7827960
Brian Clozel <brian.clozel@broadcom.com>
2025-03-17 20:13:20 +0100

    Create spring-boot-mail module

--- spring-boot-project/spring-boot-mail/src/main/java/org/springframework/boot/mail/autoconfigure/MailProperties.java
+++ spring-boot-project/spring-boot-mail/src/main/java/org/springframework/boot/mail/autoconfigure/MailProperties.java
@@ -14,7 +14,7 @@
  * limitations under the License.
  */
 
-package org.springframework.boot.autoconfigure.mail;
+package org.springframework.boot.mail.autoconfigure;
 
 import java.nio.charset.Charset;
 import java.nio.charset.StandardCharsets;
