db2bb7f5bde35d322d3d6090ede24bc846074894
Stéphane Nicoll <stephane.nicoll@broadcom.com>
2025-03-18 15:00:11 +0100

    Create spring-boot-artemis module

--- spring-boot-project/spring-boot-artemis/src/main/java/org/springframework/boot/artemis/autoconfigure/package-info.java
+++ spring-boot-project/spring-boot-artemis/src/main/java/org/springframework/boot/artemis/autoconfigure/package-info.java
@@ -14,35 +14,9 @@
  * limitations under the License.
  */
 
-package org.springframework.boot.autoconfigure.jms.artemis;
-
-import org.apache.activemq.artemis.spi.core.naming.BindingRegistry;
-
 /**
- * A no-op implementation of the {@link BindingRegistry}.
+ * Auto-configuration for Artemis.
  *
  * @author Eddú Meléndez
- * @author Stephane Nicoll
- * @since 1.3.0
  */
-public class ArtemisNoOpBindingRegistry implements BindingRegistry {
-
-	@Override
-	public Object lookup(String s) {
-		return null;
-	}
-
-	@Override
-	public boolean bind(String s, Object o) {
-		return false;
-	}
-
-	@Override
-	public void unbind(String s) {
-	}
-
-	@Override
-	public void close() {
-	}
-
-}
+package org.springframework.boot.artemis.autoconfigure;
