413ab0f54d4ca6d025c85c8827e204e82d7b7898
Yanming Zhou <zhouyanming@gmail.com>
2025-03-20 15:52:34 +0800

    Allow to configure validateTransactionState for Spring Batch
    Signed-off-by: Yanming Zhou <zhouyanming@gmail.com>

--- spring-boot-project/spring-boot-autoconfigure/src/main/java/org/springframework/boot/autoconfigure/batch/BatchAutoConfiguration.java
+++ spring-boot-project/spring-boot-autoconfigure/src/main/java/org/springframework/boot/autoconfigure/batch/BatchAutoConfiguration.java
@@ -66,6 +66,7 @@ import org.springframework.util.StringUtils;
  * @author Mahmoud Ben Hassine
  * @author Lars Uffmann
  * @author Lasse Wulff
+ * @author Yanming Zhou
  * @since 1.0.0
  */
 @AutoConfiguration(after = { HibernateJpaAutoConfiguration.class, TransactionAutoConfiguration.class })
@@ -138,6 +139,12 @@ public class BatchAutoConfiguration {
 		protected String getTablePrefix() {
 			String tablePrefix = this.properties.getJdbc().getTablePrefix();
 			return (tablePrefix != null) ? tablePrefix : super.getTablePrefix();
+		}
+
+		@Override
+		protected boolean getValidateTransactionState() {
+			Boolean validateTransactionState = this.properties.getJdbc().getValidateTransactionState();
+			return (validateTransactionState != null) ? validateTransactionState : super.getValidateTransactionState();
 		}
 
 		@Override
