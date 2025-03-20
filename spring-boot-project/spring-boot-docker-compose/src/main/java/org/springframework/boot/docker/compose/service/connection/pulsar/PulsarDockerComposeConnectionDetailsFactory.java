fd6de31331157339f06bd46897124e8891bb20c7
Andy Wilkinson <andy.wilkinson@broadcom.com>
2025-03-20 17:38:21 +0000

    Create spring-boot-pulsar module

--- spring-boot-project/spring-boot-docker-compose/src/main/java/org/springframework/boot/docker/compose/service/connection/pulsar/PulsarDockerComposeConnectionDetailsFactory.java
+++ spring-boot-project/spring-boot-docker-compose/src/main/java/org/springframework/boot/docker/compose/service/connection/pulsar/PulsarDockerComposeConnectionDetailsFactory.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2012-2023 the original author or authors.
+ * Copyright 2012-2025 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -16,11 +16,11 @@
 
 package org.springframework.boot.docker.compose.service.connection.pulsar;
 
-import org.springframework.boot.autoconfigure.pulsar.PulsarConnectionDetails;
 import org.springframework.boot.docker.compose.core.ConnectionPorts;
 import org.springframework.boot.docker.compose.core.RunningService;
 import org.springframework.boot.docker.compose.service.connection.DockerComposeConnectionDetailsFactory;
 import org.springframework.boot.docker.compose.service.connection.DockerComposeConnectionSource;
+import org.springframework.boot.pulsar.autoconfigure.PulsarConnectionDetails;
 
 /**
  * {@link DockerComposeConnectionDetailsFactory} to create {@link PulsarConnectionDetails}
