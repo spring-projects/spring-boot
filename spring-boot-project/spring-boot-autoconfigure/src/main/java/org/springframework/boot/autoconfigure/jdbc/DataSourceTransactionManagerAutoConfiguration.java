9091f3f58f0e23ae6c08675da1564cafbf8307da
Stéphane Nicoll <stephane.nicoll@broadcom.com>
2025-03-20 15:41:23 +0100

    Make link to DataSourceAutoConfiguration explicit
    This commit updates DataSourceTransactionManagerAutoConfiguration to
    explicitly states it should run after DataSourceAutoConfiguration.
    
    Closes gh-44810

--- spring-boot-project/spring-boot-autoconfigure/src/main/java/org/springframework/boot/autoconfigure/jdbc/DataSourceTransactionManagerAutoConfiguration.java
+++ spring-boot-project/spring-boot-autoconfigure/src/main/java/org/springframework/boot/autoconfigure/jdbc/DataSourceTransactionManagerAutoConfiguration.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2012-2024 the original author or authors.
+ * Copyright 2012-2025 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -48,7 +48,7 @@ import org.springframework.transaction.TransactionManager;
  * @since 1.0.0
  */
 @AutoConfiguration(before = TransactionAutoConfiguration.class,
-		after = TransactionManagerCustomizationAutoConfiguration.class)
+		after = { DataSourceAutoConfiguration.class, TransactionManagerCustomizationAutoConfiguration.class })
 @ConditionalOnClass({ DataSource.class, JdbcTemplate.class, TransactionManager.class })
 @AutoConfigureOrder(Ordered.LOWEST_PRECEDENCE)
 @EnableConfigurationProperties(DataSourceProperties.class)
