37171802b95a9faf3f6d857e138495be0f8a99d4
Stéphane Nicoll <stephane.nicoll@broadcom.com>
2025-03-24 16:05:11 +0100

    Create spring-boot-quartz module

--- spring-boot-project/spring-boot-actuator-autoconfigure/src/main/java/org/springframework/boot/actuate/autoconfigure/quartz/QuartzEndpointAutoConfiguration.java
+++ spring-boot-project/spring-boot-actuator-autoconfigure/src/main/java/org/springframework/boot/actuate/autoconfigure/quartz/QuartzEndpointAutoConfiguration.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2012-2024 the original author or authors.
+ * Copyright 2012-2025 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -29,8 +29,8 @@ import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnBean;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
-import org.springframework.boot.autoconfigure.quartz.QuartzAutoConfiguration;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.boot.quartz.autoconfigure.QuartzAutoConfiguration;
 import org.springframework.context.annotation.Bean;
 
 /**
--- spring-boot-project/spring-boot-quartz/src/main/java/org/springframework/boot/quartz/autoconfigure/package-info.java
+++ spring-boot-project/spring-boot-quartz/src/main/java/org/springframework/boot/quartz/autoconfigure/package-info.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2012-2019 the original author or authors.
+ * Copyright 2012-2025 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -14,24 +14,7 @@
  * limitations under the License.
  */
 
-package org.springframework.boot.autoconfigure.quartz;
-
 /**
- * Define the supported Quartz {@code JobStore}.
- *
- * @author Stephane Nicoll
- * @since 2.0.0
+ * Auto-configuration for Quartz Scheduler.
  */
-public enum JobStoreType {
-
-	/**
-	 * Store jobs in memory.
-	 */
-	MEMORY,
-
-	/**
-	 * Store jobs in the database.
-	 */
-	JDBC
-
-}
+package org.springframework.boot.quartz.autoconfigure;
