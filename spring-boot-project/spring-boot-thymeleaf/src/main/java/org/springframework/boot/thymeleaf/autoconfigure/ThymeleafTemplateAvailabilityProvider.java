d88a66bee6f7eb86a4d3daccb0c2143d4e5b4336
Moritz Halbritter <moritz.halbritter@broadcom.com>
2025-03-25 11:22:30 +0100

    Create spring-boot-thymeleaf module

--- spring-boot-project/spring-boot-thymeleaf/src/main/java/org/springframework/boot/thymeleaf/autoconfigure/ThymeleafTemplateAvailabilityProvider.java
+++ spring-boot-project/spring-boot-thymeleaf/src/main/java/org/springframework/boot/thymeleaf/autoconfigure/ThymeleafTemplateAvailabilityProvider.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2012-2022 the original author or authors.
+ * Copyright 2012-2025 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -14,7 +14,7 @@
  * limitations under the License.
  */
 
-package org.springframework.boot.autoconfigure.thymeleaf;
+package org.springframework.boot.thymeleaf.autoconfigure;
 
 import org.springframework.boot.autoconfigure.template.TemplateAvailabilityProvider;
 import org.springframework.core.env.Environment;
@@ -27,7 +27,7 @@ import org.springframework.util.ClassUtils;
  *
  * @author Andy Wilkinson
  * @author Madhura Bhave
- * @since 1.1.0
+ * @since 4.0.0
  */
 public class ThymeleafTemplateAvailabilityProvider implements TemplateAvailabilityProvider {
 
