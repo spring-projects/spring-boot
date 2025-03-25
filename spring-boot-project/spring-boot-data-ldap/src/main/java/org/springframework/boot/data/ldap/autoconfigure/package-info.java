ae00ac30898b3f48db37dd4bfe45e4baae91ccae
Stéphane Nicoll <stephane.nicoll@broadcom.com>
2025-03-25 10:58:46 +0100

    Create spring-boot-data-ldap module

--- spring-boot-project/spring-boot-data-ldap/src/main/java/org/springframework/boot/data/ldap/autoconfigure/package-info.java
+++ spring-boot-project/spring-boot-data-ldap/src/main/java/org/springframework/boot/data/ldap/autoconfigure/package-info.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2012-2019 the original author or authors.
+ * Copyright 2012-2025 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -17,4 +17,4 @@
 /**
  * Auto-configuration for Spring Data LDAP.
  */
-package org.springframework.boot.autoconfigure.data.ldap;
+package org.springframework.boot.data.ldap.autoconfigure;
