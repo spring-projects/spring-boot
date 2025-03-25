ee0d00bfc6abaf54bbcf7b14340e225946d48610
Moritz Halbritter <moritz.halbritter@broadcom.com>
2025-03-25 14:27:08 +0100

    Create spring-boot-h2console module

--- spring-boot-project/spring-boot-h2console/src/test/java/org/springframework/boot/h2console/autoconfigure/H2ConsolePropertiesTests.java
+++ spring-boot-project/spring-boot-h2console/src/test/java/org/springframework/boot/h2console/autoconfigure/H2ConsolePropertiesTests.java
@@ -14,7 +14,7 @@
  * limitations under the License.
  */
 
-package org.springframework.boot.autoconfigure.h2;
+package org.springframework.boot.h2console.autoconfigure;
 
 import org.junit.jupiter.api.Test;
 
