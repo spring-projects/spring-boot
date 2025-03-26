13bd61445b1aa8610ce4010d68193ecd23729a09
Stéphane Nicoll <stephane.nicoll@broadcom.com>
2025-03-26 10:04:24 +0100

    Allow configuring a TaskExecutor even if an Executor is present
    This commit updates `TaskExecutionAutoConfiguration` to permit the
    auto-configuration of a `TaskExecutor` even if a user-defined `Executor`
    bean is present.
    
    Such `Executor` may have been created for totally unrelated reason, and
    it may or may not be an `AsyncTaskExecutor`. The default behavior has
    not changed, but this commit provides a new property,
    `spring.task.execution.mode` that can be set to `force` to
    auto-configure the `TaskExecutor` anyway.
    
    Because this mode made it so that two `Executor` will be present in the
    context, this commit also automatically configures an `AsyncConfigurer`
    if none is present already to make sure task processing uses the
    auto-configured TaskExecutor.
    
    Closes gh-44659

--- spring-boot-project/spring-boot-autoconfigure/src/main/java/org/springframework/boot/autoconfigure/task/TaskExecutorConfigurations.java
+++ spring-boot-project/spring-boot-autoconfigure/src/main/java/org/springframework/boot/autoconfigure/task/TaskExecutorConfigurations.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2012-2024 the original author or authors.
+ * Copyright 2012-2025 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -18,8 +18,11 @@ package org.springframework.boot.autoconfigure.task;
 
 import java.util.concurrent.Executor;
 
+import org.springframework.beans.factory.BeanFactory;
 import org.springframework.beans.factory.ObjectProvider;
+import org.springframework.boot.autoconfigure.condition.AnyNestedCondition;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnThreading;
 import org.springframework.boot.autoconfigure.thread.Threading;
 import org.springframework.boot.task.SimpleAsyncTaskExecutorBuilder;
@@ -27,12 +30,15 @@ import org.springframework.boot.task.SimpleAsyncTaskExecutorCustomizer;
 import org.springframework.boot.task.ThreadPoolTaskExecutorBuilder;
 import org.springframework.boot.task.ThreadPoolTaskExecutorCustomizer;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Conditional;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
 import org.springframework.context.annotation.Lazy;
 import org.springframework.core.task.SimpleAsyncTaskExecutor;
 import org.springframework.core.task.TaskDecorator;
 import org.springframework.core.task.TaskExecutor;
 import org.springframework.scheduling.annotation.AsyncAnnotationBeanPostProcessor;
+import org.springframework.scheduling.annotation.AsyncConfigurer;
 import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
 
 /**
@@ -46,19 +52,18 @@ import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
 class TaskExecutorConfigurations {
 
 	@Configuration(proxyBeanMethods = false)
-	@ConditionalOnMissingBean(Executor.class)
+	@Conditional(OnExecutorCondition.class)
+	@Import(AsyncConfigurerConfiguration.class)
 	static class TaskExecutorConfiguration {
 
-		@Bean(name = { TaskExecutionAutoConfiguration.APPLICATION_TASK_EXECUTOR_BEAN_NAME,
-				AsyncAnnotationBeanPostProcessor.DEFAULT_TASK_EXECUTOR_BEAN_NAME })
+		@Bean(TaskExecutionAutoConfiguration.APPLICATION_TASK_EXECUTOR_BEAN_NAME)
 		@ConditionalOnThreading(Threading.VIRTUAL)
 		SimpleAsyncTaskExecutor applicationTaskExecutorVirtualThreads(SimpleAsyncTaskExecutorBuilder builder) {
 			return builder.build();
 		}
 
+		@Bean(TaskExecutionAutoConfiguration.APPLICATION_TASK_EXECUTOR_BEAN_NAME)
 		@Lazy
-		@Bean(name = { TaskExecutionAutoConfiguration.APPLICATION_TASK_EXECUTOR_BEAN_NAME,
-				AsyncAnnotationBeanPostProcessor.DEFAULT_TASK_EXECUTOR_BEAN_NAME })
 		@ConditionalOnThreading(Threading.PLATFORM)
 		ThreadPoolTaskExecutor applicationTaskExecutor(ThreadPoolTaskExecutorBuilder threadPoolTaskExecutorBuilder) {
 			return threadPoolTaskExecutorBuilder.build();
@@ -136,6 +141,43 @@ class TaskExecutorConfigurations {
 				builder = builder.taskTerminationTimeout(shutdown.getAwaitTerminationPeriod());
 			}
 			return builder;
+		}
+
+	}
+
+	@Configuration(proxyBeanMethods = false)
+	@ConditionalOnMissingBean(name = AsyncAnnotationBeanPostProcessor.DEFAULT_TASK_EXECUTOR_BEAN_NAME,
+			value = AsyncConfigurer.class)
+	static class AsyncConfigurerConfiguration {
+
+		@Bean
+		@ConditionalOnMissingBean
+		AsyncConfigurer applicationTaskExecutorAsyncConfigurer(BeanFactory beanFactory) {
+			return new AsyncConfigurer() {
+				@Override
+				public Executor getAsyncExecutor() {
+					return beanFactory.getBean(TaskExecutionAutoConfiguration.APPLICATION_TASK_EXECUTOR_BEAN_NAME,
+							Executor.class);
+				}
+			};
+		}
+
+	}
+
+	static class OnExecutorCondition extends AnyNestedCondition {
+
+		OnExecutorCondition() {
+			super(ConfigurationPhase.REGISTER_BEAN);
+		}
+
+		@ConditionalOnMissingBean(Executor.class)
+		private static final class ExecutorBeanCondition {
+
+		}
+
+		@ConditionalOnProperty(value = "spring.task.execution.mode", havingValue = "force")
+		private static final class ModelCondition {
+
 		}
 
 	}
