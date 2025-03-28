dd8bdfc8e38ff6a22a1d4a858b0af4e40a661d64
Andy Wilkinson <andy.wilkinson@broadcom.com>
2025-03-28 10:23:50 +0000

    Create spring-boot-webservices module

--- spring-boot-project/spring-boot-webservices/src/test/java/org/springframework/boot/webservices/autoconfigure/WebServicesAutoConfigurationTests.java
+++ spring-boot-project/spring-boot-webservices/src/test/java/org/springframework/boot/webservices/autoconfigure/WebServicesAutoConfigurationTests.java
@@ -14,7 +14,7 @@
  * limitations under the License.
  */
 
-package org.springframework.boot.autoconfigure.webservices;
+package org.springframework.boot.webservices.autoconfigure;
 
 import java.util.Collection;
 
