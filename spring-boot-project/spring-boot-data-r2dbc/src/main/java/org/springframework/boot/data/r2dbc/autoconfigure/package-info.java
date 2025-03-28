8174211b5c1180acfba1b23c8d4efc6ca9da89ff
Stéphane Nicoll <stephane.nicoll@broadcom.com>
2025-03-28 17:17:27 +0100

    Create spring-boot-data-r2dbc module

--- spring-boot-project/spring-boot-data-r2dbc/src/main/java/org/springframework/boot/data/r2dbc/autoconfigure/package-info.java
+++ spring-boot-project/spring-boot-data-r2dbc/src/main/java/org/springframework/boot/data/r2dbc/autoconfigure/package-info.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2012-2019 the original author or authors.
+ * Copyright 2012-2025 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -14,13 +14,7 @@
  * limitations under the License.
  */
 
-package org.springframework.boot.autoconfigure.data.empty;
-
 /**
- * Empty package used with data tests.
- *
- * @author Phillip Webb
+ * Auto-Configuration for Spring Data R2DBC.
  */
-public class EmptyDataPackage {
-
-}
+package org.springframework.boot.data.r2dbc.autoconfigure;
