d5460697b4125114245e73ff025cce1b39e2addb
Brian Clozel <brian.clozel@broadcom.com>
2025-04-08 09:34:00 +0200

    Upgrade to Spring for GraphQL 1.4.0-SNAPSHOT
    See gh-45029

--- spring-boot-project/spring-boot-actuator-autoconfigure/src/main/java/org/springframework/boot/actuate/autoconfigure/observation/graphql/GraphQlObservationAutoConfiguration.java
+++ spring-boot-project/spring-boot-actuator-autoconfigure/src/main/java/org/springframework/boot/actuate/autoconfigure/observation/graphql/GraphQlObservationAutoConfiguration.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2012-2023 the original author or authors.
+ * Copyright 2012-2025 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -30,6 +30,7 @@ import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean
 import org.springframework.context.annotation.Bean;
 import org.springframework.graphql.execution.GraphQlSource;
 import org.springframework.graphql.observation.DataFetcherObservationConvention;
+import org.springframework.graphql.observation.DataLoaderObservationConvention;
 import org.springframework.graphql.observation.ExecutionRequestObservationConvention;
 import org.springframework.graphql.observation.GraphQlObservationInstrumentation;
 
@@ -49,9 +50,10 @@ public class GraphQlObservationAutoConfiguration {
 	@ConditionalOnMissingBean
 	public GraphQlObservationInstrumentation graphQlObservationInstrumentation(ObservationRegistry observationRegistry,
 			ObjectProvider<ExecutionRequestObservationConvention> executionConvention,
-			ObjectProvider<DataFetcherObservationConvention> dataFetcherConvention) {
+			ObjectProvider<DataFetcherObservationConvention> dataFetcherConvention,
+			ObjectProvider<DataLoaderObservationConvention> dataLoaderObservationConvention) {
 		return new GraphQlObservationInstrumentation(observationRegistry, executionConvention.getIfAvailable(),
-				dataFetcherConvention.getIfAvailable());
+				dataFetcherConvention.getIfAvailable(), dataLoaderObservationConvention.getIfAvailable());
 	}
 
 }
