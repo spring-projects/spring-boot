935d7597542bd0da8ece940cc2f8d6bb05fd27f3
Phillip Webb <phil.webb@broadcom.com>
2025-04-10 16:29:31 -0700

    Fix 'Protect against empty OS string in Docker `Image`' logic
    See gh-45152

--- spring-boot-project/spring-boot-tools/spring-boot-buildpack-platform/src/main/java/org/springframework/boot/buildpack/platform/docker/type/Image.java
+++ spring-boot-project/spring-boot-tools/spring-boot-buildpack-platform/src/main/java/org/springframework/boot/buildpack/platform/docker/type/Image.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2012-2024 the original author or authors.
+ * Copyright 2012-2025 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -91,7 +91,7 @@ public class Image extends MappedObject {
 	 * @return the image OS
 	 */
 	public String getOs() {
-		return (!StringUtils.hasText(this.os)) ? this.os : "linux";
+		return (!StringUtils.hasText(this.os)) ? "linux" : this.os;
 	}
 
 	/**
