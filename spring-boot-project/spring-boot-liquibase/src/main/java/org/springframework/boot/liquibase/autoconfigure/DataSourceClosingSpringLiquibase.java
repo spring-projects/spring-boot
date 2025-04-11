89a728253ca86251457c9fce8f41ee3661a63d11
Phillip Webb <phil.webb@broadcom.com>
2025-04-11 14:25:07 -0700

    Create spring-boot-liquibase module

--- spring-boot-project/spring-boot-liquibase/src/main/java/org/springframework/boot/liquibase/autoconfigure/DataSourceClosingSpringLiquibase.java
+++ spring-boot-project/spring-boot-liquibase/src/main/java/org/springframework/boot/liquibase/autoconfigure/DataSourceClosingSpringLiquibase.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2012-2019 the original author or authors.
+ * Copyright 2012-2025 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -14,7 +14,7 @@
  * limitations under the License.
  */
 
-package org.springframework.boot.autoconfigure.liquibase;
+package org.springframework.boot.liquibase.autoconfigure;
 
 import java.lang.reflect.Method;
 
@@ -31,7 +31,7 @@ import org.springframework.util.ReflectionUtils;
  * {@link DataSource} once the database has been migrated.
  *
  * @author Andy Wilkinson
- * @since 2.0.6
+ * @since 4.0.0
  */
 public class DataSourceClosingSpringLiquibase extends SpringLiquibase implements DisposableBean {
 
