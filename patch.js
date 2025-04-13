a73815f255df031ff9bea4bb0c99971a0eaf1398
Emily Xiong <xiongemi@gmail.com>
2025-04-13 17:25:22 -0400

    enable local cache, disable remote cache

--- patch.js
+++ patch.js
@@ -2,12 +2,12 @@ const { readFileSync, writeFileSync } = require('fs');
 
 console.log('Patching NX...');
 
-
+/*
 writeFileSync(require.resolve('nx/src/tasks-runner/forked-process-task-runner.js'), readFileSync(
     require('path').join(__dirname, 'replacement-fork.js')
 ));
 
-/*
+
 writeFileSync(require.resolve('nx/src/tasks-runner/batch/run-batch.js'), readFileSync(
     require('path').join(__dirname, 'replacement.js')
 ));
