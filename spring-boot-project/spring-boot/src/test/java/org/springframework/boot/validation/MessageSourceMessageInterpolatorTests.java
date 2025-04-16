83901a22b6714033d4c595d014f92725ed37a6ea
Dmytro Nosan <dimanosan@gmail.com>
2025-04-16 16:02:28 +0300

    Fix parameter replacement when message matches its code
    This commit addresses an issue where placeholders like {foo}
    remain as {foo} if the message is the same as its code.
    
    See gh-45212
    
    Signed-off-by: Dmytro Nosan <dimanosan@gmail.com>

--- spring-boot-project/spring-boot/src/test/java/org/springframework/boot/validation/MessageSourceMessageInterpolatorTests.java
+++ spring-boot-project/spring-boot/src/test/java/org/springframework/boot/validation/MessageSourceMessageInterpolatorTests.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2012-2023 the original author or authors.
+ * Copyright 2012-2025 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -62,8 +62,8 @@ class MessageSourceMessageInterpolatorTests {
 	void interpolateWhenParametersAreUnknownUsingCodeAsDefaultShouldLeaveThemUnchanged() {
 		this.messageSource.setUseCodeAsDefaultMessage(true);
 		this.messageSource.addMessage("top", Locale.getDefault(), "{child}+{child}");
-		assertThat(this.interpolator.interpolate("{foo}{top}{bar}", this.context))
-			.isEqualTo("{foo}{child}+{child}{bar}");
+		this.messageSource.addMessage("foo", Locale.getDefault(), "foo");
+		assertThat(this.interpolator.interpolate("{foo}{top}{bar}", this.context)).isEqualTo("foo{child}+{child}{bar}");
 	}
 
 	@Test
