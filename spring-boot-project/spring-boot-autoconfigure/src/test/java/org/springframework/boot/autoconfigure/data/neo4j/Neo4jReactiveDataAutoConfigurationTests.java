9bcf32afe719f48eed31d43db0b525934ac37e2b
Stéphane Nicoll <stephane.nicoll@broadcom.com>
2025-04-18 15:43:55 +0200

    Stop assuming that Neo4jDataAutConfiguration ran
    This commit updates the conditions in Neo4jReactiveDataAutoConfiguration
    so that it gracefully backs off if certain beans are not present, rather
    than assuming its sibling Neo4jDataAutoConfiguration has run.
    
    Closes gh-44930

--- spring-boot-project/spring-boot-autoconfigure/src/test/java/org/springframework/boot/autoconfigure/data/neo4j/Neo4jReactiveDataAutoConfigurationTests.java
+++ spring-boot-project/spring-boot-autoconfigure/src/test/java/org/springframework/boot/autoconfigure/data/neo4j/Neo4jReactiveDataAutoConfigurationTests.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2012-2023 the original author or authors.
+ * Copyright 2012-2025 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -55,6 +55,14 @@ class Neo4jReactiveDataAutoConfigurationTests {
 		.withUserConfiguration(MockedDriverConfiguration.class)
 		.withConfiguration(AutoConfigurations.of(Neo4jAutoConfiguration.class, Neo4jDataAutoConfiguration.class,
 				Neo4jReactiveDataAutoConfiguration.class));
+
+	@Test
+	void shouldBackOffIfNoMappingContextIsProvided() {
+		new ApplicationContextRunner().withUserConfiguration(MockedDriverConfiguration.class)
+			.withConfiguration(
+					AutoConfigurations.of(Neo4jAutoConfiguration.class, Neo4jReactiveDataAutoConfiguration.class))
+			.run((context) -> assertThat(context).doesNotHaveBean(Neo4jMappingContext.class));
+	}
 
 	@Test
 	void shouldProvideDefaultDatabaseNameProvider() {
