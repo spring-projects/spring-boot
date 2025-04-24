0fb369e8b8f4af3354daf82c7da39016521b36aa
Moritz Halbritter <moritz.halbritter@broadcom.com>
2025-04-24 09:45:41 +0200

    Use paketobuildpacks/builder-noble-java-tiny as the default image builder
    Closes gh-42711

--- spring-boot-tests/spring-boot-integration-tests/spring-boot-sni-tests/src/intTest/java/org/springframework/boot/sni/SniIntegrationTests.java
+++ spring-boot-tests/spring-boot-integration-tests/spring-boot-sni-tests/src/intTest/java/org/springframework/boot/sni/SniIntegrationTests.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2012-2024 the original author or authors.
+ * Copyright 2012-2025 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -106,7 +106,7 @@ class SniIntegrationTests {
 
 		protected ApplicationContainer(String appName, String fileSuffix, String... entryPointArgs) {
 			super(new ImageFromDockerfile().withFileFromFile("spring-boot.jar", findJarFile(appName, fileSuffix))
-				.withDockerfileFromBuilder((builder) -> builder.from("eclipse-temurin:17-jre-jammy")
+				.withDockerfileFromBuilder((builder) -> builder.from("eclipse-temurin:17-jre-noble")
 					.add("spring-boot.jar", "/spring-boot.jar")
 					.entryPoint(buildEntryPoint(entryPointArgs))));
 			withExposedPorts(SERVER_PORT);
