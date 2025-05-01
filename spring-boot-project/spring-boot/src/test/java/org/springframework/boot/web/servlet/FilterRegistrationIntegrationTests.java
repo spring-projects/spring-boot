1897aab287d6dc1d724a43bc0bb420ae28cac0d8
Andy Wilkinson <andy.wilkinson@broadcom.com>
2025-05-01 13:40:49 +0100

    Create spring-boot-web-server module

--- spring-boot-project/spring-boot/src/test/java/org/springframework/boot/web/servlet/FilterRegistrationIntegrationTests.java
+++ spring-boot-project/spring-boot/src/test/java/org/springframework/boot/web/servlet/FilterRegistrationIntegrationTests.java
@@ -16,17 +16,26 @@
 
 package org.springframework.boot.web.servlet;
 
+import java.util.Collections;
+
 import jakarta.servlet.Filter;
+import jakarta.servlet.FilterRegistration.Dynamic;
+import jakarta.servlet.ServletContext;
+import jakarta.servlet.ServletException;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Test;
 
-import org.springframework.boot.web.server.servlet.MockServletWebServerFactory;
-import org.springframework.boot.web.server.servlet.context.AnnotationConfigServletWebServerApplicationContext;
+import org.springframework.boot.web.context.servlet.AnnotationConfigServletWebApplicationContext;
+import org.springframework.boot.web.context.servlet.WebApplicationContextInitializer;
 import org.springframework.boot.web.servlet.mock.MockFilter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.BDDMockito.given;
+import static org.mockito.BDDMockito.then;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.times;
 
 /**
  * Integration tests for {@link Filter} registration.
@@ -35,7 +44,7 @@ import static org.assertj.core.api.Assertions.assertThat;
  */
 class FilterRegistrationIntegrationTests {
 
-	private AnnotationConfigServletWebServerApplicationContext context;
+	private AnnotationConfigServletWebApplicationContext context;
 
 	@AfterEach
 	void cleanUp() {
@@ -47,28 +56,31 @@ class FilterRegistrationIntegrationTests {
 	@Test
 	void normalFiltersAreRegistered() {
 		load(FilterConfiguration.class);
-		assertThat(this.context.getServletContext().getFilterRegistrations()).hasSize(1);
+		then(this.context.getServletContext()).should()
+			.addFilter("myFilter", this.context.getBean("myFilter", MockFilter.class));
 	}
 
 	@Test
 	void scopedTargetFiltersAreNotRegistered() {
 		load(ScopedTargetFilterConfiguration.class);
-		assertThat(this.context.getServletContext().getFilterRegistrations()).isEmpty();
+		then(this.context.getServletContext()).should(times(0)).addFilter(any(String.class), any(Filter.class));
 	}
 
 	private void load(Class<?> configuration) {
-		this.context = new AnnotationConfigServletWebServerApplicationContext(ContainerConfiguration.class,
-				configuration);
-	}
-
-	@Configuration(proxyBeanMethods = false)
-	static class ContainerConfiguration {
-
-		@Bean
-		MockServletWebServerFactory webServerFactory() {
-			return new MockServletWebServerFactory();
+		ServletContext servletContext = mock(ServletContext.class);
+		given(servletContext.addFilter(any(), any(Filter.class))).willReturn(mock(Dynamic.class));
+		given(servletContext.getInitParameterNames()).willReturn(Collections.emptyEnumeration());
+		given(servletContext.getAttributeNames()).willReturn(Collections.emptyEnumeration());
+		this.context = new AnnotationConfigServletWebApplicationContext();
+		this.context.setServletContext(servletContext);
+		this.context.register(configuration);
+		this.context.refresh();
+		try {
+			new WebApplicationContextInitializer(this.context).initialize(servletContext);
 		}
-
+		catch (ServletException ex) {
+			throw new RuntimeException(ex);
+		}
 	}
 
 	@Configuration(proxyBeanMethods = false)
