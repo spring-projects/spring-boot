985c7eb6f5cf539061f97f75bef092aeae2058bc
Andy Wilkinson <andy.wilkinson@broadcom.com>
2025-05-08 10:36:48 +0100

    Tidy up tests in spring-boot-autoconfigure-all

--- spring-boot-project/spring-boot-autoconfigure/src/test/java/org/springframework/boot/autoconfigure/template/TemplateRuntimeHintsTests.java
+++ spring-boot-project/spring-boot-autoconfigure/src/test/java/org/springframework/boot/autoconfigure/template/TemplateRuntimeHintsTests.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2012-2023 the original author or authors.
+ * Copyright 2012-2025 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -25,6 +25,7 @@ import org.springframework.aot.hint.RuntimeHintsRegistrar;
 import org.springframework.aot.hint.predicate.RuntimeHintsPredicates;
 import org.springframework.beans.factory.aot.AotServices;
 import org.springframework.boot.test.context.FilteredClassLoader;
+import org.springframework.boot.testsupport.classpath.resources.WithResource;
 import org.springframework.core.io.ClassPathResource;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -46,8 +47,9 @@ class TemplateRuntimeHintsTests {
 	}
 
 	@Test
+	@WithResource(name = "templates/test.html")
 	void contributeWhenTemplateLocationExists() {
-		RuntimeHints runtimeHints = contribute(getClass().getClassLoader());
+		RuntimeHints runtimeHints = contribute(Thread.currentThread().getContextClassLoader());
 		assertThat(TEST_PREDICATE.test(runtimeHints)).isTrue();
 	}
 
