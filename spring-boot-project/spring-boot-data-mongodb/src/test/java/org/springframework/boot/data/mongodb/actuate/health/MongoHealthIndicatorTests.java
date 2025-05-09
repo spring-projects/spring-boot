c3447bc14812d620c41d510ab7d99b2077b73d5a
Andy Wilkinson <andy.wilkinson@broadcom.com>
2025-05-09 14:57:35 +0100

    Move code from spring-boot-actuator to spring-boot-data-mongodb

--- spring-boot-project/spring-boot-data-mongodb/src/test/java/org/springframework/boot/data/mongodb/actuate/health/MongoHealthIndicatorTests.java
+++ spring-boot-project/spring-boot-data-mongodb/src/test/java/org/springframework/boot/data/mongodb/actuate/health/MongoHealthIndicatorTests.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2012-2024 the original author or authors.
+ * Copyright 2012-2025 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -14,13 +14,12 @@
  * limitations under the License.
  */
 
-package org.springframework.boot.actuate.mongo;
+package org.springframework.boot.data.mongodb.actuate.health;
 
 import com.mongodb.MongoException;
 import org.bson.Document;
 import org.junit.jupiter.api.Test;
 
-import org.springframework.boot.actuate.data.mongo.MongoHealthIndicator;
 import org.springframework.boot.actuate.health.Health;
 import org.springframework.boot.actuate.health.Status;
 import org.springframework.data.mongodb.core.MongoTemplate;
