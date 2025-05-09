77d40382e4dd63d360f4eb6367e21823d4eda539
Andy Wilkinson <andy.wilkinson@broadcom.com>
2025-05-09 16:55:15 +0100

    Move code from spring-boot-actuator to spring-boot-ldap

--- spring-boot-project/spring-boot-actuator-autoconfigure/src/main/java/org/springframework/boot/actuate/autoconfigure/ldap/LdapHealthContributorAutoConfiguration.java
+++ spring-boot-project/spring-boot-actuator-autoconfigure/src/main/java/org/springframework/boot/actuate/autoconfigure/ldap/LdapHealthContributorAutoConfiguration.java
@@ -20,12 +20,12 @@ import org.springframework.beans.factory.config.ConfigurableListableBeanFactory;
 import org.springframework.boot.actuate.autoconfigure.health.CompositeHealthContributorConfiguration;
 import org.springframework.boot.actuate.autoconfigure.health.ConditionalOnEnabledHealthIndicator;
 import org.springframework.boot.actuate.health.HealthContributor;
-import org.springframework.boot.actuate.ldap.LdapHealthIndicator;
 import org.springframework.boot.autoconfigure.AutoConfiguration;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnBean;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
+import org.springframework.boot.ldap.actuate.health.LdapHealthIndicator;
 import org.springframework.context.annotation.Bean;
 import org.springframework.ldap.core.LdapOperations;
 
@@ -37,7 +37,7 @@ import org.springframework.ldap.core.LdapOperations;
  * @since 2.0.0
  */
 @AutoConfiguration(afterName = "org.springframework.boot.ldap.autoconfigure.LdapAutoConfiguration")
-@ConditionalOnClass(LdapOperations.class)
+@ConditionalOnClass({ LdapOperations.class, LdapHealthIndicator.class })
 @ConditionalOnBean(LdapOperations.class)
 @ConditionalOnEnabledHealthIndicator("ldap")
 public class LdapHealthContributorAutoConfiguration
