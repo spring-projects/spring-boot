2fa8d38aa75be200d51668fcee5f65b29f189f9e
Stéphane Nicoll <stephane.nicoll@broadcom.com>
2025-05-13 13:42:17 +0200

    Move web-specific FailureAnalyzer to a more focused package
    This commit moves PortInUseFailureAnalyzer to o.s.b.web.server as its
    current location means the whole general failure analyzer package has
    to know about web servers.
    
    Closes gh-45524

--- spring-boot-project/spring-boot/src/main/java/org/springframework/boot/web/server/PortInUseFailureAnalyzer.java
+++ spring-boot-project/spring-boot/src/main/java/org/springframework/boot/web/server/PortInUseFailureAnalyzer.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2012-2019 the original author or authors.
+ * Copyright 2012-2025 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -14,11 +14,10 @@
  * limitations under the License.
  */
 
-package org.springframework.boot.diagnostics.analyzer;
+package org.springframework.boot.web.server;
 
 import org.springframework.boot.diagnostics.AbstractFailureAnalyzer;
 import org.springframework.boot.diagnostics.FailureAnalysis;
-import org.springframework.boot.web.server.PortInUseException;
 
 /**
  * A {@code FailureAnalyzer} that performs analysis of failures caused by a
