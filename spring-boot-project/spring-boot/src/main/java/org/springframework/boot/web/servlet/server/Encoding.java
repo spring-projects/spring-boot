d8a9db534ff91d97e53e621d6aaec3b4169f133e
Stéphane Nicoll <stephane.nicoll@broadcom.com>
2025-05-13 13:16:32 +0200

    Apply ConfigurationPropertySource where possible
    See gh-18366

--- spring-boot-project/spring-boot/src/main/java/org/springframework/boot/web/servlet/server/Encoding.java
+++ spring-boot-project/spring-boot/src/main/java/org/springframework/boot/web/servlet/server/Encoding.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2012-2024 the original author or authors.
+ * Copyright 2012-2025 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -21,6 +21,8 @@ import java.nio.charset.StandardCharsets;
 import java.util.Locale;
 import java.util.Map;
 
+import org.springframework.boot.context.properties.ConfigurationPropertiesSource;
+
 /**
  * Configuration properties for server HTTP encoding.
  *
@@ -29,6 +31,7 @@ import java.util.Map;
  * @author Brian Clozel
  * @since 2.3.0
  */
+@ConfigurationPropertiesSource
 public class Encoding {
 
 	/**
--- spring-boot-project/spring-boot/src/main/java/org/springframework/boot/web/servlet/server/Session.java
+++ spring-boot-project/spring-boot/src/main/java/org/springframework/boot/web/servlet/server/Session.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2012-2024 the original author or authors.
+ * Copyright 2012-2025 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -21,6 +21,7 @@ import java.time.Duration;
 import java.time.temporal.ChronoUnit;
 import java.util.Set;
 
+import org.springframework.boot.context.properties.ConfigurationPropertiesSource;
 import org.springframework.boot.context.properties.NestedConfigurationProperty;
 import org.springframework.boot.convert.DurationUnit;
 
@@ -30,13 +31,23 @@ import org.springframework.boot.convert.DurationUnit;
  * @author Andy Wilkinson
  * @since 2.0.0
  */
+@ConfigurationPropertiesSource
 public class Session {
 
+	/**
+	 * Session timeout. If a duration suffix is not specified, seconds will be used.
+	 */
 	@DurationUnit(ChronoUnit.SECONDS)
 	private Duration timeout = Duration.ofMinutes(30);
 
+	/**
+	 * Session tracking modes.
+	 */
 	private Set<Session.SessionTrackingMode> trackingModes;
 
+	/**
+	 * Whether to persist session data between restarts.
+	 */
 	private boolean persistent;
 
 	/**
