e569d65eb450cee63cafc52e4f5d67687903b9e9
Andy Wilkinson <andy.wilkinson@broadcom.com>
2025-05-16 16:25:39 +0100

    Move Hazelcast health auto-configuration into spring-boot-hazelcast
    Issue: 46099

--- spring-boot-project/spring-boot-hazelcast/src/test/java/org/springframework/boot/hazelcast/actuate/health/autoconfigure/HazelcastHealthContributorAutoConfigurationIntegrationTests.java
+++ spring-boot-project/spring-boot-hazelcast/src/test/java/org/springframework/boot/hazelcast/actuate/health/autoconfigure/HazelcastHealthContributorAutoConfigurationIntegrationTests.java
@@ -14,11 +14,14 @@
  * limitations under the License.
  */
 
-package org.springframework.boot.actuate.autoconfigure.hazelcast;
+package org.springframework.boot.hazelcast.actuate.health.autoconfigure;
 
+import com.hazelcast.core.HazelcastInstance;
 import org.junit.jupiter.api.Test;
 
 import org.springframework.boot.actuate.autoconfigure.health.HealthContributorAutoConfiguration;
+import org.springframework.boot.actuate.health.Health;
+import org.springframework.boot.actuate.health.Status;
 import org.springframework.boot.autoconfigure.AutoConfigurations;
 import org.springframework.boot.hazelcast.actuate.health.HazelcastHealthIndicator;
 import org.springframework.boot.hazelcast.autoconfigure.HazelcastAutoConfiguration;
@@ -28,7 +31,7 @@ import org.springframework.boot.testsupport.classpath.resources.WithResource;
 import static org.assertj.core.api.Assertions.assertThat;
 
 /**
- * Tests for {@link HazelcastHealthContributorAutoConfiguration}.
+ * Integration tests for {@link HazelcastHealthContributorAutoConfiguration}.
  *
  * @author Dmytro Nosan
  */
@@ -46,21 +49,33 @@ import static org.assertj.core.api.Assertions.assertThat;
 			</network>
 		</hazelcast>
 		""")
-class HazelcastHealthContributorAutoConfigurationTests {
+class HazelcastHealthContributorAutoConfigurationIntegrationTests {
 
 	private final ApplicationContextRunner contextRunner = new ApplicationContextRunner()
-		.withConfiguration(AutoConfigurations.of(HazelcastAutoConfiguration.class,
-				HazelcastHealthContributorAutoConfiguration.class, HealthContributorAutoConfiguration.class));
+		.withConfiguration(AutoConfigurations.of(HazelcastHealthContributorAutoConfiguration.class,
+				HazelcastAutoConfiguration.class, HealthContributorAutoConfiguration.class));
 
 	@Test
-	void runShouldCreateIndicator() {
-		this.contextRunner.run((context) -> assertThat(context).hasSingleBean(HazelcastHealthIndicator.class));
+	void hazelcastUp() {
+		this.contextRunner.run((context) -> {
+			assertThat(context).hasSingleBean(HazelcastInstance.class).hasSingleBean(HazelcastHealthIndicator.class);
+			HazelcastInstance hazelcast = context.getBean(HazelcastInstance.class);
+			Health health = context.getBean(HazelcastHealthIndicator.class).health();
+			assertThat(health.getStatus()).isEqualTo(Status.UP);
+			assertThat(health.getDetails()).containsOnlyKeys("name", "uuid")
+				.containsEntry("name", hazelcast.getName())
+				.containsEntry("uuid", hazelcast.getLocalEndpoint().getUuid().toString());
+		});
 	}
 
 	@Test
-	void runWhenDisabledShouldNotCreateIndicator() {
-		this.contextRunner.withPropertyValues("management.health.hazelcast.enabled:false")
-			.run((context) -> assertThat(context).doesNotHaveBean(HazelcastHealthIndicator.class));
+	void hazelcastDown() {
+		this.contextRunner.run((context) -> {
+			context.getBean(HazelcastInstance.class).shutdown();
+			assertThat(context).hasSingleBean(HazelcastHealthIndicator.class);
+			Health health = context.getBean(HazelcastHealthIndicator.class).health();
+			assertThat(health.getStatus()).isEqualTo(Status.DOWN);
+		});
 	}
 
 }
