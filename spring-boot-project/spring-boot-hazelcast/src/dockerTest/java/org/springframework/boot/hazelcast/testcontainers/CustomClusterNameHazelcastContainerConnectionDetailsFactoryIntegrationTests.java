3e387b670a76dcb1ff84abf6bde8266b03fa7ed2
Stéphane Nicoll <stephane.nicoll@broadcom.com>
2025-05-19 15:57:38 +0200

    Move Hazelcast testcontainers support into spring-boot-hazelcast

--- spring-boot-project/spring-boot-hazelcast/src/dockerTest/java/org/springframework/boot/hazelcast/testcontainers/CustomClusterNameHazelcastContainerConnectionDetailsFactoryIntegrationTests.java
+++ spring-boot-project/spring-boot-hazelcast/src/dockerTest/java/org/springframework/boot/hazelcast/testcontainers/CustomClusterNameHazelcastContainerConnectionDetailsFactoryIntegrationTests.java
@@ -14,10 +14,13 @@
  * limitations under the License.
  */
 
-package org.springframework.boot.testcontainers.service.connection.hazelcast;
+package org.springframework.boot.hazelcast.testcontainers;
 
 import java.util.UUID;
+import java.util.function.Consumer;
 
+import com.hazelcast.client.config.ClientConfig;
+import com.hazelcast.client.impl.clientside.HazelcastClientProxy;
 import com.hazelcast.core.HazelcastInstance;
 import com.hazelcast.map.IMap;
 import org.junit.jupiter.api.Test;
@@ -37,17 +40,19 @@ import org.springframework.test.context.junit.jupiter.SpringJUnitConfig;
 import static org.assertj.core.api.Assertions.assertThat;
 
 /**
- * Tests for {@link HazelcastContainerConnectionDetailsFactory}.
+ * Tests for {@link HazelcastContainerConnectionDetailsFactory} with a custom hazelcast
+ * cluster name.
  *
  * @author Dmytro Nosan
  */
 @SpringJUnitConfig
 @Testcontainers(disabledWithoutDocker = true)
-class HazelcastContainerConnectionDetailsFactoryIntegrationTests {
+class CustomClusterNameHazelcastContainerConnectionDetailsFactoryIntegrationTests {
 
 	@Container
 	@ServiceConnection
-	static final HazelcastContainer hazelcast = TestImage.container(HazelcastContainer.class);
+	static final HazelcastContainer hazelcast = TestImage.container(HazelcastContainer.class)
+		.withClusterName("spring-boot");
 
 	@Autowired(required = false)
 	private HazelcastConnectionDetails connectionDetails;
@@ -58,9 +63,18 @@ class HazelcastContainerConnectionDetailsFactoryIntegrationTests {
 	@Test
 	void connectionCanBeMadeToHazelcastContainer() {
 		assertThat(this.connectionDetails).isNotNull();
+		assertThat(this.hazelcastInstance).satisfies(clusterName("spring-boot"));
 		IMap<String, String> map = this.hazelcastInstance.getMap(UUID.randomUUID().toString());
 		map.put("test", "containers");
 		assertThat(map.get("test")).isEqualTo("containers");
+	}
+
+	private static Consumer<HazelcastInstance> clusterName(String name) {
+		return (hazelcastInstance) -> {
+			assertThat(hazelcastInstance).isInstanceOf(HazelcastClientProxy.class);
+			HazelcastClientProxy proxy = (HazelcastClientProxy) hazelcastInstance;
+			assertThat(proxy.getClientConfig()).extracting(ClientConfig::getClusterName).isEqualTo(name);
+		};
 	}
 
 	@Configuration(proxyBeanMethods = false)
