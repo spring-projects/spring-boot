dda62b4f1798d5aa4b685facd1783424f6c5c6b3
Phillip Webb <phil.webb@broadcom.com>
2025-05-19 19:30:25 -0700

    Move LDAP Docker Compose support into spring-boot-ldap

--- spring-boot-project/spring-boot-ldap/src/main/java/org/springframework/boot/ldap/docker/compose/LLdapDockerComposeConnectionDetailsFactory.java
+++ spring-boot-project/spring-boot-ldap/src/main/java/org/springframework/boot/ldap/docker/compose/LLdapDockerComposeConnectionDetailsFactory.java
@@ -14,7 +14,7 @@
  * limitations under the License.
  */
 
-package org.springframework.boot.docker.compose.service.connection.ldap;
+package org.springframework.boot.ldap.docker.compose;
 
 import java.util.Map;
 
