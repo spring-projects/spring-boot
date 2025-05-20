49df0edf2a171e64318b01476ef4315f3b349f2f
Phillip Webb <phil.webb@broadcom.com>
2025-05-19 19:28:29 -0700

    Move Hazelcast Docker Compose support into spring-boot-hazelcast

--- spring-boot-project/spring-boot-hazelcast/src/dockerTest/java/org/springframework/boot/hazelcast/docker/compose/HazelcastDockerComposeConnectionDetailsFactoryIntegrationTests.java
+++ spring-boot-project/spring-boot-hazelcast/src/dockerTest/java/org/springframework/boot/hazelcast/docker/compose/HazelcastDockerComposeConnectionDetailsFactoryIntegrationTests.java
@@ -14,7 +14,7 @@
  * limitations under the License.
  */
 
-package org.springframework.boot.docker.compose.service.connection.hazelcast;
+package org.springframework.boot.hazelcast.docker.compose;
 
 import java.util.UUID;
 
