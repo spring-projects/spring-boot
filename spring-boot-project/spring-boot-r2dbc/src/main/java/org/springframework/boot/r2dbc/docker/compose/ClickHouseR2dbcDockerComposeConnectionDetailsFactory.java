2a26ce187a171f4f07ba2ecda11cc97907feb738
Phillip Webb <phil.webb@broadcom.com>
2025-05-19 19:34:03 -0700

    Move R2DBC Docker Compose support into spring-boot-r2dbc
    See gh-46094

---
+++ spring-boot-project/spring-boot-r2dbc/src/main/java/org/springframework/boot/r2dbc/docker/compose/ClickHouseR2dbcDockerComposeConnectionDetailsFactory.java
@@ -0,0 +1,72 @@
+/*
+ * Copyright 2012-present the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.springframework.boot.r2dbc.docker.compose;
+
+import io.r2dbc.spi.ConnectionFactoryOptions;
+
+import org.springframework.boot.docker.compose.core.RunningService;
+import org.springframework.boot.docker.compose.service.connection.DockerComposeConnectionDetailsFactory;
+import org.springframework.boot.docker.compose.service.connection.DockerComposeConnectionSource;
+import org.springframework.boot.r2dbc.autoconfigure.R2dbcConnectionDetails;
+
+/**
+ * {@link DockerComposeConnectionDetailsFactory} to create {@link R2dbcConnectionDetails}
+ * for a {@code clickhouse} service.
+ *
+ * @author Stephane Nicoll
+ */
+class ClickHouseR2dbcDockerComposeConnectionDetailsFactory
+		extends DockerComposeConnectionDetailsFactory<R2dbcConnectionDetails> {
+
+	private static final String[] CLICKHOUSE_CONTAINER_NAMES = { "clickhouse/clickhouse-server", "bitnami/clickhouse" };
+
+	ClickHouseR2dbcDockerComposeConnectionDetailsFactory() {
+		super(CLICKHOUSE_CONTAINER_NAMES, "io.r2dbc.spi.ConnectionFactoryOptions");
+	}
+
+	@Override
+	protected R2dbcConnectionDetails getDockerComposeConnectionDetails(DockerComposeConnectionSource source) {
+		return new ClickhouseDbR2dbcDockerComposeConnectionDetails(source.getRunningService());
+	}
+
+	/**
+	 * {@link R2dbcConnectionDetails} backed by a {@code clickhouse}
+	 * {@link RunningService}.
+	 */
+	static class ClickhouseDbR2dbcDockerComposeConnectionDetails extends DockerComposeConnectionDetails
+			implements R2dbcConnectionDetails {
+
+		private static final ConnectionFactoryOptionsBuilder connectionFactoryOptionsBuilder = new ConnectionFactoryOptionsBuilder(
+				"clickhouse", 8123);
+
+		private final ConnectionFactoryOptions connectionFactoryOptions;
+
+		ClickhouseDbR2dbcDockerComposeConnectionDetails(RunningService service) {
+			super(service);
+			ClickHouseEnvironment environment = new ClickHouseEnvironment(service.env());
+			this.connectionFactoryOptions = connectionFactoryOptionsBuilder.build(service, environment.getDatabase(),
+					environment.getUsername(), environment.getPassword());
+		}
+
+		@Override
+		public ConnectionFactoryOptions getConnectionFactoryOptions() {
+			return this.connectionFactoryOptions;
+		}
+
+	}
+
+}
