2a9de80686f9c8700fc526addb68cc3c9afcf932
Andy Wilkinson <andy.wilkinson@broadcom.com>
2025-05-21 17:36:21 +0100

    Invert dependency between actuator-autoconfigure and webmvc
    See gh-46071

---
+++ spring-boot-project/spring-boot-webmvc/src/test/java/org/springframework/boot/webmvc/actuate/autoconfigure/endpoint/web/WebMvcEndpointManagementContextConfigurationTests.java
@@ -0,0 +1,87 @@
+/*
+ * Copyright 2012-present the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.springframework.boot.webmvc.actuate.autoconfigure.endpoint.web;
+
+import java.util.Collections;
+
+import org.junit.jupiter.api.Test;
+
+import org.springframework.boot.actuate.autoconfigure.endpoint.web.WebEndpointProperties;
+import org.springframework.boot.actuate.endpoint.Access;
+import org.springframework.boot.actuate.endpoint.EndpointAccessResolver;
+import org.springframework.boot.actuate.endpoint.web.ServletEndpointRegistrar;
+import org.springframework.boot.actuate.endpoint.web.annotation.ServletEndpointsSupplier;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.boot.test.context.runner.ApplicationContextRunner;
+import org.springframework.boot.test.context.runner.WebApplicationContextRunner;
+import org.springframework.boot.webmvc.autoconfigure.DispatcherServletPath;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+/**
+ * Tests for {@link WebMvcEndpointManagementContextConfiguration}.
+ *
+ * @author Phillip Webb
+ * @author Madhura Bhave
+ */
+@SuppressWarnings("removal")
+class WebMvcEndpointManagementContextConfigurationTests {
+
+	private final WebApplicationContextRunner contextRunner = new WebApplicationContextRunner()
+		.withUserConfiguration(TestConfig.class);
+
+	@Test
+	void contextShouldContainServletEndpointRegistrar() {
+		this.contextRunner.run((context) -> {
+			assertThat(context).hasSingleBean(ServletEndpointRegistrar.class);
+			ServletEndpointRegistrar bean = context.getBean(ServletEndpointRegistrar.class);
+			assertThat(bean).hasFieldOrPropertyWithValue("basePath", "/test/actuator");
+		});
+	}
+
+	@Test
+	void contextWhenNotServletBasedShouldNotContainServletEndpointRegistrar() {
+		new ApplicationContextRunner().withUserConfiguration(TestConfig.class)
+			.run((context) -> assertThat(context).doesNotHaveBean(ServletEndpointRegistrar.class));
+	}
+
+	@Configuration(proxyBeanMethods = false)
+	@Import(WebMvcEndpointManagementContextConfiguration.class)
+	@EnableConfigurationProperties(WebEndpointProperties.class)
+	static class TestConfig {
+
+		@Bean
+		ServletEndpointsSupplier servletEndpointsSupplier() {
+			return Collections::emptyList;
+		}
+
+		@Bean
+		DispatcherServletPath dispatcherServletPath() {
+			return () -> "/test";
+		}
+
+		@Bean
+		EndpointAccessResolver endpointAccessResolver() {
+			return (endpointId, defaultAccess) -> Access.UNRESTRICTED;
+		}
+
+	}
+
+}
