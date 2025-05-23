43d2abbcc6284711070fcf9eb20249cedb0fe31c
Yanming Zhou <zhouyanming@gmail.com>
2025-05-23 11:01:55 +0800

    Harmonize Otlp*Configurations
    See gh-45643
    
    Signed-off-by: Yanming Zhou <zhouyanming@gmail.com>

--- spring-boot-project/spring-boot-actuator-autoconfigure/src/main/java/org/springframework/boot/actuate/autoconfigure/logging/otlp/OtlpLoggingConfigurations.java
+++ spring-boot-project/spring-boot-actuator-autoconfigure/src/main/java/org/springframework/boot/actuate/autoconfigure/logging/otlp/OtlpLoggingConfigurations.java
@@ -38,9 +38,6 @@ import org.springframework.util.Assert;
  */
 final class OtlpLoggingConfigurations {
 
-	private OtlpLoggingConfigurations() {
-	}
-
 	@Configuration(proxyBeanMethods = false)
 	static class ConnectionDetails {
 
