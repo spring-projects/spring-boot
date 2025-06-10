spring-projects/spring-boot

commit 020fd7b155e68f15cae03c1586da603b4c281d91

Add property to specify the management access log prefix
See gh-43434

spring-boot-project/spring-boot-actuator-autoconfigure/src/main/java/org/springframework/boot/actuate/autoconfigure/web/servlet/ServletManagementChildContextConfiguration.java
@@ -47,6 +47,7 @@ import org.springframework.boot.autoconfigure.web.embedded.UndertowWebServerFact
 import org.springframework.boot.autoconfigure.web.servlet.ServletWebServerFactoryCustomizer;
 import org.springframework.boot.autoconfigure.web.servlet.TomcatServletWebServerFactoryCustomizer;
 import org.springframework.boot.autoconfigure.web.servlet.UndertowServletWebServerFactoryCustomizer;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.boot.web.embedded.jetty.JettyServletWebServerFactory;
 import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
 import org.springframework.boot.web.embedded.undertow.UndertowServletWebServerFactory;
@@ -73,6 +74,7 @@ import org.springframework.util.StringUtils;
  */
 @ManagementContextConfiguration(value = ManagementContextType.CHILD, proxyBeanMethods = false)
 @ConditionalOnWebApplication(type = Type.SERVLET)
+@EnableConfigurationProperties(ManagementServerProperties.class)
 class ServletManagementChildContextConfiguration {
 
 	@Bean
@@ -83,20 +85,22 @@ class ServletManagementChildContextConfiguration {
 
 	@Bean
 	@ConditionalOnClass(name = "io.undertow.Undertow")
-	UndertowAccessLogCustomizer undertowManagementAccessLogCustomizer() {
-		return new UndertowAccessLogCustomizer();
+	UndertowAccessLogCustomizer undertowManagementAccessLogCustomizer(
+			ManagementServerProperties managementServerProperties) {
+		return new UndertowAccessLogCustomizer(managementServerProperties);
 	}
 
 	@Bean
 	@ConditionalOnClass(name = "org.apache.catalina.valves.AccessLogValve")
-	TomcatAccessLogCustomizer tomcatManagementAccessLogCustomizer() {
-		return new TomcatAccessLogCustomizer();
+	TomcatAccessLogCustomizer tomcatManagementAccessLogCustomizer(
+			ManagementServerProperties managementServerProperties) {
+		return new TomcatAccessLogCustomizer(managementServerProperties);
 	}
 
 	@Bean
 	@ConditionalOnClass(name = "org.eclipse.jetty.server.Server")
-	JettyAccessLogCustomizer jettyManagementAccessLogCustomizer() {
-		return new JettyAccessLogCustomizer();
+	JettyAccessLogCustomizer jettyManagementAccessLogCustomizer(ManagementServerProperties managementServerProperties) {
+		return new JettyAccessLogCustomizer(managementServerProperties);
 	}
 
 	@Configuration(proxyBeanMethods = false)
@@ -145,14 +149,18 @@ class ServletManagementChildContextConfiguration {
 
 	abstract static class AccessLogCustomizer implements Ordered {
 
-		private static final String MANAGEMENT_PREFIX = "management_";
+		protected final ManagementServerProperties managementServerProperties;
+
+		AccessLogCustomizer(ManagementServerProperties managementServerProperties) {
+			this.managementServerProperties = managementServerProperties;
+		}
 
 		protected String customizePrefix(String prefix) {
 			prefix = (prefix != null) ? prefix : "";
-			if (prefix.startsWith(MANAGEMENT_PREFIX)) {
+			if (prefix.startsWith(this.managementServerProperties.getAccesslog().getPrefix())) {
 				return prefix;
 			}
-			return MANAGEMENT_PREFIX + prefix;
+			return this.managementServerProperties.getAccesslog().getPrefix() + prefix;
 		}
 
 		@Override
@@ -165,12 +173,17 @@ class ServletManagementChildContextConfiguration {
 	static class TomcatAccessLogCustomizer extends AccessLogCustomizer
 			implements WebServerFactoryCustomizer<TomcatServletWebServerFactory> {
 
+		TomcatAccessLogCustomizer(ManagementServerProperties managementServerProperties) {
+			super(managementServerProperties);
+		}
+
 		@Override
 		public void customize(TomcatServletWebServerFactory factory) {
 			AccessLogValve accessLogValve = findAccessLogValve(factory);
 			if (accessLogValve == null) {
 				return;
 			}
+
 			accessLogValve.setPrefix(customizePrefix(accessLogValve.getPrefix()));
 		}
 
@@ -188,6 +201,10 @@ class ServletManagementChildContextConfiguration {
 	static class UndertowAccessLogCustomizer extends AccessLogCustomizer
 			implements WebServerFactoryCustomizer<UndertowServletWebServerFactory> {
 
+		UndertowAccessLogCustomizer(ManagementServerProperties managementServerProperties) {
+			super(managementServerProperties);
+		}
+
 		@Override
 		public void customize(UndertowServletWebServerFactory factory) {
 			factory.setAccessLogPrefix(customizePrefix(factory.getAccessLogPrefix()));
@@ -197,6 +214,10 @@ class ServletManagementChildContextConfiguration {
 
 	static class JettyAccessLogCustomizer extends AccessLogCustomizer
 			implements WebServerFactoryCustomizer<JettyServletWebServerFactory> {
+
+		JettyAccessLogCustomizer(ManagementServerProperties managementServerProperties) {
+			super(managementServerProperties);
+		}
 
 		@Override
 		public void customize(JettyServletWebServerFactory factory) {

spring-boot-project/spring-boot-actuator-autoconfigure/src/main/java/org/springframework/boot/actuate/autoconfigure/web/server/ManagementServerProperties.java
@@ -57,6 +57,8 @@ public class ManagementServerProperties {
 	@NestedConfigurationProperty
 	private Ssl ssl;
 
+	private final Accesslog accesslog = new Accesslog();
+
 	/**
 	 * Returns the management port or {@code null} if the
 	 * {@link ServerProperties#getPort() server port} should be used.
@@ -115,6 +117,28 @@ public class ManagementServerProperties {
 			}
 		}
 		return candidate;
+	}
+
+	public Accesslog getAccesslog() {
+		return this.accesslog;
+	}
+
+	public static class Accesslog {
+
+		/**
+		 * Enable management access logs prefix customization
+		 * management.server.accesslog.prefix.
+		 */
+		private String prefix = "management_";
+
+		public String getPrefix() {
+			return this.prefix;
+		}
+
+		public void setPrefix(String prefix) {
+			this.prefix = prefix;
+		}
+
 	}
 
 }

spring-boot-project/spring-boot-actuator-autoconfigure/src/test/java/org/springframework/boot/actuate/autoconfigure/web/server/ManagementServerPropertiesTests.java
@@ -68,4 +68,10 @@ class ManagementServerPropertiesTests {
 		assertThat(properties.getBasePath()).isEmpty();
 	}
 
+	@Test
+	void accessLogsArePrefixedByDefault() {
+		ManagementServerProperties properties = new ManagementServerProperties();
+		assertThat(properties.getAccesslog().getPrefix()).isEqualTo("management_");
+	}
+
 }

commit 9a1ca2fe83734fbce5fb3734dfc9b4a48284f536

Stop ignoring prefixed environment variables in management context
Binding in the child context does not work correctly when an
environment prefix has been configured. The prefix is not applied
to the child context's Environment and, therefore, prefixed
environment variables are ignored during binding.

We can fix the problem by reusing the parent context's
ManagementServerProperties rather than binding them again in the
child context. Doing so will fix the problem reported in gh-45857
that was introduced in 020fd7b and will also avoid an unnecessary
second binding of the properties.

gh-45858 may fix the problem more generally by applying the prefix
to the child context's environment. This would benefit situations
where the properties need to be bound in the child context because
they haven't already been bound in the parent.

Closes gh-45847

spring-boot-project/spring-boot-actuator-autoconfigure/src/main/java/org/springframework/boot/actuate/autoconfigure/web/servlet/ServletManagementChildContextConfiguration.java
@@ -39,7 +39,6 @@ import org.springframework.boot.autoconfigure.condition.ConditionalOnWebApplicat
 import org.springframework.boot.autoconfigure.condition.ConditionalOnWebApplication.Type;
 import org.springframework.boot.autoconfigure.condition.SearchStrategy;
 import org.springframework.boot.autoconfigure.web.ServerProperties;
-import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.boot.web.embedded.jetty.JettyServletWebServerFactory;
 import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
 import org.springframework.boot.web.embedded.undertow.UndertowServletWebServerFactory;
@@ -67,7 +66,6 @@ import org.springframework.util.StringUtils;
  */
 @ManagementContextConfiguration(value = ManagementContextType.CHILD, proxyBeanMethods = false)
 @ConditionalOnWebApplication(type = Type.SERVLET)
-@EnableConfigurationProperties(ManagementServerProperties.class)
 class ServletManagementChildContextConfiguration {
 
 	@Bean

spring-boot-project/spring-boot-actuator-autoconfigure/src/test/java/org/springframework/boot/actuate/autoconfigure/web/reactive/ReactiveManagementChildContextConfigurationTests.java
@@ -19,6 +19,8 @@ package org.springframework.boot.actuate.autoconfigure.web.reactive;
 import org.junit.jupiter.api.Test;
 
 import org.springframework.boot.actuate.autoconfigure.web.reactive.ReactiveManagementChildContextConfiguration.AccessLogCustomizer;
+import org.springframework.boot.actuate.autoconfigure.web.server.ManagementServerProperties;
+import org.springframework.boot.test.context.runner.ReactiveWebApplicationContextRunner;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -44,6 +46,26 @@ class ReactiveManagementChildContextConfigurationTests {
 		};
 		assertThat(customizer.customizePrefix(null)).isEqualTo(null);
 		assertThat(customizer.customizePrefix("existing")).isEqualTo("existing");
+	}
+
+	@Test
+	// gh-45857
+	void failsWithoutManagementServerPropertiesBeanFromParent() {
+		new ReactiveWebApplicationContextRunner().run((parent) -> {
+			new ReactiveWebApplicationContextRunner().withParent(parent)
+				.withUserConfiguration(ReactiveManagementChildContextConfiguration.class)
+				.run((context) -> assertThat(context).hasFailed());
+		});
+	}
+
+	@Test
+	// gh-45857
+	void succeedsWithManagementServerPropertiesBeanFromParent() {
+		new ReactiveWebApplicationContextRunner().withBean(ManagementServerProperties.class).run((parent) -> {
+			new ReactiveWebApplicationContextRunner().withParent(parent)
+				.withUserConfiguration(ReactiveManagementChildContextConfiguration.class)
+				.run((context) -> assertThat(context).hasNotFailed());
+		});
 	}
 
 }

spring-boot-project/spring-boot-actuator-autoconfigure/src/test/java/org/springframework/boot/actuate/autoconfigure/web/servlet/ServletManagementChildContextConfigurationTests.java
@@ -18,7 +18,9 @@ package org.springframework.boot.actuate.autoconfigure.web.servlet;
 
 import org.junit.jupiter.api.Test;
 
+import org.springframework.boot.actuate.autoconfigure.web.server.ManagementServerProperties;
 import org.springframework.boot.actuate.autoconfigure.web.servlet.ServletManagementChildContextConfiguration.AccessLogCustomizer;
+import org.springframework.boot.test.context.runner.WebApplicationContextRunner;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -44,6 +46,26 @@ class ServletManagementChildContextConfigurationTests {
 		};
 		assertThat(customizer.customizePrefix(null)).isEqualTo(null);
 		assertThat(customizer.customizePrefix("existing")).isEqualTo("existing");
+	}
+
+	@Test
+	// gh-45857
+	void failsWithoutManagementServerPropertiesBeanFromParent() {
+		new WebApplicationContextRunner().run((parent) -> {
+			new WebApplicationContextRunner().withParent(parent)
+				.withUserConfiguration(ServletManagementChildContextConfiguration.class)
+				.run((context) -> assertThat(context).hasFailed());
+		});
+	}
+
+	@Test
+	// gh-45857
+	void succeedsWithManagementServerPropertiesBeanFromParent() {
+		new WebApplicationContextRunner().withBean(ManagementServerProperties.class).run((parent) -> {
+			new WebApplicationContextRunner().withParent(parent)
+				.withUserConfiguration(ServletManagementChildContextConfiguration.class)
+				.run((context) -> assertThat(context).hasNotFailed());
+		});
 	}
 
 }