4996c70f48fc0e00936052c21cb8a368f0f47e49
Andy Wilkinson <andy.wilkinson@broadcom.com>
2025-06-12 09:19:59 +0100

    Upgrade to Maven Invoker 3.3.0
    Closes gh-45904

--- spring-boot-project/spring-boot-tools/spring-boot-maven-plugin/src/intTest/java/org/springframework/boot/maven/MavenBuild.java
+++ spring-boot-project/spring-boot-tools/spring-boot-maven-plugin/src/intTest/java/org/springframework/boot/maven/MavenBuild.java
@@ -167,7 +167,7 @@ class MavenBuild {
 			request.setBaseDirectory(this.temp);
 			request.setJavaHome(new File(System.getProperty("java.home")));
 			request.setProperties(this.properties);
-			request.setGoals(this.goals.isEmpty() ? Collections.singletonList("package") : this.goals);
+			request.addArgs(this.goals.isEmpty() ? Collections.singletonList("package") : this.goals);
 			request.setUserSettingsFile(new File(this.temp, "settings.xml"));
 			request.setUpdateSnapshots(true);
 			request.setBatchMode(true);
