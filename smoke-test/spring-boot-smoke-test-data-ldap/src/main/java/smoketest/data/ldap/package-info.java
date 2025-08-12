fab69e444b29a3bd7c235571eccd63173fe3a3b8
Moritz Halbritter <moritz.halbritter@broadcom.com>
2025-08-12 10:11:39 +0200

    Add nullability annotations to smoke-test/spring-boot-smoke-test-data-ldap
    See gh-46587

--- smoke-test/spring-boot-smoke-test-data-ldap/src/main/java/smoketest/data/ldap/package-info.java
+++ smoke-test/spring-boot-smoke-test-data-ldap/src/main/java/smoketest/data/ldap/package-info.java
@@ -14,28 +14,7 @@
  * limitations under the License.
  */
 
+@NullMarked
 package smoketest.data.ldap;
 
-import javax.naming.Name;
-
-import org.jspecify.annotations.Nullable;
-
-import org.springframework.ldap.odm.annotations.Attribute;
-import org.springframework.ldap.odm.annotations.Entry;
-import org.springframework.ldap.odm.annotations.Id;
-
-@Entry(objectClasses = { "person", "top" })
-public class Person {
-
-	@Id
-	private @Nullable Name dn;
-
-	@Attribute(name = "telephoneNumber")
-	private @Nullable String phone;
-
-	@Override
-	public String toString() {
-		return String.format("Customer[dn=%s, phone='%s']", this.dn, this.phone);
-	}
-
-}
+import org.jspecify.annotations.NullMarked;
