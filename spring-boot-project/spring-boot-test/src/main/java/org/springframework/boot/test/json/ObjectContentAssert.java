c7b659cb7e673c22d9778a5781c3597f2fd37cd9
Stefano Cordio <stefano.cordio@gmail.com>
2025-08-17 09:22:57 +0200

    Replace AssertJ internal APIs with alternative implementations
    See gh-46855
    
    Signed-off-by: Stefano Cordio <stefano.cordio@gmail.com>

--- spring-boot-project/spring-boot-test/src/main/java/org/springframework/boot/test/json/ObjectContentAssert.java
+++ spring-boot-project/spring-boot-test/src/main/java/org/springframework/boot/test/json/ObjectContentAssert.java
@@ -16,20 +16,18 @@
 
 package org.springframework.boot.test.json;
 
-import java.util.Map;
-
 import org.assertj.core.api.AbstractMapAssert;
 import org.assertj.core.api.AbstractObjectArrayAssert;
 import org.assertj.core.api.AbstractObjectAssert;
 import org.assertj.core.api.Assert;
-import org.assertj.core.api.Assertions;
-import org.assertj.core.internal.Objects;
+import org.assertj.core.api.InstanceOfAssertFactories;
 
 /**
  * AssertJ {@link Assert} for {@link ObjectContent}.
  *
  * @param <A> the actual type
  * @author Phillip Webb
+ * @author Stefano Cordio
  * @since 1.4.0
  */
 public class ObjectContentAssert<A> extends AbstractObjectAssert<ObjectContentAssert<A>, A> {
@@ -44,8 +42,7 @@ public class ObjectContentAssert<A> extends AbstractObjectAssert<ObjectContentAs
 	 * @return an array assertion object
 	 */
 	public AbstractObjectArrayAssert<?, Object> asArray() {
-		Objects.instance().assertIsInstanceOf(this.info, this.actual, Object[].class);
-		return Assertions.assertThat((Object[]) this.actual);
+		return asInstanceOf(InstanceOfAssertFactories.ARRAY);
 	}
 
 	/**
@@ -53,10 +50,8 @@ public class ObjectContentAssert<A> extends AbstractObjectAssert<ObjectContentAs
 	 * chaining of map-specific assertions from this call.
 	 * @return a map assertion object
 	 */
-	@SuppressWarnings("unchecked")
 	public AbstractMapAssert<?, ?, Object, Object> asMap() {
-		Objects.instance().assertIsInstanceOf(this.info, this.actual, Map.class);
-		return Assertions.assertThat((Map<Object, Object>) this.actual);
+		return asInstanceOf(InstanceOfAssertFactories.MAP);
 	}
 
 }
