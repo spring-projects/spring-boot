c0aafe67344cc046781cb1895da239d408a54328
adnan <adnan.siraz09@gmail.com>
2025-08-21 20:14:02 +0600

    added Calculator class with basic operations add sub multiplication divide

---
+++ buildSrc/src/main/java/org/springframework/boot/build/Calculator.java
@@ -0,0 +1,23 @@
+package org.springframework.boot.build;
+
+public class Calculator {
+
+    public double add(double a, double b) {
+        return a + b;
+    }
+
+    public double subtract(double a, double b) {
+        return a - b;
+    }
+
+    public double multiply(double a, double b) {
+        return a * b;
+    }
+
+    public double divide(double a, double b) {
+        if (b == 0) {
+            throw new IllegalArgumentException("Division by zero");
+        }
+        return a / b;
+    }
+}
