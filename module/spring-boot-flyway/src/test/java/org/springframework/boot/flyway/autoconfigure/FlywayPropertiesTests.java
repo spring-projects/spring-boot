cfb097e0b19c78faf00899008c6c21e2af9ff804
Chanwon-Seo <scwonn60@gmail.com>
2025-08-30 02:06:30 +0900

    Fix Correct assertion order for Flyway ignore migration patterns
     Reversed actual/expected comparison for ignoreMigrationPatterns, using type-safe ValidatePattern.
    
    Signed-off-by: Chanwon-Seo <scwonn60@gmail.com>

--- module/spring-boot-flyway/src/test/java/org/springframework/boot/flyway/autoconfigure/FlywayPropertiesTests.java
+++ module/spring-boot-flyway/src/test/java/org/springframework/boot/flyway/autoconfigure/FlywayPropertiesTests.java
@@ -30,6 +30,7 @@ import org.flywaydb.core.api.MigrationVersion;
 import org.flywaydb.core.api.configuration.ClassicConfiguration;
 import org.flywaydb.core.api.configuration.Configuration;
 import org.flywaydb.core.api.configuration.FluentConfiguration;
+import org.flywaydb.core.api.pattern.ValidatePattern;
 import org.junit.jupiter.api.Test;
 
 import org.springframework.beans.BeanWrapper;
@@ -95,8 +96,8 @@ class FlywayPropertiesTests {
 		assertThat(properties.getScriptPlaceholderSuffix()).isEqualTo(configuration.getScriptPlaceholderSuffix());
 		assertThat(properties.isExecuteInTransaction()).isEqualTo(configuration.isExecuteInTransaction());
 		assertThat(properties.getCommunityDbSupportEnabled()).isNull();
-		assertThat(configuration.getIgnoreMigrationPatterns()).extracting(Object::toString)
-			.isEqualTo(properties.getIgnoreMigrationPatterns());
+		assertThat(properties.getIgnoreMigrationPatterns().stream().map(ValidatePattern::fromPattern))
+			.containsExactly(configuration.getIgnoreMigrationPatterns());
 	}
 
 	@Test
