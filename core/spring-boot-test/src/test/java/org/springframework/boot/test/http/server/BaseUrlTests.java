79091f926d64a0062046a331bf9fab8052c75d10
Phillip Webb <phil.webb@broadcom.com>
2025-10-01 17:24:20 -0700

    Introduce `BaseUrl` and `BaseUrlProvider`
    Add the concept of a `BaseUrl` to the core `spring-boot-test`
    module for use when making test HTTP calls. The web server module
    provides `BaseUrlProvider` implementations that provide the actual
    base URL (usually `https://localhost:<local-server-port>`).
    
    Test utilities will be able to use `BaseUrlProviders` to find the
    `BaseUrl`.
    
    See gh-46356

---
+++ core/spring-boot-test/src/test/java/org/springframework/boot/test/http/server/BaseUrlTests.java
@@ -0,0 +1,85 @@
+/*
+ * Copyright 2012-present the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.springframework.boot.test.http.server;
+
+import java.util.concurrent.atomic.AtomicInteger;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatIllegalArgumentException;
+
+/**
+ * Tests for {@link BaseUrl}.
+ *
+ * @author Phillip Webb
+ */
+class BaseUrlTests {
+
+	@Test
+	void resolveWithString() {
+		assertThat(BaseUrl.of("http://localhost").resolve(null)).isEqualTo("http://localhost");
+		assertThat(BaseUrl.of("http://localhost").resolve("")).isEqualTo("http://localhost");
+		assertThat(BaseUrl.of("http://localhost").resolve("path")).isEqualTo("http://localhost/path");
+		assertThat(BaseUrl.of("http://localhost").resolve("/path")).isEqualTo("http://localhost/path");
+		assertThat(BaseUrl.of("http://localhost/").resolve("path")).isEqualTo("http://localhost/path");
+		assertThat(BaseUrl.of("http://localhost/").resolve("/path")).isEqualTo("http://localhost/path");
+	}
+
+	@Test
+	void ofWhenHttp() {
+		BaseUrl baseUrl = BaseUrl.of("http://localhost:8080/context");
+		assertThat(baseUrl.isHttps()).isFalse();
+		assertThat(baseUrl.resolve()).isEqualTo("http://localhost:8080/context");
+	}
+
+	@Test
+	void ofWhenHttps() {
+		BaseUrl baseUrl = BaseUrl.of("https://localhost:8080/context");
+		assertThat(baseUrl.isHttps()).isTrue();
+		assertThat(baseUrl.resolve()).isEqualTo("https://localhost:8080/context");
+	}
+
+	@Test
+	void ofWhenUppercaseHttps() {
+		BaseUrl baseUrl = BaseUrl.of("HTTPS://localhost:8080/context");
+		assertThat(baseUrl.isHttps()).isTrue();
+		assertThat(baseUrl.resolve()).isEqualTo("HTTPS://localhost:8080/context");
+	}
+
+	@Test
+	void ofWhenUrlIssNull() {
+		assertThatIllegalArgumentException().isThrownBy(() -> BaseUrl.of(null)).withMessage("'url' must not be null");
+	}
+
+	@Test
+	void of() {
+		AtomicInteger atomicInteger = new AtomicInteger();
+		BaseUrl baseUrl = BaseUrl.of(true, () -> String.valueOf(atomicInteger.incrementAndGet()));
+		assertThat(atomicInteger.get()).isZero();
+		assertThat(baseUrl.isHttps()).isTrue();
+		assertThat(baseUrl.resolve()).isEqualTo("1");
+		assertThat(baseUrl.resolve()).isEqualTo("2");
+	}
+
+	@Test
+	void ofWhenResolverIsNull() {
+		assertThatIllegalArgumentException().isThrownBy(() -> BaseUrl.of(true, null))
+			.withMessage("'resolver' must not be null");
+	}
+
+}
