spring-projects/spring-boot

commit c14b57a3eba14c10251afbfdbea221891b43259b

Check scoped proxy target for isDefaultCandidate
Update `OnBeanCondition` to check the scoped proxy target for
`isDefaultCandidate`. This fixes an issue introduced by
https://github.com/spring-projects/spring-framework/pull/35627.

Fixes gh-47633

spring-boot-project/spring-boot-autoconfigure/src/main/java/org/springframework/boot/autoconfigure/condition/OnBeanCondition.java
@@ -270,23 +270,24 @@ class OnBeanCondition extends FilteringSpringBootCondition implements Configurat
 
 	private boolean isCandidate(ConfigurableListableBeanFactory beanFactory, String name, BeanDefinition definition,
 			Set<String> ignoredBeans) {
-		return (!ignoredBeans.contains(name)) && (definition == null
-				|| isAutowireCandidate(beanFactory, name, definition) && isDefaultCandidate(definition));
-	}
-
-	private boolean isAutowireCandidate(ConfigurableListableBeanFactory beanFactory, String name,
-			BeanDefinition definition) {
-		return definition.isAutowireCandidate() || isScopeTargetAutowireCandidate(beanFactory, name);
-	}
-
-	private boolean isScopeTargetAutowireCandidate(ConfigurableListableBeanFactory beanFactory, String name) {
-		try {
-			return ScopedProxyUtils.isScopedTarget(name)
-					&& beanFactory.getBeanDefinition(ScopedProxyUtils.getOriginalBeanName(name)).isAutowireCandidate();
-		}
-		catch (NoSuchBeanDefinitionException ex) {
+		if (ignoredBeans.contains(name) || definition == null) {
 			return false;
 		}
+		if (definition.isAutowireCandidate() && isDefaultCandidate(definition)) {
+			return true;
+		}
+		if (ScopedProxyUtils.isScopedTarget(name)) {
+			try {
+				BeanDefinition originalDefinition = beanFactory
+					.getBeanDefinition(ScopedProxyUtils.getOriginalBeanName(name));
+				if (originalDefinition.isAutowireCandidate() && isDefaultCandidate(originalDefinition)) {
+					return true;
+				}
+			}
+			catch (NoSuchBeanDefinitionException ex) {
+			}
+		}
+		return false;
 	}
 
 	private boolean isDefaultCandidate(BeanDefinition definition) {

commit 682ad665f51ede9d33bcb934e355f52b5e1ce218

Fix 'Check scoped proxy target for isDefaultCandidate'
Fix accidental logic issue introduced in commit c14b57a3eb.

See gh-47633

spring-boot-project/spring-boot-autoconfigure/src/main/java/org/springframework/boot/autoconfigure/condition/OnBeanCondition.java
@@ -270,10 +270,10 @@ class OnBeanCondition extends FilteringSpringBootCondition implements Configurat
 
 	private boolean isCandidate(ConfigurableListableBeanFactory beanFactory, String name, BeanDefinition definition,
 			Set<String> ignoredBeans) {
-		if (ignoredBeans.contains(name) || definition == null) {
+		if (ignoredBeans.contains(name)) {
 			return false;
 		}
-		if (definition.isAutowireCandidate() && isDefaultCandidate(definition)) {
+		if (definition == null || (definition.isAutowireCandidate() && isDefaultCandidate(definition))) {
 			return true;
 		}
 		if (ScopedProxyUtils.isScopedTarget(name)) {