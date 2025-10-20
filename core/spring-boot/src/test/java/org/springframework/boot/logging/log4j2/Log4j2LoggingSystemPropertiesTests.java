2e0bb46c15b452be1ef6ce6914b03499c2685dcf
hojooo <ghwn5833@gmail.com>
2025-10-21 00:48:20 +0900

    Refactor Log4j2LoggingSystemPropertiesTests to verify System.setProperty path
    Signed-off-by: hojooo <ghwn5833@gmail.com>

--- core/spring-boot/src/test/java/org/springframework/boot/logging/log4j2/Log4j2LoggingSystemPropertiesTests.java
+++ core/spring-boot/src/test/java/org/springframework/boot/logging/log4j2/Log4j2LoggingSystemPropertiesTests.java
@@ -16,17 +16,18 @@
 
 package org.springframework.boot.logging.log4j2;
 
-import java.util.LinkedHashMap;
-import java.util.Map;
-import java.util.function.BiConsumer;
+import java.util.HashSet;
+import java.util.Set;
 
 import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import org.springframework.boot.convert.ApplicationConversionService;
 import org.springframework.boot.logging.LogFile;
-import org.springframework.core.env.Environment;
-import org.springframework.core.env.MapPropertySource;
-import org.springframework.core.env.StandardEnvironment;
+import org.springframework.boot.logging.LoggingSystemProperty;
+import org.springframework.core.convert.support.ConfigurableConversionService;
+import org.springframework.mock.env.MockEnvironment;
 import org.springframework.util.unit.DataSize;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -38,86 +39,88 @@ import static org.assertj.core.api.Assertions.assertThat;
  */
 class Log4j2LoggingSystemPropertiesTests {
 
-	private final Map<String, String> systemProperties = new LinkedHashMap<>();
+	private Set<Object> systemPropertyNames;
 
-	private final BiConsumer<String, String> systemPropertySetter = this.systemProperties::put;
+	private MockEnvironment environment;
+
+	@BeforeEach
+	void captureSystemPropertyNames() {
+		for (LoggingSystemProperty property : LoggingSystemProperty.values()) {
+			System.getProperties().remove(property.getEnvironmentVariableName());
+		}
+		this.systemPropertyNames = new HashSet<>(System.getProperties().keySet());
+		this.environment = new MockEnvironment();
+		this.environment
+			.setConversionService((ConfigurableConversionService) ApplicationConversionService.getSharedInstance());
+	}
 
 	@AfterEach
-	void clearSystemProperties() {
-		this.systemProperties.clear();
+	void restoreSystemProperties() {
+		System.getProperties().keySet().retainAll(this.systemPropertyNames);
 	}
 
 	@Test
 	void appliesLog4j2RollingPolicyProperties() {
-		Environment environment = new StandardEnvironment();
-		addPropertiesToEnvironment(environment, "logging.log4j2.rollingpolicy.max-file-size", "50MB",
-				"logging.log4j2.rollingpolicy.clean-history-on-start", "true",
-				"logging.log4j2.rollingpolicy.max-history", "30", "logging.log4j2.rollingpolicy.total-size-cap", "10GB",
-				"logging.log4j2.rollingpolicy.file-name-pattern", "test.%d{yyyy-MM-dd}.%i.log",
-				"logging.log4j2.rollingpolicy.strategy", "time", "logging.log4j2.rollingpolicy.time-based.interval",
-				"2", "logging.log4j2.rollingpolicy.time-based.modulate", "true",
-				"logging.log4j2.rollingpolicy.cron.schedule", "0 0 0 * * ?");
-		new Log4j2LoggingSystemProperties(environment, this.systemPropertySetter).apply(null);
-		assertThat(this.systemProperties).containsEntry("LOG4J2_ROLLINGPOLICY_MAX_FILE_SIZE",
-				String.valueOf(DataSize.ofMegabytes(50).toBytes()));
-		assertThat(this.systemProperties).containsEntry("LOG4J2_ROLLINGPOLICY_CLEAN_HISTORY_ON_START", "true");
-		assertThat(this.systemProperties).containsEntry("LOG4J2_ROLLINGPOLICY_MAX_HISTORY", "30");
-		assertThat(this.systemProperties).containsEntry("LOG4J2_ROLLINGPOLICY_TOTAL_SIZE_CAP",
-				String.valueOf(DataSize.ofGigabytes(10).toBytes()));
-		assertThat(this.systemProperties).containsEntry("LOG4J2_ROLLINGPOLICY_FILE_NAME_PATTERN",
-				"test.%d{yyyy-MM-dd}.%i.log");
-		assertThat(this.systemProperties).containsEntry("LOG4J2_ROLLINGPOLICY_STRATEGY", "time");
-		assertThat(this.systemProperties).containsEntry("LOG4J2_ROLLINGPOLICY_TIME_INTERVAL", "2");
-		assertThat(this.systemProperties).containsEntry("LOG4J2_ROLLINGPOLICY_TIME_MODULATE", "true");
-		assertThat(this.systemProperties).containsEntry("LOG4J2_ROLLINGPOLICY_CRON_SCHEDULE", "0 0 0 * * ?");
+		this.environment.setProperty("logging.log4j2.rollingpolicy.max-file-size", "50MB");
+		this.environment.setProperty("logging.log4j2.rollingpolicy.clean-history-on-start", "true");
+		this.environment.setProperty("logging.log4j2.rollingpolicy.max-history", "30");
+		this.environment.setProperty("logging.log4j2.rollingpolicy.total-size-cap", "10GB");
+		this.environment.setProperty("logging.log4j2.rollingpolicy.file-name-pattern", "test.%d{yyyy-MM-dd}.%i.log");
+		this.environment.setProperty("logging.log4j2.rollingpolicy.strategy", "time");
+		this.environment.setProperty("logging.log4j2.rollingpolicy.time-based.interval", "2");
+		this.environment.setProperty("logging.log4j2.rollingpolicy.time-based.modulate", "true");
+		this.environment.setProperty("logging.log4j2.rollingpolicy.cron.schedule", "0 0 0 * * ?");
+
+		new Log4j2LoggingSystemProperties(this.environment).apply(null);
+
+		assertThat(System.getProperties())
+			.containsEntry("LOG4J2_ROLLINGPOLICY_MAX_FILE_SIZE", String.valueOf(DataSize.ofMegabytes(50).toBytes()))
+			.containsEntry("LOG4J2_ROLLINGPOLICY_CLEAN_HISTORY_ON_START", "true")
+			.containsEntry("LOG4J2_ROLLINGPOLICY_MAX_HISTORY", "30")
+			.containsEntry("LOG4J2_ROLLINGPOLICY_TOTAL_SIZE_CAP", String.valueOf(DataSize.ofGigabytes(10).toBytes()))
+			.containsEntry("LOG4J2_ROLLINGPOLICY_FILE_NAME_PATTERN", "test.%d{yyyy-MM-dd}.%i.log")
+			.containsEntry("LOG4J2_ROLLINGPOLICY_STRATEGY", "time")
+			.containsEntry("LOG4J2_ROLLINGPOLICY_TIME_INTERVAL", "2")
+			.containsEntry("LOG4J2_ROLLINGPOLICY_TIME_MODULATE", "true")
+			.containsEntry("LOG4J2_ROLLINGPOLICY_CRON_SCHEDULE", "0 0 0 * * ?");
 	}
 
 	@Test
 	void appliesLog4j2RollingPolicyPropertiesWithDefaults() {
-		Environment environment = new StandardEnvironment();
-		new Log4j2LoggingSystemProperties(environment, this.systemPropertySetter).apply(null);
-		// Should not set any rolling policy properties when not configured
-		assertThat(this.systemProperties.keySet()).noneMatch((key) -> key.startsWith("LOG4J2_ROLLINGPOLICY"));
+		new Log4j2LoggingSystemProperties(this.environment).apply(null);
+		assertThat(System.getProperties().keySet())
+			.noneMatch((key) -> ((String) key).startsWith("LOG4J2_ROLLINGPOLICY"));
 	}
 
 	@Test
 	void appliesDeprecatedProperties() {
-		Environment environment = new StandardEnvironment();
-		addPropertiesToEnvironment(environment, "logging.file.max-size", "20MB", "logging.file.max-history", "15");
-		new Log4j2LoggingSystemProperties(environment, this.systemPropertySetter).apply(null);
-		assertThat(this.systemProperties).containsEntry("LOG4J2_ROLLINGPOLICY_MAX_FILE_SIZE",
-				String.valueOf(DataSize.ofMegabytes(20).toBytes()));
-		assertThat(this.systemProperties).containsEntry("LOG4J2_ROLLINGPOLICY_MAX_HISTORY", "15");
+		this.environment.setProperty("logging.file.max-size", "20MB");
+		this.environment.setProperty("logging.file.max-history", "15");
+		new Log4j2LoggingSystemProperties(this.environment).apply(null);
+		assertThat(System.getProperties())
+			.containsEntry("LOG4J2_ROLLINGPOLICY_MAX_FILE_SIZE", String.valueOf(DataSize.ofMegabytes(20).toBytes()))
+			.containsEntry("LOG4J2_ROLLINGPOLICY_MAX_HISTORY", "15");
 	}
 
 	@Test
 	void newPropertiesOverrideDeprecatedProperties() {
-		Environment environment = new StandardEnvironment();
-		addPropertiesToEnvironment(environment, "logging.log4j2.rollingpolicy.max-file-size", "100MB",
-				"logging.file.max-size", "20MB", "logging.log4j2.rollingpolicy.max-history", "50",
-				"logging.file.max-history", "15");
-		new Log4j2LoggingSystemProperties(environment, this.systemPropertySetter).apply(null);
-		assertThat(this.systemProperties).containsEntry("LOG4J2_ROLLINGPOLICY_MAX_FILE_SIZE",
-				String.valueOf(DataSize.ofMegabytes(100).toBytes()));
-		assertThat(this.systemProperties).containsEntry("LOG4J2_ROLLINGPOLICY_MAX_HISTORY", "50");
+		this.environment.setProperty("logging.log4j2.rollingpolicy.max-file-size", "100MB");
+		this.environment.setProperty("logging.file.max-size", "20MB");
+		this.environment.setProperty("logging.log4j2.rollingpolicy.max-history", "50");
+		this.environment.setProperty("logging.file.max-history", "15");
+		new Log4j2LoggingSystemProperties(this.environment).apply(null);
+		assertThat(System.getProperties())
+			.containsEntry("LOG4J2_ROLLINGPOLICY_MAX_FILE_SIZE", String.valueOf(DataSize.ofMegabytes(100).toBytes()))
+			.containsEntry("LOG4J2_ROLLINGPOLICY_MAX_HISTORY", "50");
 	}
 
 	@Test
 	void appliesWithLogFile() {
-		Environment environment = new StandardEnvironment();
-		addPropertiesToEnvironment(environment, "logging.log4j2.rollingpolicy.max-file-size", "25MB");
-		LogFile logFile = LogFile.get(environment);
-		new Log4j2LoggingSystemProperties(environment, this.systemPropertySetter).apply(logFile);
-		assertThat(this.systemProperties).containsEntry("LOG4J2_ROLLINGPOLICY_MAX_FILE_SIZE",
-				String.valueOf(DataSize.ofMegabytes(25).toBytes()));
-	}
-
-	private void addPropertiesToEnvironment(Environment environment, String... pairs) {
-		Map<String, Object> map = new LinkedHashMap<>();
-		for (int i = 0; i < pairs.length; i += 2) {
-			map.put(pairs[i], pairs[i + 1]);
-		}
-		((StandardEnvironment) environment).getPropertySources().addFirst(new MapPropertySource("test", map));
+		this.environment.setProperty("logging.log4j2.rollingpolicy.max-file-size", "25MB");
+		LogFile logFile = LogFile.get(this.environment);
+		new Log4j2LoggingSystemProperties(this.environment).apply(logFile);
+		assertThat(System.getProperties())
+			.containsEntry("LOG4J2_ROLLINGPOLICY_MAX_FILE_SIZE", String.valueOf(DataSize.ofMegabytes(25).toBytes()));
 	}
 
 }
