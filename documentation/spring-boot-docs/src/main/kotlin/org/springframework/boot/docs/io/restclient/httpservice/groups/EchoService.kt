423373ba06ce604faa5cd0aa5b41887a27b9cd64
jwalter <joakim.waltersson@gmail.com>
2025-12-18 14:53:54 +0100

    Harmonize Kotlin example for HTTP Service client support
    See gh-48577
    
    Signed-off-by: jwalter <joakim.waltersson@gmail.com>

--- documentation/spring-boot-docs/src/main/kotlin/org/springframework/boot/docs/io/restclient/httpservice/groups/EchoService.kt
+++ documentation/spring-boot-docs/src/main/kotlin/org/springframework/boot/docs/io/restclient/httpservice/groups/EchoService.kt
@@ -19,7 +19,6 @@ import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.service.annotation.HttpExchange
 import org.springframework.web.service.annotation.PostExchange
 
-@HttpExchange("echo")
 interface EchoService {
 
 	@PostExchange
