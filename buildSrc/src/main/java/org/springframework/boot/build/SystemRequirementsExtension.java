a749b2fd5818e521b71fd684732f435d74c3f021
Andy Wilkinson <andy.wilkinson@broadcom.com>
2026-01-06 12:25:53 +0000

    Allow projects to configure their system requirements
    Closes gh-48684

---
+++ buildSrc/src/main/java/org/springframework/boot/build/SystemRequirementsExtension.java
@@ -0,0 +1,58 @@
+/*
+ * Copyright 2026 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.springframework.boot.build;
+
+import javax.inject.Inject;
+
+import org.gradle.api.Action;
+import org.gradle.api.model.ObjectFactory;
+import org.gradle.api.provider.Property;
+import org.gradle.jvm.toolchain.JavaLanguageVersion;
+
+/**
+ * DSL extension for configuring a project's system requirements.
+ *
+ * @author Andy Wilkinson
+ */
+public class SystemRequirementsExtension {
+
+	private final JavaSpec javaSpec;
+
+	@Inject
+	public SystemRequirementsExtension(ObjectFactory objects) {
+		this.javaSpec = objects.newInstance(JavaSpec.class);
+	}
+
+	public void java(Action<JavaSpec> action) {
+		action.execute(this.javaSpec);
+	}
+
+	public JavaSpec getJava() {
+		return this.javaSpec;
+	}
+
+	public abstract static class JavaSpec {
+
+		public JavaSpec() {
+			getVersion().convention(JavaLanguageVersion.of(17));
+		}
+
+		public abstract Property<JavaLanguageVersion> getVersion();
+
+	}
+
+}
