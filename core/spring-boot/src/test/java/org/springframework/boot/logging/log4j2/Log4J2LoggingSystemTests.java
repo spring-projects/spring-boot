7a7a75850939f2928de73896f35f764ace6e741a
Fabrice Bibonne <fabrice.bibonne@courriel.eco>
2026-02-28 22:20:54 +0100

    refactor: change tests after constructor Log4J2LoggingSystem.<init>(ClassLoader, LoggerContext) removal - remove LoggerContext from LoggerContextFactory after each unit test to avoid that it is reused
    Signed-off-by: Fabrice Bibonne <fabrice.bibonne@courriel.eco>

--- core/spring-boot/src/test/java/org/springframework/boot/logging/log4j2/Log4J2LoggingSystemTests.java
+++ core/spring-boot/src/test/java/org/springframework/boot/logging/log4j2/Log4J2LoggingSystemTests.java
@@ -116,6 +116,7 @@ class Log4J2LoggingSystemTests extends AbstractLoggingSystemTests {
 		this.loggingSystem.getConfiguration().stop();
 		this.loggingSystem.cleanUp();
 		PluginRegistry.getInstance().clear();
+		LogManager.getFactory().removeContext(this.loggingSystem.getLoggerContext());
 	}
 
 	@Test
@@ -127,7 +128,7 @@ class Log4J2LoggingSystemTests extends AbstractLoggingSystemTests {
 		Configuration configuration = this.loggingSystem.getConfiguration();
 		assertThat(output).contains("Hello world").doesNotContain("Hidden");
 		assertThat(new File(tmpDir() + "/spring.log")).doesNotExist();
-		assertThat(configuration.getConfigurationSource().getFile()).isNotNull();
+		assertThat(configuration.getConfigurationSource().getLocation()).contains("log4j2-test.xml");
 	}
 
 	@Test
