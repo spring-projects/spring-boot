d01d50d99c21f6c1a0f9f30236aaccb8b068a7ba
Eddú Meléndez <eddu.melendez@gmail.com>
2026-03-19 11:39:33 -0600

    Add support for RabbitMQ AMQP 1.0
    * Add support for RabbitMQ AMQP 1.0
    * Add smoke test for RabbitMQ AMQP 1.0
    
    Signed-off-by: Eddú Meléndez <eddu.melendez@gmail.com>

---
+++ module/spring-boot-amqp-rabbitmq/src/main/java/org/springframework/boot/amqp/docker/compose/RabbitDockerComposeConnectionDetailsFactory.java
@@ -0,0 +1,91 @@
+/*
+ * Copyright 2012-present the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.springframework.boot.amqp.docker.compose;
+
+import org.jspecify.annotations.Nullable;
+
+import org.springframework.boot.amqp.autoconfigure.RabbitAmqpConnectionDetails;
+import org.springframework.boot.docker.compose.core.RunningService;
+import org.springframework.boot.docker.compose.service.connection.DockerComposeConnectionDetailsFactory;
+import org.springframework.boot.docker.compose.service.connection.DockerComposeConnectionSource;
+
+/**
+ * {@link DockerComposeConnectionDetailsFactory} to create
+ * {@link RabbitAmqpConnectionDetails} for a {@code rabbitmq} service.
+ *
+ * @author Eddú Meléndez
+ */
+class RabbitDockerComposeConnectionDetailsFactory
+		extends DockerComposeConnectionDetailsFactory<RabbitAmqpConnectionDetails> {
+
+	private static final int RABBITMQ_PORT = 5672;
+
+	protected RabbitDockerComposeConnectionDetailsFactory() {
+		super("rabbitmq");
+	}
+
+	@Override
+	protected @Nullable RabbitAmqpConnectionDetails getDockerComposeConnectionDetails(
+			DockerComposeConnectionSource source) {
+		try {
+			return new RabbitAmqpDockerComposeConnectionDetails(source.getRunningService());
+		}
+		catch (IllegalStateException ex) {
+			return null;
+		}
+	}
+
+	/**
+	 * {@link RabbitAmqpConnectionDetails} backed by a {@code rabbitmq}
+	 * {@link RunningService}.
+	 */
+	static class RabbitAmqpDockerComposeConnectionDetails extends DockerComposeConnectionDetails
+			implements RabbitAmqpConnectionDetails {
+
+		private final RabbitEnvironment environment;
+
+		private final Address address;
+
+		protected RabbitAmqpDockerComposeConnectionDetails(RunningService service) {
+			super(service);
+			this.environment = new RabbitEnvironment(service.env());
+			this.address = new Address(service.host(), service.ports().get(RABBITMQ_PORT));
+		}
+
+		@Override
+		public @Nullable String getUsername() {
+			return this.environment.getUsername();
+		}
+
+		@Override
+		public @Nullable String getPassword() {
+			return this.environment.getPassword();
+		}
+
+		@Override
+		public String getVirtualHost() {
+			return "/";
+		}
+
+		@Override
+		public Address getAddress() {
+			return this.address;
+		}
+
+	}
+
+}
