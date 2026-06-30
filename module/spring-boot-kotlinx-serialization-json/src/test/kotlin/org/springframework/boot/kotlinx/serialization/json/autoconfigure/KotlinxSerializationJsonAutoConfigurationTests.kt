22eef972934f7c1bfdb85accf29d136b0c65a5b7
Stéphane Nicoll <stephane.nicoll@broadcom.com>
2026-06-30 10:04:39 +0200

    Add missing or outdated copyright headers
    See gh-50894

--- module/spring-boot-kotlinx-serialization-json/src/test/kotlin/org/springframework/boot/kotlinx/serialization/json/autoconfigure/KotlinxSerializationJsonAutoConfigurationTests.kt
+++ module/spring-boot-kotlinx-serialization-json/src/test/kotlin/org/springframework/boot/kotlinx/serialization/json/autoconfigure/KotlinxSerializationJsonAutoConfigurationTests.kt
@@ -1,7 +1,7 @@
 /*
  * Copyright 2012-present the original author or authors.
  *
- * Licensed under the Apache License, Version 2.0 (the "License")
+ * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
