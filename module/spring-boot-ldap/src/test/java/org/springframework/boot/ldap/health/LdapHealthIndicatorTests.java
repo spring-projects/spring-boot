spring-projects/spring-boot

commit d26b6895efe7e0c2bc69d1b4cd69746a6a26f3f1

Start building against Spring LDAP 4.1.0-M1 snapshots
See gh-49119

module/spring-boot-ldap/src/main/java/org/springframework/boot/ldap/health/LdapHealthIndicator.java
@@ -37,7 +37,7 @@ import org.springframework.util.Assert;
  */
 public class LdapHealthIndicator extends AbstractHealthIndicator {
 
-	private static final ContextExecutor<String> versionContextExecutor = new VersionContextExecutor();
+	private static final ContextExecutor<@Nullable String> versionContextExecutor = new VersionContextExecutor();
 
 	private final LdapOperations ldapOperations;
 
@@ -50,10 +50,12 @@ public class LdapHealthIndicator extends AbstractHealthIndicator {
 	@Override
 	protected void doHealthCheck(Health.Builder builder) throws Exception {
 		String version = this.ldapOperations.executeReadOnly(versionContextExecutor);
-		builder.up().withDetail("version", version);
+		if (version != null) {
+			builder.up().withDetail("version", version);
+		}
 	}
 
-	private static final class VersionContextExecutor implements ContextExecutor<String> {
+	private static final class VersionContextExecutor implements ContextExecutor<@Nullable String> {
 
 		@Override
 		public @Nullable String executeWithContext(DirContext ctx) throws NamingException {

module/spring-boot-ldap/src/main/java/org/springframework/boot/ldap/autoconfigure/embedded/EmbeddedLdapAutoConfiguration.java
@@ -244,9 +244,11 @@ public final class EmbeddedLdapAutoConfiguration implements DisposableBean {
 				EmbeddedLdapProperties embeddedProperties) {
 			LdapContextSource source = new LdapContextSource();
 			source.setBase(properties.getBase());
-			if (embeddedProperties.getCredential().isAvailable()) {
-				source.setUserDn(embeddedProperties.getCredential().getUsername());
-				source.setPassword(embeddedProperties.getCredential().getPassword());
+			String username = embeddedProperties.getCredential().getUsername();
+			String password = embeddedProperties.getCredential().getPassword();
+			if (StringUtils.hasText(username) && StringUtils.hasText(password)) {
+				source.setUserDn(username);
+				source.setPassword(password);
 			}
 			source.setUrls(properties.determineUrls(environment));
 			return source;

module/spring-boot-ldap/src/main/java/org/springframework/boot/ldap/autoconfigure/embedded/EmbeddedLdapProperties.java
@@ -24,7 +24,6 @@ import org.jspecify.annotations.Nullable;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.boot.convert.Delimiter;
 import org.springframework.core.io.Resource;
-import org.springframework.util.StringUtils;
 
 /**
  * Configuration properties for Embedded LDAP.
@@ -133,10 +132,6 @@ public class EmbeddedLdapProperties {
 
 		public void setPassword(@Nullable String password) {
 			this.password = password;
-		}
-
-		boolean isAvailable() {
-			return StringUtils.hasText(this.username) && StringUtils.hasText(this.password);
 		}
 
 	}

module/spring-boot-ldap/src/test/java/org/springframework/boot/ldap/autoconfigure/LdapAutoConfigurationTests.java
@@ -272,9 +272,7 @@ class LdapAutoConfigurationTests {
 		@Bean
 		@Primary
 		PooledContextSource pooledContextSource(LdapContextSource ldapContextSource) {
-			PooledContextSource pooledContextSource = new PooledContextSource(new PoolConfig());
-			pooledContextSource.setContextSource(ldapContextSource);
-			return pooledContextSource;
+			return new PooledContextSource(ldapContextSource, new PoolConfig());
 		}
 
 	}

platform/spring-boot-dependencies/build.gradle
@@ -2549,7 +2549,7 @@ bom {
 			releaseNotes("https://github.com/spring-projects/spring-kafka/releases/tag/v{version}")
 		}
 	}
-	library("Spring LDAP", "4.0.1") {
+	library("Spring LDAP", "4.1.0-SNAPSHOT") {
 		considerSnapshots()
 		group("org.springframework.ldap") {
 			modules = [

commit d5ace369bdff311f764be572ff1c2f92a5a62484

Fix LdapHealthIndicator reporting when LDAP version is unavailable
Fix regression caused by commit d26b6895 which caused
`doHealthCheck()` to only call `builder.up()` when the LDAP
server's protocol version (`java.naming.ldap.version`) could be
read from the JNDI environment.

See gh-51441

Signed-off-by: 2heunxun <seapeon@naver.com>

module/spring-boot-ldap/src/main/java/org/springframework/boot/ldap/health/LdapHealthIndicator.java
@@ -50,8 +50,9 @@ public class LdapHealthIndicator extends AbstractHealthIndicator {
 	@Override
 	protected void doHealthCheck(Health.Builder builder) throws Exception {
 		String version = this.ldapOperations.executeReadOnly(versionContextExecutor);
+		builder.up();
 		if (version != null) {
-			builder.up().withDetail("version", version);
+			builder.withDetail("version", version);
 		}
 	}
 

module/spring-boot-ldap/src/test/java/org/springframework/boot/ldap/health/LdapHealthIndicatorTests.java
@@ -51,6 +51,18 @@ class LdapHealthIndicatorTests {
 
 	@Test
 	@SuppressWarnings("unchecked")
+	void ldapIsUpWhenVersionIsUnavailable() {
+		LdapTemplate ldapTemplate = mock(LdapTemplate.class);
+		given(ldapTemplate.executeReadOnly((ContextExecutor<String>) any())).willReturn(null);
+		LdapHealthIndicator healthIndicator = new LdapHealthIndicator(ldapTemplate);
+		Health health = healthIndicator.health();
+		assertThat(health.getStatus()).isEqualTo(Status.UP);
+		assertThat(health.getDetails()).doesNotContainKey("version");
+		then(ldapTemplate).should().executeReadOnly((ContextExecutor<String>) any());
+	}
+
+	@Test
+	@SuppressWarnings("unchecked")
 	void ldapIsDown() {
 		LdapTemplate ldapTemplate = mock(LdapTemplate.class);
 		given(ldapTemplate.executeReadOnly((ContextExecutor<String>) any()))