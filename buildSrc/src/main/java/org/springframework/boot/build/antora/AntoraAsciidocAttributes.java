0eb25c7d3052f8312fc02be00a662e7b085c3a3a
Phillip Webb <phil.webb@broadcom.com>
2026-09-04 12:23:49 -0700

    Use module DSL for Jackson Antora attributes
    Replace custom attributes and processing logic with the module
    links DSL.
    
    See gh-51498

--- buildSrc/src/main/java/org/springframework/boot/build/antora/AntoraAsciidocAttributes.java
+++ buildSrc/src/main/java/org/springframework/boot/build/antora/AntoraAsciidocAttributes.java
@@ -114,11 +114,6 @@ public class AntoraAsciidocAttributes {
 		this.libraries.forEach((library) -> addVersionAttributes(attributes, library));
 		attributes.put("version-native-build-tools", (String) this.projectProperties.get("nativeBuildToolsVersion"));
 		attributes.put("version-graal", (String) this.projectProperties.get("graalVersion"));
-		addDependencyVersion(attributes, "jackson-annotations", "com.fasterxml.jackson.core:jackson-annotations");
-		addDependencyVersion(attributes, "jackson-core", "tools.jackson.core:jackson-core");
-		addDependencyVersion(attributes, "jackson-databind", "tools.jackson.core:jackson-databind");
-		addDependencyVersion(attributes, "jackson-dataformat-xml", "tools.jackson.dataformat:jackson-dataformat-xml");
-		addDependencyVersion(attributes, "jackson2-databind", "com.fasterxml.jackson.core:jackson-databind");
 		addDependencyVersion(attributes, "pulsar-client-api", "org.apache.pulsar:pulsar-client-api");
 	}
 
