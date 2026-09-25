a0df1831158f64267b271842c3dcae74bca9ecad
itaekyung <taeyun1411@gmail.com>
2026-09-25 18:53:43 +0900

    Enable nullability checking of spring-boot-jersey's tests
    See gh-51861
    
    Signed-off-by: itaekyung <taeyun1411@gmail.com>

--- module/spring-boot-jersey/src/test/java/org/springframework/boot/jersey/autoconfigure/JerseyAutoConfigurationCustomObjectMapperProviderTests.java
+++ module/spring-boot-jersey/src/test/java/org/springframework/boot/jersey/autoconfigure/JerseyAutoConfigurationCustomObjectMapperProviderTests.java
@@ -26,6 +26,7 @@ import jakarta.ws.rs.ApplicationPath;
 import jakarta.ws.rs.GET;
 import jakarta.ws.rs.Path;
 import org.glassfish.jersey.server.ResourceConfig;
+import org.jspecify.annotations.Nullable;
 import org.junit.jupiter.api.Test;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -89,9 +90,9 @@ class JerseyAutoConfigurationCustomObjectMapperProviderTests {
 
 		private String subject;
 
-		private String body;
+		private @Nullable String body;
 
-		Message(String subject, String body) {
+		Message(String subject, @Nullable String body) {
 			this.subject = subject;
 			this.body = body;
 		}
@@ -104,11 +105,11 @@ class JerseyAutoConfigurationCustomObjectMapperProviderTests {
 			this.subject = subject;
 		}
 
-		public String getBody() {
+		public @Nullable String getBody() {
 			return this.body;
 		}
 
-		public void setBody(String body) {
+		public void setBody(@Nullable String body) {
 			this.body = body;
 		}
 
